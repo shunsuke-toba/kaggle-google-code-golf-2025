69627624568e4fb72110bd7818334ca6e7004cb9
pech1 <pechi2327@gmail.com>
2025-08-14 01:26:32 +0900

    304:olution, plain, submissionの更新

--- 304/3_submission.py
+++ 304/3_submission.py
@@ -1,5 +1,1 @@
-def p(j,A=range(9),c=range(3)):
- E,k=__import__('collections').Counter(j[0]+j[1]+j[2]).most_common(1)[0][0],[[0 for _ in A]for _ in A]
- for W,l in[(W,l)for l in c for W in c if j[W][l]==E]:
-  for J in A:k[3*W+J%3][3*l+J//3]=j[J%3][J//3]
- return k
+p=lambda g,r=range:[[(g[i//3][j//3]==max(r(10),key=sum(g,[]).count))*g[i%3][j%3]for j in r(9)]for i in r(9)]
