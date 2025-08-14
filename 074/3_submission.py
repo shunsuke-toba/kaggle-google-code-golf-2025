d201af49383a3ff97bcee5d75c9cb1df6f339a90
sak74y <yusei.sakurai@third-inc.co.jp>
2025-08-14 18:49:57 +0900

    074:軽微な修正

--- 074/3_submission.py
+++ 074/3_submission.py
@@ -2,6 +2,5 @@ def p(g,A=range):
  E=[[g[r][c]if r<30and c<30else 9for c in A(32)]for r in A(32)]
  for _ in A(2):
   for r in A(32):
-   for k in A(32):
-    E[r][k]=min(E[r][k],E[31-r][k],E[k][r])
+   for c in A(32):E[r][c]=min(E[r][c],E[31-r][c],E[c][r])
  return[E[r][:30]for r in A(30)]
