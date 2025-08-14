8b9d5552433f0ce554975610b3ad5f221976f72a
sak74y <yusei.sakurai@third-inc.co.jp>
2025-08-14 16:38:52 +0900

    002:改善

--- 002/3_submission.py
+++ 002/3_submission.py
@@ -1,10 +1,8 @@
 def p(j):
- A=range;c=len(j);R=A(c)
- for r in j:r[:]=[x or 4 for x in r]
- d=1,0,-1,0,1
- for z in A(64*c*c):
-  r,q,i=z%c,z//c%c,z//c//c%4
-  if j[r][q]==4:
-   x,y=r+d[i],q+d[i+1]
-   if not(0<=x<c and 0<=y<c and j[x][y]!=0):j[r][q]=0
+ for r in j:r[:]=[x or 4for x in r]
+ for _ in range(64):
+  for i,r in enumerate(j):
+   for k in range(len(r)):
+    if r[k]==4and(i<1or j[i-1][k]<1):r[k]=0
+  j[:]=[list(r)for r in zip(*j[::-1])]
  return j
