f2b26e60dab229b1001743e32d070597ea947a5d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-16 15:42:43 +0900

    問題005: コードゴルフ最適化

--- 005/3_submission.py
+++ 005/3_submission.py
@@ -1,16 +1,16 @@
 def p(g):
- h=len(g);w=len(g[0]);R=range(3);r=c=0
- while not all(map(any,(P:=[g[r+i][c:c+3]for i in R])+list(zip(*P)))):
-  c+=1
-  if c>w-3:c=0;r+=1
- for Y in-1,0,1:
-  for X in-1,0,1:
-   if X|Y and-1<(y:=r+4*Y)<h-2and-1<(x:=c+4*X)<w-2:
-    t=max(g[y+i][x+j]for i in R for j in R);s=0
+ H=len(g)-2;W=len(g[0])-2;R=range(3);r=c=0
+ while 0 in map(sum,(P:=[g[r+i][c:c+3]for i in R])+[*zip(*P)]):
+  c+=1;r+=c//W;c%=W
+ for Y in-4,0,4:
+  for X in-4,0,4:
+   y=r+Y;x=c+X
+   if X|Y and-1<y<H and-1<x<W:
+    t=max(max(g[y+i][x:x+3])for i in R);yy=r;xx=c
     while t:
-     s+=1;yy=r+4*Y*s;xx=c+4*X*s
+     yy+=Y;xx+=X
      for i in R:
       for j in R:
-       if 0<=yy+i<h and 0<=xx+j<w and P[i][j]:g[yy+i][xx+j]=t
-     if not(-1<yy<h-2and-1<xx<w-2):break
+       if P[i][j]and-1<yy+i<H+2and-1<xx+j<W+2:g[yy+i][xx+j]=t
+     if(-1<yy<H)*(-1<xx<W)<1:break
  return g
