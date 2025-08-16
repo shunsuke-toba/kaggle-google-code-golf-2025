9943791c94d62c009af12777ead0b9c264a22d7c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 08:59:36 +0900

    問題71: コードゴルフ解答の最適化

--- 071/3_submission.py
+++ 071/3_submission.py
@@ -1,24 +1,22 @@
 def p(g):
- h,w=len(g),len(g[0]);C=set(sum(g,[]))-{0};P=B=0
- for c in C:
-  L=[(i,j)for i in range(h)for j in range(w)if g[i][j]==c]
-  if L:
-   a,b,d,e=min(x[0]for x in L),min(x[1]for x in L),max(x[0]for x in L),max(x[1]for x in L)
-   if all(g[r][x]==c for r in range(a,-~d)for x in range(b,-~e)):B=(a,b,d,e)
-   else:P=c
- if not P:return g
- O=[[P*(g[i][j]==P)for j in range(w)]for i in range(h)]
+ b={};h=len(g);w=len(g[0])
+ for y,r in enumerate(g):
+  for x,v in enumerate(r):
+   if v:
+    t=b.setdefault(v,[y,x,y,x]);y<t[0]and t.__setitem__(0,y);x<t[1]and t.__setitem__(1,x);y>t[2]and t.__setitem__(2,y);x>t[3]and t.__setitem__(3,x)
+ P=B=None
+ for c,(a,l,d,r) in b.items():
+  if all(g[i][j]==c for i in range(a,d+1)for j in range(l,r+1)):B=(c,a,l,d,r)
+  else:P=c
+ s=b[P][1]+b[P][3]
  if B:
-  a,b,d,e=B
-  for i in range(h):
-   if i<a or i>d:
-    L=[j for j in range(w)if g[i][j]==P]
-    if L:
-     m=(min(L)+max(L))/2
-     for k in range(h*w):r,c=k//w,k%w;O[r][c]==P and 0<=int(2*m-c)<w and exec("O[r][int(2*m-c)]=P")
-     return O
- L=[j for k in range(h*w)if g[k//w][k%w]==P for j in[k%w]]
- if L:
-  m=(min(L)+max(L))/2
-  for k in range(h*w):i,j=k//w,k%w;O[i][j]==P and 0<=int(2*m-j)<w and exec("O[i][int(2*m-j)]=P")
- return O
+  c,a,l,d,r=B
+  for y in range(h):
+   if y<a or y>d:
+    L=[j for j,v in enumerate(g[y])if v==P]
+    if L:s=min(L)+max(L);break
+ for y,r in enumerate(g):
+  for x,v in enumerate(r):
+   if v and v!=P:
+    j=s-x;r[x]=0<=j<w and g[y][j]
+ return g
