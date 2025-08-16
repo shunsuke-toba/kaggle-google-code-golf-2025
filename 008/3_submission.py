42fa4f559ce87c5de97952783ec7c41288426214
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-16 15:44:18 +0900

    問題008: コードゴルフ最適化

--- 008/3_submission.py
+++ 008/3_submission.py
@@ -1,9 +1,13 @@
-def p(j,A=enumerate):
-	c,E=[(c,b)for(c,E)in A(j)for(b,d)in A(E)if d==2],[(c,b)for(c,E)in A(j)for(b,d)in A(E)if d==8]
-	if not c or not E:return j
-	k=lambda W:(min(c for(c,E)in W),max(c for(c,E)in W),min(c for(E,c)in W),max(c for(E,c)in W));l,J,a,C=k(c);e,K,w,L=k(E);b=d=0
-	if C<w:d=w-C-1
-	elif L<a:d=L-a+1
-	elif J<e:b=e-J-1
-	elif K<l:b=K-l+1
-	f,g={*c},{*E};return[[8 if(c,E)in g else 2 if(c-b,E-d)in f else 0 for(E,k)in A(j[0])]for(c,E)in A(j)]
+def p(G,E=enumerate):
+ R=[];a=b=c=d=99;e=f=g=h=0;m=min;M=max
+ for i,r in E(G):
+  for j,v in E(r):
+   if v==2:R+=[(i,j)];a=m(a,i);e=M(e,i);b=m(b,j);f=M(f,j)
+   elif v==8:c=m(c,i);g=M(g,i);d=m(d,j);h=M(h,j)
+ y=x=0
+ if e<c:y=c-e-1
+ elif g<a:y=g-a+1
+ else:x=d-f-1 if f<d else h-b+1
+ for i,j in R:G[i][j]=0
+ for i,j in R:G[i+y][j+x]=2
+ return G
