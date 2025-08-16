a99cac6b1c3fecec619aa949824274b3a6d3cef2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 01:03:29 +0900

    問題48: セル間の連結パターンと条件付き変換の実装

--- 048/3_submission.py
+++ 048/3_submission.py
@@ -1,18 +1,9 @@
-def f(j,A,c):
-	global W;l.append((j,A))
-	for E in C(j-1,j+2):
-		for k in C(A-1,A+2):
-			if(E,k)in l:continue
-			l.append((E,k))
-			if E<0 or E>=J or k<0 or k>=a or(E,k)in[(K,L),(K+1,L),(K,L+1),(K+1,L+1)]:continue
-			if c[E][k]==2:W=8
-			if c[E][k]==8:f(E,k,c)
-def p(c):
-	global W,l,K,L,J,a,C;W,l,J,a,C,e=0,[],len(c),len(c[0]),range,enumerate
-	for(K,w)in e(c):
-		for(L,b)in e(w):
-			if b==2:
-				for E in C(K-1,K+3):
-					for k in C(L-1,L+3):
-						if E>=0 and E<J and k>=0 and k<a and c[E][k]==8:f(E,k,c)
-				return[[W]]
+def p(g):
+ h=len(g);w=len(g[0]);k=sum(g,[]).index(2);i=k//w;j=k%w;q=[(i,j)]
+ for x,y in q:
+  if g[x][y]==2 and (abs(x-i)>2 or abs(y-j)>2):return[[8]]
+  g[x][y]=0
+  for a,b in((1,0),(0,1),(-1,0),(0,-1)):
+   nx,ny=x+a,y+b
+   if 0<=nx<h and 0<=ny<w and g[nx][ny]:q+=[(nx,ny)]
+ return[[0]]
