48e496fd64ade0c77a70f08797e92b6d4d6943e0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 01:04:18 +0900

    問題50: グリッドセルの変換と配置ロジックの実装

--- 050/3_submission.py
+++ 050/3_submission.py
@@ -1,11 +1,7 @@
-def p(j):
-	A=range;c=[e[:]for e in j];E,k=len(j),len(j[0]);W=[(e,l)for e in A(E)for l in A(k)if j[e][l]==8]
-	for(l,J)in W:
-		for(a,C)in[(0,1),(1,0),(0,-1),(-1,0)]:
-			e=1
-			while 0<=l+e*a<E and 0<=J+e*C<k:
-				if j[l+e*a][J+e*C]==8:
-					for K in A(1,e):c[l+K*a][J+K*C]=3
-					break
-				e+=1
-	return c
+def p(g):
+ for _ in g,g:
+  for r in g:
+   t=[i for i,x in enumerate(r)if x>7]
+   for a,b in zip(t,t[1:]):r[a+1:b]=[3]*(b+~a)
+  g=list(map(list,zip(*g)))
+ return g
