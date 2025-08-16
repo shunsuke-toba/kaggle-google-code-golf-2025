30559630a45742d0163b74abf6b2fdbea458c0c1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-16 15:41:40 +0900

    問題002: コードゴルフ最適化 - 182バイトから163バイトに削減

--- 002/3_submission.py
+++ 002/3_submission.py
@@ -1,10 +1,7 @@
 def p(j):
- A=range;c=len(j);R=A(c)
- for r in j:r[:]=[x or 4 for x in r]
+ c=len(j);j=[[x or 4 for x in r]for r in j]
  d=1,0,-1,0,1
- for z in A(64*c*c):
+ for z in range(c**4):
   r,q,i=z%c,z//c%c,z//c//c%4
-  if j[r][q]==4:
-   x,y=r+d[i],q+d[i+1]
-   if not(0<=x<c and 0<=y<c and j[x][y]!=0):j[r][q]=0
+  if j[r][q]>3:x,y=r+d[i],q+d[i+1];j[r][q]*=0<=x<c>y>=0<j[x][y]
  return j
