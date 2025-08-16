5883ec6e0c49d28960383b9091eaa941adbf87bf
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 00:17:40 +0900

    問題34: グリッドパターン変換アルゴリズムの実装

--- 034/3_submission.py
+++ 034/3_submission.py
@@ -1,14 +1,16 @@
 def p(g):
- g=[r[:]for r in g];h,w=len(g),len(g[0])
- for y in range(h-1):
-  for x in range(w-1):
-   if all(g[y+i][x+j]for i in(0,1)for j in(0,1)):
-    v=[g[y+i][x+j]for i in(0,1)for j in(0,1)]
-    if 2 in v:
-     c=[i for i in v if i-2][0]
-     for k in range(4):
-      if v[k]==2:
-       i,j=y+k//2,x+k%2;a,b=k//2*2-1,k%2*2-1
-       while-1<i<h and-1<j<w:g[i][j]=c;-1<i+a<h and exec('g[i+a][j]=c');-1<j+b<w and exec('g[i][j+b]=c');i+=a;j+=b
-     return g
- return g
+ for i in range(81):
+  r=i//9;c=i%9;a=g[r][c]
+  if a:
+   t=a,g[r][c+1],g[r+1][c],g[r+1][c+1];o=r,c
+   for x in t:
+    if x-2:break
+   for j in 0,1,2,3:
+    if t[j]==2:
+     r,c=o
+     while 9>r>-2<c<9:
+      for k in 0,1,2,3:
+       u=r+k//2;v=c+k%2
+       if 9>u>-1<v<9:g[u][v]=x
+      r+=j//2*2-1;c+=j%2*2-1
+   return g
