ead7fb78bc2828d0e60654e140c07afb9a54a937
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 02:56:07 +0900

    問題70: コードゴルフ解答の最適化

--- 070/3_submission.py
+++ 070/3_submission.py
@@ -1,8 +1,5 @@
-def p(j):
-	A=range;c=[E[:]for E in j];E=[(E,c)for E in A(len(j))for c in A(len(j[0]))if j[E][c]==8]
-	if E:
-		k,W=min(E for(E,A)in E),max(E for(E,A)in E);l,J=min(E for(A,E)in E),max(E for(A,E)in E)
-		for a in A(k,W+1):
-			for C in A(l,J+1):
-				if j[a][C]==1:c[a][C]=3
-	return c
+def p(g,e=enumerate):
+ y=[i for i,r in e(g)if 8 in r];x=[i for i,c in e(zip(*g))if 8 in c]
+ for i in range(y[0],-~y[-1]):
+  for j in range(x[0],-~x[-1]):g[i][j]+=2*(g[i][j]==1)
+ return g
