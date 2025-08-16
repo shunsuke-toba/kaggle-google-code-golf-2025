b9f1b7dc12f1a4ff674e9477ca493ea208757212
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-16 22:46:19 +0900

    問題31: パターン完成アルゴリズムの実装

--- 031/3_submission.py
+++ 031/3_submission.py
@@ -1,1 +1,2 @@
-def p(j,A=enumerate):c,E=zip(*[(i,j)for i,r in A(j)for j,x in A(r)if x]);return[r[min(E):max(E)+1]for r in j[min(c):max(c)+1]]
+from numpy import*
+def p(g):g=array(g);a,b=where(g);return g[a.min():a.max()+1,b.min():b.max()+1].tolist()
