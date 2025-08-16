064fe529389fb89e1e69d99661ca7f8d6f016056
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-16 12:46:10 +0900

    問題005: 3x3パターンの8方向への繰り返しコピー処理をコードゴルフ最適化

--- 005/3_submission.py
+++ 005/3_submission.py
@@ -1,27 +1,16 @@
 def p(g):
- R,C=len(g),len(g[0])
- r=c=-1
- for i in range(R):
-  for j in range(C):
-   if i+2<R and j+2<C:
-    if all(any(g[i+x][j+y]for y in range(3))for x in range(3))and all(any(g[i+x][j+y]for x in range(3))for y in range(3)):
-     r,c=i,j;break
-  if r!=-1:break
- for dr,dc in[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]:
-  x,y=r+dr*4,c+dc*4
-  if 0<=x<R-2 and 0<=y<C-2:
-   col=-1
-   for i in range(3):
-    for j in range(3):
-     if g[x+i][y+j]:col=g[x+i][y+j];break
-    if col!=-1:break
-   if col!=-1:
-    s=1
-    while 1:
-     tx,ty=r+dr*4*s,c+dc*4*s
-     for i in range(3):
-      for j in range(3):
-       if 0<=tx+i<R and 0<=ty+j<C and g[r+i][c+j]:g[tx+i][ty+j]=col
-     if tx<0 or tx+2>=R or ty<0 or ty+2>=C:break
-     s+=1
+ h=len(g);w=len(g[0]);R=range(3);r=c=0
+ while not all(map(any,(P:=[g[r+i][c:c+3]for i in R])+list(zip(*P)))):
+  c+=1
+  if c>w-3:c=0;r+=1
+ for Y in-1,0,1:
+  for X in-1,0,1:
+   if X|Y and-1<(y:=r+4*Y)<h-2and-1<(x:=c+4*X)<w-2:
+    t=max(g[y+i][x+j]for i in R for j in R);s=0
+    while t:
+     s+=1;yy=r+4*Y*s;xx=c+4*X*s
+     for i in R:
+      for j in R:
+       if 0<=yy+i<h and 0<=xx+j<w and P[i][j]:g[yy+i][xx+j]=t
+     if not(-1<yy<h-2and-1<xx<w-2):break
  return g
