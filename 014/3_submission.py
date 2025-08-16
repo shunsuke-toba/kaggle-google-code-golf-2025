0ef53205f08a299ecf4533f00efaa14f5ac3746b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-16 16:34:48 +0900

    問題014: コードゴルフ最適化

--- 014/3_submission.py
+++ 014/3_submission.py
@@ -1,7 +1,6 @@
 from collections import*
-def p(j):
- A=[x for k in j for x in k];c=Counter(A).most_common(3);c=[c for c in c if c[0]>0][-1][0];j=[k for k in j if c in k];E=[]
- for k in j:
-  for W in range(len(k)):
-   if k[W]==c:E+=[W]
- return[k[min(E):max(E)+1]for k in j]
+def p(g):
+ c=min((v,k)for k,v in Counter(sum(g,[])).items()if k)
+ g=[r for r in g if c[1]in r]
+ e=[i for r in g for i,x in enumerate(r)if x==c[1]]
+ return[r[min(e):max(e)+1]for r in g]
