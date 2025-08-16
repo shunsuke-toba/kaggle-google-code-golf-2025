3bbd20584cc59f3b9c3863f2ba7f36ff13e3d4f6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 02:53:00 +0900

    問題63: コードゴルフ解答の最適化

--- 063/3_submission.py
+++ 063/3_submission.py
@@ -1,12 +1,3 @@
-def p(j):
- A=range
- c=len(j)
- E=[o[:]for o in j]
- for k in range(c):
-  if j[1][k]==0and j[c-2][k]==0and sum(j[W][k]for W in A(1,c-1))==0:
-   for W in A(1,c-1):E[W][k]=3
- for W in range(c):
-  if j[W][1]==0and j[W][c-2]==0and sum(j[W][k]for k in A(1,c-1))==0:
-   for k in A(1,c-1):
-    if E[W][k]==0:E[W][k]=3
- return E
+def p(g):
+ r=range;n=len(g)
+ return[[[g[i][j],3][0<i<n-1and 0<j<n-1and(all(g[y][j]%3<1for y in r(1,n-1))or all(x%3<1for x in g[i][1:-1]))]for j in r(n)]for i in r(n)]
