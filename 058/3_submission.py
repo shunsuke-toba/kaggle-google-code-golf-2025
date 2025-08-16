2e87f29db1171c6d40c8d931058b544dbb8f3c13
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 01:57:14 +0900

    問題58: グリッドパターンの変換と配置ロジックの実装

--- 058/3_submission.py
+++ 058/3_submission.py
@@ -1,16 +1,9 @@
-def p(j):
-	A=range;c=len(j);E=[[0]*c for W in A(c)];k,W=0,0;l=[(0,1),(1,0),(0,-1),(-1,0)]
-	for J in A(c):
-		E[k][W]=3
-		if J<c-1:W+=1
-	a=c-1;C=1
-	while a>0:
-		for e in A(2):
-			if a>0:
-				k,W=k+l[C][0],W+l[C][1]
-				for J in A(a):
-					E[k][W]=3
-					if J<a-1:k,W=k+l[C][0],W+l[C][1]
-				C=(C+1)%4
-		a-=2
-	return E
+def p(g):
+ n=len(g);o=[[0]*n for _ in g];o[0]=[3]*n;x=n-1;y=0;u=(1,0,-1,0);a=n-1;i=1
+ while a>0:
+  for _ in 0,1:
+   dx=u[i];dy=u[i-1]
+   for _ in[0]*a:y+=dy;x+=dx;o[y][x]=3
+   i=(i+1)%4
+  a-=2
+ return o
