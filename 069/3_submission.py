b1c343206140bddb982976e503c20e341f00b25a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 02:55:40 +0900

    問題69: コードゴルフ解答の最適化

--- 069/3_submission.py
+++ 069/3_submission.py
@@ -1,21 +1,16 @@
 def p(g):
- h,w=len(g),len(g[0]);V=[[0]*w for _ in range(h)];C=[]
- def D(i,j,c,s):
-  if i<0 or i>=h or j<0 or j>=w or V[i][j]or g[i][j]<1:return
-  V[i][j]=1;c+=[(i,j)];s.add(g[i][j])
-  for a,b in[(0,1),(0,-1),(1,0),(-1,0)]:D(i+a,j+b,c,s)
- for i in range(h):
-  for j in range(w):
-   if V[i][j]<1 and g[i][j]:c=[];s=set();D(i,j,c,s);C+=[(c,s)]if c else[]
- T=0;S=[]
- for c,s in C:
-  if len(s)>1:T=c
-  else:S+=[c]
- if not T:return g
- m=min(x[0]for x in T);n=min(x[1]for x in T);P={(i-m,j-n):g[i][j]for i,j in T}
- R=[[0]*w for _ in range(h)]
- for c in S:
-  a=min(x[0]for x in c);b=min(x[1]for x in c)
-  for(d,e),v in P.items():
-   if 0<=a+d<h and 0<=b+e<w:R[a+d][b+e]=v
- return R
+ s={(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v};t=[];d=1,0,-1,0,1
+ while s:
+  q=[s.pop()];L=[];C=set();a=b=99
+  while q:
+   i,j=q.pop();L+=[(i,j)];C|={g[i][j]};a=min(a,i);b=min(b,j)
+   for k in range(4):
+    u=i+d[k];v=j+d[k+1]
+    if(u,v)in s:s.remove((u,v));q+=[(u,v)]
+  if len(C)>1:P=L;A,B=a,b
+  else:t+=[(a,b)]
+ O=[]
+ for i,j in P:O+=((i-A,j-B,g[i][j]),);g[i][j]=0
+ for a,b in t:
+  for x,y,v in O:g[a+x][b+y]=v
+ return g
