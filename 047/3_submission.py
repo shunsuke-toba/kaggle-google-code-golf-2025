0820505b9faa20c9760bbdfecbc7df38d04ff8b0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 01:03:06 +0900

    問題47: グリッドパターンの抽出と変換ロジックの実装

--- 047/3_submission.py
+++ 047/3_submission.py
@@ -1,5 +1,1 @@
-def p(j):
-	A=range;c=[[0]*9 for c in A(9)];E=[(c,E,j[c][E])for c in A(9)for E in A(9)if j[c][E]]
-	for(k,W,l)in E:
-		for J in range(9):c[k][J]=c[J][W]=l
-	c[E[0][0]][E[1][1]]=c[E[1][0]][E[0][1]]=2;return c
+p=lambda g:(r:=[{*a}-{0}for a in g],c:=[{*a}-{0}for a in zip(*g)],[[(s:=r[i]|c[j])and[2,sum(s)][len(s)<2]or 0 for j in range(len(c))]for i in range(len(r))])[2]
