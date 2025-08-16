63b65959bda014d31d8b84d3d863c6d041ce62e7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 01:02:35 +0900

    問題45: グリッドセルの条件付き変換と配置の実装

--- 045/3_submission.py
+++ 045/3_submission.py
@@ -1,7 +1,1 @@
-def p(j):
-	for A in j:
-		for c in{*A}-{0}:
-			E=A.index(c);k=len(A)-A[::-1].index(c)
-			for W in range(E,k):
-				if~A[W]:A[W]=c
-	return j
+p=lambda g:[[c]*10 if(c:=r[0])==r[9]>0 else r for r in g]
