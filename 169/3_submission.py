3d7301097696e599a26f221257bab224afa42c5a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 23:04:31 +0900

    問題169: 3x3グリッド変換の最適化実装

--- 169/3_submission.py
+++ 169/3_submission.py
@@ -1,14 +1,12 @@
-def p(j):
-	A=range;c=[L[:]for L in j];E=set();k=[(0,1),(1,0),(0,-1),(-1,0)]
-	for W in A(10):
-		for l in A(10):
-			if j[W][l]==5 and(W,l)not in E:
-				J,a=set(),[(W,l)];J.add((W,l));E.add((W,l))
-				while a:
-					C,e=a.pop(0)
-					for(K,w)in k:
-						L,b=C+K,e+w
-						if 0<=L<10 and 0<=b<10 and j[L][b]==5 and(L,b)not in J:J.add((L,b));E.add((L,b));a.append((L,b))
-				d=5-len(J)
-				for(C,e)in J:c[C][e]=d
-	return c
+def p(g):
+ d=1,0,-1,0,1;k=100
+ while k:
+  k-=1;i=k//10;j=k%10
+  if g[i][j]==5:
+   q=[(i,j)];g[i][j]=0
+   for i,j in q:
+    for n in 0,1,2,3:
+     x,y=i+d[n],j+d[n+1]
+     if 0<=x<10>y>=0<g[x][y]==5:g[x][y]=0;q+=[(x,y)]
+   for i,j in q:g[i][j]=5-len(q)
+ return g
