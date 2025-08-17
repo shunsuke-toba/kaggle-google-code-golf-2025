56291a73b210594a764b690137d110e569f0889b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 11:50:17 +0900

    問題104: コードゴルフ解答の最適化

--- 104/3_submission.py
+++ 104/3_submission.py
@@ -1,8 +1,4 @@
 def p(g):
- d='[{"Ip3bl,2q]e"Op3swwwxxxxamg]]},{"Izch,2bhd]e"OzggAAyyyy]]},{"Izdh,2bk]e"OzgiyyyAAmg]]},{"Izcj,2lba]e"Ozggktxxxwwwwa]]}]'
- m='vvAp0zmiyqtxlswugvhiuaitrgsdbrakqo[pn[o":nhgmajlhckf3jddif0hccge[f],ebbdaac,3b,0a'
- m=[[m[i:i+2],m[i+2]]for i in range(0,len(m),3)]
- for r in m:d=d.replace(r[1],r[0])
- d=eval(d)
- for k in d:
-  if k['I']==g:g=k['O'];return g
+ r=not any(g[0]);c=not any(r[0]for r in g);d=r==c;o=[[0]*9 for _ in g*3];p=c+4*(d^1);q=c+4*d
+ for i in range(4):o[r+i][p:p+4]=o[r+4+i][q:q+4]=[3]*4
+ return o
