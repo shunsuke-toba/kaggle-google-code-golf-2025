354b6ad70524612c832f345a5f8852d19070e181
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 15:37:49 +0900

    問題124: コードゴルフ解答の修正

--- 124/3_submission.py
+++ 124/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
  h=len(g);w=len(g[0])
- s=lambda r,d:[0 if j-d<0 or j-d>=w else r[j-d]for j in range(w)]
+ s=lambda r,d:(w*[0]+r+w*[0])[w-d:][:w]
  for k in range(1,h+1):
   for d in range(-w,w+1):
-   if all(s(g[i-k],d)==g[i]for i in range(k,h)):
-    while len(g)<10:g.append(s(g[-k],d))
+   if g[k:]==[s(r,d)for r in g[:h-k]]:
+    while len(g)<10:g+=s(g[-k],d),
     return g
