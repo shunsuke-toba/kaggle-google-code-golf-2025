186076add23836ca03681bf24bf25b543c2b6601
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 09:02:58 +0900

    問題80: コードゴルフ解答の最適化

--- 080/3_submission.py
+++ 080/3_submission.py
@@ -1,24 +1,19 @@
 def p(g):
- R=range;h=len(g);w=len(g[0])
- for n,r in enumerate(g):
-  if r==r[:1]*w:l=r[0];break
- s=n+1;m=(h+1)//s;A=[[g[i*s][j*s]for j in R(m)]for i in R(m)]
- b=4
+ R=range;h=len(g);n=0
+ while (r:=g[n])!=r[:1]*len(r):n+=1
+ s=n+1;m=-~h//s;A=[g[i*s][::s][:m]for i in R(m)];b=4
  for y in R(m-2):
   for x in R(m-2):
-   t=[A[y+i][x:x+3]for i in R(3)];u=sum(v>0 for r in t for v in r)
-   if u>b:b=u;P=t;Y=y+1;X=x+1
- d=P[1][1]
- for y in R(m):
-  for x in R(m):
-   if A[y][x]==d and(y,x)!=(Y,X):
-    for I in(-1,0,1):
-     for J in(-1,0,1):
-      u=y+I;v=x+J
-      if m>u>=0<=v<m:A[u][v]=P[I+1][J+1]
- o=[[l]*w for _ in R(h)]
- for y in R(m):
-  for x in R(m):
-   v=A[y][x]
-   for i in R(n):o[y*s+i][x*s:x*s+n]=[v]*n
+   t=[r[x:x+3]for r in A[y:y+3]];u=sum(map(bool,sum(t,[])))
+   if u>b:b=u;P=t;c=y+1,x+1
+ for y in R(m*m):
+  x=y%m;y//=m
+  if A[y][x]==P[1][1] and(y,x)!=c:
+   for k in R(9):
+    u=y+k//3-1;v=x+k%3-1
+    if m>u>=0<=v<m:A[u][v]=P[k//3][k%3]
+ o=[r[:1]*len(r) for _ in R(h)]
+ for y in R(m*m):
+  x=y%m;y//=m
+  for i in R(n):o[y*s+i][x*s:x*s+n]=[A[y][x]]*n
  return o
