f146508ea711f5a5fd8e5ef18fca7808fd56b45b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 17:45:40 +0900

    問題138: コードゴルフ解答の実装

--- 138/3_submission.py
+++ 138/3_submission.py
@@ -1,14 +1,6 @@
 def p(g):
- h=len(g);w=len(g[0]);R=range(1,10)
- a=[i for i,c in enumerate(zip(*g))if max(map(c.count,R))>=h-2]
- b=[i for i,r in enumerate(g)if max(map(r.count,R))>=w-2]
- g=[r[a[0]:a[1]+1]for r in g[b[0]:b[1]+1]]
- h=len(g);w=len(g[0]);D={g[0][1]:(-1,0),g[-1][1]:(1,0),g[1][0]:(0,-1),g[1][-1]:(0,1)}
- for y in range(1,h-1):
-  for x in range(1,w-1):
-   d=D.get(c:=g[y][x])
-   if d:
-    i,j=y+d[0],x+d[1]
-    while 0<=i<h and 0<=j<w:
-     g[i][j]=c;i+=d[0];j+=d[1]
+ a=[i for i,c in enumerate(zip(*g))if max(map(c.count,R:=range(1,10)))>len(g)-3];b=[i for i,r in enumerate(g)if max(map(r.count,R))>len(g[0])-3];g=[r[a[0]:a[1]+1]for r in g[b[0]:b[1]+1]];h=len(g);w=len(g[0]);W=w-2;D={g[0][1]:(-1,0),g[-1][1]:(1,0),g[1][0]:(0,-1),g[1][-1]:(0,1)}
+ for k in range((h-2)*W):
+  i,j=k//W+1,k%W+1
+  while(d:=D.get(c:=g[i][j]))and h>(i:=i+d[0])>=0<=(j:=j+d[1])<w:g[i][j]=c
  return g
