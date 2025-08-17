c9384feef7d334a78c8865bda951a4d4dc4acd9d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 15:37:29 +0900

    問題123: コードゴルフ解答の修正

--- 123/3_submission.py
+++ 123/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):c=[*filter(None,g[0])];m=len(c);return[[c[max(i,j)%m]for j in range(10)]for i in range(10)]
+def p(g):c=[*filter(None,g[0])];r=range(10);return[[c[max(i,j)%len(c)]for j in r]for i in r]
