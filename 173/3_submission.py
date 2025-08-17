c7299bc20f3d3cea044b95d6b2c6d905eda0378b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 23:43:46 +0900

    問題173: 3x3サブグリッド座標変換の最適化実装

--- 173/3_submission.py
+++ 173/3_submission.py
@@ -1,23 +1,12 @@
 def p(g):
- w=len(g[0]);W=w+2;g=[[0]*W]+[[0,*r,0]for r in g]+[[0]*W];h=len(g);m=w*(h-2);S=(A:=((1,0),(-1,0),(0,1),(0,-1)),((1,1),(1,-1),(-1,1),(-1,-1)),A[:2],A[2:]);T=[]
- for i in(R:=range(m)):
-  r=i//w+1;c=i%w+1
-  if v:=g[r][c]:
-   for d in S:
-    a=0
-    for x,y in d:
-     t=g[r+x][c+y]
-     if t in(0,v)or a^t and a:break
-     a=t
-    else:T+=[(d,a,v)]
+ W=(w:=len(g[0]))+2;G=[0]*W+sum(([0,*r,0]for r in g),[])+[0]*W;P=[W+1+i+i//w*2 for i in range(w*len(g))];T=[(d,a,b)for j in P if (b:=G[j]) for d in((1,-1,W,-W),(W+1,W-1,-W+1,-W-1),(1,-1),(W,-W)) if (a:=G[j+d[0]]) and a-b and all(G[j+x]==a for x in d[1:])]
  z=1
  while z:
   z=0
-  for d,a,b in T:
-   for i in R:
-    r=i//w+1;c=i%w+1;t=g[r][c]
-    if t==b:
-     for x,y in d:
-      if g[r+x][c+y]<1:g[r+x][c+y]=a;z=1
-    elif t<1 and all(g[r+x][c+y]==a for x,y in d):g[r][c]=b;z=1
- return[r[1:-1]for r in g[1:-1]]
+  for j in P:
+   for d,a,b in T:
+    if G[j]==b:
+     for x in d:
+      if G[j+x]<1:G[j+x]=a;z=1
+    elif G[j]<1 and all(G[j+x]==a for x in d):G[j]=b;z=1
+ return[G[j:j+w]for j in P[::w]]
