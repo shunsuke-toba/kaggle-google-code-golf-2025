dc0bb09fa9c3cdf86322cadb0bb0e70a83a21608
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 11:49:31 +0900

    問題102: コードゴルフ解答の最適化

--- 102/3_submission.py
+++ 102/3_submission.py
@@ -1,15 +1,8 @@
 def p(g):
- H=len(g);W=len(g[0]);V=set();z=[r[:]for r in g]
- for i in range(H*W):
-  y,x=divmod(i,W)
-  if g[y][x]==0and(y,x)not in V:
-   Q=[(y,x)];B={(y,x)};V|={(y,x)}
-   while Q:
-    r,c=Q.pop(0)
-    for d,e in(0,1),(1,0),(0,-1),(-1,0):
-     n,m=r+d,c+e
-     if H>n>-1<W>m>-1and(n,m)not in V and g[n][m]==0:V|={(n,m)};Q+=[(n,m)];B|={(n,m)}
-   Y=[b[0]for b in B];X=[b[1]for b in B];a=min(Y);b=max(Y);c=min(X);d=max(X);h=b-a+1
-   if h==d-c+1==len(B)**.5and(a<1or all(g[a-1][x]==5for x in range(c,d+1)))and(b>=H-1or all(g[b+1][x]==5for x in range(c,d+1)))and(c<1or all(g[y][c-1]==5for y in range(a,b+1)))and(d>=W-1or all(g[y][d+1]==5for y in range(a,b+1))):
-    for y,x in B:z[y][x]=2
- return z
+ h,w,r=len(g),len(g[0]),range
+ for s in r(min(h,w),0,-1):
+  for i in r(1,h-s):
+   for j in r(1,w-s):
+    if all(g[i-1][k]==g[i+s][k]==5 for k in r(j,j+s))and all(g[k][j-1]==g[k][j+s]==5>max(g[k][j:j+s])+4 for k in r(i,i+s)):
+     for v in g[i:i+s]:v[j:j+s]=[2]*s
+ return g
