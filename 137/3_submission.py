cfc52018ca30cef54d018bc1558d6411d5e3eb52
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 17:45:20 +0900

    問題137: コードゴルフ解答の実装

--- 137/3_submission.py
+++ 137/3_submission.py
@@ -1,14 +1,14 @@
 def p(a):
- h=len(a);w=len(a[0]);R=range;m=min;M=max
- z=[(i,j)for i in R(h)for j in R(w)if a[i][j]]
- r,c=zip(*z);k=a[r[0]][c[0]];y,x=m(r),m(c);Y,X=M(r),M(c);d=(Y-y)//2
+ h,w=len(a),len(a[0]);R=range
+ r,c=zip(*[(i,j)for i in R(h)for j in R(w)if a[i][j]])
+ k=a[r[0]][c[0]];y,x=min(r),min(c);Y,X=max(r),max(c);d=(Y-y)//2
  while 1:
-  for j in R(M(x,0),m(X+1,w)):
-   if h>y>=0:a[y][j]=k
-   if h>Y>=0:a[Y][j]=k
-  for i in R(M(y,0),m(Y+1,h)):
-   if w>x>=0:a[i][x]=k
-   if w>X>=0:a[i][X]=k
+  for j in R(x,X+1):
+   for b in y,Y:
+    if h>b>-1<j<w:a[b][j]=k
+  for i in R(y,Y+1):
+   for b in x,X:
+    if w>b>-1<i<h:a[i][b]=k
   y-=d;Y+=d;x-=d;X+=d
   if y<0>x and Y>=h<=X>=w:break
  return a
