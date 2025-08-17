101da7e421c4ad29678a09d2d3db329d2b1a82cf
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 11:51:59 +0900

    問題109: コードゴルフ解答の最適化

--- 109/3_submission.py
+++ 109/3_submission.py
@@ -1,2 +1,2 @@
 def p(g):
- v=next(i for i in range(1,len(g[0])-1)if len({r[i]for r in g})<2);H=next(i for i in range(1,len(g)-1)if len({*g[i]})<2);P=[r[:v]for r in g[:H]];M=max(map(max,P));P=[[g[0][v]*(e==M)or e for e in r]for r in P];A=[r+r[::-1]for r in P];return A+A[::-1]
+ n=len(g)//2;g=[r+r[::-1]for r in[[e and g[0][n]for e in r[:n]]for r in g[:n]]];return g+g[::-1]
