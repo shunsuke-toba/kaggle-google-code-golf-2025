7f002ec594f0fc186b7de2067d793afe73a8e479
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 08:04:39 +0900

    問題196: 3x3グリッド回転パターンのコードゴルフ最適化

--- 196/3_submission.py
+++ 196/3_submission.py
@@ -1,12 +1,13 @@
-def p(j):
-	A=range;c,E=len(j),len(j[0]);k=set();W=[e[:]for e in j]
-	def M(l,J):
-		if(l,J)in k or not(0<=l<c and 0<=J<E)or j[l][J]!=1:return[]
-		k.add((l,J));return[(l,J)]+sum([M(l+e,J+A)for(e,A)in[(-1,0),(1,0),(0,-1),(0,1)]],[])
-	for a in A(c):
-		for C in A(E):
-			if j[a][C]==1 and(a,C)not in k:
-				e=M(a,C);K,w,L,b=min(e[0]for e in e),max(e[0]for e in e),min(e[1]for e in e),max(e[1]for e in e)
-				if len(e)==2*(w-K+b-L)and w>K and b>L and any(j[e][k]==0 for e in A(K+1,w)for k in A(L+1,b)):
-					for(d,f)in e:W[d][f]=3
-	return W
+def p(g):
+ R=range;h=len(g);w=len(g[0]);v=set()
+ def f(i,j):
+  if(i,j)in v or not(h>i>=0<=j<w)or g[i][j]-1:return[]
+  v.add((i,j))
+  return[(i,j)]+f(i+1,j)+f(i-1,j)+f(i,j+1)+f(i,j-1)
+ for k in R(h*w):
+  i,j=divmod(k,w)
+  if c:=f(i,j):
+   Y,X=zip(*c);a,b=min(Y),max(Y);d,e=min(X),max(X)
+   if len(c)==2*(b-a+e-d)and b-a>1<e-d:
+    for y,x in c:g[y][x]=3
+ return g
