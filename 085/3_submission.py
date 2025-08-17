ecae24606fa3084629d0adb62e162dc10e6d4377
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 09:54:05 +0900

    問題85: コードゴルフ解答の最適化

--- 085/3_submission.py
+++ 085/3_submission.py
@@ -1,15 +1,7 @@
-def p(g,V=range):
- r=[d[:]for d in g]
- v=set()
- for i in V(len(g)-2):
-  for j in V(len(g[0])):
-   if g[i][j]and(i,j)not in v:
-    c=g[i][j]
-    if all(g[i+k][j]==c for k in V(3)):
-     a=j
-     while a<len(g[0])and all(g[i+k][a]==c for k in V(3)):
-      for k in V(3):v.add((i+k,a))
-      a+=1
-     for x in V(j,a):
-      if(x-j)%2==1:r[i+1][x]=0
- return r
+def p(g,R=range):
+ h=len(g)
+ for y in R(1,h-1):
+  for x in R(1,len(g[0])):
+   c=g[y][x]
+   if c and g[y][x-1]==c and g[y-1][x]==c==g[y+1][x]:g[y][x]=0
+ return g
