9c154ad2dbd4bbdc643f9fe0bd402f1e5d62fdf3
pech1 <pechi2327@gmail.com>
2025-08-17 15:20:52 +0900

    332:solution,subnissionの更新,333:微短縮

--- 333/3_submission.py
+++ 333/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- n=len(g);R=range(n-1);r,c=next((i,j)for i in R for j in R if g[i][j]&g[i][j+1]&g[i+1][j]&g[i+1][j+1]&3);s=lambda i,j,I,J:(n>(i:=i+I)>-1<(j:=j+J)<n)and(g[i][j]or g[i].__setitem__(j,s(i,j,I,J))or g[i][j])
- for _ in 0,1,2,3:a,b=divmod(_,2);s(r+a,c+b,2*a-1,0);s(r+a,c+b,0,2*b-1)
+ n=len(g);R=range(n-1);r,c=next((i,j)for i in R for j in R if g[i][j]&g[i][j+1]&g[i+1][j]&g[i+1][j+1]);s=lambda i,j,I,J:(n>(i:=i+I)>-1<(j:=j+J)<n)and(g[i][j]or g[i].__setitem__(j,s(i,j,I,J))or g[i][j])
+ for _ in 0,1,2,3:a=_//2;b=_%2;s(r+a,c+b,2*a-1,0);s(r+a,c+b,0,2*b-1)
  return g
