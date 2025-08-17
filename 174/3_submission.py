4e3455b16a9e246564785380fd4bded96dc03e42
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 23:44:09 +0900

    問題174: 3x3サブグリッド処理の最適化実装

--- 174/3_submission.py
+++ 174/3_submission.py
@@ -1,4 +1,5 @@
 def p(g):
- r=range(10);s={*sum(g,[])}-{0};f=lambda c:(x:=[i for i in r if c in g[i]],y:=[j for j in r if c in[r[j]for r in g]],[r[y[0]:y[-1]+1]for r in g[x[0]:x[-1]+1]])[2];a=b=0
- for c in s:m=f(c);d=len(sum(m,[]));m==[r[::-1]for r in m]and d>a and(a:=d,b:=m)
- return b or f(sorted(s)[len(s)//2])
+ r=range(10)
+ for c in {*sum(g,[])}-{0}:
+  u,v=zip(*((i,j)for i in r for j in r if g[i][j]==c));m=[r[min(v):max(v)+1]for r in g[min(u):max(u)+1]]
+  if m==[r[::-1]for r in m]:return m
