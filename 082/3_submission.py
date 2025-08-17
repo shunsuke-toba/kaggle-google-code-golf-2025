91c47080c592706a2d078d562092ea9df3dffea2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 09:52:17 +0900

    問題82: コードゴルフ解答の最適化

--- 082/3_submission.py
+++ 082/3_submission.py
@@ -1,10 +1,1 @@
-def p(j):
-	A=[k[:]for k in j];c,E=len(j),len(j[0])
-	for k in range(E):
-		if j[0][k]:
-			for W in range(c):
-				if W%2==0:A[W][k]=j[0][k]
-				else:
-					if k>0:A[W][k-1]=j[0][k]
-					if k<E-1:A[W][k+1]=j[0][k]
-	return A
+p=lambda g:(r:=g[0],s:=list(map(max,r[1:]+[0],[0]+r[:-1])),([r,s]*len(g))[:len(g)])[2]
