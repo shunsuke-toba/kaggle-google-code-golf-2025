b5d21933d77c27e83ca846c46d6d62451d428992
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 08:59:11 +0900

    問題196: 3x3グリッド回転パターンのコードゴルフ最適化

--- 196/3_submission.py
+++ 196/3_submission.py
@@ -1,13 +1,13 @@
 def p(g):
- R=range;h=len(g);w=len(g[0]);v=set()
+ h=len(g);w=len(g[0])
  def f(i,j):
-  if(i,j)in v or not(h>i>=0<=j<w)or g[i][j]-1:return[]
-  v.add((i,j))
+  if not(h>i>=0<=j<w)or g[i][j]-1:return[]
+  g[i][j]=0
   return[(i,j)]+f(i+1,j)+f(i-1,j)+f(i,j+1)+f(i,j-1)
- for k in R(h*w):
-  i,j=divmod(k,w)
+ for k in range(h*w):
+  i=k//w;j=k%w
   if c:=f(i,j):
-   Y,X=zip(*c);a,b=min(Y),max(Y);d,e=min(X),max(X)
-   if len(c)==2*(b-a+e-d)and b-a>1<e-d:
-    for y,x in c:g[y][x]=3
+   Y,X=zip(*c);a,b=min(Y),max(Y);d,e=min(X),max(X);t=1
+   if len(c)==2*(b-a+e-d)and b-a>1<e-d:t=3
+   for y,x in c:g[y][x]=t
  return g
