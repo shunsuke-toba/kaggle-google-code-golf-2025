71f01a40dd535153f226cbcde64da9e2e61641f2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 14:48:56 +0900

    問題120: コードゴルフ解答の修正

--- 120/3_submission.py
+++ 120/3_submission.py
@@ -1,15 +1,11 @@
-def p(j):
-	A=range;c=len;E=[W[:]for W in j];k=set()
-	for W in A(c(j)):
-		for l in A(c(j[0])):
-			if j[W][l]and(W,l)not in k:
-				J,a=[(W,l)],[(W,l)];k.add((W,l));C=j[W][l]
-				while a:
-					e,K=a.pop()
-					for(w,L)in[(0,1),(1,0),(0,-1),(-1,0)]:
-						b,d=e+w,K+L
-						if 0<=b<c(j)and 0<=d<c(j[0])and j[b][d]==C and(b,d)not in k:k.add((b,d));J.append((b,d));a.append((b,d))
-				f=min(W[0]for W in J);g=max(W[0]for W in J);h=min(W[1]for W in J);i=max(W[1]for W in J)
-				for e in A(f+1,g):
-					for K in A(h+1,i):E[e][K]=8
-	return E
+def p(g):
+ e=enumerate
+ for i,r in e(g):
+  for j,c in e(r):
+   if c%8 and(i<1 or g[i-1][j]-c)and(j<1 or r[j-1]-c):
+    x=j+1
+    while x<len(r)and r[x]==c:x+=1
+    y=i+1
+    while y<len(g)and g[y][j]==c:y+=1
+    for z in range(i+1,y-1):g[z][j+1:x-1]=[8]*(x-j-2)
+ return g
