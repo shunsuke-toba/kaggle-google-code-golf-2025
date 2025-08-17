df60794dd98c4d0f80c47202f156c284151cc026
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 23:03:15 +0900

    問題166: 3x3サブグリッド処理の最適化実装

--- 166/3_submission.py
+++ 166/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[[2if(t:=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==8])and min(i for i,j in t)<=i<=max(i for i,j in t)and min(j for i,j in t)<=j<=max(j for i,j in t)and g[i][j]==0else g[i][j]for j in range(len(g[0]))]for i in range(len(g))]
+from numpy import*;p=lambda g:(g:=array(g),t:=where(g==8),s:=g[t[0].min():t[0].max()+1,t[1].min():t[1].max()+1],putmask(s,s<1,2),g.tolist())[-1]
