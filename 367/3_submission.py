55e4b51dfe36f9eef2a3e99a7ae425d87f347e2a
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-17 20:03:34 +0900

    367をスプシの最短よりも短く

--- 367/3_submission.py
+++ 367/3_submission.py
@@ -2,8 +2,10 @@ def p(g):
  H,W=len(g),len(g[0]);D=(-1,0,1,0)
  for k in range(9999):
   r,c,s=k//(W*4)%H,k//4%W,k%4
-  n,u,v,t=[g[r+D[i]][c+D[i-3]]if H>r+D[i]>-1<c+D[i-3]<W else-1for i in(0,1,2,3)],r+D[s-1]+D[s],c+D[s]+D[s-3],g[r][c]
-  if n[s:]+n[:s]==[5,0,0,5]and k<4*H*W>t>4<H>u>-1<v<W:g[u][v]=4
+  try:n=[g[r+D[i]][c+D[i-3]]for i in(0,1,2,3)]*2
+  except:n=D
+  u,v,t=r+D[s-1]+D[s],c+D[s]+D[s-3],g[r][c]
+  if(n[s:s+4]==[5,0,0,5])*t>4<H>u>-1<v<W:g[u][v]=4
   a,b=r+D[s],c+D[s-3]
   if H>a>-1<b<W>5>g[a][b]>t:g[r][c]=4
  return g
