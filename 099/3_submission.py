5d3f27ab4e7c87b0aa75141865134dbe38c9004d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 10:48:58 +0900

    問題99: コードゴルフ解答の最適化

--- 099/3_submission.py
+++ 099/3_submission.py
@@ -1,12 +1,16 @@
 def p(g):
- h,w=len(g),len(g[0]);r=[*map(list,g)]
- def d(i,j):
-  if-1<i<h and-1<j<w and g[i][j]==1:
-   g[i][j]=2;return[(i,j)]+d(i-1,j)+d(i+1,j)+d(i,j-1)+d(i,j+1)
-  return[]
- for k in range(h*w):
-  if c:=d(k//w,k%w):
-   A,B=zip(*c);a=min(A);b=max(A);e=min(B);f=max(B);F=max(max(x[e:f+1])for x in r[a:b+1])
-   for i in range(a-(a>0),b+1):r[i][e:f+1]=[F]*(f-e+1)
-   for i,j in c:r[i][j]=1
- return r
+ a=[r[:]for r in g];h=len(g);w=len(g[0])
+ for y,r in enumerate(a):
+  for x,v in enumerate(r):
+   if v>1:
+    X=range(max(0,x-2),min(w,x+3))
+    for i in range(max(0,y-2),min(h,y+3)):
+     for j in X:
+      if g[i][j]<1:g[i][j]=v
+    u=y-2
+    if u>=0 and any(a[u][j]==1 for j in X):
+     u-=1
+     if u>=0:
+      for j in X:
+       if g[u][j]<1:g[u][j]=v
+ return g
