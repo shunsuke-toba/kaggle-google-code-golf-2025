804fd5ff60c41dbdeb143357541eebc767f0ce00
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 14:48:33 +0900

    問題119: コードゴルフ解答の最適化

--- 119/3_submission.py
+++ 119/3_submission.py
@@ -1,24 +1,15 @@
 def p(g):
- m,n=len(g),len(g[0]);R=[*map(list,g)]
- for y in range(m):
-  for x in range(n):
-   c=R[y][x]
-   if c in(0,2,3):continue
-   for v,w in((-1,-1),(-1,1),(1,-1),(1,1)):
-    Y,X=y+v,x+w
-    if 0<=Y<m and 0<=X<n and R[Y][X]==c:
-     i,j=y,x;dy,dx=v,w
-     while 1:
-      Y,X=i+dy,j+dx
-      if not(0<=Y<m and 0<=X<n):break
-      t=R[Y][X]
-      if t==2:
-       V=(Y>0 and R[Y-1][X]==2)+(Y<m-1 and R[Y+1][X]==2)
-       H=(X>0 and R[Y][X-1]==2)+(X<n-1 and R[Y][X+1]==2)
-       if V>H:dx=-dx
-       elif H>V:dy=-dy
-       elif V:dx=-dx
-       else:break
-      elif t in(0,c,3):R[Y][X]=t or 3;i,j=Y,X
-      else:break
- return R
+ h=len(g);w=len(g[0])
+ r=[a.count(2)for a in g];c=[b.count(2)for b in zip(*g)]
+ s=[[y,x,-i,-j]for y in range(h)for x in range(w)if g[y][x]==8 for i,j in((1,1),(1,-1),(-1,1),(-1,-1))if 0<=y+i<h and 0<=x+j<w and g[y+i][x+j]==8]
+ for y,x,i,j in s:
+  while 1:
+   Y,X=y+i,x+j
+   if not(0<=Y<h and 0<=X<w):break
+   if g[Y][X]==2:
+    if c[X]>r[Y]:j=-j
+    else:i=-i
+   else:
+    y,x=Y,X
+    if g[y][x]<1:g[y][x]=3
+ return g
