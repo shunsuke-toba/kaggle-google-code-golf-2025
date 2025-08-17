ee7be006f85cbb31226f29317dc36986f3bf4a11
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 08:04:08 +0900

    問題195: 3x3グリッド回転パターンのコードゴルフ最適化

--- 195/3_submission.py
+++ 195/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,A=range(9):((r:=min(i for i,R in enumerate(g)if 5 in R)),(c:=min(j for j,C in enumerate(zip(*g))if 5 in C)),[[g[r+x//3*3][c+y//3*3]and g[r+x%3*3][c+y%3*3]for y in A]for x in A])[2]
+p=lambda g,A=range(9):(r:=min(i for i,R in enumerate(g)if 5 in R),c:=min(R.index(5)for R in g if 5 in R),[[g[r+x//3*3][c+y//3*3]and g[r+x%3*3][c+y%3*3]for y in A]for x in A])[2]
