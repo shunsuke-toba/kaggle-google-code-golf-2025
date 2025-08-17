fb88fdf21c305ae503804813b3b0c0b02e665f8c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 21:20:54 +0900

    問題158: 3x3サブグリッド処理の改善実装

--- 158/3_submission.py
+++ 158/3_submission.py
@@ -1,37 +1,34 @@
 def p(g):
- h=len(g);w=len(g[0]);B=max(f:=sum(g,[]),key=f.count);V=[[0]*w for _ in g];d={};T=0
+ h=len(g);w=len(g[0]);B=max(f:=sum(g,[]),key=f.count);V=[[0]*w for _ in g];m={};T=0
  for k in range(h*w):
   r,c=divmod(k,w)
   if g[r][c]!=B and not V[r][c]:
-   S=[(r,c)];V[r][c]=1;A=[];C=set()
+   S=[(r,c)];V[r][c]=1;A=[];C=set();a=b=r;e=f=c
    while S:
     r,c=S.pop();A+=(r,c),;C|={g[r][c]}
+    a=min(a,r);b=max(b,r);e=min(e,c);f=max(f,c)
     for i in-1,0,1:
      for j in-1,0,1:
       if i|j and h>(x:=r+i)>=0<= (y:=c+j)<w and g[x][y]!=B and not V[x][y]:V[x][y]=1;S+=(x,y),
-   len(C)==3 and(T:=(A,C))
-   len(C)==1 and d.setdefault(*C,[]).append(A)
- A,C=T
- q=[g[r][c]for r,c in A];b=max(q,key=q.count);a,c=[x for x in C if x!=b]
- for p0 in A:col=g[p0[0]][p0[1]];col==a and(pa:=p0);col==c and(pc:=p0)
- R=pc[0]-pa[0];C=pc[1]-pa[1];Bs=[(r-pa[0],c-pa[1])for r,c in A if g[r][c]==b];o=[r[:]for r in g]
- for A in d[a]:
-  r,e=zip(*A);ar=min(r);ac=min(e);s=max(r)-ar+1
-  if max(e)-ac+1==s:
-   for A2 in d[c]:
-    r2,e2=zip(*A2);br=min(r2);bc=min(e2)
-    if max(r2)-br+1==s and max(e2)-bc+1==s:
-     dr=br-ar;dc=bc-ac
-     for u in 0,1:
-      for v in-1,1:
-       for w in-1,1:
-        dx,dy=R,C
-        if u:dx,dy=dy,dx
-        dx*=v;dy*=w
-        if dx and dr%dx==0 and dy*(k:=dr//dx)==dc and k>0 or dy and dc%dy==0 and dx*(k:=dc//dy)==dr and k>0:
-         for rb,cb in Bs:
-          if u:rb,cb=cb,rb
-          rb*=v;cb*=w
-          for i in range(k):
-           for j in range(k):o[ar+rb*k+i][ac+cb*k+j]=b
+   len(C)>2 and(T:=A)
+   len(C)<2 and (s:=b-a+1)==f-e+1 and m.setdefault(*C,[]).append((a,e,s))
+ A=T
+ L=[g[r][c]for r,c in A];b=max(L,key=L.count);p,q=set(L)-{b}
+ for r,c in A:col=g[r][c];col==p and(pa:=(r,c));col==q and(pc:=(r,c))
+ R=pc[0]-pa[0];C=pc[1]-pa[1];S=[(r-pa[0],c-pa[1])for r,c in A if g[r][c]==b];o=[r[:]for r in g]
+ for ar,ac,s in m[p]:
+  for br,bc,t in m[q]:
+   if s==t:
+    dr=br-ar;dc=bc-ac
+    for u in 0,1:
+     for v in-1,1:
+      for w in-1,1:
+       x,y=R*v,C*w
+       if u:x,y=y,x
+       if x and (k:=dr//x)>0 and dr%x==0 and y*k==dc or y and (k:=dc//y)>0 and dc%y==0 and x*k==dr:
+        for r,c in S:
+         r*=v;c*=w
+         if u:r,c=c,r
+         rr=ar+r*k;cc=ac+c*k
+         for i in range(k):o[rr+i][cc:cc+k]=[b]*k
  return o
