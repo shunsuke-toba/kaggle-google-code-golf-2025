a7e714578946e2810008940939253289bf58a2a7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 08:59:34 +0900

    問題198: 3x3グリッド回転パターンのコードゴルフ最適化

--- 198/3_submission.py
+++ 198/3_submission.py
@@ -1,14 +1,11 @@
 def p(g):
- L=len;R=range;z=zip;x=max(map(max,g));Z=[*z(*g)];h=L(g);w=L(Z)
- H=[i for i in R(h)if g[i].count(x)*2>w]
- V=[i for i in R(w)if Z[i].count(x)*2>h]
- o=[*map(list,g)];A=-1,*H,h;B=-1,*V,w
- for i,j in z(A,A[1:]):
-  for k,l in z(B,B[1:]):
-   t=(i>=0 and 0 in g[i][k+1:l] or j<h and 0 in g[j][k+1:l] or k>=0 and 0 in Z[k][i+1:j] or l<w and 0 in Z[l][i+1:j])+3
-   for y in R(i+1,j):
-    for x in R(k+1,l):o[y][x]=o[y][x]or t
- for r in H:o[r]=[x or 4 for x in o[r]]
- for r in o:
-  for c in V:r[c]=r[c]or 4
- return o
+ R=range;x=max(sum(g,[]));h=len(g);w=len(g[0])
+ H=[i for i in R(h)if g[i].count(x)*2>w];V=[i for i,c in enumerate(zip(*g))if c.count(x)*2>h]
+ o=[*map(list,g)]
+ q=[(y,x)for y in H for x in R(w)if o[y][x]<1]+[(y,x)for x in V for y in R(h)if o[y][x]<1]
+ for y,x in q:o[y][x]=4
+ while q:
+  y,x=q.pop()
+  for Y,X in((y+1,x),(y-1,x),(y,x+1),(y,x-1)):
+   if h>Y>=0<=X<w and o[Y][X]<1:o[Y][X]=4;q+=[(Y,X)]
+ return [[c or 3 for c in r]for r in o]
