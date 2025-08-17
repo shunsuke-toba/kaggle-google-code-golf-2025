4ee7df2656f936821d129aa5d96a0fd847183919
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 17:46:02 +0900

    問題139: コードゴルフ解答の実装

--- 139/3_submission.py
+++ 139/3_submission.py
@@ -1,7 +1,7 @@
-from itertools import product
-def p(j,A=range):
- for c,E in product(A(len(j)-2),A(len(j[0])-2)):
-  k=A(c,c+3)
-  if not all(4 in i for i in[j[c][E:E+3],j[c+2][E:E+3],[j[W][E]for W in k],[j[W][E+2]for W in k]]):continue
-  for W,l in product(k,A(E,E+3)):j[W][l]+=7*(j[W][l]==0)
- return j
+def p(g,r=range):
+ for c in r(7):
+  for E in r(7):
+   q=[s[E:E+3]for s in g[c:c+3]];t=[*zip(*q)]
+   if all(4 in u for u in(q[0],q[2],t[0],t[2])):
+    for w in r(3):g[c+w][E:E+3]=[x or 7 for x in q[w]]
+ return g
