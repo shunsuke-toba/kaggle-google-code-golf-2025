0b17536afa63477d42e7ca1b9e1074e129224b2d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 08:11:35 +0900

    問題205: 3x3グリッド回転パターンのコードゴルフ最適化

--- 205/3_submission.py
+++ 205/3_submission.py
@@ -1,33 +1,14 @@
 def p(g):
- R,C=len(g),len(g[0])
- s=[0]*10
- for x in range(10):
-  s[x]=[[0]*(C+1)for _ in range(R+1)]
-  for r in range(R):
-   for c in range(C):s[x][r+1][c+1]=s[x][r][c+1]+s[x][r+1][c]-s[x][r][c]+(g[r][c]==x)
- def q(x,t,l,b,r):return s[x][b+1][r+1]-s[x][t][r+1]-s[x][b+1][l]+s[x][t][l]
- B,M=None,0
- for t in range(R):
-  for l in range(C):
-   for b in range(t,R):
-    for r in range(l,C):
-     d={x:q(x,t,l,b,r)for x in range(10)}
-     d={x:v for x,v in d.items()if v>0}
-     if len(d)>=3:break
-     if len(d)==2:
-      k=list(d.keys())
-      if d[k[0]]<d[k[1]]:k[0],k[1]=k[1],k[0]
-      if d[k[0]]<M:continue
-      M,B=d[k[0]],(t,l,b,r,k[0],k[1])
- t,l,b,r,j,n=B
- p=[]
- for i in range(t,b+1):
-  for u in range(l,r+1):
-   if g[i][u]==n:p.append((i-t,u-l))
- w,h=b-t+1,r-l+1
- nr=set(o[0]for o in p)
- nc=set(o[1]for o in p)
- z=[]
- for i in range(w):
-  z.append([n if i in nr or u in nc else j for u in range(h)])
- return z
+ R=range;l=len;E=enumerate;h=l(g);w=l(g[0]);m=0
+ for a in R(h):
+  for b in R(a+1,h+1):
+   for c in R(w):
+    for d in R(c+1,w+1):
+     if(k:=l({v for r in g[a:b] for v in r[c:d]})==2 and(b-a)*(d-c))>m:m=k;A,B,C,D=a,b,c,d
+ o=[r[C:D]for r in g[A:B]];g=[r[:]for r in o];h=B-A;w=D-C
+ for i,r in E(o):
+  for j,v in E(r):
+   if(i and o[i-1][j]==v)|(i<h-1 and o[i+1][j]==v)|(j and r[j-1]==v)|(j<w-1 and r[j+1]==v):continue
+   for k in g:k[j]=v
+   g[i]=[v]*w
+ return g
