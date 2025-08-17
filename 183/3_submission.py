5e5ada6b623014d4d1bbcd5387b375fe3186b3d3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 01:35:02 +0900

    問題183: 3×3サブグリッド展開のコードゴルフ最適化

--- 183/3_submission.py
+++ 183/3_submission.py
@@ -1,10 +1,1 @@
-def p(j):
-	A=range;c=len(j);E=c//2-2;k=[];W=[j[0][0],j[0][-1],j[-1][0],j[-1][-1]]
-	for l in A(2,c-2):
-		J=[]
-		for a in A(2,c-2):
-			C=j[l][a]
-			if C==8:e=(l-2)//E;K=(a-2)//E;C=W[e*2+K]
-			J.append(C)
-		k.append(J)
-	return k
+p=lambda g:(n:=len(g)-1,d:=n//2-1,c:=g[::n],e:=enumerate,[[c[i>=d][-(j>=d)]if v>7 else v for j,v in e(r[2:-2])]for i,r in e(g[2:-2])])[-1]
