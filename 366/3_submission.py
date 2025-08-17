cfa2c73b1e97b4eb7c322bc8d52a552cb34c8477
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 03:05:34 +0900

    問題366: コードゴルフ最適化

--- 366/3_submission.py
+++ 366/3_submission.py
@@ -1,43 +1,37 @@
-from collections import*
 def p(g):
- h=len(g);w=len(g[0]);c=Counter();b=defaultdict(lambda:[h,w,0,0])
+ c=[0]*10;b=[[9e9,9e9,0,0]for _ in range(10)]
  for i,r in enumerate(g):
   for j,v in enumerate(r):
    c[v]+=1;m=b[v];m[0]=min(m[0],i);m[1]=min(m[1],j);m[2]=max(m[2],i);m[3]=max(m[3],j)
- d,s,t=[*sorted(c,key=c.get,reverse=True)][:3];sx,sy,ex,ey=b[s];dx,dy,fx,fy=b[d]
- V=set();L=[]
+  t,s,d=sorted(range(10),key=c.__getitem__)[7:];sx,sy,ex,ey=b[s];dx,dy,fx,fy=b[d]
+ V=set();L=[];P=1,-1,0,0,0,0,1,-1
  for i in range(sx,ex+1):
   for j in range(sy,ey+1):
    if (i,j)in V or g[i][j]in(s,t):continue
    q=[(i,j)];V.add((i,j));C=[]
    while q:
-    x,y=q.pop();C+=[(x,y)]
-    for X,Y in((1,0),(-1,0),(0,1),(0,-1)):
-     X+=x;Y+=y
-     if sx<=X<=ex and sy<=Y<=ey and(X,Y)not in V and g[X][Y]!=s:V.add((X,Y));q+=[(X,Y)]
-   L+=[C]
+    x,y=q.pop();C+=(x,y),
+    for k in range(4):
+     X=x+P[k];Y=y+P[k+4]
+     if sx<=X<=ex and sy<=Y<=ey and g[X][Y]-s and(X,Y)not in V:V.add((X,Y));q+=(X,Y),
+   L+=C,
  L.sort(key=lambda C:-sum(g[x][y]!=t for x,y in C))
- R=lambda a:[list(r)for r in zip(*a[::-1])]
+ R=lambda a:[*zip(*a[::-1])]
  for C in L:
-  xs=[x for x,_ in C];ys=[y for _,y in C]
-  a=[[t]*(max(ys)-min(ys)+1)for _ in range(max(xs)-min(xs)+1)]
-  for x,y in C:a[x-min(xs)][y-min(ys)]=g[x][y]
-  T=[a]
-  for _ in'123':a=R(a);T+=[a]
-  a=[r[::-1]for r in T[0]];T+=[a]
-  for _ in'123':a=R(a);T+=[a]
+  xs,ys=zip(*C);m=min(xs);a=[[t]*(max(ys)-(n:=min(ys))+1)for _ in range(max(xs)-m+1)];
+  for x,y in C:a[x-m][y-n]=g[x][y]
   ok=0
-  for tr in T:
-   nb=[(i,j,v)for i,r in enumerate(tr)for j,v in enumerate(r)if v!=t]
-   bg=[(i,j)for i,r in enumerate(tr)for j,v in enumerate(r)if v==t]
-   H=len(tr);W=len(tr[0])
-   for x in range(dx,fx-H+2):
-    for y in range(dy,fy-W+2):
-     if all(g[x+i][y+j]==v for i,j,v in nb)and all(g[x+i][y+j]==d for i,j in bg):
-      for i,r in enumerate(tr):
-       for j,v in enumerate(r):g[x+i][y+j]=v
-      ok=1;break
+  for _ in 0,1:
+   for _ in'1234':
+    for x in range(dx,fx-len(a)+2):
+     for y in range(dy,fy-len(a[0])+2):
+      if all(g[x+i][y+j]==(d,v)[v!=t]for i,r in enumerate(a)for j,v in enumerate(r)):
+       for i,r in enumerate(a):
+        for j,v in enumerate(r):g[x+i][y+j]=v
+       ok=1;break
+     if ok:break
     if ok:break
+    a=R(a)
    if ok:break
- dx,dy,fx,fy=b[d]
- return[g[i][dy:fy+1]for i in range(dx,fx+1)]
+   a=[r[::-1]for r in a]
+ return[r[dy:fy+1]for r in g[dx:fx+1]]
