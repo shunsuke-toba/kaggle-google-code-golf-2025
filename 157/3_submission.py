fffb960f681e1d1522d6fe65c988cd035523d2ce
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 21:19:38 +0900

    問題157: 3x3サブグリッド抽出の最適化実装

--- 157/3_submission.py
+++ 157/3_submission.py
@@ -1,28 +1,27 @@
 def p(g):
- h=len(g);w=len(g[0]);r=range;d=(1,0,-1,0,1);c=[]
- for y in r(h):
-  for x in r(w):
-   if g[y][x]==5:
-    q=[(y,x)];g[y][x]=0;t=[]
+ h=len(g);w=len(g[0]);r=range;d=1,0,-1,0,1;c=[]
+ for Y in r(h):
+  for X in r(w):
+   if g[Y][X]>4:
+    q=[Y,X];g[Y][X]=0;t=[];m,n=Y,X
     while q:
-     Y,X=q.pop();t+=[(Y,X)]
+     x=q.pop();y=q.pop();t+=y,x;m=min(m,y);n=min(n,x)
      for i in r(4):
-      ny,nx=Y+d[i],X+d[i+1]
-      if 0<=ny<h and 0<=nx<w and g[ny][nx]==5:g[ny][nx]=0;q+=[(ny,nx)]
-    m=min(y for y,x in t);n=min(x for y,x in t)
-    c+=[[(y-m,x-n)for y,x in t]]
+      Y=y+d[i];X=x+d[i+1]
+      if 0<=Y<h and 0<=X<w and g[Y][X]>4:g[Y][X]=0;q+=Y,X
+    c+=[[[t[i]-m,t[i+1]-n]for i in r(0,len(t),2)]]
  while c:
-  b=-1
-  for i,t in enumerate(c):
+  B=0
+  for I,T in enumerate(c):
    for Y in r(3):
     for X in r(w):
-     ok=1;s=0
-     for y,x in t:
+     s=0
+     for y,x in T:
       u=Y+y;v=X+x
-      if u>=h or v>=w or g[u][v]:ok=0;break
+      if u>=h or v>=w or g[u][v]:break
       for j in r(4):
-       ny,nx=u+d[j],v+d[j+1]
-       if 0<=ny<h and 0<=nx<w and g[ny][nx]==2:s+=1
-     if ok and s>b:b=s;I=i;Y0=Y;X0=X
-  for y,x in c.pop(I):g[Y0+y][X0+x]=1
+       a=u+d[j];b=v+d[j+1];s+=0<=a<h and 0<=b<w and g[a][b]==2
+     else:
+      if s>B:B=s;i=I;Y0=Y;X0=X
+  for y,x in c.pop(i):g[Y0+y][X0+x]=1
  return g
