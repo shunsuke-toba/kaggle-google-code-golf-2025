38f05a5ba49580e31c8876ef0ef62cfbbd0dceef
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 18:39:56 +0900

    問題147: 3x3グリッド分割と選択的要素抽出の実装

--- 147/3_submission.py
+++ 147/3_submission.py
@@ -1,8 +1,2 @@
-def p(j):
-	A=[k[:]for k in j];c,E=len(j),len(j[0])
-	for k in range(c):
-		for W in range(E):
-			if j[k][W]==3:
-				for(l,J)in[(0,1),(1,0),(0,-1),(-1,0)]:
-					if 0<=k+l<c and 0<=W+J<E and j[k+l][W+J]==3:A[k][W]=8;break
-	return A
+from numpy import*
+p=lambda g:(a:=array(g),m:=pad(a==3,1),s:=slice(1,-1),a.__setitem__(m[s,s]&(m[2:,s]|m[:-2,s]|m[s,2:]|m[s,:-2]),8),a.tolist())[4]
