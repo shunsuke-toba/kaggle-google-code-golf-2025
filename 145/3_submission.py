9612664cdca421d36cc66c718175facf48acab7d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 18:39:08 +0900

    問題145: グリッド反転パターンの実装

--- 145/3_submission.py
+++ 145/3_submission.py
@@ -1,19 +1,13 @@
-def p(j):
-	A=[K[:]for K in j];c,E=len(j),len(j[0]);k=set();W=[]
-	for l in range(c):
-		for J in range(E):
-			if j[l][J]!=2 and(l,J)not in k:
-				a,C=[],[(l,J)];k.add((l,J));e=0
-				while C:
-					K,w=C.pop();a.append((K,w))
-					if j[K][w]==0:e+=1
-					for(L,b)in[(0,1),(1,0),(0,-1),(-1,0)]:
-						if 0<=K+L<c and 0<=w+b<E and j[K+L][w+b]!=2 and(K+L,w+b)not in k:k.add((K+L,w+b));C.append((K+L,w+b))
-				W.append((e,a))
-	d=max(K[0]for K in W);f=min(K[0]for K in W)
-	for(e,a)in W:
-		k=1 if e==d else 8 if e==f else 0
-		if k:
-			for(K,w)in a:
-				if j[K][w]==0:A[K][w]=k
-	return A
+def p(g):
+ h=len(g);w=len(g[0]);r=range;s=[]
+ def f(y,x):
+  if h>y>=0<=x<w and g[y][x]<1:
+   g[y][x]=v;s[-1]+=1;f(y+1,x);f(y-1,x);f(y,x+1);f(y,x-1)
+ for y in r(h):
+  for x in r(w):
+   if g[y][x]<1:s+=0,;v=len(s)+2;f(y,x)
+ m=max(s);n=min(s)
+ for R in g:
+  for x in r(w):
+   if (v:=R[x])>2:a=s[v-3];R[x]=a==m or 8*(a==n)
+ return g
