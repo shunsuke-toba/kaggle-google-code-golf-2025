521591fd0d6e81cd21cb4555445c0ad42902b3a0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 18:39:35 +0900

    問題146: 3x3グリッドの内部パターン抽出実装

--- 146/3_submission.py
+++ 146/3_submission.py
@@ -1,1 +1,3 @@
-p=lambda g,R=range:[[[g[k+i][j]for j in R(3)]for i in R(3)]for k in R(0,9,3)if[[g[k+i][j]for j in R(3)]for i in R(3)]!=[[g[k+j][i]for j in R(3)]for i in R(3)]][0]
+def p(g):
+ while(b:=g[:3])==[*map(list,zip(*b))]:g=g[3:]
+ return b
