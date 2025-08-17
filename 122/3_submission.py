71e26b86793dd5bbedd04b304dd7a9e134815f9d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 15:37:08 +0900

    問題122: コードゴルフ解答の最適化

--- 122/3_submission.py
+++ 122/3_submission.py
@@ -1,9 +1,10 @@
 def p(g):
  for y,r in enumerate(g):
-  if 2 in r:x=r.index(2);break
- d=x+3<len(g[0])and g[y+1][x+3]==3
- for r in g[y:y+3]:r[x:x+3]=[0]*3
- X=x+2*d;Y=y+2-2*d
- for r in g[Y:Y+3]:r[X:X+3]=[2]*3
+  if 2 in r:break
+ x=r.index(2)
+ d=3in g[y+1][x+3:x+4]
+ for r in g[y:y+3]:r[x:x+3]=0,0,0
+ X,Y=x+2*d,y+2-2*d
+ for r in g[Y:Y+3]:r[X:X+3]=2,2,2
  g[y+1][x+1]=g[Y+1][X+1]=3
  return g
