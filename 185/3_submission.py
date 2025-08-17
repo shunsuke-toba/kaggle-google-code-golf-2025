2526982f78975094237a23ee366bb04860956d77
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 01:35:53 +0900

    問題185: 3×3サブグリッド展開のコードゴルフ最適化

--- 185/3_submission.py
+++ 185/3_submission.py
@@ -1,5 +1,1 @@
-def p(g):
- r,e=range,enumerate;c=max(r(1,10),key=sum(g,[]).count)
- y,x=zip(*((i,j)for i,R in e(g)for j,v in e(R)if v*(v-c)))
- s=min(x);t=min(y);d=(max(x)-s)//3
- return [[((v:=g[Y][X])==g[Y][X+d]==g[Y+d][X]==g[Y+d][X+d]!=c)*v for X in r(s,s+3*d,d)]for Y in r(t,t+3*d,d)]
+def p(g):r,e=range,enumerate;c=max(r(1,10),key=sum(g,[]).count);y,x=zip(*((i,j)for i,R in e(g)for j,v in e(R)if c!=v>0));s,t=min(x),min(y);d=(max(x)-s)//3;return[[((v:=g[y][x])==g[y][x+d]==g[y+d][x]==g[y+d][x+d]!=c)*v for x in r(s,s+3*d,d)]for y in r(t,t+3*d,d)]
