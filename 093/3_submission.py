53e7d4a31d06e47f9dd58f90cd7c7d9dd49912e5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-17 10:46:05 +0900

    問題93: コードゴルフ解答の最適化

--- 093/3_submission.py
+++ 093/3_submission.py
@@ -1,17 +1,15 @@
 def p(g):
- H,W=len(g),len(g[0]);G=[(r,j)for r in range(H)for j in range(W)if g[r][j]==5];R=[[0]*W for _ in[0]*H]
- if G:
-  a=min(r for r,j in G);b=max(r for r,j in G);c=min(j for r,j in G);d=max(j for r,j in G)
-  for r in range(a,b+1):
-   for j in range(c,d+1):R[r][j]=5
-  for j in range(c,d+1):
-   n=sum(0<g[r][j]!=5 for r in range(a))
-   for i in range(n):R[a+~i][j]=5
-   n=sum(0<g[r][j]!=5 for r in range(b+1,H))
-   for i in range(n):R[b-~i][j]=5
-  for r in range(a,b+1):
-   n=sum(0<g[r][j]!=5 for j in range(c))
-   for i in range(n):R[r][c+~i]=5
-   n=sum(0<g[r][j]!=5 for j in range(d+1,W))
-   for i in range(n):R[r][d-~i]=5
- return R
+ r=[[0 if c and c-5 else c for c in r]for r in g];F=lambda a:sum(c and c-5 and 1 for c in a)
+ for j,x in enumerate(zip(*g)):
+  if 5 in x:
+   u=x.index(5);d=len(g)+~x[::-1].index(5);a=F(x[:u])
+   while a:r[u-a][j]=5;a-=1
+   a=F(x[d+1:])
+   while a:r[d+a][j]=5;a-=1
+ for i,x in enumerate(g):
+  if 5 in x:
+   L=x.index(5);R=len(x)+~x[::-1].index(5);a=F(x[:L])
+   while a:r[i][L-a]=5;a-=1
+   a=F(x[R+1:])
+   while a:r[i][R+a]=5;a-=1
+ return r
