85031de866162c364a05fc99f917ddc2cfcdc86c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 22:07:17 +0900

    問題212: グリッドの塗りつぶしアルゴリズムの最適化

--- 212/3_submission.py
+++ 212/3_submission.py
@@ -1,10 +1,6 @@
 def p(g):
- R,C=len(g),len(g[0]);L=-1
- for r in range(R):
-  if g[r][0]==5:L=r
- for b in range(R*C):
-  r,b=b//C,b%C
-  if g[r][b]%5==0:continue
-  d=-1if(g[r][b]-1)!=(r<L)else 1;i=r
-  while 0<=i<R and i!=L:g[i][b]=g[r][b];i+=d
+ l=g.index([5]*10)
+ for b in range(100):
+  v=g[r:=b//10][c:=b%10]
+  while v%5*(r-l)*~r*(10-r):g[r][c]=v;r+=(r<l)^(v<2)or-1
  return g
