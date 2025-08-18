bc8bf8cf305b9a0c50468ce00d9d7a1cb5308da4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-19 01:02:57 +0900

    問題240: コードゴルフ最適化の実装

--- 240/3_submission.py
+++ 240/3_submission.py
@@ -1,15 +1,11 @@
 def p(g):
- n=19;c=[0]*10
- for j2 in range(n//2):
-  for i in range(n):
-   for j in[j2,n-j2-1]:
-    if g[i][j]>0:
-     c[g[i][j]]+=1
-     if c[g[i][j]]==2:
-      k=j2
-      while k<=n-j2-1:g[i][k]=g[i][j];k+=2
- for _ in[1]*3:
-  for i in range(n):
-   for j in range(n):
-    if g[i][j]>0:g[j][n-i-1]=g[i][j]
+ c=[0]*10
+ for d in range(9):
+  for r in g:
+   for v in r[d],r[~d]:
+    c[v]+=v>0
+    if c[v]>1:r[d:-d or 19:2]=[v]*(10-d)
+ for i,r in enumerate(g):
+  for j,v in enumerate(r):
+   if v:g[j][~i]=g[~i][~j]=g[~j][i]=v
  return g
