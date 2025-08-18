86a01aba807d6ebb320cafb8d6da1619d17f8f90
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-18 18:31:26 +0900

    367の改善

--- 367/3_submission.py
+++ 367/3_submission.py
@@ -1,11 +1,8 @@
 def p(g):
- H,W=len(g),len(g[0]);D=(-1,0,1,0)
- for k in range(9999):
-  r,c,s=k//(W*4)%H,k//4%W,k%4
+ for k in range(9**5):
+  H,W=len(g),len(g[0]);D=(-1,0,1,0);r,c,s=k%97%H,k%89%W,k%4;x,y=D[s],D[s-3];u,v,t,a,b=r+D[s-1]+x,c+x+y,g[r][c],r+x,c+y
   try:n=[g[r+D[i]][c+D[i-3]]for i in(0,1,2,3)]*2
   except:n=D
-  u,v,t=r+D[s-1]+D[s],c+D[s]+D[s-3],g[r][c]
   if(n[s:s+4]==[5,0,0,5])*t>4<H>u>-1<v<W:g[u][v]=4
-  a,b=r+D[s],c+D[s-3]
   if H>a>-1<b<W>5>g[a][b]>t:g[r][c]=4
  return g
