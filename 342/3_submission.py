2f7b96675003a6101de77597613e54f4cf5d989e
pech1 <pechi2327@gmail.com>
2025-08-19 00:09:04 +0900

    342:solution,submissionの更新

--- 342/3_submission.py
+++ 342/3_submission.py
@@ -1,5 +1,4 @@
-def p(j,A=enumerate):
- c=lambda E,k:sum([[L,b]for L,r in A(j)for b,v in A(r)if v in E and v not in k],[])
- E,k,W,l,J,a,C,e=c(range(10),[0,8]);K,w=c([8],[])[:2];j[K][w:w+2]=[j[E][k],j[W][l]][::(1,-1)[k>l]];j[K+1][w:w+2]=[j[J][a],j[C][e]][::(1,-1)[a>e]]
- for L,b in(E,k),(W,l),(J,a),(C,e):j[L][b]=0
- return j
+def p(g):
+ s=sum(g,[]);r,c=divmod(s.index(8),10);a=[-8]*4;C=8-c
+ for q in range(100):a[(q//10>r)*2+(q%10>c)]+=s[q]
+ z=[[0]*10];return z*r+[[0]*c+a[:2]+[0]*C,[0]*c+a[2:]+[0]*C]+z*(8-r)
