bafa4ba39bbb2181a6c7a4104d6b1878bbd9505e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-19 07:58:37 +0900

    問題253: 2x2パターンの4x4グリッド展開を簡潔に実装

--- 253/3_submission.py
+++ 253/3_submission.py
@@ -1,9 +1,8 @@
-def p(j):
- A=len(j)-1;c=[0]*16
- for E in range(A):
-  for k in range(A):
-   if(W:=j[E][k])and j[E+1][k]==W and j[E][k+1]==W:c[0]=c[4]=c[1]=W
-   if W and j[E+1][k]==W and j[E+1][k+1]==W:c[8]=c[12]=c[13]=W
-   if W and j[E][k+1]==W and j[E+1][k+1]==W:c[2]=c[3]=c[7]=W
-   if(l:=j[E+1][k+1])and j[E+1][k]==l and j[E][k+1]==l:c[11]=c[14]=c[15]=l
- return[c[E:E+4]for E in(0,4,8,12)]
+def p(g):
+ o=[0]*16
+ for r,s in zip(g,g[1:]):
+  for w in zip(r,r[1:],s,s[1:]):
+   if w.count(max(w))==3:
+    i=w.index(0);k=(10,8,2,0)[i]
+    o[k],o[k+1],o[k+4],o[k+5]=w
+ return[o[:4],o[4:8],o[8:12],o[12:]]
