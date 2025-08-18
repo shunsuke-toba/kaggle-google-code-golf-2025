f38c086358d94c55f3062ffb4180fc96727163df
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 22:59:21 +0900

    問題221: 3x3ブロックのパターン識別と変換の最適化

--- 221/3_submission.py
+++ 221/3_submission.py
@@ -1,14 +1,1 @@
-def p(g):
- A=range(9)
- z=n=0
- for b in A:
-  if g[b//3][b%3]:n+=1
-  else:z+=1
- s=z*3;o=[[0]*s for _ in range(s)];p=0
- m=s//3
- for a in range(m*m):
-  i,j=a//m,a%m
-  for b in A:
-   if p<n:o[i*3+b//3][j*3+b%3]=g[b//3][b%3]
-  p+=1
- return o
+p=lambda g:(n:=sum(j<1 for i in g for j in i),(r:=range(3*n)))and[[(i//3*n+j//3<9-n)*g[i%3][j%3]for j in r]for i in r]
