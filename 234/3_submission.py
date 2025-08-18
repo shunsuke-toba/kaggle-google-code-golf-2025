3452ce9660fe79cdcdb229363e5579e4a8f1f436
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-19 01:01:02 +0900

    問題234: コードゴルフ最適化の実装

--- 234/3_submission.py
+++ 234/3_submission.py
@@ -1,14 +1,10 @@
 def p(g):
- c=n=r=0;N=len(g);M=len(g[0]);d=1,0,-1,0,1
- for i in range(1,N-1):
-  for j in range(1,M-1):
-   if g[i][j]and((g[i-1][j]==0and g[i+1][j]==0)or(g[i][j-1]==0and g[i][j+1]==0)):c+=1;n=g[i][j];g[i][j]=0;[r:=k for k in range(4)if g[i+d[k]][j+d[k+1]]!=n and g[i+d[k]][j+d[k+1]]]
- I=range(N);J=range(M)
- if r==3:J=range(M-1,-1,-1)
- if r==0:I=range(N-1,-1,-1)
- for i in I:
-  for j in J:
+ c=n=r=0;N,M=len(g),len(g[0]);d=1,0,-1,0,1;R=range
+ for i in R(1,N-1):
+  for j in R(1,M-1):
+   if(m:=g[i][j])and(g[i-1][j]+g[i+1][j]<1or g[i][j-1]+g[i][j+1]<1):c+=1;n=m;g[i][j]=0;[r:=k for k in R(4)if(v:=g[i+d[k]][j+d[k+1]])*(v-n)]
+ for i in R(N)[::1-2*(r<1)]:
+  for j in R(M)[::1-2*(r>2)]:
    if g[i][j]==n:
-    ni,nj=i+d[r]*c,j+d[r+1]*c
-    if g[ni][nj]==0:g[ni][nj]=n;g[i][j]=0
+    if g[a:=i+d[r]*c][b:=j+d[r+1]*c]<1:g[a][b]=n;g[i][j]=0
  return g
