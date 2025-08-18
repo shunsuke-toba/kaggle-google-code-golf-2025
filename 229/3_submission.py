0516bc96df4cfd0fc1f406b6292c061a52128916
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 23:02:49 +0900

    問題229: コードゴルフ最適化の実装

--- 229/3_submission.py
+++ 229/3_submission.py
@@ -1,1 +1,1 @@
-def p(j):A=__import__('collections').Counter([x for R in j for x in R]).most_common(1);c=A[0][0];return[[A if A==c else 5 for A in R]for R in j]
+def p(g):a=sum(g,[]);return[[[5,c][c==max(a,key=a.count)]for c in r]for r in g]
