80493afba3d30a235373033e1493a74da152fc3f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-19 01:44:06 +0900

    問題244: 3x3グリッドマッピング処理の最適化実装

--- 244/3_submission.py
+++ 244/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- n=len(g[0])
- for p in range(n):
-  if g[0][p]!=g[0][p+1]:A=range(0,n,p+2);return[[g[i][j]for j in A][::-1]for i in A]
+def p(g,f=0):return g[0][f]-g[0][-~f]and[r[::f+2][::-1]for r in g[::f+2]]or p(g,-~f)
