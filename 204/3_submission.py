960f5adb871ebe7b4b6ec8d3fb67b65e3b88a682
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 22:04:41 +0900

    問題204: 3x3グリッドパターンの展開と再構築の最適化

--- 204/3_submission.py
+++ 204/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
- n=len(g);r=range
- for d in r(1,n):
-  for y in r(n-d-1):
-   for x in r(n-d-1):
-    if all(g[y][x+i]*g[y+d+1][x+i]*g[y+i][x]*g[y+i][x+d+1]==1 for i in r(d+2)):
+ r=range
+ for d in r(n:=len(g)):
+  for y in r(m:=n-d-1):
+   for x in r(m):
+    if{g[y][x+i]*g[y+d+1][x+i]*g[y+i][x]*g[y+i][x+d+1]for i in r(d+2)}=={1}:
      for j in g[y+1:y+d+1]:j[x+1:x+d+1]=[2|d%2*5]*d
  return g
