f995e2fadb55a5839d87b16e701a2887bc596128
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 20:39:36 +0900

    問題208: グリッドパターンの最適化

--- 208/3_submission.py
+++ 208/3_submission.py
@@ -1,15 +1,13 @@
+from collections import Counter as C
 def p(g):
- m=r=c=h=w=0;R=len(g);C=len(g[0])
- for r1 in range(R):
-  for c1 in range(C):
-   for r2 in range(r1+2,R):
-    for c2 in range(c1+2,C):
-     if all(g[x][c1]==g[r1][c1]for x in range(r1,r2+1))&all(g[r1][y]==g[r1][c1]for y in range(c1,c2+1))&all(g[x][y]==0for x in range(r1+1,r2)for y in range(c1+1,c2)):
-      a=(r2-r1+1)*(c2-c1+1)
-      if a>m:m,r,c,h,w=a,r1,c1,r2-r1+1,c2-c1+1
- for r1 in range(R-h+1):
-  for c1 in range(C-w+1):
-   if all(g[x][y]==0for x in range(r1+1,r1+h-1)for y in range(c1+1,c1+w-1)):
-    for i in range(h):
-     for j in range(w):g[r1+i][c1+j]=g[r+i][c+j]
- return g
+ R,C=len(g),len(g[0]);F=range;m=x=y=s=t=0
+ for a in F(R*C):
+  q=g[i:=a//C];j=a%C
+  for h in F(3,-~R-i):
+   for w in F(3,-~C-j):
+    if q[j:j+w]==[q[j]]*w and all(r[j]==q[j] for r in g[i:i+h])*all(z<1 for r in g[i+1:i+h-1]for z in r[j+1:j+w-1])and(A:=h*w)>m:m,x,y,s,t=A,i,j,h,w
+ for i in F(-~R-s):
+  for j in F(-~C-t):
+   if(i^x|j^y)*all(z<1 for r in g[i+1:i+s-1]for z in r[j+1:j+t-1]):
+    for h in F(s):g[i+h][j:j+t]=g[x+h][y:y+t]
+    return g
