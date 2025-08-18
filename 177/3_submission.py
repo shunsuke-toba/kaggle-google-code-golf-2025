fb5270f94f1249f8ab1fa44ba0e41e04850c5fa2
Shunsuke Toba <toba@third-inc.co.jp>
2025-08-19 00:39:04 +0900

    177

--- 177/3_submission.py
+++ 177/3_submission.py
@@ -1,2 +1,1 @@
-from numpy import*
-p=lambda g:(g:=array(g),w:=where(g),g[min(w[0]):-~max(w[0]),min(w[1]):-~max(w[1])][:,::-1].tolist())[2]
+p=lambda g:[[e for e in r if e][::-1]for r in g if sum(r)]
