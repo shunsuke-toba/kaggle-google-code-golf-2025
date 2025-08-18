32a63a636bd59f527bc79e35f51449d247e227fe
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-18 20:39:55 +0900

    問題209: 繰り返しパターンの検出と塗りつぶしの最適化

--- 209/3_submission.py
+++ 209/3_submission.py
@@ -1,28 +1,22 @@
 def p(g):
- R,C=len(g),len(g[0]);c=[]
- for r in range(R):
-  for j in range(C):
-   if g[r][j]==4:c+=[(r,j)]
- mr,xr,mc,xc=min(r for r,j in c),max(r for r,j in c),min(j for r,j in c),max(j for r,j in c)
- pr,xpr,pc,xpc=R,-1,C,-1
- for r in range(R):
-  for j in range(C):
-   if g[r][j]!=0and not(mr<=r<=xr and mc<=j<=xc):pr,xpr,pc,xpc=min(pr,r),max(xpr,r),min(pc,j),max(xpc,j)
- A,S,P=0,1,None;s=[None]*5
- for k in[2,3,4]:
-  s[k]=[[0]*((xpc-pc+1)*k)for _ in range((xpr-pr+1)*k)]
-  for r in range(len(s[k])):
-   for j in range(len(s[k][0])):s[k][r][j]=g[pr+r//k][pc+j//k]
-  for r in range(mr,xr-len(s[k])+2):
-   for j in range(mc,xc-len(s[k][0])+2):
-    M,a=1,0
-    for i in range(len(s[k])):
-     for o in range(len(s[k][0])):
-      if g[r+i][j+o]!=0:
-       if g[r+i][j+o]!=s[k][i][o]:M=0
-       a+=1
-    if M and a>A:A,S,P=a,k,(r,j)
- r,j=P
- for i in range(len(s[S])):
-  for o in range(len(s[S][0])):g[r+i][j+o]=s[S][i][o]
- return[[g[r][j]for j in range(mc,xc+1)]for r in range(mr,xr+1)]
+ y=len(g);x=len(g[0]);a=y;c=x;b=d=0
+ for t in range(y*x):
+  r=t//x;j=t%x
+  if g[r][j]==4:a=min(a,r);b=max(b,r);c=min(c,j);d=max(d,j)
+ e=y;i=x;f=j=0
+ for t in range(y*x):
+  r=t//x;o=t%x
+  if g[r][o]and not(a<=r<=b and c<=o<=d):e=min(e,r);f=max(f,r);i=min(i,o);j=max(j,o)
+ h=f-e+1;w=j-i+1;z=k=P=0
+ for s in(2,3,4):
+  H=h*s;W=w*s
+  for r in range(a,b-H+2):
+   for o in range(c,d-W+2):
+    m=n=0
+    for t in range(H*W):
+      if(v:=g[r+t//W][o+t%W]):n+=1;m+=v!=g[e+t//W//s][i+t%W//s]
+    if m<1<n-z+1:z=n;k=s;P=r,o
+ r,o=P
+ for q in range(h*k):
+  for p in range(w*k):g[r+q][o+p]=g[e+q//k][i+p//k]
+ return[g[r][c:d+1]for r in range(a,b+1)]
