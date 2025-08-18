7801132b78a3aebe1c952900346977236f5dbef2
Shunsuke Toba <toba@third-inc.co.jp>
2025-08-19 01:01:44 +0900

    166

--- 166/3_submission.py
+++ 166/3_submission.py
@@ -1,1 +1,1 @@
-from numpy import*;p=lambda g:(g:=array(g),t:=where(g==8),s:=g[t[0].min():t[0].max()+1,t[1].min():t[1].max()+1],putmask(s,s<1,2),g.tolist())[-1]
+p=lambda g:[[x+2*(x==0)*any(r)*any(s[i] for s in g)for i,x in enumerate(r)]for r in g]
