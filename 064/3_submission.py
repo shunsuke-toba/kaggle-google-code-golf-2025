f696f56ad2b2f49a81c856f96bb86a7566a4c8ff
Shunsuke Toba <toba@third-inc.co.jp>
2025-08-20 01:55:21 +0900

    064

--- 064/3_submission.py
+++ 064/3_submission.py
@@ -1,24 +1,8 @@
-def p(g):
- h=len(g);w=len(g[0]);r=[*map(list,g)];s=sum(g,[]);b=max(s,key=s.count);v=set();M=0;R=[]
- for i in range(h):
-  for j in range(w):
-   if g[i][j]-b and(i,j)not in v:
-    o=g[i][j];A=i;D=j
-    while D+1<w and g[i][D+1]==o:D+=1
-    while A+1<h and g[A+1][j:D+1]==[o]*(D-j+1):A+=1
-    if(k:=(D-j+1)*(A-i+1))-1:
-     v|={(x,y)for x in range(i,A+1)for y in range(j,D+1)}
-     if k>M:M=k;R=[(i,j,A,D)]
-     elif k==M:R+=[(i,j,A,D)]
- for a,d,A,D in R:
-  for s in 0,1,2,3:
-   for m in range(*[(d,D+1),(a,A+1)][s>1]):
-    i,j,p,q=o=[(a-1,m,-1,0),(A+1,m,1,0),(m,d-1,0,-1),(m,D+1,0,1)][s];L=u=v=0
-    while-1<i<h and-1<j<w:
-     if g[i][j]-b:L=g[i][j];u=i;v=j
-     i+=p;j+=q
-    i,j,p,q=o
-    while L and(i,j)!=(u,v):
-     if g[i][j]==b:r[i][j]=L
-     i+=p;j+=q
- return r
+def p(g,E=enumerate):
+ C=sorted({*sum(g,[])},key=lambda x:-sum(r.count(x)for r in g))
+ for _ in [0]*4:
+  for r in g:
+   a,b=[i for i,x in E(r)if x==C[2]],[i for i,x in E(r)if x==C[1]]
+   if a and b and a[0]<b[0]:r[a[0]:b[0]]=[C[2]]*(b[0]-a[0])
+  g=[*map(list,zip(*g[::-1]))]
+ return g
