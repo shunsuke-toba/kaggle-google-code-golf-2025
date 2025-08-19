c0034aa01b78bb22931cc2fcb40a8e6968dea037
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-19 19:46:22 +0900

    問題270: コードゴルフ最適化

--- 270/3_submission.py
+++ 270/3_submission.py
@@ -1,10 +1,10 @@
 def p(g):
- h=len(g);w=len(g[0]);r=[[0]*w for _ in g];F=sum(g,[])
- for i in(1,2):
-  y,x=divmod(F.index(i),w);r[y][x]=i;t=11-4*i
+ r=[[0]*15 for _ in g]
+ for i in 1,2:
+  y,x=divmod(sum(g,[]).index(i),15);r[y][x]=i;t=11-4*i
   for Y,X in(1,0),(-1,0),(0,1),(0,-1):
-   a,b=y+Y,x+X
-   while h>a>=0<=b<w:
-    if g[a][b]==t:r[y+Y][x+X]=t;break
+   c,d=a,b=y+Y,x+X
+   while 15>a>=0<=b<15:
+    if g[a][b]==t:r[c][d]=t;break
     a+=Y;b+=X
  return r
