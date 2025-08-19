6e22d6b9333b44c892fe80db9d8b48338276a0c9
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-20 03:32:02 +0900

    002の微改善

--- 002/3_submission.py
+++ 002/3_submission.py
@@ -1,7 +1,6 @@
 def p(j):
  c=len(j);j=[[x or 4 for x in r]for r in j]
- d=1,0,-1,0
+ d=1-c,0,-1,0
  for z in range(9**5):
-  r,q,i=z%97%c,z%89%c,z%4
-  if j[r][q]>3:x,y=r+d[i],q+d[i-3];j[r][q]*=0<=x<c>y>=0<j[x][y]
+  r,q=z%97%c,z%89%c;j[r][q]*=j[r+d[z%4]][q+d[z%4-3]]*r*q*(r-c+1)+(j[r][q]==3)!=0
  return j
