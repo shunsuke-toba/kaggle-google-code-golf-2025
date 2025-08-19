38d27cafcee2f068c3fa303b5a7553f207e9fa71
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 00:09:56 +0900

    問題280: コードゴルフ最適化

--- 280/3_submission.py
+++ 280/3_submission.py
@@ -1,20 +1,16 @@
 def p(g):
- h=len(g);w=len(g[0])
- for r,c in[(i,j)for i in range(h)for j in range(w)if g[i][j]==2]:
-  u=r
-  while u and g[u-1][c]>2:u-=1
-  d=r
-  while d+1<h and g[d+1][c]>2:d+=1
-  l=c
-  while l and g[r][l-1]>2:l-=1
-  e=c
-  while e+1<w and g[r][e+1]>2:e+=1
-  for dx,dy in(-1,0),(1,0),(0,-1),(0,1):
-   x=r+dx;y=c+dy
-   if x<0 or y<0 or x>=h or y>=w or g[x][y]<1:
-    s=(d-u,e-l)[dx==0];L=(l,u)[dx==0];R=(e,d)[dx==0];C=(c,r)[dx==0]
-    for i in range(max(L,C-s),min(R,C+s)+1):
-     x,y=(r,i)if dx else(i,c);v=g[x][y]
-     while 0<=x<h and 0<=y<w:g[x][y]=v;x+=dx;y+=dy
-    break
+ *h,=map(list,g);H=len(g);W=len(g[0])
+ for i in range(H*W):
+  y=i//W;x=i%W
+  if h[y][x]-2:continue
+  for a,b in(1,0),(0,1),(-1,0),(0,-1):
+   if H>y+a>-1<x+b<W and h[y+a][x+b]<1:break
+  k=0;Y=y-a;X=x-b
+  while H>Y>-1<X<W and h[Y][X]==3:k+=1;Y-=a;X-=b
+  while 1:
+   g[y][x]=2
+   for t in range(1,k+1):
+    try:g[y+b*t][x+a*t]=g[y-b*t][x-a*t]=3
+    except:0
+   if not(H>(y:=y+a)>-1<(x:=x+b)<W and h[y][x]<1):break
  return g
