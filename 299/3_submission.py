ccbfd32ea818cbca86adb457bf8569a7a5fce329
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 03:01:22 +0900

    問題299: コードゴルフ最適化 - 行列転置と条件分岐の簡略化

--- 299/3_submission.py
+++ 299/3_submission.py
@@ -1,8 +1,4 @@
 def p(g):
- d='[AbZLTUbZLjGHSZNPxkiWgUPPPXjYPxNsmBLgUQQjXYmNsmBiWgFQQjXYmNQjTUQjXYQmNbRkLTFbuRLGjHRNbRBiWgFbuuRLGjHnNbRkiWgUbuRLGjHRNPxiWTFPPXjYPPxNbuuWTFbRLGjHuRNbZkLTFbSZLjGHZNPxBiWgFPPPPXjYxNbRLTUbRLGjHuRNsmkiWgUQsjXYsmNsmiWTFQjXYQmNbZBLgUbSSZLjGHvNsmkLTFQsjXYsmNbRBLgUbuuRLGjHnK]'
- m='SvZhtYhGXVHWbhVkFUgBTviSunRssQxePMANK,MejLJ}Kr]JtbH4,GEqFDOEC"DrcCkkBzqAyIz{"ywgxafwbfvniu2dtmes0]rp[qo[p":ofbnlamgfligkhhjebi2,hbag8,f0dec[d],caab0,a'
- m=[[m[i:i+2],m[i+2]]for i in range(0,len(m),3)]
- for r in m:d=d.replace(r[1],r[0])
- d=eval(d)
- for k in d:
-  if k['I']==g:g=k['O'];return g
+ i=sum(g,[]).index;g[y:=i(2)//6]=[2]*6
+ for r in g:r[x:=i(8)%6]=8
+ g[y][x]=4;return g
