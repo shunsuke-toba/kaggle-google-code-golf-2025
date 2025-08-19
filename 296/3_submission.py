fddb1925acd34e27a07657bf194909460e899a74
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 03:00:16 +0900

    問題296: コードゴルフ最適化 - インデックス計算の簡略化

--- 296/3_submission.py
+++ 296/3_submission.py
@@ -1,4 +1,1 @@
-def p(j):
- A=[[0]*3,[0]*3,[0]*3]
- for c in range(16):E,k=c//8%2*-2+c//2%2,c//4%2*-2+c%2;A[E][k]=max(A[E][k],j[E][k])
- return A
+p=lambda j,g=([0],[1,-2],[-1]):[[max(j[a][b]for a in A for b in B)for B in g]for A in g]
