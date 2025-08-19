f3a1b9b83fb42ec661b035c4c79f14b325581073
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-19 19:43:55 +0900

    問題264: インデックス計算とリスト内包表記の最適化

--- 264/3_submission.py
+++ 264/3_submission.py
@@ -1,11 +1,8 @@
 def p(g):
- r=range
- d={0:4,11:0,38:2,23:1,89:3,308:5,464:7,200:6,416:8}
- o=[[5]*9 for _ in r(9)]
+ r=range;d=11,23,38,89,0,308,200,464,416;o=[[5]*9for _ in[0]*9];a=r(9)
  for y in r(len(g)-2):
   for x in r(len(g[0])-2):
-   t=[g[y+a][x+b]for a in r(3)for b in r(3)]
-   if 0 in t:continue
-   k=d[sum(1<<i for i in r(9)if t[i]-5)]
-   for i in r(9):o[k//3*3+i//3][k%3*3+i%3]=t[i]
+   if all(t:=[g[y+i//3][x+i%3]for i in a]):
+    k=d.index(sum((t[i]!=5)<<i for i in a))
+    for i in a:o[k//3*3+i//3][k*3%9+i%3]=t[i]
  return o
