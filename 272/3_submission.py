df73cb142e2f56db63903bc8e694799f8dfe22ae
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 00:05:44 +0900

    問題272: 孤立セル判定の最適化

--- 272/3_submission.py
+++ 272/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):h,w=len(g),len(g[0]);return[[1if g[i][j]and all(g[i+a][j+b]==0for a,b in[(-1,0),(1,0),(0,-1),(0,1)]if 0<=i+a<h and 0<=j+b<w)else g[i][j]for j in range(w)]for i in range(h)]
+p=lambda g:[[c**(c<1 or c in r[j-1:j]+r[j+1:j+2]+[k[j]for k in g[i-1:i]+g[i+1:i+2]]) for j,c in enumerate(r)]for i,r in enumerate(g)]
