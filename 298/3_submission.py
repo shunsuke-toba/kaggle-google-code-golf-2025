ee301b652ec894c4a7d7a1f880856fa17418ee6d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 03:01:05 +0900

    問題298: コードゴルフ最適化 - 辞書内包表記とmap関数の活用

--- 298/3_submission.py
+++ 298/3_submission.py
@@ -1,1 +1,1 @@
-def p(j):A=len(j)//2;c=[j[i][i]for i in range(A)];E={c[i]:c[i-1]for i in range(A)};return[[E[i]for i in r]for r in j]
+def p(j):a=len(j)//2;return[[*map({j[i][i]:j[(t:=(i-1)%a)][t]for i in range(a)}.get,r)]for r in j]
