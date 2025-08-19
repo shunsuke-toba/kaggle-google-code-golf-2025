7b8cce67ccd0a5018d40e06e88c9928b657d99d6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 02:59:35 +0900

    問題294: コードゴルフ最適化 - 3x3領域判定の簡略化

--- 294/3_submission.py
+++ 294/3_submission.py
@@ -1,1 +1,5 @@
-p=lambda g:[[2 if g[i][j]==5and all(0<=i+d[0]<10and 0<=j+d[1]<10and g[i+d[0]][j+d[1]]==5 for d in[(-1,0),(1,0),(0,-1),(0,1)])else g[i][j]for j in range(10)]for i in range(10)]
+def p(g):
+ for i in range(64):
+  j=i%8;i//=8
+  if all(all(r[j:j+3])for r in g[i:i+3]):g[i+1][j+1]=2
+ return g
