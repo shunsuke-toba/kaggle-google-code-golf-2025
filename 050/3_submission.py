61f39ea055555c9742e69d64f98b7fbb5e39b3f6
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-19 21:14:33 +0900

    50の修正

--- 050/3_submission.py
+++ 050/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  for _ in g,g:
   for r in g:
-   t=[i for i,x in enumerate(r)if x>7];a,b=t[0],t[1]
-   if t[1:]:r[a+1:b]=[3]*(b+~a)
+   t=[i for i,x in enumerate(r)if x>7]
+   if t[1:]:r[t[0]+1:t[1]]=[3]*(t[1]+~t[0])
   g=list(map(list,zip(*g)))
  return g
