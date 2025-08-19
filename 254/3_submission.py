b97e2acf2490ea5f002758754c4d29f4551be3fd
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-19 19:39:03 +0900

    問題254: コードゴルフ最適化 - リスト内包表記への変換

--- 254/3_submission.py
+++ 254/3_submission.py
@@ -1,5 +1,1 @@
-def p(j):
- a=[r.count(5)for r in zip(*j)]
- for r in j:
-  for i in range(9):r[i]=[0,a[i]==max(a) or 2*(a[i]==min(filter(None,a)))][r[i]>4]
- return j
+def p(j):a=[r.count(5)for r in zip(*j)];return[[r[i]>4and(a[i]==max(a)or(a[i]==min({*a}-{0}))*2)for i in range(9)]for r in j]
