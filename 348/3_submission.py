73edb565bc9533cd2dd5bc46515318bb91b58d08
pech1 <pechi2327@gmail.com>
2025-08-19 23:45:20 +0900

    348:solution,submissionの更新

--- 348/3_submission.py
+++ 348/3_submission.py
@@ -1,11 +1,1 @@
-def p(j,A=range):
- c,E,k,W=len(j),len(j[0]),0,0
- for l in A(c):
-  for J in A(E):
-   if j[l][J]:k,W=l+2,J
- def s(l,J,a):
-  if 0<=J<E:j[l][J]=a
- for C in A(E):
-  k,a=k-1,7+C%2
-  for l in A(k):s(l,W-C,a);s(l,W+C,a)
- return j
+def p(g,E=enumerate):c=g[0].index(7);return[[(v,7+(j-c)%2)[abs(j-c)<sum(0<r[c]for r in g)-i]for j,v in E(r)]for i,r in E(g)]
