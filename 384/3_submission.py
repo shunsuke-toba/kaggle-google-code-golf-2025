74fd7b0a62c235880043c86747230a40d2a752e5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 01:18:01 +0900

    問題384のコードゴルフ最適化 (223 -> 154)

--- 384/3_submission.py
+++ 384/3_submission.py
@@ -1,1 +1,1 @@
-def p(j):A=[max(r)>0 for r in j].index(1);c=len(j)-1-[max(r)>0for r in j][::-1].index(1);p=[j for j,E in enumerate(zip(*j))if max(E)>0];E=p[0];k=p[-1];return[[x for x in r[E:k+1]for _ in[0]*2]for r in j[A:c+1]for _ in[0]*2]
+def p(g):y,x=zip(*[(i//9,i%9)for i in range(81)if g[i//9][i%9]]);return[[*sum(zip(r:=g[i//2][min(x):-~max(x)],r),())]for i in range(min(y)*2,-~max(y)*2)]
