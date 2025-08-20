a4d9bc03430840b003c15bc30fb7c5f663aac966
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 00:04:18 +0900

    問題153: コードゴルフ最適化 (488 -> 329)

--- 153/3_submission.py
+++ 153/3_submission.py
@@ -1,12 +1,8 @@
 def p(g):
- d={}
- for i in range(10):
-  for j in range(10):
-   if(v:=g[i][j]):d.setdefault(v,[]).append((i,j))
- a=[]
- for k,v in d.items():
-  I,J=zip(*v);i=min(I);j=min(J);h=max(I)-i+1;w=max(J)-j+1;m=sum(1<<(I-i)*3+J-j for I,J in v)
-  a.append((k,[m<<(3*y+x)for y in range(4-h)for x in range(4-w)]))
- (c1,p1),(c2,p2)=a
- t=max(((A,B)for A in p1 for B in p2 if A&B<1),key=lambda x:(x[0]|x[1]).bit_count())
- return [[(t[0]>>3*y+x&1)*c1+(t[1]>>3*y+x&1)*c2 for x in range(3)]for y in range(3)]
+ d={};[x and d.setdefault(x,set()).add(divmod(i,10))for i,x in enumerate(sum(g,[]))];f=lambda s:{(x-a,y-b)for a,b in[map(min,zip(*s))]for x,y in s};(a,A),(b,B)=d.items();A=f(A);B=f(B)
+ while 1:
+  if f({(i//3,i%3)for i in range(9)}-A)==B:
+   o=[[b]*3,[b]*3,[b]*3]
+   for x,y in A:o[x][y]=a
+   return o
+  a,b,A,B=b,a,B,A
