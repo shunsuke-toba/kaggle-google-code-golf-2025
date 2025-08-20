7c0e4ffbf9b3069e9babe7fb77449d7d0f664201
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 19:56:35 +0900

    問題224: コードゴルフ最適化 - 変数名短縮と条件式の簡略化

--- 224/3_submission.py
+++ 224/3_submission.py
@@ -1,1 +1,4 @@
-def p(g):import numpy as t;a=t.array(g);r,c=t.where(a==5);b=a[min(r)+1:max(r),min(c)+1:max(c)];b[[0,-1]]=b[:,[0,-1]]=a[a!=5].max();return a.tolist()
+def p(g):
+ E=enumerate;u,v=zip(*[(i,j)for i,s in E(g)for j,x in E(s)if x==5]);a,b=min(u)+1,max(u);c,d=min(v)+1,max(v);m=max({*sum(g,[])}-{5})
+ for i in range(a,b):g[i][c]=g[i][d-1]=m
+ g[a][c:d]=g[b-1][c:d]=[m]*(d-c);return g
