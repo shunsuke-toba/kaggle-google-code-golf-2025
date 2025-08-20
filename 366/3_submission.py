e40f6ef825baf204aad612654428a8ec0343e66a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 23:57:17 +0900

    問題366: コードゴルフ最適化 (1258 -> 1236)

--- 366/3_submission.py
+++ 366/3_submission.py
@@ -1,25 +1,25 @@
 def p(g):
- c=[0]*10;b=[[9e9,9e9,0,0]for _ in range(10)]
+ c=[0]*10;b=[[99]*2+[0]*2 for _ in[0]*10]
  for i,r in enumerate(g):
   for j,v in enumerate(r):
    c[v]+=1;m=b[v];m[0]=min(m[0],i);m[1]=min(m[1],j);m[2]=max(m[2],i);m[3]=max(m[3],j)
-  t,s,d=sorted(range(10),key=c.__getitem__)[7:];sx,sy,ex,ey=b[s];dx,dy,fx,fy=b[d]
- V=set();L=[];P=1,-1,0,0,0,0,1,-1
+ *_,t,s,d=sorted(range(10),key=c.__getitem__);sx,sy,ex,ey=b[s];dx,dy,fx,fy=b[d]
+ L=[];P=1,-1,0,0,0,0,1,-1
  for i in range(sx,ex+1):
   for j in range(sy,ey+1):
-   if (i,j)in V or g[i][j]in(s,t):continue
-   q=[(i,j)];V.add((i,j));C=[]
+   if g[i][j]in(s,t):continue
+   q=[(i,j)];C=[];v=g[i][j];g[i][j]=s;C+=(i,j,v),
    while q:
-    x,y=q.pop();C+=(x,y),
+    x,y=q.pop()
     for k in range(4):
      X=x+P[k];Y=y+P[k+4]
-     if sx<=X<=ex and sy<=Y<=ey and g[X][Y]-s and(X,Y)not in V:V.add((X,Y));q+=(X,Y),
+     if sx<=X<=ex and sy<=Y<=ey and g[X][Y]-s:v=g[X][Y];g[X][Y]=s;q+=(X,Y),;C+=(X,Y,v),
    L+=C,
- L.sort(key=lambda C:-sum(g[x][y]!=t for x,y in C))
+ L.sort(key=lambda C:-sum(v!=t for _,_,v in C))
  R=lambda a:[*zip(*a[::-1])]
  for C in L:
-  xs,ys=zip(*C);m=min(xs);a=[[t]*(max(ys)-(n:=min(ys))+1)for _ in range(max(xs)-m+1)];
-  for x,y in C:a[x-m][y-n]=g[x][y]
+  xs,ys,_=zip(*C);m=min(xs);a=[[t]*(max(ys)-(n:=min(ys))+1)for _ in range(max(xs)-m+1)];
+  for x,y,v in C:a[x-m][y-n]=v
   ok=0
   for _ in 0,1:
    for _ in'1234':
