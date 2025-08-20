cec09d5bd91f7f2d5b192e4b9124394b5b54981b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 23:54:05 +0900

    問題034: コードゴルフ最適化 - ループ統合と条件判定の簡略化 (348 -> 279)

--- 034/3_submission.py
+++ 034/3_submission.py
@@ -1,16 +1,11 @@
 def p(g):
- for i in range(81):
-  r=i//9;c=i%9;a=g[r][c]
-  if a:
-   t=a,g[r][c+1],g[r+1][c],g[r+1][c+1];o=r,c
-   for x in t:
-    if x-2:break
-   for j in 0,1,2,3:
-    if t[j]==2:
-     r,c=o
-     while 9>r>-2<c<9:
-      for k in 0,1,2,3:
-       u=r+k//2;v=c+k%2
-       if 9>u>-1<v<9:g[u][v]=x
-      r+=j//2*2-1;c+=j%2*2-1
-   return g
+ r=c=0;k=sum({*sum(g,[])})-2
+ while g[r][c]<1:c+=1;r+=c>8;c%=9
+ for i,j in[(i,j)for i in(0,1)for j in(0,1)if g[r+i][c+j]==2]:
+  a,b=r,c
+  while-2<a<9>-2<b<9:
+   for u in a,a+1:
+    for v in b,b+1:
+     if 0<=u<9>v>=0:g[u][v]=k
+   a+=i*2-1;b+=j*2-1
+ return g
