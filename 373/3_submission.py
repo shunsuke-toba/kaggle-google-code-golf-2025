c00b76ddbdde06fd74fd7d09c08f5bd3ea8bbb61
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-20 20:58:02 +0900

    373,385

--- 373/3_submission.py
+++ 373/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[[[g[i][j],g[1-i][j]][j%2]for j in range(6)]for i in range(2)]
+p=lambda g:[[g[i-j%2][0]for j in range(6)]for i in(0,1)]
--- 385/3_submission.py
+++ 385/3_submission.py
@@ -1,5 +1,1 @@
-def p(j,A=enumerate):
- for c,E in A(j):
-  for k,W in A(E):
-   if c<len(j)//2:j[c][k]=j[-(c+1)][k]
- return j
+p=lambda g:g[:4:-1]+g[5:]
