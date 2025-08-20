2535f385ef58621c79587c311b734044bf745dd1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 00:00:12 +0900

    問題198: コードゴルフ最適化 (448 -> 342)

--- 198/3_submission.py
+++ 198/3_submission.py
@@ -1,11 +1,8 @@
 def p(g):
- R=range;x=max(sum(g,[]));h=len(g);w=len(g[0])
- H=[i for i in R(h)if g[i].count(x)*2>w];V=[i for i,c in enumerate(zip(*g))if c.count(x)*2>h]
- o=[*map(list,g)]
- q=[(y,x)for y in H for x in R(w)if o[y][x]<1]+[(y,x)for x in V for y in R(h)if o[y][x]<1]
- for y,x in q:o[y][x]=4
- while q:
-  y,x=q.pop()
-  for Y,X in((y+1,x),(y-1,x),(y,x+1),(y,x-1)):
-   if h>Y>=0<=X<w and o[Y][X]<1:o[Y][X]=4;q+=[(Y,X)]
- return [[c or 3 for c in r]for r in o]
+ g=[*map(list,g)];h=len(g);w=len(g[0]);m=max(map(max,g))
+ q=[(y,x)for y in range(h)for x in range(w)if g[y][x]<1<(g[y].count(m)*2>w)+(sum(r[x]==m for r in g)*2>h)+1]
+ for y,x in q:
+  g[y][x]=4
+  for Y,X in (y+1,x),(y-1,x),(y,x+1),(y,x-1):
+   if h>Y>=0<=X<w and g[Y][X]<1:g[Y][X]=4;q+=[(Y,X)]
+ return [[c or 3 for c in r]for r in g]
