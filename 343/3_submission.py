5e2d6de92a0d0bbd4ed6e5862c9cf1c412784ce1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 08:18:01 +0900

    問題343のコードゴルフ最適化 (121 -> 111)

--- 343/3_submission.py
+++ 343/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- r=sum(map(any,c:=list(zip(*g))));n=1
+ c=[*zip(*g)];r=g[4].index(0);n=1
  while(c[:n]*9)[:r]!=c[:r]:n+=1
- return[*map(list,zip(*(c[:n]*15)[:15]))]
+ return[(i[:n]*15)[:15]for i in g]
