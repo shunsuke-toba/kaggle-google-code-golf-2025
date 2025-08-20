945265c06fc5a1c431419c17f4baa916a3b21a75
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 08:17:20 +0900

    問題344のコードゴルフ最適化 (171 -> 169)

--- 344/3_submission.py
+++ 344/3_submission.py
@@ -2,5 +2,5 @@ def p(g,e=enumerate):
  for i,r in e(g):
   for k,v in e(r):
    for a,b in(i+1,k),(i,k+1):
-    if(len(g)>a)&(b<len(r))and g[a][b]*v==6:r[k],g[a][b]=8*(v>2),8*(v<3)
+    if(len(g)>a)&(b<len(r))and g[a][b]*v==6:r[k],g[a][b]=v%2*8,~v%2*8
  return g
