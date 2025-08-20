fb00f15ab985d38fccf441eacfdda49695187f64
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 23:55:37 +0900

    問題044: コードゴルフ最適化 - リスト内包表記の簡略化 (661 -> 636)

--- 044/3_submission.py
+++ 044/3_submission.py
@@ -1,23 +1,22 @@
 def p(g):
- h=len(g);w=len(g[0]);v,A,B=set(),[],{}
- for k in range(h*w):
-  y,x=divmod(k,w)
+ v,A,B=set(),[],{}
+ for k in range(100):
+  y,x=divmod(k,10)
   if(y,x)in v:continue
-  c=g[y][x];q=[(y,x)];S={*q};f=1
+  c=g[y][x];q=[(y,x)];S={q[0]};f=c<1
   while q:
    y,x=q.pop()
    for Y,X in(y+1,x),(y-1,x),(y,x+1),(y,x-1):
-    if 0<=Y<h and 0<=X<w:
+    if 0<=Y<10 and 0<=X<10:
      if g[Y][X]==c:
       if(Y,X)not in S:S.add((Y,X));q.append((Y,X))
-     elif c<1 and g[Y][X]-5:f=0
-    elif c<1:f=0
+     elif f and g[Y][X]-5:f=0
+    elif f:f=0
   v|=S;y,x=map(min,zip(*S));s={(i-y,j-x)for i,j in S}
-  if c<1:f and A.append((s,y,x))
+  if f:A.append((s,y,x))
   elif c-5:B[c]=B.get(c,[])+[(s,S,y,x)]
  r=[*map(list,g)]
- for c in B:
-  b=B[c]
+ for c,b in B.items():
   if b[1:]:continue
   s,b,y,x=b[0]
   for t,Y,X in A:
