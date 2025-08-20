554e1636083c6a54e001a49f3564351be3387f3c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 08:16:35 +0900

    問題362のコードゴルフ最適化 (124 -> 90)

--- 362/3_submission.py
+++ 362/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- a=str(g).count('5');r=[*map(list,g)]
- for k in range(100):u,v=k//10,k%10;x=g[u-a][v];r[u][v-a]=x*(x!=5)
- return r
+def p(g):d=str(g).count('5');return[[c*(c!=5)for c in r[d:]+r[:d]]for r in g[-d:]+g[:-d]]
