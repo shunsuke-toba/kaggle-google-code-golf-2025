8219c9403539108a3821912db7fde0cc54b8464b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 19:57:01 +0900

    問題259: コードゴルフ最適化実装

--- 259/3_submission.py
+++ 259/3_submission.py
@@ -1,2 +1,1 @@
-import numpy as n
-def p(g):g=n.array(g);y,x=n.where(z:=g>1);return (g*z)[y.min():y.max()+1,x.min():x.max()+1].tolist()
+def p(g):a,b=zip(*[(i,j)for i,r in enumerate(g)for j,c in enumerate(r)if c>1]);return[[c*(c>1)for c in r[min(b):-~max(b)]]for r in g[min(a):-~max(a)]]
