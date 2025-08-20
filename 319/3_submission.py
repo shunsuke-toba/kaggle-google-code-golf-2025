13fada41e2c94758cbdc75b104e2fb8b7682a716
Shunsuke Toba <toba@third-inc.co.jp>
2025-08-21 00:33:04 +0900

    319

--- 319/3_submission.py
+++ 319/3_submission.py
@@ -1,20 +1,19 @@
-def p(g,R=range):
- L=len;h=L(g);w=L(g[0]);n=10;a=[99]*n;b=[99]*n;c=[-1]*n;d=[-1]*n;e=[0]*n
- for y in R(h):
-  for z in R(w):x=g[y][z];e[x]+=1;a[x]=min(a[x],y);c[x]=max(c[x],y);b[x]=min(b[x],z);d[x]=max(d[x],z)
- m=e.index(max(e));r=R(1,n)
- for x in r:
-  if x==m or c[x]<a[x]:continue
-  y=z=2
-  for p in R(-((c[x]-a[x]+1)&1),1):
-   for q in R(-((d[x]-b[x]+1)&1),1):
-    if all((P:=sum(0<=s<h and 0<=t<w and g[s][t]==x for s in(u,u+1)for t in(v,v+1)))<1 or P==sum((0<=s<h)*(0<=t<w)for s in(u,u+1)for t in(v,v+1))for u in R(a[x]+p,c[x]+p+1,2)for v in R(b[x]+q,d[x]+q+1,2)):y=p;z=q
-  if y>1:continue
-  y-=a[x]&1;z-=b[x]&1
-  t=[[any(0<=s<h and 0<=t<w and g[s][t]==x for s in(i,i+1)for t in(j,j+1))for j in R(z,w,2)]for i in R(y,h,2)]
-  for k in r:
-   if k in{x,m}or c[k]<a[k]:continue
-   for u in R(-h,h):
-    for v in R(-w,w):
-     if all((0<=u+i<h and 0<=v+j<w and g[u+i][v+j]==k)==t[i][j]for i in R(L(t))for j in R(L(t[0]))):
-      return [[k*(g[i][j]==k)or m for j in R(b[k],d[k]+1)]for i in R(a[k],c[k]+1)]
+def p(g,R=range,L=len):
+ p,C=[],sorted({*(s:=sum(g,[]))},key=s.count)
+ for c in C[:3]:
+  h=[r for r in g]
+  for _ in R(4):
+   while h[-1].count(c)<1:h.pop()
+   h=[*map(list,zip(*h[::-1]))]
+  p+=[[[x*(x==c)for x in r]for r in h]]
+ for b in R(9):
+  q,r=p[b//3],p[b%3]
+  n,m,N,M=L(q)*2,L(q[0])*2,L(r),L(r[0])
+  s=[[q[i//2][j//2]for j in R(m)]for i in R(n)]
+  for i in R(n-N+1):
+   for j in R(m-M+1):
+    f=q!=r
+    for z in R(N*M):
+     x,y=z//M,z%M
+     if(s[i+x][j+y]>0)!=(r[x][y]>0):f=0
+    if f:return [[x+(x<1)*C[3]for x in r]for r in q]
