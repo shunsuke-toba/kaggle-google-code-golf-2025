2e92079ce07320b6cdd77c9b357098ee3a850f66
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 19:47:48 +0900

    問題091: numpyライブラリ削除とピュアPython実装への変更

--- 091/3_submission.py
+++ 091/3_submission.py
@@ -1,2 +1,4 @@
-from numpy import*
-def p(g):g=array(g);a,b=where(g==5);return g[max(a.min()-1,0):a.max()+2,b.min():b.max()+1].tolist()
+def p(g):
+ E=enumerate
+ a,b=zip(*[(i,j)for i,r in E(g)for j,x in E(r)if x==5])
+ return[r[min(b):max(b)+1]for r in g[a[0]-(a[0]>0):a[-1]+2]]
