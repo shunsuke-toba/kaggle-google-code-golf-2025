c52dfaaaa5b1a9cb42c312bfcfa760e0a9bff0f1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 19:55:36 +0900

    問題217: コードゴルフ最適化 - 変数名短縮と条件式の簡略化

--- 217/3_submission.py
+++ 217/3_submission.py
@@ -1,2 +1,1 @@
-from numpy import*
-p=lambda g:(a:=array(g).reshape(3,3,3,3).swapaxes(1,2),b:=a[a.any((2,3))][0],kron(b>0,b).tolist())[-1]
+p=lambda g:(a:=[*zip(*filter(sum,zip(*filter(sum,g))))],r:=range(9),[[a[i//3][j//3]and a[i%3][j%3]for j in r]for i in r])[2]
