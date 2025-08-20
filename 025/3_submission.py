564b59c05f54e151993eb8301a7013e5354f797a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-20 23:08:17 +0900

    問題025: コードゴルフ最適化 - 転置処理の統一化で短縮

--- 025/3_submission.py
+++ 025/3_submission.py
@@ -1,16 +1,9 @@
-def p(g,E=enumerate):
- w=len(g[0]);t=[*zip(*g)];o=[[0]*w for _ in g]
- for c in{*sum(g,[])}-{0}:
-  for x,col in E(t):
-   if col.count(c)==len(g):
-    for y,r in E(g):
-     o[y][x]=c
-     if c in r[:x]:o[y][x-1]=c
-     if c in r[x+1:]:o[y][x+1]=c
-  for y,r in E(g):
-   if r.count(c)==w:
-    o[y]=[c]*w
-    for x,col in E(t):
-     if c in col[:y]:o[y-1][x]=c
-     if c in col[y+1:]:o[y+1][x]=c
+def p(g,e=enumerate):
+ w=len(g[0]);o=[w*[0]for _ in g]
+ for _ in 0,1:
+  for x,k in e(zip(*g)):
+   if{*k}=={c:=k[0]}!={0}:
+    for y,r in e(g):
+     q=o[y];q[x-(c in r[:x])]=q[x]=q[x+(c in r[x+1:])]=c
+  g,o=[*zip(*g)],[*map(list,zip(*o))]
  return o
