61e5ea2cec825e1ba1c6b817476514930531cadd
pech1 <pechi2327@gmail.com>
2025-08-21 02:05:24 +0900

    286:微改善(実行時間が怪しいか?)

--- 286/3_submission.py
+++ 286/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
- h=len(g);w=len(g[0]);d=0,1,0,-1;t=sum(g,[]);c=sum({*t}-{0,8});q=[divmod(i,w)for i,v in enumerate(t)if v%8]
- for i,j in q:
-  for k in 0,1,2,3:
-   x=i+d[k];y=j+d[k-1]
-   if h>x>~0<y<w and g[x][y]<1:g[x][y]=c-g[i][j];q+=(x,y),
+ h=len(g);w=len(g[0]);t=[*{*sum(g,[])}-{0,8}];d=1,0,-1,0
+ for z in range(9**6):
+  r,q=z%97%h,z%89%w
+  if(h>(i:=r+d[z%4])>-1<(j:=q+d[z%4-3])<w)and(g[r][q]-8)*(0<(c:=g[i][j])!=8):g[r][q]=t[c==t[0]]
  return g
