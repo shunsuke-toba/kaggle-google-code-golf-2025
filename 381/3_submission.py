85ee35dbad65a22e6b5897de0db4819764f4371f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 08:06:05 +0900

    問題381のコードゴルフ最適化 (205 -> 115)

--- 381/3_submission.py
+++ 381/3_submission.py
@@ -1,10 +1,4 @@
-def p(j,A=range):
- c=len(j)
- for E in A(1,c-1):
-  k=W=0
-  for l in A(c):
-   J=j[E][l];k=[k,1][k<1and J>1]
-   if k==1and J<1:k=2;W=[W,l][~W]
-   if k>1and J>1:
-    for a in A(W,l):j[E][a]=9;k=1;W=0
- return j
+def p(g):
+ for r in g[1:9]:
+  if 2in r:i=r.index(2);j=10-r[::-1].index(2);r[i:j]=[x or 9for x in r[i:j]]
+ return g
