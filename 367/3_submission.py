fb35867ae60d5181593d264f0d6feceeb5eb1ebe
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-21 21:09:27 +0900

    367: 258 -> 256

--- 367/3_submission.py
+++ 367/3_submission.py
@@ -2,7 +2,7 @@ def p(g):
  h,w=len(g),len(g[0]);d=-1,0,1,0
  for k in range(9**5):
   r,c,s=k%97%h,k%89%w,k%4;x,y=d[s]+r,d[~s]+c;t=g[r][c]
-  try:g[x-d[~s]-h][x-r+y]|=([g[r+d[s+~i]][c+d[s-i]]for i in range(4)]==[5,t-5,0,5])*4
+  try:g[x-y+c-h][x-r+y]|=([g[r+d[s+~i]][c+d[s-i]]for i in range(4)]==[5,t-5,0,5])*4
   except:0
   if h>x>-1<y<w>5>t:g[x][y]|=t
  return g
