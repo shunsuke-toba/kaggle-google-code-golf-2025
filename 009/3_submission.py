cfb09af629ae9d258fa905983a5e650c308015f4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 00:01:28 +0900

    009: コードゴルフ最適化（スペース削除とリスト展開構文） (258 -> 252)

--- 009/3_submission.py
+++ 009/3_submission.py
@@ -3,6 +3,6 @@ def p(g):
  for c in range(20):
   for r in g:
    t=[i for i,x in enumerate(r)if x==(d:=c//2)]
-   if d>0<len(t)==4:r[t[1]+1:t[-2]]=[b+(d+10-b)*(r[i]!=b) for i in range(t[1]+1,t[-2])]
-  g=list(map(list,zip(*g)))
- return [[x%10 for x in r] for r in g]
+   if d>0<len(t)==4:r[t[1]+1:t[-2]]=[b+(d+10-b)*(r[i]!=b)for i in range(t[1]+1,t[-2])]
+  g=[*map(list,zip(*g))]
+ return[[x%10for x in r]for r in g]
