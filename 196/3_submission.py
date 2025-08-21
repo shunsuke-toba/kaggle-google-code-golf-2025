1e802b5e472563c258b6fffa8e91cf049e5154a1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:56:16 +0900

    196: コードゴルフ最適化 (339 -> 311)

--- 196/3_submission.py
+++ 196/3_submission.py
@@ -1,13 +1,10 @@
 def p(g):
- h=len(g);w=len(g[0])
+ w=h=len(g)
  def f(i,j):
   if not(h>i>=0<=j<w)or g[i][j]-1:return[]
-  g[i][j]=0
-  return[(i,j)]+f(i+1,j)+f(i-1,j)+f(i,j+1)+f(i,j-1)
+  g[i][j]=0;return[(i,j)]+f(i+1,j)+f(i-1,j)+f(i,j+1)+f(i,j-1)
  for k in range(h*w):
-  i=k//w;j=k%w
-  if c:=f(i,j):
-   Y,X=zip(*c);a,b=min(Y),max(Y);d,e=min(X),max(X);t=1
-   if len(c)==2*(b-a+e-d)and b-a>1<e-d:t=3
+  if c:=f(k//w,k%w):
+   y,x=zip(*c);a,b=min(y),max(y);d,e=min(x),max(x);t=1+2*(len(c)==2*(b-a+e-d)and b-a>1<e-d)
    for y,x in c:g[y][x]=t
  return g
