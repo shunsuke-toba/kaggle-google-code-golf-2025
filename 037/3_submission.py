2b4fa3dd2c1764f50d976c69966ccd6f9242b1c9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:28:35 +0900

    037: コードゴルフ最適化の実装 (207 -> 169)

--- 037/3_submission.py
+++ 037/3_submission.py
@@ -1,9 +1,9 @@
-def p(g,R=range):
+def p(g):
  d={}
- for y in R(10):
-  for x in R(10):
-   v=g[y][x]
-   if v:
-    try:a,b=d[v];s=1-2*(y<a);t=1-2*(x<b);[g[a+i*s].__setitem__(b+i*t,v)for i in R((y-a)*s+1)]
-    except:d[v]=(y,x)
+ for k in range(100):
+  if v:=g[y:=k//10][x:=k%10]:
+   try:
+    a,b=d[v];n=y-a
+    while n+1:g[a+n][b+n*(1-2*(x<b))]=v;n-=1
+   except:d[v]=y,x
  return g
