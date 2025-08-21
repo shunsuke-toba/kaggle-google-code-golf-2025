b13ca6854d3d240d47c518ea19ed950fa6d08d5e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:30:23 +0900

    043: コードゴルフ最適化の実装 (110 -> 86)

--- 043/3_submission.py
+++ 043/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):a=[i for i,v in enumerate(g[0])if v>4];[r.__setitem__(i,2)for r in g if r[-1]>4 for i in a];return g
+def p(g):[r.__setitem__(i,2)for r in g for i,v in enumerate(g[0])if v*r[-1]];return g
