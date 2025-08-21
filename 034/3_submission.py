d8363b906a9be5120cc8c24a64b79d80b27d1965
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:26:38 +0900

    034: コードゴルフ最適化の実装 (279 -> 275)

--- 034/3_submission.py
+++ 034/3_submission.py
@@ -1,11 +1,11 @@
 def p(g):
- r=c=0;k=sum({*sum(g,[])})-2
- while g[r][c]<1:c+=1;r+=c>8;c%=9
- for i,j in[(i,j)for i in(0,1)for j in(0,1)if g[r+i][c+j]==2]:
+ k=sum({*sum(g,[])})-2;r=c=0
+ while g[r][c]<1:c=(c+1)%9;r+=c<1
+ for i,j in[(i,j)for i in(0,1)for j in(0,1)if g[r+i][c+j]-k]:
   a,b=r,c
-  while-2<a<9>-2<b<9:
+  while-2<a<9>b>-2:
    for u in a,a+1:
     for v in b,b+1:
-     if 0<=u<9>v>=0:g[u][v]=k
+     if-1<u<9>v>-1:g[u][v]=k
    a+=i*2-1;b+=j*2-1
  return g
