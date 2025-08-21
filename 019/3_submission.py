054619070524ae2796f9f2a52eeba905034f05e4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:19:20 +0900

    019: コードゴルフ最適化の実装 (213 -> 205)

--- 019/3_submission.py
+++ 019/3_submission.py
@@ -1,9 +1,8 @@
 def p(g):
- g=[r*2 for _ in(0,1)for r in g]
- h=len(g);w=len(g[0])
+ g=[r+r for _ in(0,1)for r in g];h=len(g);w=len(g[0])
  for k in range(h*w):
   if g[r:=k//w][c:=k%w]%8:
    for i in r-1,r+1:
     for j in c-1,c+1:
-     if h>i>-1<w>j>-1 and g[i][j]<1:g[i][j]=8
+     if h>i>-1<j<w:g[i][j]=g[i][j]or 8
  return g
