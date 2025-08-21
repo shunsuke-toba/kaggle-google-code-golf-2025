9d40ee91ebb69315c94b5a0250f9bac0a8973ad1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:26:04 +0900

    033: コードゴルフ最適化の実装 (130 -> 102)

--- 033/3_submission.py
+++ 033/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):k=g[5][5];[g[r].__setitem__(c,k)for r in range(17)for c in range(17)if r%6<5>c%6 and g[r][c]<1 and g[r%6][c%6]];return g
+p=lambda g,R=range(17):[g[r][c]<g[r%6][c%6]and g[r].__setitem__(c,g[5][5])for r in R for c in R]and g
