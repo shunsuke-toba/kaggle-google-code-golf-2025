a39eab73eededbb9eeddb0ba54aa27c9e47b4d03
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:49:36 +0900

    358: コードゴルフ最適化 - 巨大矩形検出の簡略化 (315 -> 279)

--- 358/3_submission.py
+++ 358/3_submission.py
@@ -1,10 +1,11 @@
 def p(g,R=range):
- h,w=len(g),len(g[0])
- for d in 0,1:
-  H,W=(h,w)if d else(w,h)
-  for k in R(H):
-   c=sum(1 for j in R(W) if(g[k][j] if d else g[j][k]))
-   for m in R(2*W):
-    y=2*W-1-m;j,x=(m,m-c)if m<W else(y,y+c)
-    if 1<c<W>x>=0<(v:=g[k][x] if d else g[x][k]):(g[k] if d else g[j])[j if d else k]=v
+ r=0
+ while len({*g[r]})<3:r+=1
+ a=g[r]
+ s=[*filter(None,a)];d=a.index(s[0])
+ for j in R(len(a)):a[j]=s[(j-d)%len(s)]
+ C=(*zip(*g),);c=0
+ while len({*C[c]})<3:c+=1
+ s=[*filter(None,C[c])];d=C[c].index(s[0])
+ for i in R(len(g)):g[i][c]=s[(i-d)%len(s)]
  return g
