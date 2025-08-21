8b0e8db1edf7c8fcd7db28a3e70ac55e38019e7e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:31:11 +0900

    046: コードゴルフ最適化の実装 (770 -> 344)

--- 046/3_submission.py
+++ 046/3_submission.py
@@ -1,31 +1,14 @@
 def p(g):
- import random
- def s(g):
-  z=[i for i in range(len(g[0]))if all(g[j][i]==0for j in range(3))]+[len(g[0])]
-  l=0
-  for r in z:
-   n=5
-   for _ in[1]*2:
-    for i in range(3):
-     for j in range(l,r):
-      if g[i][j]%5:n=g[i][j]
-      elif g[i][j]==5:g[i][j]=n
-   l=r+1
-  p=0;S=0;R=[[0]*(len(g[0])-len(z)+1)for _ in range(3)]
-  for j in range(len(g[0])):
-   if j in z:S=random.randint(0,2);continue
-   for i in range(3):R[i][p]=g[(i+S)%3][j]
-   p+=1
-  y=1
-  for j in range(p):
-   if R[0][j]and R[1][j]==0and R[2][j]:return
-   y1,y2=2,0
-   for i in range(3):
-    if R[i][j]:y1,y2=min(y1,i),max(y2,i)
-   if y1==y:y=y2
-   elif y2==y:y=y1
-   else:return
-  return R
- for _ in[1]*999:
-  G=[[g[i][j]for j in range(len(g[0]))]for i in range(3)];r=s(G)
-  if r:return r
+ c=[*filter(any,zip(*g))];r=[[]for _ in g];o=e=0
+ while c:
+  j=1
+  while j<len(c)and 5 not in c[j]:j+=1
+  k=j+(j<len(c));s=c[:k];c=c[k:]
+  d=next(v for t in s for v in t if v%5)
+  if r[0]:o+=s[0].index(5)-e
+  e=5 in s[-1]and s[-1].index(5)
+  for t in s:
+   for x in r:x+=0,
+   for y,v in enumerate(t):
+    if v:r[y-o][-1]=d
+ return r
