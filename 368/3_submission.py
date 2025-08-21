54f0063922c4ad481fb3aaca37b209813732891b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 22:17:20 +0900

    368: コードゴルフ最適化 (366 -> 290)

--- 368/3_submission.py
+++ 368/3_submission.py
@@ -1,15 +1,14 @@
-def p(j,A=range):
-	c=len(j);E=1;k,W=0,0;l=[0,5];J,a=0,0
-	for C in A(c):
-		for e in A(c):
-			if j[C][e]not in l and E:
-				E=0;J,a=C,e;K=C;w=e
-				while K<c and j[K][e]not in l:K+=1
-				while w<c and j[C][w]not in l:w+=1
-				k=K-C;W=w-e
-	for C in A(c-k+1):
-		for e in A(c-W+1):
-			if j[C][e]==5:
-				for L in A(k):
-					for b in A(W):j[C+L][e+b]=j[J+L][a+b]
-	return j
+def p(g,r=range):
+ c=len(g)
+ for q in r(c*c):
+  y=q//c;x=q%c
+  if g[y][x]%5:
+   Y,X=y,x;h=w=1
+   while y+h<c and g[y+h][x]%5:h+=1
+   while x+w<c and g[y][x+w]%5:w+=1
+   break
+ for y in r(c-h+1):
+  for x in r(c-w+1):
+   if g[y][x]==5:
+    for q in r(h):g[y+q][x:x+w]=g[Y+q][X:X+w]
+ return g
