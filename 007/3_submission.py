7f5377ac87c506b6111ac7a2636a53a53285cf4f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:09:46 +0900

    007: コードゴルフ最適化の実装 (112 -> 107)

--- 007/3_submission.py
+++ 007/3_submission.py
@@ -1,1 +1,1 @@
-def p(g,r=range(7)):d={(i+j)%3:c for i in r for j in r if(c:=g[i][j])};return[[d[(i+j)%3]for j in r]for i in r]
+def p(g,r=range(7)):d={i%3:v for i,v in enumerate(sum(g,[]))if v};return[[d[(i+j)%3]for j in r]for i in r]
