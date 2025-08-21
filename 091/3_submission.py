32cb2f11d90dc5d89adbb05f6dce1bb2ecf85116
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:57:09 +0900

    091: コードゴルフ最適化の実装 (140 -> 127)

--- 091/3_submission.py
+++ 091/3_submission.py
@@ -1,4 +1,2 @@
 def p(g):
- E=enumerate
- a,b=zip(*[(i,j)for i,r in E(g)for j,x in E(r)if x==5])
- return[r[min(b):max(b)+1]for r in g[a[0]-(a[0]>0):a[-1]+2]]
+ e=enumerate;a,b=zip(*[(i,j)for i,r in e(g)for j,x in e(r)if x==5]);return[r[b[0]:b[1]+1]for r in g[a[0]-1:a[-1]+2]]
