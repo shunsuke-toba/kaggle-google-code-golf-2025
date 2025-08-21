7ee742a5aed61869c7971ae21a5392bf1dc84868
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 00:00:03 +0900

    137: コードゴルフ最適化 (365 -> 218)

--- 137/3_submission.py
+++ 137/3_submission.py
@@ -1,14 +1,1 @@
-def p(a):
- h,w=len(a),len(a[0]);R=range
- r,c=zip(*[(i,j)for i in R(h)for j in R(w)if a[i][j]])
- k=a[r[0]][c[0]];y,x=min(r),min(c);Y,X=max(r),max(c);d=(Y-y)//2
- while 1:
-  for j in R(x,X+1):
-   for b in y,Y:
-    if h>b>-1<j<w:a[b][j]=k
-  for i in R(y,Y+1):
-   for b in x,X:
-    if w>b>-1<i<h:a[i][b]=k
-  y-=d;Y+=d;x-=d;X+=d
-  if y<0>x and Y>=h<=X>=w:break
- return a
+def p(g):k=len(g);t=[(i,j)for i in range(k)for j in range(k)if g[i][j]];r=sorted(i for i,_ in t);a=r[1];b=sum(j for _,j in t)//3;s=a-r[0];return [[g[a][b]*(max(a-i,i-a,b-j,j-b)%s<1)for j in range(k)]for i in range(k)]
