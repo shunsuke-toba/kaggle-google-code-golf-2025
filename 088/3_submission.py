7566bd29bffd1a41ec57fd12c5237723ae82425c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:54:47 +0900

    088: コードゴルフ最適化の実装 (171 -> 136)

--- 088/3_submission.py
+++ 088/3_submission.py
@@ -1,5 +1,5 @@
-def p(g,R=range):
- for _ in R(96):
-  if sum(g[-1])<1:g=g[:-1]
-  g=[*map(list,zip(*g[::-1]))]
- return [[(g[i][j]>0)*g[0][0]for j in R(1,len(g[0])-1)]for i in R(1,len(g)-1)]
+def p(g):
+ for _ in[0]*96:
+  if sum(g[-1])<1:g.pop()
+  g=[*zip(*g[::-1])]
+ k=g[0][0];return[[c and k for c in r[1:-1]]for r in g[1:-1]]
