61039687a27427611162ce2ad423bac099fa41d3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:21:53 +0900

    024: コードゴルフ最適化の実装 (128 -> 89)

--- 024/3_submission.py
+++ 024/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(c:=[2 in z for z in zip(*g)],[(r:=next((v for v in R if v&1),0)) and [r]*len(R) or [2*i for i in c] for R in g])[1]
+p=lambda g:(c:=[2*(2 in z)for z in zip(*g)])and[(m:=max(R))%2*[m]*len(c)or c for R in g]
