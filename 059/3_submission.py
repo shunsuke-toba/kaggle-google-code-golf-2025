cd068d63e9560a55aad8178421851b8cb00b1754
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:36:38 +0900

    059: コードゴルフ最適化の実装 (208 -> 190)

--- 059/3_submission.py
+++ 059/3_submission.py
@@ -1,7 +1,5 @@
 def p(g):
- w=[0]*9
- for y,r in enumerate(g):
-  for x,v in enumerate(r):
-   if 0<v!=5:w[y//4*3+x//4]+=1;E=v
- m=max(w);R=range(11)
- return [[5*(i%4==3 or j%4==3) or E*(w[i//4*3+j//4]==m) for j in R]for i in R]
+ w=[0]*9;R=range(11)
+ for i,v in enumerate(sum(g,[])):
+  w[i//44*3+i%11//4]+=v%5>0;v%5 and(E:=v)
+ m=max(w);return[[5*(i%4>2 or j%4>2)or E*(w[i//4*3+j//4]==m)for j in R]for i in R]
