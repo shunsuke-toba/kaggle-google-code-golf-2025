f56cfb7106aec7fa1603540094d09dc095cc0ad6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:59:27 +0900

    093: コードゴルフ最適化の実装 (222 -> 213)

--- 093/3_submission.py
+++ 093/3_submission.py
@@ -1,9 +1,8 @@
 def p(g):
- f=lambda r:list(map(list,zip(*r)))
- if T:=all(any(r)for r in g):g=f(g)
+ f=lambda x:[*map(list,zip(*x))]
+ if T:=all(map(any,g)):g=f(g)
  for z in range(9**5):
   x,y=z//14%12+1,z%14
   if g[x][y]>0:g[x][y]=5
-  else:g[x][y]=g[i:=(x+1-2*(x<7))][y];g[i][y]=0
- if T:g=f(g)
- return g
+  else:g[x][y]=g[i:=x+1-2*(x<7)][y];g[i][y]=0
+ return f(g)if T else g
