6acc792a3f1dc718ad11373bd30136a752bede3c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 20:24:38 +0900

    107: コードゴルフ最適化 (416 -> 321)

--- 107/3_submission.py
+++ 107/3_submission.py
@@ -1,12 +1,11 @@
-def p(j,u=range):
- A=len(j);c=len(j[0]);E=len({*sum(j,[])}-{0})
- j=[[j[W//E][l//E]for l in u(c*E)]for W in u(A*E)];A*=E;c*=E
- for k in u(min(A,c),0,-1):
-  for W in u(A-k+1):
-   for l in u(c-k+1):
-    J=j[W][l]
-    if J and all(r[l:l+k]==[J]*k for r in j[W:W+k]):
-     for a,C in(-1,-1),(-1,k),(k,-1),(k,k):
-      e=W+a;K=l+C
-      while-1<e<A and-1<K<c and not j[e][K]:j[e][K]=2;e+=a>0 or-1;K+=C>0 or-1
-     return j
+def p(g):
+ n=len({*sum(g,[])}-{0})
+ for m in range(16):
+  i,j=m>>2,m&3
+  if g[i][j]==g[i+1][j]==g[i][j+1]==g[i+1][j+1]>0:
+   r=range(L:=5*n);g=[[g[x//n][y//n]for y in r]for x in r]
+   for s in-1,1:
+    for t in-1,1:
+     x=n*(i+s+1)-(s<0);y=n*(j+t+1)-(t<0)
+     while-1<x<L>y>-1<g[x][y]<1:g[x][y]=2;x+=s;y+=t
+   return g
