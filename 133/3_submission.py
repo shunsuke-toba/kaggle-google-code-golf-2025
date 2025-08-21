be650679419c97bb3dad38ecb49f03b659dcb735
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 00:05:00 +0900

    133: コードゴルフ最適化 (範囲チェック削除による短縮) (761 -> 722)

--- 133/3_submission.py
+++ 133/3_submission.py
@@ -12,13 +12,11 @@ def p(g):
   for k in{*(d:=[g[y][x]for y,x in c])}:
    if d.count(k)<2 and len(c)>m:
     p=c[d.index(k)];O=[(y-p[0],x-p[1])for y,x in c if g[y][x]^k];K=k;m=len(c)
- if m<1:return g
  o=[*map(list,g)]
  for c in C:
   if(A:=[(y,x)for y,x in c if g[y][x]==K]):
    Y,X=zip(*A);a=min(Y);b=min(X);Y=max(Y)+1-a;X=max(X)+1-b;L=next((v for y,x in c if(v:=g[y][x])^K),0)
    for u,v in O:
     for y in range(s:=a+u*Y,s+Y):
-     for x in range(t:=b+v*X,t+X):
-      if h>y>=0<=x<w:o[y][x]=L
+     for x in range(t:=b+v*X,t+X):o[y][x]=L
  return o
