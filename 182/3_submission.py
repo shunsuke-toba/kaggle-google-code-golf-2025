65d65b55cdde594cc3fcfc01f00768dab9435133
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:57:01 +0900

    182: コードゴルフ最適化 (370 -> 330)

--- 182/3_submission.py
+++ 182/3_submission.py
@@ -1,12 +1,10 @@
 def p(g):
- R=range;h=len(g);d=0,0,1,0
- for z in R((h-6)**2):
-  y,x=divmod(z,h-6)
-  if all(g[y+d[k]][x+d[k-2]]==5 for k in R(3)):
-   t={(i,j)for i in R(5)for j in R(5)if g[y+i+1][x+j+1]>1}
-   i,j=next(iter(t));c=g[y+i+1][x+j+1]
- for z in R((h-4)**2):
-  y,x=divmod(z,h-4)
+ R=range;h=len(g)-6
+ for z in R(h**2):
+  y,x=divmod(z,h)
+  if g[y][x]==g[y][x+1]==g[y+1][x]==5 and(t:={(i,j)for i in R(5)for j in R(5)if(v:=g[y+i+1][x+j+1])>1 and(c:=v)}):break
+ for z in R((h+2)**2):
+  y,x=divmod(z,h+2)
   if {(i,j)for i in R(5)for j in R(5)if g[y+i][x+j]==1}==t:
    for i,j in t:g[y+i][x+j]=c
  return g
