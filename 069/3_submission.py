d73430e92458727027e806c40a1f90be5b4ec900
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:43:16 +0900

    069: コードゴルフ最適化の実装 (448 -> 310)

--- 069/3_submission.py
+++ 069/3_submission.py
@@ -1,16 +1,13 @@
 def p(g):
- s={(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v};t=[];d=1,0,-1,0,1
- while s:
-  q=[s.pop()];L=[];C=set();a=b=99
-  while q:
-   i,j=q.pop();L+=[(i,j)];C|={g[i][j]};a=min(a,i);b=min(b,j)
-   for k in range(4):
-    u=i+d[k];v=j+d[k+1]
-    if(u,v)in s:s.remove((u,v));q+=[(u,v)]
-  if len(C)>1:P=L;A,B=a,b
-  else:t+=[(a,b)]
- O=[]
- for i,j in P:O+=((i-A,j-B,g[i][j]),);g[i][j]=0
- for a,b in t:
-  for x,y,v in O:g[a+x][b+y]=v
- return g
+ r=range(10);p=[]
+ for i in r:
+  for j in r:
+   if(v:=g[i][j])%8:p+=[(i,j,v)];g[i][j]=0
+ i,j,_=p[0];p=[(x-i,y-j,z)for x,y,z in p];o=[[0]*10 for _ in r]
+ for i in r:
+  for j in r:
+   try:
+    if all(g[i+x][j+y]==8 for x,y,_ in p):
+     for x,y,v in p:o[i+x][j+y]=v;g[i+x][j+y]=0
+   except:0
+ return o
