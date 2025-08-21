3eb5c8b39804e41024ad3e6991bf64c015096db8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:42:41 +0900

    369: コードゴルフ最適化 (241 -> 230)

--- 369/3_submission.py
+++ 369/3_submission.py
@@ -1,10 +1,8 @@
 def p(g):
  for n in range(100):
-  i,j=divmod(n,10)
-  q=[(i,j)]*(g[i][j]<1)
+  q=[divmod(n,10)]*(g[n//10][n%10]<1)
   for x,y in q:
    g[x][y]=1
-   for X,Y in(x+1,y),(x-1,y),(x,y+1),(x,y-1):
-    9>=X>=0<=Y<=9>g[X][Y]*10 and q.append((X,Y))
+   for a,b in(x+1,y),(x-1,y),(x,y+1),(x,y-1):9>=a>=0<=b<=9>g[a][b]*9 and q.append((a,b))
   for x,y in q:g[x][y]=4-len(q)
  return g
