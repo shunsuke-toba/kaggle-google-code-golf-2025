c10a49278d25395da78db455da3d0991d2070afd
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:58:46 +0900

    089: コードゴルフ最適化 (828 -> 465)

--- 089/3_submission.py
+++ 089/3_submission.py
@@ -1,30 +1,14 @@
 def p(g):
- H,W=len(g),len(g[0]);v=[[0]*W for _ in g];P=[];M=[]
- def f(i,j):
-  if min(i,j,H-1-i,W-1-j)<0 or v[i][j]or g[i][j]<1:return[]
-  v[i][j]=1;c=[(i,j)]
-  for a in-1,0,1:
-   for b in-1,0,1:a|b and c.extend(f(i+a,j+b))
-  return c
- for i in range(H):
-  for j in range(W):
-   if (V:=g[i][j])>0>=v[i][j]:
-    c=f(i,j)
-    if len(c)<2<=(n:=V)<4:M+=[(i,j,n)]
-    else:
-     I,J=zip(*c);A=min(I);B=min(J);h=max(I)-A+1;w=max(J)-B+1;p=[[0]*w for _ in[0]*h];R=S=0
-     for x,y in c:
-      a=x-A;b=y-B;p[a][b]=V=g[x][y]
-      if V==2:R=a,b
-      if V==3:S=a,b
-     P+=[(p,R,S,h,w)]
- for i,j,c in M:
-  for p,R,S,h,w in P:
-   d=c<3;T=(S,R)[d]
-   if T:
-    a,b=T;o=i-a;u=j-b+(2*b-w+1)*d
-    for k in range(h*w):
-     x,y=divmod(k,w);V=p[x][y+(w-2*y-1)*d]
-     if V:n=o+x;m=u+y;H>n>=0 and W>m>=0 and exec("g[n][m]=V")
-    break
+ h,w=len(g),len(g[0])
+ for k,m in(2,-1),(3,1):
+  a=[(i,j)for i in range(h)for j in range(w)if g[i][j]==k];B=s=C=()
+  for i,j in a:
+   t=[(x,y,g[i+x][j+y])for x in range(-2,3)for y in range(-2,3)if 0<=i+x<h and 0<=j+y<w and(v:=g[i+x][j+y])and v!=k]
+   if t:
+    V=[v for _,_,v in t];c=max(V,key=V.count);d=[(x,y)for x,y,v in t if v==c]
+    if len(d)>len(B):B=d;s=(i,j);C=c
+  if B:
+   a.remove(s)
+   for i,j in a:
+    for x,y in B:g[i+x][j+y*m]=C
  return g
