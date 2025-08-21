d67296d41841118e74f13a90c1bc4b75c53ef0d9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:14:06 +0900

    013: コードゴルフ最適化の実装 (303 -> 282)

--- 013/3_submission.py
+++ 013/3_submission.py
@@ -1,12 +1,9 @@
 def p(g,R=range):
- h=len(g);w=len(g[0])
- (x,y,a),(X,Y,b)=[(i,j,g[i][j])for i in R(h)for j in R(w)if g[i][j]]
- if x==X or{x,X}=={0,h-1}:
+ h=len(g);w=len(g[0]);(x,y,a),(X,Y,b)=[(i,j,v)for i in R(h)for j in R(w)if(v:=g[i][j])]
+ if{x,X}<={0,h-1}:
   if y>Y:y,Y,a,b=Y,y,b,a
-  s=Y-y;C=a,b
-  for c in R(y,w,s):
-   for r in g:r[c]=C[(c-y)//s&1]
+  for c in R(y,w,s:=Y-y):
+   for r in g:r[c]=(a,b)[(c-y)//s&1]
  else:
-  s=X-x;C=a,b
-  for r in R(x,h,s):g[r]=[C[(r-x)//s&1]]*w
+  for r in R(x,h,s:=X-x):g[r]=[(a,b)[(r-x)//s&1]]*w
  return g
