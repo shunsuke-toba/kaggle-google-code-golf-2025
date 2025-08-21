8e63dcbd9dc25e864d3c93512100587d3184d7e4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:49:57 +0900

    079: コードゴルフ最適化の実装 (314 -> 226)

--- 079/3_submission.py
+++ 079/3_submission.py
@@ -1,2 +1,8 @@
-from collections import Counter as C
-p=lambda g:[*map(list,k:=max(d:=C(tuple(map(tuple,s))for i in range(len(g)-2)for j in range(len(g[0])-2)for s in[[r[j:j+3]for r in g[i:i+3]]]if sum(c>0 for r in s for c in r)>2 and all(map(sum,s))and all(map(sum,zip(*s)))),key=lambda k:(d[k],sum(c>0 for r in k for c in r))))]
+def p(g):
+ d={};r=range
+ for i in r(len(g)-2):
+  for j in r(len(g[0])-2):
+   t=tuple(tuple(r[j:j+3])for r in g[i:i+3])
+   if 0 not in[*map(sum,t),*map(sum,zip(*t))]:
+    d[t]=d.get(t,0)+1
+ return [*map(list,max(d,key=d.get))]
