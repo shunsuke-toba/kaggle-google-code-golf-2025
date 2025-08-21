72b1befb5ef712a3a4f10c8ff52853017f18ce85
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:22:43 +0900

    025: コードゴルフ最適化の実装 (257 -> 239)

--- 025/3_submission.py
+++ 025/3_submission.py
@@ -1,9 +1,8 @@
-def p(g,e=enumerate):
- w=len(g[0]);o=[w*[0]for _ in g]
+def p(g):
+ o=[[0]*len(g[0])for _ in g]
  for _ in 0,1:
-  for x,k in e(zip(*g)):
+  for x,k in enumerate(zip(*g)):
    if{*k}=={c:=k[0]}!={0}:
-    for y,r in e(g):
-     q=o[y];q[x-(c in r[:x])]=q[x]=q[x+(c in r[x+1:])]=c
+    for q,r in zip(o,g):q[x-(c in r[:x])]=q[x]=q[x+(c in r[x+1:])]=c
   g,o=[*zip(*g)],[*map(list,zip(*o))]
  return o
