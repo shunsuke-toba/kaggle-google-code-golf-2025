6cf3966d8e01806f1246aab913549354f64a7e77
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:25:29 +0900

    030: コードゴルフ最適化の実装 (203 -> 186)

--- 030/3_submission.py
+++ 030/3_submission.py
@@ -1,6 +1,1 @@
-def p(g):
- t={};[t.setdefault(v,i)for i,r in enumerate(g)for v in set(r)if v];n=[[0]*len(g[0])for _ in g]
- for i,r in enumerate(g):
-  for j,v in enumerate(r):
-   if v:n[i+t[1]-t[v]][j]=v
- return n
+def p(g):t={};[t.setdefault(v,i)for i,r in enumerate(g)for v in r];n=[[0]*len(r)for r in g];[n[i+t[1]-t[v]].__setitem__(j,v)for i,r in enumerate(g)for j,v in enumerate(r)if v];return n
