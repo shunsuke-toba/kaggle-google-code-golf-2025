53bd6a2d56c36dd33a89c35556fecb25d7866522
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:09:03 +0900

    005: コードゴルフ最適化の実装 (422 -> 409)

--- 005/3_submission.py
+++ 005/3_submission.py
@@ -1,11 +1,11 @@
 def p(g):
- H=len(g)-2;W=len(g[0])-2;R=0,1,2;r=c=0
+ H=W=len(g)-2;R=0,1,2;r=c=0
  while 0 in map(sum,(P:=[g[r+i][c:c+3]for i in R])+[*zip(*P)]):
   c+=1;r+=c//W;c%=W
  for Y in-4,0,4:
   for X in-4,0,4:
-   if X|Y and-1<r+Y<H and-1<c+X<W:
-    t=max(max(g[r+Y+i][c+X:c+X+3])for i in R);y=r;x=c
+   if(X|Y)*(-1<r+Y<H)*(-1<c+X<W):
+    t=max(max(g[r+Y+i][c+X:c+X+3])for i in R);y,x=r,c
     while t:
      y+=Y;x+=X
      for i in R:
