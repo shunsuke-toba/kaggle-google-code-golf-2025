f08e1d3b9c0819aa18b49de933cb38d47ce94fa1
pech1 <pechi2327@gmail.com>
2025-08-22 01:16:41 +0900

    14:微改善(ラムダ式で書くと短縮できる気がするが、思いつかず)

--- 014/3_submission.py
+++ 014/3_submission.py
@@ -1,3 +1,2 @@
-def p(g):
- s=sum(g,[]);exec("while~-(min({*s}-{0},key=s.count)in g[-1]):g.pop()\ng[:]=[*map(list,zip(*g[::-1]))]\n"*4)
- return g
+def p(g,F=filter):
+ s=sum(g,[]);f=lambda r:min({*s}-{0},key=s.count)in r;return[*map(list,zip(*F(f,zip(*F(f,g)))))]
