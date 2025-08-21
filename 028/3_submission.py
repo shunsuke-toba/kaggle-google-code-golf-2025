617fb686c9958df8c469b22b9e95705302ff0e1f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:24:16 +0900

    028: コードゴルフ最適化の実装 (129 -> 83)

--- 028/3_submission.py
+++ 028/3_submission.py
@@ -1,1 +1,1 @@
-def p(g,m=max):e,a=m(map(m,g[:5])),m(map(m,g[5:]));return[[(e,a)[r>4]*(r%7in(0,2)or c%9<1)for c in range(10)]for r in range(10)]
+p=lambda g,R=range(10):[[max(g[2|r//5*5])*(4*r%7<2 or c%9<1)for c in R]for r in R]
