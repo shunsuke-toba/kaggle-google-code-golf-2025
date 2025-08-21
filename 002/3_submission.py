aee77a8f53ea67b56895f012b46bb86da68abf6d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:00:50 +0900

    002: コードゴルフ最適化 (178 -> 176)

--- 002/3_submission.py
+++ 002/3_submission.py
@@ -1,4 +1,4 @@
 def p(j):
  c=len(j);j=[[x or 4 for x in r]for r in j];d=1-c,0,-1,0
- for z in range(9**5):r,q=z%97%c,z%89%c;j[r][q]*=j[r+d[z%4]][q+d[z%4-3]]*r*q*(r-c+1)+(j[r][q]==3)!=0
+ for z in range(9**5):r,q=z%97%c,z%89%c;j[r][q]*=j[r+d[z%4]][q+d[~z%4]]*r*q*(r-c+1)+(j[r][q]&1)!=0
  return j
