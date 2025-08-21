e84f6551eef5070bfa1a80ac31f481695cbe63df
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:53:42 +0900

    157: コードゴルフ最適化 (704 -> 698)

--- 157/3_submission.py
+++ 157/3_submission.py
@@ -1,17 +1,18 @@
 def p(g):
  h=len(g);w=len(g[0]);r=range;d=1,0,-1,0,1;c=[]
- for Y in r(h):
-  for X in r(w):
-   if g[Y][X]>4:
-    q=[Y,X];g[Y][X]=0;t=[];m,n=Y,X
-    while q:
-     x=q.pop();y=q.pop();t+=y,x;m=min(m,y);n=min(n,x)
-     for i in r(4):
-      Y=y+d[i];X=x+d[i+1]
-      if 0<=Y<h and 0<=X<w and g[Y][X]>4:g[Y][X]=0;q+=Y,X
-    c+=[[[t[i]-m,t[i+1]-n]for i in r(0,len(t),2)]]
+ for z in r(h*w):
+  y=z//w;x=z%w
+  if g[y][x]>4:
+   q=[y,x];g[y][x]=0;t=[]
+   while q:
+    x=q.pop();y=q.pop();t+=y,x
+    for i in r(4):
+     Y=y+d[i];X=x+d[i+1]
+     if 0<=Y<h and 0<=X<w and g[Y][X]>4:g[Y][X]=0;q+=Y,X
+   m=min(t[::2]);n=min(t[1::2])
+   c+=[[[t[i]-m,t[i+1]-n]for i in r(0,len(t),2)]]
  while c:
-  B=0
+  b=0
   for I,T in enumerate(c):
    for Y in r(3):
     for X in r(w):
@@ -20,8 +21,8 @@ def p(g):
       u=Y+y;v=X+x
       if u>=h or v>=w or g[u][v]:break
       for j in r(4):
-       a=u+d[j];b=v+d[j+1];s+=0<=a<h and 0<=b<w and g[a][b]==2
+       a=u+d[j];z=v+d[j+1];s+=0<=a<h and 0<=z<w and g[a][z]==2
      else:
-      if s>B:B=s;i=I;Y0=Y;X0=X
+      if s>b:b=s;i=I;Y0=Y;X0=X
   for y,x in c.pop(i):g[Y0+y][X0+x]=1
  return g
