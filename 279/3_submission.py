5e4dd579f213a57b94bc0640bd0a94b99331f0c7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:52:53 +0900

    279: コードゴルフ最適化 (357 -> 324)

--- 279/3_submission.py
+++ 279/3_submission.py
@@ -1,15 +1,14 @@
 def p(g):
- h=len(g);w=len(g[0]);v=set()
- for i in range(h*w):
-  y,x=divmod(i,w);c=g[y][x]
-  if c-9 and i not in v:
-   m=[i];v|={i};e=0
+ h=len(g);w=len(g[0]);s=h*w;v=[0]*s
+ for i in range(s):
+  c=g[i//w][i%w]
+  if c^9 and not v[i]:
+   m=[i];v[i]=1;e=0
    for z in m:
-    y,x=divmod(z,w)
-    for n in(z-1,z+1,z-w,z+w):
-     if 0<=n<h*w and-2<n%w-x<2 and g[n//w][n%w]==c:
+    for n in z-1,z+1,z-w,z+w:
+     if 0<=n<s and-2<n%w-z%w<2 and g[n//w][n%w]==c:
       e+=1
-      if n not in v:v|={n};m+=[n]
+      if not v[n]:v[n]=1;m+=n,
    if e>=2*len(m):
     for z in m:g[z//w][z%w]=8
  return g
