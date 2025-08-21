7d5f4f2bb8f46382c3e648d5f2dfe284df9ce9da
pech1 <pechi2327@gmail.com>
2025-08-22 00:55:35 +0900

    14:submissionの更新

--- 014/3_submission.py
+++ 014/3_submission.py
@@ -1,6 +1,3 @@
-from collections import*
 def p(g):
- c=min((v,k)for k,v in Counter(sum(g,[])).items()if k)
- g=[r for r in g if c[1]in r]
- e=[i for r in g for i,x in enumerate(r)if x==c[1]]
- return[r[min(e):max(e)+1]for r in g]
+ s=sum(g,[]);exec("while~-(min({*s}-{0},key=s.count)in g[-1]):g.pop()\ng[:]=[*map(list,zip(*g[::-1]))]\n"*4)
+ return g
