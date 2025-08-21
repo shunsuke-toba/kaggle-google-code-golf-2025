44a1fef73ba9365b18f7f103e6ba20562f5e7216
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:24:54 +0900

    029: コードゴルフ最適化の実装 (333 -> 270)

--- 029/3_submission.py
+++ 029/3_submission.py
@@ -1,8 +1,4 @@
-def p(g,R=range,E=enumerate):
- d={}
- for i,r in E(g):
-  for j,v in E(r):
-   x,y,X,Y=d.get(v,(i,j,i,j));d[v]=min(x,i),min(y,j),max(X,i),max(Y,j)
- for v,(a,c,b,d) in d.items():
-  if b-a>1 and d-c>1 and all(g[a][k]==v==g[b][k]for k in R(c,d+1))and all(g[k][c]==v==g[k][d]for k in R(a,b+1)):
-   return[r[c+1:d]for r in g[a+1:b]]
+def p(g):
+ for k in{*sum(g,[])}:
+  y=[i for i,r in enumerate(g)if k in r];x=[i for i,c in enumerate(zip(*g))if k in c];a,b=y[0],y[-1];c,d=x[0],x[-1]
+  if g[a][c:d+1]==[k]*-~(d-c)==g[b][c:d+1]and all(r[c]==r[d]==k for r in g[a:b+1]):return[r[c+1:d]for r in g[a+1:b]]
