ada9de084e74f67f22e2f4142901ea3b6fb72809
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:52:10 +0900

    コードゴルフ最適化: 280番問題を最適化 (431 -> 384)

--- 280/3_submission.py
+++ 280/3_submission.py
@@ -1,16 +1,15 @@
 def p(g):
  *h,=map(list,g);H=len(g);W=len(g[0])
  for i in range(H*W):
-  y=i//W;x=i%W
-  if h[y][x]-2:continue
+  if h[y:=i//W][x:=i%W]-2:continue
   for a,b in(1,0),(0,1),(-1,0),(0,-1):
-   if H>y+a>-1<x+b<W and h[y+a][x+b]<1:break
+   if h[y+a][x+b]<1:break
   k=0;Y=y-a;X=x-b
-  while H>Y>-1<X<W and h[Y][X]==3:k+=1;Y-=a;X-=b
+  while H>Y>-1<X<W>2<h[Y][X]:k+=1;Y-=a;X-=b
   while 1:
-   g[y][x]=2
-   for t in range(1,k+1):
-    try:g[y+b*t][x+a*t]=g[y-b*t][x-a*t]=3
+   for t in range(-k,k+1):
+    try:g[y+b*t][x+a*t]=3
     except:0
-   if not(H>(y:=y+a)>-1<(x:=x+b)<W and h[y][x]<1):break
+   g[y][x]=2
+   if not(H>(y:=y+a)>-1<(x:=x+b)<W>1>h[y][x]):break
  return g
