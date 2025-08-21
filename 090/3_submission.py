b2357f13748b7d6fa5847c659e27d10d18c5fa16
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:56:37 +0900

    090: コードゴルフ最適化の実装 (240 -> 231)

--- 090/3_submission.py
+++ 090/3_submission.py
@@ -1,7 +1,5 @@
 def p(g):
- h=len(g);w=len(g[0]);r=range
- l=[(a*b,y,x,a,b)for a in r(2,h+1)for b in r(2,w+1)for y in r(h-a+1)for x in r(w-b+1)if all(not any(R[x:x+b])for R in g[y:y+a])]
- if l:
-  _,y,x,a,b=max(l)
-  for R in g[y:y+a]:R[x:x+b]=[6]*b
+ r=range;h=len(g);w=len(g[0])
+ _,y,x,a,b=max([(a*b,y,x,a,b)for y in r(h)for x in r(w)for a in r(2,h-y+1)for b in r(2,w-x+1)if not any(sum(R[x:x+b])for R in g[y:y+a])]or[[0]*5])
+ for R in g[y:y+a]:R[x:x+b]=[6]*b
  return g
