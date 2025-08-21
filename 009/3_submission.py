e7e8c548e2b53a14a3bed801daa46f6687de1733
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:11:12 +0900

    009: コードゴルフ最適化の実装 (252 -> 243)

--- 009/3_submission.py
+++ 009/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
  b=g[0][2]
- for c in range(20):
+ for c in range(18):
   for r in g:
-   t=[i for i,x in enumerate(r)if x==(d:=c//2)]
-   if d>0<len(t)==4:r[t[1]+1:t[-2]]=[b+(d+10-b)*(r[i]!=b)for i in range(t[1]+1,t[-2])]
+   t=[i for i,x in enumerate(r)if x==(d:=c//2+1)]
+   if len(t)==4:r[t[1]+1:t[-2]]=[b+(d+10-b)*(x!=b)for x in r[t[1]+1:t[-2]]]
   g=[*map(list,zip(*g))]
  return[[x%10for x in r]for r in g]
