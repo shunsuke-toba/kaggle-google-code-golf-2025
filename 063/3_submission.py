8a62b582e1c923104cc2ab3da9446fb93608f998
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:39:42 +0900

    063: コードゴルフ最適化の実装 (167 -> 148)

--- 063/3_submission.py
+++ 063/3_submission.py
@@ -1,3 +1,1 @@
-def p(g):
- r=range;n=len(g)
- return[[[g[i][j],3][0<i<n-1and 0<j<n-1and(all(g[y][j]%3<1for y in r(1,n-1))or all(x%3<1for x in g[i][1:-1]))]for j in r(n)]for i in r(n)]
+p=lambda g:[[[x,3][0<i<len(g)-1>j>0and(all(t[j]%3<1for t in g[1:-1])or all(y%3<1for y in r[1:-1]))]for j,x in enumerate(r)]for i,r in enumerate(g)]
