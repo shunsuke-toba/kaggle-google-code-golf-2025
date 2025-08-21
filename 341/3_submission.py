c8895878fa75f2f74635ec36136e12bcf903ba57
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:48:18 +0900

    341: コードゴルフ最適化 (373 -> 350)

--- 341/3_submission.py
+++ 341/3_submission.py
@@ -1,10 +1,10 @@
-def p(g,E=enumerate):
- d={}
- for i,r in E(g):
-  for j,v in E(r):
-   if v:t=d.setdefault(v,[i,i,j,j]);t[1]=i;t[3]=j
- a,b=d.values();C=lambda x,y:y[2]<=x[2]<x[3]<=y[3];H=C(a,b)|C(b,a)
- if~-H:g=[*map(list,zip(*g))];a=a[2:]+a[:2];b=b[2:]+b[:2]
- I=(a,b)[C(b,a)];L=I[2]+1;U=I[3]
- for r in g[min(a[1],b[1])+1:max(a[0],b[0])]:r[L:U]=[8]*(U-L)
- return~-H and[*map(list,zip(*g))]or g
+def p(g):
+ s=sum(g,[]);A,B=[(i//10,j//10,i%10,j%10)for k in{*s}-{0}for i,j in[(s.index(k),99-s[::-1].index(k))]]
+ if A[0]>B[0]:A,B=B,A
+ if A[1]<B[0]:
+  c=max(A[2],B[2])+1;d=min(A[3],B[3])
+  for r in g[A[1]+1:B[0]]:r[c:d]=[8]*(d-c)
+ else:
+  if A[2]>B[2]:A,B=B,A
+  for r in g[max(A[0],B[0])+1:min(A[1],B[1])]:r[A[3]+1:B[2]]=[8]*(B[2]-A[3]-1)
+ return g
