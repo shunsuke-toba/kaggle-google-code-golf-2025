61fdb35b4f6496d5b7c8e260c5e0d26006e4029f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:43:53 +0900

    070: コードゴルフ最適化の実装 (187 -> 169)

--- 070/3_submission.py
+++ 070/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,e=enumerate):
- y=[i for i,r in e(g)if 8 in r];x=[i for i,c in e(zip(*g))if 8 in c]
- for i in range(y[0],-~y[-1]):
-  for j in range(x[0],-~x[-1]):g[i][j]+=2*(g[i][j]==1)
- return g
+	y=[i for i,r in e(g)if 8 in r];x=[i for i,c in e(zip(*g))if 8 in c]
+	for i in y:
+		for j in range(x[0],-~x[-1]):g[i][j]+=2*(g[i][j]==1)
+	return g
