19255fc2b3d6558ff7cfedb66f40ea7429fbd3f6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:29:11 +0900

    040: コードゴルフ最適化の実装 (157 -> 140)

--- 040/3_submission.py
+++ 040/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
- a=g[0][0];t=a==g[0][1];b=g[9*t][9-9*t];r=range(10)
+ t=g[0][0]==g[0][1];r=range(10)
  for y in r:
   for x in r:
-   if g[y][x]and 0<(x,y)[t]<9:g[y][x]=(a,b)[(x>4,y>4)[t]]
+   if g[y][x]==3:g[y][x]=(g[0][0],g[9*t][9*t^9])[(x,y)[t]>4]
  return g
