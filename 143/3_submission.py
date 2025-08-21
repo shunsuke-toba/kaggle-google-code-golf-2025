52b5e71d5d1bc0a9f38c35e9724d1a215f4f3d14
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:45:15 +0900

    143: コードゴルフ最適化 (383 -> 310)

--- 143/3_submission.py
+++ 143/3_submission.py
@@ -1,13 +1,10 @@
 def p(g):
- A=set();d=1,0,-1,0,1
- for z in range(100):
-  i=z//10;j=z%10;v=g[i][j]
-  if v%5:
-   q=[(i,j)];g[i][j]=0
-   for x,y in q:
-    for k in 0,1,2,3:
-     a=x+d[k];b=y+d[k+1]
-     if 9>=a>=0<=b<=9 and g[a][b]==v:g[a][b]=0;q+=[(a,b)]
-   r,c=zip(*q);s=tuple(sorted((x-min(r),y-min(c))for x,y in q));t=i<4>j;t and A.add(s);v=[v,5][s in A and t^1]
-   for x,y in q:g[x][y]=v
+ s={k for k in range(9) if g[k//3][k%3]};t=next(iter(s));S=str(g)
+ for n in range(1,64):
+  y=n//8;x=n%8
+  if {k for k in range(9) if g[y+k//3][x+k%3]}==s:
+   C={g[y+k//3][x+k%3]for k in s}
+   if len(C)==1 and S.count(str(c:=C.pop()))==len(s):
+    for k in s:g[y+k//3][x+k%3]=5
+    return g
  return g
