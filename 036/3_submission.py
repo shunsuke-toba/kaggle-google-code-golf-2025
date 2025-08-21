f97664e9a81f7fef719e5ad08cbe7f5b679df831
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:27:57 +0900

    036: コードゴルフ最適化の実装 (262 -> 247)

--- 036/3_submission.py
+++ 036/3_submission.py
@@ -2,5 +2,5 @@ def p(g,E=enumerate):
  m={}
  for y,r in E(g):
   for x,v in E(r):
-   t=m.setdefault(v,[99,0,99,0]);t[0]=min(t[0],x);t[1]=max(t[1],x+1);t[2]=min(t[2],y);t[3]=max(t[3],y+1)
+   t=m.setdefault(v,[99,0,99,0]);t[:]=min(t[0],x),max(t[1],x+1),min(t[2],y),max(t[3],y+1)
  a,b,c,d=min(m.values(),key=lambda t:(t[1]-t[0])*(t[3]-t[2]));return[r[a:b]for r in g[c:d]]
