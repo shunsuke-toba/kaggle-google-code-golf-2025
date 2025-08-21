a7d4fd9b96abb0deac5bbd4974108393abbc873c
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-22 04:40:42 +0900

    374: 微更新(163 -> 158)

--- 374/3_submission.py
+++ 374/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  x=1
- for k in range(1600):
-  i,c,r=9-k//200,k%10,k//10%10
-  if g[r][c:c+i]==[5]*i:g[r][c:c+i]=[x]*i;x=x*4%7
-  if~k%100:g=[*map(list,zip(*g))]
+ for k in range(1440):
+  i,c,r=9-k//180,k%9,k//9%10
+  if[5]*i==g[r][c:c+i]:g[r][c:c+i]=[x]*i;x=x*4%7
+  if~k%90:g=[*map(list,zip(*g))]
  return g
