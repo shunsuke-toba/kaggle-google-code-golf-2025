1db55c63e064139e705648a260bce187606b2a1c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 06:18:40 +0900

    018: コードゴルフ最適化の実装 (642 -> 640)

--- 018/3_submission.py
+++ 018/3_submission.py
@@ -6,7 +6,7 @@ def p(g):
    for s in R(r+1,h+1):
     for t in R(c+1,w+1):
      if (x:=[g[i][c:t] for i in R(r,s)])==[*map(list,zip(*F(any,zip(*list(F(any,x))))))]:
-      if len({*sum(x,[])}|{0})>4:q,u,v=x,s,t
+      if len({*sum(x,[]),0})>4:q,u,v=x,s,t
    if q:p+=[q]
    for i in R(r,u): g[i][c:v]=[0]*(v-c)
  for r in p:
@@ -17,5 +17,5 @@ def p(g):
      if all(r[i][j]*(sum(r,[]).count(r[i][j])==1)==g[y+i][x+j] for i in R(H) for j in R(W)):
       for i in R(H): g[y+i][x:x+W]=r[i]
    r=list(map(list, zip(*r[::-1])))
-   if z%4>2: r=r[::-1]
+   if z&3>2:r=r[::-1]
  return g
