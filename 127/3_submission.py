e5af1b467912c23af6b543f2320354b360b701a4
sak74y <yusei.sakurai@third-inc.co.jp>
2025-08-21 12:10:14 +0900

    127

--- 127/3_submission.py
+++ 127/3_submission.py
@@ -1,7 +1,1 @@
-def p(g):
- r=range
- for i in r(1,len(g),4):
-  for j in r(1,len(g[0]),4):
-   v=g[i][j]+5
-   for a in g[i-1:i+2]:a[j-1:j+2]=[v]*3
- return g
+p=lambda g:[[5+g[i&4|1][j&12|1]*(g[i][j]<5)for j in range(11)]for i in range(len(g))]
