a1bfa3b105698593216a8afde0fda7f365fb165d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:50:38 +0900

    080: コードゴルフ最適化の実装 (518 -> 465)

--- 080/3_submission.py
+++ 080/3_submission.py
@@ -1,19 +1,17 @@
 def p(g):
- R=range;h=len(g);n=0
- while (r:=g[n])!=r[:1]*len(r):n+=1
- s=n+1;m=-~h//s;A=[g[i*s][::s][:m]for i in R(m)];b=4
- for y in R(m-2):
-  for x in R(m-2):
-   t=[r[x:x+3]for r in A[y:y+3]];u=sum(map(bool,sum(t,[])))
-   if u>b:b=u;P=t;c=y+1,x+1
- for y in R(m*m):
-  x=y%m;y//=m
-  if A[y][x]==P[1][1] and(y,x)!=c:
+ R=range;n=0
+ while g[n]!=g[n][:1]*len(g[n]):n+=1
+ s=n+1;m=-~len(g)//s;A=[g[i*s][::s]for i in R(m)];b=0
+ for i in R((m-2)**2):
+  y,x=divmod(i,m-2);t=[r[x:x+3]for r in A[y:y+3]];u=sum(map(bool,sum(t,[])))
+  if u>b:b=u;P=t
+ for i in R(m*m):
+  y,x=divmod(i,m)
+  if A[y][x]==P[1][1]:
    for k in R(9):
-    u=y+k//3-1;v=x+k%3-1
-    if m>u>=0<=v<m:A[u][v]=P[k//3][k%3]
- o=[r[:1]*len(r) for _ in R(h)]
- for y in R(m*m):
-  x=y%m;y//=m
-  for i in R(n):o[y*s+i][x*s:x*s+n]=[A[y][x]]*n
- return o
+    u,v=y+k//3-1,x+k%3-1
+    if 0<=u<m>v>=0:A[u][v]=P[k//3][k%3]
+ for y in R(m):
+  for x in R(m):
+   for j in R(n):g[y*s+j][x*s:x*s+n]=[A[y][x]]*n
+ return g
