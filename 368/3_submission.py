f7503164a0c697897541d71de7fab62343d28bc5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:48:58 +0900

    368: コードゴルフ最適化 - 矩形検出とコピー処理の簡略化 (290 -> 284)

--- 368/3_submission.py
+++ 368/3_submission.py
@@ -1,14 +1,7 @@
-def p(g,r=range):
- c=len(g)
- for q in r(c*c):
-  y=q//c;x=q%c
-  if g[y][x]%5:
-   Y,X=y,x;h=w=1
-   while y+h<c and g[y+h][x]%5:h+=1
-   while x+w<c and g[y][x+w]%5:w+=1
-   break
- for y in r(c-h+1):
-  for x in r(c-w+1):
-   if g[y][x]==5:
-    for q in r(h):g[y+q][x:x+w]=g[Y+q][X:X+w]
+def p(g):
+ a,b=zip(*[divmod(k,10)for k in range(100)if g[k//10][k%10]%5]);t=min(a);l=min(b);h=max(a)-t+1;w=max(b)-l+1
+ for Y in range(11-h):
+  for X in range(11-w):
+   if{g[Y+i][X+j]for i in range(h)for j in range(w)}=={5}:
+    for i in range(h):g[Y+i][X:X+w]=g[t+i][l:l+w]
  return g
