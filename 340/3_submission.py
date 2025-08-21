ef66a1a97bd93c05fb898f79bafeff13beaa0da0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-21 23:50:11 +0900

    340: コードゴルフ最適化 (267 -> 219)

--- 340/3_submission.py
+++ 340/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- Z=[*zip(*g)];T=[r[1:-1]for r in g[1:-1]]
- for r in g[1:-1]:r[1:-1]=[0]*(len(Z)-2)
- for j,c in enumerate(Z[1:-1],1):g[1][j]=(c[0]in c[1:])*c[0];g[-2][j]=(c[-1]in c[:-1])*c[-1]
- for r,q in zip(g[1:-1],T):r[1]=(r[0]in q)*r[0];r[-2]=(r[-1]in q)*r[-1]
+ z=[*zip(*g)]
+ for r in g[1:-1]:a=r[1:-1];r[1:-1]=[0]*len(a);r[1]=r[0]*(r[0]in a);r[-2]=r[-1]*(r[-1]in a)
+ for j,c in enumerate(z[1:-1],1):g[1][j]=(c[0]in c[1:])*c[0];g[-2][j]=(c[-1]in c[:-1])*c[-1]
  return g
