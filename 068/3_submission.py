47f8ab21bc3c84eccd06443fa5281207d9a759f9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 08:42:35 +0900

    `068: コードゴルフ最適化の実装` (173 -> 157)

--- 068/3_submission.py
+++ 068/3_submission.py
@@ -1,6 +1,4 @@
 def p(g):
- a=sum(g,[]);b=[v for v in a if v and a.count(v)<2][0]
- y,x=divmod(a.index(b),10)
- r=[[0]*10 for _ in g]
- for R in r[y-1:y+2]:R[x-1:x+2]=2,2,2
- r[y][x]=b;return r
+ a=sum(g,[]);b=min({*a}-{0},key=a.count);y,x=divmod(a.index(b),10);g=[[0]*10 for _ in g]
+ for R in g[y-1:y+2]:R[x-1:x+2]=2,2,2
+ g[y][x]=b;return g
