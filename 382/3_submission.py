3befc64e7a39d30833ad8e5057f881d609f3d138
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 02:41:42 +0900

    タスク 382: 更新 (376 -> 311)

--- 382/3_submission.py
+++ 382/3_submission.py
@@ -1,16 +1,12 @@
 def p(g):
- v=2 in{r[0]+r[-1]for r in g}
- if not v:g=[*map(list,zip(*g))]
- a=2 in[r[-1]for r in g];b=8 in g[-1]
- if a:g=[r[::-1]for r in g]
- if b:g=g[::-1]
- w=len(g[0])
- t=[i for i in range(w)if 7<g[0][i]];s=0
- for i in range(1,len(g)):
-  s+=g[i][0]>1
-  for j in t:
-   if j+s<w:g[i][j+s]=8
- if b:g=g[::-1]
- if a:g=[r[::-1]for r in g]
- if not v:g=[*map(list,zip(*g))]
+ v=all(r[0]+r[-1]-2 for r in g)
+ if v:g=[*map(list,zip(*g))]
+ a=2 in[r[-1]for r in g];b=8 in g[-1];g=[r[::1-2*a]for r in g][::1-2*b]
+ w=len(g[0]);s=0
+ for r in g[1:]:
+  s+=r[0]>1
+  for j in range(w-s):
+   if g[0][j]>7:r[j+s]=8
+ g=[r[::1-2*a]for r in g[::1-2*b]]
+ if v:g=[*map(list,zip(*g))]
  return g
