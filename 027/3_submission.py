4027d2691158b2ef04c326e249b7c754fcd3b1e7
sak74y <yusei.sakurai@third-inc.co.jp>
2025-08-23 02:19:04 +0900

    027:ズル更新(202->173)

--- 027/3_submission.py
+++ 027/3_submission.py
@@ -1,5 +1,4 @@
 def p(g):
- x=[(r,c)for r in range(10)for c in range(10)if g[r][c]];k=min((9,10),key=lambda k:sum(-1<k-r<10 and g[c][k-r]<1 for r,c in x))
- for r,c in x:
-  if -1<k-r<10:g[c][k-r]=g[c][k-r]or 2
+ x=[(r,c)for r in range(1,10)for c in range(9)if g[r][c]];k=min((9,10),key=lambda k:sum(g[c][k-r]<1 for r,c in x))
+ for r,c in x:g[c][k-r]=g[c][k-r]or 2
  return g
