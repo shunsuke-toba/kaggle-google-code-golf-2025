c0d694124fea8b30a1ecebd87f89b1fa86f1d88f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 09:05:45 +0900

    100: コードゴルフ最適化の実装 (101 -> 98)

--- 100/3_submission.py
+++ 100/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[[max(range(1,10),key=lambda c:sum(c in r for r in g)*sum(c in r for r in zip(*g)))]*2]*2
+p=lambda g:[[max(range(1,10),key=lambda c:max(r.count(c)for r in g)*sum(c in r for r in g))]*2]*2
