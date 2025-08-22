c6c8686dd12966ab73531c9ebac8561f44c5988c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 02:41:37 +0900

    タスク 165: 更新 (277 -> 258)

--- 165/3_submission.py
+++ 165/3_submission.py
@@ -1,8 +1,8 @@
 R=range
 def p(g):
- h,w=len(g),len(g[0]);b=min(g[y][x]for y in R(1,h)for x in R(1,w-1)if g[y][x]==g[y-1][x]==g[y][x-1]==g[y][x+1]!=0)
- for x in R(w):
-  j=i=h
+ b=min(g[y][x]for y in R(1,20)for x in R(1,19)if g[y][x]==g[y-1][x]==g[y][x-1]==g[y][x+1]!=0)
+ for x in R(20):
+  j=i=20
   while i and g[i-1][x]^b:i-=1
   while j>i and(c:=g[j-1][x])in(0,b):j-=1
   if 0<i<j:
