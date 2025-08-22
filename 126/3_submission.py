d49cabb11b53d9fa6b29817db36e70bfa109643d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:00:46 +0900

    タスク 126: 更新 (121 -> 120)

--- 126/3_submission.py
+++ 126/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:([g[-1].__setitem__(x,4)for u,r in zip(g,g[1:])for x in range(1,len(r)-1)if u[x]==r[x-1]==r[x+1]!=r[x]],g)[1]
+p=lambda g:0*[g[-1].__setitem__(x,4)for u,r in zip(g,g[1:])for x in range(1,len(r)-1)if u[x]==r[x-1]==r[x+1]!=r[x]]or g
