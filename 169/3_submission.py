d2a501264b2ae72598cf996702f7ebc335a4db4b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:42:32 +0900

    タスク 169: 更新 (262 -> 255)

--- 169/3_submission.py
+++ 169/3_submission.py
@@ -1,12 +1,12 @@
 def p(g):
- d=1,0,-1,0,1;k=100
- while k:
-  k-=1;i=k//10;j=k%10
-  if g[i][j]==5:
+ d=1,0,-1,0;k=99
+ while~k:
+  i=k//10;j=k%10;k-=1
+  if g[i][j]>4:
    q=[(i,j)];g[i][j]=0
    for i,j in q:
     for n in 0,1,2,3:
-     x,y=i+d[n],j+d[n+1]
-     if 0<=x<10>y>=0<g[x][y]==5:g[x][y]=0;q+=[(x,y)]
+     x,y=i+d[n],j+d[n-1]
+     if-1<x<10>y>-1<g[x][y]>4:g[x][y]=0;q+=[(x,y)]
    for i,j in q:g[i][j]=5-len(q)
  return g
