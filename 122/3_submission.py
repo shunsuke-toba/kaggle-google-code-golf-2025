d0806ce1342e534132427e99b307e9630133d709
sak74y <yusei.sakurai@third-inc.co.jp>
2025-08-23 01:14:06 +0900

    122:更新(175->147)

--- 122/3_submission.py
+++ 122/3_submission.py
@@ -1,5 +1,4 @@
-def p(g):
- h=any(r.count(3)>1 for r in g);s=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==2];d=2*h
- for i,j in s:g[i][j]=0
- for i,j in s:g[i+2-d][j+d]=2
+def p(g,e=enumerate):
+ h=(g[0]+g[1]).count(3)!=1
+ for i,j in [(i,j)for i,r in e(g)for j,v in e(r)if v==2]:g[i][j]^=2;g[i+2-h*2][j+h*2]^=2
  return g
