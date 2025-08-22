d4e73400873cddb0c6ee7fda54bc01f2d1eb1311
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 00:43:28 +0900

    タスク 331: 更新 (135 -> 133)

--- 331/3_submission.py
+++ 331/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,e=enumerate:[[v or i<9and g[i+1][j]*2or i and g[i-1][j]*8or j and r[j-1]*6or j<9and r[j+1]*7for j,v in e(r)]for i,r in e(g)]
+p=lambda g,e=enumerate:[[v or i<9and g[i+1][j]*2or j<9and r[j+1]*7or(i>0)*g[i-1][j]*8+(j>0)*r[j-1]*6for j,v in e(r)]for i,r in e(g)]
