53c91f5186296beee5450131e42194aaa97a01cc
pech1 <pechi2327@gmail.com>
2025-08-22 23:00:30 +0900

    283:微短縮

--- 283/3_submission.py
+++ 283/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[[(r[j],(1,4,2)[sum((j and r[j-1],j<9 and r[j+1],i and g[i-1][j],i<9 and g[i+1][j]))//5-2])[r[j]>4]for j in range(10)]for i,r in enumerate(g)]
+p=lambda g,E=enumerate:[[(c,(1,4,2)[sum((j and r[j-1],j<9and r[j+1],i and g[i-1][j],i<9and g[i+1][j]))//5-2])[r[j]>4]for j,c in E(r)]for i,r in E(g)]
