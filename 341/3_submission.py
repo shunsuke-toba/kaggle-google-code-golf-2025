a1726b744d209539c0b0e23a80a51cd5ff54d438
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 00:43:30 +0900

    タスク 341: 更新 (350 -> 326)

--- 341/3_submission.py
+++ 341/3_submission.py
@@ -1,10 +1,10 @@
 def p(g):
  s=sum(g,[]);A,B=[(i//10,j//10,i%10,j%10)for k in{*s}-{0}for i,j in[(s.index(k),99-s[::-1].index(k))]]
- if A[0]>B[0]:A,B=B,A
+ if A>B:A,B=B,A
  if A[1]<B[0]:
-  c=max(A[2],B[2])+1;d=min(A[3],B[3])
-  for r in g[A[1]+1:B[0]]:r[c:d]=[8]*(d-c)
+  x,y=A[1]+1,B[0];c=max(A[2],B[2])+1;d=min(A[3],B[3])
  else:
   if A[2]>B[2]:A,B=B,A
-  for r in g[max(A[0],B[0])+1:min(A[1],B[1])]:r[A[3]+1:B[2]]=[8]*(B[2]-A[3]-1)
+  x,y=max(A[0],B[0])+1,min(A[1],B[1]);c=A[3]+1;d=B[2]
+ for r in g[x:y]:r[c:d]=[8]*(d-c)
  return g
