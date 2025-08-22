eaf7db4c816c213ab09caaf85b16332556527672
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:42:32 +0900

    タスク 168: 更新 (255 -> 242)

--- 168/3_submission.py
+++ 168/3_submission.py
@@ -1,8 +1,9 @@
 def p(g):
- n=len(g);N=n-1
- for t in range(N*N):
-  r=t//N;c=t%N;q=g[r][c],g[r][c+1],g[r+1][c],g[r+1][c+1]
-  if(m:=max(q))and 0 in q and sum(q)==3*m:
-   k=q.index(0);a=k//2*2-1;b=k%2*2-1;r+=k//2+a;c+=k%2+b
-   while 0<=r<n>c>=0:g[r][c]=m;r+=a;c+=b
+ n=len(g)
+ for r in range(n-1):
+  for c in range(n-1):
+   v=g[r][c:c+2]+g[r+1][c:c+2]
+   if v.count(0)==1:
+    i,j=divmod(v.index(0),2);k=v[i*2+j-1];a=i*2-1;b=j*2-1;x=r+i+a;y=c+j+b
+    while 0<=x<n>y>=0:g[x][y]=k;x+=a;y+=b
  return g
