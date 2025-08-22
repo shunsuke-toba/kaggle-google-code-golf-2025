a5b8ffe1eb3b93b3172b4c9c7ac920e3cdcb9570
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:42:35 +0900

    タスク 190: 更新 (201 -> 179)

--- 190/3_submission.py
+++ 190/3_submission.py
@@ -1,7 +1,5 @@
-def p(g,R=range):
- d=1,0,-1,0
- e=1,-1,-1,1
- for b in R(9**5):
-  i,j,k=b%9%8+1,b%89%8+1,b%4
-  if all(g[i+d[l]][j+d[l-3]]==0 for l in R(4))and g[i+e[k]][j+e[k-3]]>0:g[i-e[k]][j-e[k-3]]=g[i][j]
+def p(g):
+ for b in range(9**5):
+  i,j=b%9%8+1,b%89%8+1;a,c=1-2*(0<(k:=b%4)<3),1-2*(k<2)
+  if g[i+1][j]+g[i-1][j]+g[i][j+1]+g[i][j-1]<1<g[i+a][j+c]+1:g[i-a][j-c]=g[i][j]
  return g
