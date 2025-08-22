e4e6afbee1039229ef8bc28f45023d3a7e33af27
sak74y <yusei.sakurai@third-inc.co.jp>
2025-08-22 17:28:45 +0900

    088:微短縮

--- 088/3_submission.py
+++ 088/3_submission.py
@@ -2,4 +2,4 @@ def p(g):
  for _ in[0]*96:
   if sum(g[-1])<1:g.pop()
   g=[*zip(*g[::-1])]
- k=g[0][0];return[[c and k for c in r[1:-1]]for r in g[1:-1]]
+ return[[c and g[0][0]for c in r[1:-1]]for r in g[1:-1]]
