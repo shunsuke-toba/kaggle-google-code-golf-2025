857ba7454c52eca1200875f4ea4e580762dc4947
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 19:28:47 +0900

    117: グリッド操作の最適化と簡潔化 (292 -> 288)

--- 117/3_submission.py
+++ 117/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
- h=len(g);w=len(g[0]);f=sum(g,[])
- a,b=next((i,j)for i in range(1,h-1)for j in range(1,w-1)if(v:=g[i][j])and f.count(v)<6 and v==g[i-1][j-1]==g[i-1][j+1]==g[i+1][j-1]==g[i+1][j+1])
+ w=len(g[0]);f=sum(g,[])
+ a,b=next((i,j)for i in range(1,len(g)-1)for j in range(1,w-1)if(v:=g[i][j])and f.count(v)<6 and v==g[i-1][j-1]==g[i-1][j+1]==g[i+1][j-1]==g[i+1][j+1])
  for k,v in enumerate(f):
   if v:r,c=divmod(k,w);g[r][2*b-c]=g[2*a-r][c]=g[2*a-r][2*b-c]=v
  return g
