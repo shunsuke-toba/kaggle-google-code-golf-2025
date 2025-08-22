16130604b3caf8630134928dbaf6cdd637e3a0a8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 23:33:08 +0900

    タスク 264: 更新 (275 -> 272)

--- 264/3_submission.py
+++ 264/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- r=range;d=11,23,38,89,0,308,200,464,416;o=[[5]*9for _ in[0]*9];a=r(9)
+ r=range;a=r(9);d=11,23,38,89,0,308,200,464,416;o=[[5]*9for _ in a]
  for y in r(len(g)-2):
   for x in r(len(g[0])-2):
    if all(t:=[g[y+i//3][x+i%3]for i in a]):
