2a82df0991a464fb441519855c7045652043e5e1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 02:41:34 +0900

    タスク 096: 更新 (589 -> 536)

--- 096/3_submission.py
+++ 096/3_submission.py
@@ -1,27 +1,24 @@
 def p(g):
- import re
- R=range
- f=sum(g,[])
- b=max(f,key=f.count)
- I=[]
- for x in set(f):
-  if x==b:continue
-  s=None
-  for w in R(2,9):
-   for d in R(1,15):
-    for _ in R(4):
-     if any(re.search(str(x)*w+f"[^{x}]"*d+str(x),"".join(map(str,r)))for r in g):s=w,d
-     g=[list(r)for r in zip(*g[::-1])]
-  if not s:s=(1,-1) if f.count(x)==1 else (3,-3)
-  I+=[(x,*s)]
- I.sort(key=lambda t:t[1]*2+t[2])
- z=I[-1][1]*2+I[-1][2]
- o=[[b]*z for _ in R(z)]
- i=0
- while I:
-  x,w,d=I.pop()
-  for _ in R(4):
-   for k in R(w):o[i][i+k]=o[i+k][i]=x
-   o=[list(r)for r in zip(*o[::-1])]
-  i+=1
+ from re import search as S
+ r=range
+ f=sum(g,[]);s=set(f);b=max(s,key=f.count)
+ t=lambda m:[*map(list,zip(*m[::-1]))]
+ L=[]
+ for x in s-{b}:
+  s=str(x);y=None
+  for w in r(2,9):
+   for d in r(1,15):
+    for _ in r(4):
+     if any(S(s*w+f"[^{s}]"*d+s,"".join(map(str,R)))for R in g):y=2*w+d,x,w
+     g=t(g)
+  if not y:y=(1,x,1)if f.count(x)==1 else(3,x,3)
+  L+=y,
+ L.sort()
+ z=L[-1][0]
+ o=[[b]*z for _ in r(z)]
+ for i,(_,x,w) in enumerate(L[::-1]):
+  for _ in r(4):
+   for k in r(w):o[i][i+k]=o[i+k][i]=x
+   o=t(o)
  return o
+
