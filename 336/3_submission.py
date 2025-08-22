67fcbb3fb3b05bd27164c6928694b79041017f1d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 00:43:29 +0900

    タスク 336: 更新 (277 -> 274)

--- 336/3_submission.py
+++ 336/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
  s=sum(g,[]);t,l=divmod(s.index(5),10);b,d=divmod(99-s[::-1].index(5),10)
  for r in g[t+1:b]:r[l+1:d]=[8]*~(l-d)
- i,j=next((i,j)for i in range(t,b+1)for j in range(l,d+1)if not g[i][j]);u=(i==b)-(i==t);v=(j==d)-(j==l)
- while 10>i>-1<j<10:g[i][j]=8;i+=u;j+=v
+ i,j=next((i,j)for i in range(t,b+1)for j in range(l,d+1)if g[i][j]<1);t,l=(i==b)-(i==t),(j==d)-(j==l)
+ while j<10>i>-1<j:g[i][j]=8;i+=t;j+=l
  return g
