b15cdfa67aac06ec47f58b13a3ad6aa7a101e101
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:02:00 +0900

    タスク 147: 更新 (159 -> 134)

--- 147/3_submission.py
+++ 147/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[[8if c==3and 3in(i and g[i-1][j],-~i<len(g)and g[i+1][j],j and r[j-1],-~j<len(r)and r[j+1])else c for j,c in enumerate(r)]for i,r in enumerate(g)]
+p=lambda g:[[c+5*(c==3and 3in(r[j-1:j]+r[j+1:j+2]+[y[j]for y in g[i-1:i]+g[i+1:i+2]]))for j,c in enumerate(r)]for i,r in enumerate(g)]
