74e4c05791a3fde4284224faa7ea783f91952b40
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 22:22:03 +0900

    タスク 217: 更新 (125 -> 122)

--- 217/3_submission.py
+++ 217/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(a:=[*zip(*filter(sum,zip(*filter(sum,g))))],r:=range(9),[[a[i//3][j//3]and a[i%3][j%3]for j in r]for i in r])[2]
+p=lambda g:(a:=[*zip(*filter(sum,zip(*filter(sum,g))))],r:=range(9),[[a[i//3][j//3]&a[i%3][j%3]for j in r]for i in r])[2]
