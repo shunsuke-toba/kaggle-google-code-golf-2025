45eebdff9dc34ca487b67d8b10cdd01ec77210f6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:42:33 +0900

    タスク 182: 更新 (330 -> 308)

--- 182/3_submission.py
+++ 182/3_submission.py
@@ -1,10 +1,8 @@
 def p(g):
- R=range;h=len(g)-6
- for z in R(h**2):
-  y,x=divmod(z,h)
-  if g[y][x]==g[y][x+1]==g[y+1][x]==5 and(t:={(i,j)for i in R(5)for j in R(5)if(v:=g[y+i+1][x+j+1])>1 and(c:=v)}):break
- for z in R((h+2)**2):
-  y,x=divmod(z,h+2)
-  if {(i,j)for i in R(5)for j in R(5)if g[y+i][x+j]==1}==t:
-   for i,j in t:g[y+i][x+j]=c
+ R=range;r=R(5);n=len(g)-6
+ t=next(({(i,j)for i in r for j in r if(v:=g[y+i+1][x+j+1])>1 and(c:=v)}for y in R(n)for x in R(n)if g[y][x]*g[y][x+1]*g[y+1][x]==125))
+ for y in R(n+2):
+  for x in R(n+2):
+   if {(i,j)for i in r for j in r if g[y+i][x+j]==1}==t:
+    for i,j in t:g[y+i][x+j]=c
  return g
