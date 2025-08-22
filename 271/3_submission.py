97de28a7a5d52e6daede1efe0ccfe8e11d5ce82f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 23:33:10 +0900

    タスク 271: 更新 (128 -> 124)

--- 271/3_submission.py
+++ 271/3_submission.py
@@ -1,2 +1,1 @@
-R=range(7)
-p=lambda g:max(([x[c:c+3]for x in g[i:i+3]]for i in R for c in R),key=lambda t:(r:=sum(t,[])).count(1)*2+r.count(8))
+R=range(7);p=lambda g:max(([x[c:c+3]for x in g[i:i+3]]for i in R for c in R),key=lambda t:sum(x%2*2+x//8for x in sum(t,[])))
