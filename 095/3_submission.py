caa2af22344fa734ca001162a08dfbc14ac84ec3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 09:00:41 +0900

    095: コードゴルフ最適化の実装 (128 -> 117)

--- 095/3_submission.py
+++ 095/3_submission.py
@@ -1,6 +1,5 @@
-def p(g,E=enumerate):
- for i,r in E(g):
-  for j,v in E(r):
-   if v>4:
-    for x in g[i-1:i+2]:x[j-1:j+2]=1,1,1;r[j]=5
+def p(g):
+ for z in range(81):
+  i,j=z//9,z%9
+  for x in (g[i][j]>4)*g[i-1:i+2]:x[j-1:j+2]=1,1,1;g[i][j]=5
  return g
