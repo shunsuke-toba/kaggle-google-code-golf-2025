f1c54563124e64751fd29f3184901ab3ce011db2
pech1 <pechi2327@gmail.com>
2025-08-22 22:20:05 +0900

    300:submissionの更新

--- 300/3_submission.py
+++ 300/3_submission.py
@@ -1,7 +1,1 @@
-def p(g,e=enumerate):
- f=sum(g,[]);k=max({*f}-{0},key=f.count)
- a=b=99;c=d=0
- for i,r in e(g):
-  for j,v in e(r):
-   if v==k:a=min(a,i);c=max(c,i);b=min(b,j);d=max(d,j)
- return[r[b:d+1]for r in g[a:c+1]]
+def p(g):f=lambda a:zip(*filter(lambda r:max({*(s:=sum(g,[]))}-{0},key=s.count)in r,a));return[*map(list,f(f(g)))]
