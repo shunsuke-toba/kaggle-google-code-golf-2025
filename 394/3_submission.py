c93637353027cd4ca72d16c1a2261973a2413603
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 01:35:23 +0900

    タスク 394: 更新 (362 -> 353)

--- 394/3_submission.py
+++ 394/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- w=len(g[0]);r=range;p=[divmod(i,w)for i in r(len(g)*w)];z=[(i,j)for i,j in p if g[i][j]<1];a,b,c,d=z[0]+z[-1];R=C=1
+ w=len(g);r=range;p=[(i//w,i%w)for i in r(w*w)];z=[(i,j)for i,j in p if g[i][j]<1];a,b,c,d=z[0]+z[-1];R=C=2
  while any((g[i][j]-g[i%R][j])*g[i][j]*g[i%R][j]for i,j in p):R+=1
  while any((g[i][j]-g[i][j%C])*g[i][j]*g[i][j%C]for i,j in p):C+=1
  t={(i%R,j%C):k for i,j in p if(k:=g[i][j])}
