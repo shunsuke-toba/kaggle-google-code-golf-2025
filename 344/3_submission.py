2ac9133f8bb973e97103a30ed7119d49f8dd84ae
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 00:43:31 +0900

    タスク 344: 更新 (169 -> 167)

--- 344/3_submission.py
+++ 344/3_submission.py
@@ -2,5 +2,7 @@ def p(g,e=enumerate):
  for i,r in e(g):
   for k,v in e(r):
    for a,b in(i+1,k),(i,k+1):
-    if(len(g)>a)&(b<len(r))and g[a][b]*v==6:r[k],g[a][b]=v%2*8,~v%2*8
+    try:
+     if g[a][b]*v==6:r[k],g[a][b]=v%2*8,~v%2*8
+    except:0
  return g
