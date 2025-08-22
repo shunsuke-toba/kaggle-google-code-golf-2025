294412933a24912861a7d54bb88949d551ef4de7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 02:41:36 +0900

    タスク 134: 更新 (354 -> 302)

--- 134/3_submission.py
+++ 134/3_submission.py
@@ -3,8 +3,6 @@ def p(g):
  for d in s:
   x,y=zip(*[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==d])
   t=max(x)-min(x)+1
-  if t==max(y)-min(y)+1>5<t<19 and t%3<1:
-   a=min(x);b=min(y);m=t//3;break
- c=sum(s)-d
- r=range
- return[[c*any(g[i][j]==d for i in r(a+R*m,a+R*m+m) for j in r(b+C*m,b+C*m+m))for C in r(3)]for R in r(3)]
+  if t==max(y)-min(y)+1>5<t<19 and t%3<1:break
+ a=min(x);b=min(y);m=t//3;c=sum(s)-d
+ return[[c*(g[a+i*m][b+j*m]==d)for j in range(3)]for i in range(3)]
