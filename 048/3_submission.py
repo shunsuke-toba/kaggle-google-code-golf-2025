7e9cca9e5ba7a24daca190808c41904e223434ab
sak74y <yusei.sakurai@third-inc.co.jp>
2025-08-22 13:25:08 +0900

    048:微改善(でも方針を変えた方がよさそう)

--- 048/3_submission.py
+++ 048/3_submission.py
@@ -1,10 +1,7 @@
-def p(g):
- i=sum(g,[]).index(2)
- s=[divmod(i,len(g[0]))]
- o=0
+def p(g,o=0):
+ s=[divmod(sum(g,[]).index(2),len(g[0]))]
  while s:
   y,x=s.pop()
-  if y|x<0:continue
-  try:v=g[y][x];g[y][x]=0;v and(s.extend(((y-1,x),(y+1,x),(y,x-1),(y,x+1))),v<3 and(o:=o+1))
+  try:1/(y|x>=0);v=g[y][x];g[y][x]=0/v;(s.extend(((y-1,x),(y+1,x),(y,x-1),(y,x+1))),o:=o+(v<3))
   except:0
- return[[[0]],[[8]]][o>4]
+ return[[o//5*8]]
