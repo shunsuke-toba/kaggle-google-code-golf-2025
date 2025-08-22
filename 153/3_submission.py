e4b85b688517cd5c795f4f944035166599ebf669
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:42:30 +0900

    タスク 153: 更新 (329 -> 306)

--- 153/3_submission.py
+++ 153/3_submission.py
@@ -1,8 +1,9 @@
 def p(g):
- d={};[x and d.setdefault(x,set()).add(divmod(i,10))for i,x in enumerate(sum(g,[]))];f=lambda s:{(x-a,y-b)for a,b in[map(min,zip(*s))]for x,y in s};(a,A),(b,B)=d.items();A=f(A);B=f(B)
- while 1:
-  if f({(i//3,i%3)for i in range(9)}-A)==B:
-   o=[[b]*3,[b]*3,[b]*3]
-   for x,y in A:o[x][y]=a
-   return o
-  a,b,A,B=b,a,B,A
+ d={};[d.setdefault(x,set()).add(divmod(i,10))for i,x in enumerate(sum(g,[]))if x]
+ (a,A),(b,B)=d.items()
+ f=lambda s:{(x-a,y-b)for a,b in[map(min,zip(*s))]for x,y in s}
+ A=f(A);B=f(B)
+ if f({divmod(i,3)for i in range(9)}-A)^B:a,b,A=b,a,B
+ o=[[b]*3,[b]*3,[b]*3]
+ for x,y in A:o[x][y]=a
+ return o
