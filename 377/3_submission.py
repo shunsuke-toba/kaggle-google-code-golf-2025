fd88b9e9053aaa4997465e1043c89c824d8d8040
sak74y <yusei.sakurai@third-inc.co.jp>
2025-08-22 12:44:40 +0900

    377:取り急ぎ101Bytes

--- 377/3_submission.py
+++ 377/3_submission.py
@@ -1,11 +1,3 @@
-R=range
 def p(g):
- s=[]
- while g:
-  c=g[0][0];s+=c,
-  while g and {c}=={*g[0]}:g=g[1:]
-  while g and {c}=={*g[-1]}:g=g[:-1]
-  while g and {c}=={r[0]for r in g}:g=[r[1:]for r in g]
-  while g and {c}=={r[-1]for r in g}:g=[r[:-1]for r in g]
- m=len(s)*2-1
- return [[s[min(i,j,m-1-i,m-1-j)]for j in R(m)]for i in R(m)]
+ for _ in"__":a=[g[0]];[a.append(r)for r in g if a[-1]!=r];g=[*map(list,zip(*a))]
+ return a
