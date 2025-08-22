ae3933e87c19010f273aeebf0e69ae648c45e2e7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:01:59 +0900

    タスク 145: 更新 (329 -> 327)

--- 145/3_submission.py
+++ 145/3_submission.py
@@ -1,13 +1,14 @@
 def p(g):
  h=len(g);w=len(g[0]);r=range;s=[]
- def f(y,x):
+ def F(y,x):
   if h>y>=0<=x<w and g[y][x]<1:
-   g[y][x]=v;s[-1]+=1;f(y+1,x);f(y-1,x);f(y,x+1);f(y,x-1)
+   g[y][x]=v;s[-1]+=1
+   for d in 1,-1:F(y+d,x);F(y,x+d)
  for y in r(h):
   for x in r(w):
-   if g[y][x]<1:s+=0,;v=len(s)+2;f(y,x)
+   if g[y][x]<1:s+=0,;v=len(s)+2;F(y,x)
  m=max(s);n=min(s)
  for R in g:
   for x in r(w):
-   if (v:=R[x])>2:a=s[v-3];R[x]=a==m or 8*(a==n)
+   if (v:=R[x])>2:a=s[v-3];R[x]=(a==m)+8*(a==n)
  return g
