7f5e72a3353297f2b3673638cde0cece24c5c066
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 09:00:06 +0900

    094: コードゴルフ最適化の実装 (161 -> 158)

--- 094/3_submission.py
+++ 094/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
  for z in range(288):
   x,y=z//24,z%24//2
-  if g[x][y]==g[x][y+1]==g[x+1][y]<2:g[x+2]=[c-2*(c>7) for c in g[x+2]]
-  g=list(map(list,zip(*g)))
+  if g[x][y]==g[x][y+1]==g[x+1][y]<2:g[x+2]=[c-2*(c>7)for c in g[x+2]]
+  g=[*map(list,zip(*g))]
  return g
