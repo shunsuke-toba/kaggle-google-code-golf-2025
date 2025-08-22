0f8715cb296eca3aeb9df597993d5653eafd470f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 02:41:39 +0900

    タスク 209: 更新 (640 -> 637)

--- 209/3_submission.py
+++ 209/3_submission.py
@@ -8,14 +8,14 @@ def p(g):
   r=t//x;o=t%x
   if g[r][o]and not(a<=r<=b and c<=o<=d):e=min(e,r);f=max(f,r);i=min(i,o);j=max(j,o)
  h=f-e+1;w=j-i+1;z=k=P=0
- for s in(2,3,4):
+ for s in 2,3,4:
   H=h*s;W=w*s
   for r in range(a,b-H+2):
    for o in range(c,d-W+2):
     m=n=0
     for t in range(H*W):
       if(v:=g[r+t//W][o+t%W]):n+=1;m+=v!=g[e+t//W//s][i+t%W//s]
-    if m<1<n-z+1:z=n;k=s;P=r,o
+    if m<1<n-z:z=n;k=s;P=r,o
  r,o=P
  for q in range(h*k):g[r+q][o:o+w*k]=[g[e+q//k][i+p//k]for p in range(w*k)]
  return[r[c:d+1]for r in g[a:b+1]]
