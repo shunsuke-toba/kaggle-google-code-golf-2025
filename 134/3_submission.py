e273396a2c728b27a79bf7b935d0da53f6c1d7e1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:01:58 +0900

    タスク 134: 更新 (366 -> 354)

--- 134/3_submission.py
+++ 134/3_submission.py
@@ -1,8 +1,10 @@
-def p(g,R=range,E=enumerate):
- m={}
- for y,r in E(g):
-  for x,v in E(r):
-   if v:b=m.setdefault(v,[x,y,x,y]);b[:]=min(b[0],x),min(b[1],y),max(b[2],x),max(b[3],y)
- s,o=sorted(m,key=lambda k:((b:=m[k])[2]-b[0]!=b[3]-b[1],(b[2]-b[0])*(b[3]-b[1])))
- a,b,c,d=m[s];x=-~(c-a)//3
- return[[o*all(g[b+i*x+Y][a+j*x+X]==s for Y in R(x)for X in R(x))for j in R(3)]for i in R(3)]
+def p(g):
+ s={v for r in g for v in r if v}
+ for d in s:
+  x,y=zip(*[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==d])
+  t=max(x)-min(x)+1
+  if t==max(y)-min(y)+1>5<t<19 and t%3<1:
+   a=min(x);b=min(y);m=t//3;break
+ c=sum(s)-d
+ r=range
+ return[[c*any(g[i][j]==d for i in r(a+R*m,a+R*m+m) for j in r(b+C*m,b+C*m+m))for C in r(3)]for R in r(3)]
