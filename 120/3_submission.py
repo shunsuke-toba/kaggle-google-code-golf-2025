0a93c3514f7e2d7523b32e0f9c64c0ca746c6bf5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 19:30:16 +0900

    120: 座標変換の最適化 (272 -> 176)

--- 120/3_submission.py
+++ 120/3_submission.py
@@ -1,11 +1,1 @@
-def p(g):
- e=enumerate
- for i,r in e(g):
-  for j,c in e(r):
-   if c%8 and(i<1 or g[i-1][j]-c)and(j<1 or r[j-1]-c):
-    x=j+1
-    while x<len(r)and r[x]==c:x+=1
-    y=i+1
-    while y<len(g)and g[y][j]==c:y+=1
-    for z in range(i+1,y-1):g[z][j+1:x-1]=[8]*(x-j-2)
- return g
+p=lambda g:(b:=[r[:]for r in g],[b[r][c]==b[r][c+1]==b[r][c-1]==b[r+1][c]==b[r-1][c]!=0 and g[r].__setitem__(c,8)for r in range(1,len(g)-1)for c in range(1,len(g[0])-1)],g)[2]
