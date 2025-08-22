58cb6347cbf6a7efa0075ce3c396637b4847ff86
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 23:33:09 +0900

    タスク 270: 更新 (243 -> 241)

--- 270/3_submission.py
+++ 270/3_submission.py
@@ -4,7 +4,7 @@ def p(g):
   y,x=divmod(sum(g,[]).index(i),15);r[y][x]=i;t=11-4*i
   for Y,X in(1,0),(-1,0),(0,1),(0,-1):
    c,d=a,b=y+Y,x+X
-   while 15>a>=0<=b<15:
+   while-1<a<15>b>-1:
     if g[a][b]==t:r[c][d]=t;break
     a+=Y;b+=X
  return r
