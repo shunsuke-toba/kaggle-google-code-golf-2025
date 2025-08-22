149db55dfbe017a49437579d20a6cb4a0aea5144
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 02:41:33 +0900

    タスク 055: 更新 (245 -> 235)

--- 055/3_submission.py
+++ 055/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- a,b=[i for i,r in enumerate(g)if r[0]>7];c,d=[i for i,v in enumerate(g[0])if v>7];w=d-c-1
- for r in g[:a]:r[c+1:d]=[2]*w
+ a,b=[i for i,r in enumerate(g)if r[0]];c,d=[i for i,v in enumerate(g[0])if v];w=d-c-1
+ for i,r in enumerate(g[:a]+g[b+1:]):r[c+1:d]=[1+(i<a)]*w
  for r in g[a+1:b]:r[:c]=[4]*c;r[c+1:d]=[6]*w;r[d+1:]=[3]*(len(r)+~d)
- for r in g[b+1:]:r[c+1:d]=[1]*w
  return g
