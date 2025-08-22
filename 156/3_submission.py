dd397c659d81abd730201844415abff8b5fc2e04
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:42:31 +0900

    タスク 156: 更新 (341 -> 315)

--- 156/3_submission.py
+++ 156/3_submission.py
@@ -1,12 +1,10 @@
 def p(g):
- t=[]
+ a=[];b=[];t=a
  for y,r in enumerate(g):
-  for x,v in enumerate(r):
-   if v>3and(y<1or g[y-1][x]^4)and(x<1or r[x-1]^4):
-    X=x;Y=y
-    while X<len(r)and r[X]==4:X+=1
-    while Y<len(g)and g[Y][x]==4:Y+=1
-    t+=((Y-y)*(X-x),y,x,Y,X),
- for(a,y,x,Y,X),c in zip(sorted(t),(1,2)):
-  for y in range(y+1,Y-1):g[y][x+1:X-1]=[c]*(X-x-2)
+  if 4 in r:t+=[[y,r.index(4),10+~r[::-1].index(4)]]
+  elif t:t=b
+ if len(a)*(a[0][2]+~a[0][1])>len(b)*(b[0][2]+~b[0][1]):a,b=b,a
+ for t,c in(a,1),(b,2):
+  y0,y1,x0,x1=t[0][0],t[-1][0],t[0][1],t[0][2]
+  for y in range(-~y0,y1):g[y][-~x0:x1]=[c]*(~x0+x1)
  return g
