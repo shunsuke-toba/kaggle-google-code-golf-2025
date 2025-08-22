79eb859fbe69a41c04d98e2f1a5f995967f462f0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 19:57:43 +0900

    タスク 123: 更新 (93 -> 92)

--- 123/3_submission.py
+++ 123/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):c=[*filter(None,g[0])];r=range(10);return[[c[max(i,j)%len(c)]for j in r]for i in r]
+def p(g):r=range(10);c=[*filter(int,g[0])];return[[c[max(i,j)%len(c)]for j in r]for i in r]
