cf049f9e76c15e37565aefec961bf4bf8881215a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 20:42:31 +0900

    タスク 161: 更新 (164 -> 145)

--- 161/3_submission.py
+++ 161/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(s:=sum(g,[]),c:=next(x for x in s if s.count(x)==4),e:=enumerate)and[[((g[i][0]==g[i][-1]==c)|(g[0][j]==g[-1][j]==c))*c for j,x in e(r)]for i,r in e(g)]
+p=lambda g:(s:=sum(g,[]),c:=[i for i in s if s.count(i)==4][0])and[[c*(r[0]==r[-1]==c or g[0][j]==g[-1][j]==c)for j in range(len(r))]for r in g]
