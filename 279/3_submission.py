cbcf76fb8af7935a8d4ebd6247456771d2de1cee
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 23:33:11 +0900

    タスク 279: 更新 (324 -> 308)

--- 279/3_submission.py
+++ 279/3_submission.py
@@ -1,14 +1,13 @@
 def p(g):
- h=len(g);w=len(g[0]);s=h*w;v=[0]*s
+ w=len(g[0]);s=w*len(g);v=[0]*s
  for i in range(s):
-  c=g[i//w][i%w]
-  if c^9 and not v[i]:
-   m=[i];v[i]=1;e=0
-   for z in m:
-    for n in z-1,z+1,z-w,z+w:
-     if 0<=n<s and-2<n%w-z%w<2 and g[n//w][n%w]==c:
+  if g[i//w][i%w]^9 and v[i]<1:
+   q=[i];v[i]=1;e=0
+   for j in q:
+    for k in j-1,j+1,j-w,j+w:
+     if 0<=k<s and-2<k%w-j%w<2 and g[k//w][k%w]^9:
       e+=1
-      if not v[n]:v[n]=1;m+=n,
-   if e>=2*len(m):
-    for z in m:g[z//w][z%w]=8
+      if v[k]<1:v[k]=1;q+=k,
+   if e>=2*len(q):
+    for k in q:g[k//w][k%w]=8
  return g
