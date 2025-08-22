fecce31aab2e062fb7812582cd9d646da195e350
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 19:27:54 +0900

    115: 矩形マスク適用の最適化 (127 -> 101)

--- 115/3_submission.py
+++ 115/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):f=lambda s:[x for i,x in enumerate(s)if i<1 or s[i-1]^x];r=f(g[0]);return r[1:]and[r]or[[x]for x in f(next(zip(*g)))]
+p=lambda g:(r:=[*dict.fromkeys(g[0])])and([r]if r[1:]else[[c]for c in dict.fromkeys(next(zip(*g)))])
