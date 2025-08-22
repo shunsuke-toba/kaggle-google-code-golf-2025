3cd8db5a5f66af0a28d3d7560fee416a404691a4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 09:05:07 +0900

    099: コードゴルフ最適化の実装 (215 -> 212)

--- 099/3_submission.py
+++ 099/3_submission.py
@@ -1,11 +1,10 @@
 def p(g,A=range):
- r=[x[:]for x in g]
+ r=[*map(list,g)]
  for c in A(60):
-  i,j=c//6,c%6
-  F=g[i][j+2]
-  if F>1:
+  i,j=c//6,c%6;f=g[i][j+2]
+  if f>1:
    for y in A(i-2,min(i+3,9)):
     for x in A(j,j+5):
-     if g[y][x]^1:r[y][x]=F
-     if g[i-2][j]:r[i-3][x]=F
+     if g[i-2][j]:r[i-3][x]=f
+     if g[y][x]^1:r[y][x]=f
  return r
