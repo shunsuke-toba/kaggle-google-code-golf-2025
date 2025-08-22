ffeb45e458027830a21fd84695eef55bb3ce9f41
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 09:03:52 +0900

    097: コードゴルフ最適化の実装 (177 -> 126)

--- 097/3_submission.py
+++ 097/3_submission.py
@@ -1,5 +1,2 @@
 def p(g):
- h=len(g);w=len(g[0]);r=range
- for y in r(h):
-  for x in r(w):g[y][x]*=any(-1<i<h and -1<j<w and g[i][j]for i in r(y-1,y+2)for j in r(x-1,x+2)if i-y or j-x)
- return g
+ return [[v*(sum(sum(r[x and x-1:x+2])for r in g[y and y-1:y+2])>v)for x,v in enumerate(r)]for y,r in enumerate(g)]
