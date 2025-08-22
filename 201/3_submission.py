944cd2d721d9298b62ee8e5baa42259a2ede1f1d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 22:21:59 +0900

    タスク 201: 更新 (494 -> 471)

--- 201/3_submission.py
+++ 201/3_submission.py
@@ -1,10 +1,10 @@
-def p(j):
- A=enumerate;c=next
- E=lambda k,W:k<1or j[k-1][W]<1or k>2and j[k-1][W]==4and j[k-2][W]>0
- (k,W),(l,J),(a,l),l=[divmod(i,13)for i,v in A(sum(j,[]))if v==4]
- C=c(u for r in zip(*j)if 4not in r for u in r if u)
- e=c(i for i,r in A(j)if any(u==C and E(i,v)for v,u in A(r)))
- K=c(i for i,r in A(zip(*j))if any(u==C and E(v,i)for v,u in A(r)))
- for w in range(a-k-1):
-  for L in range(J-W-1):j[k+w+1][[J-L-1,W+L+1][j[k+1][W]==C]],j[e+w][K+L]=j[e+w][K+L],0
- return[r[W:J+1]for r in j[k:a+1]]
+def p(g):
+ f=[divmod(i,len(g[0]))for i,v in enumerate(sum(g,[]))if v==4];(a,b),(c,d)=f[0],f[-1];L,R=g[a+1][b],g[a+1][d];h=c-a-1;w=d-b-1
+ i,j=map(min,zip(*[(i,j)for i,r in enumerate(g)for j,u in enumerate(r)if u and u-4 and not(a<=i<=c and j in(b,d))]));S=i
+ while g[i][j]<1:i+=1
+ f=g[i][j]==R
+ o=[[0]*(w+2)for _ in range(h+2)]
+ o[0][0]=o[0][-1]=o[-1][0]=o[-1][-1]=4
+ for i in range(h):
+  o[i+1][0]=L;o[i+1][-1]=R;r=g[S+i][j:j+w];o[i+1][1:-1]=r[::-1]if f else r
+ return o
