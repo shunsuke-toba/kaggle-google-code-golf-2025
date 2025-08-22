b6913bf2ad41021050ef16b504b49076e4235d12
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 23:33:12 +0900

    タスク 280: 更新 (384 -> 332)

--- 280/3_submission.py
+++ 280/3_submission.py
@@ -1,15 +1,13 @@
 def p(g):
- *h,=map(list,g);H=len(g);W=len(g[0])
- for i in range(H*W):
-  if h[y:=i//W][x:=i%W]-2:continue
+ H=len(g);W=len(g[0])
+ P=[(y,x)for y in range(H)for x in range(W)if g[y][x]==2]
+ for y,x in P:
   for a,b in(1,0),(0,1),(-1,0),(0,-1):
-   if h[y+a][x+b]<1:break
+   if g[y+a][x+b]<1:break
   k=0;Y=y-a;X=x-b
-  while H>Y>-1<X<W>2<h[Y][X]:k+=1;Y-=a;X-=b
-  while 1:
-   for t in range(-k,k+1):
-    try:g[y+b*t][x+a*t]=3
-    except:0
+  while H>Y>-1<X<W>2<g[Y][X]:k+=1;Y-=a;X-=b
+  while H>y>-1<x<W:
+   for t in range(-k,k+1):g[y+b*t][x+a*t]=3
    g[y][x]=2
-   if not(H>(y:=y+a)>-1<(x:=x+b)<W>1>h[y][x]):break
+   y+=a;x+=b
  return g
