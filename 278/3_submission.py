0f140819b7b7fca39f09518e3bdd7b456b5a1785
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 23:33:11 +0900

    タスク 278: 更新 (247 -> 242)

--- 278/3_submission.py
+++ 278/3_submission.py
@@ -1,8 +1,1 @@
-def p(g):
- R=range;Y=len(g);X=len(g[0]);s={(i//X,i%X)for i in R(Y*X)if g[i//X][i%X]>1}
- for i,j in s:
-  if {(i+1,j),(i-1,j),(i,j+1),(i,j-1)}&s:
-   for y in R(i-1,i+2):
-    for x in R(j-1,j+2):
-     if X>x>=0<=y<Y and g[y][x]<1:g[y][x]=3
- return g
+p=lambda g:(R:=range,Y:=len(g),X:=len(g[0]),s:={(i,j)for i in R(Y)for j in R(X)if g[i][j]>1},[g[y].__setitem__(x,3)for i,j in s if {(i+1,j),(i-1,j),(i,j+1),(i,j-1)}&s for y in R(i-1,i+2)for x in R(j-1,j+2)if X>x>=0<=y<Y and g[y][x]<1],g)[-1]
