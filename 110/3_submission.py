58d9f2fa32051aac6474907d238b2361c6953f2e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 19:20:33 +0900

    110: 正方形グリッドパターン認識の最適化 (270 -> 177)

--- 110/3_submission.py
+++ 110/3_submission.py
@@ -1,7 +1,8 @@
 def p(g):
- R=range;n=len(g);m=len(g[0]);P=n;Q=m;b=n*m;D={}
- for p in R(1,n+1):
-  for q in R(1,m+1):
-   d={}
-   if p*q<b and all((v:=g[i][j])<1 or d.setdefault((i%p,j%q),v)==v for i in R(n) for j in R(m)):P,Q,b,D=p,q,p*q,d
- return[[D[i%P,j%Q]for j in R(m)]for i in R(n)]
+ t=[]
+ for i,r in enumerate(g):
+  for s in t:
+   if all(a==b or a*b<1 for a,b in zip(r,s)):break
+  else:t+=r,;s=r
+  s[:]=[x or y for x,y in zip(s,r)];g[i]=s
+ return g
