d95b0302483c345f09e76ae6612a5ce3d95445d7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 00:43:32 +0900

    タスク 350: 更新 (163 -> 157)

--- 350/3_submission.py
+++ 350/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  for _ in g,g:
   for r in g:
-   t=[i for i,x in enumerate(r)if x==1]
-   for a,b in zip(t,t[1:]):r[a+1:b]=[8]*(b+~a)
-  g=list(map(list,zip(*g)))
+   for a,b in zip(t:=[i for i,x in enumerate(r)if x&1],t[1:]):r[a+1:b]=[8]*(b+~a)
+  g=[*map(list,zip(*g))]
  return g
+
