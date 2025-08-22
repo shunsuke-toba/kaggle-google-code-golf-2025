e6868856cc9b68e399098d4dfb2d333b641f9afe
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-22 23:33:12 +0900

    タスク 281: 更新 (265 -> 262)

--- 281/3_submission.py
+++ 281/3_submission.py
@@ -2,7 +2,7 @@ def p(j):
  R=range;x=w=len(j[0]);u=len(j);v=y=b=c=0
  for i in R(u*w):
   k=i%w;i//=w
-  if(q:=j[i][k]):u=min(u,i);v=max(v,i);x=min(x,k);y=max(y,k);q%8and(b<1 and(b:=q)or(q-b and(c:=q)))
+  if(q:=j[i][k]):u=min(u,i);v=max(v,i);x=min(x,k);y=max(y,k);q%8and(b or(b:=q))and q-b and(c:=q)
  for i in R(u,v+1):
   for k in R(x,y+1):j[i][k]=[b,c][u<i<v and x<k<y]
  return j
