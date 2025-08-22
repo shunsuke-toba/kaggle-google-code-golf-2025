3456817cb3394513045fec1636e0203f3d5f7094
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 01:35:18 +0900

    タスク 361: 更新 (348 -> 333)

--- 361/3_submission.py
+++ 361/3_submission.py
@@ -1,12 +1,13 @@
 def p(g,R=range):
- h,w=len(g),len(g[0])
+ s=len(g)
  for k in 3,2:
-  for i in R(h-k+1):
-   for j in R(w-k+1):
+  for i in R(s-k+1):
+   for j in R(s-k+1):
     if all(g[y][x]for y in R(i,i+k)for x in R(j,j+k)):
      c,d=i+k/2-.5,j+k/2-.5
-     for z in R(h*w):
-      y,x=z//w,z%w;v=g[y][x];a,b=y-c,x-d
-      for m,n in(c-b,d+a),(c-a,d-b),(c+b,d-a):
-       if 0<v<h>m>-1<n<w:g[int(m)][int(n)]=v
+     for z in R(s*s):
+      y,x=z//s,z%s;a,b=y-c,x-d;v=g[y][x]
+      if v:
+       for m,n in(c-b,d+a),(c-a,d-b),(c+b,d-a):
+        g[int(m)][int(n)]=v
      return g
