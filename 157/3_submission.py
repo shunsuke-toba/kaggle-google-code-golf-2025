551c73006cb8b351d74137ddd92d37054e6f52df
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 02:41:37 +0900

    タスク 157: 更新 (698 -> 694)

--- 157/3_submission.py
+++ 157/3_submission.py
@@ -3,14 +3,14 @@ def p(g):
  for z in r(h*w):
   y=z//w;x=z%w
   if g[y][x]>4:
-   q=[y,x];g[y][x]=0;t=[]
+   q=[y,x];t=[];g[y][x]=0
    while q:
     x=q.pop();y=q.pop();t+=y,x
     for i in r(4):
      Y=y+d[i];X=x+d[i+1]
-     if 0<=Y<h and 0<=X<w and g[Y][X]>4:g[Y][X]=0;q+=Y,X
-   m=min(t[::2]);n=min(t[1::2])
-   c+=[[[t[i]-m,t[i+1]-n]for i in r(0,len(t),2)]]
+     if h>Y>=0<=X<w and g[Y][X]>4:g[Y][X]=0;q+=Y,X
+   m=min(t[::2]);n=min(t[1::2]);c+=[[[t[i]-m,t[i+1]-n]for i in r(0,len(t),2)]]
+ i=Y0=X0=0
  while c:
   b=0
   for I,T in enumerate(c):
@@ -21,7 +21,7 @@ def p(g):
       u=Y+y;v=X+x
       if u>=h or v>=w or g[u][v]:break
       for j in r(4):
-       a=u+d[j];z=v+d[j+1];s+=0<=a<h and 0<=z<w and g[a][z]==2
+       a=u+d[j];z=v+d[j+1];s+=h>a>=0<=z<w and g[a][z]==2
      else:
       if s>b:b=s;i=I;Y0=Y;X0=X
   for y,x in c.pop(i):g[Y0+y][X0+x]=1
