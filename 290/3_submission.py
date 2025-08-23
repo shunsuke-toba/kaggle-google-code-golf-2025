0a6e2e7821a79afd61d7148abe9f7fff793c4a83
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 08:36:23 +0900

    タスク 290: 更新 (157 -> 150)

--- 290/3_submission.py
+++ 290/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):e=enumerate;y,x,v=zip(*((i,j,v)for i,r in e(g)for j,v in e(r)if v));a,b={*v};return[[a^b^k for k in r[min(x):max(x)+1]]for r in g[min(y):max(y)+1]]
+def p(g):e=enumerate;y,x,v=zip(*((i,j,k)for i,r in e(g)for j,k in e(r)if k));a,b={*v};return[[a^b^k for k in r[x[0]:x[-1]+1]]for r in g[y[0]:y[-1]+1]]
