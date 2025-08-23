6fedbddc7a7f62bf4590ad52fa97b70657903e8b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 08:36:19 +0900

    タスク 093: 更新 (190 -> 187)

--- 093/3_submission.py
+++ 093/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- if 5 in g[0]:return [*map(list,zip(*p([*map(list,zip(*g))])))]
- for z in range(9**5):x,y=z//14%12+1,z%14;i=x+1-2*(x<7);u=g[x][y];g[x][y]=u and 5 or g[i][y];g[i][y]*=u>0
- return g
+p=lambda g:5 in g[0]and[*map(list,zip(*p([*map(list,zip(*g))])))]or(exec('for z in range(9**5):x,y=z//14%12+1,z%14;i=x+1-2*(x<7);u=g[x][y]>0;g[x][y]=u and 5 or g[i][y];g[i][y]*=u'),g)[1]
