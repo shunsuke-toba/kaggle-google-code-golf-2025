8b4dd7d44774e15e4b961831d231306a2cb9fc43
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 00:23:54 +0900

    タスク 191: 更新 (410 -> 408)

--- 191/3_submission.py
+++ 191/3_submission.py
@@ -1,12 +1,12 @@
 def p(g,R=range,n=23):
  f=sum(g,[]);i=f.index(1);k=~f[::-1].index(1);m=[[x&4for x in r[i%n+1:k%n]]for r in g[i//n+1:n+k//n]];f=[*map(list,g)]
  for _ in R(4):
-  h=len(m);w=len(m[0])
+  r=m[::-1];h=len(r);w=len(r[0])
   for y in R(n-h+1):
    for x in R(n-w+1):
-    if[g[x:x+w]for g in g[y:y+h]]in(m,m[::-1]):
+    if[g[x:x+w]for g in g[y:y+h]]in(m,r):
      for V in R(y-1,y+h+1):
       for U in R(x-1,x+w+1):
        if-1<V<n>U>-1:f[V][U]=g[V][U]or 1
-  m=[*map(list,zip(*m[::-1]))]
+  m=[*map(list,zip(*r))]
  return f
