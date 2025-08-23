190a3d6a263f9286703034247f9569899a4c874c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:57:00 +0900

    タスク 383: 更新 (285 -> 283)

--- 383/3_submission.py
+++ 383/3_submission.py
@@ -1,7 +1,7 @@
 def p(g,R=range):
  h=len(g);w=len(g[0]);b=0;L=[]
  for k in R(h*w):
-  if (v:=g[y:=k//w][x:=k%w]):L+=[(y,x,v)];b or(a:=y,c:=x,t:=v);b=y;d=x
+  if(v:=g[y:=k//w][x:=k%w]):L+=(y,x,v),;b or(a:=y,c:=x,t:=v);b=y;d=x
  for y,x,v in L:
   if v-t:
    for i in R((y-1in(a,b-2))*h):g[i][x]=[v,t][a<=i<=b]
