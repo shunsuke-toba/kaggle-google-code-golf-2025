d64982c596297fbece426efe7197e2884bb05e71
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 22:10:51 +0900

    タスク 080: 更新 (465 -> 443)

--- 080/3_submission.py
+++ 080/3_submission.py
@@ -1,13 +1,13 @@
 def p(g):
- R=range;n=0
- while g[n]!=g[n][:1]*len(g[n]):n+=1
- s=n+1;m=-~len(g)//s;A=[g[i*s][::s]for i in R(m)];b=0
+ R=range;D=divmod;n=0
+ while len({*g[n]})>1:n+=1
+ s=n+1;A=[r[::s]for r in g[::s]];m=len(A);b=9
  for i in R((m-2)**2):
-  y,x=divmod(i,m-2);t=[r[x:x+3]for r in A[y:y+3]];u=sum(map(bool,sum(t,[])))
-  if u>b:b=u;P=t
+  y,x=D(i,m-2);t=[r[x:x+3]for r in A[y:y+3]];u=str(t).count('0')
+  if u<b:b,P,c=u,t,t[1][1]
  for i in R(m*m):
-  y,x=divmod(i,m)
-  if A[y][x]==P[1][1]:
+  y,x=D(i,m)
+  if A[y][x]==c:
    for k in R(9):
     u,v=y+k//3-1,x+k%3-1
     if 0<=u<m>v>=0:A[u][v]=P[k//3][k%3]
