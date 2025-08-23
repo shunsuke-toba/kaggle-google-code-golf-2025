51cbecfa71345939e3ddc3065ffdb3d4053ac715
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:57 +0900

    タスク 216: 更新 (237 -> 224)

--- 216/3_submission.py
+++ 216/3_submission.py
@@ -2,9 +2,8 @@ def p(g):
  b=o=0
  for n in range(400):
   if g[i:=n//20][j:=n%20]:
-   x=j;y=i
+   x=j;r=0;a=[];y=i
    while x<20>g[i][x]>0:x+=1
-   while y<20>g[y][j]>0:y+=1;a=[];r=0
-   for k in g[i:y]:s=k[j:x];a+=s,;r+=s.count(2);k[j:x]=[0]*(x-j)
+   while y<20>g[y][j]>0:s=g[y][j:x];a+=s,;r+=s.count(2);g[y][j:x]=[0]*(x-j);y+=1
    if r>b:o,b=a,r
  return o
