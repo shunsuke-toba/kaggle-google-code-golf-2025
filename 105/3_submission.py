667e5bf145d16ac7be7ae61d19d7b1007cb934b4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:39 +0900

    タスク 105: 更新 (369 -> 294)

--- 105/3_submission.py
+++ 105/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
- E=enumerate;R=range;P=[(i,j)for i,r in E(g)for j,v in E(r)if v];Y,X=zip(*P);a,b=min(Y),max(Y);c,d=min(X),max(X);I=R(a,b+1);J=R(c,d+1);S={i for i in R(a+1,b)if 1<sum(c<j<d for y,j in P if y==i)};T={j for j in R(c+1,d)if 1<sum(a<i<b for i,x in P if x==j)}
- for i in I:
-  for j in J:
-   if i in(a,b)or j in(c,d)or i in S or j in T:g[i][j]=g[i][j]or 2
+ E=enumerate;P=[(i,j)for i,r in E(g)for j,v in E(r)if v];Y,X=zip(*P);a,b=min(Y),max(Y);c,d=min(X),max(X)
+ for i in range(a,b+1):
+  for j in range(c,d+1):
+   if i in(a,b)or j in(c,d)or 1<sum(c<j<d for y,j in P if y==i)or 1<sum(a<i<b for i,x in P if x==j):g[i][j]=g[i][j]or 2
  return g
