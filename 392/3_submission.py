44187fe4f4a6053ea653985579de0f373e203ca6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:53 +0900

    タスク 392: 更新 (399 -> 367)

--- 392/3_submission.py
+++ 392/3_submission.py
@@ -1,14 +1,10 @@
 def p(g,R=range):
  n=len(g);v=max(sum(g,[]))
  def D(r,c,t,m,b=0):
-  o=[[b]*60 for _ in[0]*60];r+=24;c+=24
+  o=[[b]*40 for _ in[0]*40]
   for i in R(m):
    s=(t+1)*i;A=r-s+t;B=r+s;C=c-s+t;E=c+s
    for y in R(A,B):o[y][C]=o[y][E-1]=v
    for x in R(C,E):o[A][x]=o[B-1][x]=v
-  return[o[24+i][24:24+n]for i in R(n)]
- for k in R(n):
-  for r,c in(k,0),(0,k):
-   for t in 1,2:
-    for m in 3,4:
-     if D(r,c,t,m)==g:return D(r,c,t,n,5)
+  return[o[i][:n]for i in R(n)]
+ return next(D(r,c,t,n,5)for k in R(n)for r,c in((k,0),(0,k))for t in(1,2)for m in(3,4)if D(r,c,t,m)==g)
