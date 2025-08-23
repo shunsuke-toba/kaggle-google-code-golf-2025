fd90d48c0debf10f7c53547403b6fe2f88354a78
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:58 +0900

    タスク 255: 更新 (698 -> 697)

--- 255/3_submission.py
+++ 255/3_submission.py
@@ -1,18 +1,18 @@
 def p(a):
- R=range;S=[[0]*31for _ in R(31)]
+ w=range(31);S=[[0]*31for _ in w]
  for t,u,r in zip(S[1:],S,a):
-  for j in R(30):t[j+1]=t[j]+u[j+1]-u[j]+(r[j]>0)
+  for j in w[:30]:t[j+1]=t[j]+u[j+1]-u[j]+(r[j]>0)
  f=lambda y,x,Y,X:S[Y][X]-S[y][X]-S[Y][x]+S[y][x]<1
  b=0
- for y in R(30):
-  for x in R(30):
-   for Y in R(y+3,31):
-    for X in R(x+3,31):
+ for y in w[:30]:
+  for x in w[:30]:
+   for Y in w[y+3:]:
+    for X in w[x+3:]:
      if f(y,x,Y,X)*f(y-(y>0),x-(x>0),Y+(Y<30),X+(X<30))*(t:=max(Y-y,X-x)<<10|(Y-y)*(X-x))>b:b=t;A=y;B=x;C=Y;D=X
  for r in a[A:C]:r[B:D]=[3]*(D-B)
  O=[[0]*32]+[[0]+r+[0]for r in a]+[[0]*32]
- for i in R(A,C):
-  for j in R(B,D):
+ for i in w[A:C]:
+  for j in w[B:D]:
    for U,V in(1,0),(-1,0),(0,1),(0,-1):
     p=i+U+1;q=j+V+1
     while 0<p<31>q>0==O[p][q]|O[p+V][q+U]|O[p-V][q-U]:p+=U;q+=V
