fe7c9883a9046861e45f1c013ebcb1aa599b2313
pech1 <pechi2327@gmail.com>
2025-08-23 23:43:41 +0900

    355:submissionの更新

--- 355/3_submission.py
+++ 355/3_submission.py
@@ -1,5 +1,2 @@
-def p(g):
- c,h,w=min(f:=sum(g,[]),key=f.count),len(g),len(g[0]);r=[0]*h
- for k in range(h*w*9):
-  if g[i:=k//w%h][j:=k%w]==c and (n:=[g[x][y]for x,y in((i-1,j),(i+1,j),(i,j-1),(i,j+1))if h>x>-1<y<w]).count(s:=max(n,key=n.count))>1:g[i][j]=s;r[s]+=1
- return[[r.index(max(r))]]
+f=lambda a,b:zip(*filter(lambda r:b in r,a))
+def p(g):s=sum(g,[]);return[[max({*s}-{c:=min(s,key=s.count)},key=lambda b:sum(r.count(c)for r in f(f(g,b),b)))]]
