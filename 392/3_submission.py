d20299389bd97e382e101ff5df1ce0c3a7d1278a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 22:10:57 +0900

    タスク 392: 更新 (294 -> 277)

--- 392/3_submission.py
+++ 392/3_submission.py
@@ -1,4 +1,1 @@
-def p(g,R=range):
- n=len(g);v=max(sum(g,[]))
- D=lambda r,c,t,m,b=0:[[v if(d:=max(abs(2*i-2*r-t+1),abs(2*j-2*c-t+1)))and d%(t*2+2)==-~t and d<(m+m+1)*-~t else b for j in R(n)]for i in R(n)]
- return next(D(r,c,t,n,5)for k in R(n)for r,c in((k,0),(0,k))for t in(1,2)for m in(2,3)if D(r,c,t,m)==g)
+def p(g,R=range(10)):v=max(sum(g,[]));D=lambda r,c,t,m,b=0:[[v*((d:=max(abs(2*i-2*r-t+1),abs(2*j-2*c-t+1)))%(u:=t+1)<1<d//u%2*2 and d<m*2*u+u)or b for j in R]for i in R];return next(D(r,c,t,10,5)for k in R for r,c in((k,0),(0,k))for t in (1,2) for m in (2,3) if D(r,c,t,m)==g)
