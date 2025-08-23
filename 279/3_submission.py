65c880cf17f58f927597fc78e896382d90c2a7c1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:58 +0900

    タスク 279: 更新 (267 -> 265)

--- 279/3_submission.py
+++ 279/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  w=len(g[0]);s=w*len(g);v=[1]*s
  for i in range(s):
-  if v[i]>0<g[i//w][i%w]^9:
+  if v[i]&g[i//w][i%w]%9:
    q=[i];v[i]=d=0
    for j in q:
     for k in j-1,j+1,j-w,j+w:
