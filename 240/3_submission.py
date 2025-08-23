4d22c3aa29e329fb8c06250653fb595b4fb27e50
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 05:58:35 +0900

    タスク 240: 更新 (206 -> 205)

--- 240/3_submission.py
+++ 240/3_submission.py
@@ -3,7 +3,7 @@ def p(g):
  for i in R:
   for j in R:
    if v:=g[i][j]:g[j][~i]=g[~i][~j]=g[~j][i]=v
- for i in R[1:9:2]:
+ for i in 1,3,5,7:
   if v:=g[i][i+2]:
    for k in R[i+2:~i:2]:g[i][k]=g[~i][k]=g[k][i]=g[k][~i]=v
  return g
