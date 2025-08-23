2f8d6b5e1f662eabdcfddfcc5d8508af2cd40ae9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 05:58:33 +0900

    タスク 182: 更新 (270 -> 269)

--- 182/3_submission.py
+++ 182/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- r=range(25);m=range(len(g)-4)
- t=next([k for k in r if(v:=g[y+1+k//5][x+1+k%5])and(c:=v)]for y in m for x in m if g[y][x]&g[y][x+1]&g[y+1][x]>4)
+ r=range(25);m=range(len(g)-4);t=next([k for k in r if(v:=g[y+1+k//5][x+1+k%5])and(c:=v)]for y in m for x in m if g[y][x]&g[y][x+1]&g[y+1][x]>4)
  for y in m:
   for x in m:
    if[k for k in r if g[y+k//5][x+k%5]]==t:
