be1e87e2c54d3d1525f2aed6b006c559035e6eae
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 08:36:19 +0900

    タスク 101: 更新 (510 -> 498)

--- 101/3_submission.py
+++ 101/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
  h=len(g);w=len(g[0]);R=range
- P=next(x for r in R(h)for c in R(w)for s in R(h,r,-1)for t in R(w,c,-1)if all(map(any,x:=[g[i][c:t]for i in R(r,s)]))*all(map(any,zip(*x)))*({1,2}<={*sum(x,[])}))
+ P=next(x for r in R(h)for c in R(w)for s in R(h,r,-1)for t in R(w,c,-1)if all(map(any,(x:=[g[i][c:t]for i in R(r,s)])+[*zip(*x)]))*sum({*sum(x,[])})>2)
  for S in 3,2,1:
   H=len(P)*S;W=len(P[0])*S
   for y in R(-H,h):
@@ -9,5 +9,4 @@ def p(g):
      for i in R(H):
       for j in R(W):
        if w>x+j>-1<y+i<h:g[y+i][x+j]=-P[i//S][j//S]
- for r in g:r[:]=map(abs,r)
- return g
+ return [list(map(abs,r))for r in g]
