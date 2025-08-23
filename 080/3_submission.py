9167926075a54a508ec61f3e65a6ae42f1a67c47
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:07:01 +0900

    タスク 080: 更新 (443 -> 397)

--- 080/3_submission.py
+++ 080/3_submission.py
@@ -1,17 +1,14 @@
 def p(g):
- R=range;D=divmod;n=0
+ R=range;n=0
  while len({*g[n]})>1:n+=1
- s=n+1;A=[r[::s]for r in g[::s]];m=len(A);b=9
- for i in R((m-2)**2):
-  y,x=D(i,m-2);t=[r[x:x+3]for r in A[y:y+3]];u=str(t).count('0')
-  if u<b:b,P,c=u,t,t[1][1]
+ s=n+1;A=[r[::s]for r in g[::s]];m=len(A)
+ P=min(((str(t:=[r[x:x+3]for r in A[y:y+3]]).count('0'),t)for y in R(m-2)for x in R(m-2)))[1];c=P[1][1]
  for i in R(m*m):
-  y,x=D(i,m)
+  y,x=divmod(i,m)
   if A[y][x]==c:
    for k in R(9):
     u,v=y+k//3-1,x+k%3-1
-    if 0<=u<m>v>=0:A[u][v]=P[k//3][k%3]
- for y in R(m):
-  for x in R(m):
-   for j in R(n):g[y*s+j][x*s:x*s+n]=[A[y][x]]*n
+    if 0<=u<m>v>=0:
+     w=P[k//3][k%3];A[u][v]=w
+     for j in R(n):g[u*s+j][v*s:v*s+n]=[w]*n
  return g
