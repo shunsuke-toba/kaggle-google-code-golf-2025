7c39eefe16975db188ef3f23ca5b70507911d857
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:53 +0900

    タスク 071: 更新 (280 -> 268)

--- 071/3_submission.py
+++ 071/3_submission.py
@@ -1,10 +1,8 @@
 def p(g,E=enumerate):
  a=sum(g,[])
- for b in{*a}-{0}:
-  if a.count(b)==sum(b in r for r in g)*sum(b in c for c in zip(*g)):break
- r=next(r for r in g if sum(r)and b not in r)
- t=[i for i,x in E(r)if x];s=t[0]+t[-1]
+ b=next(c for c in{*a}-{0}if a.count(c)==sum(c in r for r in g)*sum(c in r for r in zip(*g)))
+ s=min(t[0]+t[-1]for r in g if b not in r if(t:=[i for i,x in E(r)if x]))
  for r in g:
-  for c,x in E(r):
-   if x==b:r[c]=r[s-c]
+  for i,x in E(r):
+   if x==b:r[i]=r[s-i]
  return g
