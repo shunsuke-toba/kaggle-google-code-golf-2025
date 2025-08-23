c55fc033c853e3a3c788fc9459238aecdda83023
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:28 +0900

    タスク 034: 更新 (264 -> 258)

--- 034/3_submission.py
+++ 034/3_submission.py
@@ -1,11 +1,11 @@
 def p(g):
  k=sum({*sum(g,[])})-2;r=c=0
  while g[r][c]<1:c=(c+1)%9;r+=c<1
- for i,j in[(i,j)for i in(0,1)for j in(0,1)if g[r+i][c+j]-k]:
+ for t in[t for t in(0,1,3,2)if g[r+t//2][c+t%2]-k]:
   a,b=r,c
   while-2<a<9>b>-2:
    for u in a,a+1:
     for v in b,b+1:
      if-1<u<9>v>-1:g[u][v]=k
-   a+=i*2-1;b+=j*2-1
+   a+=t-t%2-1;b+=t%2*2-1
  return g
