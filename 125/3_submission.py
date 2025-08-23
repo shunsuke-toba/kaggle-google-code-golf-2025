a1ab0e0070a8791df67485c0c3f7408614ddf5b9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 05:58:31 +0900

    タスク 125: 更新 (234 -> 230)

--- 125/3_submission.py
+++ 125/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
- W=15
  for i in range(225):
-  if g[i//W][i%W]==8>0<i%W:g[i//W][i%W]=4
+  if g[i//15][i%15]==8>0<i%15:g[i//15][i%15]=4
  for i in range(5**7):
-  x=i//135%W;y=i//9%W;a=i//3%3-1;b=i%3-1;t=g[x][y];u=g[(x+a)%W][(y+b)%W];g[x][y]=3if t>u>5else 8if 7<u==t*2else t
+  x=i//135%15;y=i//9%15;a=i//3%3-1;b=i%3-1;t=g[x][y];u=g[(x+a)%15][(y+b)%15];g[x][y]=3if t>u>5else 8if 7<u==t*2else t
  return g
