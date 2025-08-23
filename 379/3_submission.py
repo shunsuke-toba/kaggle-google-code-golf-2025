612bfc2a7b17a119a31addf5a09470d048b7a759
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:52 +0900

    タスク 379: 更新 (344 -> 335)

--- 379/3_submission.py
+++ 379/3_submission.py
@@ -2,9 +2,9 @@ def p(g):
  h=len(g);w=len(g[0]);t=1,0,-1,0,1
  for i,j in[(i,j)for i in range(h)for j in range(w)if g[i][j]==2]:
   for d,e in zip(t,t[1:]):
-   a,b=i,j;y,x=a+d,b+e
-   while h>y>-1<=x<w and g[y][x]<1:y+=d;x+=e
-   if h>y>-1<=x<w and g[y][x]>7:
+   a,b=i,j;y,x=i+d,j+e
+   while w>x>-1<y<h>g[y][x]<1:y+=d;x+=e
+   if w>x>-1<y<h>7<g[y][x]:
     while(a,b)!=(y,x):a+=d;b+=e;g[a][b]=2
-    for m in y-1,y,y+1:g[m][x-1:x+2]=[8]*3;g[y][x]=2
+    for m in y-1,y,y+1:g[m][x-1:x+2]=8,8,8;g[y][x]=2
  return g
