2a311530f7c027385f3de33726648a17849790b4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:17 +0900

    タスク 132: 更新 (215 -> 203)

--- 132/3_submission.py
+++ 132/3_submission.py
@@ -2,9 +2,9 @@ def p(g):
  m={};e=enumerate;s=sorted
  for y,R in e(g):
   for x,v in e(R):
-   if v:
-    try:
-     Y,X=m[v];a,b=s((y,Y));c,d=s((x,X))
-     for Y in range(a,b+1):g[Y][c:d+1]=[v]*(d-c+1)
-    except:m[v]=(y,x)
+   try:
+    Y,X=m[v];a,b=s((y,Y));c,d=s((x,X))
+    for R in g[a:b+1]:R[c:d+1]=[v]*(d-c+1)
+   except:
+    if v:m[v]=y,x
  return g
