7b10eb8fd1aa4e938d4ceead0f2782d8e7bbc948
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:15 +0900

    タスク 069: 更新 (310 -> 259)

--- 069/3_submission.py
+++ 069/3_submission.py
@@ -2,12 +2,13 @@ def p(g):
  r=range(10);p=[]
  for i in r:
   for j in r:
-   if(v:=g[i][j])%8:p+=[(i,j,v)];g[i][j]=0
- i,j,_=p[0];p=[(x-i,y-j,z)for x,y,z in p];o=[[0]*10 for _ in r]
+   if(v:=g[i][j])%8:
+    if not p:a,b=i,j
+    p+=[(i-a,j-b,v)];g[i][j]=0
  for i in r:
   for j in r:
    try:
-    if all(g[i+x][j+y]==8 for x,y,_ in p):
-     for x,y,v in p:o[i+x][j+y]=v;g[i+x][j+y]=0
+    if{g[i+x][j+y]for x,y,_ in p}=={8}:
+     for x,y,v in p:g[i+x][j+y]=v
    except:0
- return o
+ return g
