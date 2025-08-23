a4f7aba799727c05c2ea6897621b1b2ec0f282ce
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 08:36:22 +0900

    タスク 247: 更新 (183 -> 129)

--- 247/3_submission.py
+++ 247/3_submission.py
@@ -1,7 +1,5 @@
 def p(g):
- c=[0]*10;l=[9]*10
- for r in g:
-  for x,v in enumerate(r):
-   if v:c[v]+=1;l[v]=min(l[v],x)
+ c=[0]*10
+ for v in sum(g,[]):c[v]+=v>0
  m=max(c)
- return[[i for x,i in sorted((l[i],i)for i in range(10)if c[i]==m)]]*m
+ return[[*dict.fromkeys(v for v in sum(zip(*g),()) if m==c[v]>0)]]*m
