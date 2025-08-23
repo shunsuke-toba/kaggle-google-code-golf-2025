db02f9debcfdc19b522a78e5f194f23f758ca2db
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:10 +0900

    タスク 005: 更新 (384 -> 375)

--- 005/3_submission.py
+++ 005/3_submission.py
@@ -4,12 +4,11 @@ def p(g):
   c+=1;r+=c//N;c%=N
  for Y in-4,0,4:
   for X in-4,0,4:
-   if X|Y:
-    t=max(max(g[r+Y+i][c+X:c+X+3])for i in R);y,x=r,c
-    while t:
-     y+=Y;x+=X
-     for i in R:
-      for j in R:
-       if P[i][j]*(n>y+i>-1)*(n>x+j>-1):g[y+i][x+j]=t
-     if(-1<y<N)*(-1<x<N)<1:break
+   t=(X|Y)and max(max(g[r+Y+i][c+X:c+X+3])for i in R);y,x=r,c
+   while t:
+    y+=Y;x+=X
+    for i in R:
+     for j in R:
+      if P[i][j]*(n>y+i>-1)*(n>x+j>-1):g[y+i][x+j]=t
+    if(-1<y<N)*(-1<x<N)<1:break
  return g
