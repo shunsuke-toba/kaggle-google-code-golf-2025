bf7775236969e93084b0606205f0719b02450df9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:07:05 +0900

    タスク 286: 更新 (211 -> 210)

--- 286/3_submission.py
+++ 286/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
- h=len(g);w=len(g[0]);t=[*{*sum(g,[])}-{0,8}];d=1,0,-1,0
- for z in range(9**6):
-  r,q=z%97%h,z%89%w;i,j=r+d[z%4],q+d[z%4-3]
+ h=len(g);w=len(g[0]);t=[*{*sum(g,[])}-{0,8}];d=0,1,0,-1;z=9**6
+ while z:
+  z-=1;r,q=z%97%h,z%89%w;i,j=r+d[z%4-1],q+d[z%4]
   if h>i>-1<j<w and g[r][q]-8 and (c:=g[i][j])%8:g[r][q]=t[c==t[0]]
  return g
