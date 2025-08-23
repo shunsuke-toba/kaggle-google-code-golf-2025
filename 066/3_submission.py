3a08f3237465e620e268d34223656339f62c3b26
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:07:00 +0900

    タスク 066: 更新 (419 -> 404)

--- 066/3_submission.py
+++ 066/3_submission.py
@@ -1,12 +1,6 @@
 def p(g):
  (a,b),(c,d)=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==3]
- def f(y,x,a,b,t=2):
-  y+=a;x+=b
-  if len(g)>y>=0<=x<len(g[0]):
-   v=g[y][x]
-   if v==2:return 1
-   if v==8 and t and(f(y-a,x-b,b,-a,t-1)or f(y-a,x-b,-b,a,t-1)):return 1
-   if v-8 and f(y,x,a,b,t):g[y][x]=3;return 1
- for y,x,a,b in(((a,b,0,-1),(c,d,0,1)),((a,b,-1,0),(c,d,1,0)))[a!=c]:
-  if g[y+a][x+b]-8 and f(y,x,a,b):break
+ f=lambda y,x,A,B,t=2:(y:=y+A,x:=x+B,len(g)>y>=0<=x<len(g[0])and((v:=g[y][x])==2 or v==8 and t and(f(y-A,x-B,B,-A,t-1)or f(y-A,x-B,-B,A,t-1)) or v-8 and f(y,x,A,B,t)and(g[y].__setitem__(x,3)or 1)))[2]
+ for y,x,A,B in(((a,b,0,-1),(c,d,0,1)),((a,b,-1,0),(c,d,1,0)))[a!=c]:
+  if g[y+A][x+B]-8 and f(y,x,A,B):break
  return g
