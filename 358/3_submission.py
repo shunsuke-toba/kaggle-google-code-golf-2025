77d6fec692409be98c27718187e6df1537c1d072
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:23 +0900

    タスク 358: 更新 (265 -> 192)

--- 358/3_submission.py
+++ 358/3_submission.py
@@ -1,10 +1,9 @@
 def p(g,L=len,F=filter):
- r=c=0
- while L({*g[r]})<3:r+=1
- a=g[r];s=[*F(None,a)];n=L(s);d=a.index(s[0])
- a[:]=(s*(L(a)//n+2))[-d%n:][:L(a)]
- C=*zip(*g),
- while L({*C[c]})<3:c+=1
- s=[*F(None,C[c])];d=C[c].index(s[0])
- for i in range(L(g)):g[i][c]=s[(i-d)%n]
+ for _ in 0,1:
+  r=0
+  while L({*g[r]})<3:r+=1
+  a=g[r];s=[*F(None,a)];n=L(s);d=a.index(s[0])
+  a[:]=(s*(L(a)//n+2))[-d%n:][:L(a)]
+  g=[*map(list,zip(*g))]
  return g
+
