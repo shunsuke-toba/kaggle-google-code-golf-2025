bff5a6372fe8f1ac38e3ae2d4ec5eead53a82bf8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 08:36:20 +0900

    タスク 133: 更新 (530 -> 522)

--- 133/3_submission.py
+++ 133/3_submission.py
@@ -1,16 +1,15 @@
 def p(g):
- l=len;r=range;w=l(g[0])
+ l=len;h=l(g);w=l(g[0]);r=range
  def f(y,x):
   c=[(y,x,g[y][x])];g[y][x]=0
-  for Y,X in(y+1,x),(y-1,x),(y,x+1),(y,x-1):
-   if l(g)>Y>=0<=X<w and g[Y][X]:c+=f(Y,X)
+  for U,V in(y+1,x),(y-1,x),(y,x+1),(y,x-1):
+   if h>U>=0<=V<w and g[U][V]:c+=f(U,V)
   return c
- C=[f(i//w,i%w)for i in r(l(g)*w)if g[i//w][i%w]]
- B=min((c for c in C if l(c)>2),key=l)
- d=[*zip(*B)][2];Py,Px=B[d.index(p:=min(d,key=d.count))][:2]
+ C=[f(i//w,i%w)for i in r(h*w)if g[i//w][i%w]]
+ B=min((c for c in C if c[2:]),key=l)
+ d=[*zip(*B)][2];Y,X=B[d.index(p:=min(d,key=d.count))][:2]
  for c in C:
-  m=int(l(A:=[(y,x)for y,x,v in c if v==p])**.5);a,b=min(A);L=sum({v for *_,v in c})-p
+  m=int(l(A:=[x[:2]for x in c if x[2]==p])**.5);a,b=min(A);L=sum({x[2]for x in c})-p
   for y,x,t in B:
-   for i in r(m):g[a+(y-Py)*m+i][b+(x-Px)*m:b+(x-Px)*m+m]=[[p,L][t!=p]]*m
+   for i in r(m):g[a+(y-Y)*m+i][b+(x-X)*m:b+(x-X)*m+m]=[[p,L][t!=p]]*m
  return g
-
