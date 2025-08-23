c0c55955ff99ede80bc5b3c5dd8c7451ebf29bd9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:07:02 +0900

    タスク 165: 更新 (242 -> 228)

--- 165/3_submission.py
+++ 165/3_submission.py
@@ -3,10 +3,10 @@ def p(g):
  k=min(k for y in r[1:]for x in r[1:-1]if(k:=g[y][x])==g[y-1][x]==g[y][x-1]==g[y][x+1]>0)
  for x in r:
   c=0
-  for y in r[::-1]:
-   v=g[y][x]
+  for y in r:
+   v=g[~y][x]
    if v==k:
-    for t in g[y+1:]*(c>0):t[x]=c
+    for t in g[-y:]*c:t[x]=c
     break
-   c=c or v
+   c|=v
  return g
