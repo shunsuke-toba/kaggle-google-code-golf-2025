e849d6cf70311981fbfdb9e22c661bdd35080f6a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 05:58:32 +0900

    タスク 133: 更新 (543 -> 530)

--- 133/3_submission.py
+++ 133/3_submission.py
@@ -7,10 +7,10 @@ def p(g):
   return c
  C=[f(i//w,i%w)for i in r(l(g)*w)if g[i//w][i%w]]
  B=min((c for c in C if l(c)>2),key=l)
- d=[*zip(*B)][2];a,b=B[d.index(p:=min(d,key=d.count))][:2];B=[(y-a,x-b,v)for y,x,v in B]
+ d=[*zip(*B)][2];Py,Px=B[d.index(p:=min(d,key=d.count))][:2]
  for c in C:
   m=int(l(A:=[(y,x)for y,x,v in c if v==p])**.5);a,b=min(A);L=sum({v for *_,v in c})-p
-  for u,v,t in B:
-   for i in r(m):g[a+u*m+i][b+v*m:b+v*m+m]=[[p,L][t!=p]]*m
+  for y,x,t in B:
+   for i in r(m):g[a+(y-Py)*m+i][b+(x-Px)*m:b+(x-Px)*m+m]=[[p,L][t!=p]]*m
  return g
 
