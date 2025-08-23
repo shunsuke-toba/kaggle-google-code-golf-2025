611260bd686d7f1de6ae79bd4c6fe3e58945d173
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 00:23:54 +0900

    タスク 196: 更新 (270 -> 263)

--- 196/3_submission.py
+++ 196/3_submission.py
@@ -1,9 +1,9 @@
 def p(g):
- n=len(g)
+ n=len(g);k=n*n
  def f(i,j):
   if-1<i<n>j>=0<g[i][j]<2:g[i][j]=0;c.append((i,j));f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
- for k in range(n*n):
-  c=[];f(k//n,k%n)
+ while k:
+  k-=1;c=[];f(k//n,k%n)
   if c:h,w=[max(t)-min(t)for t in zip(*c)]
-  for y,x in c:g[y][x]=1+2*(c and len(c)/2==h+w>h>1<w)
+  for y,x in c:g[y][x]=1+2*(len(c)/2==h+w>h>1<w)
  return g
