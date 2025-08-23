b16a84da034d5c6e471c87f01b5de77f280fb92b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:44 +0900

    タスク 023: 更新 (314 -> 312)

--- 023/3_submission.py
+++ 023/3_submission.py
@@ -3,8 +3,8 @@ def p(g):
  def f(s):
   if not s:return 1
   r,c=min(s)
-  for *q,k in(((0,0),(1,0),(2,0),2),((0,0),(0,1),(0,2),2),((0,0),(1,0),(0,1),(1,1),8)):
-   q={(r+i,c+j)for i,j in q}
+  for *q,k in((0,0,1,0,2,0,2),(0,0,0,1,0,2,2),(0,0,1,0,0,1,1,1,8)):
+   q={(r+i,c+j)for i,j in zip(q[::2],q[1::2])}
    if q<=s and f(s-q):
     for r,c in q:g[r][c]=k
     return 1
