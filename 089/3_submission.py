f2c4a8ae317d30d01931e19f1a0323dc01a4988f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:53 +0900

    タスク 089: 更新 (402 -> 376)

--- 089/3_submission.py
+++ 089/3_submission.py
@@ -1,12 +1,11 @@
+R=range(-2,3)
 def p(g):
- n=len(g);R=range(-2,3)
  for k in 2,3:
-  a=[(i,j)for i in range(n) for j in range(n) if g[i][j]==k];B=s=()
+  a=[divmod(t,13)for t in range(169)if g[t//13][t%13]==k];B=s=()
   for i,j in a:
-   t=[(x,y,g[i+x][j+y])for x in R for y in R if 0<=i+x<n>j+y>=0 and 0<(v:=g[i+x][j+y])!=k]
+   t=[(x,y,v)for x in R for y in R if 13>i+x>-1<j+y<13 and 0<(v:=g[i+x][j+y])!=k]
    if len(t)>len(B):c=sorted(v for*_,v in t)[len(t)//2];B=[(x,y)for x,y,v in t if v==c];s=i,j
-  m=k*2-5
   for i,j in a:
    if(i,j)!=s:
-    for x,y in B:g[i+x][j+y*m]=c
+    for x,y in B:g[i+x][j+y*(k*2-5)]=c
  return g
