8e6d740f5694184f38c3d8e43211fb7790d9b5d4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:37 +0900

    タスク 044: 更新 (575 -> 561)

--- 044/3_submission.py
+++ 044/3_submission.py
@@ -7,16 +7,16 @@ def p(g):
    q=[(y,x)];g[y][x]=5;f=c<1
    for y,x in q:
     for Y,X in(y+1,x),(y-1,x),(y,x+1),(y,x-1):
-     if 0<=Y<10 and 0<=X<10:
+     if-1<Y<10> X>-1:
       d=g[Y][X]
-      if d==c:g[Y][X]=5;q.append((Y,X))
+      if d==c:g[Y][X]=5;q+=[(Y,X)]
       elif f and d-5:f=0
      elif f:f=0
    Y,X=map(min,zip(*q));s=frozenset((i-Y,j-X)for i,j in q)
    if f:a[s]=Y,X
-   elif c:b[c]=c in b or not q[1:] and 1 or(s,q,Y,X)
+   elif c:b[c]=(c in b or not q[1:])and[0]or(s,q,Y,X)
  for c,v in b.items():
-  if v!=1 and v[0] in a:
+  if v and v[0] in a:
    s,q,y,x=v
    Y,X=a[s]
    for i,j in q:r[i][j]=0;r[i-y+Y][j-x+X]=c
