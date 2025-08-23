5119002d89d2887a1f1216897603fd29f194eb23
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:36 +0900

    タスク 018: 更新 (623 -> 617)

--- 018/3_submission.py
+++ 018/3_submission.py
@@ -7,14 +7,15 @@ def p(g):
     for t in R(x+1,w+1):
      if (m:=[g[i][x:t] for i in R(y,s)])==[*map(list,zip(*F(any,zip(*F(any,m)))))]and L({0,*sum(m,[])})>4:q,a,b=m,s,t
    if q:S+=q,
-   for i in R(y,a):g[i][x:b]=[0]*(b-x)
+   for r in g[y:a]:r[x:b]=[0]*(b-x)
  for r in S:
+  u=sum(r,[])
   for z in R(8):
-   a=L(r);b=L(r[0]);u=sum(r,[])
+   a=L(r);b=L(r[0])
    for y in R(h-a+1):
     for x in R(w-b+1):
      if all(r[i][j]*(u.count(r[i][j])==1)==g[y+i][x+j]for i in R(a)for j in R(b)):
       for i in R(a):g[y+i][x:x+b]=r[i]
-   r=[list(t)for t in zip(*r[::-1])]
+   r=[*map(list,zip(*r[::-1]))]
    if z&3>2:r=r[::-1]
  return g
