ca71d3812465976aea539c7c703250eaf5f93730
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:30 +0900

    タスク 066: 更新 (530 -> 430)

--- 066/3_submission.py
+++ 066/3_submission.py
@@ -1,19 +1,12 @@
 def p(g):
- h=len(g);w=len(g[0]);s=[(i,j)for i in range(h)for j in range(w)if g[i][j]==3]
- def f(y,x,a,b,t=0):
-  y+=a;x+=b
-  if not(0<=y<h and 0<=x<w):return
+ h=len(g);w=len(g[0]);(a,b),(c,d)=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==3]
+ def f(y,x,A,B,t=0):
+  y+=A;x+=B
+  if not(-1<y<h and-1<x<w):return
   v=g[y][x]
-  if v==2:return[(y-a,x-b)]
-  if v<8:r=f(y,x,a,b,t)
-  elif t<2:r=f(y-a,x-b,-b,a,t+1)or f(y-a,x-b,b,-a,t+1)
-  else:return
-  return r and [(y-a,x-b)]+r
- (y,x),(Y,X)=sorted(s)
- for y,x,a,b in(((y,x,0,-1),(Y,X,0,1)),((y,x,-1,0),(Y,X,1,0)))[y!=Y]:
-  if 0<=y+a<h and 0<=x+b<w and g[y+a][x+b]-8:
-   r=f(y,x,a,b)
-   if r:
-    for y,x in r:g[y][x]=3
-    break
+  if v==2:return 1
+  if v==8:return t<2 and (f(y-A,x-B,-B,A,t+1)or f(y-A,x-B,B,-A,t+1))
+  if f(y,x,A,B,t):g[y][x]=3;return 1
+ for y,x,A,B in(((a,b,0,-1),(c,d,0,1)),((a,b,-1,0),(c,d,1,0)))[a!=c]:
+  if g[y+A][x+B]!=8 and f(y,x,A,B):break
  return g
