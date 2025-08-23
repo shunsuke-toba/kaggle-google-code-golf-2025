70e8931b79f304fa02e5f8e7d8ff6f1bf3a80ee4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 08:36:20 +0900

    タスク 138: 更新 (282 -> 279)

--- 138/3_submission.py
+++ 138/3_submission.py
@@ -1,5 +1,6 @@
 def p(g):
- f=lambda z:[i for i,r in enumerate(z)if min(r)];a,b,c,d=f(g)+f(zip(*g))
+ f=lambda z:[*map(z.index,filter(min,z))]
+ a,b,c,d=f(g)+f([*zip(*g)])
  for i in range(a+1,b):
   for j in range(c+1,d):
    x=g[I:=i][j];y=g[b][j]==x;y-=g[a][j]==x;z=g[i][d]==x;z-=g[i][c]==x
