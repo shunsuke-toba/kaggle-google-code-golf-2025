63406ab848de0fdad31c3e80c741c3221d4e9e74
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:51 +0900

    タスク 234: 更新 (366 -> 363)

--- 234/3_submission.py
+++ 234/3_submission.py
@@ -1,9 +1,9 @@
 def p(g):
- c=n=k=0;N=len(g);M=len(g[0]);d=1,0,-1,0,1;r=range
- for i in r(N):
-  for j in r(M):
+ c=n=k=0;d=1,0,-1,0,1;r=range;H=len(g);R=r(len(g[0]))
+ for i in r(H):
+  for j in R:
    if(m:=g[i][j])and(g[i-1][j]|g[i+1][j])*(g[i][j-1]|g[i][j+1])<1:c+=1;n=m;g[i][j]=0;[k:=t for t in r(4)if(v:=g[i+d[t]][j+d[t+1]])*(v-n)]
- for i in r(N)[::k>0 or-1]:
-  for j in r(M)[::k<3 or-1]:
+ for i in r(H)[::k>0 or-1]:
+  for j in R[::k<3 or-1]:
    if g[i][j]==n>g[a:=i+d[k]*c][b:=j+d[k+1]*c]:g[a][b]=n;g[i][j]=0
  return g
