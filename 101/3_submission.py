7ffce7ffba067664c9232e77a57f268b90349ed8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:38 +0900

    タスク 101: 更新 (646 -> 631)

--- 101/3_submission.py
+++ 101/3_submission.py
@@ -7,8 +7,7 @@ def p(g):
     for t in R(c+1,w+1):
      if all(map(any,x:=[g[i][c:t]for i in R(r,s)]))and all(map(any,zip(*x)))and{1,2}<={*sum(x,[])}:p=x;u={(i,j)for i in R(r,s)for j in R(c,t)}
  for s in 3,2,1:
-  r=[[p[i//s][j//s]for j in R(len(p[0])*s)]for i in R(len(p)*s)]
-  H=len(r);W=len(r[0])
+  H=len(p)*s;W=len(p[0])*s;r=[[p[i//s][j//s]for j in R(W)]for i in R(H)]
   for y in R(-H,h):
    for x in R(-W,w):
     if all((r[i][j]==2)==(w>x+j>-1<y+i<h>g[y+i][x+j]==2)and(y+i,x+j)not in u for i in R(H)for j in R(W)):
