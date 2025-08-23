062269cca9f6b145c65aa9acb55c9b98f47ccc55
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:14 +0900

    タスク 029: 更新 (262 -> 249)

--- 029/3_submission.py
+++ 029/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  for k in{*sum(g,[])}:
-  y=[i for i,r in enumerate(g)if k in r];x=[i for i,c in enumerate(zip(*g))if k in c];a,b=y[0],y[-1];c,d=x[0],x[-1]
+  y=[i for i,r in enumerate(g)if k in r];a,b=y[0],y[-1];r=g[a];c=r.index(k);d=len(r)+~r[::-1].index(k)
   if{k}=={*g[a][c:d+1],*g[b][c:d+1],*(v for r in g[a:b+1]for v in(r[c],r[d]))}:return[r[c+1:d]for r in g[a+1:b]]
