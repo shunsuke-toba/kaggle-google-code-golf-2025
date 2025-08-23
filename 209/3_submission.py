f9e1c8154cdac80a9446971ee6226ec778dd8894
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 00:23:55 +0900

    タスク 209: 更新 (592 -> 575)

--- 209/3_submission.py
+++ 209/3_submission.py
@@ -1,19 +1,17 @@
 def p(g):
- n=len(g);m=len(g[0]);a=n;c=m;b=d=0;S=[]
- for t in range(n*m):
+ m=len(g[0]);a=e=len(g);c=i=m;b=d=f=j=0;S=[];I=[]
+ for t in range(e*m):
   if (v:=g[r:=t//m][o:=t%m])==4:a=min(a,r);b=max(b,r);c=min(c,o);d=max(d,o)
   elif v:S+=(r,o),
- e=n;f=0;i=m;j=0;I=[]
  for r,o in S:
   if a<=r<=b and c<=o<=d:I+=(r,o),
   else:e=min(e,r);f=max(f,r);i=min(i,o);j=max(j,o)
  h=f-e+1;w=j-i+1
  for k in 2,3,4:
-  H=h*k;W=w*k
-  for R in range(a,b-H+2):
-   for O in range(c,d-W+2):
+  for R in range(a,b-h*k+2):
+   for O in range(c,d-w*k+2):
     try:
      if all(g[e+(r-R)//k][i+(o-O)//k]==g[r][o]for r,o in I):
-      for q in range(H):g[R+q][O:O+W]=[g[e+q//k][i+p//k]for p in range(W)]
+      for q in range(h*k):g[R+q][O:O+w*k]=[g[e+q//k][i+p//k]for p in range(w*k)]
       return[r[c:d+1]for r in g[a:b+1]]
     except:0
