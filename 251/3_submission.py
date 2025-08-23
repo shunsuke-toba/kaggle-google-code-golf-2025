299e3f35781ae454cd5a91b4156aad301e8be3d3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:57 +0900

    タスク 251: 更新 (198 -> 196)

--- 251/3_submission.py
+++ 251/3_submission.py
@@ -1,6 +1,7 @@
 def p(g):
- h=len(g)
  def f(i,j):
-  if i*i<h*h>j*j and g[i][j]<1:g[i][j]=1;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
- for i in range(h):f(i,0);f(i,-1);f(0,i);f(-1,i)
+  try:
+   if g[i][j]<1:g[i][j]=1;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
+  except:0
+ for i in range(len(g)):f(i,0);f(i,-1);f(0,i);f(-1,i)
  return[[v^(v<2)for v in r]for r in g]
