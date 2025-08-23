f9179665c5b313fd0476d13b2b89139fc870c0e7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:13 +0900

    タスク 018: 更新 (627 -> 623)

--- 018/3_submission.py
+++ 018/3_submission.py
@@ -1,16 +1,16 @@
 def p(g):
- h=len(g);w=len(g[0]);R=range;F=filter;S=[]
+ L=len;h=L(g);w=L(g[0]);R=range;F=filter;S=[]
  for y in R(h):
   for x in R(w):
    q,a,b=[],y,x
    for s in R(y+1,h+1):
     for t in R(x+1,w+1):
-     if (m:=[g[i][x:t] for i in R(y,s)])==[*map(list,zip(*F(any,zip(*F(any,m)))))]and len({*sum(m,[]),0})>4:q,a,b=m,s,t
+     if (m:=[g[i][x:t] for i in R(y,s)])==[*map(list,zip(*F(any,zip(*F(any,m)))))]and L({0,*sum(m,[])})>4:q,a,b=m,s,t
    if q:S+=q,
    for i in R(y,a):g[i][x:b]=[0]*(b-x)
  for r in S:
   for z in R(8):
-   a=len(r);b=len(r[0]);u=sum(r,[])
+   a=L(r);b=L(r[0]);u=sum(r,[])
    for y in R(h-a+1):
     for x in R(w-b+1):
      if all(r[i][j]*(u.count(r[i][j])==1)==g[y+i][x+j]for i in R(a)for j in R(b)):
