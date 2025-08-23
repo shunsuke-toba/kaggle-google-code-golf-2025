3401f21a376f491d06860dfeabe726f74972b21d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:43 +0900

    タスク 208: 更新 (404 -> 304)

--- 208/3_submission.py
+++ 208/3_submission.py
@@ -1,13 +1,8 @@
 def p(g):
- s=[v for r in g for v in r if v]
- k=min(s,key=s.count)
- I=range;h=len(g);w=len(g[0])
- i=[r for r in I(h)if k in g[r]]
- u,v=i[0],i[-1]
- j=[c for c in I(w)if any(g[r][c]==k for r in i)]
- l,r=j[0],j[-1];H=v-u+1;W=r-l+1
- for y in I(h-H+1):
-  for x in I(w-W+1):
-   if(x-l|y-u)and all(g[Y][X]<1 for Y in I(y+1,y+H-1)for X in I(x+1,x+W-1)):
-    for t in I(H):g[y+t][x:x+W]=g[u+t][l:l+W]
+ f=sum(g,[]);k=min({*f}-{0},key=f.count);R=range
+ a=f.index(k);b=441+~f[::-1].index(k);d=b-a;H=d//21+1;W=d%21+1;u,l=divmod(a,21)
+ for y in R(22-H):
+  for x in R(22-W):
+   if(x-l|y-u)and not sum(sum(r[x+1:x+W-1])for r in g[y+1:y+H-1]):
+    for t in R(H):g[y+t][x:x+W]=g[u+t][l:l+W]
     return g
