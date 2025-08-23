5f85071344d7d205106e81cad9b314ae1b87a5b5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 05:58:30 +0900

    タスク 062: 更新 (240 -> 233)

--- 062/3_submission.py
+++ 062/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
- f=sum(g,[]);k=({*f}-{0,2}).pop();x,y=divmod(f.index(2),10)
+ f=sum(g,[]);k=sum({*f})-2;x,y=divmod(f.index(2),10)
  b=k in g[x];a=(x,y)[b]+(g[x-1+b][y-b]!=k);g=[[3]*10 for _ in g];n=100
- while n:
-  n-=1;i,j=divmod(n,10)
+ while n:=n-1:
+  i,j=divmod(n,10)
   if f[n]==k:g[i][j]=g[(2*a+~i,i)[b]][(j,2*a+~j)[b]]=k
  return g
