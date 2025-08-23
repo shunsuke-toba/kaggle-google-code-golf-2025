e610ba88977891b98d41a217cc4e59e5fdc3c569
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:22 +0900

    タスク 333: 更新 (229 -> 221)

--- 333/3_submission.py
+++ 333/3_submission.py
@@ -1,4 +1,2 @@
 def p(g):
- n=len(g);r,c=divmod(sum(g,[]).index(3),n);s=lambda i,j,I,J:n>(i:=i+I)>-1<(j:=j+J)<n and(g[i][j]or g[i].__setitem__(j,s(i,j,I,J))or g[i][j])
- for _ in 0,1,2,3:a=_>1;b=_&1;s(r+a,c+b,2*a-1,0);s(r+a,c+b,0,2*b-1)
- return g
+ r,c=divmod(sum(g,[]).index(3),10);s=lambda i,j,I,J:10>(i:=i+I)>-1<(j:=j+J)<10 and(g[i][j]or g[i].__setitem__(j,s(i,j,I,J))or g[i][j]);[(s(r+a,c+b,2*a-1,0),s(r+a,c+b,0,2*b-1))for a in(0,1)for b in(0,1)];return g
