b4df2c70ae7839d1dc2f2881ab1b265b14207ead
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:16 +0900

    タスク 105: 更新 (418 -> 369)

--- 105/3_submission.py
+++ 105/3_submission.py
@@ -1,13 +1,6 @@
 def p(g):
- E=enumerate;R=range;Y,X=zip(*[(i,j)for i,r in E(g)for j,v in E(r)if v]);a,b=min(Y),max(Y);c,d=min(X),max(X);I=R(a,b+1);J=R(c,d+1)
+ E=enumerate;R=range;P=[(i,j)for i,r in E(g)for j,v in E(r)if v];Y,X=zip(*P);a,b=min(Y),max(Y);c,d=min(X),max(X);I=R(a,b+1);J=R(c,d+1);S={i for i in R(a+1,b)if 1<sum(c<j<d for y,j in P if y==i)};T={j for j in R(c+1,d)if 1<sum(a<i<b for i,x in P if x==j)}
  for i in I:
-  for j in c,d:g[i][j]=2-g[i][j]%2
- for j in J:
-  for i in a,b:g[i][j]=2-g[i][j]%2
- for i in R(a+1,b):
-  if 1<sum(g[i][c+1:d]):
-   for j in J:g[i][j]=2-g[i][j]%2
- for j in R(c+1,d):
-  if 1<sum(r[j]&1 for r in g[a+1:b]):
-   for i in I:g[i][j]=2-g[i][j]%2
+  for j in J:
+   if i in(a,b)or j in(c,d)or i in S or j in T:g[i][j]=g[i][j]or 2
  return g
