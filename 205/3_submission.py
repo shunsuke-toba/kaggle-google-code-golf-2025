f8490633301a3d53530cc68a27b6918485dffa00
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:43 +0900

    タスク 205: 更新 (442 -> 406)

--- 205/3_submission.py
+++ 205/3_submission.py
@@ -1,15 +1,15 @@
 def p(g):
- R=range;E=enumerate;h=len(g);w=len(g[0]);m=0
+ R=range;h=len(g);w=len(g[0]);m=0
  for a in R(h):
   for b in R(a+1,h+1):
    for c in R(w):
     v=g[a][c]
     for d in R(c+1,w+1):
-     if(t:=(b-a)*(d-c))>m and all(g[a][x]==v==g[b-1][x]for x in R(c,d))and all(g[y][c]==v==g[y][d-1]for y in R(a,b)):m=t;A,B,C,D,V=a,b,c,d,v
- o=[r[C:D]for r in g[A:B]];g=[r[:]for r in o];c=V
- for i,r in E(o):
-  for j,v in E(r):
-   if v-c:
-    for k in g:k[j]=v
-    g[i]=[v]*(D-C)
+     t=(b-a)*(d-c)
+     if t>m and sum(g[y][x]!=v for y in R(a,b) for x in R(c,d))<4:m=t;A,B,C,D,V=a,b,c,d,v
+ g=[r[C:D]for r in g[A:B]]
+ P=[(y,x,v)for y,r in enumerate(g)for x,v in enumerate(r)if v!=V]
+ for y,x,v in P:
+  for r in g:r[x]=v
+  g[y]=[v]*(D-C)
  return g
