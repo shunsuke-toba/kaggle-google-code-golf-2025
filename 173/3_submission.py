4b6d370275e40f1dd688b5dcd0fc47ff78dca581
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:19 +0900

    タスク 173: 更新 (414 -> 403)

--- 173/3_submission.py
+++ 173/3_submission.py
@@ -1,9 +1,9 @@
 def p(g):
  w=len(g[0]);W=w+2;G=[0]*W+sum(([0,*r,0]for r in g),[])+[0]*W;P=[W+1+i+i//w*2 for i in range(w*len(g))]
- T=[(d,a,b)for j in P if(b:=G[j])for d in((1,-1,W,-W),(W+1,W-1,-W+1,-W-1),(1,-1),(W,-W))if(a:=G[j+d[0]])and a-b and all(G[j+x]==a for x in d[1:])]
+ T=[(d,a,b)for j in P if(b:=G[j])for d in((1,-1,W,-W),(W+1,W-1,-W+1,-W-1),(1,-1),(W,-W))if(a:=G[j+d[0]])*(a-b)and all(G[j+x]==a for x in d[1:])]
  for j in P:
   for d,a,b in T:
    if G[j]==b:
-    for x in d:G[j+x]=G[j+x]or a
+    for x in d:G[j+x]=a
    elif all(G[j+x]==a for x in d):G[j]=b
  return[G[j:j+w]for j in P[::w]]
