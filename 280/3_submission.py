2982d1c2be66ca104b1c5ae4ec99d61a41b8f118
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:46 +0900

    タスク 280: 更新 (323 -> 307)

--- 280/3_submission.py
+++ 280/3_submission.py
@@ -1,11 +1,11 @@
 def p(g):
- H=len(g);W=len(g[0])
- for y,x in[(y,x)for y in range(H)for x in range(W)if g[y][x]==2]:
+ n=len(g)
+ for y,x in[divmod(i,n)for i in range(n*n)if g[i//n][i%n]==2]:
   for a,b in(1,0),(0,1),(-1,0),(0,-1):
    if g[y+a][x+b]<1:break
   Y=y-a;X=x-b;k=0
-  while H>Y>-1<X<W>2<g[Y][X]:k+=1;Y-=a;X-=b
-  while H>y>-1<x<W:
+  while n>Y>-1<X<n>2<g[Y][X]:k+=1;Y-=a;X-=b
+  while n>y>-1<x<n:
    for t in range(-k,k+1):g[y+b*t][x+a*t]=3
    g[y][x]=2;y+=a;x+=b
  return g
