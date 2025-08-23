bfea09781cb20bb4bcf54df115a2345ebe982223
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 00:23:57 +0900

    タスク 285: 更新 (631 -> 555)

--- 285/3_submission.py
+++ 285/3_submission.py
@@ -8,10 +8,9 @@ def p(g):
      if n>(Y:=y+a)>-1<(X:=x+b)<n and(c:=o[Y][X])>0:o[Y][X]=-c;q+=(Y,X),;A+=c,
    m=max(A,key=A.count);s=[*zip(q,A)]
    for(y,x),v in s:
-    if m==v*any(n>y+a>-1<x+b<n and 0<abs(o[y+a][x+b])!=m for a,b in d if a*b==0):
+    if m==v*any(-m!=o[y+a][x+b]!=0 for a,b in d if a*b==0):
      p=[(r-y,c-x)for(r,c),v in s if v==m];break
    for a,b in d:
-    if n>(Y:=y+a)>-1<(X:=x+b)<n and 0<(c:=abs(o[Y][X]))!=m:
-     for u,v in p:
-      if n>(V:=X+(v,-v)[b&1])>-1<(U:=Y+(u,-u)[a&1])<n:g[U][V]=c
+    if 0>(c:=o[y+a][x+b])!=-m:
+     for u,v in p:g[y+a+(u,-u)[a&1]][x+b+(v,-v)[b&1]]=-c
  return g
