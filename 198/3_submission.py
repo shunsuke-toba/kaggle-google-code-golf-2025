bfb1413a4a72529c03f9fa1003db68ff7c93f148
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:20 +0900

    タスク 198: 更新 (316 -> 293)

--- 198/3_submission.py
+++ 198/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  h=len(g);w=len(g[0]);m=max(sum(g,[]))
- q=[(y,x)for y in range(h)for x in range(w)if g[y][x]<1<(g[y].count(m)*2>w)+(sum(r[x]==m for r in g)*2>h)+1]
+ q=[(y,x)for y in range(h)for x in range(w)if g[y][x]<1and(g[y].count(m)*2>w or sum(r[x]==m for r in g)*2>h)]
  for y,x in q:
-  for Y,X in (y,x),(y+1,x),(y-1,x),(y,x+1),(y,x-1):
-   if h>Y>=0<=X<w and g[Y][X]<1:g[Y][X]=4;q+=(Y,X),
+  if h>y>=0<=x<w and g[y][x]<1:
+   g[y][x]=4;q+=(y+1,x),(y-1,x),(y,x+1),(y,x-1)
  return[[c or 3 for c in r]for r in g]
