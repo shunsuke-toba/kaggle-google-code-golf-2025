26b1e3ccecc41d6c8c976a70e66138bb2e51fc9d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:45 +0900

    タスク 240: 更新 (229 -> 225)

--- 240/3_submission.py
+++ 240/3_submission.py
@@ -1,11 +1,11 @@
 def p(g):
- c=[0]*10
+ c=[0]*10;R=range(19)
  for d in range(9):
   for r in g:
    for v in r[d],r[~d]:
     c[v]+=v>0
     if c[v]>1:r[d:19-d:2]=[v]*(10-d)
- for i in range(19):
-  for j in range(19):
+ for i in R:
+  for j in R:
    if v:=g[i][j]:g[j][~i]=g[~i][~j]=g[~j][i]=v
  return g
