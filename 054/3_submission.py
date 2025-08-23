82b16bfebdd80a2e587dd213478c5ef8720b86b2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:45 +0900

    タスク 054: 更新 (563 -> 552)

--- 054/3_submission.py
+++ 054/3_submission.py
@@ -1,18 +1,18 @@
 def p(g,R=range):
- s=sum(g,[]);b=s[0];a=max({*s}-{b},key=s.count);D=1,0,-1,0
- for z in R(676):
-  i,j=z//26,z%26;p=[r[j:j+5]for r in g[i:i+5]]
-  if{*sum(p,[])}-{b,a,p[2][2]}and p==p[::-1]==[r[::-1]for r in p]:
+ s=sum(g,[]);b=s[0];a=max({*s}-{b},key=s.count);D=1,0,-1,0;T=R(676)
+ for z in T:
+  i,j=z//26,z%26;q=sum(p:=[r[j:j+5]for r in g[i:i+5]],[])
+  if{*q}-{b,a,q[12]}and p==p[::-1]==[r[::-1]for r in p]:
    for r in g[i:i+5]:r[j:j+5]=[b]*5
-   break
- for z in R(676):
+   p=q;break
+ for z in T:
   i,j=z//26+2,z%26+2
   if g[i-1][j]==g[i][j-1]==a!=g[i][j]:
    for t in R(25):
-    k,l=divmod(t,5);c=p[k][l]
-    if g[i+k-2][j+l-2]!=b!=c!=a:g[i+k-2][j+l-2]=c
+    c=p[t];x=i+t//5-2;y=j+t%5-2
+    if g[x][y]!=b!=c!=a:g[x][y]=c
    for d in R(4):
     x,y=i+D[d]*2,j+D[d-3]*2;c=g[x][y]
     if a!=c!=b:
-     while g[x][y]!=b:g[x][y]=c;x+=D[d];y+=D[d-3]
+     while g[x][y]-b:g[x][y]=c;x+=D[d];y+=D[d-3]
  return g
