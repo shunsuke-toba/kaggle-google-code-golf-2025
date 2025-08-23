a49221f5012847c3f363e6e0e3ef51c98a9ec617
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:33 +0900

    タスク 158: 更新 (440 -> 431)

--- 158/3_submission.py
+++ 158/3_submission.py
@@ -1,14 +1,13 @@
 def p(g,R=range):
- for z in R(999):
-  x,y=divmod(z,len(g[0])-2)
-  if len({*(l:=sum(p:=[r[x:x+3]for r in g[y:y+3]],[]))})>3:break
+ for t in R(999):
+  x,y=divmod(t,len(g[0])-2);l=sum(p:=[r[x:x+3]for r in g[y:y+3]],[])
+  if len({*l})>3:break
  for s in 3,2,1:
-  z=3*s
-  r=[[p[i//s][j//s]for j in R(z)]for i in R(z)]
+  z=3*s;u=R(z);r=[[p[i//s][j//s]for j in u]for i in u]
   for _ in R(4):
-   for y in R(len(g)-z+1):
-    for x in R(len(g[0])-z+1):
-     if all((g[-1][0],r[i][j])[l.count(r[i][j])==1]==g[y+i][x+j]for i in R(z)for j in R(z)):
-      for i in R(z):g[y+i][x:x+z]=r[i]
+   for y in R(len(g)+1-z):
+    for x in R(len(g[0])+1-z):
+     if all((g[-1][0],r[i][j])[l.count(r[i][j])==1]==g[y+i][x+j]for i in u for j in u):
+      for i in u:g[y+i][x:x+z]=r[i]
    r=[*zip(*r[::-1])]
  return g
