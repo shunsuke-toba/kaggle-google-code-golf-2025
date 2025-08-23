3bdf4d621b91f8113b5cba787462bc8dc29e11eb
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:42 +0900

    タスク 182: 更新 (303 -> 297)

--- 182/3_submission.py
+++ 182/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
- R=range;r=R(5);n=len(g)-6
- t=next([(i,j)for i in r for j in r if(v:=g[y+i+1][x+j+1])>1 and(c:=v)]for y in R(n)for x in R(n)if g[y][x]&g[y][x+1]&g[y+1][x]>4)
- for y in R(n+2):
-  for x in R(n+2):
-   if [(i,j)for i in r for j in r if g[y+i][x+j]==1]==t:
+ R=range;r=R(5);n=len(g)-6;m=R(n+2)
+ t=next({(i,j)for i in r for j in r if(v:=g[y+i+1][x+j+1])>1and(c:=v)}for y in R(n)for x in R(n)if g[y][x]&g[y][x+1]&g[y+1][x]>4)
+ for y in m:
+  for x in m:
+   if{(i,j)for i in r for j in r if g[y+i][x+j]}==t:
     for i,j in t:g[y+i][x+j]=c
  return g
