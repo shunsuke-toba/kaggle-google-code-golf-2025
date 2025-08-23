5419f0b1064be44af8e5cc347398ea9149bfa02a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:13 +0900

    タスク 013: 更新 (282 -> 275)

--- 013/3_submission.py
+++ 013/3_submission.py
@@ -3,7 +3,10 @@ def p(g,R=range):
  if{x,X}<={0,h-1}:
   if y>Y:y,Y,a,b=Y,y,b,a
   for c in R(y,w,s:=Y-y):
-   for r in g:r[c]=(a,b)[(c-y)//s&1]
+   for r in g:r[c]=a
+   a,b=b,a
  else:
-  for r in R(x,h,s:=X-x):g[r]=[(a,b)[(r-x)//s&1]]*w
+  for r in R(x,h,s:=X-x):
+   g[r]=[a]*w
+   a,b=b,a
  return g
