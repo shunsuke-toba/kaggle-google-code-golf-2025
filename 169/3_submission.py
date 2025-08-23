571a8c87c169d3918e38c9bfcc0aed2a6a6a3ea6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:41 +0900

    タスク 169: 更新 (255 -> 214)

--- 169/3_submission.py
+++ 169/3_submission.py
@@ -1,12 +1,11 @@
 def p(g):
- d=1,0,-1,0;k=99
+ def f(i,j,t=0):
+  if -1<i<10>j>-1<g[i][j]-5<4+t:
+   g[i][j]=t or 9
+   return 1+f(i+1,j,t)+f(i-1,j,t)+f(i,j+1,t)+f(i,j-1,t)
+  return 0
+ k=99
  while~k:
-  i=k//10;j=k%10;k-=1
-  if g[i][j]>4:
-   q=[(i,j)];g[i][j]=0
-   for i,j in q:
-    for n in 0,1,2,3:
-     x,y=i+d[n],j+d[n-1]
-     if-1<x<10>y>-1<g[x][y]>4:g[x][y]=0;q+=[(x,y)]
-   for i,j in q:g[i][j]=5-len(q)
+  i,j=k//10,k%10;k-=1
+  t=5-f(i,j);f(i,j,t)
  return g
