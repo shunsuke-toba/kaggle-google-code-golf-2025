6dcdde4ec1dc6a12b11f2dc4bdbb289220f02299
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 22:10:57 +0900

    タスク 349: 更新 (387 -> 379)

--- 349/3_submission.py
+++ 349/3_submission.py
@@ -1,12 +1,12 @@
 def p(g,R=range):
- n=len(g);s=[r[:]for r in g]
+ n=len(g);s=[*map(list,g)]
  for i in R(n):
   for j in R(n):
    if 8<s[i][j]and(i<1 or s[i-1][j]-9)and(j<1 or s[i][j-1]-9):
-    a=c=0
+    a=0;b=i
     while j+a<n>s[i][j+a]>8:a+=1
-    while i+c<n>s[i+c][j]>8:c+=1
+    while b<n>s[b][j]>8:b+=1
     r=a//2;L=max(j-r,0);E=min(j+a+r,n)
-    for y in g[i+c:]:y[j:j+a]=[1]*a
-    for y in R(max(i-r,0),min(i+c+r,n)):g[y][L:E]=[9-6*(s[y][x]<9)for x in R(L,E)]
+    for y in g[b:]:y[j:j+a]=[1]*a
+    for y in R(max(i-r,0),min(b+r,n)):g[y][L:E]=[9-6*(s[y][x]<9)for x in R(L,E)]
  return g
