9ed1ad8071b7d3251f902a5e921c5eb41c01f5ba
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 00:23:54 +0900

    タスク 201: 更新 (361 -> 344)

--- 201/3_submission.py
+++ 201/3_submission.py
@@ -1,9 +1,4 @@
 def p(g):
- W=len(g);f=sum(g,[])
- (a,b),*_,(c,d)=[divmod(i,W)for i,v in enumerate(f)if v==4]
- s=[(u,v)for i,w in enumerate(f)if w*(w-4)and min((u:=i//W)-a,c-u,(v:=i%W)-b,d-v)<0]
- S,J=map(min,zip(*s))
- o=[r[b:d+1]for r in g[a:c+1]]
- f=next(g[i][J]for i,j in s if j==J)==g[a+1][d]
- for k in range(~a+c):r=g[S+k][J:J+~b+d];o[k+1][1:-1]=r[::-1]if f else r
+ W=len(g);f=sum(g,[]);(a,b),*_,(c,d)=[divmod(i,W)for i,v in enumerate(f)if v==4];s=[(u,v)for i,w in enumerate(f)if w*(w-4)and min((u:=i//W)-a,c-u,(v:=i%W)-b,d-v)<0];S,J=map(min,zip(*s));o=[r[b:d+1]for r in g[a:c+1]];f=next(g[i][J]==g[a+1][d]for i,j in s if j==J)
+ for k in range(~a+c):o[k+1][1:-1]=g[S+k][J:J+~b+d][::1-2*f]
  return o
