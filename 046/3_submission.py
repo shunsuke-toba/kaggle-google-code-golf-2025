3bbebd4d2d6bd2b898fe6fc34e82c7df04d1a812
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:29 +0900

    タスク 046: 更新 (314 -> 310)

--- 046/3_submission.py
+++ 046/3_submission.py
@@ -1,13 +1,10 @@
 f=lambda x:5 in x and x.index(5)
 def p(g):
- c=[*filter(any,zip(*g))]
- r=[[0]*len(c)for _ in g]
- o=e=i=0
+ c=[*filter(any,zip(*g))];r=[[0]*len(c)for _ in g];o=e=i=0
  while c:
   j=1
   while j<len(c)and 5 not in c[j]:j+=1
-  s,c=c[:j+1],c[j+1:];d=max({*sum(s,())}-{0,5})
-  o+=f(s[0])-e;e=f(s[-1])
+  s,c=c[:j+1],c[j+1:];d=max({*sum(s,())}-{0,5});o+=f(s[0])-e;e=f(s[-1])
   for t in s:
    for y,v in enumerate(t):
     if v:r[y-o][i]=d
