3274e4d4e4accda970020178fdbcea26243c0b7d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 22:10:51 +0900

    タスク 089: 更新 (419 -> 415)

--- 089/3_submission.py
+++ 089/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  n=len(g);r=range(n);R=range(-2,3)
- for k,m in(2,-1),(3,1):
-  a=[(i,j)for i in r for j in r if g[i][j]==k];B=s=c=()
+ for k in 2,3:
+  a=[(i,j)for i in r for j in r if g[i][j]==k];m=k*2-5;B=s=()
   for i,j in a:
    t=[(x,y,g[i+x][j+y])for x in R for y in R if n>i+x>=0<=j+y<n and 0<(v:=g[i+x][j+y])!=k]
    if len(t)>len(B):
