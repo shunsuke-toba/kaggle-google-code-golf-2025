c6d48999b0f90fed749bfab34c48a58179995fad
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 05:58:25 +0900

    タスク 004: 更新 (171 -> 170)

--- 004/3_submission.py
+++ 004/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,e=enumerate):
- for v in {*sum(g,[])}-{0}:
+ for v in{*sum(g,[])}-{0}:
   k=[(r,c)for r,R in e(g)for c,b in e(R)if b==v][::-1];r,c=k[0]
   for R,C in k:g[R][C]=0;g[R][C+(R<r)*(C<c)]=v
  return g
