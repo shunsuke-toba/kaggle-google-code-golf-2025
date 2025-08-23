8ec8dbc1a78e1ce03f600d7ab49576a1034c9ad9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 00:23:56 +0900

    タスク 234: 更新 (349 -> 345)

--- 234/3_submission.py
+++ 234/3_submission.py
@@ -1,9 +1,10 @@
 def p(g):
- c=n=k=0;d=1,0,-1,0,1;r=range;H=len(g);R=r(len(g[0]))
- for i in r(H):
-  for j in R:
-   if(m:=g[i][j])and(g[i-1][j]|g[i+1][j])*(g[i][j-1]|g[i][j+1])<1:c+=1;n=m;g[i][j]=0;[k:=t for t in r(4)if(v:=g[i+d[t]][j+d[t+1]])*(v-n)]
- for i in r(H)[::k>0 or-1]:
-  for j in R[::k<3 or-1]:
-   if g[i][j]==n:g[i+d[k]*c][j+d[k+1]*c]=n;g[i][j]=0
- return g
+	c=n=k=0;d=1,0,-1,0,1;r=range;H=len(g);R=r(len(g[0]))
+	for i in r(H):
+		a=g[i]
+		for j in R:
+			if a[j]and(g[i-1][j]|g[i+1][j])*(a[j-1]|a[j+1])<1:c+=1;n=a[j];a[j]=0;[k:=t for t in r(4)if(v:=g[i+d[t]][j+d[t+1]])*(v-n)]
+	for i in r(H)[::k>0 or-1]:
+		for j in R[::k<3 or-1]:
+			if g[i][j]==n:g[i+d[k]*c][j+d[k+1]*c]=n;g[i][j]=0
+	return g
