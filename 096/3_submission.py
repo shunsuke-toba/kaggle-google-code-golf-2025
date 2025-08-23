df4fd3bd0bb3f4a79afea091ef60a91b08430638
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:46 +0900

    タスク 096: 更新 (501 -> 497)

--- 096/3_submission.py
+++ 096/3_submission.py
@@ -9,13 +9,12 @@ def p(g):
   for w in r(2,9):
    for d in r(1,15):
     for _ in r(4):
-     if any(re.search(s*w+f"[^{s}]"*d+s,"".join(map(str,r)))for r in g):y=(2*w+d,x,w)
+     if any(re.search(s*w+f"[^{s}]"*d+s,"".join(map(str,R)))for R in g):y=2*w+d,x,w
      g=t(g)
-  L+=y or ((1,x,1),(3,x,3))[f.count(x)>1],
- L.sort();z=L[-1][0]
- o=[[b]*z for _ in r(z)]
+  L+=y or((1,x,1),(3,x,3))[f.count(x)>1],
+ L.sort();z=L[-1][0];o=[[b]*z for _ in r(z)]
  for i in r(len(L)):
-  _,x,w=L[~i]
+  z,x,w=L[~i]
   for _ in r(4):
    for k in r(w):o[i][i+k]=o[i+k][i]=x
    o=t(o)
