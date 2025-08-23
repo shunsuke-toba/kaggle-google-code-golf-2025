a1b1535ff979671a538e5f70877207f830da709f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:07:00 +0900

    タスク 013: 更新 (260 -> 233)

--- 013/3_submission.py
+++ 013/3_submission.py
@@ -1,8 +1,6 @@
-def p(g,R=range):
- h=len(g);w=len(g[0]);(x,y,a),(X,Y,b)=[(i,j,v)for i in R(h)for j in R(w)if(v:=g[i][j])]
- if{x,X}<={0,h-1}:
-  if y>Y:y,Y,a,b=Y,y,b,a
-  for r in g:d=Y-y;r[y::d]=([a,b]*9)[:(w-y+d-1)//d]
- else:
-  for r in R(x,h,X-x):g[r]=[a]*w;a,b=b,a
+def p(g):
+ (y,a),(Y,b)=[(j,v)for r in g for j,v in enumerate(r)if v];w=len(g[0])
+ if{y,Y}<={0,w-1}:return[*map(list,zip(*p([*map(list,zip(*g))])))]
+ if y>Y:y,Y,a,b=Y,y,b,a
+ for r in g:d=Y-y;r[y::d]=([a,b]*9)[:(w-y+d-1)//d]
  return g
