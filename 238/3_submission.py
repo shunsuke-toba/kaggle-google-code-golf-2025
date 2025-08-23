9e2b267c0c99b9495051978bedac366c9e1dbcb5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 22:10:55 +0900

    タスク 238: 更新 (351 -> 335)

--- 238/3_submission.py
+++ 238/3_submission.py
@@ -1,10 +1,10 @@
 def p(g):
- a=c=A=C=99;b=-1;e=enumerate
- for i,r in e(g):
-  for j,v in e(r):
-   if v&7:a=min(a,i);b=max(b,i);c=min(c,j)
-   elif v:A=min(A,i);C=min(C,j)
- n=b-a-1;R=range(n);G=[r[c:c+n+2]for r in g[a:b+1]];k=G[0][1],G[1][-1],G[1][0],G[-1][1]
- for i in R:
-  for j in R:v=g[A+i][C+j];G[i+1][j+1]=v>7 and(i-j)*(n-i-j-1)and k[(i>j)*2+(i+j>=n)]or v
+ a=c=d=e=99;b=0;f=enumerate
+ for i,r in f(g):
+  for j,v in f(r):
+   if v&7:a=min(a,i);b=i;c=min(c,j)
+   elif v:d=min(d,i);e=min(e,j)
+ r=range(n:=b+~a);G=[x[c:c+n+2]for x in g[a:b+1]];k=G[0][1],G[1][-1],G[1][0],G[-1][1]
+ for i in r:
+  for j in r:v=g[d+i][e+j];G[i+1][j+1]=v*(i-j)*~(i+j-n)and k[(i>j)*2+(i+j>=n)]or v
  return G
