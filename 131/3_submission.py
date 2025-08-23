3c392721c15afa555c786598abacb6a1db355082
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:55 +0900

    タスク 131: 更新 (220 -> 219)

--- 131/3_submission.py
+++ 131/3_submission.py
@@ -1,6 +1,6 @@
 t=lambda a:[*map(list,zip(*a))]
 def p(a):
- h=len(a);w=len(a[0])
+ w=len(a[0]);h=len(a)
  if w>h:return t(p(t(a)))
- s=a.index;r=s([2]*w);g=[r for r in a if 3 in r]
- return s(g[0])<r and p(a[::-1])[::-1]or a[:r+1]+g+[[8]*w]+[[0]*w]*(h-r-len(g)-2)
+ r=a.index([2]*w);g=[r for r in a if 3 in r]
+ return g[0]in a[:r] and p(a[::-1])[::-1]or a[:r+1]+g+[[8]*w]+[[0]*w]*(h-r-len(g)-2)
