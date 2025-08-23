1e0483fda577606d13ecd8eb41623b4470ece52c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:59 +0900

    タスク 319: 更新 (463 -> 443)

--- 319/3_submission.py
+++ 319/3_submission.py
@@ -3,16 +3,14 @@ def p(g,R=range,L=len):
  C=sorted({*s},key=s.count)
  P=[]
  for c in C[:-1]:
-  h=[*g]
-  for _ in[0]*4:
-   while c not in h[-1]:h.pop()
+  h=g
+  for _ in R(4):
+   while c not in h[-1]:h=h[:-1]
    h=[*zip(*h[::-1])]
   P+=[[[(x==c)*c for x in r]for r in h]]
  for q in P:
   for r in P:
-   if q!=r:
-    a,b,c,d=L(q)*2,L(q[0])*2,L(r),L(r[0])
-    for i in R(a-c+1):
-     for j in R(b-d+1):
-      if all((q[i+x>>1][j+y>>1]>0)==(r[x][y]>0)for x in R(c)for y in R(d)):
-       return[[x or C[-1] for x in t]for t in q]
+   for i in R(L(q)*2-L(r)+1):
+    for j in R(L(q[0])*2-L(r[0])+1):
+     if q!=r and all((q[i+x>>1][j+y>>1]>0)==(r[x][y]>0)for x in R(L(r))for y in R(L(r[0]))):
+      return[[x or C[-1] for x in t]for t in q]
