43ddd8d26efa401909f3c741414107174afb0c22
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:53 +0900

    タスク 046: 更新 (297 -> 294)

--- 046/3_submission.py
+++ 046/3_submission.py
@@ -2,7 +2,7 @@ def p(g):
  c=[*filter(any,zip(*g))];r=[[0]*len(c)for _ in g];o=e=i=0
  while c:
   j=1
-  while c[j:j+1]and 5 not in c[j]:j+=1
+  while c[j:]and 5 not in c[j]:j+=1
   s,c=c[:j+1],c[j+1:];d=sum({*sum(s,())})-5;o+=(s[0]+(5,)).index(5)%3-e;e=(s[-1]+(5,)).index(5)%3
   for t in s:
    for y in 0,1,2:
