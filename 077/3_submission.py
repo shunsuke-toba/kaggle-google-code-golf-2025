03f70673371e3dbd2c9f22b8ede905c40898a5a5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:46 +0900

    タスク 077: 更新 (291 -> 290)

--- 077/3_submission.py
+++ 077/3_submission.py
@@ -3,5 +3,5 @@ def p(g):
  while s:
   t={s.pop()};a=c=99;b=d=0
   while t:y,x=t.pop();a=min(a,y);b=max(b,y);c=min(c,x);d=max(d,x);t|=s&{(y+i//5-2,x+i%5-2)for i in range(25)};s-=t
-  for r in g[a:b+1]:r[c:d+1]=[4-2*(x==2)for x in r[c:d+1]]
+  for r in g[a:b+1]:r[c:d+1]=[4>>(x==2)for x in r[c:d+1]]
  return g
