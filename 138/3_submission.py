07500abfa37fd4fd96a1376d716ca6bf41b08436
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:40 +0900

    タスク 138: 更新 (326 -> 322)

--- 138/3_submission.py
+++ 138/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
- f=lambda x:[i for i,s in enumerate(x)if min(s)];x,y=f(zip(*g));u,v=f(g);g=[r[x:y+1]for r in g[u:v+1]];h=len(g)-1;w=len(g[0])-1
- for i in range(1,h):
-  for j in range(1,w):
-   c=g[i][j];d=(c==g[-1][1])-(c==g[0][1]);e=(c==g[1][-1])-(c==g[1][0]);I,J=i,j
-   while(d|e)&(0<I+d<h)&(0<J+e<w):g[I:=I+d][J:=J+e]=c
- return g
+ f=lambda z:[i for i,s in enumerate(z)if min(s)];a,b=f(g);c,d=f(zip(*g));e,f=g[a][c+1],g[b][c+1];h,k=g[a+1][c],g[a+1][d]
+ for i in range(a+1,b):
+  for j in range(c+1,d):
+   x=g[i][j];y=(x==f)-(x==e);z=(x==k)-(x==h);I,J=i,j
+   while(y|z)&(a<I+y<b)&(c<J+z<d):I+=y;J+=z;g[I][J]=x
+ return[q[c:d+1]for q in g[a:b+1]]
