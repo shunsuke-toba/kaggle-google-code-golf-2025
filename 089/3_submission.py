138de470977eb47a6882fc7f41315c34cb37dffd
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 00:23:51 +0900

    タスク 089: 更新 (405 -> 402)

--- 089/3_submission.py
+++ 089/3_submission.py
@@ -1,11 +1,11 @@
 def p(g):
- n=len(g);r=range(n);R=range(-2,3)
+ n=len(g);R=range(-2,3)
  for k in 2,3:
-  a=[(i,j)for i in r for j in r if g[i][j]==k];m=k*2-5;B=s=()
+  a=[(i,j)for i in range(n) for j in range(n) if g[i][j]==k];B=s=()
   for i,j in a:
-   t=[(x,y,g[i+x][j+y])for x in R for y in R if n>i+x>-1<j+y<n and 0<(v:=g[i+x][j+y])!=k]
-   if len(t)>len(B):
-    V=[v for*_,v in t];c=max(V,key=V.count);B=[(x,y)for x,y,v in t if v==c];s=i,j
+   t=[(x,y,g[i+x][j+y])for x in R for y in R if 0<=i+x<n>j+y>=0 and 0<(v:=g[i+x][j+y])!=k]
+   if len(t)>len(B):c=sorted(v for*_,v in t)[len(t)//2];B=[(x,y)for x,y,v in t if v==c];s=i,j
+  m=k*2-5
   for i,j in a:
    if(i,j)!=s:
     for x,y in B:g[i+x][j+y*m]=c
