103210af64b1387d840c9ecc1f2ef150e9939a44
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:34 +0900

    タスク 173: 更新 (387 -> 344)

--- 173/3_submission.py
+++ 173/3_submission.py
@@ -1,10 +1,11 @@
 def p(g):
- w=len(g[0]);W=w+2;G=[0]*W+sum(([0,*r,0]for r in g),[])+[0]*W;P=[W+1+i+i//w*2 for i in range(w*len(g))]
- D=((1,-1),(W,-W),(W+1,-W-1),(W-1,-W+1))
- T=[(d,a,b)for j in P if(b:=G[j])for d in D if(a:=G[j+d[0]])*(a-b)and G[j+d[1]]==a]
+ h=len(g);w=len(g[0]);W=w+2
+ G=[0]*W+sum(([0,*r,0]for r in g),[])+[0]*W
+ P=range(W+1,W*(h+1)-1)
+ D=(1,W,W+1,W-1)
+ T=[(d,a,b)for j in P if(b:=G[j])for d in D if(a:=G[j+d])*(a-b)and G[j-d]==a]
  for j in P:
-  for d,a,b in T:
-   if G[j]==b:
-    for x in d:G[j+x]=a
-   elif G[j+d[0]]==a and G[j+d[1]]==a:G[j]=b
- return[G[j:j+w]for j in P[::w]]
+  for d,a,c in T:
+   if G[j]==c:G[j+d]=G[j-d]=a
+   elif G[j+d]==G[j-d]==a:G[j]=c
+ return[G[i:i+w]for i in range(W+1,W*(h+1)-1,W)]
