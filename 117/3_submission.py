2d563edf46f530e2bb501b0460b6dc16c603f45d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:39 +0900

    タスク 117: 更新 (288 -> 268)

--- 117/3_submission.py
+++ 117/3_submission.py
@@ -1,6 +1,8 @@
 def p(g):
  w=len(g[0]);f=sum(g,[])
- a,b=next((i,j)for i in range(1,len(g)-1)for j in range(1,w-1)if(v:=g[i][j])and f.count(v)<6 and v==g[i-1][j-1]==g[i-1][j+1]==g[i+1][j-1]==g[i+1][j+1])
+ n=len(f)
+ for k in range(w+1,n-w-1):
+  if (v:=f[k]) and f.count(v)<6 and v==f[k-w-1]==f[k-w+1]==f[k+w-1]==f[k+w+1]:a,b=divmod(k,w)
  for k,v in enumerate(f):
   if v:r,c=divmod(k,w);g[r][2*b-c]=g[2*a-r][c]=g[2*a-r][2*b-c]=v
  return g
