edd43771f933a6b48c6d901f774f44bae9f672a8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:33 +0900

    タスク 009: 更新 (240 -> 229)

--- 009/3_submission.py
+++ 009/3_submission.py
@@ -1,8 +1,9 @@
 def p(g):
  b=g[0][2]
- for c in range(18):
+ for _ in 0,1:
   for r in g:
-   t=[i for i,x in enumerate(r)if x==(d:=c//2+1)]
-   if len(t)==4:r[t[1]+1:t[-2]]=[(d+10,b)[x==b]for x in r[t[1]+1:t[-2]]]
+   for c in{*r}-{0,b}:
+    if r.count(c)>2:
+     q=r.index(c)+2;j=r.index(c,q);r[q:j]=[(c+10,b)[x==b]for x in r[q:j]]
   g=[*map(list,zip(*g))]
  return[[x%10for x in r]for r in g]
