93a3c6eae2f51d9ef97a3e9c62a3d235e05916c5
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-24 05:58:05 +0900

    367: xまたはyが-1のとき0除算を発生させる(250 -> 249)

--- 367/3_submission.py
+++ 367/3_submission.py
@@ -1,6 +1,7 @@
 def p(g):
  h,w=len(g),len(g[0]);d=-1,0,1,0
  for k in range(9**5):
-  r,c,s=k%97%h,k%89%w,k%4;x,y=d[s]+r,d[~s]+c;t=g[r][c]-5;g[(x-y+c)%h][(x-r+y)%w]|=([g[(r+d[s+~i])%h][(c+d[s-i])%w]for i in range(4)]==[5,t,0,5])*4
-  if h>x>t<y<w:g[x][y]|=4&t
+  r,c,s=k%97%h,k%89%w,k%4;x,y=d[s]+r,d[~s]+c;t=g[r][c]-5
+  try:g[x-y+c][x-r+y]|=([g[r+d[s+~i]-h][(c+d[s-i])%w]for i in range(4)]==[5,t,0/~(x|y),5])*4;g[x][y]|=t&4
+  except:0
  return g
