4543c1e6bacb9cfaf4dddc5a258d54ec70e5deb8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:45 +0900

    タスク 278: 更新 (242 -> 238)

--- 278/3_submission.py
+++ 278/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(R:=range,Y:=len(g),X:=len(g[0]),s:={(i,j)for i in R(Y)for j in R(X)if g[i][j]>1},[g[y].__setitem__(x,3)for i,j in s if {(i+1,j),(i-1,j),(i,j+1),(i,j-1)}&s for y in R(i-1,i+2)for x in R(j-1,j+2)if X>x>=0<=y<Y and g[y][x]<1],g)[-1]
+p=lambda g:(R:=range,X:=len(g[0]),s:={(i,j)for i in R(len(g))for j in R(X)if g[i][j]>1},[g[y].__setitem__(x,3)for i,j in s if {(i+1,j),(i-1,j),(i,j+1),(i,j-1)}&s for y in R(i-1,i+2)for x in R(j-1,j+2)if X>x>=0<=y<len(g)>g[y][x]<1],g)[-1]
