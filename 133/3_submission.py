c4f47fe2620e021dce05c75d630da532afb9df5b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:40 +0900

    タスク 133: 更新 (655 -> 644)

--- 133/3_submission.py
+++ 133/3_submission.py
@@ -7,15 +7,15 @@ def p(g):
    if h>Y>=0<=X<w and g[Y][X]:c+=f(Y,X)
   return c
  C=[f(y,x)for y in range(h)for x in range(w)if g[y][x]]
- B=[];K=0
+ B=[];P=0
  for c in C:
-  d=[v for _,_,v in c]
+  d=[v for *_,v in c]
   for k in{*d}:
-   if d.count(k)==1 and len(c)>len(B):B=c;K=k;p0=B[d.index(k)][:2]
- O=[(y-p0[0],x-p0[1])for y,x,v in B if v!=K]
+   if d.count(k)==1 and len(c)>len(B):B=c;P=k;y0,x0=B[d.index(k)][:2]
+ O=[(y-y0,x-x0)for y,x,v in B if v^P]
  for c in C:
-  if(A:=[(y,x)for y,x,v in c if v==K]):
-   Y,X=zip(*A);a=min(Y);b=min(X);Y=max(Y)+1-a;X=max(X)+1-b;L=next(v for y,x,v in c if v^K)
+  if(A:=[(y,x)for y,x,v in c if v==P]):
+   h,w=zip(*A);a=min(h);b=min(w);h=max(h)+1-a;w=max(w)+1-b;L=next(v for *_,v in c if v^P)
    for u,v in O:
-    for y in range(a+u*Y,a+u*Y+Y):o[y][b+v*X:b+v*X+X]=[L]*X
+    for i in range(h):o[a+u*h+i][b+v*w:b+v*w+w]=[L]*w
  return o
