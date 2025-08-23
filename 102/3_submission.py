7a54392553543f92845d211339be977d83954822
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 08:36:20 +0900

    タスク 102: 更新 (260 -> 254)

--- 102/3_submission.py
+++ 102/3_submission.py
@@ -1,8 +1,9 @@
 def p(g):
- n=len(g);R=range
- for s in R(2,n):
-  for y in R(n-s):
-   for x in R(n-s):
-    if all(g[y][x+i]==g[y+s][x+i]==g[y+i][x]==g[y+i][x+s]==5 for i in R(s+1))>max(max(m[x+1:x+s])for m in g[y+1:y+s]):
-     for m in g[y+1:y+s]:m[x+1:x+s]=[2]*(s-1)
+ r=range;n=len(g)
+ for s in r(2,n):
+  for y in r(n-s):
+   H=g[y+1:y+s]
+   for x in r(n-s):
+    if all(g[y][x+i]*g[y+s][x+i]*g[y+i][x]*g[y+i][x+s]>624 for i in r(s+1))>sum(sum(m[x+1:x+s])for m in H):
+     for m in H:m[x+1:x+s]=[2]*~-s
  return g
