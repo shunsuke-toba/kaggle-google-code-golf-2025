08c0a501932d9caead4a45e848a4de72724ca7c4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:28 +0900

    タスク 023: 更新 (312 -> 280)

--- 023/3_submission.py
+++ 023/3_submission.py
@@ -1,10 +1,10 @@
 def p(g):
- s={(i,j)for i,r in enumerate(g)for j,c in enumerate(r)if c==5}
+ s={(i,j)for i,r in enumerate(g)for j,c in enumerate(r)if c}
  def f(s):
   if not s:return 1
   r,c=min(s)
-  for *q,k in((0,0,1,0,2,0,2),(0,0,0,1,0,2,2),(0,0,1,0,0,1,1,1,8)):
-   q={(r+i,c+j)for i,j in zip(q[::2],q[1::2])}
+  for *q,k in((0,20,40,2),(0,1,2,2),(0,20,1,21,8)):
+   q={(r+d//20,c+d%20)for d in q}
    if q<=s and f(s-q):
     for r,c in q:g[r][c]=k
     return 1
