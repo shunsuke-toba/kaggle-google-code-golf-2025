0eadf3517344ae600fa24b8e35c763fa765b475d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 00:23:50 +0900

    タスク 044: 更新 (504 -> 487)

--- 044/3_submission.py
+++ 044/3_submission.py
@@ -4,18 +4,16 @@ def p(g):
   if c^5:
    q=[i];g[i]=5;f=c<1
    for n in q:
-    for o in 1,-1,10,-10:
-     m=n+o
-     if 100>m>-1 and 2>m%10-n%10>-2:
-      d=g[m]
-      if d==c:g[m]=5;q+=m,
+    for m in n+1,n-1,n+10,n-10:
+     if -1<m<100 and 2>m%10-n%10>-2:
+      if(d:=g[m])==c:g[m]=5;q+=m,
       elif f and d^5:f=0
      elif f:f=0
    s=tuple(j-i for j in q)
    if f and q[1:]:a[s]=i
-   elif c:b[c]=(c in b or q[1:]<=[])and[0]or(s,q,i)
- for c,v in b.items():
-  if v and v[0] in a:
+   elif c:b[c]=(c in b or len(q)<2)and[0]or(s,q,i)
+ for c in b:
+  if(v:=b[c])and v[0]in a:
    s,q,i=v
-   for n in q:r[n]=0;r[n-i+a[s]]=c
+   for n in q:r[n],r[n-i+a[s]]=0,c
  return[r[i*10:][:10]for i in range(10)]
