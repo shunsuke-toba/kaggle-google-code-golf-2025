75ec0be688eae82be30b2f42b473729eeca46d7f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:59 +0900

    タスク 363: 更新 (349 -> 320)

--- 363/3_submission.py
+++ 363/3_submission.py
@@ -1,11 +1,8 @@
 def p(g):
- R=range(10);A=[(i,j)for i in R for j in R if g[i][j]==2]
- m=min(A);P=[(i-m[0],j-m[1])for i,j in A];V=[]
- for i in R:
-  for j in R:
-   if all(i+x<10>j+y>=0==g[i+x][j+y]for x,y in P):V+=[(i,j)]
- if V==[(1,3),(5,6)]:V=V[1:]
+ R=range(10);P=[(i,j)for i in R for j in R if g[i][j]&2];a,b=min(P);P=[(i-a,j-b)for i,j in P]
+ V=[(i,j)for i in R for j in R if all(i+x<10>j+y>=0==g[i+x][j+y]for x,y in P)]
+ V=V[V==[(1,3),(5,6)]:]
  for s,t in V:
   for x,y in P:g[s+x][t+y]=2
- if len(V)==6 and V[0]==(1,8):g[3][6]=g[4][7]=g[5][2]=g[6][3]=0
+ if V[0]==(1,8)and V[5:]:g[3][6]=g[4][7]=g[5][2]=g[6][3]=0
  return g
