3059592e902e4ed51cc00a86f0c0e63f1abe1332
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 00:23:52 +0900

    タスク 133: 更新 (567 -> 557)

--- 133/3_submission.py
+++ 133/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- l=len;r=range;h=l(g);w=l(g[0]);o=[*map(list,g)]
+ l=len;r=range;h=l(g);w=l(g[0])
  def f(y,x):
   c=[(y,x,g[y][x])];g[y][x]=0
   for Y,X in(1,0),(-1,0),(0,1),(0,-1):
@@ -8,9 +8,9 @@ def p(g):
   return c
  C=[f(y,x)for y in r(h)for x in r(w)if g[y][x]]
  B=min((c for c in C if l(c)>2),key=l)
- d=[*zip(*B)][2];p=min(d,key=d.count);a,b=B[d.index(p)][:2];B=[(y-a,x-b)for y,x,v in B if v^p]
+ d=[*zip(*B)][2];p=min(d,key=d.count);a,b=B[d.index(p)][:2];B=[(y-a,x-b,v)for y,x,v in B]
  for c in C:
   A=[(y,x)for y,x,v in c if v==p];a,b=min(A);m=int(l(A)**.5);L=next(v for *_,v in c if v^p)
-  for u,v in B:
-   for i in r(m):o[a+u*m+i][b+v*m:b+v*m+m]=[L]*m
- return o
+  for u,v,t in B:
+   for i in r(m):g[a+u*m+i][b+v*m:b+v*m+m]=[(p,L)[t!=p]]*m
+ return g
