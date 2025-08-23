2dda8a4d88db2a1aec274731002d1148b2cc005b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 00:23:52 +0900

    タスク 131: 更新 (222 -> 220)

--- 131/3_submission.py
+++ 131/3_submission.py
@@ -3,4 +3,4 @@ def p(a):
  h=len(a);w=len(a[0])
  if w>h:return t(p(t(a)))
  s=a.index;r=s([2]*w);g=[r for r in a if 3 in r]
- return p(a[::-1])[::-1] if s(g[0])<r else a[:r+1]+g+[[8]*w]+[[0]*w]*(h-r-len(g)-2)
+ return s(g[0])<r and p(a[::-1])[::-1]or a[:r+1]+g+[[8]*w]+[[0]*w]*(h-r-len(g)-2)
