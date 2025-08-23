c45890a5a0dbdff9771c7c3a913cd48f6654629b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:43 +0900

    タスク 009: 更新 (229 -> 224)

--- 009/3_submission.py
+++ 009/3_submission.py
@@ -3,7 +3,7 @@ def p(g):
  for _ in 0,1:
   for r in g:
    for c in{*r}-{0,b}:
-    if r.count(c)>2:
-     q=r.index(c)+2;j=r.index(c,q);r[q:j]=[(c+10,b)[x==b]for x in r[q:j]]
+    try:i=r.index(c)+2;j=r.index(c,i);r[i:j]=[(c+10,b)[x==b]for x in r[i:j]]
+    except:0
   g=[*map(list,zip(*g))]
  return[[x%10for x in r]for r in g]
