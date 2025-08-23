0000ba9fb5faa5b9ffb9390839672cee7afff854
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:16 +0900

    タスク 101: 更新 (666 -> 646)

--- 101/3_submission.py
+++ 101/3_submission.py
@@ -1,11 +1,11 @@
 def p(g):
- h=len(g);w=len(g[0]);R=range;F=filter;p=0
+ h=len(g);w=len(g[0]);R=range;p=0
  for r in R(h):
   for c in R(w):
    if p:break
    for s in R(r+1,h+1):
     for t in R(c+1,w+1):
-     if (x:=[g[i][c:t]for i in R(r,s)])==[*map(list,zip(*F(any,zip(*[*F(any,x)]))))]and len({*sum(x,[]),0})>2:p=x;u={(i,j)for i in R(r,s)for j in R(c,t)}
+     if all(map(any,x:=[g[i][c:t]for i in R(r,s)]))and all(map(any,zip(*x)))and{1,2}<={*sum(x,[])}:p=x;u={(i,j)for i in R(r,s)for j in R(c,t)}
  for s in 3,2,1:
   r=[[p[i//s][j//s]for j in R(len(p[0])*s)]for i in R(len(p)*s)]
   H=len(r);W=len(r[0])
