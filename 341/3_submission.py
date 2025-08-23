5ee3a30ff7f1a20927d07d34eacbedfe368509f7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:52 +0900

    タスク 341: 更新 (306 -> 300)

--- 341/3_submission.py
+++ 341/3_submission.py
@@ -1,8 +1,6 @@
 def p(g):
  s=sum(g,[]);A,B=sorted(((i:=s.index(k))//10,(j:=99-s[::-1].index(k))//10,i%10,j%10)for k in{*s}-{0})
  if A[1]<B[0]:x,y=A[1]+1,B[0];c=max(A[2],B[2])+1;d=min(A[3],B[3])
- else:
-  if A[2]>B[2]:A,B=B,A
-  x,y=max(A[0],B[0])+1,min(A[1],B[1]);c=A[3]+1;d=B[2]
+ else:x,y=max(A[0],B[0])+1,min(A[1],B[1]);c=min(A[3],B[3])+1;d=max(A[2],B[2])
  for r in g[x:y]:r[c:d]=[8]*(d-c)
  return g
