6e880d551a519aa9ffccf82a2da30a6a5a0b756f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:41 +0900

    タスク 154: 更新 (229 -> 226)

--- 154/3_submission.py
+++ 154/3_submission.py
@@ -1,11 +1,9 @@
 def p(g):
- h=eval(str(g))
- for k in range(225):
-  i=k//15;j=k%15
-  for y,x in((1,0),(-1,0),(0,1),(0,-1))*(g[i][j]>4):
+ for i,j in[(i,j)for k in range(225)if g[i:=k//15][j:=k%15]>4]:
+  for y,x in((1,0),(-1,0),(0,1),(0,-1)):
    d=1
    try:
     while g[i+y*d][j+x*d]^2:d+=1
-    h[i][j]=0;h[i+y*d*2][j+x*d*2]=5;break
+    g[i][j]=0;g[i+y*d*2][j+x*d*2]=5;break
    except:0
- return h
+ return g
