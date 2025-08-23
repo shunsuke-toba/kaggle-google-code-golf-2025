0a8ec35cf6821d0bac603e3ef37388e5e63a42d3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:35 +0900

    タスク 205: 更新 (369 -> 360)

--- 205/3_submission.py
+++ 205/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- R=range;h=len(g);w=len(g[0]);_,a,b,c,d,B=max((i*j,y,y+i,x,x+j,g[y][x])for i in R(6,11)for j in R(6,11)for y in R(h-i+1)for x in R(w-j+1)if sum(g[Y][X]!=g[y][x]for Y in R(y,y+i)for X in R(x,x+j))<4);g=[r[c:d]for r in g[a:b]];w=d-c
- for y,x,v in[(y,x,v)for y,r in enumerate(g)for x,v in enumerate(r)if v!=B]:[r.__setitem__(x,v)for r in g];g[y]=[v]*w
+ R=range;S=R(6,11);_,a,b,c,d=max((i*j,y,y+i,x,x+j)for i in S for j in S for y in R(len(g)-i+1)for x in R(len(g[0])-j+1)if sum(g[Y][X]!=g[y][x] for Y in R(y,y+i)for X in R(x,x+j))<4);g=[r[c:d]for r in g[a:b]];B=g[0][0]
+ for y,x,v in[(y,x,v)for y,r in enumerate(g)for x,v in enumerate(r)if v!=B]:[r.__setitem__(x,v)for r in g];g[y]=[v]*(d-c)
  return g
