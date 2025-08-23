33691d17697f9606e4a8b7d376545fcbebf9da9c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:22 +0900

    タスク 319: 更新 (522 -> 463)

--- 319/3_submission.py
+++ 319/3_submission.py
@@ -1,21 +1,18 @@
 def p(g,R=range,L=len):
- p=[]
  s=sum(g,[])
  C=sorted({*s},key=s.count)
+ P=[]
  for c in C[:-1]:
   h=[*g]
-  for _ in R(4):
+  for _ in[0]*4:
    while c not in h[-1]:h.pop()
    h=[*zip(*h[::-1])]
-  p+=[[[x*(x==c)for x in r]for r in h]]
- B=C[-1]
- for b in R(9):
-  q,r=p[b//3],p[b%3]
-  if q==r:continue
-  n,m=L(q)*2,L(q[0])*2
-  N,M=L(r),L(r[0])
-  s=[[q[i//2][j//2]for j in R(m)]for i in R(n)]
-  for i in R(n-N+1):
-   for j in R(m-M+1):
-    if all((s[i+x][j+y]>0)==(r[x][y]>0)for x in R(N)for y in R(M)):
-     return [[x+(x<1)*B for x in r]for r in q]
+  P+=[[[(x==c)*c for x in r]for r in h]]
+ for q in P:
+  for r in P:
+   if q!=r:
+    a,b,c,d=L(q)*2,L(q[0])*2,L(r),L(r[0])
+    for i in R(a-c+1):
+     for j in R(b-d+1):
+      if all((q[i+x>>1][j+y>>1]>0)==(r[x][y]>0)for x in R(c)for y in R(d)):
+       return[[x or C[-1] for x in t]for t in q]
