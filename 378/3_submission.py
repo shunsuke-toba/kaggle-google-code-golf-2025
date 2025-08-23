10e5333c4f31a82a891576df287ef19c7235e583
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:07:06 +0900

    タスク 378: 更新 (257 -> 248)

--- 378/3_submission.py
+++ 378/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
- n=len(g)-1
- for i in range(1,n):
-  for j in range(1,n):
-   if(c:=g[i][j])and(d:=(g[i+1][j]==c)-(g[i-1][j]==c))*(e:=(g[i][j+1]==c)-(g[i][j-1]==c))and g[i+d][j+e]^c:
-    y,x=i,j;k=g[i+2*d][j+2*e]
-    while 0<y<n>0<x<n:g[y:=y-d][x:=x-e]=k
+ n=len(g)-1;m=n-1
+ for y in range(m*m):
+  x=y%m+1;y=y//m+1
+  if(c:=g[y][x])and(d:=(g[y+1][x]==c)-(g[y-1][x]==c))*(e:=(g[y][x+1]==c)-(g[y][x-1]==c))and g[y+d][x+e]^c:
+   c=g[y+2*d][x+2*e]
+   while 0<y<n>0<x<n:g[y:=y-d][x:=x-e]=c
  return g
