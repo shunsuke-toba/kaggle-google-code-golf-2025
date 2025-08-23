8c601e313a9a846830e67956f47aff1963611407
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 01:56:54 +0900

    タスク 118: 更新 (465 -> 434)

--- 118/3_submission.py
+++ 118/3_submission.py
@@ -1,16 +1,14 @@
 def p(g):
- h=len(g);w=len(g[0]);u=set();f=lambda y,x:(g[y][x]==2)-(g[y][x]<1)*10-((y,x)in u)*99;m=5;R=range
+ h=len(g);w=len(g[0]);u=set();f=lambda y,x:((a:=g[y][x])==2)-2*(a<1 or (y,x)in u);R=range;t=2
  while 1:
-  B=S=I=J=0
-  for s in 7,5:
-   if m<=s:
-    r=s//2
-    for k in R(h*w):
-     i=k//w;j=k%w
-     b=sum(f(a,j)for a in R(i-r,i+r+1)if-1<a<h)+sum(f(i,c)for c in R(j-r,j+r+1)if-1<c<w)-f(i,j)
-     if(b,-s)>(B,-S):B,S,I,J=b,s,i,j
+  B=m=I=J=0
+  for r in(3,2)[::t-1]:
+   for k in R(h*w):
+    i=k//w;j=k%w
+    b=sum(f(a,j)for a in R(i-r,i+r+1)if-1<a<h)+sum(f(i,c)for c in R(j-r,j+r+1)if-1<c<w)-f(i,j)
+    if(b,-r)>(B,-m):B,m,I,J=b,r,i,j
   if B<1:return g
-  m=S;r=m//2
-  for d in R(-r,r+1):
-   for y,x in((I+d,J),(I,J+d)):
+  t=m
+  for d in R(-t,t+1):
+   for y,x in (I+d,J),(I,J+d):
     u|={(y,x)};g[y][x]+=3*(g[y][x]==5)
