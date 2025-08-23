8abb51289528e7cd02674284cc87ac34daf5a8ae
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:38 +0900

    タスク 096: 更新 (527 -> 501)

--- 096/3_submission.py
+++ 096/3_submission.py
@@ -1,23 +1,22 @@
 def p(g):
- from re import search as S
+ import re
  r=range
- f=sum(g,[]);b=max(s:=set(f),key=f.count)
+ f=sum(g,[]);b=max(f,key=f.count)
  t=lambda m:[*map(list,zip(*m[::-1]))]
  L=[]
- for x in s-{b}:
-  s=str(x);y=None
+ for x in{*f}-{b}:
+  s=str(x);y=()
   for w in r(2,9):
    for d in r(1,15):
     for _ in r(4):
-     if any(S(s*w+f"[^{s}]"*d+s,"".join(map(str,R)))for R in g):y=2*w+d,x,w
+     if any(re.search(s*w+f"[^{s}]"*d+s,"".join(map(str,r)))for r in g):y=(2*w+d,x,w)
      g=t(g)
-  L+=y or ((1,x,1)if f.count(x)==1 else(3,x,3)),
- L.sort()
- z=L[-1][0]
+  L+=y or ((1,x,1),(3,x,3))[f.count(x)>1],
+ L.sort();z=L[-1][0]
  o=[[b]*z for _ in r(z)]
- for i,(_,x,w) in enumerate(L[::-1]):
+ for i in r(len(L)):
+  _,x,w=L[~i]
   for _ in r(4):
    for k in r(w):o[i][i+k]=o[i+k][i]=x
    o=t(o)
  return o
-
