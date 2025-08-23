60bd12b6f645ac6d18db4b1282582043df6e081c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:52 +0900

    タスク 368: 更新 (268 -> 231)

--- 368/3_submission.py
+++ 368/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  r=range;a,b=zip(*[divmod(k,10)for k in r(100)if g[k//10][k%10]%5]);t=min(a);l=min(b);a=max(a)-t+1;b=max(b)-l+1
- for Y in r(11-a):
-  for X in r(11-b):
-   if{g[Y+i][X+j]for i in r(a)for j in r(b)}=={5}:
+ for Y in r(10):
+  for X in r(10):
+   if g[Y][X]==5:
     for i in r(a):g[Y+i][X:X+b]=g[t+i][l:l+b]
  return g
