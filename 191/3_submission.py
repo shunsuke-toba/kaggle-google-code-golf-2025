74fc6bcb832e79fcd1efd871329e5620872bd14c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:49 +0900

    タスク 191: 更新 (444 -> 436)

--- 191/3_submission.py
+++ 191/3_submission.py
@@ -1,13 +1,13 @@
 def p(g,R=range):
- S=len(g);f=sum(g,[]);i=f.index(1);k=f[::-1].index(1);m=[tuple(x&4 for x in r[i%S+1:~k%S])for r in g[i//S+1:S-1-k//S]];r=[*map(list,g)]
+ n=23;f=sum(g,[]);i=f.index(1);k=~f[::-1].index(1);m=[[x&4 for x in r[i%n+1:k%n]]for r in g[i//n+1:n+k//n]];f=[*map(list,g)]
  for _ in R(4):
   for q in m,m[::-1]:
    h=len(q);w=len(q[0])
-   for y in R(S-h+1):
-    for x in R(S-w+1):
-     if [tuple(g[x:x+w])for g in g[y:y+h]]==q:
+   for y in R(n-h+1):
+    for x in R(n-w+1):
+     if [g[x:x+w]for g in g[y:y+h]]==q:
       for V in R(y-1,y+h+1):
        for U in R(x-1,x+w+1):
-        if S>V>=0<=U<S and g[V][U]-4:r[V][U]=1
-  m=[*zip(*m[::-1])]
- return r
+        if n>V>=0<=U<n and g[V][U]^4:f[V][U]=1
+  m=[*map(list,zip(*m[::-1]))]
+ return f
