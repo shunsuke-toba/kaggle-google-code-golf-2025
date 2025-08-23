1b4089ff117f3752aef423e0536e84b523b702c7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:10:08 +0900

    タスク 255: 更新 (704 -> 698)

--- 255/3_submission.py
+++ 255/3_submission.py
@@ -8,15 +8,14 @@ def p(a):
   for x in R(30):
    for Y in R(y+3,31):
     for X in R(x+3,31):
-     if f(y,x,Y,X)*f(y-(y>0),x-(x>0),Y+(Y<30),X+(X<30))*(t:=max(Y-y,X-x)<<10|(Y-y)*(X-x))>b:b=t;B=y,x,Y,X
- y,x,Y,X=B
- for r in a[y:Y]:r[x:X]=[3]*(X-x)
+     if f(y,x,Y,X)*f(y-(y>0),x-(x>0),Y+(Y<30),X+(X<30))*(t:=max(Y-y,X-x)<<10|(Y-y)*(X-x))>b:b=t;A=y;B=x;C=Y;D=X
+ for r in a[A:C]:r[B:D]=[3]*(D-B)
  O=[[0]*32]+[[0]+r+[0]for r in a]+[[0]*32]
- for i in R(y,Y):
-  for j in R(x,X):
+ for i in R(A,C):
+  for j in R(B,D):
    for U,V in(1,0),(-1,0),(0,1),(0,-1):
     p=i+U+1;q=j+V+1
     while 0<p<31>q>0==O[p][q]|O[p+V][q+U]|O[p-V][q-U]:p+=U;q+=V
     if{p,q}&{0,31}:
-     while (p,q)!=(i+U+1,j+V+1):p-=U;q-=V;a[p-1][q-1]=3
+     while(p,q)!=(i+U+1,j+V+1):p-=U;q-=V;a[p-1][q-1]=3
  return a
