acd7f127eb55a6fa71b1949bfa06bbc48b786fce
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 08:36:18 +0900

    タスク 054: 更新 (522 -> 518)

--- 054/3_submission.py
+++ 054/3_submission.py
@@ -1,11 +1,10 @@
 def p(g,R=range):
  s=sum(g,[]);b=s[0];a=max({*s}-{b},key=s.count);D=1,0,-1,0
  for z in R(676):
-  i=z//26;j=z%26;q=sum(p:=[r[j:j+5]for r in g[i:i+5]],[])
-  if{*q}-{a,b,q[12]}and p==[r[::-1]for r in p][::-1]:
-   for r in g[i:i+5]:r[j:j+5]=[b]*5
-   break
- for i,j in[(i,j)for i in R(30)for j in R(30)if a!=g[i][j]!=b]:
+  i=z//26;j=z%26;p=[r[j:j+5]for r in g[i:i+5]];q=sum(p,[])
+  if{*q}-{a,b,q[12]}and p==[r[::-1]for r in p][::-1]:break
+ for r in g[i:i+5]:r[j:j+5]=[b]*5
+ for i,j in[(i,j)for i in R(30)for j in R(30)if g[i][j]==q[12]]:
   for t in R(25):
    c=q[t];x=i+t//5-2;y=j+t%5-2
    if g[x][y]!=b!=c:g[x][y]=c
