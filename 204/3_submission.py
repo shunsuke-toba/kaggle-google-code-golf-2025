3affb58a20590d2ce4bd0c1f67f9a67a34bdba55
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:07:03 +0900

    タスク 204: 更新 (206 -> 176)

--- 204/3_submission.py
+++ 204/3_submission.py
@@ -1,9 +1,2 @@
-def p(g):
- r=range;n=r(len(g))
- for y in n:
-  a=g[y]
-  for x in n:
-   if a[x]<1 or y*g[y-1][x] or x*a[x-1]:continue
-   l=(a+[0]).index(0,x)-x
-   for j in g[y+1:y+l-1]:j[x+1:x+l-1]=[2|l%2*5]*(l-2)
- return g
+p=lambda g:[exec("l=(a+[0]).index(0,x)-x\nfor j in g[y+1:y+l-1]:j[x+1:x+l-1]=[l%2*5|2]*(l-2)")for y,a in enumerate(g)for x,v in enumerate(a)if v==1>y*g[y-1][x]+x*a[x-1]]and g
+
