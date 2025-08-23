6ab961582bff87affcfee5f12e68540f26f0f3dc
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:07:04 +0900

    タスク 222: 更新 (268 -> 232)

--- 222/3_submission.py
+++ 222/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- R=range;h=len(g);w=len(g[0])
- _,a,b,c,d,v=max(((c-a+1)*(d-b+1),a,b,c,d,v)for a in R(h)for b in R(w)if(v:=g[a][b])for c in R(a,h)for d in R(b,w)if all(g[x][y]==v for x in R(a,c+1)for y in R(b,d+1)))
- return[[v*(a<=i<=c)*(b<=j<=d)for j in R(w)]for i in R(h)]
+def p(g):r=range;R=r(16);_,a,b,c,d,v=max(((c-a)*(d-b),a,b,c,d,v)for a in R for b in R if(v:=g[a][b])for c in R[a+1:]for d in R[b+1:]if all(g[x][y]==v for x in r(a,c)for y in r(b,d)));return[[v*(a<=i<c)*(b<=j<d)for j in R]for i in R]
