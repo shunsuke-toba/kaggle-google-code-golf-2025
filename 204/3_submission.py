3132d709de922bb0a046380d20ef57f13543f49a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:43 +0900

    タスク 204: 更新 (224 -> 206)

--- 204/3_submission.py
+++ 204/3_submission.py
@@ -1,8 +1,9 @@
 def p(g):
- r=range
- for d in r(n:=len(g)):
-  for y in r(m:=n-d-1):
-   for x in r(m):
-    if{g[y][x+i]*g[y+d+1][x+i]*g[y+i][x]*g[y+i][x+d+1]for i in r(d+2)}=={1}:
-     for j in g[y+1:y+d+1]:j[x+1:x+d+1]=[2|d%2*5]*d
+ r=range;n=r(len(g))
+ for y in n:
+  a=g[y]
+  for x in n:
+   if a[x]<1 or y*g[y-1][x] or x*a[x-1]:continue
+   l=(a+[0]).index(0,x)-x
+   for j in g[y+1:y+l-1]:j[x+1:x+l-1]=[2|l%2*5]*(l-2)
  return g
