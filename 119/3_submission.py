d0f0eb1d347586bc530eac14dc057e8bc8880918
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:31 +0900

    タスク 119: 更新 (230 -> 229)

--- 119/3_submission.py
+++ 119/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  r=lambda g:[*map(list,zip(*g[::-1]))];c=0
  while g[6][0]-2:g=r(g);c+=1
- f=8in g[-1];s=g[f*-1].index(8);w=g[0].count(2)
+ f=8 in g[-1];s=g[-f].index(8);w=g[0].index(0)
  for i,h in enumerate(g):j=w+abs(s-(i,11-i)[f]-w);h[j]=h[j]or 3
  for _ in[0]*(-c%4):g=r(g)
  return g
