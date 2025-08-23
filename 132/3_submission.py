038c69ca0b096a2fc5a0f0d274ceb4076397aec5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:07:02 +0900

    タスク 132: 更新 (183 -> 174)

--- 132/3_submission.py
+++ 132/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
- f=sum(g,[]);w=len(g[0]);s=sorted
+ f=sum(g,[]);w=len(g[0])
  for v in {*f}-{0}:
-  i=f.index(v);j=f.index(v,-~i);a,b=s((i//w,j//w));c,d=s((i%w,j%w))
+  i=f.index(v);j=f.index(v,-~i);a=i//w;b=j//w;c,d=sorted((i%w,j%w))
   for R in g[a:b+1]:R[c:d+1]=[v]*-~(d-c)
  return g
