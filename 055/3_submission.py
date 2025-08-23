790c9c22503a91dd8a31ef9ceb1a7210edc2a374
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 05:58:30 +0900

    タスク 055: 更新 (178 -> 171)

--- 055/3_submission.py
+++ 055/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
- c=g[0].index(8);d=g[0].index(8,c+1);w=d+~c;k=0
+ c=g[0].index(8);d=g[0].index(8,c+1);k=0
  for r in g:
-  if r[0]:k+=1;continue
-  r[c+1:d]=[2,6,1][k:k+1]*w
-  if k%2:r[:c]=[4]*c;r[d+1:]=[3]*(len(r)+~d)
+  if r[0]:k+=1
+  else:r[c+1:d]=[2,6,1][k:k+1]*(d+~c);r[:c]=[k%2*4]*c;r[d+1:]=[k%2*3]*(len(r)+~d)
  return g
