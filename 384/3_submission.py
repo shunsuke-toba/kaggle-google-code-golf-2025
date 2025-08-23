7e6129bdbddb0f273fa9c8a544761b56720cd9ff
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 08:36:24 +0900

    タスク 384: 更新 (154 -> 150)

--- 384/3_submission.py
+++ 384/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):y,x=zip(*[(i//9,i%9)for i in range(81)if g[i//9][i%9]]);return[[*sum(zip(r:=g[i//2][min(x):-~max(x)],r),())]for i in range(min(y)*2,-~max(y)*2)]
+def p(g):y,x=zip(*[(i//9,i%9)for i in range(81)if g[i//9][i%9]]);return[sum(zip(r:=g[i//2][min(x):-~max(x)],r),())for i in range(min(y)*2,-~max(y)*2)]
