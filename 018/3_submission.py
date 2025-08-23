90d9666cd78886b832e496b42eb8315f205af853
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:28 +0900

    タスク 018: 更新 (617 -> 601)

--- 018/3_submission.py
+++ 018/3_submission.py
@@ -1,21 +1,21 @@
 def p(g):
- L=len;h=L(g);w=L(g[0]);R=range;F=filter;S=[]
+ l=len;h=l(g);w=l(g[0]);R=range;f=filter;S=[]
  for y in R(h):
   for x in R(w):
-   q,a,b=[],y,x
+   q=a=b=0
    for s in R(y+1,h+1):
     for t in R(x+1,w+1):
-     if (m:=[g[i][x:t] for i in R(y,s)])==[*map(list,zip(*F(any,zip(*F(any,m)))))]and L({0,*sum(m,[])})>4:q,a,b=m,s,t
+     if (m:=[g[i][x:t]for i in R(y,s)])==[*map(list,zip(*f(any,zip(*f(any,m)))))]and l({0,*sum(m,[])})>4:q,a,b=m,s,t
    if q:S+=q,
    for r in g[y:a]:r[x:b]=[0]*(b-x)
  for r in S:
   u=sum(r,[])
   for z in R(8):
-   a=L(r);b=L(r[0])
+   a=l(r);b=l(r[0])
    for y in R(h-a+1):
     for x in R(w-b+1):
      if all(r[i][j]*(u.count(r[i][j])==1)==g[y+i][x+j]for i in R(a)for j in R(b)):
       for i in R(a):g[y+i][x:x+b]=r[i]
-   r=[*map(list,zip(*r[::-1]))]
+   r=[*zip(*r[::-1])]
    if z&3>2:r=r[::-1]
  return g
