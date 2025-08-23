c67f7c9c5a5313ea60db4d25bc7f32a44f4082ec
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:31 +0900

    タスク 101: 更新 (631 -> 597)

--- 101/3_submission.py
+++ 101/3_submission.py
@@ -7,11 +7,11 @@ def p(g):
     for t in R(c+1,w+1):
      if all(map(any,x:=[g[i][c:t]for i in R(r,s)]))and all(map(any,zip(*x)))and{1,2}<={*sum(x,[])}:p=x;u={(i,j)for i in R(r,s)for j in R(c,t)}
  for s in 3,2,1:
-  H=len(p)*s;W=len(p[0])*s;r=[[p[i//s][j//s]for j in R(W)]for i in R(H)]
+  H=len(p)*s;W=len(p[0])*s
   for y in R(-H,h):
    for x in R(-W,w):
-    if all((r[i][j]==2)==(w>x+j>-1<y+i<h>g[y+i][x+j]==2)and(y+i,x+j)not in u for i in R(H)for j in R(W)):
+    if all((p[i//s][j//s]==2)==(w>x+j>-1<y+i<h>g[y+i][x+j]==2)and(y+i,x+j)not in u for i in R(H)for j in R(W)):
      for i in R(H):
       for j in R(W):
-       if w>x+j>-1<y+i<h:g[y+i][x+j]=r[i][j];u|={(y+i,x+j)}
+       if w>x+j>-1<y+i<h:g[y+i][x+j]=p[i//s][j//s];u|={(y+i,x+j)}
  return g
