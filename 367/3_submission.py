9e377439cd87414955c5b7191c0e43e9fd847122
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-23 21:35:51 +0900

    367: 嘘解法(253 -> 252)

--- 367/3_submission.py
+++ 367/3_submission.py
@@ -1,6 +1,7 @@
 def p(g):
  h,w=len(g),len(g[0]);d=-1,0,1,0
  for k in range(9**5):
-  r,c,s=k%97%h,k%89%w,k%4;x,y=d[s]+r,d[~s]+c;t=g[r][c];g[(x-y+c)%h][(x-r+y)%w]|=([g[(r+d[s+~i])%h][(c+d[s-i])%w]for i in range(4)]==[5,t-5,0,5])*4
-  if h>x>-1<y<w>5>t:g[x][y]|=t
+  r,c,s=k%97%h,k%89%w,k%4;x,y=d[s]+r,d[~s]+c;t=g[r][c]
+  try:g[x-y+c][x-r+y]|=([g[r+d[s+~i]-h][(c+d[s-i])%w]for i in range(4)]==[5,t-5,0,5])*4;g[x][y]|=t*(0>t-5<x*y)
+  except:0
  return g
