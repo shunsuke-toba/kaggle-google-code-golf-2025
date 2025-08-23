e1078aaafee2b0bcbdda6912f1240506009154e9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:39 +0900

    タスク 355: 更新 (281 -> 276)

--- 355/3_submission.py
+++ 355/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- f=sum(g,[]);c,h,w=min(f,key=f.count),len(g),len(g[0]);r=[0]*h
+ c,h,w=min(f:=sum(g,[]),key=f.count),len(g),len(g[0]);r=[0]*h
  for k in range(h*w*9):
-  i,j=k//w%h,k%w
-  if g[i][j]==c and (n:=[g[x][y]for x,y in((i-1,j),(i+1,j),(i,j-1),(i,j+1))if h>x>-1<y<w]).count(s:=max(n,key=n.count))>1:g[i][j]=s;r[s]+=1
+  if g[i:=k//w%h][j:=k%w]==c and (n:=[g[x][y]for x,y in((i-1,j),(i+1,j),(i,j-1),(i,j+1))if h>x>-1<y<w]).count(s:=max(n,key=n.count))>1:g[i][j]=s;r[s]+=1
  return[[r.index(max(r))]]
