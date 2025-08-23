1f5fd67c0d1ba45a9d73f00d3020921c09d48f4b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:36 +0900

    タスク 238: 更新 (367 -> 351)

--- 238/3_submission.py
+++ 238/3_submission.py
@@ -1,10 +1,10 @@
 def p(g):
- a=c=A=C=99;b=d=-1;R=range;e=enumerate
+ a=c=A=C=99;b=-1;e=enumerate
  for i,r in e(g):
   for j,v in e(r):
-   if v%8:a=min(a,i);b=max(b,i);c=min(c,j);d=max(d,j)
+   if v&7:a=min(a,i);b=max(b,i);c=min(c,j)
    elif v:A=min(A,i);C=min(C,j)
- n=b-a-1;G=[r[c:d+1]for r in g[a:b+1]];k=G[0][1],G[1][-1],G[1][0],G[-1][1]
- for i in R(n):
-  for j in R(n):v=g[A+i][C+j];G[i+1][j+1]=v==8 and(i-j)*(i+j+1-n)and k[(i>j)*2+(i+j>n-1)]or v
+ n=b-a-1;R=range(n);G=[r[c:c+n+2]for r in g[a:b+1]];k=G[0][1],G[1][-1],G[1][0],G[-1][1]
+ for i in R:
+  for j in R:v=g[A+i][C+j];G[i+1][j+1]=v>7 and(i-j)*(n-i-j-1)and k[(i>j)*2+(i+j>=n)]or v
  return G
