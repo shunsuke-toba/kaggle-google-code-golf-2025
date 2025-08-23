c37e5ee38dba20fe6eb205a36d44185443ce31b8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:10:07 +0900

    タスク 209: 更新 (613 -> 592)

--- 209/3_submission.py
+++ 209/3_submission.py
@@ -1,19 +1,19 @@
 def p(g):
- a=e=y=len(g);c=i=x=len(g[0]);b=d=f=j=0;L=()
- for t in range(y*x):
-  v=g[r:=t//x][o:=t%x]
-  if v==4:a=min(a,r);b=max(b,r);c=min(c,o);d=max(d,o)
-  elif v:L+=(r,o),
- for r,o in L:
-  if r<a or r>b or o<c or o>d:e=min(e,r);f=max(f,r);i=min(i,o);j=max(j,o)
- h=f-e+1;w=j-i+1;z=k=R=O=0
- for s in 2,3,4:
-  W=w*s
-  for r in range(a,b-h*s+2):
-   for o in range(c,d-W+2):
-    m=n=0
-    for t in range(h*s*W):
-     if(v:=g[r+t//W][o+t%W]):n+=1;m+=v!=g[e+t//W//s][i+t%W//s]
-    if m<1<n-z:z=n;k=s;R=r;O=o
- for q in range(h*k):g[R+q][O:O+w*k]=[g[e+q//k][i+p//k]for p in range(w*k)]
- return[r[c:d+1]for r in g[a:b+1]]
+ n=len(g);m=len(g[0]);a=n;c=m;b=d=0;S=[]
+ for t in range(n*m):
+  if (v:=g[r:=t//m][o:=t%m])==4:a=min(a,r);b=max(b,r);c=min(c,o);d=max(d,o)
+  elif v:S+=(r,o),
+ e=n;f=0;i=m;j=0;I=[]
+ for r,o in S:
+  if a<=r<=b and c<=o<=d:I+=(r,o),
+  else:e=min(e,r);f=max(f,r);i=min(i,o);j=max(j,o)
+ h=f-e+1;w=j-i+1
+ for k in 2,3,4:
+  H=h*k;W=w*k
+  for R in range(a,b-H+2):
+   for O in range(c,d-W+2):
+    try:
+     if all(g[e+(r-R)//k][i+(o-O)//k]==g[r][o]for r,o in I):
+      for q in range(H):g[R+q][O:O+W]=[g[e+q//k][i+p//k]for p in range(W)]
+      return[r[c:d+1]for r in g[a:b+1]]
+    except:0
