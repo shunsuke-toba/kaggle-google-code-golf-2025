0ffb41962d027f3ffa323168a7f234d1e76b7809
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:44 +0900

    タスク 234: 更新 (374 -> 366)

--- 234/3_submission.py
+++ 234/3_submission.py
@@ -1,9 +1,9 @@
 def p(g):
- c=n=r=0;N,M=len(g),len(g[0]);d=1,0,-1,0,1;R=range
- for i in R(1,N-1):
-  for j in R(1,M-1):
-   if(m:=g[i][j])and(g[i-1][j]|g[i+1][j])*(g[i][j-1]|g[i][j+1])<1:c+=1;n=m;g[i][j]=0;[r:=k for k in R(4)if(v:=g[i+d[k]][j+d[k+1]])*(v-n)]
- for i in R(N)[::r>0 or-1]:
-  for j in R(M)[::r<3 or-1]:
-   if g[i][j]==n>g[a:=i+d[r]*c][b:=j+d[r+1]*c]:g[a][b]=n;g[i][j]=0
+ c=n=k=0;N=len(g);M=len(g[0]);d=1,0,-1,0,1;r=range
+ for i in r(N):
+  for j in r(M):
+   if(m:=g[i][j])and(g[i-1][j]|g[i+1][j])*(g[i][j-1]|g[i][j+1])<1:c+=1;n=m;g[i][j]=0;[k:=t for t in r(4)if(v:=g[i+d[t]][j+d[t+1]])*(v-n)]
+ for i in r(N)[::k>0 or-1]:
+  for j in r(M)[::k<3 or-1]:
+   if g[i][j]==n>g[a:=i+d[k]*c][b:=j+d[k+1]*c]:g[a][b]=n;g[i][j]=0
  return g
