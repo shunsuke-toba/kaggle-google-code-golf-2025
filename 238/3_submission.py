8601d88ec43bda1e94eeebe875eaf1392dcb2268
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 10:42:44 +0900

    タスク 238: 更新 (388 -> 371)

--- 238/3_submission.py
+++ 238/3_submission.py
@@ -1,12 +1,11 @@
 def p(g):
- a=c=A=C=99;b=d=B=-1;R=range;e=enumerate
+ a=c=A=C=99;b=d=-1;R=range;e=enumerate
  for i,r in e(g):
   for j,v in e(r):
    if v%8:a=min(a,i);b=max(b,i);c=min(c,j);d=max(d,j)
-   elif v:A=min(A,i);B=max(B,i);C=min(C,j)
- n=B-A+1;G=[r[c:d+1]for r in g[a:b+1]];k=[G[0][1],G[1][-1],G[1][0],G[-1][1]]
+   elif v:A=min(A,i);C=min(C,j)
+ n=b-a-1;G=[r[c:d+1]for r in g[a:b+1]];k=G[0][1],G[1][-1],G[1][0],G[-1][1]
  for i in R(n):
   for j in R(n):
-   v=g[A+i][C+j]
-   G[i+1][j+1]=v==8and(i-j)*(i+j+1-n)and k[(i>j)*2+(i+j>n-1)]or v
+   v=g[A+i][C+j];G[i+1][j+1]=v==8 and(i-j)*(i+j+1-n)and k[(i>j)*2+(i+j>n-1)]or v
  return G
