632db6eb4c99dcdf04a89d996befe7f02aaebe3c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 13:47:46 +0900

    タスク 102: 更新 (280 -> 260)

--- 102/3_submission.py
+++ 102/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
- n=len(g)
- for s in range(1,n):
-  for y in range(n-s):
-   for x in range(n-s):
-    if all(g[y][x+i]==g[y+s][x+i]==g[y+i][x]==g[y+i][x+s]==5 for i in range(s+1))and not sum(sum(r[x+1:x+s])for r in g[y+1:y+s]):
-     for i in range(1,s):g[y+i][x+1:x+s]=[2]*(s-1)
+ n=len(g);R=range
+ for s in R(2,n):
+  for y in R(n-s):
+   for x in R(n-s):
+    if all(g[y][x+i]==g[y+s][x+i]==g[y+i][x]==g[y+i][x+s]==5 for i in R(s+1))>max(max(m[x+1:x+s])for m in g[y+1:y+s]):
+     for m in g[y+1:y+s]:m[x+1:x+s]=[2]*(s-1)
  return g
