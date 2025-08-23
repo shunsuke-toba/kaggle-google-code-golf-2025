874e2fb66ff60e645dd64235b5b4a9667fc62c9a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:15 +0900

    タスク 066: 更新 (552 -> 533)

--- 066/3_submission.py
+++ 066/3_submission.py
@@ -1,24 +1,20 @@
 def p(g):
- h=len(g);w=len(g[0]);a=[(i,j)for i in range(h)for j in range(w)if g[i][j]==3]
- def f(y,x,a,b,t):
-  Y,X=y+a,x+b
-  if-1<Y<h>-1<X<w:
-   v=g[Y][X]
-   if v==2:return[(y,x)]
-   if v>7:
-    if t>1:return
-    for d in(-b,a),(b,-a):
-     r=f(y,x,*d,t+1)
-     if r:return[(y,x)]+r
-   else:
-    r=f(Y,X,a,b,t)
-    if r:return[(y,x)]+r
- (y,x),(Y,X)=sorted(a)
+ h=len(g);w=len(g[0]);s=[(i,j)for i in range(h)for j in range(w)if g[i][j]==3]
+ def f(y,x,a,b,t=0):
+  y+=a;x+=b
+  if not(0<=y<h and 0<=x<w):return
+  v=g[y][x]
+  if v==2:return[(y-a,x-b)]
+  if v<8:r=f(y,x,a,b,t)
+  else:
+   if t>1:return
+   r=f(y-a,x-b,-b,a,t+1)or f(y-a,x-b,b,-a,t+1)
+  return r and [(y-a,x-b)]+r
+ (y,x),(Y,X)=sorted(s)
  for y,x,a,b in(((y,x,0,-1),(Y,X,0,1)),((y,x,-1,0),(Y,X,1,0)))[y!=Y]:
-  Y,X=y+a,x+b
-  if-1<Y<h>-1<X<w and g[Y][X]-8:
-   r=f(y,x,a,b,0)
+  if 0<=y+a<h and 0<=x+b<w and g[y+a][x+b]-8:
+   r=f(y,x,a,b)
    if r:
     for y,x in r:g[y][x]=3
-    return g
+    break
  return g
