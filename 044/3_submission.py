8237fbd560658bda1c34ab82345d85ba1fd486df
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:14 +0900

    タスク 044: 更新 (616 -> 575)

--- 044/3_submission.py
+++ 044/3_submission.py
@@ -1,24 +1,23 @@
 def p(g):
- v,A,B=set(),{},{};r=[*map(list,g)]
+ r=[*map(list,g)]
+ a={};b={}
  for k in range(100):
-  y,x=divmod(k,10)
-  if(y,x)in v:continue
-  c=g[y][x];q=[(y,x)];S={q[0]};f=c<1
-  while q:
-   y,x=q.pop()
-   for Y,X in(y+1,x),(y-1,x),(y,x+1),(y,x-1):
-    if 0<=Y<10 and 0<=X<10:
-     if g[Y][X]==c:
-      if(Y,X)not in S:S.add((Y,X));q.append((Y,X))
-     elif f and g[Y][X]-5:f=0
-    elif f:f=0
-  v|=S;y,x=map(min,zip(*S));s=frozenset((i-y,j-x)for i,j in S)
-  if f:A[s]=y,x
-  elif c-5:B[c]=c in B and 1 or(s,S,y,x)
- for c,v in B.items():
-  if v!=1:
-   s,S,y,x=v
-   if s in A:
-    Y,X=A.pop(s)
-    for i,j in S:r[i][j]=0;r[i-y+Y][j-x+X]=c
+  y,x=divmod(k,10);c=g[y][x]
+  if c-5:
+   q=[(y,x)];g[y][x]=5;f=c<1
+   for y,x in q:
+    for Y,X in(y+1,x),(y-1,x),(y,x+1),(y,x-1):
+     if 0<=Y<10 and 0<=X<10:
+      d=g[Y][X]
+      if d==c:g[Y][X]=5;q.append((Y,X))
+      elif f and d-5:f=0
+     elif f:f=0
+   Y,X=map(min,zip(*q));s=frozenset((i-Y,j-X)for i,j in q)
+   if f:a[s]=Y,X
+   elif c:b[c]=c in b or not q[1:] and 1 or(s,q,Y,X)
+ for c,v in b.items():
+  if v!=1 and v[0] in a:
+   s,q,y,x=v
+   Y,X=a[s]
+   for i,j in q:r[i][j]=0;r[i-y+Y][j-x+X]=c
  return r
