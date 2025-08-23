fa78e1efa60851745aaa439d29e132f064e87e51
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 23:10:09 +0900

    タスク 379: 更新 (331 -> 318)

--- 379/3_submission.py
+++ 379/3_submission.py
@@ -1,12 +1,11 @@
 def p(g):
- e=enumerate;t=lambda x:list(map(list,zip(*x)))
- L=[i for i,r in e(g) if{8}==set(r)]
+ e=enumerate;t=lambda x:[*map(list,zip(*x))]
+ L=[i for i,r in e(g)if{8}=={*r}]
  if not L:return t(p(t(g)))
  for i,j in((i,j)for i,r in e(g)for j,v in e(r)if v==2):
   for l in L:
    k=i
-   while g[k][j]-8 and k-l:
-    g[k][j]=2;k+=1-2*(l<k)
+   while g[k][j]-8 and k-l:g[k][j]=2;k+=1-2*(l<k)
    if k==l:
-    for m in l-1,l,l+1:g[m][j-1:j+2]=8,8,8;g[l][j]=2
+    for m in-1,0,1:g[l+m][j-1:j+2]=8,8,8;g[l][j]=2
  return g
