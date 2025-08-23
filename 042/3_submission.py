1f92a77b55e3d1c8b519c17f2d7b7dbd009558d9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 22:10:51 +0900

    タスク 042: 更新 (317 -> 311)

--- 042/3_submission.py
+++ 042/3_submission.py
@@ -1,11 +1,11 @@
 def p(g):
- h=[*map(list,g)]
- for r,a in enumerate(h):
+ for r,a in enumerate(g):
   for c in range(10):
-   if a[c]==3>a[c-1]+h[r-1][c]:
-    k=a[c:].index(0)
+   if a[c]==3>a[c-1]|g[r-1][c]:
+    k=1
+    while a[c+k]==3:k+=1
     for C in c-k,c+k:
-     if h[r+k][C:C+1]==[3]:
+     if g[r+k][C:C+1]==[3]:
       for R,C in((r-k,2*C-c),(r+2*k,2*c-C)):
        for i in range(k*k):x=R+i//k;y=C+i%k;0<=x<10>y>=0 and g[x].__setitem__(y,8)
  return g
