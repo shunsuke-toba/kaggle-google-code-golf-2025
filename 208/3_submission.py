c2e858d34ff901aba44cbe20e0eed796685e0560
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 09:17:20 +0900

    タスク 208: 更新 (455 -> 404)

--- 208/3_submission.py
+++ 208/3_submission.py
@@ -1,12 +1,13 @@
 def p(g):
- R,C=len(g),len(g[0]);F=range;m=x=y=s=t=0
- for a in F(R*C):
-  q=g[i:=a//C];j=a%C
-  for h in F(3,-~R-i):
-   for w in F(3,-~C-j):
-    if q[j:j+w]==[q[j]]*w and all(r[j]==q[j] for r in g[i:i+h])*all(z<1 for r in g[i+1:i+h-1]for z in r[j+1:j+w-1])and(A:=h*w)>m:m,x,y,s,t=A,i,j,h,w
- for i in F(-~R-s):
-  for j in F(-~C-t):
-   if(i^x|j^y)*all(z<1 for r in g[i+1:i+s-1]for z in r[j+1:j+t-1]):
-    for h in F(s):g[i+h][j:j+t]=g[x+h][y:y+t]
+ s=[v for r in g for v in r if v]
+ k=min(s,key=s.count)
+ I=range;h=len(g);w=len(g[0])
+ i=[r for r in I(h)if k in g[r]]
+ u,v=i[0],i[-1]
+ j=[c for c in I(w)if any(g[r][c]==k for r in i)]
+ l,r=j[0],j[-1];H=v-u+1;W=r-l+1
+ for y in I(h-H+1):
+  for x in I(w-W+1):
+   if(x-l|y-u)and all(g[Y][X]<1 for Y in I(y+1,y+H-1)for X in I(x+1,x+W-1)):
+    for t in I(H):g[y+t][x:x+W]=g[u+t][l:l+W]
     return g
