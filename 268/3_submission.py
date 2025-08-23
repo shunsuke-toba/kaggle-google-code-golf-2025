7e7671cedbac1f4beda8922021aa394e940d7769
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-23 20:12:37 +0900

    タスク 268: 更新 (443 -> 411)

--- 268/3_submission.py
+++ 268/3_submission.py
@@ -1,16 +1,16 @@
 def p(g):
- R=range;r=lambda z:list(map(list,zip(*z[::-1])))
+ n=len(g);R=range;r=lambda z:[*map(list,zip(*z[::-1]))]
  for k in R(4):
-  x,y=zip(*[(i,j)for i,s in enumerate(g)for j,v in enumerate(s)if v])
+  x,y=zip(*[(i,j)for i in R(n)for j in R(n)if g[i][j]])
   a,b=min(x),max(x);c,d=min(y),max(y)
   if 0 in g[a][c+2:d-1]:break
   g=r(g)
- h=[s[:]for s in g];w=d-c-1
- for e in h[a+1:b]:e[c+1:d]=[4]*w
- for e in h[:b]:e[c+2:d-1]=[4]*(w-2)
+ w=d-c-1
+ for e in g[a+1:b]:e[c+1:d]=[4]*w
+ for e in g[:b]:e[c+2:d-1]=[4]*(w-2)
  for j in R(a):
   i=a-1-j;L=c+1-j;E=d-1+j
-  if L>=0:h[i][L]=4
-  if E<len(g):h[i][E]=4
- for k in R(-k%4):h=r(h)
- return h
+  if L>=0:g[i][L]=4
+  if E<n:g[i][E]=4
+ for k in R(-k%4):g=r(g)
+ return g
