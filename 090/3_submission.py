401f44ed0a440386b643a3515bcec5082c127629
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 20:23:22 +0900

    タスク 090: 更新 (218 -> 216)

--- 090/3_submission.py
+++ 090/3_submission.py
@@ -1,5 +1,4 @@
 def p(g):
- r=range;h=len(g);w=len(g[0])
- _,y,x,a,b=max((a*b,y,x,a,b)for y in r(h)for x in r(w)for a in r(2,h-y+1)for b in r(2,w-x+1)if sum(max(R[x:x+b])for R in g[y:y+a])<1)
+ r=range;h=len(g);w=len(g[0]);_,y,x,a,b=max((a*b,y,x,a,b)for y in r(h)for x in r(w)for a in r(2,h-y+1)for b in r(2,w-x+1)if sum(max(R[x:x+b])for R in g[y:y+a])<1)
  for R in g[y:y+a]:R[x:x+b]=[6]*b
  return g
