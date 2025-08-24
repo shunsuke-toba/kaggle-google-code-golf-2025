5adcf6e2f56e1df91f49317a8e554d57bea2db0c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:06 +0900

    タスク 036: 更新 (135 -> 131)

--- 036/3_submission.py
+++ 036/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):f=lambda a:zip(*filter(lambda r,s=sum(g,[]):max(s,key=lambda x:s.index(x)+s[::-1].index(x))in r,a));return[*map(list,f(f(g)))]
+def p(g):k=max(z:=sum(g,[]),key=lambda x:z.index(x)+z[::-1].index(x));f=lambda a:[*zip(*filter(lambda r:k in r,a))];return f(f(g))
