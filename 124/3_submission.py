ea4b3e79474115c1ed91001b10a7939e7be21c19
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 10:54:18 +0900

    タスク 124: 更新 (209 -> 147)

--- 124/3_submission.py
+++ 124/3_submission.py
@@ -1,8 +1,6 @@
 def p(g):
- h=len(g);w=len(g[0])
- s=lambda r,d:(w*[0]+r+w*[0])[w-d:][:w]
- for k in range(1,h+1):
-  for d in range(-w,w+1):
-   if g[k:]==[s(r,d)for r in g[:h-k]]:
-    while len(g)<10:g+=s(g[-k],d),
+ for k in 1,2,3:
+  for d in 0,1,2:
+   if g[k:]==[d*[0]+r[:10-d]for r in g[:-k]]:
+    while len(g)<10:g+=d*[0]+g[-k][:10-d],
     return g
