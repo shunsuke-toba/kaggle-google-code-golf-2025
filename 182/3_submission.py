840160da2c55f9a2683af76a55c87e6e744f3d23
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 01:02:00 +0900

    タスク 182: 更新 (260 -> 258)

--- 182/3_submission.py
+++ 182/3_submission.py
@@ -1,7 +1,1 @@
-def p(g):
- r=range(25);m=r[:16];t=next([k for k in r if(v:=g[y-~k//5][x+1+k%5])and(c:=v)]for y in m for x in m if g[y][x]&g[y][x+1]&g[y+1][x]>4)
- for y in m:
-  for x in m:
-   if[k for k in r if g[y+k//5][x+k%5]]==t:
-    for k in t:g[y+k//5][x+k%5]=c
- return g
+def p(g):r=range(25);m=r[:16];t=next([k for k in r if(v:=g[y-~k//5][x+1+k%5])and(c:=v)]for y in m for x in m if g[y][x]&g[y][x+1]&g[y+1][x]>4);[[g[y+k//5].__setitem__(x+k%5,c)for k in t]for y in m for x in m if[k for k in r if g[y+k//5][x+k%5]]==t];return g
