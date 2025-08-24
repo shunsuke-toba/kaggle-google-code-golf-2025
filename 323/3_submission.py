97646c61e5269e071dd11608c0f934a3b4a38b5c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:33 +0900

    タスク 323: 更新 (167 -> 144)

--- 323/3_submission.py
+++ 323/3_submission.py
@@ -1,9 +1,6 @@
 def p(g):
  i=sum(g,[]).index(8)
  for s in-1,1:
-  x,y=i//13,i%13
-  for t in range(24):
-   x-=s*(t&2<1);y+=s*(t&2>0)
-   if 13>x>-1<y<13:g[x][y]=5
-   else:break
+  x,y=i//13,i%13;t=0
+  while 13>(x:=x-s*(t&2<1))>-1<(y:=y+s*(t&2>0))<13:g[x][y]=5;t+=1
  return g
