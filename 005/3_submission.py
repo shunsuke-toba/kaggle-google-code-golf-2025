e1cf1b6a25d50590bad33714e6313179d45758a3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:18 +0900

    タスク 005: 更新 (319 -> 317)

--- 005/3_submission.py
+++ 005/3_submission.py
@@ -3,6 +3,6 @@ def p(g):
  while 0in map(sum,(P:=[g[r+i][c:c+3]for i in R])+[*zip(*P)]):c+=1;r+=c>18;c%=19
  for Y in-4,0,4:
   for X in-4,0,4:
-   t=(X|Y)and max(max(g[r+Y+i][c+X:c+X+3])for i in R);y,x=r,c
-   for _ in g*t:y+=Y;x+=X;[g[y+i].__setitem__(x+j,t)for i in R for j in R if P[i][j]and 21>y+i>-1<x+j<21]
+   t=max(max(g[r+Y+i][c+X:c+X+3])for i in R);y,x=r,c
+   for _ in g*(X|Y)**2:y+=Y;x+=X;[g[y+i].__setitem__(x+j,t)for i in R for j in R if P[i][j]and 0<=y+i<21>x+j>=0]
  return g
