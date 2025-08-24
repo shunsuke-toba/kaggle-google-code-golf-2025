3bca03412d15e3357a2857ba228a71712f4d6e15
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 23:43:11 +0900

    タスク 079: 更新 (136 -> 130)

--- 079/3_submission.py
+++ 079/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):r=range(12);a=[t for i in r for j in r if all(map(sum,(t:=[x[j:j+3]for x in g[i:i+3]])+[*zip(*t)]))];return max(a,key=a.count)
+p=lambda g,r=range(12):max(a:=[t for i in r for j in r if all(map(sum,(t:=[x[j:j+3]for x in g[i:i+3]])+[*zip(*t)]))],key=a.count)
