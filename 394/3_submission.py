9ce9825a4e33c8550006baf557993b59b4436522
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:48 +0900

    タスク 394: 更新 (159 -> 140)

--- 394/3_submission.py
+++ 394/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):n=len(g);x=sum(g,[]);a=x.index(0);c=n*n+~x[::-1].index(0);t=2+(n>6);return[[g[i+(i<t)*2*t-t][j]for j in range(a%n,1+c%n)]for i in range(a//n,1+c//n)]
+def p(g):n=len(g);x=sum(g,[]);a=x.index(0);b=n*n+~x[::-1].index(0);t=3-(n<7);return[g[i+(i<t)*2*t-t][a%n:b%n+1]for i in range(a//n,b//n+1)]
