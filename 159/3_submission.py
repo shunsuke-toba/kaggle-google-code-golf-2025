c6a50c5a48aadade86b097afaa99b1bae230862e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 20:23:24 +0900

    タスク 159: 更新 (199 -> 197)

--- 159/3_submission.py
+++ 159/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):e=enumerate;z=sum(2in r for r in g);a,b=map(min,zip(*{(i,j)for i,r in e(g)for j,v in e(r)if v&-3}));k=z//3;r=range(z-2);return[[2]*z,*[[2,*[g[a+i//k][b+j//k]for j in r],2]for i in r],[2]*z]
+def p(g):e=enumerate;k=sum(2in r for r in g);t=[2]*k;a,b=map(min,zip(*{(i,j)for i,r in e(g)for j,v in e(r)if v&-3}));k//=3;r=range(3*k);return[t,*[[2,*[g[a+i//k][b+j//k]for j in r],2]for i in r],t]
