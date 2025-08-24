cbc72cfb07a9be7c4dc1d11a87ae4fd235b3c778
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:39 +0900

    タスク 062: 更新 (226 -> 224)

--- 062/3_submission.py
+++ 062/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- f=sum(g,[]);k=sum({*f})-2;i=f.index(2)
- a=(i//10,i%10)[b:=k in g[i//10]]+(f[i-10+9*b]!=k);g=[[3]*10 for _ in g];n=100
+ f=sum(g,[]);k=sum({*f})-2;i=f.index(2);a=(i//10,i%10)[b:=k in g[i//10]]+(f[i-10+9*b]!=k);g=[[3]*10 for _ in g];n=100
  while n:=n-1:
   if f[n]==k:g[(i:=n//10)][(j:=n%10)]=g[(2*a+~i,i)[b]][(j,2*a+~j)[b]]=k
  return g
