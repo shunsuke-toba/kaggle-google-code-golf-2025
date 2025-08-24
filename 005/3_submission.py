ccff3b489660071d3e38797cfe2179ad69aa3d4c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 20:23:20 +0900

    タスク 005: 更新 (317 -> 308)

--- 005/3_submission.py
+++ 005/3_submission.py
@@ -4,5 +4,5 @@ def p(g):
  for Y in-4,0,4:
   for X in-4,0,4:
    t=max(max(g[r+Y+i][c+X:c+X+3])for i in R);y,x=r,c
-   for _ in g*(X|Y)**2:y+=Y;x+=X;[g[y+i].__setitem__(x+j,t)for i in R for j in R if P[i][j]and 0<=y+i<21>x+j>=0]
+   for _ in g:y+=Y;x+=X;[g[y+i].__setitem__(x+j,t)for i in R for j in R if P[i][j]and 0<=y+i<21>x+j>=0]
  return g
