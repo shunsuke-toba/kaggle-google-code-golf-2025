010651dfc88b2a0c3c1b8104bf3f9d10b16bcf0e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 02:03:33 +0900

    タスク 245: 更新 (188 -> 179)

--- 245/3_submission.py
+++ 245/3_submission.py
@@ -1,9 +1,5 @@
 def p(g):
- R=[];a=enumerate
- for i,r in a(g):
-  for j,v in a(r):
-   if v%3:R+=[(i,j)]
-   if v>2:c,d=i-5,j-5
+ a=enumerate;R=[(i,j)for i,r in a(g)for j,v in a(r)if v%3 or v>2and(c:=i-5,d:=j-5)and 0]
  a,b=map(min,zip(*R))
  for i,j in R[::-1]:g[i][j]=0;g[i+c-a][j+d-b]=2
  return g
