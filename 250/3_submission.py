e97db05ac940f0df58328e05716570e1ad9c74ea
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 10:54:20 +0900

    タスク 250: 更新 (226 -> 183)

--- 250/3_submission.py
+++ 250/3_submission.py
@@ -1,9 +1,6 @@
 def p(g):
- r=0
- while 2 not in g[r]:r+=1
- c=g[r].index(2);o=[[0]*len(g[0])for _ in g]
- for i,R in enumerate(g):
-  for j,v in enumerate(R):
-   if v>4:o[min(max(i,r-1),r+2)][min(max(j,c-1),c+2)]=5
-   if v==2:o[i][j]=2
+ r,c=divmod(sum(g,[]).index(2),10)
+ o=[[v&2 for v in r]for r in g]
+ for k in range(100):
+  if g[i:=k//10][j:=k%10]>4:o[min(max(i,r-1),r+2)][min(max(j,c-1),c+2)]=5
  return o
