c869679159a94c76915f870f8e45864b432d8391
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 13:37:53 +0900

    タスク 120: 更新 (176 -> 167)

--- 120/3_submission.py
+++ 120/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(b:=[r[:]for r in g],[b[r][c]==b[r][c+1]==b[r][c-1]==b[r+1][c]==b[r-1][c]!=0 and g[r].__setitem__(c,8)for r in range(1,len(g)-1)for c in range(1,len(g[0])-1)],g)[2]
+p=lambda g:(b:=eval(str(g)),[b[r][c]==b[r][c+1]==b[r][c-1]==b[r+1][c]==b[r-1][c]>0 and exec('g[r][c]=8')for r in range(1,len(g)-1)for c in range(1,len(g[0])-1)])and g
