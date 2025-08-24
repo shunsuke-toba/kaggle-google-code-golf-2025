1e5a97fc9c98a65575b6c35451cbac8f96f9a911
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:43 +0900

    タスク 220: 更新 (146 -> 140)

--- 220/3_submission.py
+++ 220/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,e=enumerate:[g[i+u].__setitem__(j+v,u|v and c*5%9 or c)for i,r in e(g)for j,c in e(r)if c&4<1<c for u in(-1,0,1)for v in(-1,0,1)]and g
+p=lambda g,e=enumerate:[g[i+u].__setitem__(j+v,(c,c*5%9)[u|v])for i,r in e(g)for j,c in e(r)if c%6&2 for u in(-1,0,1)for v in(-1,0,1)]and g
