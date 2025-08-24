1c6b20162b5f8a9aaf957153d41434204881dc7f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:46 +0900

    タスク 346: 更新 (154 -> 152)

--- 346/3_submission.py
+++ 346/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,R=range:next([[b+c]]for i in R(len(g)-2)for j in R(len(g[0])-2)if(b:=g[i][j])*(c:=g[i+1][j+1]-b)and{g[i+u//3][j+u%3]for u in R(9)if u-4}=={b})
+p=lambda g,R=range:next([[s[4]]]for i in R(len(g)-2)for j in R(len(g[0])-2)if(s:=[g[i+u//3][j+u%3]for u in R(9)])[0] and s[4]-s[0] and s.count(s[0])==8)
