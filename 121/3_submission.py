ddacd59ba65ef2584c5891b86014e7f85c24d1fb
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:09 +0900

    タスク 121: 更新 (125 -> 116)

--- 121/3_submission.py
+++ 121/3_submission.py
@@ -1,2 +1,1 @@
-def p(g):
- y,x=divmod(sum(g,[]).index(8),len(g[0]));a=[r[x-1:x+2]for r in g[y-1:y+2]];a[1][1]=max({*sum(a,[])}-{8});return a
+def p(g):y,x=divmod(sum(g,[]).index(8),13);a=[r[x-1:x+2]for r in g[y-1:y+2]];a[1][1]=max({*sum(a,[])}-{8});return a
