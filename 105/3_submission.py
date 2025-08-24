6c5c7391f0860b0ffd6c5a4c23ee3664244584a0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 01:01:59 +0900

    タスク 105: 更新 (259 -> 256)

--- 105/3_submission.py
+++ 105/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- e=enumerate;r=range;p=[(i,j)for i,u in e(g)for j,v in e(u)if v];y,x=zip(*p);a,b=min(y),max(y);d=max(x)
+ e=enumerate;r=range;p=[(i,j)for i,u in e(g)for j,v in e(u)if v];y,x=zip(*p);a,b=y[0],y[-1];d=max(x)
  for i in r(a,b+1):
   for j in r(2,d+1):g[i][j]=g[i][j]or 2*(i in(a,b)or j in(2,d)or sum((2<x<d)*(y==i)+(a<y<b)*(x==j)for y,x in p)>1)
  return g
