08676ac253a984cf444e889fe5e6411ec2cc3753
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 02:03:33 +0900

    タスク 263: 更新 (170 -> 169)

--- 263/3_submission.py
+++ 263/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[b:=[[[r[i:i+3]for r in g],g[i:i+3]][len(g)>3]for i in range(0,len(g[0])*len(g)//3,3)],m:=[[*map(bool,sum(t,[]))]for t in b],b[m.index(min(m,key=m.count))]][2]
+p=lambda g:[b:=[[[r[i:i+3]for r in g],g[i:i+3]][len(g)>3]for i in range(0,len(g)+len(g[0])-3,3)],m:=[[*map(bool,sum(t,[]))]for t in b],b[m.index(min(m,key=m.count))]][2]
