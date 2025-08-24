2f05cc27c1334d95c21831cd3dd21c179750ec53
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 16:00:23 +0900

    タスク 035: 更新 (153 -> 151)

--- 035/3_submission.py
+++ 035/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- l=g[3].index(8);b=2+sum(8 in r for r in g)
- for k in range(100):
+ l=g[3].index(8);b=2+sum(8in r for r in g)
+ for k in range(99):
   if x:=g[i:=k//10][j:=k%10]:g[max(min(i,b),3)][max(min(j,5),l)]=x
  return g
