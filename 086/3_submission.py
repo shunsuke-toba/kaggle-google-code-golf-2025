06a991038b3ead6acc1677015723222a5b4ad1ec
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 13:37:52 +0900

    タスク 086: 更新 (325 -> 321)

--- 086/3_submission.py
+++ 086/3_submission.py
@@ -2,9 +2,10 @@ def p(g):
  n=len(g:=eval(str(h:=g)))
  for t in range(n*n):
   y,x=t//n,t%n;b=h[y][x]
-  if b>0==h[y-1][x]+h[y][x-1]:
+  if b>h[y-1][x]+h[y][x-1]<1:
    s=h[y][x:].index(0);a=h[y+1][x+1];Y=y+s;X=x+s;S=s-2
    for r in g[y-S:Y+S]:r[x:X]=[b]*s
    for r in g[y:Y]:r[x-S:X+S]=[b]*(S*3+2)
-   for i in range(s):g[y][x+i]=g[Y-1][x+i]=g[y+i][x]=g[y+i][X-1]=a
+   g[y][x:X]=g[Y-1][x:X]=[a]*s
+   for r in g[y:Y]:r[x]=r[X-1]=a
  return g
