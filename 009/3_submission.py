15c8bee96ee0f218eacc833af0fc43c5812b628c
pech1 <pechi2327@gmail.com>
2025-08-25 00:50:15 +0900

    009:submissionの更新

--- 009/3_submission.py
+++ 009/3_submission.py
@@ -1,7 +1,1 @@
-def p(g):
- b=g[0][2]
- for _ in 0,1:
-  for r in g:
-   for c in{*r}-{0,b}:i=r.index(c)+2;j=-r[::-1].index(c)-2;r[i:j]=[[b,-c][x!=b]for x in r[i:j]]
-  g=[*map(list,zip(*g))]
- return[[*map(abs,r)]for r in g]
+f=lambda a:[[max(({*r[:j]}&{*r[j:]}-{r[2]})|{0})for j,_ in enumerate(r)]for r in a];p=lambda g:[[a or b|c for a,b,c in zip(r,h,v)]for r,h,v in zip(g,f(g),zip(*f(zip(*g))))]
