9e100122586fc395898c656e4ba9c097db557834
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 10:54:22 +0900

    タスク 361: 更新 (307 -> 302)

--- 361/3_submission.py
+++ 361/3_submission.py
@@ -4,10 +4,10 @@ def p(g,R=range):
   t=s-k+1
   for n in R(t*t):
    i,j=divmod(n,t)
-   if all(all(r[j:j+k])for r in g[i:i+k]):
+   if all(min(r[j:j+k])for r in g[i:i+k]):
     r=k/2-.5;i+=r;j+=r
     for n in R(s*s):
-     y,x=n//s,n%s;a,b=y-i,x-j
-     if v:=g[y][x]:
-      for m,n in(i-b,j+a),(i-a,j-b),(i+b,j-a):g[int(m)][int(n)]=v
+     if v:=g[y:=n//s][x:=n%s]:
+      a,b=y-i,x-j
+      for m,n in(-b,a),(-a,-b),(b,-a):g[int(i+m)][int(j+n)]=v
     return g
