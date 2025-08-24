d4ec8c8f21b001ece5cf83cfeffdd5907a02e7a7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:14 +0900

    タスク 234: 更新 (162 -> 161)

--- 234/3_submission.py
+++ 234/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
  for _ in[0]*16:
   s={0}
-  for r in g:s|=set(r);len(s)==3*sum(map(bool,r))and[g.remove(r),g.append([0]*len(r))]
+  for r in g:s|={*r};len(s)==3*sum(map(bool,r))and(g.append([0]*len(r)),g.remove(r))
   g=[*map(list,zip(*g[::-1]))]
  return g
