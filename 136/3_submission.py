3e90b58cb20d2de05d9a52438aa7ab9948f2e887
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 02:03:30 +0900

    タスク 136: 更新 (154 -> 118)

--- 136/3_submission.py
+++ 136/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- n=len(g)
  for k in 1,2:
-  r,c=divmod(next(i for i in range(n*n)if g[i//n][i%n]==k),n)
-  while-1<r<n>c>-1:g[r][c]=k;r+=2*k-3;c+=2*k-3
+  r,c=divmod(sum(g,[]).index(k),10)
+  while r<10>c>-1<r:g[r][c]=k;r+=2*k-3;c+=2*k-3
  return g
