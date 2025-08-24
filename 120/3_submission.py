77da576cec385a767d941560d1e59ec8f809b1d9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 01:02:00 +0900

    タスク 120: 更新 (160 -> 142)

--- 120/3_submission.py
+++ 120/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[[8 if 0<r<len(g)-1 and 0<c<len(row)-1 and g[r-1][c]==g[r+1][c]==row[c-1]==row[c+1]==a>0 else a for c,a in enumerate(row)]for r,row in enumerate(g)]
+p=lambda g:[[8*(0<j<len(r)-1>0<i<len(g)-1 and g[i-1][j]==g[i+1][j]==r[j-1]==r[j+1]==a>0)or a for j,a in enumerate(r)]for i,r in enumerate(g)]
