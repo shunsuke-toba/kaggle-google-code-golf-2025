f2f8d40c87eb841d0d117c4ae1bf9f0b64a2b762
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 23:43:14 +0900

    タスク 183: 更新 (114 -> 111)

--- 183/3_submission.py
+++ 183/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(n:=len(g)-4,c:=g[::n+3],r:=range(n),[[c[i>=n/2][-(j>=n/2)]*(g[i+2][j+2]>7)for j in r]for i in r])[-1]
+p=lambda g:(n:=len(g)-4,c:=g[::n+3],r:=range(n))and[[c[i>=n/2][(j<n/2)-1]*(g[i+2][j+2]>7)for j in r]for i in r]
