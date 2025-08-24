c8f66b6671e0a1466ec5e9d9194bb43f34fd71d9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:23 +0900

    タスク 069: 更新 (227 -> 193)

--- 069/3_submission.py
+++ 069/3_submission.py
@@ -1,11 +1,9 @@
 def p(g):
- g=sum(g,[]);r=range(100);p=[]
+ g=sum(g,[]);p=[];r=range(100)
  for i in r:
-  if(v:=g[i])%8:p+=[(i,v)];g[i]=0
- a=p[0][0]
+  if(v:=g[i])%8:p+=i*10+v,;g[i]=0
+ a=p[0]//10
  for i in r:
-  try:
-   if{g[i+d-a]for d,_ in p}=={8}:
-    for d,v in p:g[i+d-a]=v
-  except:0
+  if g[i]==8:
+   for j in p:g[i+j//10-a]=j%10
  return[g[i:i+10]for i in r[::10]]
