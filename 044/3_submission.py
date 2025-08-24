16bc92005aadcd5b7118337a7e4dbf331aa8b2ed
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 19:17:22 +0900

    タスク 044: 更新 (462 -> 454)

--- 044/3_submission.py
+++ 044/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- g=sum(g,[]);r=g[:];a={};b={}
+ g=sum(g,[]);r=g[:];a={};b=()
  for i,c in enumerate(g):
   if c^5:
    q=[i];g[i]=5;f=c<1
@@ -9,11 +9,11 @@ def p(g):
       d=g[m]
       if d==c:g[m]=5;q+=m,
       elif f*d^5:f=0
-     elif f:f=0
-   s=tuple(x-i for x in q)
-   if f:a[s]=i
-   elif c and r.count(c)==len(q)>1:b[c]=(s,q)
- for c,(s,q)in b.items():
+     else:f=0
+   t=tuple(x-i for x in q)
+   if f:a[t]=i
+   elif c and r.count(c)==len(q)>1:b+=((t,q,c),)
+ for s,q,c in b:
   if s in a:
    for n in q:r[n]=0;r[n+a[s]-q[0]]=c
  return[*map(list,zip(*[iter(r)]*10))]
