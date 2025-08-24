22cdea80cd3be3c1c6d893abc907e0788f5b1161
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 11:56:41 +0900

    タスク 228: 更新 (224 -> 204)

--- 228/3_submission.py
+++ 228/3_submission.py
@@ -1,5 +1,4 @@
-J=lambda a:(b:=[i for i,r in enumerate(a)if any(r)])and(b[0],b[-1])
 def p(a):
- c,e=J(a);k,w=J(zip(*a))
+ b=[(i//10,i%10)for i in range(100)if a[i//10][i%10]];c,k=b[0];e,w=b[-1]
  for i in 1,-1:a[c+i][k+i],a[e+i][w+i],a[c+i][w-i],a[e+i][k-i]=a[e+i][w+i],a[c+i][k+i],a[e+i][k-i],a[c+i][w-i]
  return a
