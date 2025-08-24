15fe199b7a47de25e65b2456f5a873c84b267c49
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 16:00:32 +0900

    タスク 340: 更新 (186 -> 184)

--- 340/3_submission.py
+++ 340/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  for r in g[1:-1]:
-  for j in range(1,len(r)-1):
-   c=r[j];r[j]=0
+  for j,c in enumerate(r[2:-2],2):
+   r[j]=0
    if c==r[0]:r[1]=c
    if c==r[-1]:r[-2]=c
    if c==g[0][j]:g[1][j]=c
