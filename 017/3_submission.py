2df44747ed70f29ebf14d0e63a1c33ee4bbdedd8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:24 +0900

    タスク 017: 更新 (166 -> 159)

--- 017/3_submission.py
+++ 017/3_submission.py
@@ -1,2 +1,1 @@
-def p(g,s=1):
- q=range(len(g));d={};return[[d[i%s,j%s]for j in q]for i in q]if all((v:=g[i][j])<1 or d.setdefault((i%s,j%s),v)==v for j in q for i in q)else p(g,s+1)
+def p(g,s=1):r=range(21);d={};return all((v:=g[i][j])<1 or d.setdefault((i%s,j%s),v)==v for j in r for i in r)and[[d[i%s,j%s]for j in r]for i in r]or p(g,s+1)
