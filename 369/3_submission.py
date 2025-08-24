e23a6ceea6c1d52909b1c7720944bb7ed4a2fd35
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 01:02:03 +0900

    タスク 369: 更新 (210 -> 209)

--- 369/3_submission.py
+++ 369/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for n in range(100):
-  for x,y in(q:=[divmod(n,10)][g[n//10][n%10]:]):g[x][y]=1;q+=[(a,b)for d in(1,-1)for a,b in((x+d,y),(x,y+d))if 9>=a>=0<=b<=9>g[a][b]*9]
+  for x,y in(q:=[divmod(n,10)][g[n//10][n%10]:]):g[x][y]=1;q+=[(a,b)for d in(1,-1)for a,b in((x+d,y),(x,y+d))if 10>a>-1<b<10>g[a][b]<1]
   for x,y in q:g[x][y]=4-len(q)
  return g
