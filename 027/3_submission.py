639e3b913c11a6362575a70db235e13506131cf9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:39 +0900

    タスク 027: 更新 (173 -> 170)

--- 027/3_submission.py
+++ 027/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- x=[(r,c)for r in range(1,10)for c in range(9)if g[r][c]];k=min((9,10),key=lambda k:sum(g[c][k-r]<1 for r,c in x))
+ x=[(r,c)for r in range(1,10)for c in range(9)if g[r][c]];k=min(9,10,key=lambda k:sum(g[c][k-r]<1 for r,c in x))
  for r,c in x:g[c][k-r]=g[c][k-r]or 2
  return g
