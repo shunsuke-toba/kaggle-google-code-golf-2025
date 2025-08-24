3585e8dae4f899119e246088ac95e4e63eca1532
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 16:00:24 +0900

    タスク 048: 更新 (161 -> 153)

--- 048/3_submission.py
+++ 048/3_submission.py
@@ -1,9 +1,7 @@
-def p(g,o=0):
- w=len(g[0])+2
- f=[0]*w
+def p(g):
+ w=len(g[0])+2;f=[0]*w
  for r in g:f+=0,*r,0
- f+=f[:w]
- s=[f.index(2)]
+ f+=f[:w];s=[f.index(2)]
  for i in s:
-  if(v:=f[i]):f[i]=0;s+=i+1,i-1,i+w,i-w;o+=v<3
- return[[o&8]]
+  if f[i]:f[i]=0;s+=i+1,i-1,i+w,i-w
+ return[[8-(2 in f)*8]]
