8913a99f30a3a5b6947d802fd5476c9529d2fffa
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:38 +0900

    タスク 004: 更新 (153 -> 142)

--- 004/3_submission.py
+++ 004/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
  d={}
  for r in g[::-1]:
-  for j in range(len(r))[::-1]:
-   if v:=r[j]:
-    R,c=d.setdefault(v,(r,j))
-    r[j]=0;r[j+(r!=R)*(j<c)]=v
+  j=len(r)
+  while j:
+   j-=1
+   if v:=r[j]:R,c=d.setdefault(v,(r,j));r[j]=0;r[j+(r!=R)*(j<c)]=v
  return g
