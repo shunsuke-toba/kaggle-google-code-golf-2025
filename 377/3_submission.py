d0951e8137668143b5d878334c6c5386499c5bb8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:34 +0900

    タスク 377: 更新 (98 -> 87)

--- 377/3_submission.py
+++ 377/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):h=g[:1];[h.append(r)for r in g if r!=h[-1]];return g==h and h or p([*map(list,zip(*h))])
+def p(g):h=g[:1];h.extend(r for r in g if r!=h[-1]);return g==h and h or p([*zip(*h)])
