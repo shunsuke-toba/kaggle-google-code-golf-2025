b356a9a1082a0e6431740fbc293b995137fbd32a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:46 +0900

    タスク 319: 更新 (443 -> 437)

--- 319/3_submission.py
+++ 319/3_submission.py
@@ -1,16 +1,15 @@
 def p(g,R=range,L=len):
  s=sum(g,[])
- C=sorted({*s},key=s.count)
- P=[]
- for c in C[:-1]:
-  h=g
+ C=sorted({*s},key=s.count);z=C.pop()
+ def F(c,h=g):
   for _ in R(4):
-   while c not in h[-1]:h=h[:-1]
+   while c not in h[0]:h=h[1:]
    h=[*zip(*h[::-1])]
-  P+=[[[(x==c)*c for x in r]for r in h]]
- for q in P:
-  for r in P:
-   for i in R(L(q)*2-L(r)+1):
-    for j in R(L(q[0])*2-L(r[0])+1):
-     if q!=r and all((q[i+x>>1][j+y>>1]>0)==(r[x][y]>0)for x in R(L(r))for y in R(L(r[0]))):
-      return[[x or C[-1] for x in t]for t in q]
+  return[[x==c for x in r]for r in h]
+ for c in C:
+  for d in C:
+   q,r=F(c),F(d);A=L(r);B=L(r[0])
+   for i in R(L(q)*2-A+1):
+    for j in R(L(q[0])*2-B+1):
+     if c-d and all(q[i+x>>1][j+y>>1]==r[x][y]for x in R(A)for y in R(B)):
+      return[[[z,c][x]for x in t]for t in q]
