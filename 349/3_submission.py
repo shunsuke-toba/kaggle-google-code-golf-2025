331263cf4c1b2a36565e3e373001671d1e74355d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 16:00:32 +0900

    タスク 349: 更新 (321 -> 318)

--- 349/3_submission.py
+++ 349/3_submission.py
@@ -1,9 +1,9 @@
 def p(g,R=range):
  n=len(o:=eval(str(g)))
  for m in R(n*n):
-  i=m//n;u=g[i];j=m%n;a=b=i
+  u=g[i:=m//n];j=m%n
   if u[j]>g[i-1][j]*i|u[j-1]*j:
-   a=(u+[0]).index(0,j)-j
+   a=(u+[0]).index(0,j)-j;b=i
    while b<n>g[b][j]>0:b+=1
    r=a>>1;l=max(j-r,0)
    for y in o[b:]:y[j:j+a]=[1]*a
