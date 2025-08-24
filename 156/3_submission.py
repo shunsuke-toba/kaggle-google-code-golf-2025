eb8cc88dfb9f2c235f4cc555aa0de2306a08bc0f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:43 +0900

    タスク 156: 更新 (229 -> 226)

--- 156/3_submission.py
+++ 156/3_submission.py
@@ -3,7 +3,7 @@ def p(g):
  for y,r in enumerate(g):
   if s:=sum(r)//4:t+=y,r.index(4),s
   elif t:t=b
- if len(a)*a[2]>len(b)*b[2]:a,b=b,a
- for t,c in(a,1),(b,2):
+ c=(len(a)*a[2]>len(b)*b[2])+1
+ for t,c in(a,c),(b,3-c):
   for r in g[-~t[0]:t[-3]]:r[t[1]+1:t[1]+t[2]-1]=[c]*(t[2]-2)
  return g
