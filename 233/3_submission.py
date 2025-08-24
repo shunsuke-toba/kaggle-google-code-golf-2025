79f7affb8af4e719b201dca5f6d63500ab8f2bd8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:26 +0900

    タスク 233: 更新 (505 -> 501)

--- 233/3_submission.py
+++ 233/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
- R=range;T=R(3);f=filter;t=[]
- for y in R(len(g)-2):
-  for x in R(len(g[0])-2):
+ R=range;T=R(3);f=filter;L=len;t=[]
+ for y in R(L(g)-2):
+  for x in R(L(g[0])-2):
    b=[g[y+i][x:x+3]for i in T];s={*sum(b,[])}
    if{2}<s<s|{0}:
     t+=(-str(b).count('2'),b),
@@ -9,7 +9,7 @@ def p(g):
  g=[*map(list,zip(*f(any,zip(*f(any,g)))))]
  for _,b in sorted(t):
   for _ in R(4):
-   try:y,x=next((y,x)for y in R(len(g)-2)for x in R(len(g[0])-2)if all((b[i][j]==2)==(g[y+i][x+j]<1)for i in T for j in T));break
+   try:y,x=next((y,x)for y in R(L(g)-2)for x in R(L(g[0])-2)if all(g[y+i][x+j]==2*(b[i][j]!=2)for i in T for j in T));break
    except:b=[*zip(*b[::-1])]
   for i in T:g[y+i][x:x+3]=b[i]
  return g
