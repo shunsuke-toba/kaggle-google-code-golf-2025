3369b0ed1a52a86ec57986073c0793041d6ea282
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:30 +0900

    タスク 396: 更新 (271 -> 267)

--- 396/3_submission.py
+++ 396/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- s=sum(g,[]);o,f=sorted({*s}-{0},key=s.count)
- w,c,b=max((len(e),-j,t.find(e))for j,r in enumerate(g)for t in[bytes(x==f for x in r)]for e in t.split(b'\0'));a=c=-c
- while g[c:]and g[c][b]==f:c+=1
- return[(h:=[o]*w),*[[o,*r[b+1:b+w-1],o]for r in g[a+1:c-1]],h]
+ s=sum(g,[]);o,f,_=sorted({*s},key=s.count)
+ w,c,b=max((len(e),-j,t.find(e))for j,r in enumerate(g)for t in[bytes(x==f for x in r)]for e in t.split(b'\0'));t=-c
+ while g[t:]and g[t][b]==f:t+=1
+ return[[o]*w,*[[o,*r[b+1:b+w-1],o]for r in g[-c+1:t-1]],[o]*w]
