da63395bbecf6df845b5d268d0f26d7336ada4ef
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:37 +0900

    タスク 040: 更新 (131 -> 125)

--- 040/3_submission.py
+++ 040/3_submission.py
@@ -1,6 +1,3 @@
 def p(g):
- for y in (r:=range(10)):
-  for x in r:
-   if g[y][x]==3:g[y][x]=(g[0][0],g[9][9])[(x,y)[g[0][0]!=g[9][0]]>4]
- return g
+ e=enumerate;return [[(g[0][0],g[9][9])[(x,y)[g[0][0]!=g[9][0]]>4]if c==3 else c for x,c in e(r)]for y,r in e(g)]
 
