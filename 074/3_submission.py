b77c9a4d13b5f124c228143666cb1cb6cc8ba967
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:08 +0900

    タスク 074: 更新 (128 -> 124)

--- 074/3_submission.py
+++ 074/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):R=range(30);return[[min(g[a][b]for Y in(y,31^y)for X in(x,31^x)for a,b in((Y,X),(X,Y))if a<30>b)for x in R]for y in R]
+p=lambda g,R=range(30):[[min(g[a][b]for Y in(y,31^y)for X in(x,31^x)for a,b in((Y,X),(X,Y))if a<30>b)for x in R]for y in R]
