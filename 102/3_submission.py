5ffac235858b1275481b9b5c0f6d1e51c2df612a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:42 +0900

    タスク 102: 更新 (246 -> 243)

--- 102/3_submission.py
+++ 102/3_submission.py
@@ -1,9 +1,9 @@
 def p(g):
  r=range
- for s in r(2,12):
-  for y in r(12-s):
+ for s in r(2,6):
+  for y in(R:=r(12-s)):
    H=g[y+1:y+s]
-   for x in r(12-s):
-    if all(g[y][x+i]+g[y+s][x+i]+g[y+i][x]+g[y+i][x+s]>19for i in r(s+1))>sum(sum(m[x+1:x+s])for m in H):
+   for x in R:
+    if all(g[y][x+i]+g[y+s][x+i]+g[y+i][x]+g[y+i][x+s]>19for i in r(s+1))>any(5 in m[x+1:x+s]for m in H):
      for m in H:m[x+1:x+s]=[2]*~-s
  return g
