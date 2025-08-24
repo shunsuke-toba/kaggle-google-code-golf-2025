d3ff01a5a987ce217efdc366f3a3c20165ea04a1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:46 +0900

    タスク 352: 更新 (154 -> 141)

--- 352/3_submission.py
+++ 352/3_submission.py
@@ -1,5 +1,4 @@
 def p(g):
- for i in range(9**5):
-  h,w=len(g),len(g[0]);n,m,x,y=i%97%h,i%87%w,i%83%3-1,i%79%3-1
-  g[n][m]|=h>n+x>-1<m+y<w>2==g[n+x][m+y]>g[n][m]
+ for i,r in enumerate(g):
+  for j,v in enumerate(r):r[j]|=v<2 and any(2 in s[(j and j-1):j+2]for s in g[(i and i-1):i+2])
  return g
