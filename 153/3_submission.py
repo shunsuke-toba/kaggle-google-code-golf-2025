d1f3e0ab71cb45eb0e001af8a55974701a15f686
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 13:37:55 +0900

    タスク 153: 更新 (277 -> 276)

--- 153/3_submission.py
+++ 153/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- s=sum(g,[]);a,b={*s}-{0};f=lambda s:{(x-a,y-b)for a,b in[map(min,zip(*s))]for x,y in s};A,B=[f({divmod(i,10)for i,x in enumerate(s)if x==c})for c in(a,b)]
+ s=sum(g,[]);a,b={*s}-{0};f=lambda s:{(x-a,y-b)for a,b in[map(min,zip(*s))]for x,y in s};A,B=[f({divmod(i,10)for i in range(100)if s[i]==c})for c in(a,b)]
  if f({(i//3,i%3)for i in range(9)}-A)^B:a,b,A=b,a,B
  o=[[b]*3,[b]*3,[b]*3]
  for x,y in A:o[x][y]=a
