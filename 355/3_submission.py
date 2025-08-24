a41487da8a06623730ecc46056407e5f43d4ad37
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:47 +0900

    タスク 355: 更新 (158 -> 150)

--- 355/3_submission.py
+++ 355/3_submission.py
@@ -1,2 +1,2 @@
-f=lambda a,b:zip(*filter(lambda r:b in r,a))
-def p(g):s=sum(g,[]);return[[max({*s}-{c:=min(s,key=s.count)},key=lambda b:sum(r.count(c)for r in f(f(g,b),b)))]]
+f=lambda a,b:zip(*[r for r in a if b in r])
+def p(g):s=sum(g,[]);return[[max({*s}-{c:=min(s,key=s.count)},key=lambda b:sum(f(f(g,b),b),()).count(c))]]
