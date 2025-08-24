dd0177c7a88776832562c0b8fc4caf38e1be51ca
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:25 +0900

    タスク 030: 更新 (170 -> 153)

--- 030/3_submission.py
+++ 030/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):e=enumerate;t={v:~i+len(g)for i,r in e(g[::-1])for v in r};n=[[0]*len(r)for r in g];[n[i+t[1]-t[v]].__setitem__(j,v)for i,r in e(g)for j,v in e(r)if v];return n
+def p(g):e=enumerate;t={v:i for i,r in e(g)for v in r};n=[[0]*10for _ in g];[n[i+t[1]-t[v]].__setitem__(j,v)for i,r in e(g)for j,v in e(r)if v];return n
