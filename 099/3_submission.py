b3adbd1f4cebafcba30034727d2f7c780489efa4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:39 +0900

    タスク 099: 更新 (197 -> 182)

--- 099/3_submission.py
+++ 099/3_submission.py
@@ -1,9 +1,8 @@
 def p(g,r=range):
- h=g;g=[*map(list,g)]
+ h=[*map(list,g)]
  for c in r(60):
-  y,x=c//6,c%6;f=h[y][x+2]
+  y,x=c//6,c%6;f=g[y][x+2]
   if f>1:
-   for y in r(y-2-h[y-2][x],min(y+3,10)):
-    for X in r(x,x+5):
-     if h[y][X]^1:g[y][X]=f
- return g
+   for y in r(y-2-g[y-2][x],y+3-(y>7)):
+    for X in r(x,x+5):h[y][X]=g[y][X]or f
+ return h
