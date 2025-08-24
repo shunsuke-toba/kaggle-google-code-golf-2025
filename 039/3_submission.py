437349a1776f6ac2dd762a9c16772d702b5dbbd3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:37 +0900

    タスク 039: 更新 (121 -> 112)

--- 039/3_submission.py
+++ 039/3_submission.py
@@ -1,2 +1,1 @@
-E=enumerate
-def p(a):y,x=map(min,zip(*[(i,j)for i,r in E(a)for j,v in E(r)if v]));return[a[y+i][x:x+3]for i in range(3)]
+def p(a):y,x=map(min,zip(*(divmod(i,10)for i,v in enumerate(sum(a,[]))if v)));return[r[x:x+3]for r in a[y:y+3]]
