aef83c0d9ca56f9876f4cb176f4f6ceca2a9ce2c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:30 +0900

    タスク 265: 更新 (193 -> 192)

--- 265/3_submission.py
+++ 265/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for a,b,s,t in zip(g,g[1:],r:=[*map(list,g)],r[1:]):
   for j in range(17):
-   if(sum(a[j:j+2]+b[j:j+2])<1)*(j%16<1or a[j-1]+b[j-1]|a[j+2]-5|b[j+2]-5):s[j:j+2]=t[j:j+2]=2,2
+   if(sum(a[j:j+2]+b[j:j+2])<1)*(j%16<1or a[j-1]+b[j-1]|a[j+2]*b[j+2]-25):s[j:j+2]=t[j:j+2]=2,2
  return r
