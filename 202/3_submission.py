e44a6fbab5305621105bf31612d6adc012ac7b1a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:28 +0900

    タスク 202: 更新 (157 -> 151)

--- 202/3_submission.py
+++ 202/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,t=lambda a:[*zip(*a)]:len({*g[0]}-{0})>1 and t(p(t(g)))or[[0if any(s[i]<1 for s in g if max(s)==max(r))else v for i,v in enumerate(r)]for r in g]
+p=lambda g,t=lambda a:[*zip(*a)]:len({*g[0]}-{0})>1 and t(p(t(g)))or[[0if any(max(s)==max(r)>s[i]for s in g)else v for i,v in enumerate(r)]for r in g]
