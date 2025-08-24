0f28bd560fccc3f4269cf0b4a5fe1a6620528335
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:26 +0900

    タスク 201: 更新 (316 -> 315)

--- 201/3_submission.py
+++ 201/3_submission.py
@@ -5,5 +5,5 @@ def p(g):
    if r<S:S=r
    if k<J:J=k;t=v==g[a+1][d]
  o=[r[b:d+1]for r in g[a:c+1]]
- for i in range(~a+c):o[i+1][1:-1]=g[S+i][J:J+~b+d][::1-2*t]
+ for r,y in zip(o[1:-1],g[S:]):r[1:-1]=y[J:J+~b+d][::1-2*t]
  return o
