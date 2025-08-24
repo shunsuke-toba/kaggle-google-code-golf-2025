79f216cee308e8934715b3c252883f5830a5ab22
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:12 +0900

    タスク 206: 更新 (186 -> 179)

--- 206/3_submission.py
+++ 206/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
- s=t=a=b=99
- for i,r in enumerate(g):
-  for j,v in enumerate(r):
+ s=t=a=b=9;e=enumerate
+ for i,r in e(g):
+  for j,v in e(r):
    if v==5:s,t=i,j
-   elif v%5:a,b=min(a,i),min(b,j)
+   if v%5:a,b=min(a,i),min(b,j)
  for k in 0,1,2:g[s+k-1][t-1:t+2]=g[a+k][b:b+3]
  return g
