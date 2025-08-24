a5f0bf45cbf8afd7505a9702908c6c2e49291b6a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 02:03:30 +0900

    タスク 160: 更新 (161 -> 153)

--- 160/3_submission.py
+++ 160/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- for a,b,c in zip(g,g[1:],g[2:]):
-  for x in range(8):
-   if sum(a[x:x+3]+b[x:x+3]+c[x:x+3])-b[x+1]==8:a[x:x+3]=c[x:x+3]=0,2,0;b[x:x+3]=2,2,2
+ for z in range(64):
+  a,b,c=g[z>>3:][:3];x=z%8
+  if sum(a[x:x+3]+b[x:x+3]+c[x:x+3])-b[x+1]==8:a[x:x+3]=c[x:x+3]=0,2,0;b[x:x+3]=2,2,2
  return g
