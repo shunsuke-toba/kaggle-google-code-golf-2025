83b78c7efb26c0850f7286cfe0290ab6b8c3589a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 02:03:31 +0900

    タスク 162: 更新 (145 -> 140)

--- 162/3_submission.py
+++ 162/3_submission.py
@@ -1,7 +1,6 @@
-def p(j,A=range(18)):
- for c in A:
-  E,k,W=j[c:c+3]
-  for l in A:
-   s=l+3
-   if sum(E[l:s]+k[l:s]+W[l:s])<1:E[l:s]=k[l:s]=W[l:s]=3*[1]
- return j
+def p(g):
+ for t in zip(g,g[1:],g[2:]):
+  for l in range(18):
+   if sum(sum(r[l:l+3])for r in t)<1:
+    for r in t:r[l:l+3]=1,1,1
+ return g
