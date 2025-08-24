9e0549d5e04eafa7282c49b794141ad99139f061
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 01:01:59 +0900

    タスク 102: 更新 (235 -> 233)

--- 102/3_submission.py
+++ 102/3_submission.py
@@ -1,9 +1,9 @@
 def p(g):
- r=range
+ r=range;S=sum
  for L in r(3,7):
   for y in(R:=r(13-L)):
    M=g[y:y+L]
    for x in R:
-    if sum(sum(m[x:x+L])for m in M)==20*~-L and sum(sum(m[x+1:x+L-1])for m in M[1:-1])<1:
+    if S(S(m[x:x+L])for m in M)==20*~-L and S(S(m[x+1:x+L-1])for m in M[1:-1])<1:
      for m in M[1:-1]:m[x+1:x+L-1]=[2]*(L-2)
  return g
