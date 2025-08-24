e4c8511cf37575630909ba5cbbde4661052c9e6a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 01:02:00 +0900

    タスク 131: 更新 (203 -> 202)

--- 131/3_submission.py
+++ 131/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,t=lambda a:[*map(list,zip(*a))]:(w:=len(g[0]))>len(g)and t(p(t(g)))or[r:=g.index([2]*w),h:=[r for r in g if 3in r],h[0]in g[:r]and p(g[::-1])[::-1]or(g[:r+1]+h+[[8]*w]+g[:1]*15)[:len(g)]][-1]
+p=lambda g,t=lambda a:[*map(list,zip(*a))]:(w:=len(g[0]))>len(g)and t(p(t(g)))or(r:=g.index([2]*w),h:=[r for r in g if 3in r],h[0]in g[:r]and p(g[::-1])[::-1]or(g[:r+1]+h+[[8]*w]+g[:1]*15)[:len(g)])[2]
