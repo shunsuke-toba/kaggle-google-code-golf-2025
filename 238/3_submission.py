51be620bb949589fee25d509999fa41edbbc1ada
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 10:54:20 +0900

    タスク 238: 更新 (331 -> 328)

--- 238/3_submission.py
+++ 238/3_submission.py
@@ -4,7 +4,7 @@ def p(g):
   for j,v in f(r):
    if v&7:a=min(a,i);b=i;c=min(c,j)
    elif v:d=min(d,i);e=min(e,j)
- r=range(n:=b+~a);G=[x[c:c+n+2]for x in g[a:b+1]];k=G[0][1],G[1][-1],G[1][0],G[-1][1]
+ r=range(n:=b+~a);G=[x[c:c+n+2]for x in g[a:b+1]]
  for i in r:
-  for j in r:v=g[d+i][e+j];G[i+1][j+1]=v*(i-j)*~(i+j-n)and k[(i>j)*2+(i+j>=n)]or v
+  for j in r:v=g[d+i][e+j];G[i+1][j+1]=v*(i-j)*~(i+j-n)and(G[0][1],G[1][-1],G[1][0],G[-1][1])[(i>j)*2+(i+j>=n)]or v
  return G
