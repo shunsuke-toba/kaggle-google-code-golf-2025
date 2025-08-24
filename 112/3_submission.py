6aa43ce94d0b7e77ac8ee38d0d85d8def9ace1fd
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:39 +0900

    タスク 112: 更新 (171 -> 169)

--- 112/3_submission.py
+++ 112/3_submission.py
@@ -1,7 +1,8 @@
-def p(j,p=enumerate):
- for Y,r in p(j):
-  if 3in r:X=r.index(3)*2+1;Y=Y*2+1;break
- for y,r in p(j):
-  for x,l in p(r):
-   if l==2:r[X-x]=j[Y-y][x]=j[Y-y][X-x]=2
- return j
+def p(g,e=enumerate):
+ y=0
+ while 3not in g[y]:y+=1
+ X=g[y].index(3)*2+1;y=y*2+1
+ for Y,r in e(g):
+  for x,c in e(r):
+   if c%3:r[X-x]=g[y-Y][x]=g[y-Y][X-x]=2
+ return g
