e2f0f3424d5a8858e38734d18b63094d8db64193
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 02:03:28 +0900

    タスク 095: 更新 (116 -> 115)

--- 095/3_submission.py
+++ 095/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for z in range(81):
-  i,j=z//9,z%9
-  for x in(g[i][j]>4)*g[i-1:i+2]:x[j-1:j+2]=1,1,1;g[i][j]=5
+  i=z//9;z%=9
+  for x in(g[i][z]>4)*g[i-1:i+2]:x[z-1:z+2]=1,1,1;g[i][z]=5
  return g
