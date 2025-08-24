e8a8e86de06eb5a4ae7834c239133bd7e6c0d2b2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:45 +0900

    タスク 308: 更新 (331 -> 309)

--- 308/3_submission.py
+++ 308/3_submission.py
@@ -1,11 +1,11 @@
-def p(g,R=range,E=enumerate,A=abs,M=max):
- f=sum(g,[]);b=M(R(10),key=f.count);r=[0]*10;c=[0]*10;p=[]
+def p(g,E=enumerate,M=max):
+ f=sum(g,[]);b=M(f,key=f.count);s=[0]*20;p=[]
  for i,x in E(g):
   for j,v in E(x):
-   if v-b:r[v]+=i;c[v]+=j;p+=[(i,j,v)]
+   if v-b:s[v]+=i;s[v+10]+=j;p+=(i,j,v),
  m=0
  for k,(i,j,v) in E(p):
-  i-=r[v]>>2;j-=c[v]>>2;p[k]=i,j,v;m=M(m,A(i),A(j))
- k=2*m+1;o=[[b]*k for _ in R(k)]
+  i-=s[v]>>2;j-=s[v+10]>>2;p[k]=i,j,v;m=M(m,i,-i,j,-j)
+ k=2*m+1;o=[[b]*k for _ in[0]*k]
  for i,j,v in p:o[m+i][m+j]=v
  return o
