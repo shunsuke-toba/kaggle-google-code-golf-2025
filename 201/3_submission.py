0218d401038f0c9e33bc3193db572244fca31d5d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 13:37:56 +0900

    タスク 201: 更新 (344 -> 340)

--- 201/3_submission.py
+++ 201/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- W=len(g);f=sum(g,[]);(a,b),*_,(c,d)=[divmod(i,W)for i,v in enumerate(f)if v==4];s=[(u,v)for i,w in enumerate(f)if w*(w-4)and min((u:=i//W)-a,c-u,(v:=i%W)-b,d-v)<0];S,J=map(min,zip(*s));o=[r[b:d+1]for r in g[a:c+1]];f=next(g[i][J]==g[a+1][d]for i,j in s if j==J)
+ W=len(g);f=sum(g,[]);(a,b),*_,(c,d)=[divmod(i,W)for i,v in enumerate(f)if v==4];s=[(u,v)for i,w in enumerate(f)if w*(w-4)and min((u:=i//W)-a,c-u,(v:=i%W)-b,d-v)<0];S,J=map(min,zip(*s));o=[r[b:d+1]for r in g[a:c+1]];f=g[a+1][d]in(g[i][J]for i,j in s if j==J)
  for k in range(~a+c):o[k+1][1:-1]=g[S+k][J:J+~b+d][::1-2*f]
  return o
