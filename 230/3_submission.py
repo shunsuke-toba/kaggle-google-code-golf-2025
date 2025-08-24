22587f36a3278164c9a91a1c8bde19f79f040d87
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:29 +0900

    タスク 230: 更新 (163 -> 156)

--- 230/3_submission.py
+++ 230/3_submission.py
@@ -1,6 +1,5 @@
-def p(g,R=range):
- for y in R(len(g)-3):
-  a,b,c,d=g[y:y+4]
-  for x in R(len(g[0])-3):
-   if b[x+1:x+3]==c[x+1:x+3]==[5]*2:a[x:x+4:3]=1,2;d[x:x+4:3]=3,4
+def p(g):
+ for a,b,c,d in zip(g,g[1:],g[2:],g[3:]):
+  for x in range(len(a)-3):
+   if b[x+1:x+3]==c[x+1:x+3]==[5,5]:a[x:x+4:3]=1,2;d[x:x+4:3]=3,4
  return g
