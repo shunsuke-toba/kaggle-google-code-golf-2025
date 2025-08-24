d6e6cfef95ff3ad1931a783932b4ee0bab83fc1a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:24 +0900

    タスク 080: 更新 (365 -> 361)

--- 080/3_submission.py
+++ 080/3_submission.py
@@ -2,12 +2,12 @@ def p(g):
  R=range;n=0
  while len({*g[n]})>1:n+=1
  s=n+1;A=[r[::s]for r in g[::s]];m=len(A)
- P=min(((t:=A[y][x:x+3]+A[y+1][x:x+3]+A[y+2][x:x+3]).count(0),t)for y in R(m-2)for x in R(m-2))[1]
+ P=min(((t:=sum((A[y+i][x:x+3]for i in R(3)),[])).count(0),t)for y in R(m-2)for x in R(m-2))[1]
  for i in R(m*m):
   y,x=i//m,i%m
   if A[y][x]==P[4]:
    for k in R(9):
     u,v=y+k//3-1,x+k%3-1
-    if 0<=u<m>v>=0:
+    if-1<u<m>v>-1:
      for j in R(n):g[u*s+j][v*s:v*s+n]=[P[k]]*n
  return g
