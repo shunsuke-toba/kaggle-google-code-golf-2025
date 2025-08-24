df4615785d836bf0335c95822a63eb4ea22c6625
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 13:37:54 +0900

    タスク 143: 更新 (266 -> 258)

--- 143/3_submission.py
+++ 143/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
- r=range(9);s={i for i in r if g[i//3][i%3]};T=str(g);n=0
+ r=range(9);s={i for i in r if g[i//3][i%3]};n=0
  while 1:
   n+=1;y=n//8;x=n%8
-  if{j for j in r if g[y+j//3][x+j%3]}==s and len(c:={g[y+j//3][x+j%3]for j in s})<2 and T.count(str(c:=c.pop()))==len(s):
+  if{j for j in r if g[y+j//3][x+j%3]}==s and len(c:={g[y+j//3][x+j%3]for j in s})<2<str(g).count(str(c:=c.pop()))==len(s):
    for j in s:g[y+j//3][x+j%3]=5
    return g
