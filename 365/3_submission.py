0db5d66730654e65b39d206297bf9c9881d7898b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:34 +0900

    タスク 365: 更新 (148 -> 146)

--- 365/3_submission.py
+++ 365/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,R=range:max((F.count(2),len(F),S)for Y in R(11)for y in R(Y)for X in R(11)for x in R(X)if min(F:=sum(S:=[t[x:X]for t in g[y:Y]],[])))[2]
+p=lambda g,R=range:max((all(F:=sum(S:=[t[x:X]for t in g[y:Y]],[])),F.count(2),len(F),S)for Y in R(11)for y in R(Y)for X in R(11)for x in R(X))[3]
