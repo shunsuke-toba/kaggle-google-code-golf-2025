6b504acb3253f8b91b740860ddb919d4262a480e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:15 +0900

    タスク 272: 更新 (127 -> 122)

--- 272/3_submission.py
+++ 272/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[[c and-~(c<sum(r[j-(j>0):j+2]+[k[j]for k in g[i-1:i]+g[i+1:i+2]])) for j,c in enumerate(r)]for i,r in enumerate(g)]
+p=lambda g:[[c and-~(sum(r[j-(j>0):j+2]+[k[j]for k in g[i-(i>0):i+2]])>4)for j,c in enumerate(r)]for i,r in enumerate(g)]
