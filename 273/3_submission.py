102dd576a4fb9372ea6d2f66a5c72613a1e83e58
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 02:03:33 +0900

    タスク 273: 更新 (165 -> 163)

--- 273/3_submission.py
+++ 273/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- s={divmod(i,10)for i,v in enumerate(sum(g,[]))if v>3}
+ s={divmod(i,10)for i,v in enumerate(sum(g,[]))if v}
  for a,b in s:
   for c,d in s:
    for r in g[a+1:c]*({(a,d),(c,b)}<=s):r[b+1:d]=[2]*(~b+d)
