f54932d8be15dc75f7e2a1d89045211fba23c0e2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:08 +0900

    タスク 081: 更新 (130 -> 128)

--- 081/3_submission.py
+++ 081/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  for i in range(36):
-  if 24==sum(l:=g[y:=i//6][(x:=i%6):x+2]+g[y+1][x:x+2]):g[y+(i:=l.index(0))//2][x+i%2]=1
+  if sum(l:=g[y:=i//6][(x:=i%6):x+2]+g[y+1][x:x+2])>23:i=l.index(0);g[y+i//2][x+i%2]=1
  return g
