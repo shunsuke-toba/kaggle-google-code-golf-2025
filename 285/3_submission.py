bd5473ed788a06a160932a7deb256607b2222b61
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:28 +0900

    タスク 285: 更新 (542 -> 541)

--- 285/3_submission.py
+++ 285/3_submission.py
@@ -2,14 +2,16 @@ def p(g):
  o=[*map(list,g)];n=len(g);d=[(i//3-1,i%3-1)for i in range(9)if i^4]
  for k in range(n*n):
   if(c:=o[i:=k//n][j:=k%n])>0:
-   q=[(i,j)];C=c,;o[i][j]=-c
+   q=[(i,j)];o[i][j]=-c
    for y,x in q:
     for a,b in d:
-     if n>(Y:=y+a)>-1<(X:=x+b)<n and(c:=o[Y][X])>0:o[Y][X]=-c;q+=(Y,X),;C+=c,
-   for(y,x),v in zip(q,C):
-    if C.count(v)-1 and any(-v!=o[y+a][x+b]!=0 for a,b in d if a*b==0):
-     p=[(r-y,c-x)for(r,c),w in zip(q,C)if w==v];break
+     if n>(Y:=y+a)>-1<(X:=x+b)<n and o[Y][X]==c:o[Y][X]=-c;q+=(Y,X),
+   if len(q)<2:continue
+   for y,x in q:
+    if any(-c!=o[y+a][x+b]!=0 for a,b in d if a*b==0):
+     p=[(r-y,c-x)for r,c in q];break
    for a,b in d:
-    if 0>(c:=o[y+a][x+b])!=-v:
-     for u,w in p:g[y+a+(u,-u)[a&1]][x+b+(w,-w)[b&1]]=-c
+    if(v:=o[y+a][x+b])and v!=-c:
+     if v>0:o[y+a][x+b]=-v
+     for u,w in p:g[y+a+(u,-u)[a&1]][x+b+(w,-w)[b&1]]=abs(v)
  return g
