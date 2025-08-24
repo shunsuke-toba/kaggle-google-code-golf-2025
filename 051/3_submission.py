a5735538430d4bab626c32812031f92fb87a671d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 13:37:51 +0900

    タスク 051: 更新 (199 -> 188)

--- 051/3_submission.py
+++ 051/3_submission.py
@@ -1,8 +1,7 @@
-def p(g,R=range):
+def p(g,r=range):
  for _ in[0]*4:
-  s=sum(g,[])
-  for r in g:
-   for y in R(len(r)-1):
-    if s.count(c:=r[y])<2>1>r[y+1]:r[:y]=[(d:=r[i])or c for i in R(y)]
+  for a in g:
+   for y in r(len(a)-1):
+    if sum(g,[]).count(c:=a[y])<2>1>a[y+1]:a[:y]=[a[i]or c for i in r(y)]
   g=[*map(list,zip(*g[::-1]))]
  return g
