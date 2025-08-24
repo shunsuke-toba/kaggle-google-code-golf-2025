f768efcc2168893e01b7759727b9b166ba44b95b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 20:23:30 +0900

    タスク 356: 更新 (168 -> 167)

--- 356/3_submission.py
+++ 356/3_submission.py
@@ -3,6 +3,6 @@ def p(g):
   for r in g:
    a=9
    for i in range(10):
-    if r[i]==8:r[a+1:i]=[9]*(i+~a);a=i
+    if r[i]==8:r[a:i]=[9]*(i-a);a=i+1
   g=[*map(list,zip(*g))]
  return[[x&8 for x in r]for r in g]
