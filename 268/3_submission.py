75bc87c7131f0d711b80d6b6a58c738e42fc5b65
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:27 +0900

    タスク 268: 更新 (374 -> 370)

--- 268/3_submission.py
+++ 268/3_submission.py
@@ -1,14 +1,14 @@
 def p(g):
  n=len(g);R=range;r=lambda z:[*map(list,zip(*z[::-1]))]
  for k in R(4):
-  s=[(i,j)for i in R(n)for j in R(n)if g[i][j]]
-  a,c=s[0];b,d=s[-1]
+  s=[(i,j)for i in R(n)for j in R(n)if g[i][j]];a,c=s[0];b,d=s[-1]
   if 0 in g[a][c:d]:break
   g=r(g)
  for e in g[a+1:b]:e[c+1:d]=[4]*(d-c-1)
  for e in g[:b]:e[c+2:d-1]=[4]*(d-c-3)
  for j in R(a):
-  if j<c+2:g[a-1-j][c+1-j]=4
-  if j<=n-d:g[a-1-j][d-1+j]=4
+  h=g[a+~j]
+  if j<c+2:h[c+1-j]=4
+  if j<=n-d:h[d-1+j]=4
  for _ in R(4-k):g=r(g)
  return g
