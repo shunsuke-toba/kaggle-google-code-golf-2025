2938df53316373596c8c32fd3463e1e3ccea4a8b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:38 +0900

    タスク 098: 更新 (157 -> 150)

--- 098/3_submission.py
+++ 098/3_submission.py
@@ -1,5 +1,4 @@
 def p(g):
- h=[r[:]for r in g]
- for a,b,c,d in zip(h,h[1:],h[2:],g[1:-1]):
-  for j in range(1,len(b)-1):d[j]=b[j]*(a[j]+c[j]+b[j-1]+b[j+1]!=4*b[j])
+ for a,b,c,d in zip(h:=eval(str(g)),h[1:],h[2:],g[1:-1]):
+  for j,x in enumerate(b[1:-1],1):d[j]=x*(a[j]+c[j]+b[j-1]+b[j+1]!=4*x)
  return g
