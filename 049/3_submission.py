38e336f8d64bbc58022801d3b8e634bbdaa13d31
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:07 +0900

    タスク 049: 更新 (111 -> 107)

--- 049/3_submission.py
+++ 049/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(s:=sum(g,[]),c:=min({*s}-{0},key=s.count),w:=max(r.count(c)for r in g),[[c]*w]*(s.count(c)//w))[3]
+p=lambda g:(s:=sum(g,[]),c:=min({*s}-{0},key=s.count),h:=sum(c in r for r in g),[[c]*(s.count(c)//h)]*h)[3]
