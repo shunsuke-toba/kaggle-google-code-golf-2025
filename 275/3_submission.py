19ed4480227445ceb4091fde26c0ca7683dd2c65
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:15 +0900

    タスク 275: 更新 (183 -> 169)

--- 275/3_submission.py
+++ 275/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- n=min(len(g),len(g[0]));p,c=[r[:n]for r in g[:n]],[r[-n:]for r in g[-n:]]
- if'8'in str(p):p,c=c,p
- g=range(n*n);return[[p[i//n][j//n]*c[i%n][j%n]//8 for j in g]for i in g]
+ n=min(len(g),len(g[0]));a=[r[:n]for r in g[:n]];b=[r[-n:]for r in g[-n:]]
+ if'8'in str(a):a,b=b,a
+ return[[x*y//8 for x in r for y in s]for r in a for s in b]
