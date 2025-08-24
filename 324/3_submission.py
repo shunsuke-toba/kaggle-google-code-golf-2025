d5a5218a267d9116d50cb47a615e4f5ea2107898
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:29 +0900

    タスク 324: 更新 (362 -> 318)

--- 324/3_submission.py
+++ 324/3_submission.py
@@ -1,9 +1,8 @@
-def p(g,R=range):
- f=sum(g,[]);a=max(g[0][:2]+g[1][:2],key=f.count);b=max({*f}-{a},key=f.count)
- s=[];m=[0]*10
- for i,r in enumerate(g):
-  for j,d in enumerate(r):
+def p(g):
+ f=sum(g,[]);a=max(g[0][:2]+g[1][:2],key=f.count);b=max({*f}-{a},key=f.count);s=[];m={};e=enumerate
+ for i,r in e(g):
+  for j,d in e(r):
    if d in(a,b):continue
-   s+=i+j,60+i-j,
-   m[a if a in r and a in[t[j]for t in g]else b]=d
- return [[(d,m[d])[d in(a,b)and(i+j in s or 60+i-j in s)]for j,d in enumerate(g[i])]for i in R(len(g))]
+   s+=i+j,60+i-j
+   m[(b,a)[a in r and a in[t[j]for t in g]]]=d
+ return [[(d,m.get(d,d))[i+j in s or 60+i-j in s]for j,d in e(r)]for i,r in e(g)]
