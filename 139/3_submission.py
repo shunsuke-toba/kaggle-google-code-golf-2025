8d42b8997ba90ed5522b68c173a5596d9e54bec3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 02:03:30 +0900

    タスク 139: 更新 (148 -> 121)

--- 139/3_submission.py
+++ 139/3_submission.py
@@ -1,5 +1,6 @@
 def p(g):
- s=[(1,0),(4,5)]if 4 not in g[8]else[(1,2),(6,5)]
- for r,c in s:
-  for i in 0,1,2:g[r+i][c:c+3]=[v or 7 for v in g[r+i][c:c+3]]
+ a=any(g[8])
+ for r,c in (1,2*a),(4+2*a,5):
+  for R in g[r:r+3]:R[c:c+3]=[x or 7 for x in R[c:c+3]]
  return g
+
