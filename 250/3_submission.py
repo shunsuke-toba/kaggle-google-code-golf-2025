3ff439e2e6daa8eba20c74858092de9b86ade5b9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:30 +0900

    タスク 250: 更新 (183 -> 175)

--- 250/3_submission.py
+++ 250/3_submission.py
@@ -1,6 +1,4 @@
 def p(g):
- r,c=divmod(sum(g,[]).index(2),10)
- o=[[v&2 for v in r]for r in g]
- for k in range(100):
-  if g[i:=k//10][j:=k%10]>4:o[min(max(i,r-1),r+2)][min(max(j,c-1),c+2)]=5
+ a=sum(g,[]);r,c=divmod(a.index(2),10);o=[[c&2 for c in r]for r in g]
+ for k in range(100):o[min(max(k//10,r-1),r+2)][min(max(k%10,c-1),c+2)]+=a[k]>4and 5
  return o
