cd7e29dea4b6b096f2b2d935a8999eed5c02823a
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-25 03:02:05 +0900

    085: 改善(112 -> 104)

--- 085/3_submission.py
+++ 085/3_submission.py
@@ -1,5 +1,4 @@
 def p(g):
- for a,b,c in zip(g,g[1:],g[2:]):
-  i=p=0
-  while b[i:]:b[i]*=p^1;p=a[i]==b[i]==c[i]>0;i+=1
+ for a,b in zip(g,g[1:]):
+  for i in range(len(b)):b[i]*=(a[i-1]==a[i]==b[i-1]>0)^1
  return g
