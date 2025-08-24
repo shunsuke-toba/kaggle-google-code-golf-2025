219b49d374ff68a0d81178d1757521c96e9eb6a6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 11:56:42 +0900

    タスク 364: 更新 (396 -> 376)

--- 364/3_submission.py
+++ 364/3_submission.py
@@ -3,12 +3,10 @@ def p(g):
  for k in range(h*w):
   i=k//w;j=k%w
   if g[i][j]:
-   s=[(i,j)];g[i][j]=0
+   s=[(i,j)];g[i][j]=0;m=t=0
    for x,y in s:
     for a,b in (x+1,y),(x-1,y),(x,y+1),(x,y-1):
      if h>a>-1<b<w>0<g[a][b]:g[a][b]=0;s+=(a,b),
-   m=t=0
-   for x,y in s:
     a=(x+1,y)in s;b=(x-1,y)in s;c=(x,y+1)in s;d=(x,y-1)in s
     m|=a+b+c+d>2;t+=a+b==c+d==1
    n=m*2 or 1+5*(t>1)
