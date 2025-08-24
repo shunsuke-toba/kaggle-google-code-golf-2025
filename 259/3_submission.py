9a1bc66b2afeadfe71156ba3ac90df595d8d412c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 20:23:27 +0900

    タスク 259: 更新 (147 -> 137)

--- 259/3_submission.py
+++ 259/3_submission.py
@@ -1,1 +1,6 @@
-def p(g):E=enumerate;a,b=zip(*((i,j)for i,r in E(g)for j,c in E(r)if c>1));return[[c*(c>1)for c in r[min(b):-~max(b)]]for r in g[min(a):-~max(a)]]
+def p(g):
+ for _ in 0,1:
+  while max(g[0])<2:g=g[1:]
+  while max(g[-1])<2:g=g[:-1]
+  g=[*zip(*g)]
+ return[[c*(c>1)for c in r]for r in g]
