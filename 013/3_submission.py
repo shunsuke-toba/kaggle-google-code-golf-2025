91c62ff4adb3f6269f389c4839f786e3b9cfb1a0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 13:37:46 +0900

    タスク 013: 更新 (233 -> 213)

--- 013/3_submission.py
+++ 013/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- (y,a),(Y,b)=[(j,v)for r in g for j,v in enumerate(r)if v];w=len(g[0])
- if{y,Y}<={0,w-1}:return[*map(list,zip(*p([*map(list,zip(*g))])))]
- if y>Y:y,Y,a,b=Y,y,b,a
- for r in g:d=Y-y;r[y::d]=([a,b]*9)[:(w-y+d-1)//d]
+ (y,a),(Y,b)=sorted((j,v)for r in g for j,v in enumerate(r)if v);w=len(g[0])-1
+ if{y,Y}<={0,w}:return[*map(list,zip(*p([*map(list,zip(*g))])))]
+ for r in g:d=Y-y;r[y::d]=([a,b]*8)[:(w-y+d)//d]
  return g
