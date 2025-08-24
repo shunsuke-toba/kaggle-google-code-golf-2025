ecd89c91b18ffce117296d7134c9c801a767e1fc
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 10:54:17 +0900

    タスク 118: 更新 (421 -> 413)

--- 118/3_submission.py
+++ 118/3_submission.py
@@ -1,10 +1,9 @@
 def p(g):
  h=len(g);w=len(g[0]);u=set();f=lambda y,x:((a:=g[y][x])==2)-2*(a<1 or (y,x)in u);R=range;t=2
  while 1:
-  B=m=I=J=0
+  B=I=J=0
   for r in(3,2)[::t-1]:
-   for k in R(h*w):i=k//w;j=k%w;b=sum(f(a,j)for a in R(i-r,i+r+1)if-1<a<h)+sum(f(i,c)for c in R(j-r,j+r+1)if-1<c<w)-f(i,j);(b,-r)>(B,-m)and(B:=b,m:=r,I:=i,J:=j)
+   for k in R(h*w):i=k//w;j=k%w;b=sum(f(a,j)for a in R(i-r,i+r+1)if-1<a<h)+sum(f(i,c)for c in R(j-r,j+r+1)if-1<c<w)-f(i,j);(b,-r)>(B,-t)and(B:=b,t:=r,I:=i,J:=j)
   if B<1:return g
-  t=m
   for d in R(-t,t+1):
    for y,x in(I+d,J),(I,J+d):u|={(y,x)};g[y][x]+=3*(g[y][x]==5)
