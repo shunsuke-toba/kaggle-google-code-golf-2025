183a1d8cf5e2ca1acc98840efd5fc81f9a2f99ea
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-25 02:23:03 +0900

    110: 改善(157 -> 156)

--- 110/3_submission.py
+++ 110/3_submission.py
@@ -2,7 +2,7 @@ def p(g):
  t=[];o=[]
  for r in g:
   for s in t:
-   if all((a^b)*a*b<1 for a,b in zip(r,s)):break
+   if all((a^b)*a*b<1for a,b in zip(r,s)):break
   else:t+=[s:=r]
   o+=s,;s[:]=map(int.__or__,s,r)
  return o
