a7a31dbdf1a66a1ae9b9b19a7908f110c5ab087a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:09 +0900

    タスク 088: 更新 (130 -> 122)

--- 088/3_submission.py
+++ 088/3_submission.py
@@ -1,5 +1,3 @@
 def p(g):
- for _ in[0]*96:
-  if sum(g[-1])<1:g.pop()
-  g=[*zip(*g[::-1])]
+ for _ in[0]*44:sum(g[-1])or g.pop();g=[*zip(*g[::-1])]
  return[[c and g[0][0]for c in r[1:-1]]for r in g[1:-1]]
