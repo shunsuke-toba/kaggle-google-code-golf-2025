d956b9169d1edc19cceffb42103e1504ca8b4962
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 19:17:22 +0900

    タスク 054: 更新 (496 -> 478)

--- 054/3_submission.py
+++ 054/3_submission.py
@@ -1,15 +1,16 @@
 def p(g,R=range):
- s=sum(g,[]);b=s[0];a=max({*s}-{b},key=s.count);D=1,0,-1,0
+ s=sum(g,[]);b=s[0];D=1,0,-1,0;I=22,10,2,14
  for z in R(676):
   i=z//26;j=z%26;q=sum((r[j:j+5]for r in g[i:i+5]),[])
-  if{*q}-{a,b,q[12]}and q==q[::-1]:break
+  if q==q[::-1]and len({*q})>2:break
  for r in g[i:i+5]:r[j:j+5]=[b]*5
- for i,j in[(i,j)for i in R(30)for j in R(30)if g[i][j]==q[12]]:
+ c=q[12]
+ for i,j in[(i,j)for i in R(30)for j in R(30)if g[i][j]==c]:
   for t in R(25):
-   c=q[t];x=i+t//5-2;y=j+t%5-2
-   if g[x][y]!=b!=c:g[x][y]=c
+   v=q[t];x=i+t//5-2;y=j+t%5-2
+   if g[x][y]!=b!=v:g[x][y]=v
   for d in R(4):
-   x=i+D[d]*2;y=j+D[d-3]*2;c=g[x][y]
-   if a!=c!=b:
-    while g[x][y]-b:g[x][y]=c;x+=D[d];y+=D[d-3]
+   v=q[I[d]];x=i+D[d]*2;y=j+D[d-3]*2
+   if v-b:
+    while g[x][y]-b:g[x][y]=v;x+=D[d];y+=D[d-3]
  return g
