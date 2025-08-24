ff535c658ea9934e2c9b449a77261facac8fdbca
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 11:56:37 +0900

    タスク 048: 更新 (179 -> 161)

--- 048/3_submission.py
+++ 048/3_submission.py
@@ -1,6 +1,9 @@
 def p(g,o=0):
- s=[divmod(sum(g,[]).index(2),len(g[0]))]
- for y,x in s:
-  try:1/(y|x>=0);v=g[y][x];g[y][x]=0/v;s+=((y-1,x),(y+1,x),(y,x-1),(y,x+1));o+=v<3
-  except:0
+ w=len(g[0])+2
+ f=[0]*w
+ for r in g:f+=0,*r,0
+ f+=f[:w]
+ s=[f.index(2)]
+ for i in s:
+  if(v:=f[i]):f[i]=0;s+=i+1,i-1,i+w,i-w;o+=v<3
  return[[o&8]]
