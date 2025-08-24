b4d8a37bcb56406cb0bdf0745ee8909301d92a98
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:45 +0900

    タスク 225: 更新 (231 -> 188)

--- 225/3_submission.py
+++ 225/3_submission.py
@@ -1,9 +1,1 @@
-def p(G,R=range(5)):
- for y in R:
-  for x in R:
-   if G[y][x]:
-    for a,b,v in zip((2,2,-2,-2),(2,-2,2,-2),G[y][x:x+2]+G[y+1][x:x+2]):
-     for i in y+a,y+a+1:
-      for j in x+b,x+b+1:
-       if-1<i<6>j>-1:G[i][j]=v
-    return G
+p=lambda g,R=range(5):next(g for y in R for x in R if g[y][x]and[g[i].__setitem__(j,g[y+(a<0)][x+(b<0)])for a in(2,-2)for b in(2,-2)for i in(y+a,y+a+1) for j in(x+b,x+b+1) if-1<i<6>j>-1])
