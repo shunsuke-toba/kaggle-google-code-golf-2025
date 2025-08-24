c0eb77c84b9bed9e1adb6e347d0ffbbd83c7958a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:41 +0900

    タスク 089: 更新 (376 -> 369)

--- 089/3_submission.py
+++ 089/3_submission.py
@@ -3,9 +3,9 @@ def p(g):
  for k in 2,3:
   a=[divmod(t,13)for t in range(169)if g[t//13][t%13]==k];B=s=()
   for i,j in a:
-   t=[(x,y,v)for x in R for y in R if 13>i+x>-1<j+y<13 and 0<(v:=g[i+x][j+y])!=k]
-   if len(t)>len(B):c=sorted(v for*_,v in t)[len(t)//2];B=[(x,y)for x,y,v in t if v==c];s=i,j
+   t=[(v,x,y)for x in R for y in R if 13>i+x>-1<j+y<13 and 0<(v:=g[i+x][j+y])!=k]
+   if len(t)>len(B):c=sorted(t)[len(t)//2][0];B=[(x,y)for v,x,y in t if v==c];s=i,j
+  if a:a.remove(s)
   for i,j in a:
-   if(i,j)!=s:
-    for x,y in B:g[i+x][j+y*(k*2-5)]=c
+   for x,y in B:g[i+x][j+y*(k*2-5)]=c
  return g
