d73e5871fca4944c7cf9056c8ba07687e351e4a4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:15 +0900

    タスク 294: 更新 (110 -> 103)

--- 294/3_submission.py
+++ 294/3_submission.py
@@ -1,5 +1,3 @@
 def p(g):
- for i in range(64):
-  j=i%8;i//=8
-  if all(all(r[j:j+3])for r in g[i:i+3]):g[i+1][j+1]=2
+ for i in range(64):j=i%8;i//=8;g[i+1][j+1]-=3*all(all(r[j:j+3])for r in g[i:i+3])
  return g
