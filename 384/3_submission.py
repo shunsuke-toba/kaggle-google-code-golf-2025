b4aacd6d03dfa66d672fe9e7f0709773c8f4f140
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:47 +0900

    タスク 384: 更新 (133 -> 87)

--- 384/3_submission.py
+++ 384/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):g=[r for r in g if any(r)];g=list(zip(*g));g=[c for c in g if any(c)];return[sum(zip(r,r),())for r in zip(*g) for _ in"00"]
+p=lambda g:[sum(zip(r,r),())for r in zip(*filter(any,zip(*filter(any,g))))for _ in'00']
