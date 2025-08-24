b23a2c84aaabe5185ea0d6b974eb43df0f1f978f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:27 +0900

    タスク 264: 更新 (272 -> 268)

--- 264/3_submission.py
+++ 264/3_submission.py
@@ -3,6 +3,5 @@ def p(g):
  for y in r(len(g)-2):
   for x in r(len(g[0])-2):
    if all(t:=[g[y+i//3][x+i%3]for i in a]):
-    k=d.index(sum((t[i]!=5)<<i for i in a))
-    for i in a:o[k//3*3+i//3][k*3%9+i%3]=t[i]
+    for i in a:o[(k:=d.index(sum((t[i]!=5)<<i for i in a)))-k%3+i//3][k%3*3+i%3]=t[i]
  return o
