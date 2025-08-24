e48e5d43478c747fa7c439337b085267e4aa7493
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:41 +0900

    タスク 193: 更新 (145 -> 138)

--- 193/3_submission.py
+++ 193/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,E=enumerate:[[v*((i and g[i-1][j])+(i+1<len(g)and g[i+1][j])+(j and r[j-1])+(j+1<len(r)and r[j+1])>v)for j,v in E(r)]for i,r in E(g)]
+p=lambda g,E=enumerate:[[v*((i>0)*g[i-1][j]+(i+1<len(g)and g[i+1][j])+(j>0)*r[j-1]+(r[j+1:j+2]+[0])[0]>v)for j,v in E(r)]for i,r in E(g)]
