54af2a7eb235dae847a93781156275ae68216f3c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 13:37:52 +0900

    タスク 079: 更新 (198 -> 136)

--- 079/3_submission.py
+++ 079/3_submission.py
@@ -1,7 +1,1 @@
-def p(g):
- d={};r=range;T=tuple
- for i in r(12):
-  for j in r(12):
-   t=T(T(r[j:j+3])for r in g[i:i+3])
-   if 0not in[*map(sum,t+T(zip(*t)))]:d[t]=d.get(t,0)+1
- return [*map(list,max(d,key=d.get))]
+def p(g):r=range(12);a=[t for i in r for j in r if all(map(sum,(t:=[x[j:j+3]for x in g[i:i+3]])+[*zip(*t)]))];return max(a,key=a.count)
