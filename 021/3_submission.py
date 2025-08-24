b4741a5ad198863a5127fc4088701b1ef165e04d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:25 +0900

    タスク 021: 更新 (105 -> 83)

--- 021/3_submission.py
+++ 021/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):a=g[0][0];return[[a]*-~sum({*c}<={c[0]}-{a}for c in zip(*g))]*-~sum({*r}<={r[0]}-{a}for r in g)
+def p(g):a=g[0][0];return[[a]*-~sum(c!=a for c in g[0])]*-~sum(r[0]!=a for r in g)
