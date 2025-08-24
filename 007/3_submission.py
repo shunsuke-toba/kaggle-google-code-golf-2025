3027420ade802fa69cc58498699e133011983c4b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:23 +0900

    タスク 007: 更新 (107 -> 89)

--- 007/3_submission.py
+++ 007/3_submission.py
@@ -1,1 +1,1 @@
-def p(g,r=range(7)):d={i%3:v for i,v in enumerate(sum(g,[]))if v};return[[d[(i+j)%3]for j in r]for i in r]
+def p(g):s=sum(g,[]);s=[max(s[k::3])for k in(0,1,2)]*5;return[s[i:i+7]for i in range(7)]
