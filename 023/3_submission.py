54740b806c80e8bff7fb1992b1d0262f04bcc312
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:22 +0900

    タスク 023: 更新 (254 -> 241)

--- 023/3_submission.py
+++ 023/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- s={i*16+j for i,r in enumerate(g)for j,c in enumerate(r)if c}
- f=lambda s:not s or any(((p:={min(s)+d for d in q})<=s)and f(s-p)and all(g[t>>4].__setitem__(t&15,k)or 1 for t in p)for*q,k in((0,1,2,2),(0,16,32,2),(0,1,16,17,8)))
- f(s);return g
+def p(g):s={i*16+j for i,r in enumerate(g)for j,c in enumerate(r)if c};f=lambda s:not s or any(((p:={min(s)+d for d in q})<=s)and f(s-p)and[g[t>>4].__setitem__(t&15,k)for t in p]for*q,k in((0,1,2,2),(0,16,32,2),(0,1,16,17,8)));f(s);return g
