af6b7b546d702986d8ca3af433d5f1e62f7030be
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:27 +0900

    タスク 192: 更新 (200 -> 189)

--- 192/3_submission.py
+++ 192/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(f:=sum(g,[]),c:=max({*f}-{0},key=f.count),[[c*(v==c or v*(v-c)and(c in[R[j]for R in g[i-1:i]+g[i+1:i+2]] and c in r[j-1:j]+r[j+1:j+2]))for j,v in enumerate(r)]for i,r in enumerate(g)])[2]
+p=lambda g:(c:=max(range(1,10),key=sum(g,[]).count),[[c*(v==c or (c in r[j-1:j]+r[j+1:j+2])*(c in[R[j]for R in g[i-1:i]+g[i+1:i+2]])*v>0)for j,v in enumerate(r)]for i,r in enumerate(g)])[1]
