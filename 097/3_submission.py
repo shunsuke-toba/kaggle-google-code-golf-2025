2a70c727a468934d522a39bd4e8c6edaf427cf0a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 23:43:11 +0900

    タスク 097: 更新 (126 -> 119)

--- 097/3_submission.py
+++ 097/3_submission.py
@@ -1,2 +1,1 @@
-def p(g):
- return [[v*(sum(sum(r[x and x-1:x+2])for r in g[y and y-1:y+2])>v)for x,v in enumerate(r)]for y,r in enumerate(g)]
+p=lambda g:[[v*(sum(sum(r[x and x-1:x+2])for r in g[y and y-1:y+2])>v)for x,v in enumerate(r)]for y,r in enumerate(g)]
