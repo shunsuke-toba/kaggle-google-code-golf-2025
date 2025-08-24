af8cc9aa16682585e20954205d87ca84a15bceeb
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 16:00:29 +0900

    タスク 195: 更新 (168 -> 163)

--- 195/3_submission.py
+++ 195/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,A=range(9):(x:=min(i for i,r in enumerate(g)if 5 in r),y:=min([*r,5].index(5)for r in g),[[g[x+i-i%3][y+j-j%3]&g[x+i*3%9][y+j*3%9]for j in A]for i in A])[2]
+p=lambda g,A=range(9):(w:=len(g[0]),x:=sum(g,[]).index(5)//w,y:=min([*r,5].index(5)for r in g),[[g[x+i-i%3][y+j-j%3]&g[x+i*3%9][y+j*3%9]for j in A]for i in A])[3]
