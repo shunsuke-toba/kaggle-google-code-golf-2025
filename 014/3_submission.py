ef83052914f5015684dc72de65ad2f0e9af4bc2f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:24 +0900

    タスク 014: 更新 (113 -> 110)

--- 014/3_submission.py
+++ 014/3_submission.py
@@ -1,1 +1,4 @@
-def p(g):f=lambda a,s=sum(g,[]):zip(*filter(lambda r:min({*s}-{0},key=s.count)in r,a));return[*map(list,f(f(g)))]
+def p(g):
+	s=sum(g,[]);m=min({*s}-{0},key=s.count)
+	for _ in 0,1:g=[*zip(*[r for r in g if m in r])]
+	return g
