b5c1128bb1ffb37133873326a62064ec3f68a87a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:44 +0900

    タスク 178: 更新 (124 -> 115)

--- 178/3_submission.py
+++ 178/3_submission.py
@@ -1,6 +1,4 @@
 def p(g):
- h=len({*g[0]})>1
- if h:g=[*zip(*g)]
- r=[]
- for a in g:r+=[a[:1]]*(r[-1:]!=[a[:1]])
- return[sum(r,())]if h else r
+ h=len({*g[0]})>1;r=[]
+ for a in(g,g:=zip(*g))[h]:r+=[a[:1]]*(r[-1:]!=[a[:1]])
+ return (r,[*zip(*r)])[h]
