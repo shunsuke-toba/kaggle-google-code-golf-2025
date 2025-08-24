c652987f76c216a009d0571182a8114226707a30
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 14:46:43 +0900

    タスク 165: 更新 (212 -> 209)

--- 165/3_submission.py
+++ 165/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
- k=[w for p,r in zip(g,g[1:])for v,w,x,y in zip(r,r[1:],r[2:],p[1:-1])if v==w==x==y>0][0]
+ k=[r[i]for r,p in zip(g[1:],g)for i in range(18)if r[i]==r[i+1]==r[i+2]==p[i+1]>0][0]
  for x in range(20):
   y=20;c=0
-  while y and (b:=g[y-1][x])-k:y-=1;c|=b
+  while y and (b:=g[y-1][x])^k:y-=1;c|=b
   for t in g[y:]*c*y:t[x]=c
  return g
 
