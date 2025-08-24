566299c50e1d9f844e4c974a5fcd7f7dbee97783
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:45 +0900

    タスク 302: 更新 (158 -> 157)

--- 302/3_submission.py
+++ 302/3_submission.py
@@ -2,6 +2,6 @@ def p(g):
  r=range(1,12)
  for y in r:
   for x in r:
-   if g[y][x]<1<g[y][x-1]*g[y-1][x]:
+   if g[y][x-1]==g[y-1][x]>g[y][x]:
     for t in g[y:y+(s:=g[y][x:].index(5))]:t[x:x+s]=[s+5]*s
  return g
