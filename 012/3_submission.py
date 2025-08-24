1e97d1ccef5e43e36104ff297f71ebcb2b33d176
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:24 +0900

    タスク 012: 更新 (198 -> 197)

--- 012/3_submission.py
+++ 012/3_submission.py
@@ -1,1 +1,1 @@
-def p(j,A=range(-2,3),c=enumerate):k=[*map(list,j)];[k[i+d].__setitem__(x+f,[b[x-1],h][e])for i,b in c(j)for x,h in c(b)if h!=0<b[x-1]*b[x+1]for d in A for f in A if(e:=d*d==f*f)or d*f==0];return k
+def p(g,A=range(-2,3),c=enumerate):k=eval(str(g));[k[i+d].__setitem__(x+f,[b[x-1],h][e])for i,b in c(g)for x,h in c(b)if h and b[x-1]*b[x+1]for d in A for f in A if(e:=d*d==f*f)or d*f==0];return k
