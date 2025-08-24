cb41031c0905a96523b30b3f2634bd318c943dbd
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:47 +0900

    タスク 352: 更新 (141 -> 122)

--- 352/3_submission.py
+++ 352/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- for i,r in enumerate(g):
-  for j,v in enumerate(r):r[j]|=v<2 and any(2 in s[(j and j-1):j+2]for s in g[(i and i-1):i+2])
- return g
+p=lambda g,e=enumerate:[[v<2 and any(2in s[j-1+(j<1):j+2]for s in g[i-1+(i<1):i+2]) or v for j,v in e(r)]for i,r in e(g)]
