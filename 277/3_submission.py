df2d5b189da23eb9f0f64c99627fa8d8f78ffeca
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:27 +0900

    タスク 277: 更新 (273 -> 261)

--- 277/3_submission.py
+++ 277/3_submission.py
@@ -1,11 +1,11 @@
 def p(g):
- r=range;m=99;p=[]
- for k in r(100):
+ p=range(100)
+ for k in p:
   if 7<g[i:=k//10][j:=k%10]:
-   c=[(i,j)];g[i][j]=1
+   g[i][j]=1;c=[(i,j)]
    for i,j in c:
-    for s in r(9):
-     if 0<=(x:=i+s//3-1)<10>(y:=j+s%3-1)>=0<g[x][y]+2>9:g[x][y]=1;c+=[(x,y)]
-   if(l:=len(c))<m:m=l;p=c
+    for s in range(9):
+     if-1<(x:=i+s//3-1)<10>(y:=j+s%3-1)>-1<g[x][y]-7:g[x][y]=1;c+=[(x,y)]
+   if len(c)<len(p):p=c
  for i,j in p:g[i][j]=2
  return g
