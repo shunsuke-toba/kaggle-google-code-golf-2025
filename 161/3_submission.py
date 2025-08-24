14a2305cb9993daa749dcd8a5507a67864a408fa
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 02:03:30 +0900

    タスク 161: 更新 (131 -> 128)

--- 161/3_submission.py
+++ 161/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(s:=sum(g,[]),c:=s[[*map(s.count,s)].index(4)])and[[c*(r[0]==r[-1]==c or x==y==c)for x,y in zip(g[0],g[-1])]for r in g]
+p=lambda g:(s:=sum(g,[]),c:=s[[*map(s.count,s)].index(4)])and[[c*(r[0]==r[-1]==c or t[0]==t[-1]==c)for t in zip(*g)]for r in g]
