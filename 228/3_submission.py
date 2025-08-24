f8263c56b9a6102a1da8beb890632cbb86d3eefb
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:44 +0900

    タスク 228: 更新 (204 -> 181)

--- 228/3_submission.py
+++ 228/3_submission.py
@@ -1,4 +1,4 @@
-def p(a):
- b=[(i//10,i%10)for i in range(100)if a[i//10][i%10]];c,k=b[0];e,w=b[-1]
- for i in 1,-1:a[c+i][k+i],a[e+i][w+i],a[c+i][w-i],a[e+i][k-i]=a[e+i][w+i],a[c+i][k+i],a[e+i][k-i],a[c+i][w-i]
- return a
+def p(g):
+ b=[divmod(i,10)for i,v in enumerate(sum(g,[]))if v];c,k=b[0];e,w=b[-1]
+ for i in 1,-1:l=g[c+i];r=g[e+i];l[k+i],r[w+i],l[w-i],r[k-i]=r[w+i],l[k+i],r[k-i],l[w-i]
+ return g
