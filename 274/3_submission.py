42c2ad2b5c79160e0258f8aebecf8e05f3004904
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 21:45:31 +0900

    タスク 274: 更新 (109 -> 101)

--- 274/3_submission.py
+++ 274/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):s=sum(g,[]);d=(s.index(8)-s.index(5))//len(g[0]);return[([8]*4+[0]*3)[4-d:7-d],[0,0,8*(d>3)],[0]*3]
+def p(g):s=sum(g,[]);d=(s.index(8)-s.index(5))//len(g[0]);return([8]*d+[0]*3)[:3],[0,0,d//4*8],[0]*3
