b685ae8d27d6b76d0ec776a9b7416e813fa85d85
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 17:01:44 +0900

    タスク 226: 更新 (208 -> 168)

--- 226/3_submission.py
+++ 226/3_submission.py
@@ -1,5 +1,6 @@
-def p(j):
- l,J=len(j),len(j[0])
- def f(a,b,e):
-  if l>a>-1<b<J and j[a][b]<1:j[a][b]=e;[f(a+d,b,e)or f(a,b+d,e)for d in(-1,1)]
- f(0,0,1);[f(l//2-1+(i&1),J//2-1+i//2,2)for i in range(4)];f(l-1,J-1,3);return j
+def p(g):
+ def f(r,c,v):
+  if 0<=r<10>c>=0 and g[r][c]<1:g[r][c]=v;[f(r+d,c,v)or f(r,c+d,v)for d in(-1,1)]
+ f(0,0,1);f(9,9,3)
+ for r in 4,5:f(r,4,2),f(r,5,2)
+ return g
