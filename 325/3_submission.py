114b7fb68d7e4e6806e6a3e80024a4ed307fd034
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 18:18:29 +0900

    タスク 325: 更新 (247 -> 246)

--- 325/3_submission.py
+++ 325/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,A=range):
  c=len(g);E=len(g[0])
- def f(x,j):g[x][j]=0;[c>x+a>-1<j+b<E and g[x+a][j+b]and f(x+a,j+b)for a,b in((1,0),(-1,0),(0,1),(0,-1))]
+ def f(x,y):g[x][y]=0;[f(x+a,y+b)for a,b in((1,0),(-1,0),(0,1),(0,-1))if c>x+a>-1<y+b<E and g[x+a][y+b]]
  k=sum(f(i,j)or 1 for i in A(c)for j in A(E)if g[i][j])
  return[[8*(i==j)for j in A(k)]for i in A(k)]
