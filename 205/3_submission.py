f392caa4bab6739c350cdeccf33245e274d3c6a3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 01:02:02 +0900

    タスク 205: 更新 (319 -> 318)

--- 205/3_submission.py
+++ 205/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- r=range;S=r(6,11);l=len;_,a,b,c,d=max((i*j,y,y+i,x,x+j)for i in S for j in S for y in r(l(g)-i+1)for x in r(l(g[0])-j+1)if l({y for R in g[y:y+i] for y in R[x:x+j]})<3);g=[R[c:d]for R in g[a:b]]
+ r=range;S=r(6,11);l=len;_,a,b,c,d=max((i*j,y,y+i,x,x+j)for i in S for j in S for y in r(l(g)-i+1)for x in r(l(g[0])-j+1)if l({*sum((R[x:x+j]for R in g[y:y+i]),[])})<3);g=[R[c:d]for R in g[a:b]]
  for R,x,v in[(R,x,R[x])for R in g for x in r(l(R))if R[x]-g[0][0]]:
   for L in g:L[x]=v;R[:]=[v]*l(R)
  return g
