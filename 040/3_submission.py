b58efd625f6024c73d227c7c040513d1d1b059fd
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 20:23:21 +0900

    タスク 040: 更新 (125 -> 111)

--- 040/3_submission.py
+++ 040/3_submission.py
@@ -1,3 +1,1 @@
-def p(g):
- e=enumerate;return [[(g[0][0],g[9][9])[(x,y)[g[0][0]!=g[9][0]]>4]if c==3 else c for x,c in e(r)]for y,r in e(g)]
-
+p=lambda g,e=enumerate:[[[c,(g[0][0],g[9][9])[(x,y)[g[0][0]!=g[9][0]]>4]][c==3]for x,c in e(r)]for y,r in e(g)]
