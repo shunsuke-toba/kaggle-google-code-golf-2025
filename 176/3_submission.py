b4abfba0c4b7e6e494bacad77809f8fa77a8e005
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 22:45:11 +0900

    タスク 176: 更新 (104 -> 98)

--- 176/3_submission.py
+++ 176/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[m>>i%12&1 and r.__setitem__(i,4)for r,m in zip(g,(224,65,2051))for i in range(len(r))]and g
+p=lambda g:[exec('r[i]=4')for r,m in zip(g,(224,65,2051))for i in range(len(r))if m>>i%12&1]and g
