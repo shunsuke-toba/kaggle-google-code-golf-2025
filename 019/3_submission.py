726fa30b725da586dc7e97757cbf5355f25934a7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 16:00:23 +0900

    タスク 019: 更新 (187 -> 185)

--- 019/3_submission.py
+++ 019/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):g=[r+r for r in g+g];h=len(g);w=len(g[0]);[h>i>-1<j<w and(g[i][j]or g[i].__setitem__(j,8))for k in range(h*w) if g[r:=k//w][c:=k%w]&7 for i in(r-1,r+1)for j in(c-1,c+1)];return g
+def p(g):g=[r+r for r in g+g];h=len(g);w=len(g[0]);[g[i][j]or g[i].__setitem__(j,8)for k in range(h*w) if g[r:=k//w][c:=k%w]%8 for i in(r-1,r+1)for j in(c-1,c+1) if h>i>-1<j<w];return g
