0a410800f7988272b6c97783d606228216d81af7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-24 13:37:56 +0900

    タスク 174: 更新 (190 -> 185)

--- 174/3_submission.py
+++ 174/3_submission.py
@@ -1,5 +1,4 @@
-R=range(10)
 def p(g):
- for c in{*sum(g,[])}-{0}:
-  u,v=zip(*((i,j)for i in R for j in R if g[i][j]==c));m=[r[min(v):max(v)+1]for r in g[min(u):max(u)+1]]
+ for c in{*(f:=sum(g,[]))}-{0}:
+  u,v=zip(*[divmod(i,10)for i in range(100)if f[i]==c]);m=[r[min(v):max(v)+1]for r in g[min(u):max(u)+1]]
   if m==[r[::-1]for r in m]:return m
