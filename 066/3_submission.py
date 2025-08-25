69028f742b2aaaef3bd0d6769b7b15dcd196610e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 22:46:02 +0900

    タスク 066: 更新 (348 -> 335)

--- 066/3_submission.py
+++ 066/3_submission.py
@@ -1,2 +1,1 @@
-def p(g):
- (a,b),(c,d)=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==3];f=lambda y,x,a,b,t=2:0<=(y:=y+a)<len(g)and 0<=(x:=x+b)<len(g[a])and((v:=g[y][x])==2 or v==8 and t and(f(y-a,x-b,b,-a,t-1)or f(y-a,x-b,-b,a,t-1)) or v-8 and f(y,x,a,b,t)and(g[y].__setitem__(x,3)or 1));g[a+a-c][b+b-d]-8 and f(a,b,a-c,b-d) or f(c,d,c-a,d-b);return g
+def p(g):(a,b),(c,d)=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==3];f=lambda y,x,a,b,t=2,n=len(g):0<=(y:=y+a)<n>(x:=x+b)>=0 and((v:=g[y][x])==2 or v==8>=t>0 and(f(y-a,x-b,b,-a,t-1)or f(y-a,x-b,-b,a,t-1)) or v^8 and f(y,x,a,b,t)and(g[y].__setitem__(x,3)or 1));g[2*a-c][2*b-d]^8and f(a,b,a-c,b-d)or f(c,d,c-a,d-b);return g
