8719f0f63f65c2697058ec71a7ce64488aa05ebb
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 18:40:52 +0900

    タスク 094: 更新 (148 -> 145)

--- 094/3_submission.py
+++ 094/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
  for z in range(288):
-  x,y=z//24,z%24//2
-  if 2>g[x][y]==g[x][y+1]==g[x+1][y]:g[x+2]=[c-2*(c>7)for c in g[x+2]]
-  g=[*zip(*g)]
+  g=[*zip(*g)];x,y=z//24,z%24//2
+  if 2>g[x][y]==g[x][y+1]==g[x+1][y]:g[x+2]=[c-c//8*2for c in g[x+2]]
  return g
