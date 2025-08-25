d64449cef9eca5965a9947bc8054b50807492123
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 08:44:38 +0900

    タスク 011: 更新 (161 -> 159)

--- 011/3_submission.py
+++ 011/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,R=range(11):next([[5*(x&3>2 or y&3>2)or g[i+x//4][j+y//4]for y in R]for x in R]for i in(0,4,8)for j in(0,4,8)if sum(8in r[j:j+3]for r in g[i:i+3])<1)
+p=lambda g,R=range(11):next([[5*(3in(x%4,y%4))or g[i+x//4][j+y//4]for y in R]for x in R]for i in(0,4,8)for j in(0,4,8)if sum(8in r[j:j+3]for r in g[i:i+3])<1)
