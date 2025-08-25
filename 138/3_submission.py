7add09a99905c8fe586e19ad36059a047a5a02f7
pech1 <pechi2327@gmail.com>
2025-08-25 22:28:49 +0900

    138:submissionの改善

--- 138/3_submission.py
+++ 138/3_submission.py
@@ -1,8 +1,2 @@
-def p(g):
- f=lambda z:map(z.index,filter(min,z))
- a,b=f(g);c,d=f([*zip(*g)])
- for i in range(a+1,b):
-  for j in range(c+1,d):
-   x=g[I:=i][j];y=g[b][j]==x;y-=g[a][j]==x;z=g[i][d]==x;z-=g[i][c]==x
-   while(y|z)&(g[I:=I+y][j:=j+z]<1):g[I][j]=x
- return[q[c:d+1]for q in g[a:b+1]]
+def p(g,e=enumerate):
+ T,B=[i for i,r in e(g)if min(r)];L,R=[i for i,c in e(zip(*g))if min(c)];f=lambda A:[*zip(*[[x or(r[0]in r[j:])*r[0]|(r[-1]in r[:j])*r[-1]for j,x in e(r)]for r in A])];return f(f(r[L:R+1]for r in g[T:B+1]))
