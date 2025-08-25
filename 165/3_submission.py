3985162e4e679bd92bb8a703172a46267469d260
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 06:37:38 +0900

    タスク 165: 更新 (203 -> 202)

--- 165/3_submission.py
+++ 165/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
- k=max(r[i]*(r[i]==r[i+1]==r[i+2]==p[i+1])for r,p in zip(g[1:],g)for i in range(18))
- for i in range(20):
-  y=20;c=0
+ k=max(r[i]*(r[i]==r[i+1]==r[i+2]==p[i+1])for r,p in zip(g[1:],g)for i in range(18));i=20
+ while i:
+  i-=1;y=20;c=0
   while y and(b:=g[y-1][i])^k:y-=1;c|=b
   for r in g[y:]*y:r[i]=c
  return g
