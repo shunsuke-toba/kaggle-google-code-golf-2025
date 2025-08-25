bf1a2a22a7b7361c29ad9bf0c136534c9d962f9d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 22:46:05 +0900

    タスク 187: 更新 (165 -> 164)

--- 187/3_submission.py
+++ 187/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- r=range;w=len(g[0]);h=len(g);s=[*r(w),*r(0,h*w,w)]
+ r=range;h=len(g);s=[*r(w:=len(g[0])),*r(0,h*w,w)]
  for p in s:
-  if(l:=g[p//w%h])[p%w]<1:l[p%w]=3;s+=p+1,p-1,p+w,p-w
+  if(l:=g[p//w%h])[p%w]<1:l[p%w]=3;s+=-~p,p-1,p+w,p-w
  return eval(str(g).replace(*'02'))
