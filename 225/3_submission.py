f39009a4d99a8315638dd3ddac2a0fa5ec536ba5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 18:40:55 +0900

    タスク 225: 更新 (186 -> 184)

--- 225/3_submission.py
+++ 225/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,R=range(5):next(g for y in R for x in R if g[y][x]and[g[i].__setitem__(j,g[y+(a<0)][x+(b<0)])for a in(2,-2)for b in(2,-2)for i in(y+a,y+a+1)for j in(x+b,x+b+1)if-1<i<6>j>-1])
+p=lambda g,R=range(5):next(g for y in R for x in R if g[y][x]and[g[i].__setitem__(j,g[y+(a<0)][x+(b<0)])for a in(-2,2)for b in(-2,2)for i in(y+a,y-~a)for j in(x+b,x-~b)if-1<i<6>j>-1])
