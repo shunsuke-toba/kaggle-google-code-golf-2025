bda89703515fbb24cafd7a58b744f24d44013545
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 02:31:10 +0900

    タスク 077: 更新 (285 -> 281)

--- 077/3_submission.py
+++ 077/3_submission.py
@@ -4,6 +4,6 @@ def p(g):
  while S:
   T={S.pop()}
   while U:=S&{(y+i//5-2,x+i%5-2)for i in range(25)for y,x in T}:T|=U;S-=U
-  y,x=zip(*T);a,b=min(y),max(y)+1;c,d=min(x),max(x)+1
-  for r in g[a:b]:r[c:d]=[4>>(v==2)for v in r[c:d]]
+  y,x=zip(*T)
+  for r in g[min(y):max(y)+1]:r[min(x):max(x)+1]=[4>>(v==2)for v in r[min(x):max(x)+1]]
  return g
