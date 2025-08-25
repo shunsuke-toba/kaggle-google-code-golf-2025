e8112b818d29ed04a730968820c5ec8c07a03e68
sak74y <yusei.sakurai@third-inc.co.jp>
2025-08-25 21:57:06 +0900

    240:微更新

--- 240/3_submission.py
+++ 240/3_submission.py
@@ -3,6 +3,6 @@ def p(g):
  for i in R:
   for j in R:
    if v:=g[i][j]:g[j][~i]=g[~i][~j]=g[~j][i]=v
- for i in 1,3,5,7:
+ for i in 1,3,5:
   for k in R[i+2:~i:2]:g[i][k]=g[~i][k]=g[k][i]=g[k][~i]=g[i][i+2]
  return g
