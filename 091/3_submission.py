1e7d32b64d4aff9fba8d027a5f925fb6fabb586b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 22:46:02 +0900

    タスク 091: 更新 (123 -> 117)

--- 091/3_submission.py
+++ 091/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):e=enumerate;p=[(i,j)for i,r in e(g)for j,x in e(r)if x==5];a,b,c,d=p[0]+p[-1];return[r[b:d+1]for r in g[a-1:c+2]]
+def p(g):e=enumerate;(a,b),*_,(c,d)=((i,j)for i,r in e(g)for j,x in e(r)if x==5);return[r[b:d+1]for r in g[a-1:c+2]]
