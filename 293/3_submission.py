6c67a1c28014f5afec45181f58e5dd888542cfb0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 23:36:54 +0900

    タスク 293: 更新 (97 -> 96)

--- 293/3_submission.py
+++ 293/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,k=lambda g:[r[0]and[r[0]]*len(r)or r for r in g]:g==k(g)and[*zip(*k(zip(*g)))]or k(g)
+p=lambda g,k=lambda g:[r[0]and[r[0]]*len(r)or r for r in g]:[k(g),[*zip(*k(zip(*g)))]][g==k(g)]
