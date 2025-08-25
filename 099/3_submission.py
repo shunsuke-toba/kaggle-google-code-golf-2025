daf40654180dc999e83603ce442284802989d57d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 08:44:43 +0900

    タスク 099: 更新 (182 -> 179)

--- 099/3_submission.py
+++ 099/3_submission.py
@@ -1,8 +1,1 @@
-def p(g,r=range):
- h=[*map(list,g)]
- for c in r(60):
-  y,x=c//6,c%6;f=g[y][x+2]
-  if f>1:
-   for y in r(y-2-g[y-2][x],y+3-(y>7)):
-    for X in r(x,x+5):h[y][X]=g[y][X]or f
- return h
+p=lambda g,r=range:(h:=[*map(list,g)],[[h[y].__setitem__(X,g[y][X]or f)for y in r(y-2-g[y-2][x],y+3-(y>7))for X in r(x,x+5)]for i in r(60)if(f:=g[(y:=i//6)][(x:=i%6)+2])>1],h)[2]
