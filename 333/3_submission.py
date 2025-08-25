ff9b42a89b8ba8fb2afa86b5de519aa8fb671aea
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 23:36:55 +0900

    タスク 333: 更新 (127 -> 126)

--- 333/3_submission.py
+++ 333/3_submission.py
@@ -1,1 +1,1 @@
-f=lambda a:[*zip(*[[x or(3in r[j:])*max(r[:j+1])+(3in r[:j])*max(r[j:])for j,x in enumerate(r)]for r in a])];p=lambda g:f(f(g))
+p=lambda g:(f:=lambda a:[*zip(*[[r[j]or(3in r[j:])*max(r[:j+1])+(3in r[:j])*max(r[j:])for j in range(10)]for r in a])])(f(g))
