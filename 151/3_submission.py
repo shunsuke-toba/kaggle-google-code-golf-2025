67aeb00f10c66d8c86b6580f565a62c6378050c2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 21:21:57 +0900

    タスク 151: 更新 (126 -> 123)

--- 151/3_submission.py
+++ 151/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):e,k=[[*map(all,t)].index(1) for t in (g,zip(*g))];g[e-1][k-1:k+2]=g[e+1][k-1:k+2]=4,4,4;g[e][k-1:k+2:2]=4,4;return g
+def p(g):e,k=[[*map(all,t)].index(1)for t in(g,zip(*g))];g[e-1][k-1:k+2]=g[e+1][k-1:k+2]=4,4,4;g[e][k-1:k+2:2]=4,4;return g
