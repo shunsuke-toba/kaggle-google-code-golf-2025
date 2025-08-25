3307ef79a8dd8c7e2264eb4d32cf253fab726b11
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 23:36:48 +0900

    タスク 013: 更新 (198 -> 194)

--- 013/3_submission.py
+++ 013/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
  (y,a),(Y,b)=sorted((r.index(v),v)for r in g for v in r if v);W=len(g[0])-1
- if y%W<1>Y%W:return[*zip(*p([*map(list,zip(*g))]))]
+ if Y%W<1:return[*zip(*p([*map(list,zip(*g))]))]
  Y-=y
  for r in g:r[y::Y]=([a,b]*8)[:(W-y)//Y+1]
  return g
