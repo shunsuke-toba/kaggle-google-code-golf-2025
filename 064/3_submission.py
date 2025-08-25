2b83b7ceaf352ed71bfeaca6a845ab8d085f545e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 08:44:42 +0900

    タスク 064: 更新 (192 -> 191)

--- 064/3_submission.py
+++ 064/3_submission.py
@@ -2,7 +2,7 @@ def p(g):
  a=sum(g,[]);d,b,_=sorted(sorted({*a}),key=a.count)
  for _ in[0]*4:
   for r in g:
-   try:p=r.index;i=p(d);r[i:p(b)]=[d]*(p(b)-i)
+   try:p=r.index;r[p(d):p(b)]=[d]*(p(b)-p(d))
    except:0
   g=[*map(list,zip(*g[::-1]))]
  return g
