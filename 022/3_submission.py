fcc507e919e20d34cfc430f5a6472c14a0065acb
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 18:40:51 +0900

    タスク 022: 更新 (109 -> 107)

--- 022/3_submission.py
+++ 022/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,a=range:[[max(g[i+x][j+y]*(g[i+1][j+1]==5)for i in a(9)for j in a(9))for y in a(3)]for x in a(3)]
+p=lambda g,a=range:[[max(g[i//9+x][i%9+y]*(g[i//9+1][i%9+1]==5)for i in a(81))for y in a(3)]for x in a(3)]
