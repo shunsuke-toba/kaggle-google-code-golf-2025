cd37433c886f633127ca0628dd2e43be6864a367
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 00:52:15 +0900

    タスク 157: 更新 (501 -> 498)

--- 157/3_submission.py
+++ 157/3_submission.py
@@ -6,19 +6,16 @@ def p(g):
     t=[(0,0)];g[y][x]=0
     for Y,X in t:
      for k in R(4):
-      U=y+Y+D[k];V=x+X+D[k+1]
-      if 10>U>-1<V<15 and g[U][V]>4:g[U][V]=0;t+=[(U-y,V-x)]
+      u=y+Y+D[k];v=x+X+D[k+1]
+      if 10>u>-1<v<15 and g[u][v]>4:g[u][v]=0;t+=[(u-y,v-x)]
     s+=t,
  while s:
-  b=T=U=V=0
+  b=-1
   for t in s:
    for Y in 1,2:
     for X in R(15):
-     f=1
-     for y,x in t:
-      u=y+Y;v=x+X;f+=u<3
-      if u>9 or v>14 or g[u][v]:f=0;break
-     if f>b:b,T,U,V=f,t,Y,X
+     if all(10>(u:=Y+y)>-1<(v:=X+x)<15 and g[u][v]<1 for y,x in t) and (f:=sum(Y+y<3 for y,x in t))>b:
+      b=f;T,U,V=t,Y,X
   for y,x in T:g[U+y][V+x]=1
   s.remove(T)
  return g
