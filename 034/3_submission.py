4e072692f2549b8e9045c2f5f880631d8b3f4cb3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 00:52:10 +0900

    タスク 034: 更新 (248 -> 238)

--- 034/3_submission.py
+++ 034/3_submission.py
@@ -1,8 +1,9 @@
 def p(g):
- f=sum(g,[]);k=sum({*f})-2
- for i,v in enumerate(f):
-  if v-2:continue
-  a,b=divmod(i,9);d=1-2*(g[a-1][b]<1);e=1-2*(g[a][b-1]<1)
+ f=sum(g,[])
+ k=sum({*f})-2
+ while 2 in f:
+  f[i:=f.index(2)]=k
+  a=i//9;b=i%9;d=1-2*(g[a-1][b]<1);e=1-2*(g[a][b-1]<1)
   while-1<a<9>-1<b<9:
    for x,y in(a,b),(a+d,b),(a,b+e):
     if-1<x<9>y>-1:g[x][y]=k
