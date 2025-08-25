5c67b14a20e87c318cdaaa7237d2782e2b7b013f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 23:36:49 +0900

    タスク 033: 更新 (102 -> 80)

--- 033/3_submission.py
+++ 033/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,R=range(17):[g[r][c]<g[r%6][c%6]and g[r].__setitem__(c,g[5][5])for r in R for c in R]and g
+p=lambda g,R=range(17):[[g[r][c]or g[r%6][c%6]and g[5][5]for c in R]for r in R]
