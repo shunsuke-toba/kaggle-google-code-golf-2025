be3596b1afe26cbc32a58b725d68c1d1c318bdbf
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 21:21:57 +0900

    タスク 137: 更新 (149 -> 147)

--- 137/3_submission.py
+++ 137/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):k=range(len(g));p,q,_=[(i,j)for i in k for j in k if g[i][j]];a,b=q;return[[g[a][b]*(max(a-i,i-a,b-j,j-b)%(a-p[0])<1)for j in k]for i in k]
+def p(g):k=range(len(g));p,(a,b),_=[(i,j)for i in k for j in k if g[i][j]];return[[g[a][b]*(max(a-i,i-a,b-j,j-b)%(_[0]-a)<1)for j in k]for i in k]
