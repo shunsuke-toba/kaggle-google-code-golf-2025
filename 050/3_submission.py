986cda513d7ec3310a872321b170e342361e757e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 22:46:01 +0900

    タスク 050: 更新 (140 -> 137)

--- 050/3_submission.py
+++ 050/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
  for _ in g,g:
-  g=[*map(list,zip(*g))]
-  for r in g:
+  for r in(g:=[*map(list,zip(*g))]):
    try:a=r.index(8)+1;b=r.index(8,a);r[a:b]=[3]*(b-a)
    except:0
  return g
