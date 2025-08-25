8bb034fa1768cc20ff68328a98341d630cfea329
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 23:36:45 +0900

    タスク 012: 更新 (194 -> 185)

--- 012/3_submission.py
+++ 012/3_submission.py
@@ -1,1 +1,1 @@
-def p(g,A=range(-2,3),c=enumerate):k=eval(str(g));[k[i+d].__setitem__(f-~x,[b[x],h][e])for i,b in c(g)for x,h in c(b[1:-1])if b[x]*b[x+2]for d in A for f in A if(e:=d*d==f*f)or d*f==0];return k
+def p(g,A=range(-2,3),c=enumerate):k=eval(str(g));[k[i+d].__setitem__(f-~x,b[x+e])for i,b in c(g)for x,a in c(b[:-2])if a*b[x+2]for d in A for f in A if(e:=d*d==f*f)or d*f==0];return k
