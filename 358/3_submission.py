e79e08f54b8cf161d142727da64b518de07072f2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 22:46:09 +0900

    タスク 358: 更新 (158 -> 154)

--- 358/3_submission.py
+++ 358/3_submission.py
@@ -1,7 +1,4 @@
 def p(g):
- for _ in 0,1:
-  for a in g:
-   if(s:=[*filter(None,a)])[1:]:break
-  a[:]=(s*8)[-a.index(s[0])%len(s):][:len(a)]
-  g=[*map(list,zip(*g))]
+ for _ in 0,0:
+  a=next(r for r in g if(s:=[*filter(None,r)])[1:]);a[:]=(s*8)[-a.index(s[0])%len(s):][:len(a)];g=[*map(list,zip(*g))]
  return g
