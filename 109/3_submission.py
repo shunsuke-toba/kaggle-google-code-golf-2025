4ccc39bc6da4c6d55aaf32fd9c09f285c18dc419
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 21:21:56 +0900

    タスク 109: 更新 (98 -> 96)

--- 109/3_submission.py
+++ 109/3_submission.py
@@ -1,2 +1,1 @@
-def p(g):
- n=len(g)//2;return[[g[0][n]*(e>0)for e in r[:n]+r[n-1::-1]]for r in g[:n]+g[:n][::-1]]
+p=lambda g:(n:=len(g)//2)and[[e and g[n][0]for e in r[:n]+r[n-1::-1]]for r in g[:n]+g[n-1::-1]]
