e38392d3ab8761e09050dd0b9c28299385e399d4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 09:01:55 +0900

    タスク 325: 更新 (235 -> 226)

--- 325/3_submission.py
+++ 325/3_submission.py
@@ -1,7 +1,5 @@
-def p(g,R=range):
- def f(x,y):
-  try:
-   if x>-1<y and g[x][y]:g[x][y]=0;f(x+1,y);f(x-1,y);f(x,y+1);f(x,y-1)
-  except:0
- n=sum(f(i,j)or 1 for i in R(len(g))for j in R(len(g[0]))if g[i][j])
- return[[8*(i==j)for j in R(n)]for i in R(n)]
+def p(g):
+ h=len(g);w=len(g[0])
+ f=lambda x,y:h>x>-1<y<w and g[x][y]and(g[x].__setitem__(y,0),f(x+1,y),f(x-1,y),f(x,y+1),f(x,y-1))and 1
+ n=sum(f(*divmod(t,w))for t in range(h*w))
+ return[[0]*i+[8]+[0]*(n+~i)for i in range(n)]
