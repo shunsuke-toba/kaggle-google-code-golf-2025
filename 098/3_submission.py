b9eb5af988d5139d16de0dce5820ae50fa7ce5db
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 09:01:49 +0900

    タスク 098: 更新 (130 -> 126)

--- 098/3_submission.py
+++ 098/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:g[:1]+[[0]+[x*(b[j-1]+b[j+1]+a[j]+c[j]!=4*x)for j,x in enumerate(b[1:-1],1)]+[0]for a,b,c in zip(g,g[1:],g[2:])]+g[:1]
+p=lambda g:g[:1]+[[0]+[x*(a[j]*b[j-1]*b[j+1]*c[j]<1)for j,x in enumerate(b[1:-1],1)]+[0]for a,b,c in zip(g,g[1:],g[2:])]+g[:1]
