170056d09e789852a34424c44179f91872fa7d06
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 22:46:09 +0900

    タスク 361: 更新 (289 -> 280)

--- 361/3_submission.py
+++ 361/3_submission.py
@@ -1,13 +1,11 @@
 def p(g):
- s=len(g)
- for k in 3,2:
-  t=s-k+1
+ for t,k in(8,3),(9,2):
   for n in range(t*t):
    i=n//t;j=n%t
-   if min(min(R[j:j+k])for R in g[i:i+k]):
+   if all(all(R[j:j+k])for R in g[i:i+k]):
     h=k/2-.5;i+=h;j+=h
-    for n in range(s*s):
-     if v:=g[y:=n//s][x:=n%s]:
+    for n in range(100):
+     if v:=g[y:=n//10][x:=n%10]:
       y-=i;x-=j
       for _ in 0,0,0:x,y=-y,x;g[int(i+y)][int(j+x)]=v
     return g
