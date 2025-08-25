a27c894c8a2c20cb78fc92eb5072083820ee235b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 08:44:44 +0900

    タスク 228: 更新 (173 -> 172)

--- 228/3_submission.py
+++ 228/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- (c,k),*_,(e,w)=[divmod(i,10)for i,v in enumerate(sum(g,[]))if v]
+ (c,k),*_,(e,w)=[divmod(i,10)for i in range(100)if sum(g,[])[i]]
  for i in-1,1:l=g[c+i];r=g[e+i];l[k+i],r[w+i],l[w-i],r[k-i]=r[w+i],l[k+i],r[k-i],l[w-i]
  return g
