00bbe184d4446e11a244cb18797a9f66dcf0ea80
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 18:40:58 +0900

    タスク 383: 更新 (186 -> 174)

--- 383/3_submission.py
+++ 383/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):a,b={*sum(g,[])}-{0};o=(b,a)[any({*r}-{0}=={a}for r in g)];k=a+b-o;return[[(x:=c[0])+((0<r.count(o)<4)|(0<c[1:].count(o)<4))*((x<1)*k+(x==k)*(o-x))for c in zip(r,*g)]for r in g]
+def p(g):f=sum(g,[]);a,b={*f}-{0};o=next(v for v in f if v);k=a+b-o;return[[(x:=c[0])+((0<r.count(o)<4)|(0<c[1:].count(o)<4))*(o-x-(o-k)*(x<1))for c in zip(r,*g)]for r in g]
