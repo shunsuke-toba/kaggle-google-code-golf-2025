9a2a457191b96f4fb69786abbaa5b6bbb5e54935
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 23:36:53 +0900

    タスク 253: 更新 (179 -> 177)

--- 253/3_submission.py
+++ 253/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
- o=[0]*16
+ k=[0]*4
  for g,h in zip(g,g[1:]):
   for w in zip(g,g[1:],h,h[1:]):
-   if w.count(0)<2:k=(10,8,2,0)[w.index(0)];o[k],o[k+1],o[k+4],o[k+5]=w
- return[*zip(*[iter(o)]*4)]
+   k[w.index(0)]+=max(w)*(w.count(0)<2)
+ a,b,c,d=k;return((d,d,c,c),(d,0,0,c),(b,0,0,a),(b,b,a,a))
