fd04fd3eed4ef9f8e7968e46215012da59381380
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 08:44:41 +0900

    タスク 015: 更新 (141 -> 130)

--- 015/3_submission.py
+++ 015/3_submission.py
@@ -1,6 +1,4 @@
-E=enumerate
 def p(g):
- for y,r in E(g):
-  for x,v in E(r):
-   if 0<v<3:w=v>1;g[y+1][x+w]=g[y-1][x-w]=g[y+w][x-1]=g[y-w][x+1]=7-3*w
+ for i in range(81):
+  if -1<(w:=g[y:=i//9][x:=i%9]-1)<2:g[y+1][x+w]=g[y-1][x-w]=g[y+w][x-1]=g[y-w][x+1]=7-3*w
  return g
