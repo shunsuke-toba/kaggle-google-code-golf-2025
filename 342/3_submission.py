2f6e7cfb4485bf28e922cf3bbc494348d826699e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 22:46:08 +0900

    タスク 342: 更新 (175 -> 173)

--- 342/3_submission.py
+++ 342/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- r,c=divmod(sum(g,[]).index(8),10);a=[-8]*4
- for q in range(100):y,x=q//10,q%10;a[(y>r)<<1|(x>c)]+=g[y][x];g[y][x]=0
+ r,c=divmod(sum(g,[]).index(8),10);a=[-8]*4;q=100
+ while q:q-=1;y,x=q//10,q%10;a[(y>r)*2+(x>c)]+=g[y][x];g[y][x]=0
  g[r][c:c+2]=a[:2];g[r+1][c:c+2]=a[2:];return g
