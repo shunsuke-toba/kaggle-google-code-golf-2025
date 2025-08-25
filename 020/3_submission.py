beedc583624a13f2df3a9304bc9b41245c68e9ce
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 18:40:50 +0900

    タスク 020: 更新 (195 -> 181)

--- 020/3_submission.py
+++ 020/3_submission.py
@@ -1,4 +1,5 @@
 def p(g):
- m,n=map(min,zip(*(divmod(k,10)for k,v in enumerate(sum(g,[]))if v)));r=range(5)
- for i in r:g[m+i][n:n+5]=[g[m+i][n+j]|g[m+4-j][n+i]|g[m+4-i][n+4-j]|g[m+j][n+4-i]for j in r]
+ m,n=map(min,zip(*(divmod(k,10)for k,v in enumerate(sum(g,[]))if v)))
+ for k in range(25):j=k%5;i=k//5;g[m+i][n+j]|=g[m+4-j][n+i]|g[m+4-i][n+4-j]|g[m+j][n+4-i]
  return g
+
