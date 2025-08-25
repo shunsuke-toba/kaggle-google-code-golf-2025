68fb8fab585de97024ae719722298a68d5b36c13
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 06:37:36 +0900

    タスク 070: 更新 (133 -> 131)

--- 070/3_submission.py
+++ 070/3_submission.py
@@ -1,5 +1,4 @@
 def p(g):
  a,*_,b=[i for i,c in enumerate(zip(*g))if 8 in c]
- for r in g:
-  for j in range(a,-~b):r[j]+=8 in r and 2//r[j]
+ for r in g:r[a:-~b]=[x+(8 in r and 2//x)for x in r[a:-~b]]
  return g
