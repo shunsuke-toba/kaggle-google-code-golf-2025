d9f6f8a032cc1f10ae7410d20db35ccc0e072be2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 21:22:00 +0900

    タスク 288: 更新 (114 -> 99)

--- 288/3_submission.py
+++ 288/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- c=len(g)//2;b=g[-1];s=c-(b[c-1]==b[c]==b[c+1])
- for i in range(s):r=g[i-s-2];r[i]=r[~i]=b[c]
+ b=g[-1];s=b.index(a:=b[len(b)//2])
+ for i in range(s):r=g[i-s-2];r[i]=r[~i]=a
  return g
