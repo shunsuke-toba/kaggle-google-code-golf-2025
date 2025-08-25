83320f481138b7f515fb7ce34644aaf1a9bce560
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 23:36:50 +0900

    タスク 065: 更新 (126 -> 124)

--- 065/3_submission.py
+++ 065/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):m=len(g);n=m//2;f=sum(g,[]);i=f.index(min(f,key=f.count));d=-~n;return[r[d*(i%m>n):][:n]for r in g[d*(i//m>n):][:n]]
+def p(g):m=len(g);n=m//2;f=sum(g,[]);i=f.index(min(f,key=f.count));return[r[-~n*(i%m>n):][:n]for r in g[-~n*(i//m>n):][:n]]
