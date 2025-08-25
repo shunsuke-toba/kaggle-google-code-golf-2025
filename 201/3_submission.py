ce0058aad8b68bc8acef2dd4f9de8be1998f2619
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 22:46:05 +0900

    タスク 201: 更新 (315 -> 313)

--- 201/3_submission.py
+++ 201/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  h=sum(g,[]);J=S=w=13;(a,b),*_,(c,d)=[divmod(i,w)for i,v in enumerate(h)if v==4]
  for i,v in enumerate(h):
-  if v*(v-4)and min((r:=i//w)-a,c-r,(k:=i%w)-b,d-k)<0:
+  if v&-5 and min((r:=i//w)-a,c-r,(k:=i%w)-b,d-k)<0:
    if r<S:S=r
    if k<J:J=k;t=v==g[a+1][d]
  o=[r[b:d+1]for r in g[a:c+1]]
