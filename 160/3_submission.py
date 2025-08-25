b934f27014988e9fdb8129a0d37d1f4d3731b68b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 23:36:51 +0900

    タスク 160: 更新 (144 -> 142)

--- 160/3_submission.py
+++ 160/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- for z in range(64):
-  a,b,c=g[z>>3:][:3];x=z%8;s=slice(x,x+3)
-  if sum(a[s]+b[s]+c[s])-b[x+1]>7:a[s]=c[s]=0,2,0;b[s]=2,2,2
- return g
+	for z in range(64):
+		a,b,c=g[z>>3:][:3];x=z&7;s=slice(x,x+3)
+		if (a[s]+b[s]+c[s]).count(1)>7:a[s]=c[s]=0,2,0;b[s]=2,2,2
+	return g
