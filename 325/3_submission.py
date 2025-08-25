d3300cc8b9c1acb1d6fe42408a0b32e9e45ed84e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 23:36:55 +0900

    タスク 325: 更新 (226 -> 218)

--- 325/3_submission.py
+++ 325/3_submission.py
@@ -1,5 +1,2 @@
 def p(g):
- h=len(g);w=len(g[0])
- f=lambda x,y:h>x>-1<y<w and g[x][y]and(g[x].__setitem__(y,0),f(x+1,y),f(x-1,y),f(x,y+1),f(x,y-1))and 1
- n=sum(f(*divmod(t,w))for t in range(h*w))
- return[[0]*i+[8]+[0]*(n+~i)for i in range(n)]
+ h=len(g);w=len(g[0]);f=lambda x,y:h>x>-1<y<w and g[x][y]and(g[x].__setitem__(y,0)or f(x+1,y)|f(x-1,y)|f(x,y+1)|f(x,y-1)|1);n=sum(f(t//w,t%w)for t in range(h*w));return[[0]*i+[8]+[0]*(n+~i)for i in range(n)]
