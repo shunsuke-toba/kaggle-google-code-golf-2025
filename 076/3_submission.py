1e0669ad294590faedc54177c075d8f026d8a113
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 00:52:14 +0900

    タスク 076: 更新 (520 -> 519)

--- 076/3_submission.py
+++ 076/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  L=len;R=range;e=enumerate;s=[divmod(next(i for i,v in e(sum(g,[]))if v&1),w:=L(g[0]))]
  for y,x in s:s+=[(Y,X)for Y in R(y-1,y+2)for X in R(x-1,x+2)if L(g)>Y>-1<X<w and g[Y][X]and(Y,X)not in s]
- r,c=zip(*s);t=[row[min(c):max(c)+1]for row in g[min(r):max(r)+1]]
+ r,c=zip(*s);t=[g[i][min(c):max(c)+1]for i in R(min(r),max(r)+1)]
  for i in R(8):
   for y in R(L(g)-L(t)+1):
    for x in R(w-L(t[0])+1):
