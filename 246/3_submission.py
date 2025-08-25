5a7b53519008060c9e688cf72c948d659fd9672e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 09:01:53 +0900

    タスク 246: 更新 (161 -> 154)

--- 246/3_submission.py
+++ 246/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- b=len(g[0]);c=sum(g,[]);i=c.index;d=divmod;y,x=d(i(2),b);Y,X=d(i(3),b)
- while x-X:g[y][x:=x+(X>x)*2-1]=8
- while(y:=y+(Y>y)*2-1)-Y:g[y][X]=8
+ b=len(g[0]);i=sum(g,[]).index;d=divmod;y,x=d(i(2),b);Y,X=d(i(3),b)
+ while x-X:x+=X>x or-1;g[y][x]=8
+ while y-Y:g[y][X]=8;y+=Y>y or-1
  return g
