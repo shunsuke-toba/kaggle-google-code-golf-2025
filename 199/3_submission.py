a828c8b74f87ec885a14f3af14574c840dac5b68
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 09:01:50 +0900

    タスク 199: 更新 (130 -> 119)

--- 199/3_submission.py
+++ 199/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
  for r,R in enumerate(g,1):
-  if v:=sum(R):g[r][c:=R.index(v)]=v;g[:r]=[[4-4*(j+c&1)for j in range(len(R))]]*r;return g
+  if v:=sum(R):g[r][c:=R.index(v)]=v;g[:r]=[([4,0]*8)[c%2:][:len(R)]]*r;return g
