f55c8a948d72a3feba82d11f89f8c052078f8067
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 23:36:54 +0900

    タスク 319: 更新 (412 -> 411)

--- 319/3_submission.py
+++ 319/3_submission.py
@@ -1,11 +1,11 @@
 def p(g,r=range,l=len):
- s=sum(g,[]);*C,z=sorted({*s},key=s.count)
- def F(c,h=g):
+ s=sum(g,[]);*C,B=sorted({*s},key=s.count)
+ def f(c,h=g):
   for _ in r(4):
    while c not in h[0]:h=h[1:]
    h=[*zip(*h[::-1])]
   return[[x==c for x in t]for t in h]
  for c in C:
   for d in C:
-   q,u=F(c),F(d)
-   if(c-d)*any(all(q[i+x>>1][j+y>>1]==u[x][y]for x in r(l(u))for y in r(l(u[0])))for i in r(l(q)*2-l(u)+1)for j in r(l(q[0])*2-l(u[0])+1)):return[[[z,c][x]for x in t]for t in q]
+   q,u=f(c),f(d);X,Y=l(u),l(u[0])
+   if(c-d)*any(all(q[i+x>>1][j+y>>1]==u[x][y]for x in r(X)for y in r(Y))for i in r(l(q)*2-X+1)for j in r(l(q[0])*2-Y+1)):return[[[B,c][x]for x in t]for t in q]
