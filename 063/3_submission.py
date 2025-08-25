a6889417d18adea786abb92108e938e966d25636
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 22:46:01 +0900

    タスク 063: 更新 (128 -> 127)

--- 063/3_submission.py
+++ 063/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[[x+3*(0<i<len(g)-1>j>0and any(t[j]for t in g[1:-1])*any(r[1:-1])<1)for j,x in enumerate(r)]for i,r in enumerate(g)]
+p=lambda g:[[x+3*(0<i<len(g)-1>j>0)*(1-any(t[j]for t in g[1:-1])*any(r[1:-1]))for j,x in enumerate(r)]for i,r in enumerate(g)]
