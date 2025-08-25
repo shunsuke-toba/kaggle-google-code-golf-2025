0c51fabc6e45629b8e54abcd4d00492860300e00
pech1 <pechi2327@gmail.com>
2025-08-26 02:41:24 +0900

    379:submissionの更新

--- 379/3_submission.py
+++ 379/3_submission.py
@@ -1,8 +1,1 @@
-def p(g):
- e=enumerate;t=lambda x:[*map(list,zip(*x))]
- if not(L:=[i for i,r in e(g)if r[0]]):return t(p(t(g)))
- for k,j,l in((i,j,l)for l in L for i,r in e(g)for j,v in e(r)if v&2):
-  while g[k][j]^8 and k-l:g[k][j]=2;k+=k<l or-1
-  if k==l:
-   for m in-1,0,1:g[l+m][j-1:j+2]=8,8,8;g[l][j]=2
- return g
+E=enumerate;f=lambda s:next(filter(int,s),0);h=lambda a:[*zip(*[[(x==8)&(2in((u:=f(r[j+1:])),(v:=f(r[:j][::-1]))))or x or(u^v>8)*2for j,x in E(r)]for r in a])];p=lambda g:(a:=h(h(g)))and[[(x&1)*2or(any(1in y[j-(j>0):j+2]for y in a[i-(i>0):i+2])*8)or x for j,x in E(r)]for i,r in E(a)]
