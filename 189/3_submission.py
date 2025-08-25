07a9a21c94d0234f9ff4e5184d24bd85dc0c9475
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-25 18:40:54 +0900

    タスク 189: 更新 (137 -> 123)

--- 189/3_submission.py
+++ 189/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,R=range(6):(r:=g.index([8]*9)>2,c:=g[0].index(8)>2,[[g[i//3+r*7][j//3+c*7]*g[i+3-3*r][j+3-3*c]//3 for j in R]for i in R])[-1]
+p=lambda g,R=range(6):[r:=g[2][0]<8,c:=g[0][2]<8,[[g[i//3+r*7][j//3+c*7]*g[i+3-3*r][j+3-3*c]//3 for j in R]for i in R]][2]
