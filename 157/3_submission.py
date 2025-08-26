70708e691f4bdf8ff17e73d275ed7245a10ab37a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 07:13:59 +0900

    タスク 157: 更新 (469 -> 462)

--- 157/3_submission.py
+++ 157/3_submission.py
@@ -1,16 +1,16 @@
 def p(g):
- R=range;D=1,0,-1,0,1;s=[]
- for Y in R(10):
-  for X in R(15):
-   if g[Y][X]>4:
-    t=[(0,0)];g[Y][X]=0
+ r=range;d=1,0,-1,0,1;s=[]
+ for I in r(10):
+  for J in r(15):
+   if g[I][J]>4:
+    t=[(0,0)];g[I][J]=0
     for y,x in t:
-     for k in R(4):
-      u=Y+y+D[k];v=X+x+D[k+1]
-      if u<10 and-1<v<15 and g[u][v]>4:g[u][v]=0;t+=(u-Y,v-X),
+     for k in r(4):
+      u=I+y+d[k];v=J+x+d[k+1]
+      if-1<v<15 and u<10>g[u][v]>4:g[u][v]=0;t+=(u-I,v-J),
     s+=t,
  while s:
-  T,U,V=max(((t,Y,X,sum(Y+y<3 for y,x in t))for t in s for Y in(1,2)for X in R(15)if all(15>X+x and g[Y+y][X+x]<1 for y,x in t)),key=lambda q:q[3])[:3]
-  for y,x in T:g[U+y][V+x]=1
-  s.remove(T)
+  t,I,J=max(((t,I,J)for t in s for I in(1,2)for J in r(15)if all(15>J+x and g[I+y][J+x]<1 for y,x in t)),key=lambda q:sum(q[1]+y<3 for y,x in q[0]))
+  for y,x in t:g[I+y][J+x]=1
+  s.remove(t)
  return g
