179955ba36d4f0312eaa898c88c39077fa120d27
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 08:22:43 +0900

    タスク 190: 更新 (168 -> 166)

--- 190/3_submission.py
+++ 190/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for b in range(1280):
-  i=b%8+1;j=b//8%8+1;a=b>>6&1 or-1;c=b>>7&1 or-1
+  i=b%8+1;j=b//8%8+1;a=b>>6&1or-1;c=b>>7&1or-1
   if g[i+a][j+c]>g[i+1][j]+g[i-1][j]+g[i][j+1]+g[i][j-1]:g[i-a][j-c]=g[i][j]
  return g
