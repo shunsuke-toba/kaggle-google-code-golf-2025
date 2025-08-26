b2dc37c4a10958b969e592079b760de622a0e3a8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 07:14:04 +0900

    タスク 370: 更新 (307 -> 297)

--- 370/3_submission.py
+++ 370/3_submission.py
@@ -1,12 +1,12 @@
 def p(g):
- w=len(g[0]);b=g[0][0];Z=[]
- for k,v in enumerate(sum(g,[])):
-  z=k//w+1j*(k%w)
+ w=len(g[0]);Z=[];k=0
+ for v in sum(g,[]):
+  z=k//w+k%w*1j;k+=1
   if v<1:Z+=z,
-  elif v-b:P=z;D=v
+  elif v-g[0][0]:P=z;d=v
  s=P-max(Z,key=lambda z:(abs((z:=P-z).real),abs(z.imag)))
  for _ in[0]*w+g:
   Z=[z+s for z in Z]
   for z in Z:
-   if len(g)>z.real>-1<z.imag<w:g[int(z.real)][int(z.imag)]=D
+   if len(g)>z.real>-1<z.imag<w:g[int(z.real)][int(z.imag)]=d
  return g
