d38b9d72a75de6d3e1ec8612cea755bf42c83f5e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 22:59:50 +0900

    タスク 002: 更新 (173 -> 171)

--- 002/3_submission.py
+++ 002/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  c=len(g);g=[[x or 4for x in r]for r in g];d=1-c,0,-1,0
- for z in range(9**5):r,q=z%97%c,z%89%c;g[r][q]*=g[r+d[z&3]][q+d[~z&3]]*r*q*(r-c+1)+g[r][q]%2!=0
+ for z in range(9**5):g[r:=z%97%c][q:=z%89%c]*=g[r+d[z&3]][q+d[~z&3]]*r*q*(r-c+1)+g[r][q]%2!=0
  return g
