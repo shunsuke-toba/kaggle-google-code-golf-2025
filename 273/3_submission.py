21886328b4a5446915f95a4a19b413b2802d830c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 01:14:00 +0900

    タスク 273: 更新 (152 -> 150)

--- 273/3_submission.py
+++ 273/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- s=[divmod(i,10)for i,v in enumerate(sum(g,[]))if v]
+ s=[(i//10,i%10)for i in range(100)if sum(g,[])[i]]
  for(a,b),(c,d)in zip(s[::4],s[3::4]):
   for r in g[a+1:c]:r[b+1:d]=[2]*(~b+d)
  return g
