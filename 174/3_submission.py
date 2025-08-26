6d9246a74a2e253bd3ced0e37ecafc4c98bfa584
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 08:22:43 +0900

    タスク 174: 更新 (178 -> 177)

--- 174/3_submission.py
+++ 174/3_submission.py
@@ -1,5 +1,4 @@
 def p(g):
- f=sum(g,[])
- for c in f:
-  u,v=zip(*[divmod(i,10)for i in range(100)if f[i]==c]);m=[r[min(v):max(v)+1]for r in g[min(u):max(u)+1]]
+ for c in(f:=sum(g,[])):
+  u,v=zip(*[divmod(i,10)for i in range(100)if c==f[i]]);m=[r[min(v):max(v)+1]for r in g[min(u):max(u)+1]]
   if m==[r[::-1]for r in m]:return m
