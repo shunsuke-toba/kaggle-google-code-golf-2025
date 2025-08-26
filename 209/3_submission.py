60720cc6141e6248a4f926e1f370bbc01317e29b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 22:59:57 +0900

    タスク 209: 更新 (558 -> 557)

--- 209/3_submission.py
+++ 209/3_submission.py
@@ -1,17 +1,17 @@
 def p(g):
- a=e=len(g);c=i=m=len(g[0]);b=d=f=j=0;S=I=();Y=range
+ A=min;B=max;a=e=len(g);c=i=m=len(g[0]);b=d=f=j=0;S=I=();Y=range
  for t in Y(e*m):
-  if (v:=g[r:=t//m][o:=t%m])==4:a=min(a,r);b=max(b,r);c=min(c,o);d=max(d,o)
-  elif v:S+=(r,o),
- for r,o in S:
-  if a<=r<=b and c<=o<=d:I+=(r,o),
-  else:e=min(e,r);f=max(f,r);i=min(i,o);j=max(j,o)
+  if (v:=g[r:=t//m][o:=t%m])==4:a=A(a,r);b=B(b,r);c=A(c,o);d=B(d,o)
+  elif v:S+=(r,o,v),
+ for r,o,v in S:
+  if a<=r<=b and c<=o<=d:I+=(r,o,v),
+  else:e=A(e,r);f=B(f,r);i=A(i,o);j=B(j,o)
  h=f-e+1;w=j-i+1
  for k in 2,3,4:
   for r in Y(a,b-h*k+2):
    for o in Y(c,d-w*k+2):
     try:
-     if all(g[e+(R-r)//k][i+(O-o)//k]==g[R][O]for R,O in I):
+     if all(g[e+(R-r)//k][i+(O-o)//k]==V for R,O,V in I):
       for q in Y(h*k):g[r+q][o:o+w*k]=[g[e+q//k][i+p//k]for p in Y(w*k)]
       return[r[c:d+1]for r in g[a:b+1]]
     except:0
