b6a8e834c25439657f7679d2b6deaa844188c962
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 22:59:56 +0900

    タスク 168: 更新 (203 -> 202)

--- 168/3_submission.py
+++ 168/3_submission.py
@@ -3,5 +3,5 @@ def p(g):
   c=r%9;r//=9;v=g[r][c:c+2]+g[r+1][c:c+2]
   if v.count(0)<2:
    p=v.index(0);a=(p&2)-1;b=p%2*2-1;r+=a+p//2;c+=b+p%2
-   while-1<r<10>c>-1:g[r][c]=v[p-1];r+=a;c+=b
+   while-1<r<10>c>-1:g[r][c]=v[~p];r+=a;c+=b
  return g
