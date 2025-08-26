50f889f685edc3d4589148badbfaa91b46f822d1
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-27 00:58:27 +0900

    336: 改善(147 -> 146)

--- 336/3_submission.py
+++ 336/3_submission.py
@@ -1,1 +1,1 @@
-f=lambda a:[*zip(*[[r[j]or 8*((5in{*r[:j]}&{*r[j:]})+(r.count(5)<2)*(5in r[(j<5)*5:10-(j>4)*5]))for j in range(10)]for r in a])];p=lambda g:f(f(g))
+f=lambda a:[*zip(*[[r[j]or 8*((5in{*r[:j]}&{*r[j:]})+(r.count(5)<2)*(5in r[(j<5)*5:10-j+j%5]))for j in range(10)]for r in a])];p=lambda g:f(f(g))
