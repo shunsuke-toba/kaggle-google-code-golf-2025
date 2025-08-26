200b909e56d5d092ceaa38cb299bf17764d25dc4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 01:13:58 +0900

    タスク 200: 更新 (118 -> 117)

--- 200/3_submission.py
+++ 200/3_submission.py
@@ -1,5 +1,7 @@
 def p(g):
  j=g[9].index(c:=max(g[9]));k=0
- for r in g:r[j::2]=[c]*(5-j//2)
- while j<9:g[k][j+1]=5;k=~k;j+=2
+ while j<10:
+  for r in g:r[j]=c
+  if j<9:g[k][j+1]=5;k=~k
+  j+=2
  return g
