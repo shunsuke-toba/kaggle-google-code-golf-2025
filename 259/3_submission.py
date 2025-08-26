9450b1bdad56aff530251e055dbabf264018c739
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 01:13:59 +0900

    タスク 259: 更新 (111 -> 102)

--- 259/3_submission.py
+++ 259/3_submission.py
@@ -1,3 +1,1 @@
-def p(g):
- exec("while max(g[0])<2:g[:]=g[1:]\ng[:]=zip(*g[::-1])\n"*4)
- return[[c*(c>1)for c in r]for r in g]
+p=lambda g:exec("while max(g[0])<2:g.pop(0)\ng[:]=zip(*g[::-1])\n"*4) or eval(str(g).replace('1','0'))
