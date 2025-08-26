e02fe12bf301c8c8f17a890ea08e5473e6af9549
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 07:14:03 +0900

    タスク 324: 更新 (304 -> 288)

--- 324/3_submission.py
+++ 324/3_submission.py
@@ -1,6 +1,8 @@
 def p(g):
- f=sum(g,[]);a=max(g[0][:2]+g[1][:2],key=f.count);b=max({*f}-{a},key=f.count);s=[];m={};e=enumerate
+ f=sum(g,[])
+ a=max(g[0][:2]+g[1][:2],key=f.count);c=[*zip(*g)]
+ s=[];m={};e=enumerate
  for i,r in e(g):
   for j,d in e(r):
-   if(d^a)*(d^b):s+=i+j,60+i-j;m[(b,a)[a in r and a in[t[j]for t in g]]]=d
- return [[(d,m.get(d,d))[i+j in s or 60+i-j in s]for j,d in e(r)]for i,r in e(g)]
+   if f.count(d)<3:s+=i+j,58+i-j;m[a in r and a in c[j]]=d
+ return [[(d,m[a in r and a in c[j]])[i+j in s or 58+i-j in s]for j,d in e(r)]for i,r in e(g)]
