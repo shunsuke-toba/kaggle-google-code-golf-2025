e977b80082de9752308867934642c05bd013926f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 21:18:20 +0900

    タスク 189: 更新 (123 -> 122)

--- 189/3_submission.py
+++ 189/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,R=range(6):[r:=g[2][0]<8,c:=g[0][2]<8,[[g[i//3+r*7][j//3+c*7]*g[i+3-3*r][j+3-3*c]//3 for j in R]for i in R]][2]
+p=lambda g,R=range(6):(r:=g[2][0]<8,c:=g[0][2]<8)and[[g[i//3+r*7][j//3+c*7]*g[i+3-3*r][j+3-3*c]//3 for j in R]for i in R]
