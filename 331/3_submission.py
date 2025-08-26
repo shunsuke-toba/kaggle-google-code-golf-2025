1b8adfe6de0e8fb8f8de9ca402e4409a8f7bd9f1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 01:14:01 +0900

    タスク 331: 更新 (121 -> 118)

--- 331/3_submission.py
+++ 331/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,z=[0]*10:[[x+u*8+d*2+l*6+r*7 for x,u,d,l,r in zip(a,b,c,[0]+a,a[1:]+[0])]for a,b,c in zip(g,[z]+g,g[1:]+[z])]
+p=lambda g,z=[[0]*10]:[[x+u*8+d*2+l*6+r*7 for x,u,d,l,r in zip(a,b,c,[0]+a,a[1:]+[0])]for a,b,c in zip(g,z+g,g[1:]+z)]
