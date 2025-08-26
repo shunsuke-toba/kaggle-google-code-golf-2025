4794ff8d71505e6ea9cfdab4344c5e2f1888f40f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 00:03:03 +0900

    タスク 099: 更新 (179 -> 178)

--- 099/3_submission.py
+++ 099/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,r=range:(h:=[*map(list,g)],[[h[y].__setitem__(X,g[y][X]or f)for y in r(y-2-g[y-2][x],y+3-(y>7))for X in r(x,x+5)]for i in r(60)if(f:=g[(y:=i//6)][(x:=i%6)+2])>1],h)[2]
+p=lambda g,r=range:(h:=[*map(list,g)],[h[a].__setitem__(b,g[a][b]or f)for y in r(10)for x in(0,1,4)if(f:=g[y][x+2])>1 for a in r(y-2-g[y-2][x],y+3-(y>7))for b in r(x,x+5)],h)[2]
