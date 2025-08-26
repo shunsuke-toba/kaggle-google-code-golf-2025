b205ec4ebcf57e83a0ecde32fc0ee874bc353860
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 00:03:06 +0900

    タスク 355: 更新 (142 -> 140)

--- 355/3_submission.py
+++ 355/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):s=sum(g,[]);return[[max({*s}-{c:=min(s,key=s.count)},key=lambda b:sum(r.count(c)for r in zip(*[q for q in g if b in q])if b in r))]]
+def p(g):s=sum(g,[]);return[[max({*s}-{c:=min(s,key=s.count)},key=lambda b:sum(b in r for r in g)*sum(b in r for r in zip(*g))-s.count(b))]]
