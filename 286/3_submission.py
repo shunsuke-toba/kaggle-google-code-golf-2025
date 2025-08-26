fcdc9c8be53866e2524694b9ad61f62f02d19f15
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-27 04:46:18 +0900

    286: 改善(198 -> 196)

--- 286/3_submission.py
+++ 286/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  h=len(g);w=len(g[0]);a,b={*sum(g,[])}-{0,8};d=0,1,0,-1;z=7**7
  while z:=z-1:
-  if g[r:=z%97%h][q:=z%89%w]<1<w>(j:=q+d[z%4])>-1<(i:=r+d[z%4-1])<h and(k:=g[i][j])%8:g[r][q]=k^a^b
+  if g[r:=z%97%h][q:=z%89%w]<1<w>(j:=q+d[z%4])>-1<(i:=r+d[~z%4])<h>(k:=g[i][j])%8>0:g[r][q]=k^a^b
  return g
