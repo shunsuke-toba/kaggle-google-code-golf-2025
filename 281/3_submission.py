9fe92b21f1dd99168885935cd1153b37c51a130c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 07:14:03 +0900

    タスク 281: 更新 (233 -> 232)

--- 281/3_submission.py
+++ 281/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
- a=c=9;d=0;p=[];e=enumerate
- for y,m in e(g):
-  for x,k in e(m):
-   if k:a=min(a,y);b=y;c=min(c,x);d=max(d,x);p+=[k]*(k^8>0)*(k not in p)
+ a=c=9;d=0;p=[];E=enumerate
+ for y,m in E(g):
+  for x,k in E(m):
+   if k:a=min(a,y);b=y;c=min(c,x);d=max(d,x);p+=[k]*(k!=8)*(k not in p)
  for y in range(a,b+1):
   for x in range(c,d+1):g[y][x]=p[a<y<b and c<x<d]
  return g
