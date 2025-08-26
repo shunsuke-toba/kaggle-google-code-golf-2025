d3d7100867e64be9da41e8c4ea91564dd7f9cebb
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 19:09:21 +0900

    タスク 176: 更新 (98 -> 86)

--- 176/3_submission.py
+++ 176/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[exec('r[i]=4')for r,m in zip(g,(224,65,2051))for i in range(len(r))if m>>i%12&1]and g
+p=lambda g:[[[c,4][m>>i%12&1]for i,c in enumerate(r)]for r,m in zip(g,(224,65,2051))]
