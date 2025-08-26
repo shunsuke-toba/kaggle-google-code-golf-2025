f60ec9e02647f3d9d4f45f3136f8d5efb6d97928
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 01:13:54 +0900

    タスク 017: 更新 (154 -> 150)

--- 017/3_submission.py
+++ 017/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,s=1,d={},r=range(21):any((v:=g[i][j])and d.setdefault((i%s,j%s),v)-v for j in r for i in r)and p(g,s+1,{})or[[d[i%s,j%s]for j in r]for i in r]
+p=lambda g,s=1,d={},r=range(21):any(0<(x:=g[i][j])!=d.setdefault((i%s,j%s),x)for j in r for i in r)and p(g,s+1,{})or[[d[i%s,j%s]for j in r]for i in r]
