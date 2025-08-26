04eb4fb557baa6d03fb172302a4e0e92e3b5a848
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 00:03:07 +0900

    タスク 361: 更新 (279 -> 272)

--- 361/3_submission.py
+++ 361/3_submission.py
@@ -1,11 +1,9 @@
 def p(g):
- for t,k in(8,3),(9,2):
-  for n in range(t*t):
-   i=n//t;j=n%t
-   if all(all(R[j:j+k])for R in g[i:i+k]):
-    h=k/2-.5;i+=h;j+=h
-    for n in range(100):
-     if v:=g[y:=n//10][x:=n%10]:
-      y-=i;x-=j
-      for _ in"-"*3:x,y=-y,x;g[int(i+y)][int(j+x)]=v
-    return g
+ z=[(i+j*1j,v)for i,r in enumerate(g)for j,v in enumerate(r)if v];d={}
+ for a,w in z:
+  for b,v in z:
+   if w==v and a-b:t=(a+b*1j)/(1+1j);d[t]=d.get(t,0)+1
+ p=max(d,key=d.get)
+ for s,v in z:
+  for _ in"1234":g[int(s.real)][int(s.imag)]=v;s=(s-p)*1j+p
+ return g
