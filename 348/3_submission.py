d5d884d56d9af7c8dd15e2064609ecb4274df50b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 21:18:22 +0900

    タスク 348: 更新 (121 -> 117)

--- 348/3_submission.py
+++ 348/3_submission.py
@@ -1,1 +1,1 @@
-def p(g,E=enumerate):c=g[0].index(7);return[[(v,7+(j-c)%2)[abs(j-c)<str(g).count('7')-i]for j,v in E(r)]for i,r in E(g)]
+def p(g,E=enumerate):c=g[0].index(7);return[[(7+(j-c)%2)*(abs(j-c)<g.index(g[-1])-i)for j,_ in E(r)]for i,r in E(g)]
