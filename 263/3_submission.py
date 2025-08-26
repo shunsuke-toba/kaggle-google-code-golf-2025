94471532eb8c9a4b23cff4fd2ae62dc3b20264b9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 01:13:59 +0900

    タスク 263: 更新 (159 -> 157)

--- 263/3_submission.py
+++ 263/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[b:=[g[i:i+3]if g[3:]else[r[i:i+3]for r in g]for i in range(0,len(g+g[0])-3,3)],m:=[str(x).count('0')for x in b],b[m.index(min(m,key=m.count))]][2]
+p=lambda g:[b:=[g[3:]and g[i:i+3]or[r[i:i+3]for r in g]for i in range(0,len(g+g[0])-3,3)],m:=[str(x).count('0')for x in b],b[m.index(min(m,key=m.count))]][2]
