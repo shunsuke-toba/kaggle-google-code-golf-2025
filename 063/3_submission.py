3ff930a3f99f02c3a72ec376c138c3712dfd150c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 01:13:55 +0900

    タスク 063: 更新 (101 -> 98)

--- 063/3_submission.py
+++ 063/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[[x+3*(x<1)*(1-any(t[j]for t in g[1:-1])*any(r[1:-1]))for j,x in enumerate(r)]for r in g]
+p=lambda g:[[x or 3*(max(t[j]for t in g[1:-1])*max(r[1:-1])<1)for j,x in enumerate(r)]for r in g]
