dd65e458c887f4d3d5db2491709a002b2a38d762
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-26 13:05:29 +0900

    219: 改善(496 -> 491)

--- 219/3_submission.py
+++ 219/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,R=range):
- i=a=0
+ i=a=0;T=10
  while~-any(g[i]):i+=1
  f=i
  while any(g[i]):a+=1;i+=1
@@ -12,11 +12,11 @@ def p(g,R=range):
   for t in R(3):
    for S in R(s-a+n,s+1):
     m=1
-    if S>g[1][3]==a*4>g[7][7]>g[1][4]:g[12][4]=g[12][6]=g[12][8]=1;m=0
-    for j in R(a*10):
-     if g[S+j//10][j%10]>1>g[f+j//10][j%10-t]<=j%10-t:m=0
+    if S>g[1][3]>a*3>g[7][7]>g[1][4]:g[12][4]=g[12][6]=g[12][8]=1;m=0
+    for j in R(a*T):
+     if g[S+j//T][j%T]>1>g[f+j//T][j%T-t]<=j%T-t:m=0
     if m:
-     for j in R(a*10):k=j%10;j//=10;g[S+j][k]|=g[f+j][k-t]>1>g[S+j][k]<=k-t;M=1
+     for j in R(a*T):k=j%T;j//=T;g[S+j][k]|=g[f+j][k-t]>1>g[S+j][k]<=k-t;M=1
      break
    if M:break
  return g
