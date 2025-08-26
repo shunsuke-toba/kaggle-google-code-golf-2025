126ad95c0757ccfe207acb9855d0ae5cc0a0f330
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 21:18:19 +0900

    タスク 058: 更新 (141 -> 127)

--- 058/3_submission.py
+++ 058/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
  n=x=len(g)-1;y=0;d=1;g[0]=[3]*-~n
  while n>0:
-  for _ in[0]*n:y+=d;g[y][x]=3
-  for _ in[0]*n:x-=d;g[y][x]=3
+  for i in[1]*n+[0]*n:y+=i*d;x-=d-i*d;g[y][x]=3
   n-=2;d=-d
  return g
