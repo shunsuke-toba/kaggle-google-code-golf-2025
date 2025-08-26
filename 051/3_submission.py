6058d4962edcc059c34bc8a3296a102875e10a87
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 08:22:40 +0900

    タスク 051: 更新 (180 -> 178)

--- 051/3_submission.py
+++ 051/3_submission.py
@@ -2,6 +2,6 @@ def p(g):
  for _ in[0]*4:
   for a in g:
    for y,b in enumerate(a[1:]):
-    if b<1<2>sum(g,[]).count(a[y]):a[:y]=[i or a[y]for i in a[:y]]
+    if 2>sum(g,[]).count(a[y])>b:a[:y]=[i or a[y]for i in a[:y]]
   g=[*map(list,zip(*g[::-1]))]
  return g
