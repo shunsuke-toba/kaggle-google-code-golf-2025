c44b20bd4ed9349cc3d4a6b958438a1c3817825d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 21:18:20 +0900

    タスク 163: 更新 (158 -> 157)

--- 163/3_submission.py
+++ 163/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  i,k=divmod(sum(g,[]).index(4),11);x=i&3;y=k&3;h=[[5*(v==5)for v in r]for r in g]
- for t in 0,1,2:h[x*4+t][y*4:y*4+3]=g[i-x+t][k-y:k-y+3]
+ for t in 0,1,2:h[x*4+t][y*4:y*4+3]=g[i-x+t][k-y:][:3]
  return h
