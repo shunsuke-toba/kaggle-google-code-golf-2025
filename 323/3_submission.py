2163fb0bb6a1649760b29774bdf103bf7f045834
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-27 01:11:39 +0900

    323: 改善(141 -> 140)

--- 323/3_submission.py
+++ 323/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for s in-1,1:
   x,y=divmod(sum(g,[]).index(8),13);t=0
-  while 13>(x:=x-s*(t&2<1))>-1<(y:=y+s*(t&2>0))<13:g[x][y]=5;t+=1
+  while-1<(x:=x-s*(t&2<1))<13>(y:=y+s*(t&2>0))>-1:g[x][y]=5;t+=1
  return g
