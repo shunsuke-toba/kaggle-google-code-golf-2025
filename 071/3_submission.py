a1dcb0d315578cd8907ae28a5443b4e97e6c231a
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-26 21:19:04 +0900

    071: 改善(157 -> 148)

--- 071/3_submission.py
+++ 071/3_submission.py
@@ -1,7 +1,6 @@
-def p(g,E=enumerate):
- a=0
+def p(g,a=0):
  for r in g:
-  if a<1 and (a:=max(r)):s=r.index(a)+len(r)+~r[::-1].index(a)
-  for i,x in E(r):
+  if 1>a<(a:=max(r)):s=r.index(a)+len(r)+~r[::-1].index(a)
+  for i,x in enumerate(r):
    if 0<x!=a:r[i]=r[s-i]
  return g
