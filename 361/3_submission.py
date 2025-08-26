10dda83bd27924e9ae29497a0a4db767e45854e7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 07:14:03 +0900

    タスク 361: 更新 (272 -> 237)

--- 361/3_submission.py
+++ 361/3_submission.py
@@ -1,9 +1,5 @@
-def p(g):
- z=[(i+j*1j,v)for i,r in enumerate(g)for j,v in enumerate(r)if v];d={}
- for a,w in z:
-  for b,v in z:
-   if w==v and a-b:t=(a+b*1j)/(1+1j);d[t]=d.get(t,0)+1
- p=max(d,key=d.get)
+def p(g,E=enumerate):
+ z=[(i+j*1j,v)for i,r in E(g)for j,v in E(r)if v];p=max(t:=[(a+b*1j)/(1+1j)for a,w in z for b,v in z if w==v and a-b],key=t.count)
  for s,v in z:
-  for _ in"1234":g[int(s.real)][int(s.imag)]=v;s=(s-p)*1j+p
+  for _ in[0]*4:g[int(s.real)][int(s.imag)]=v;s=(s-p)*1j+p
  return g
