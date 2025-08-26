ca3fde51474fad7cb16fc77c3b76f4a082203630
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 02:14:18 +0900

    タスク 253: 更新 (171 -> 162)

--- 253/3_submission.py
+++ 253/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  k=[0]*4
  for g,h in zip(g,g[1:]):
-  for w in zip(g,g[1:],h,h[1:]):k[w.index(0)]+=max(w)*(w.count(0)<2)
- a,b,c,d=k;return(d,d,c,c),(d,0,0,c),(b,0,0,a),(b,b,a,a)
+  for w in zip(g,h,g[1:],h[1:]):k[w.index(0)]|=sorted(w)[1]
+ d,b,c,a=k;return(a,a,b,b),(a,0,0,b),(c,0,0,d),(c,c,d,d)
