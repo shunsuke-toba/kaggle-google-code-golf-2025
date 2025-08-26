d1b00807d4aab61f1f599e6a347f7976247f72a6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 02:14:17 +0900

    タスク 206: 更新 (173 -> 168)

--- 206/3_submission.py
+++ 206/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- a=b=s=t=9;e=enumerate;[v%5 and(a:=min(a,i),b:=min(b,j))or(s:=i-1,t:=j-1)for i,r in e(g)for j,v in e(r)if v]
+ a=b=9;e=enumerate;[v-5 and(a:=min(a,i),b:=min(b,j))or(s:=i-1,t:=j-1)for i,r in e(g)for j,v in e(r)if v]
  for k in 0,1,2:g[s+k][t:t+3]=g[a+k][b:b+3]
  return g
