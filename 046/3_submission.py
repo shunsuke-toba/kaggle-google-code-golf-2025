5891fc062bb2fc0c3e1968175a45b421a411af92
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-26 22:59:54 +0900

    タスク 046: 更新 (262 -> 259)

--- 046/3_submission.py
+++ 046/3_submission.py
@@ -4,6 +4,5 @@ def p(g):
   j=1
   while c[j:]and 5 not in c[j]:j+=1
   s,c=c[:j+1],c[j+1:];d=sum({*sum(s,())})-5;o+=(*s[0],5).index(5)-e;e=(*s[-1],5).index(5)
-  for t in s:
-   for y in 0,1,2:r[(y-o)%3]+=t[y]and d,
+  for y in 0,1,2:r[(y-o)%3]+=[t[y]and d for t in s]
  return r
