134957ca737670b33bf696c4ea585bf3359119b9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 22:48:13 +0900

    タスク 266: 更新 (131 -> 129)

--- 266/3_submission.py
+++ 266/3_submission.py
@@ -1,5 +1,4 @@
 def p(g):
- i=sum(g,[]).index(2)
- for j,c in(i-6,3),(i-4,6),(i+4,8),(i+6,7),(i,0):
+ for j,c in(i:=sum(g,[]).index(2),0),(i-6,3),(i-4,6),(i+4,8),(i+6,7):
   if-1<j<15>-2<j%5-i%5<2:g[j//5][j%5]=c
  return g
