59d52b9934d32ad9f5c6e3543e4a7d9cc3245c61
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 02:32:45 +0900

    タスク 319: 更新 (409 -> 396)

--- 319/3_submission.py
+++ 319/3_submission.py
@@ -4,8 +4,8 @@ def p(g,r=range,l=len):
   for _ in r(4):
    while c not in h[0]:h=h[1:]
    h=[*zip(*h[::-1])]
-  return[[x==c for x in t]for t in h]
+  return h
  for c in C:
   for d in C:
-   q=f(c);X,Y=l(u:=f(d)),l(u[0])
-   if(c-d)*any(all(q[i+x>>1][j+y>>1]==u[x][y]for x in r(X)for y in r(Y))for i in r(l(q)*2-X+1)for j in r(l(q[0])*2-Y+1)):return[[[B,c][x]for x in t]for t in q]
+   q=f(c);u=f(d);X,Y=l(u),l(u[0])
+   if(c-d)*any(all((q[i+x>>1][j+y>>1]==c)==(u[x][y]==d)for x in r(X)for y in r(Y))for i in r(l(q)*2-X+1)for j in r(l(q[0])*2-Y+1)):return[[[B,c][x==c]for x in t]for t in q]
