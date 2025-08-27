71ae1a686172b5863a1c08576e61d50f66f26a84
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 02:32:41 +0900

    タスク 102: 更新 (221 -> 220)

--- 102/3_submission.py
+++ 102/3_submission.py
@@ -4,6 +4,6 @@ def p(g):
   for y in R:
    M=g[y:y+L];I=M[1:-1]
    for x in R:
-    if 20*~-L==s(s(m[x:x+L])for m in M)>s(s(m[x+1:x+L-1])for m in I)<1:
+    if s(s(m[x+1:x+L-1])for m in I)<1>20*~-L-s(s(m[x:x+L])for m in M):
      for m in I:m[x+1:x+L-1]=[2]*(L-2)
  return g
