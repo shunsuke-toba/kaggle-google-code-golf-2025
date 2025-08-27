f674e38aa383b8d49981a69a06901fcf356911a1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 02:32:44 +0900

    タスク 270: 更新 (154 -> 153)

--- 270/3_submission.py
+++ 270/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  g=sum(g,[]);a=g.index
  for t in 3,7:
-  while t in g:p=a(8%t);k=a(t)-p;l=abs(k);g[p+k//(l//15 or l)],g[p+k]=-t,0
+  while t in g:p=a(8%t);k=a(t)-p;l=abs(k);g[p+k//(l//15or l)],g[p+k]=-t,0
  return[*zip(*[map(abs,g)]*15)]
