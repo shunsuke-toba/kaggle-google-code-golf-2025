a5b91edc84f998466b73a1f45b26f78014329e30
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 00:59:53 +0900

    タスク 077: 更新 (277 -> 275)

--- 077/3_submission.py
+++ 077/3_submission.py
@@ -1,9 +1,8 @@
 def p(g):
- e=enumerate
- S={(i,j)for i,r in e(g)for j,v in e(r)if v==2}
+ e=enumerate;S={(i,j)for i,r in e(g)for j,v in e(r)if v==2}
  while S:
-  T={S.pop()}
-  while U:=S&{(y+i//5-2,x+i%5-2)for i in range(25)for y,x in T}:T|=U;S-=U
-  y,x=zip(*T);a=min(x);b=max(x)+1
+  T=[S.pop()]
+  while U:=S&{(y+i//5-2,x+i%5-2)for i in range(25)for y,x in T}:T+=U;S-=U
+  y,x=zip(*T);a,b=min(x),max(x)+1
   for r in g[min(y):max(y)+1]:r[a:b]=[4>>(v==2)for v in r[a:b]]
  return g
