c3bffb9f9944a63330d155633a0199a0a80258aa
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 19:22:42 +0900

    タスク 204: 更新 (164 -> 163)

--- 204/3_submission.py
+++ 204/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,e=enumerate:[exec("m=n=(*a,0)[x:].index(0)-2\nwhile n:g[y+n][x+1:x+m+1]=[2|m%2*5]*m;n-=1")for y,a in e(g)for x,v in e(a)if y*g[y-1][x]+x*a[x-1]<v<2]and g
+p=lambda g,e=enumerate:[exec("m=n=(*a,0)[x:].index(0)-2\nwhile n:g[y+n][x+1:x-~m]=[2|m%2*5]*m;n-=1")for y,a in e(g)for x,v in e(a)if 2>v>y*g[y-1][x]+x*a[x-1]]and g
