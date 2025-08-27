f29d532b2ce822dd55e0519811f6383b8fc64606
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 21:21:46 +0900

    タスク 381: 更新 (115 -> 106)

--- 381/3_submission.py
+++ 381/3_submission.py
@@ -1,4 +1,6 @@
 def p(g):
  for r in g[1:9]:
-  if 2in r:i=r.index(2);j=10-r[::-1].index(2);r[i:j]=[x or 9for x in r[i:j]]
+  if 2in r:
+   k=r.index(2)
+   while 2in r[k+1:]:k+=1;r[k]=r[k]or 9
  return g
