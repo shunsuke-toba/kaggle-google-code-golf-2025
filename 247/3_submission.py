8388ff25833130ec6fee1f3472de145d7df5b5b9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 22:48:13 +0900

    タスク 247: 更新 (110 -> 106)

--- 247/3_submission.py
+++ 247/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):g=sum(zip(*g),());c=g.count;m=max(map(c,{*g}-{0}));return[[i for i in dict.fromkeys(g) if c(i)==m]]*m
+def p(g):g=sum(zip(*g),());c=g.count;m=max(map(c,{*g}-{0}));return[[i for i in{}.fromkeys(g)if c(i)==m]]*m
