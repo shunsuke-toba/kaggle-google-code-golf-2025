dc36b7b9fea1ef7d2cf0acda0fc7e1c431cfb3a8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 00:59:54 +0900

    タスク 157: 更新 (459 -> 451)

--- 157/3_submission.py
+++ 157/3_submission.py
@@ -1,16 +1,15 @@
 def p(g):
- r=range;d=1,0,-1,0,1;s=[]
- for I in r(10):
-  for J in r(15):
-   if g[I][J]>4:
-    t=[(0,0)];g[I][J]=0
-    for y,x in t:
+ r=range;d=1,0,-1,0,1;s=[];C=r(15)
+ for y in r(10):
+  for x in C:
+   if g[y][x]>4:
+    t=[(0,0)];g[y][x]=0
+    for a,b in t:
      for k in r(4):
-      u=I+y+d[k];v=J+x+d[k+1]
-      if 15>v>-1<u<10>g[u][v]>4:g[u][v]=0;t+=(u-I,v-J),
+      u=y+a+d[k];v=x+b+d[k+1]
+      if 15>v>-1<u<10>g[u][v]>4:g[u][v]=0;t+=(u-y,v-x),
     s+=t,
  while s:
-  t,I,J=max(((t,I,J)for t in s for I in(1,2)for J in r(15)if all(15>J+x and g[I+y][J+x]<1 for y,x in t)),key=lambda q:sum(q[1]+y<3 for y,x in q[0]))
-  for y,x in t:g[I+y][J+x]=1
-  s.remove(t)
+  _,k,i,j=min((-sum(i+a<3 for a,b in s[k]),k,i,j)for k in r(len(s))for i in(1,2)for j in C if all(0<=j+b<15 and g[i+a][j+b]<1 for a,b in s[k]))
+  for a,b in s.pop(k):g[i+a][j+b]=1
  return g
