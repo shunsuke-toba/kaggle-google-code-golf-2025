8bb05bd1af2331402b1b162077ab6ef95fc9b1dd
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-27 12:14:19 +0900

    219: 改善(381 -> 378)

--- 219/3_submission.py
+++ 219/3_submission.py
@@ -7,6 +7,6 @@ def p(g):
    while any(g[i]):i+=1;u-=1
    for x in range(9):
     t=x//u;s=x%u+i-a;w=m=s>g[1][3]>a*3>g[7][7]>g[1][4]
-    for j in range(60):k=j%10;p=j//10%a;g[12][j%6+4]|=j%2<w;m|=g[s+p][k]>1>g[f+p][k-t]<=k-t;g[s+p][k]|=g[f+p][k-t]>1>m==g[s+p][k]<=k-t<30<=j
+    for j in range(99):k=j%10;p=j%9%a;g[12][j%6+4]|=j%2<w;m|=g[s+p][k]>1>g[f+p][k-t]<=k-t;g[s+p][k]|=g[f+p][k-t]>1>m==g[s+p][k]<=k-t<50<j
     if~-m:break
  return g
