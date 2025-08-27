55a8ff30c8fe7fed9d86ed59042579bdbac3eea3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 08:09:36 +0900

    タスク 173: 更新 (274 -> 272)

--- 173/3_submission.py
+++ 173/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
- w=len(g[0])+2;g=sum(([0]+r+[0]for r in g),[0]*w)+[0]*w;P=range(w+1,~w+len(g))
+ w=len(g[0])+2;g=sum(([0,*r,0]for r in g),[0]*w)+[0]*w;P=range(w+1,~w+len(g))
  for j in P:
   for d in 1,w,w+1,w-1:
    if(a:=g[j+d])==g[j-d]>0<(c:=g[j]):
     for k in P:
-     if g[k]==c or g[k+d]==g[k-d]==a:g[k]=c;g[k+d]=g[k-d]=a
+     if c==g[k]or a==g[k+d]==g[k-d]:g[k]=c;g[k+d]=g[k-d]=a
  return[g[i:i+w-2]for i in P[::w]]
