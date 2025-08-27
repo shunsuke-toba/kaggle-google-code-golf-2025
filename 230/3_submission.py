72f49e26321a776275544a6ce6a30eb1ec87b8ab
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 21:21:43 +0900

    タスク 230: 更新 (144 -> 141)

--- 230/3_submission.py
+++ 230/3_submission.py
@@ -1,5 +1,1 @@
-def p(g):
- for r,a in enumerate(g):
-  for c,v in enumerate(a):
-   if v>4>g[r-1][c]+a[c-1]:g[r-1][c-1:c+3:3]=1,2;g[r+2][c-1:c+3:3]=3,4
- return g
+p=lambda g,e=enumerate:exec('for r,a in e(g):\n for c,v in e(a):\n  if v>4>g[r-1][c]+a[c-1]:g[r-1][c-1:c+3:3]=1,2;g[r+2][c-1:c+3:3]=3,4')or g
