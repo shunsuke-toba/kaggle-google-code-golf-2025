c7bcbf0f184acb2fb1abe0a68601903a67327055
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 23:39:44 +0900

    タスク 354: 更新 (114 -> 113)

--- 354/3_submission.py
+++ 354/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  for r in g:
-  while 5 in r:a=r.index(5);c=(r[a:]+[0]).index(0);r[a:a+c]=[max(g[0][a:a+c])]*c
+  while 5in r:a=r.index(5);c=(r[a:]+[0]).index(0);r[a:a+c]=[max(g[0][a:a+c])]*c
  return g
