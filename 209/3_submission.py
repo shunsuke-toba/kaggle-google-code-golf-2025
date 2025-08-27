2c0f167c2802899b49135d6cde0d257b57d2a2e2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 00:59:56 +0900

    タスク 209: 更新 (528 -> 524)

--- 209/3_submission.py
+++ 209/3_submission.py
@@ -1,19 +1,18 @@
 def p(g):
- A=min;B=max;Y=range
- m=len(g[0]);e=len(g)
+ m=len(g[0]);e=len(g);R=range;A=min;B=max
  a=e;c=i=m;b=d=f=j=0;I=()
- for t in Y(e*m):
+ for t in R(e*m):
   v=g[r:=t//m][o:=t%m]
   if v==4:a=A(a,r);b=B(b,r);c=A(c,o);d=B(d,o)
   elif v:
-   if b==a or r<b:I+=(r,o,v),
-   else:e=A(e,r);f=B(f,r);i=A(i,o);j=B(j,o)
+   if a<b<=r:e=A(e,r);f=B(f,r);i=A(i,o);j=B(j,o)
+   else:I+=((r,o,v),)
  h=f-e+1;w=j-i+1
  for k in 2,3,4:
-  for r in Y(a,b-h*k+2):
-   for o in Y(c,d-w*k+2):
+  for r in R(a,b-h*k+2):
+   for o in R(c,d-w*k+2):
     try:
      if all(g[e+(R-r)//k][i+(O-o)//k]==V for R,O,V in I):
-      for q in Y(h*k):g[r+q][o:o+w*k]=[g[e+q//k][i+p//k]for p in Y(w*k)]
+      for q in R(h*k):g[r+q][o:o+w*k]=[g[e+q//k][i+p//k]for p in R(w*k)]
       return[r[c:d+1]for r in g[a:b+1]]
     except:0
