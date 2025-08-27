eb8c9d9534d55dffd99d42be06a1a8b3ece27781
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 23:39:41 +0900

    タスク 221: 更新 (104 -> 100)

--- 221/3_submission.py
+++ 221/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(R:=range((n:=str(g).count('0'))*3))and[[g[i%3][j%3]*(i//3*n+j//3<9-n)for j in R]for i in R]
+p=lambda g:(R:=range(d:=str(g).count('0')*3))and[[g[i%3][j%3]*(i//3*d+j<27-d)for j in R]for i in R]
