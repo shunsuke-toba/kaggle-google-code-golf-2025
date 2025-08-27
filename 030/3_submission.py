c426d67613a7c5fc8fa7db6cd203d2cc6b060ac3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 19:22:40 +0900

    タスク 030: 更新 (148 -> 135)

--- 030/3_submission.py
+++ 030/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):b=bytes(sum(g,[]));f=b.rfind;r=f(1)//10;n=[[0]*10for _ in g];[n[k//10+r-f(v)//10].__setitem__(k%10,v)for k,v in enumerate(b)if v];return n
+def p(g):b=sum(g,[]);f=b.index;r=f(1);n=[[0]*10for _ in g];[n[(i+r-f(v))//10].__setitem__(i%10,v)for i,v in enumerate(b)if v];return n
