e9f4d9a8642ab7d3ef40619a83b5b43eea3678d1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 22:48:14 +0900

    タスク 371: 更新 (126 -> 123)

--- 371/3_submission.py
+++ 371/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- a=sum(g,[]);y,x=divmod((i:=a.index(1))+a.index(1,i+1)>>1,len(g[0]))
+ a=bytes(sum(g,[]));y,x=divmod(a.find(1)+a.rfind(1)>>1,len(g[0]))
  for k in-1,0,1:g[y+k][x]=g[y][x+k]=3
  return g
