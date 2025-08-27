9a2c2d0ba1232908e56febad4f9e030e1444ae3a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 22:48:15 +0900

    タスク 374: 更新 (145 -> 144)

--- 374/3_submission.py
+++ 374/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,x=1):
  for k in range(1584):
-  i,c,r=9-k//198,k%9,k%-11;g=[*map(list,zip(*g))]
-  if[5]*i==g[r][c:c+i]:g[r][c:c+i]=[x]*i;x=x*4%7
+  i=9-k//198;r=k%-11;k%=9;g=[*map(list,zip(*g))]
+  if[5]*i==g[r][k:k+i]:g[r][k:k+i]=[x]*i;x=x*4%7
  return g
