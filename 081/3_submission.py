c20c7903159530d5503c985788f105685d450938
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 07:18:13 +0900

    タスク 081: 更新 (125 -> 122)

--- 081/3_submission.py
+++ 081/3_submission.py
@@ -1,4 +1,3 @@
 def p(g):
- for i in range(36):
-  l=g[y:=i//6][(x:=i%6):x+2]+g[y+1][x:x+2];g[y+(i:=l.index(0))//2][x+i%2]+=sum(l)>23
+ for i in range(36):l=g[y:=i//6][(x:=i%6):x+2]+g[y+1][x:x+2];g[y+(i:=l.index(0))//2][x+i%2]+=sum(l)>23
  return g
