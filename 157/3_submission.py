4dd43c183ba24cfc552cf8794b79e0bfddd619cf
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 11:17:33 +0900

    タスク 157: 更新 (462 -> 459)

--- 157/3_submission.py
+++ 157/3_submission.py
@@ -7,7 +7,7 @@ def p(g):
     for y,x in t:
      for k in r(4):
       u=I+y+d[k];v=J+x+d[k+1]
-      if-1<v<15 and u<10>g[u][v]>4:g[u][v]=0;t+=(u-I,v-J),
+      if 15>v>-1<u<10>g[u][v]>4:g[u][v]=0;t+=(u-I,v-J),
     s+=t,
  while s:
   t,I,J=max(((t,I,J)for t in s for I in(1,2)for J in r(15)if all(15>J+x and g[I+y][J+x]<1 for y,x in t)),key=lambda q:sum(q[1]+y<3 for y,x in q[0]))
