229c680a795e5dc08c9cb105c1bf6f2e4f246bba
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-27 11:17:36 +0900

    タスク 285: 更新 (480 -> 476)

--- 285/3_submission.py
+++ 285/3_submission.py
@@ -1,12 +1,18 @@
 def p(g):
  o=[*map(list,g)];n=len(g);d=[(i//3-1,i%3-1)for i in range(9)if i^4]
  for k in range(n*n):
-  if(c:=o[i:=k//n][j:=k%n])>0:
-   o[i][j]=-c
-   for y,x in(q:=[(i,j)]):
+  i,j=divmod(k,n)
+  if(c:=o[i][j])>0:
+   o[i][j]=-c;q=[(i,j)]
+   for y,x in q:
     for a,b in d:
-     if n>(Y:=y+a)>-1<(X:=x+b)<n>c==o[Y][X]:o[Y][X]=-c;q+=(Y,X),
-   y,x=next(((y,x)for y,x in q if any(-c!=o[y+a][x+b]!=0 for a,b in d if a*b==0)),q[0]);p=[(r-y,c-x)for r,c in q]
+     Y=y+a;X=x+b
+     if n>Y>-1<X<n:
+      v=o[Y][X]
+      if v==c:o[Y][X]=-c;q+=(Y,X),
+      if v and c-abs(v) and a*b==0:s,t=y,x
+   p=[(r-s,c-t)for r,c in q]
    for a,b in d:
-    for u,w in p:g[y+a+(u,-u)[a&1]][x+b+(w,-w)[b&1]]|=abs(v:=o[y+a][x+b])*(v+c!=0)
+    if(v:=o[s+a][t+b])*(v+c):
+     for u,w in p:g[s+a+(u,-u)[a&1]][t+b+(w,-w)[b&1]]=abs(v)
  return g
