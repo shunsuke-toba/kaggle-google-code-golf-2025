9defcab187e505b5eb3c3f89a13b0004933eaf99
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-29 05:10:01 +0900

    264: 改善(250 -> 245)

--- 264/3_submission.py
+++ 264/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
- r=range;a=r(9);o=[[5]*9for _ in a]
- for y in r(len(g)-2):
-  for x in r(len(g[0])-2):
-   if all(t:=[g[y+i//3][x+i%3]for i in a]):
-    for i in a:o[(k:='ehinatqzu'.find(chr(97+sum(i*(t[i]!=5)for i in a))))-k%3+i//3][k%3*3+i%3]=t[i]
+ a=range(9);o=[[5]*9for _ in a]
+ for x in range(1300):
+  if all(t:=[g[x%97%(len(g)-2)+i//3][x%(len(g[0])-2)+i%3]for i in a]):
+   for i in a:o[(k:='ehinatqzu'.find(chr(97+sum(i*(t[i]!=5)for i in a))))-k%3+i//3][k%3*3+i%3]=t[i]
  return o
