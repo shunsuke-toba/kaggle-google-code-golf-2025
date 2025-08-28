ae245cc0ccc4a91255427c52346ac731065c8967
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 10:20:24 +0900

    タスク 044: 更新 (427 -> 409)

--- 044/3_submission.py
+++ 044/3_submission.py
@@ -5,10 +5,10 @@ def p(g):
    q=[i];g[i]=5;e=c<1
    for n in q:
     for m in n+1,n-1,n+10,n-10:
-     if-1<m<100 and 2>m%10-n%10>-2:
-      if(x:=g[m])==c:g[m]=5;q+=m,
-      elif e*(x-5):e=0
-     else:e=0
+     try:x=g[m]
+     except:e=0;continue
+     if x==c:g[m]=5;q+=m,
+     elif e*(x-5):e=0
    t=tuple(x-i for x in q)
    if e:d[t]=i
    elif r.count(c)==len(t)>1:b[t]=i,c
