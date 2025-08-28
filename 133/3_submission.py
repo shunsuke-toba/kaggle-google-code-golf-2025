7cf410b94d1bad578f44791cdd9e137917862eec
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 10:20:26 +0900

    タスク 133: 更新 (504 -> 500)

--- 133/3_submission.py
+++ 133/3_submission.py
@@ -1,9 +1,9 @@
 def p(g):
  r=range;l=len;h=l(g);w=l(g[0])
  def f(y,x):c=[(y,x,g[y][x])];g[y][x]=0;[c:=c+f(Y,X)for Y,X in((y+1,x),(y-1,x),(y,x+1),(y,x-1))if h>Y>=0<=X<w and g[Y][X]];return c
- C=[f(i//w,i%w)for i in r(h*w)if g[i//w][i%w]];B=min((c for c in C if c[2:]),key=l);*_,d=zip(*B);Y,X,p=min(B,key=lambda t:d.count(t[2]))
+ C=[f(i//w,i%w)for i in r(h*w)if g[i//w][i%w]];B=min([c for c in C if c[2:]],key=l);*_,d=zip(*B);Y,X,p=min(B,key=lambda t:d.count(t[2]))
  for c in C:
-  m=int(l(A:=[v[:2]for v in c if v[2]==p])**.5);a,b=min(A);L=sum({v[2]for v in c})-p
+  m=int(l(A:=[v[:2]for v in c if v[2]==p])**.5);a,b=min(A)
   for y,x,t in B:
-   for i in r(m):g[a+(y-Y)*m+i][(o:=b+(x-X)*m):o+m]=[[p,L][t!=p]]*m
+   for i in r(m):g[a+(y-Y)*m+i][(o:=b+(x-X)*m):o+m]=[[p,sum({v[2]for v in c})-p][t!=p]]*m
  return g
