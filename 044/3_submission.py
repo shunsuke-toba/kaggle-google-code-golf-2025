d20a04d1a5c6d027a386b3962982b61ad8c6a37b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 02:16:12 +0900

    タスク 044: 更新 (409 -> 404)

--- 044/3_submission.py
+++ 044/3_submission.py
@@ -1,18 +1,18 @@
 def p(g):
- g=sum(g,[]);r=g[:];d,b={},{}
- for i,c in enumerate(g):
-  if c^5:
-   q=[i];g[i]=5;e=c<1
-   for n in q:
-    for m in n+1,n-1,n+10,n-10:
-     try:x=g[m]
-     except:e=0;continue
-     if x==c:g[m]=5;q+=m,
-     elif e*(x-5):e=0
+ g=sum(g,[]);r=g[:];d,e={},{}
+ for i,v in enumerate(g):
+  if v^5:
+   q=[i];g[i]=5;f=v<1
+   for j in q:
+    for k in j+1,j-1,j+10,j-10:
+     try:x=g[k]
+     except:x=5;f=0
+     if x==v:g[k]=5;q+=k,
+     elif f*(x-5):f=0
    t=tuple(x-i for x in q)
-   if e:d[t]=i
-   elif r.count(c)==len(t)>1:b[t]=i,c
- for t in b.keys()&d:
-  i,c=b[t]
-  for o in t:r[i+o]=0;r[d[t]+o]=c
+   if f:d[t]=i
+   elif r.count(v)==len(t)>1:e[t]=i,v
+ for t in e.keys()&d:
+  i,v=e[t]
+  for o in t:r[i+o]=0;r[d[t]+o]=v
  return[*zip(*[iter(r)]*10)]
