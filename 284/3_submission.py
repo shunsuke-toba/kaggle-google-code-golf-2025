ab1a3224dac63f13cd08dd5fd8b3358b8f4fae8b
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-28 21:10:48 +0900

    284: 改善(262 -> 256)

--- 284/3_submission.py
+++ 284/3_submission.py
@@ -1,8 +1,8 @@
-def p(g):
- (a,b),(d,e)=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v]
+def p(g,e=enumerate):
+ (a,b),(d,e)=[(i,j)for i,r in e(g)for j,v in e(r)if v]
  if a-d:return[*zip(*p([*map(list,zip(*g))]))]
  h=e-b>>1
  for x,s in(b,1),(e,-1):
-  z=g[a][x];u=x+s*h;g[a][x:u:s]=[z]*h
+  u=x+s*h;g[a][x:u:s]=[z:=g[a][x]]*h
   for r in g[a-2:a+3]:r[u-s]=g[a-2][u]=g[a+2][u]=z
  return g
