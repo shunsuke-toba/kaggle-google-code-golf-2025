1b3c68dce26665fe7862f943c5709aec016464e3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 22:16:02 +0900

    タスク 035: 更新 (142 -> 140)

--- 035/3_submission.py
+++ 035/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  l=g[3].index(8)
  for k,x in enumerate(sum(g,[])):
-  if x&7:g[max(min(k//10,g.index([0]*10,3)-1),3)][max(min(k%10,5),l)]=x
+  if x&7:g[max(min(k//10,g.index(g[1],3)-1),3)][max(min(k%10,5),l)]=x
  return g
