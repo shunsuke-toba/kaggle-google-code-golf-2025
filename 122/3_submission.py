24c9a98a09aa94dc85c193573595481813d57b58
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 22:16:04 +0900

    タスク 122: 更新 (145 -> 144)

--- 122/3_submission.py
+++ 122/3_submission.py
@@ -1,4 +1,3 @@
 def p(g,e=enumerate):
- h=(g[0]+g[1]).count(3)==1
- for i,j in[(i,j)for i,r in e(g)for j,v in e(r)if v%3]:g[i][j]^=2;g[i+2*h][j+2-2*h]^=2
+ for i,j,h in[(i,j,(g[0]+g[1]).count(3)==1)for i,r in e(g)for j,v in e(r)if v%3]:g[i][j]^=2;g[i+2*h][j+2-2*h]^=2
  return g
