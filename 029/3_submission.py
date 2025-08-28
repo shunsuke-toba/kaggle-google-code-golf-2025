b9912b8c6ba66da8d9e0912bc2527a6f0fe367ff
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 02:16:12 +0900

    タスク 029: 更新 (178 -> 167)

--- 029/3_submission.py
+++ 029/3_submission.py
@@ -2,4 +2,4 @@ def p(g):
  b=bytes(sum(g,[]));w=len(g[0])
  for k in b:
   n=b.find(k);m=b.rfind(k);d=m%w-n%w
-  if{*b[n+1:n+d]+b[n:m:w]+b[n+d:m:w]}<={k}:return[r[n%w+1:m%w]for r in g[n//w+1:m//w]]
+  if{*b[n+1:n+d]+b[n:m:w]}<={k}:return[r[n%w+1:m%w]for r in g[n//w+1:m//w]]
