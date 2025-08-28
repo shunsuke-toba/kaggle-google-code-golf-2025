004bc9bc68dda224976fee086b35c33786cf4104
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 10:20:26 +0900

    タスク 158: 更新 (404 -> 400)

--- 158/3_submission.py
+++ 158/3_submission.py
@@ -1,11 +1,11 @@
 def p(g,R=range):
- w=len(g[0]);t=0;u=w-2
- while len({*(p:=sum([r[t//u:][:3]for r in g[t%u:][:3]],[]))})<4:t+=1
+ W=len(g[0])-2;t=0
+ while len({*(p:=sum([r[t//W:][:3]for r in g[t%W:][:3]],[]))})<4:t+=1
  for s in 3,2,1:
   u=R(z:=3*s);r=[[p[i//s*3+j//s]for j in u]for i in u]
   for _ in R(4):
    for y in R(len(g)+1-z):
-    for x in R(w+1-z):
+    for x in R(W+3-z):
      if all((g[-1][0],r[i][j])[p.count(r[i][j])<2]==g[y+i][x+j]for i in u for j in u):
       for i in u:g[y+i][x:x+z]=r[i]
    r=[*zip(*r[::-1])]
