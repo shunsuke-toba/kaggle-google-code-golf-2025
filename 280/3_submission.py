15fe33c203147d6806ac9bff0dca782b8165bc9e
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-29 00:04:52 +0900

    280: 改善(213 -> 212)

--- 280/3_submission.py
+++ 280/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
- for x in range(2800):
-  n=len(g);i=x%n;j=x%~-n;a=0;k=n-j
+ for i in range(2800):
+  n=len(g);j=i%~-n;i%=n;a=0;k=n-j
   if 3>g[i][j]>g[i][j+1]:
    while g[i][j-a-1]:a+=1;g[i+a][j:]=g[i-a][j:]=[3]*k
    g[i][j:]=[2]*k
-  if x%7:g=[*map(list,zip(*g[::-1]))]
+  if i%4:g=[*map(list,zip(*g[::-1]))]
  return g
