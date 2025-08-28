f80ee3d70e53f538c07f0b075963267e9c547606
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 07:24:45 +0900

    タスク 101: 更新 (461 -> 455)

--- 101/3_submission.py
+++ 101/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- R=range;l=len;H=l(g);W=l(g[0])
+ R=range;l=len;H,W=l(g),l(g[0])
  p=next(x for r in R(H)for c in R(W)for s in R(H,r,-1)for t in R(W,c,-1)if all(map(any,(x:=[a[c:t]for a in g[r:s]])+[*zip(*x)]))*sum({*sum(x,[])})>2)
- for S in 3,2,1:h=l(p)*S;w=l(p[0])*S;[all((p[i//S][j//S]>1)==(W>x+j>-1<y+i<H>g[y+i][x+j]>1)for i in R(h)for j in R(w))and[W>x+j>-1<y+i<H and g[y+i].__setitem__(x+j,-p[i//S][j//S])for i in R(h)for j in R(w)]for y in R(H)for x in R(-w,W)]
+ for S in 3,2,1:h=l(p)*S;w=l(p[0])*S;[all((p[i//S][j//S]>1)==(W>x+j>=0<=g[y+i][x+j]>1)for i in R(h)for j in R(w))and[W>x+j>=0 and g[y+i].__setitem__(x+j,-p[i//S][j//S])for i in R(h)for j in R(w)]for y in R(H-h+1)for x in R(1-w,W)]
  return[[*map(abs,r)]for r in g]
