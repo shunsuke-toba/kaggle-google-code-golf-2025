81058a4e9bab9fbc7c23ebb7a2acb24d04e30080
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 19:32:09 +0900

    タスク 185: 更新 (189 -> 188)

--- 185/3_submission.py
+++ 185/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):l=max(g[0]);Z=zip;R,C=[[i for i,x in enumerate(m)if{*x}-{0,l}]for m in(g,Z(*g))];return [[(v:=g[a][b])*(v==g[a][d]==g[c][b]==g[c][d]!=l)for b,d in Z(C,C[1:])]for a,c in Z(R,R[1:])]
+def p(g):l=max(g[0]);R,C=[[i for i,x in enumerate(m)if{*x}-{0,l}]for m in(g,zip(*g))];return[[(v:=g[a][b])*(v==g[a][d]==g[c][b]==g[c][d]!=l)for b,d in zip(C,C[1:])]for a,c in zip(R,R[1:])]
