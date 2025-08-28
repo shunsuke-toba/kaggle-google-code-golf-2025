5ccbffde3e80889b25842d642e72bad3fdcc6376
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 07:24:48 +0900

    タスク 255: 更新 (489 -> 462)

--- 255/3_submission.py
+++ 255/3_submission.py
@@ -1,13 +1,13 @@
-def p(g,n=30,t=lambda x:[*map(list,zip(*x[::-1]))]):
- R=range(n);k=max(sum(g,[]))
+def p(g,t=lambda x:[*map(list,zip(*x[::-1]))]):
+ r=range(30);k=max(sum(g,[]))
  for _ in[0]*4:
-  a=[i for i in R if k in g[i][5:]]
-  for i in R:
-   d=next((t for t in a if t>i),0);u=next((t for t in a[::-1]if t<i),n)
-   if(d-i>1<i-u)*d-u>6:g[i][5:]=[n]*25
+  a=[i for i in r if k in g[i][5:]]
+  for i in r:
+   d=next((j for j in a if j>i),0);u=next((j for j in a[::-1]if j<i),30)
+   if(d-i>1<i-u)*d-u>6:g[i][5:]=[10]*25
   g=t(g)
  for _ in[0]*4:
-  for i in R:
-   if all(next((v for v in r if(v-3)*v),0)>9for r in(g[i-(i>0)],g[i],g[i+(i<29)])):p=next((j for j,x in enumerate(g[i])if x));g[i][:p]=[3]*p
+  for i in r:
+   if all(next((v for v in b if(v-3)*v),0)>9for b in g[i-(i>0):i+2]):p=next(j for j in r if g[i][j]);g[i][:p]=[3]*p
   g=t(g)
- return[[(x,3)[x>9]for x in r]for r in g]
+ return[[(x,3)[x>9]for x in b]for b in g]
