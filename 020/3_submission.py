8ed7cdbde41ef26b519cfc8718a1141dd1076904
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 19:32:05 +0900

    タスク 020: 更新 (166 -> 153)

--- 020/3_submission.py
+++ 020/3_submission.py
@@ -1,4 +1,5 @@
 def p(g):
  m=[*map(any,g)].index(1);n=[*map(any,zip(*g))].index(1)
- for k in range(25):j=k%5;i=k//5;g[m+i][n+j]|=g[m+4-j][n+i]|g[m+4-i][n+4-j]|g[m+j][n+4-i]
+ for _ in'123':
+  for k in range(25):j=k%5;i=k//5;g[m+i][n+j]|=g[m+4-j][n+i]
  return g
