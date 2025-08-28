80cb2bbafb9e51d0e7eb2568ab68fb02563bafa0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 07:24:50 +0900

    タスク 356: 更新 (165 -> 151)

--- 356/3_submission.py
+++ 356/3_submission.py
@@ -1,8 +1,3 @@
 def p(g):
- for _ in g,g:
-  for r in g:
-   a=9
-   for i in range(10):
-    if r[i]%9:r[a:i]=[9]*(i-a);a=i+1
-  g=[*map(list,zip(*g))]
- return[[x&8for x in r]for r in g]
+ R=range(10);v=[[any(c[:i+1])*any(c[i:])*8for i in R]for c in zip(*g)]
+ return[[v[j][i]|any(g[i][:j+1])*any(g[i][j:])*8for j in R]for i in R]
