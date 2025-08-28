628361dba33f83b89b2b59b107bee0b89fdf7dbd
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 23:18:13 +0900

    タスク 256: 更新 (104 -> 103)

--- 256/3_submission.py
+++ 256/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for y in range(b:=sum(g[a:=g.index(max(g))])//2+a):g[y][:b-y]=[2+(y<a)-(y>a)]*(b-y)
+ for y in range(b:=sum(g[a:=g.index(max(g))])//2+a):g[y][:b]=[2+(y<a)-(y>a)]*b;b-=1
  return g
