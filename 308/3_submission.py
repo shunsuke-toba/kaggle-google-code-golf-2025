daea542836e851cda78829cb3cd59739c2dd407f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 10:20:29 +0900

    タスク 308: 更新 (273 -> 272)

--- 308/3_submission.py
+++ 308/3_submission.py
@@ -3,7 +3,7 @@ def p(g,E=enumerate):
  for i,x in E(g):
   for j,v in E(x):
    if v-b:s[v]+=i;s[~v]+=j;p+=(i,j,v),
- p=[(i-s[v]//4,j-s[~v]//4,v)for i,j,v in p];m=max(p)[0]
+ m=max(p:=[(i-s[v]//4,j-s[~v]//4,v)for i,j,v in p])[0]
  k=2*m+1;o=[[b]*k for _ in[0]*k]
  for i,j,v in p:o[m+i][m+j]=v
  return o
