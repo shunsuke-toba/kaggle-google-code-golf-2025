2506b1b1cf9ed61251ef14e32272d877e3e8369a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 22:16:06 +0900

    タスク 342: 更新 (141 -> 134)

--- 342/3_submission.py
+++ 342/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- a=sum(g,[]);r,c=divmod(a.index(8),10);g=[10*[0]for _ in g]
- for i,v in enumerate(a):g[r+(i//10>r)][c+(i%10>c)]+=v*(v!=8)
+ a=sum(g,[]);i=a.index(8);g=[10*[0]for _ in g]
+ for j,v in enumerate(a):g[i//10+(j>i)][i%10+(j%10>i%10)]+=v*(v!=8)
  return g
