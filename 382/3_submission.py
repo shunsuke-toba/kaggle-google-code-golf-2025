e3a2ccc637adf5c9cd937300a75a6e217ddf99e4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 19:32:10 +0900

    タスク 382: 更新 (217 -> 213)

--- 382/3_submission.py
+++ 382/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
  z=lambda:[*map(list,zip(*g))];s=0
  if t:=2in g[0]+g[-1]:g=z()
- a=2in z()[-1];b=8in g[-1];f=lambda g:[r[::1-2*a]for r in g[::1-2*b]];g=f(g)
+ a=2in z()[-1];b=8in g[-1];f=lambda:[r[::1-2*a]for r in g[::1-2*b]];g=f()
  for r in g:s+=r[0]>0;r[s:]=g[0][:-s or 99]
- return(g:=f(g),z())[t]
+ return(g:=f(),z())[t]
