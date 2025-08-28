81c7520b5e687b9d59467dc67011e515d51ed941
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 10:20:28 +0900

    タスク 255: 更新 (494 -> 490)

--- 255/3_submission.py
+++ 255/3_submission.py
@@ -1,4 +1,4 @@
-def p(g,E=enumerate,n=30,T=lambda x:[*map(list,zip(*x[::-1]))]):
+def p(g,n=30,T=lambda x:[*map(list,zip(*x[::-1]))]):
  R=range(n);k=max(sum(g,[]))
  for _ in[0]*4:
   a=[i for i in R if k in g[i][5:]]
@@ -8,6 +8,6 @@ def p(g,E=enumerate,n=30,T=lambda x:[*map(list,zip(*x[::-1]))]):
   g=T(g)
  for _ in[0]*4:
   for i in R:
-   if all(next((v for v in r if(v-3)*v),0)>9for r in(g[i-(i>0)],g[i],g[i+(i+1<n)])):p=next((j for j,x in E(g[i])if x));g[i][:p]=[3]*p
+   if all(next((v for v in r if(v-3)*v),0)>9for r in(g[i-(i>0)],g[i],g[i+(i+1<n)])):p=next((j for j,x in enumerate(g[i])if x));g[i][:p]=[3]*p
   g=T(g)
  return[[(x,3)[x>9]for x in r]for r in g]
