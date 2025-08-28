18a6e3dc9da5f4619220cfcf272450b3a0781775
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 07:24:43 +0900

    タスク 025: 更新 (219 -> 213)

--- 025/3_submission.py
+++ 025/3_submission.py
@@ -1,8 +1,6 @@
 def p(g):
- z=zip;o=[[0]*len(g)for g in g]
- for _ in 0,1:
-  for x,k in enumerate(t:=[*z(*g)]):
-   if{*k}=={c:=k[0]}!={0}:
-    for r,q in z(g,o):q[x-(c in r[:x])]=q[x+(c in r[x+1:])]=c
-  g,o=t,[*map(list,z(*o))]
- return o
+ t=[*zip(*g)];o=[[0]*len(g)for g in g]
+ for x,k in enumerate(t):
+  if{*k}=={c:=k[0]}!={0}:
+   for r,q in zip(g,o):q[x-(c in r[:x])]=q[x+(c in r[x+1:])]=c
+ return any(o[0])and o or [*map(list,zip(*p(t)))]
