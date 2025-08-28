c62a9b9ae21d96f0d447bbe180fbfa07f51af878
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 07:24:51 +0900

    タスク 390: 更新 (194 -> 193)

--- 390/3_submission.py
+++ 390/3_submission.py
@@ -1,3 +1,5 @@
 def p(g,e=enumerate):
- for y,x in[(i,j)for i,r in e(g)for j,v in e(r)if v&4]:_,i,j=min((abs(x-j+y-i),i,j)for i,r in e(g)for j,v in e(r)if(i==y or j==x)*v&2);g[2*i-y][2*j-x],g[y][x]=5,0
+ a=[(i,j,v)for i,r in e(g)for j,v in e(r)]
+ for y,x,v in a:
+  if v&4:_,i,j=min((abs(x-j+y-i),i,j)for i,j,u in a if(i==y or j==x)*u&2);g[2*i-y][2*j-x],g[y][x]=5,0
  return g
