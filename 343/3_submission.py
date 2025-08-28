1bbc9c6d6c5bc72c512c62d2b5ad86b0ea36c2bf
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 23:18:14 +0900

    タスク 343: 更新 (102 -> 101)

--- 343/3_submission.py
+++ 343/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- *c,=zip(*g);r=g[4].index(0);n=1
- while c[n:r]!=c[:r-n]:n+=1
- return[*zip(*(c[:n]*15)[:15])]
+ *c,=zip(*g);i=g[4].index(0);n=1
+ while c[n:i]!=c[:i-n]:n+=1
+ return*zip(*(c[:n]*15)[:15]),
