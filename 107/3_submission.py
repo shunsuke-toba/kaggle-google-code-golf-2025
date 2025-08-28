062711dcca2eea1acd5d0fc1551ea81d9408506c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 07:24:45 +0900

    タスク 107: 更新 (217 -> 215)

--- 107/3_submission.py
+++ 107/3_submission.py
@@ -2,6 +2,6 @@ def p(g):
  n=len({*g[4]})+1;r=range(5*n);g=[[g[x//n][y//n]for y in r]for x in r]
  for s in-1,1:
   for t in-1,1:
-   x=n*((g[0][n]<1)+s+1)+s//2;y=n*((g[n][0]<1)+t+1)+t//2
+   x=n*(s-~(g[0][n]<1))+s//2;y=n*(t-~(g[n][0]<1))+t//2
    while g[x][y]<1:g[x][y]=2;x+=s;y+=t
  return g
