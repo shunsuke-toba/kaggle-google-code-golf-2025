7148657639a719e1ffe1db851d4a34410dabb396
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 23:18:12 +0900

    タスク 124: 更新 (124 -> 123)

--- 124/3_submission.py
+++ 124/3_submission.py
@@ -2,5 +2,5 @@ def p(g):
  for k in 2,3:
   for d in 0,1,2:
    o=g[:k]
-   while len(o)<10:o+=d*[0]+o[-k][:10-d],
-   if o[:len(g)]==g:return o
+   while o[:9]==o:o+=d*[0]+o[-k][:10-d],
+   if g==o[:len(g)]:return o
