6e5ff2f23705405a0c4186e97744384ae4fcf4eb
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 02:16:17 +0900

    タスク 285: 更新 (454 -> 438)

--- 285/3_submission.py
+++ 285/3_submission.py
@@ -1,16 +1,14 @@
 def p(g):
  o=[*map(list,g)];n=len(g);d=[(i//3-1,i%3-1)for i in range(9)if i^4]
  for k in range(n*n):
-  i,j=divmod(k,n)
-  if(c:=o[i][j])>0:
+  i=k//n;j=k%n;c=o[i][j]
+  if c>0:
    o[i][j]=-c;q=[(i,j)]
    for y,x in q:
     for a,b in d:
-     Y=y+a;X=x+b
-     if n>Y>-1<X<n:
-      v=o[Y][X]
-      if v==c:o[Y][X]=-c;q+=(Y,X),
-      elif v*(c-abs(v))*(a*b==0):s,t=y,x
+     Y=y+a;X=x+b;v=n>Y>-1<X<n and o[Y][X]
+     if v==c:o[Y][X]=-c;q+=(Y,X),
+     elif v*(c-abs(v))*(a*b==0):s,t=y,x
    for a,b in d:
     if(v:=o[s+a][t+b])*(v+c):
      for y,x in q:g[s+a+((y-s,s-y)[a&1])][t+b+((x-t,t-x)[b&1])]=abs(v)
