0f4510d4b2b42439fe37f14a5357614494d2f879
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 23:18:11 +0900

    タスク 121: 更新 (106 -> 105)

--- 121/3_submission.py
+++ 121/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):f=sum(g,[]);i=f.index(8);g=[f[j:j+3]for j in(i-14,i-1,i+12)];g[1][1]=sum({*sum(g,[])})-8;return g
+def p(g):f=sum(g,[]);i=f.index(8);g=[f[i+d:][:3]for d in(-14,-1,12)];g[1][1]=sum({*sum(g,[])})-8;return g
