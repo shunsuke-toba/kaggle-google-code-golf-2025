c4957004a39d8d8a436c3c204d4164308365468e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 19:32:06 +0900

    タスク 059: 更新 (176 -> 174)

--- 059/3_submission.py
+++ 059/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- w,r=[0]*9,range(11);i=0
- for v in sum(g,[]):
+ w,r=[0]*9,range(11)
+ for i,v in enumerate(sum(g,[])):
   if v%5:w[i//44*3+i%11//4]+=1;E=v
-  i+=1
- return[[5*(i%4>2 or j%4>2)or E*(w[i//4*3+j//4]==max(w))for j in r]for i in r]
+ return[[5*(g[i][j]==5)or E*(w[i//4*3+j//4]==max(w))for j in r]for i in r]
