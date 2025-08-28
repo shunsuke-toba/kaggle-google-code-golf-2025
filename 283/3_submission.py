c2d172fca02a3f4d9d849f11676278a53411fdf0
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-28 22:37:12 +0900

    283: 改善(109 -> 108)

--- 283/3_submission.py
+++ 283/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,z=[[0]*10]:[[a*sum(m)%22%5 for a,*m in zip(r,r[1:]+[0],[0]+r,u,d)]for u,r,d in zip(z+g,g,g[1:]+z)]
+p=lambda g,z=[[0]*10]:[[a*sum(m)%22%5for a,*m in zip(r,r[1:]+[0],[0]+r,u,d)]for u,r,d in zip(z+g,g,g[1:]+z)]
