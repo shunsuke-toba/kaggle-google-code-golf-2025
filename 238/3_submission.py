de85c7b9db44e68af51941f053449df779673c19
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 10:20:28 +0900

    タスク 238: 更新 (322 -> 319)

--- 238/3_submission.py
+++ 238/3_submission.py
@@ -1,5 +1,4 @@
 def p(g):
- a=c=d=e=99;f=enumerate;[v&7 and(a:=min(a,b:=i),c:=min(c,j))or v and(d:=min(d,i),e:=min(e,j))for i,r in f(g)for j,v in f(r)];r=range(n:=b+~a);G=[x[c:c+n+2]for x in g[a:b+1]]
- for i in r:
-  for j in r:v=g[d+i][e+j];G[i+1][j+1]=v*(i-j)*~(i+j-n)and(G[0][1],G[1][-1],G[1][0],G[-1][1])[(i>j)*2+(i+j>=n)]or v
+ a=c=d=e=99;f=enumerate;[v%8and(a:=min(a,b:=i),c:=min(c,j))or v and(d:=min(d,i),e:=min(e,j))for i,r in f(g)for j,v in f(r)];n=b+~a;G=[x[c:c+n+2]for x in g[a:b+1]]
+ for k in range(n*n):i=k//n;j=k%n;v=g[d+i][e+j];G[i+1][j+1]=v*(i-j)*~(i+j-n)and(G[0][1],G[1][-1],G[1][0],G[-1][1])[(i>j)*2+(i+j>=n)]or v
  return G
