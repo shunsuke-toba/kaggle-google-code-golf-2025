a3b58afc05fec0370f57557d9e34b90fc00732c3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 02:16:16 +0900

    タスク 209: 更新 (520 -> 518)

--- 209/3_submission.py
+++ 209/3_submission.py
@@ -1,17 +1,17 @@
 def p(g):
- m=len(g[0]);e=len(g);R=range;A=min;B=max
- a=e;c=i=m;b=d=f=j=0;I=[]
- for t in R(e*m):
+ m=len(g[0]);E=len(g);R=range;A=min;B=max
+ a=e=E;c=i=m;b=d=f=j=0;L=[]
+ for t in R(E*m):
   v=g[r:=t//m][o:=t%m]
   if v==4:a=A(a,r);b=B(b,r);c=A(c,o);d=B(d,o)
-  elif v and a<b<=r:e=A(e,r);f=B(f,r);i=A(i,o);j=B(j,o)
-  elif v:I+=[(r,o,v)]
- h=f-e+1;w=j-i+1
+  elif v and r>=b>a:e=A(e,r);f=B(f,r);i=A(i,o);j=B(j,o)
+  elif v:L+=[(r,o,v)]
+ h,w=f-e+1,j-i+1
  for k in 2,3,4:
   for r in R(a,b-h*k+2):
    for o in R(c,d-w*k+2):
     try:
-     if all(g[e+(R-r)//k][i+(O-o)//k]==V for R,O,V in I):
-      for q in R(h*k):g[r+q][o:o+w*k]=[g[e+q//k][i+p//k]for p in R(w*k)]
-      return[r[c:d+1]for r in g[a:b+1]]
+     for x,y,z in L:assert g[e+(x-r)//k][i+(y-o)//k]==z
+     for q in R(h*k):g[r+q][o:o+w*k]=[g[e+q//k][i+p//k]for p in R(w*k)]
+     return[r[c:d+1]for r in g[a:b+1]]
     except:0
