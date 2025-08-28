a821eda93bd0e3c2ed9f1c38d9ab1d6de2a8e20b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 07:24:50 +0900

    タスク 366: 更新 (662 -> 659)

--- 366/3_submission.py
+++ 366/3_submission.py
@@ -1,6 +1,5 @@
 def p(g,E=enumerate,R=range,L=len,F=filter):
- s=sum(g,[]);*_,z,y,X=sorted({*s},key=s.count);t=lambda q:[*map(list,zip(*F(q,zip(*F(q,g)))))]
- a,b=[t(lambda r:c in r)for c in(y,X)];n,m,p=L(a)-1,L(a[0])-1,[]
+ s=sum(g,[]);*_,z,y,X=sorted({*s},key=s.count);a,b=[[*map(list,zip(*F(lambda r:c in r,zip(*F(lambda r:c in r,g)))))]for c in(y,X)];n,m,p=L(a)-1,L(a[0])-1,[]
  for i in R(n):
   for j in R(m):
    if a[i][j]-y:
@@ -8,7 +7,7 @@ def p(g,E=enumerate,R=range,L=len,F=filter):
     while k<n and a[k+1][l]-y:k+=1
     while l<m and a[k][l+1]-y:l+=1
     q=a[i:k+1];p+=[r[j:l+1]for r in q],
-    for r in q:r[j:l+1]=[y]*(l-j+1)
+    for r in q:r[j:l+1]=[y]*-~(l-j)
  for r in sorted(p,key=lambda r:-sum(c!=z for c in sum(r,[]))):
   n,m=L(r),L(r[0])
   for i in R(L(b)-n+1):
