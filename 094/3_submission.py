31192b6e7ff34c3c2a795983e9d379c7d89ebdb9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 22:16:03 +0900

    タスク 094: 更新 (138 -> 136)

--- 094/3_submission.py
+++ 094/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
  for _ in 0,1:
-  L=[r for r in range(15)if 1in g[r]]
-  for r in L[2],L[-3]:g[r]=[c-c//8*2for c in g[r]]
+  for r in(L:=[i for i in range(15)if 1in g[i]])[2],L[-3]:g[r]=[c-c//8*2for c in g[r]]
   g=[*zip(*g)]
  return g
