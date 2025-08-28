2319e2d6d29ba6a53a628b70c0858023f36f00ae
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 02:16:12 +0900

    タスク 055: 更新 (151 -> 145)

--- 055/3_submission.py
+++ 055/3_submission.py
@@ -1,4 +1,3 @@
 def p(g):
  i=g[0].index;c=i(8);d=i(8,c+1);k=0
- for r in g:k+=r[0]>0;r[:]=r[0]and r or[k%2*4]*c+[8]+[(4*k+2)%9]*(d+~c)+[8]+[k%2*3]*(len(r)+~d)
- return g
+ return[(r[0]and(k:=k+1)and r)or[k%2*4]*c+[8]+[(4*k+2)%9]*(d+~c)+[8]+[k%2*3]*(len(r)+~d)for r in g]
