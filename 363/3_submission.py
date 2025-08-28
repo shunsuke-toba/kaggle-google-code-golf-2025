06b291d02f2c187abfc17c264265414ad6203782
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 06:09:37 +0900

    タスク 363: 更新 (260 -> 259)

--- 363/3_submission.py
+++ 363/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- r=range(10);p=[(i,j)for i in r for j in r if g[i][j]&2];a,b=p[0];v=[(i,j)for i in r for j in r if all(i+x-a<10>j+y-b>=0==g[i+x-a][j+y-b]for x,y in p)];v=v[v==[(1,3),(5,6)]:];v[1:3]*=not v[5:]
+ r=range(10);p=[(i,j)for i in r for j in r if g[i][j]&2];a,b=p[0];v=[(i,j)for i in r for j in r if all(i+x-a<10>j+y-b>=0==g[i+x-a][j+y-b]for x,y in p)];v=v[v==[(1,3),(5,6)]:];v[1:3]*=v[:5]==v
  for i,j in v:
   for x,y in p:g[i+x-a][j+y-b]=2
  return g
