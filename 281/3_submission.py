d1dac3b391d1d3a743d29b255aa22ff11ef72d51
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-28 23:07:28 +0900

    281: 改善(231 -> 227)

--- 281/3_submission.py
+++ 281/3_submission.py
@@ -2,7 +2,7 @@ def p(g):
  a=c=9;d=0;p=[];E=enumerate
  for y,m in E(g):
   for x,k in E(m):
-   if k:a=min(a,y);b=y;c=min(c,x);d=max(d,x);p+=[k]*(k!=8)*(k not in p)
+   if k:a=min(a,y);b=y;c=min(c,x);d=max(d,x);p+=[k]*(k not in p+[8])
  for y in range(a,b+1):
-  for x in range(c,d+1):g[y][x]=p[a<y<b and c<x<d]
+  for x in range(c,d+1):g[y][x]=p[a<y<b>-1<c<x<d]
  return g
