246b0099867d8a8f82bc1c655247d7066db0e663
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 19:32:10 +0900

    タスク 260: 更新 (190 -> 189)

--- 260/3_submission.py
+++ 260/3_submission.py
@@ -4,5 +4,4 @@ def p(g,r=range(10)):
   for j in r:
    if(v:=g[i][j])%5:c=v;d=j-i
    elif v:a+=j-i,
- m,M=min(a),max(a)
- return[[c*(j-i in[d]+[M+2]*(M>d)+[m-2]*(m<d))for j in r]for i in r]
+ m=min(a);M=max(a);return[[c*(j-i in[d]+[M+2]*(M>d)+[m-2]*(m<d))for j in r]for i in r]
