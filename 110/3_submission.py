f1a66ec03b3a3906320e4a8ac6052d3ab3c726e0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-28 22:16:03 +0900

    タスク 110: 更新 (139 -> 133)

--- 110/3_submission.py
+++ 110/3_submission.py
@@ -1,8 +1,7 @@
 def p(g):
  o=[]
  for r in g:
-  o+=r,
-  for s in o:
+  for s in o+[r]:
    if all((a^b)*a*b<1for a,b in zip(r,s)):
-    s[:]=map(max,s,r);o[-1]=s;break
+    s[:]=map(max,s,r);o+=s,;break
  return o
