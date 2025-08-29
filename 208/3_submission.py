55fbeec9a6d4042ead33761124fe3e1efb809be7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 13:07:29 +0900

    タスク 208: 更新 (275 -> 273)

--- 208/3_submission.py
+++ 208/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- f=sum(g,[]);y,x=divmod(a:=f.index(k:=min(f,key=f.count)),21);h,w=divmod(462-f[::-1].index(k)-a,21);R=range;i,j=[(i,j)for i in R(22-h)for j in R(22-w)if(j-x|i-y)*(sum(sum(r[j+1:j+w-1])for r in g[i+1:i+h-1])<1)][0]
+ f=sum(g,[]);a=f.index(k:=min(f,key=f.count));x=a%21;y=a//21;h,w=divmod(462-f[::-1].index(k)-a,21);i,j=[(i,j)for i in range(22-h)for j in range(22-w)if(j-x|i-y)*all(sum(r[j+1:j+w-1])<1 for r in g[i+1:i+h-1])][0]
  while h:h-=1;g[i+h][j:j+w]=g[y+h][x:x+w]
  return g
