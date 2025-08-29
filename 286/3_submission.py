a507c9714856c64cd72e196825a5e01d653acd91
pech1 <pechi2327@gmail.com>
2025-08-29 21:30:49 +0900

    286:没にしていた処理を削除

--- 286/3_submission.py
+++ 286/3_submission.py
@@ -1,5 +1,5 @@
 import re
 def p(g):
  S=re.sub;a,b={*sum(g,[])}-{0,8}
- for _ in[a,b]*256:g=[[*map(int,S(f'0(?={a})',f'{b}',S(f'0(?={b})',f'{a}',str(r)[1::3])[::-1]))]for r in zip(*g)]
+ for _ in[0]*256:g=[[*map(int,S(f'0(?={a})',f'{b}',S(f'0(?={b})',f'{a}',str(r)[1::3])[::-1]))]for r in zip(*g)]
  return g
