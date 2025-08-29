404790bf985488c53f559478a08a887c54a5904b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 06:59:02 +0900

    タスク 089: 更新 (327 -> 325)

--- 089/3_submission.py
+++ 089/3_submission.py
@@ -4,5 +4,5 @@ def p(g):
   if(a:=[divmod(i,13)for i in range(169)if sum(g,[])[i]==k]):l,I,J,B=max((len(t:=sorted((v,x,y)for x in R for y in R if-1<i+x<13>j+y>=0<k!=(v:=g[i+x][j+y])>0)),i,j,t)for i,j in a);c=B[l>>1][0]
   for i,j in a:
    for v,x,y in B:
-    if(i,j)!=(I,J)and v==c:g[i+x][j+[-y,y][k>2]]=c
+    if(i,j)!=(I,J)and v==c:g[i+x][j+y*(k*2-5)]=c
  return g
