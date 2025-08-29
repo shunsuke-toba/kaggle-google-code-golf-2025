85128fcdfb2541cbe0843991b84435003f3a4677
pech1 <pechi2327@gmail.com>
2025-08-29 23:32:37 +0900

    286:002の更新を反映

--- 286/3_submission.py
+++ 286/3_submission.py
@@ -1,5 +1,4 @@
-import re
 def p(g):
- S=re.sub;a,b={*sum(g,[])}-{0,8}
- for _ in[0]*256:g=[[*map(int,S(f'0(?={a})',f'{b}',S(f'0(?={b})',f'{a}',str(r)[1::3])[::-1]))]for r in zip(*g)]
+ R=str.replace;a,b={*sum(g,[])}-{0,8}
+ for _ in[0]*256:g=[[*map(int,R(R(str(r)[1::3],f'0{b}',f'{a}{b}')[::-1],f'0{a}',f'{b}{a}'))]for r in zip(*g)]
  return g
