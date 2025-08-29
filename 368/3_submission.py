1a9fd8adaca2cb241689d91814cfb8f662621fe0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 23:03:51 +0900

    タスク 368: 更新 (191 -> 190)

--- 368/3_submission.py
+++ 368/3_submission.py
@@ -2,5 +2,5 @@ def p(g):
  R=range(10);A=[(i,j)for i in R for j in R if g[i][j]%5];t,l,a,b=A[0]+A[-1]
  for i in R:
   for j in R:
-   for k in((),R[t:a+1])[g[i][j]==5]:g[i+k-t][j:j+b-l+1]=g[k][l:b+1]
+   for k in[*R[t:a+1]]*(g[i][j]==5):g[i+k-t][j:j+b-l+1]=g[k][l:b+1]
  return g
