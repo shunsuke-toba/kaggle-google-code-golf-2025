e7f3b053d1c51e868d649bede62f67ace1d9c900
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 13:07:28 +0900

    タスク 104: 更新 (114 -> 113)

--- 104/3_submission.py
+++ 104/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- r=g[0][1]<1;o=[[0]*9for _ in g*3]
- for i in range(8):s=4*r^-g[1][2]&5^i&4;o[r+i][s:s+4]=[3]*4
+ r=g[0][1]<1;o=[[0]*9for _ in g*3];i=8
+ while i:i-=1;s=4*r^-g[1][2]&5^i&4;o[r+i][s:s+4]=[3]*4
  return o
