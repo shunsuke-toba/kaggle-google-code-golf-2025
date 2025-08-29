3a19506c087e2a184bf9395d2920afce1d7db699
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 06:59:05 +0900

    タスク 133: 更新 (497 -> 494)

--- 133/3_submission.py
+++ 133/3_submission.py
@@ -3,7 +3,7 @@ def p(g):
  def f(y,x):c=[(y,x,g[y][x])];g[y][x]=0;[c:=c+f(Y,X)for Y,X in((y+1,x),(y-1,x),(y,x+1),(y,x-1))if h>Y>=0<=X<w and g[Y][X]];return c
  C=[f(y,x)for y in r(h)for x in r(w)if g[y][x]];B=min(C,key=lambda c:(l(c)<3,l(c)));Y,X,p=min(B,key=lambda t:sum(v[2]==t[2]for v in B))
  for c in C:
-  m=int(l(A:=[v for v in c if v[2]==p])**.5);a,b,_=min(A)
+  q=0;A=[v for v in c if v[2]==p or(q:=v[2])>9];m=int(l(A)**.5);a,b,_=min(A)
   for y,x,t in B:
-   for i in r(m):o=b+(x-X)*m;g[a+(y-Y)*m+i][o:o+m]=[[p,sum({v[2]for v in c})-p][t!=p]]*m
+   for i in r(m):o=b+(x-X)*m;g[a+(y-Y)*m+i][o:o+m]=[[p,q][t!=p]]*m
  return g
