4a2e41711b89a64128330fcc0d305cf7ab7d5129
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 01:12:48 +0900

    タスク 139: 更新 (112 -> 110)

--- 139/3_submission.py
+++ 139/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- for r,c in(1,a:=any(g[8])*2),(4+a,5):
-  for k in range(9):R=g[r+k//3];R[c+k%3]=R[c+k%3]or 7
+ a=any(g[8])*2
+ for k in range(18):R=g[(1,4+a)[k>8]+k%9//3];c=(a,5)[k>8]+k%3;R[c]=R[c]or 7
  return g
