f9eb63b9a32299e418f0df8efc3945ccd20c80ae
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 13:07:29 +0900

    タスク 162: 更新 (131 -> 126)

--- 162/3_submission.py
+++ 162/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
  for n in range(324):
   t=g[n//18:][:3];n%=18
-  if sum(sum(r[n:n+3])for r in t)<1:
-   for r in t:r[n:n+3]=1,1,1
+  for r in t*(sum(sum(r[n:n+3])for r in t)<1):r[n:n+3]=1,1,1
  return g
