c9373d7f1fd4e166223875a71477604f691eab47
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 00:15:11 +0900

    タスク 335: 更新 (152 -> 129)

--- 335/3_submission.py
+++ 335/3_submission.py
@@ -1,5 +1,4 @@
-def p(g,A=divmod):
- i=sum(g,[]).index;a,b,c,d=A(i(2),m:=len(g[0]))+A(i(8),m)
- while b-d:b+=d>b or-1;g[a][b]=4
- while a-c:g[a][d]=4;a+=c>a or-1
+def p(g):
+ s,e=map(sum(g,[]).index,(2,8));w=len(g[0])
+ while(s:=s+((e%w>s%w)-(e%w<s%w)or w*(e>s)or-w))-e:g[s//w][s%w]=4
  return g
