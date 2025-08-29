8121aa0454e303d19b6f12b9fdf0d28e528fd11e
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-29 17:46:00 +0900

    251: 002を流用(191 -> 176)

--- 251/3_submission.py
+++ 251/3_submission.py
@@ -1,6 +1,4 @@
 def p(g):
- n=i=len(g)
- def f(i,j):
-  if 0<=i<n>j>=0==g[i][j]:g[i][j]=1;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
- while i:i-=1;f(i,0);f(i,n-1);f(0,i);f(n-1,i)
- return[[v^(v<2)for v in r]for r in g]
+ c=len(g);g=eval(str(g).replace('0','1'));d=1-c,0,-1,0
+ for z in range(9**5):g[r:=z%97%c][q:=z%89%c]*=g[r+d[z&3]][q+d[~z&3]]*r*q*(c+~r)*(c+~q)+(g[r][q]&2)>0
+ return g
