d2c7527c1149f6f01ffb41f4e4c92a7c673d0fd3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 23:03:47 +0900

    タスク 055: 更新 (129 -> 115)

--- 055/3_submission.py
+++ 055/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- s=b'\0\2\0\4\6\3\0\1\0';k=0
+ k=0
  for r in g:
-  k+=r[0]>7;l=0
-  for j,x in enumerate(r):r[j]=x or s[k*3+l];l+=x>7
+  k+=r[0]>7;l=0;r[:]=[(l:=l+(x>7),x or b'020463010'[k*3+l]&15)[1] for x in r]
  return g
