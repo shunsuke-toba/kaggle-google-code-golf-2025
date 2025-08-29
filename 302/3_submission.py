ddb659a39468a723036037ca3b9d83e62bf580ca
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 23:03:49 +0900

    タスク 302: 更新 (148 -> 146)

--- 302/3_submission.py
+++ 302/3_submission.py
@@ -1,5 +1,1 @@
-def p(g):
- for x in range(132):
-  if g[y:=x%11][x:=x//11]&g[y+1][x-1]:
-   for t in g[y+1-(s:=g[y][x-1::-1].index(5)):y+1]:t[x-s:x]=[s+5]*s
- return g
+p=lambda g:[exec('k=g[y][x-1::-1].index(5)\nfor t in g[y+1-k:y+1]:t[x-k:x]=[k+5]*k') for i in range(132) if g[y:=i%11][x:=i//11]&g[y+1][x-1]]and g
