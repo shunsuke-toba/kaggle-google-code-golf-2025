fece040f7248dae6e566454b702ce73046ceb7f3
sak74y <yusei.sakurai@third-inc.co.jp>
2025-08-29 14:17:26 +0900

    364:更新(325->323)

--- 364/3_submission.py
+++ 364/3_submission.py
@@ -6,6 +6,6 @@ def p(g):
    for x,y in s:
     for a,b in(x+1,y),(x-1,y),(x,y+1),(x,y-1):
      if h>a>-1<b<w>0<g[a][b]:g[a][b]=0;s+=(a,b),
-    k+=(((x+1,y)in s)+((x-1,y)in s))*(((x,y+1)in s)+((x,y-1)in s))
+    k+=(((x+1,y)in s)+((x-1,y)in s))*(((x,y+1)in s)+((x,b)in s))
    for x,y in s:g[x][y]=-~k*5%9
  return g
