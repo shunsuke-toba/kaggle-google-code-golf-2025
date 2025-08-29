4c9e253792eae18efd3b381bcd1e2a10c82d567e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 09:52:57 +0900

    タスク 284: 更新 (256 -> 255)

--- 284/3_submission.py
+++ 284/3_submission.py
@@ -1,7 +1,6 @@
 def p(g,e=enumerate):
- (a,b),(d,e)=[(i,j)for i,r in e(g)for j,v in e(r)if v]
+ (a,b),(d,e)=[(i,j)for i,r in e(g)for j,v in e(r)if v];h=e-b>>1
  if a-d:return[*zip(*p([*map(list,zip(*g))]))]
- h=e-b>>1
  for x,s in(b,1),(e,-1):
   u=x+s*h;g[a][x:u:s]=[z:=g[a][x]]*h
   for r in g[a-2:a+3]:r[u-s]=g[a-2][u]=g[a+2][u]=z
