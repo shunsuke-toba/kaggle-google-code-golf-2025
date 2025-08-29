06f3d1839ace5de59182675a2c93d086df542c31
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 06:59:08 +0900

    タスク 240: 更新 (189 -> 178)

--- 240/3_submission.py
+++ 240/3_submission.py
@@ -1,8 +1,7 @@
 R=range(19)
 def p(g):
- for i in R:
-  for j in R:
-   if v:=g[i][j]:g[j][~i]=g[~i][~j]=g[~j][i]=v
+ for r in R:
+  for c in R:g[r][c]|=g[r][~c]|g[~r][c]|g[~r][~c]
  for i in 1,3,5:
   for k in R[i+2:~i:2]:g[i][k]=g[~i][k]=g[k][i]=g[k][~i]=g[i][i+2]
  return g
