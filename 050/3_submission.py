9893f0a7e3f1e21e90352b9da97c9a94429f49bc
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 00:15:07 +0900

    タスク 050: 更新 (124 -> 106)

--- 050/3_submission.py
+++ 050/3_submission.py
@@ -1,3 +1,1 @@
-def p(g):
- for _ in 0,1:g=[*map(list,zip(*[[3*(8in r[:i])*(8in r[i+1:])or v for i,v in enumerate(r)]for r in g]))]
- return g
+p=lambda g:exec("g[:]=zip(*[[3*(8in r[:i])*(8in r[i+1:])or v for i,v in enumerate(r)]for r in g]);"*2)or g
