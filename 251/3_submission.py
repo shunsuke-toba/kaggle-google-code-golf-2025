26c256a6bfb47d6392a3ccb04b6019fd0b3e1fd7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 23:03:49 +0900

    タスク 251: 更新 (146 -> 145)

--- 251/3_submission.py
+++ 251/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- import re;s=re.sub;g=eval(s('0','1',str(g)))
- for _ in[0]*96:g=[[*map(int,s('1(?=0|$)','0',str(r)[-2::-3]))]for r in zip(*g)]
+ g=eval(str(g).replace('0','1'))
+ for _ in[0]*36:g=[[*map(int,(str(r)[-2::-3]+'0').replace('10','00')[:-1])]for r in zip(*g)]
  return g
