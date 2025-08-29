25bf3f16ecba205597c92b49704e9fa536147adf
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 09:52:55 +0900

    タスク 118: 更新 (375 -> 374)

--- 118/3_submission.py
+++ 118/3_submission.py
@@ -4,4 +4,4 @@ def p(g):
   B,_,I,J,t=max((sum(f(a,j)for a in R(i-r,i+r+1)if-1<a<h)+sum(f(i,c)for c in R(j-r,j+r+1)if-1<c<w)-f(i,j),-r,i,j,r)for r in(3,2)[:4-t]for i in R(h)for j in R(w))
   if B<1:return g
   for d in R(-t,t+1):
-   for y,x in(I+d,J),(I,J+d):u+=[(y,x)];g[y][x]+=3*(g[y][x]==5)
+   for y,x in(I+d,J),(I,J+d):u+=(y,x),;g[y][x]+=3*(g[y][x]==5)
