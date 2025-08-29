2c269ead5c3c41f6abc3f81111e34008749fb5d0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 00:15:09 +0900

    タスク 141: 更新 (117 -> 116)

--- 141/3_submission.py
+++ 141/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):a=sum(g,[]);b=a.index(v:=max(a));a=range(n:=len(g));return[[v*(i-b//n in(j-b%n,b%n-j))for j in a]for i in a]
+def p(g):b=g.index(r:=max(g));c=r.index(v:=max(r));a=range(len(g));return[[v*(i-b in(j-c,c-j))for j in a]for i in a]
