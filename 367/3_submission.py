92782888ceefe9916672b1e3d3bb3528c490d54f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 21:52:35 +0900

    タスク 367: 更新 (236 -> 228)

--- 367/3_submission.py
+++ 367/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for s in range(8**5):
-  try:h,w=len(g),len(g[0]);d=-1,0,1,0;t=g[r:=s%97%h][c:=s%89%w]-5;s%=4;g[x:=d[s]+r][y:=d[~s]+c]|=t&4*(x|y>0);g[x-y+c][x-r+y]|=([g[r+d[s+~i]][c+d[s-i]]for i in range(4)]==[5,t,0,5])*4
+  try:d=-1,0,1,0;t=g[r:=s%97%len(g)][c:=s%89%len(g[0])]-5;s%=4;g[x:=d[s]+r][y:=d[~s]+c]|=t&4*(x|y>0);g[x-y+c][x-r+y]|=([g[r+d[s+~i]][c+d[s-i]]for i in range(4)]==[5,t,0,5])*4
   except:0
  return g
