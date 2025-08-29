c0e6db527ae7e2b2b47bdaf2262dabfcd6e4567d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 23:03:50 +0900

    タスク 356: 更新 (151 -> 114)

--- 356/3_submission.py
+++ 356/3_submission.py
@@ -1,3 +1,1 @@
-def p(g):
- R=range(10);v=[[any(c[:i+1])*any(c[i:])*8for i in R]for c in zip(*g)]
- return[[v[j][i]|any(g[i][:j+1])*any(g[i][j:])*8for j in R]for i in R]
+p=lambda g:[[max(r[:j+1])&max(r[j:])|max(c[:i+1])&max(c[i:])for j,c in enumerate(zip(*g))]for i,r in enumerate(g)]
