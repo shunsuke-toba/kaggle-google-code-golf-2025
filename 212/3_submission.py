892e4e660bce16b0876d8119a3d2d26d4ab1a254
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 06:59:07 +0900

    タスク 212: 更新 (138 -> 137)

--- 212/3_submission.py
+++ 212/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,b=100):
  while b:
   b-=1;l=g.index([5]*10);v=g[r:=b//10][b%10]
-  while v%-5*(r-l)*-~r%11:g[r][b%10]=v;r+=(r<l)^(v<2)or-1
+  while v%5*(r-l)*-~r%11:g[r][b%10]=v;r+=(r<l)^(v<2)or-1
  return g
