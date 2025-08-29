d4546ebfc20365dd8d9ec43a258f58f5529324e5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 00:15:09 +0900

    タスク 213: 更新 (115 -> 114)

--- 213/3_submission.py
+++ 213/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(t:=len({*g[0]+g[-1]})>3,h:=[c:=[y for r in[g,zip(*g)][t]for y in {*r}-{0,5}]]*len(c),[*zip(*h)],h)[2+t]
+p=lambda g:(t:=len({*g[0]+g[-1]})>3,h:=[c:=[y for r in[g,zip(*g)][t]for y in{*r}if y%5]]*len(c),[*zip(*h)],h)[2+t]
