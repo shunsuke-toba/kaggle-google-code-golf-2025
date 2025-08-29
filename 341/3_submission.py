71b43d7755ea6d305e7dcd5a4c60d1105b0c98f3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 06:59:12 +0900

    タスク 341: 更新 (194 -> 188)

--- 341/3_submission.py
+++ 341/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- b=bytes(sum(g,[]));r=c=()
- for k in{*b}-{0}:i,j=b.find(k),b.rfind(k);r+=i//10,j//10;c+=i%10,j%10
- s=sorted;x,y=s(r)[1:3];c,d=s(c)[1:3]
+ b=bytes(sum(g,[]));r,c=zip(*(divmod(F(k),10)for k in set(b)-{0} for F in (b.find,b.rfind)))
+ _,x,y,_=sorted(r);_,c,d,_=sorted(c)
  for r in g[x+1:y]:r[c+1:d]=[8]*(d+~c)
  return g
