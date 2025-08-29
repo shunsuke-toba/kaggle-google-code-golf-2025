aa0b703498bdd09d7ec15def3a5c138de2a8af05
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 09:52:48 +0900

    タスク 027: 更新 (146 -> 143)

--- 027/3_submission.py
+++ 027/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- r=range(10);x=[(g[b],a)for a in r for b in r if g[a][b]];k=9+(sum(b[-a]-b[~a]for b,a in x)>0)
- for b,a in x:b[k-a]=b[k-a]or 2
+ r=range(10);x=[(g[b],~a)for a in r for b in r if g[a][b]];o=sum(b[-~a]-b[a]for b,a in x)>0
+ for b,a in x:b[a+o]=b[a+o]or 2
  return g
