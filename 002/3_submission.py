c193aab6d270f0f646529a881d834cc050731522
pech1 <pechi2327@gmail.com>
2025-08-30 03:55:03 +0900

    002:evalを使って短縮ができる

--- 002/3_submission.py
+++ 002/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- g=eval(str(g).replace('0','4'))
- for _ in [0]*96:g=[*zip(*[[c*(c^4+i*r[i-1]>0)for i,c in enumerate(r)]for r in g[::-1]])]
- return g
+p=lambda g:eval("[*zip(*[[c*(c^4+i*r[i-1]>0)for i,c in enumerate(r)]for r in "*64+str(g).replace('0','4')+"[::-1]])]"*64)
