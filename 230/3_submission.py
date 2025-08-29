217987d476726ce02a98fc04ade32c0fee1b0d78
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 01:12:49 +0900

    タスク 230: 更新 (133 -> 132)

--- 230/3_submission.py
+++ 230/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,e=enumerate:exec('for r,a in e(g):\n for c,v in e(a):\n  if v>4:t=g[r-1][c]>0;u=a[c-1]>0;g[r+2*t-1][c+2*u-1]=t*2+u+1')or g
+p=lambda g,e=enumerate:exec('for r,a in e(g):\n for c,v in e(a):\n  if v>4:t,u=g[r-1][c]>4,a[c-1]>4;g[r+2*t-1][c+2*u-1]=t*2-~u')or g
