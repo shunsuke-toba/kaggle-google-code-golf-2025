c7ca70b5bca71aac293ff7dd0bbe02df016b1468
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 06:59:00 +0900

    タスク 055: 更新 (115 -> 110)

--- 055/3_submission.py
+++ 055/3_submission.py
@@ -1,5 +1,3 @@
-def p(g):
- k=0
- for r in g:
-  k+=r[0]>7;l=0;r[:]=[(l:=l+(x>7),x or b'020463010'[k*3+l]&15)[1] for x in r]
+def p(g,k=0):
+ for r in g:k+=r[0]>7;l=0;r[:]=[(l:=l+(x>7),x or b'020463010'[k*3+l]&15)[1]for x in r]
  return g
