456999dfaa4d37264d9269b3db49e87cc4776965
pech1 <pechi2327@gmail.com>
2025-08-29 21:29:36 +0900

    286:正規表現解法に変更

--- 286/3_submission.py
+++ 286/3_submission.py
@@ -1,5 +1,5 @@
+import re
 def p(g):
- h=len(g);w=len(g[0]);a,b={*sum(g,[])}-{0,8};d=0,1,0,-1;z=7**7
- while z:=z-1:
-  if g[r:=z%97%h][q:=z%89%w]<1<w>(j:=q+d[z%4])>-1<(i:=r+d[~z%4])<h>(k:=g[i][j])%8>0:g[r][q]=k^a^b
+ S=re.sub;a,b={*sum(g,[])}-{0,8}
+ for _ in[a,b]*256:g=[[*map(int,S(f'0(?={a})',f'{b}',S(f'0(?={b})',f'{a}',str(r)[1::3])[::-1]))]for r in zip(*g)]
  return g
