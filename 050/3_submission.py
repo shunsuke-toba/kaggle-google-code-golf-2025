f8361fe40a9e1ff15344857580322f62125282d3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 09:52:52 +0900

    タスク 050: 更新 (134 -> 124)

--- 050/3_submission.py
+++ 050/3_submission.py
@@ -1,6 +1,3 @@
 def p(g):
- for _ in g,g:
-  for r in(g:=[*map(list,zip(*g))]):
-   try:R=r.index;b=R(8,a:=R(8)+1);r[a:b]=[3]*(b-a)
-   except:0
+ for _ in 0,1:g=[*map(list,zip(*[[3*(8in r[:i])*(8in r[i+1:])or v for i,v in enumerate(r)]for r in g]))]
  return g
