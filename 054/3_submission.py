d28a218a7b33a5d0839e5d51bb8facfb1258e0f9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 09:52:53 +0900

    タスク 054: 更新 (444 -> 441)

--- 054/3_submission.py
+++ 054/3_submission.py
@@ -1,8 +1,8 @@
 def p(g,R=range):
- b=g[0][0];D=1,0,-1,0
+ D=1,0,-1,0
  for z in R(676):
   i=z//26;j=z%26;q=sum((g[i+k][j:j+5]for k in R(5)),[])
-  if q==q[::-1]and len({*q})>2:break
+  if q==q[::-1]and len({*q})>2:b=q[0];break
  for r in g[i:i+5]:r[j:j+5]=[b]*5
  for i,j in[(i,j)for i in R(30)for j in R(30)if g[i][j]==q[12]]:
   for d in R(4):
