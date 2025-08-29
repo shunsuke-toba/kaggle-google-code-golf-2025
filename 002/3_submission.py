0821ccb220a495a0bb98c433ce00ea38c93d477b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 23:03:45 +0900

    タスク 002: 更新 (146 -> 144)

--- 002/3_submission.py
+++ 002/3_submission.py
@@ -1,5 +1,4 @@
-import re
-def p(g):
- S=re.sub;g=eval(S('0','4',str(g)))
- for _ in[0]*96:g=[[*map(int,S('4(?=0|$)','0',str(r)[-2::-3]))]for r in zip(*g)]
+def p(g,r=str.replace):
+ g=eval(r(str(g),'0','4'))
+ for _ in[0]*96:g=[[*map(int,r('0'+str(R)[-2::-3],'04','00')[1:])]for R in zip(*g)]
  return g
