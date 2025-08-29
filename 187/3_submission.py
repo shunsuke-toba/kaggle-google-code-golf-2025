174b760b94151778ac708c570b319214463643cb
pech1 <pechi2327@gmail.com>
2025-08-30 00:14:38 +0900

    187:002の更新を反映

--- 187/3_submission.py
+++ 187/3_submission.py
@@ -1,5 +1,4 @@
-import re
-def p(g):
- S=re.sub;g=eval(S('0','2',str(g)))
- for _ in[0]*96:g=[[*map(int,S('2(?=3|$)','3',str(r)[-2::-3]))]for r in zip(*g)]
+def p(g,r=str.replace):
+ g=eval(r(str(g),'0','2'))
+ for _ in[0]*96:g=[[*map(int,r('3'+str(R)[-2::-3],'32','33')[1:])]for R in zip(*g)]
  return g
