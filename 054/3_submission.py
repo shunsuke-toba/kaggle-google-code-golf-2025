83d2ba658105a3791e8d14da74441dd1ca819480
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-29 21:52:28 +0900

    タスク 054: 更新 (441 -> 439)

--- 054/3_submission.py
+++ 054/3_submission.py
@@ -1,14 +1,14 @@
-def p(g,R=range):
+def p(g,r=range):
  D=1,0,-1,0
- for z in R(676):
-  i=z//26;j=z%26;q=sum((g[i+k][j:j+5]for k in R(5)),[])
+ for z in r(676):
+  i=z//26;j=z%26;q=sum((g[i+k][j:j+5]for k in r(5)),[])
   if q==q[::-1]and len({*q})>2:b=q[0];break
- for r in g[i:i+5]:r[j:j+5]=[b]*5
- for i,j in[(i,j)for i in R(30)for j in R(30)if g[i][j]==q[12]]:
-  for d in R(4):
+ for h in g[i:i+5]:h[j:j+5]=[b]*5
+ for i,j in[(i,j)for i in r(30)for j in r(30)if g[i][j]==q[12]]:
+  for d in r(4):
    v=q[(22,10,2,14)[d]];x=i;y=j
    while(v^b)*(g[x][y]^b):g[x][y]=v;x+=D[d];y+=D[d-3]
-  for t in R(25):
-   v=q[t];x=i+t//5-2;y=j+t%5-2
-   if(v^b)*(g[x][y]^b):g[x][y]=v
+  for t in r(25):
+   v=q[t];x=i-2+t//5;y=j-2+t%5
+   if v!=b!=g[x][y]:g[x][y]=v
  return g
