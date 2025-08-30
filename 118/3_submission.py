a008f151d7456d4d6046759612833a9c0206a1d5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 08:28:02 +0900

    タスク 118: 更新 (363 -> 361)

--- 118/3_submission.py
+++ 118/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- h=len(g);w=len(g[0]);R=range;F=[-2,0,1]+[0]*6;b=[*map(list,g)];t=2
+ R=range;F=[-2,0,1]+[0]*6;b=eval(str(g));t=2;h=len(g);w=len(g[0])
  while 1:
   B,i,j,t=max((sum(F[b[a][j]]for a in R(i-r,i+r+1)if-1<a<h)+sum(F[b[i][c]]for c in R(j-r,j+r+1)if-1<c<w)-F[b[i][j]]-r/3,i,j,r)for r in(3,2)[:4-t]for i in R(h)for j in R(w))
   if B<1:return g
