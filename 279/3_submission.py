d6944c4bd30322945c99cf9a09bbd510189f1071
pech1 <pechi2327@gmail.com>
2025-08-31 01:11:09 +0900

    279:196の解法を流用

--- 279/3_submission.py
+++ 279/3_submission.py
@@ -1,9 +1,1 @@
-def p(g):
- w=len(g[0]);t=s=w*len(g);v=[1]*s
- while t:
-  t-=1;q=g[t//w][t%w]%9*v[t]*[t];d=v[t]=0
-  for j in q:
-   for k in j-1,j+1,j-w,j+w:
-    if s>k>=0<k%w-j%w+2<4>g[k//w][k%w]:d+=1-2*v[k];q+=v[k]*[k];v[k]=0
-  for k in q*d:g[k//w][k%w]=8
- return g
+p=lambda g,S="[*zip(*[[c",T=")for i,c in enumerate(r)]for r in":eval(str(eval((S+"^((c^1<1)*(i and r[i-1]>>3)*9"+T)*16+(S+"*(c^9|i*r[i-1]>0"+T)*48+str(g)+"[::-1]])]"*64)).replace(*'09'))
