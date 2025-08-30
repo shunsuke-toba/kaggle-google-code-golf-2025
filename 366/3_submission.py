816bf69963a3bdb76feaf40ac542f60d3b1d1cbc
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-31 03:08:07 +0900

    366: 雑な短縮(649 -> 628)

--- 366/3_submission.py
+++ 366/3_submission.py
@@ -1,13 +1,11 @@
 def p(g,E=enumerate,R=range,L=len,F=filter):
  s=sum(g,[]);*_,z,y,x=sorted({*s},key=s.count);a,b=[[*map(list,zip(*F(lambda r:c in r,zip(*F(lambda r:c in r,g)))))]for c in(y,x)];n,m,p=L(a),L(a[0]),[]
- for i in R(n):
-  for j in R(m):
-   if a[i][j]^y:
-    k,l=i,j
-    while k+1<n and a[k+1][l]^y:k+=1
-    while l+1<m and a[k][l+1]^y:l+=1
-    q=a[i:k+1];t=[r[j:l+1]for r in q];p+=(-sum(c!=z for c in sum(t,[])),t),
-    for r in q:r[j:l+1]=[y]*(l-j+1)
+ for o in R(n*m):
+  if a[k:=(i:=o//m)][j:=o%m]^y:
+   while a[k+1-n][l:=j]^y:k+=1
+   while l+1<m and a[k][l+1]^y:l+=1
+   q=a[i:k+1];t=[r[j:l+1]for r in q];p+=(-sum(c!=z for c in sum(t,[])),t),
+   for r in q:r[j:l+1]=[y]*(l-j+1)
  for _,r in sorted(p):
   for i in R(L(b)-L(r)+1):
    for j in R(L(b[0])-L(r[0])+1):
