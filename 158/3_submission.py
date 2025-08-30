1095e439f9c7b82f3eac08e450722fcf394b6a9a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 14:39:04 +0900

    タスク 158: 更新 (400 -> 397)

--- 158/3_submission.py
+++ 158/3_submission.py
@@ -1,12 +1,12 @@
 def p(g,R=range):
- W=len(g[0])-2;t=0
- while len({*(p:=sum([r[t//W:][:3]for r in g[t%W:][:3]],[]))})<4:t+=1
- for s in 3,2,1:
-  u=R(z:=3*s);r=[[p[i//s*3+j//s]for j in u]for i in u]
+ w=len(g[0])-2;t=0
+ while len({*(p:=sum((r[t//w:][:3]for r in g[t%w:][:3]),[]))})<4:t+=1
+ for m in 3,2,1:
+  q=R(s:=3*m);a=[[p[i//m*3+j//m]for j in q]for i in q]
   for _ in R(4):
-   for y in R(len(g)+1-z):
-    for x in R(W+3-z):
-     if all((g[-1][0],r[i][j])[p.count(r[i][j])<2]==g[y+i][x+j]for i in u for j in u):
-      for i in u:g[y+i][x:x+z]=r[i]
-   r=[*zip(*r[::-1])]
+   for y in R(len(g)+1-s):
+    for x in R(w+3-s):
+     if all((g[-1][0],v:=a[i][j])[p.count(v)<2]==g[y+i][x+j]for i in q for j in q):
+      for i in q:g[y+i][x:x+s]=a[i]
+   a=[*zip(*a[::-1])]
  return g
