836f4fbc085f1ce1eed092005090757f27456616
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 22:08:19 +0900

    タスク 143: 更新 (181 -> 179)

--- 143/3_submission.py
+++ 143/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  s=[i for i in range(9)if g[i//3][i%3]];n=1
- while{(v:=g[n//8+i//3][n%8+i%3])for i in s}-{v}or len(s)<sum(g,[]).count(v):n+=1
+ while{v:=g[n//8+i//3][n%8+i%3]for i in s}-{v}or len(s)<sum(g,[]).count(v):n+=1
  for i in s:g[n//8+i//3][n%8+i%3]=5
  return g
