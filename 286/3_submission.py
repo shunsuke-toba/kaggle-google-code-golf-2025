b6e45ec3408aaa7ef8cdf8aba738fc62d0e55d04
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 23:59:48 +0900

    タスク 286: 更新 (158 -> 154)

--- 286/3_submission.py
+++ 286/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- a,b=map(str,{*sum(g,[])}-{0,8})
+ a,b={*str(g)}-set(', []08')
  for _ in[0]*160:g=[[*map(int,str(R)[1::3].replace('0'+b,a+b)[::-1].replace('0'+a,b+a))]for R in zip(*g)]
  return g
