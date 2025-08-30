47accb09560010b4725c14d8afc9b59293519d50
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 07:35:32 +0900

    タスク 240: 更新 (171 -> 170)

--- 240/3_submission.py
+++ 240/3_submission.py
@@ -1,5 +1,6 @@
 def p(g):
  g=[[*map(max,a,b[::-1],a[::-1],b)]for a,b in zip(g,g[::-1])]
  for i in 1,3,5:
-  for k in range(i+2,17-i,2):g[i][k]=g[~i][k]=g[k][i]=g[k][~i]=g[i][i+2]
+  k=i+2
+  while k<17-i:g[i][k]=g[~i][k]=g[k][i]=g[k][~i]=g[i][i+2];k+=2
  return g
