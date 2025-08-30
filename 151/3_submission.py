9d9fa1531f8d365284ed829e299f714514c44be7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 23:59:45 +0900

    タスク 151: 更新 (113 -> 110)

--- 151/3_submission.py
+++ 151/3_submission.py
@@ -1,1 +1,4 @@
-def p(g):e=g.index(max(g));k=g[0].index(max(g[0]))-1;g[e-1][k:k+3]=g[e+1][k:k+3]=4,4,4;g[e][k:k+3:2]=4,4;return g
+def p(g):
+ e=g.index(max(g));k=g[0].index(max(g[0]))
+ for i in-1,1:g[e+i][k-1:k+2]=4,4,4;g[e][k+i]=4
+ return g
