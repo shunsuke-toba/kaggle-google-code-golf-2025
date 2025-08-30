b2341c55aaee8f17a65376ad44236babac340800
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-31 04:03:48 +0900

    209: 改善(478 -> 470)

--- 209/3_submission.py
+++ 209/3_submission.py
@@ -1,12 +1,11 @@
-def p(g):
- R=range;A=min;B=max;a=e=99;b=d=f=j=0;L=[];c=i=m=len(g[0])
+def p(g,R=range):
+ c=i=a=e=99;k=b=d=f=j=0;L=[];m=len(g[0])
  for t in R(len(g)*m):
-  v=g[r:=t//m][o:=t%m]
-  if v&4:b=r;d=o;a>r and(a:=r,c:=o)
-  elif v*(a<b<=r):e=A(e,r);f=r;i=A(i,o);j=B(j,o)
+  if(v:=g[r:=t//m][o:=t%m])&4:b=r;d=o;a>r and(a:=r,c:=o)
+  elif v*(a<b<=r):e=min(e,r);f=r;i=min(i,o);j=max(j,o)
   elif v:L+=[(r,o,v)]
  f+=1-e;j+=1-i
- for k in 2,3,4:
+ while k:=k+1:
   for r in R(a,b):
    for o in R(c,d):
     try:
