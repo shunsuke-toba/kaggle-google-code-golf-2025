230d57108c783b7344d0f0b71e1383a1237c6181
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 23:59:47 +0900

    タスク 273: 更新 (140 -> 139)

--- 273/3_submission.py
+++ 273/3_submission.py
@@ -1,6 +1,4 @@
 def p(g):
- s=[i for i in range(100)if sum(g,[])[i]]
- while s:
-  a,b,c,_,*s=s
+ for a,b,c,_ in zip(*[(i for i in range(100)if sum(g,[])[i])]*4):
   for r in g[a//10+1:c//10]:r[a%10+1:b%10]=[2]*(~a+b)
  return g
