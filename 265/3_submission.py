629a115b6bc08013e93b40d5d08ab3ed2c9db372
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 23:59:47 +0900

    タスク 265: 更新 (186 -> 185)

--- 265/3_submission.py
+++ 265/3_submission.py
@@ -1,8 +1,7 @@
 def p(g):
- a=g[0];p=a*1
- for b in g[1:]:
-  q=b*1
+ q=g[0]*1
+ for a,b in zip(g,g[1:]):
+  p,q=q,b*1
   for j in range(17):
    if(sum(p[j:j+2]+q[j:j+2])<1)*(j%16<1or p[j-1]+q[j-1]|p[j+2]*q[j+2]-25):a[j:j+2]=b[j:j+2]=2,2
-  a=b;p=q
  return g
