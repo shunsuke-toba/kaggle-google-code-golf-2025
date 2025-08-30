2a1ba2998072ff215206a28aa139f85c8f2852ca
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 15:52:01 +0900

    タスク 117: 更新 (210 -> 209)

--- 117/3_submission.py
+++ 117/3_submission.py
@@ -3,5 +3,5 @@ def p(g):
  while not(f[k]==f[k+~w]==f[k-w+1]==f[k+w-1]==f[k-~w]>0):k-=1
  a=k//w*2;b=k%w*2
  for k,v in enumerate(f):
-  if v:g[r:=k//w][b-k%w]=g[a-r][k%w]=g[a-r][b-k%w]=v
+  if v:g[r:=k//w][d:=b-k%w]=g[a-r][k%w]=g[a-r][d]=v
  return g
