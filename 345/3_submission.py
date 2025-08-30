8becfc338a35e10f7b88c5d71f76773cf7d9884b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 23:59:49 +0900

    タスク 345: 更新 (102 -> 100)

--- 345/3_submission.py
+++ 345/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- for x,c in enumerate(g[9]):
+ for x in range(10):
   y=9
-  while c*y:a=g[y-1][x]>4;y-=1-a;x+=a;g[y][x]=2
+  while y*g[y][x]:a=g[y-1][x]>4;x+=a;y-=1-a;g[y][x]=2
  return g
