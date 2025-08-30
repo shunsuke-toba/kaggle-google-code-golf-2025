6327f985cfe27a4a9223de7895abe57695f36b15
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 23:59:47 +0900

    タスク 240: 更新 (178 -> 171)

--- 240/3_submission.py
+++ 240/3_submission.py
@@ -1,7 +1,5 @@
-R=range(19)
 def p(g):
- for r in R:
-  for c in R:g[r][c]|=g[r][~c]|g[~r][c]|g[~r][~c]
+ g=[[*map(max,a,b[::-1],a[::-1],b)]for a,b in zip(g,g[::-1])]
  for i in 1,3,5:
-  for k in R[i+2:~i:2]:g[i][k]=g[~i][k]=g[k][i]=g[k][~i]=g[i][i+2]
+  for k in range(i+2,17-i,2):g[i][k]=g[~i][k]=g[k][i]=g[k][~i]=g[i][i+2]
  return g
