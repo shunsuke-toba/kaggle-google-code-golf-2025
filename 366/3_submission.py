6ca00da0fa2ff61febeae18ede72eea271fd95e8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 22:08:25 +0900

    タスク 366: 更新 (651 -> 649)

--- 366/3_submission.py
+++ 366/3_submission.py
@@ -2,15 +2,15 @@ def p(g,E=enumerate,R=range,L=len,F=filter):
  s=sum(g,[]);*_,z,y,x=sorted({*s},key=s.count);a,b=[[*map(list,zip(*F(lambda r:c in r,zip(*F(lambda r:c in r,g)))))]for c in(y,x)];n,m,p=L(a),L(a[0]),[]
  for i in R(n):
   for j in R(m):
-   if a[i][j]-y:
+   if a[i][j]^y:
     k,l=i,j
-    while k+1<n and a[k+1][l]-y:k+=1
-    while l+1<m and a[k][l+1]-y:l+=1
-    q=a[i:k+1];t=[r[j:l+1]for r in q];p+=[(-sum(c!=z for c in sum(t,[])),t)]
-    for r in q:r[j:l+1]=[y]*-~(l-j)
+    while k+1<n and a[k+1][l]^y:k+=1
+    while l+1<m and a[k][l+1]^y:l+=1
+    q=a[i:k+1];t=[r[j:l+1]for r in q];p+=(-sum(c!=z for c in sum(t,[])),t),
+    for r in q:r[j:l+1]=[y]*(l-j+1)
  for _,r in sorted(p):
   for i in R(L(b)-L(r)+1):
    for j in R(L(b[0])-L(r[0])+1):
     if all(b[i+u][j+v]==(x,c)[c!=z]for u,s in E(r)for v,c in E(s)):
-     for k,s in E(r):b[i+k][j:j+L(r[0])]=s
+     for k,s in E(r):b[i+k][j:j+len(s)]=s
  return b
