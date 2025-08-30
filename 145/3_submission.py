256ee075ae74b085d0b2f1b0002329ff6626620d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 14:39:04 +0900

    タスク 145: 更新 (270 -> 269)

--- 145/3_submission.py
+++ 145/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
- h=len(g);w=len(g[0]);t=[]
+ h=len(g);w=len(g[0]);t=[];i=h*w
  def f(y,x):
   if h>y>-1<x<w>g[y][x]==0:g[y][x]=-len(t);t[-1]+=1;f(y+1,x);f(y-1,x);f(y,x+1);f(y,x-1)
- for i in range(h*w):
-  if g[i//w][i%w]==0:t+=0,;f(i//w,i%w)
+ while i:i-=1;g[i//w][i%w]or(t.append(0),f(i//w,i%w))
  return[[2*(c>1)or t[~c]//max(t)+min(t)//t[~c]*8 for c in r]for r in g]
