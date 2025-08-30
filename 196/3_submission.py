307fb24b685eff42be6786eb86290e3f662d48c9
pech1 <pechi2327@gmail.com>
2025-08-30 13:24:23 +0900

    196:002の解法を流用

--- 196/3_submission.py
+++ 196/3_submission.py
@@ -1,4 +1,1 @@
-def p(g,R=str.replace):
- e=enumerate;g=eval(R(str(g),'0','2'))
- for _ in[0]*96:g=[[*map(int,R('0'+str(r)[-2::-3],'02','00')[1:])]for r in zip(*g)]
- return[[v%2*(v+2*any(2in R[:j+2][-3:]for R in g[:i+2][-3:]))for j,v in e(r)]for i,r in e(g)]
+def p(g):e=enumerate;g=eval("[*zip(*[[c*((2^c)+i*r[i-1]>0)for i,c in enumerate(r)]for r in "*64+str(g).replace('0','2')+"[::-1]])]"*64);return[[v%2*(v+2*any(2in R[:j+2][-3:]for R in g[:i+2][-3:]))for j,v in e(r)]for i,r in e(g)]
