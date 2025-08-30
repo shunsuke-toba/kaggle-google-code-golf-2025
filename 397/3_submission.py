5dd2e31fd8ee13d6abe3f435273260d6f9863ce8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 10:37:35 +0900

    タスク 397: 更新 (137 -> 133)

--- 397/3_submission.py
+++ 397/3_submission.py
@@ -2,5 +2,5 @@ def p(g):
  t=81
  while t:
   t-=1;x=t%9;y=t//9
-  for j in g[y+2:][:len(s:={*g[y][x:x+2]+g[y+1][x:x+2]})*(min(s)>0)]:j[x:x+2]=3,3
+  for j in g[y+2:][:len(s:={*g[y][x:x+2]+g[y+1][x:x+2]})*all(s)]:j[x:x+2]=3,3
  return g
