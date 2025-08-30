87884bfb13b1e1209f2e88639d9ac852b4ca28b3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 18:09:27 +0900

    タスク 344: 更新 (95 -> 93)

--- 344/3_submission.py
+++ 344/3_submission.py
@@ -1,2 +1,1 @@
-f=lambda g:[*zip(*eval(str(g).replace('3, 2','8,0').replace('2, 3','0,8')))]
-p=lambda g:f(f(g))
+p=lambda g:exec("g[:]=zip(*eval(str(g).replace('3, 2','8,0').replace('2, 3','0,8')));"*2)or g
