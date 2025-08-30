c00a9bed19cf9f5343ecf2d15cf8e15107ebf123
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 11:41:59 +0900

    タスク 300: 更新 (100 -> 99)

--- 300/3_submission.py
+++ 300/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:(*(f:=lambda a,s=sum(g,[]):zip(*[r for r in a if max({*s}-{0},key=s.count)in r]))(f(g)),)
+p=lambda g:[*(f:=lambda a,s=sum(g,[]):zip(*[r for r in a if max({*s}-{0},key=s.count)in r]))(f(g))]
