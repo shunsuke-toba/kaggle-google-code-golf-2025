2ac0bccc59212f90c18b635a11ca10253d9449f2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 22:08:21 +0900

    タスク 208: 更新 (269 -> 265)

--- 208/3_submission.py
+++ 208/3_submission.py
@@ -1,4 +1,7 @@
 def p(g):
- f=sum(g,[]);a=f.index(k:=min(f,key=f.count));x=a%21;h=f[a::21].count(k);a//=21;w=g[a].count(k);i,j=[(i,j)for i in range(22-h)for j in range(22-w)if(j^x|i^a)>max(sum(r[j+1:j+w-1])for r in g[i+1:i+h-1])<1][0]
- while h:h-=1;g[i+h][j:j+w]=g[a+h][x:x+w]
+ f=sum(g,[]);a=f.index(k:=min(f,key=f.count));x=a%21;h=f[a::21].count(k);a//=21;w=g[a].count(k)
+ for i in range(22-h):
+  for j in range(22-w):
+   if max(sum(r[j+1:j+w-1])for r in g[i+1:i+h-1])<1:
+    for H in range(h):g[i+H][j:j+w]=g[a+H][x:x+w]
  return g
