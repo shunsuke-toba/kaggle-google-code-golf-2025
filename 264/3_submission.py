ac115a4b59ef72acadcccb7a4acd9306b0f3280d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 10:37:34 +0900

    タスク 264: 更新 (232 -> 227)

--- 264/3_submission.py
+++ 264/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  a=range(9);w=len(g[0])-2;d={}
  for x in range((len(g)-2)*w):
-  if all(t:=[g[x//w+i//3][x%w+i%3]for i in a]):d['ehinatqzu'.find(chr(97+sum(i*(t[i]!=5)for i in a)))]=t
- return[[d[r-r%3+c//3][r%3*3+c%3]for c in a]for r in a]
+  if all(t:=[g[x//w+i//3][x%w+i%3]for i in a]):d[chr(97+sum(i*(t[i]!=5)for i in a))]=t
+ return[[d['ehinatqzu'[r-r%3+c//3]][r%3*3+c%3]for c in a]for r in a]
