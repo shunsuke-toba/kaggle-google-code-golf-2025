623734e0984fd363a999543e7a492e92ea21b2c2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 07:35:34 +0900

    タスク 387: 更新 (279 -> 277)

--- 387/3_submission.py
+++ 387/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  e=enumerate;(y,i,a),(y,X,b),(Y,x,_),_=p=[(i,j,v)for i,s in e(g)for j,v in e(s)if v]
  while i<=X:g[y][i]=g[Y][i]=~min(i-x,X-i)%2*5;i+=1
- for i,s in e(g[y:Y+1],y):s[x]=s[X]=~min(i-y,Y-i)%2*5
+ for d,s in e(g[y:Y+1]):s[x]=s[X]=~min(d,Y-y-d)%2*5
  for i,j,v in p:
   for s in g[i-1:i+2]:s[j-1:j+2]=[a^b^v]*3;g[i][j]=v
  return g
