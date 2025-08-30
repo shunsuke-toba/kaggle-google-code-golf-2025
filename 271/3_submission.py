823bbcc57458b2a3880f476d4aeb1b39c85c1e34
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 00:57:54 +0900

    タスク 271: 更新 (102 -> 101)

--- 271/3_submission.py
+++ 271/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:min((0 in(s:=sum(t:=[r[k%7:][:3]for r in g[k//7:][:3]],[])),sum(s),t)for k in range(49))[2]
+p=lambda g:min((0in(s:=sum(t:=[r[k%7:][:3]for r in g[k//7:][:3]],[])),sum(s),t)for k in range(49))[2]
