f78004b6f784a1a124be7949e51c64ee0ccb1c61
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 10:37:28 +0900

    タスク 030: 更新 (123 -> 119)

--- 030/3_submission.py
+++ 030/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- b=sum(g,[]);I=b.index;g=[[0]*10for _ in g]
- for j,v in enumerate(b):g[(j+I(1)-I(v or 1))//10][j%10]+=v
+ b=sum(g,[]);I=b.index;g=[[0]*10for _ in g];j=0
+ for v in b:g[(j+I(1)-I(v or 1))//10][j%10]+=v;j+=1
  return g
