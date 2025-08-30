2493c9f09d3fd7bdc1265d392ae1113af74998ac
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 14:39:06 +0900

    タスク 268: 更新 (332 -> 330)

--- 268/3_submission.py
+++ 268/3_submission.py
@@ -4,10 +4,9 @@ def p(g):
   (a,c),*_,(b,d)=[(k//n,k%n)for k in R(n*n)if g[k//n][k%n]]
   if g[a][c+2]<1:break
   g=r(g)
- while b:b-=1;s=b<=a;g[b][c+1+s:d-s]=[4]*(d-c-1-2*s)
- for j in R(a):
-  h=g[a+~j]
-  if j<c+2:h[c+1-j]=4
-  if j<=n-d:h[d-1+j]=4
+ while b:
+  b-=1;s=b<=a;h=g[b];t=a+~b;h[c+1+s:d-s]=[4]*(d-c-1-2*s)
+  if s and t<c+2:h[c+1-t]=4
+  if s and t<=n-d:h[d-1+t]=4
  while k-4:g=r(g);k+=1
  return g
