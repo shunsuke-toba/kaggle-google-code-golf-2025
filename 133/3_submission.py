53d32ac54f4a73b87eb79b3da719d646c81ca1d8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 22:08:19 +0900

    タスク 133: 更新 (488 -> 486)

--- 133/3_submission.py
+++ 133/3_submission.py
@@ -1,9 +1,9 @@
 def p(g):
  r=range;l=len;w=l(g[0])
- def f(y,x):c=[(y,x,g[y][x])];g[y][x]=0;[c:=c+f(Y,X)for Y,X in((y+1,x),(y-1,x),(y,x+1),(y,x-1))if l(g)>Y>=0<=X<w and g[Y][X]];return c
- C=[f(y,x)for y in r(l(g))for x in r(w)if g[y][x]];B=min(C,key=lambda c:(l(c)<3,l(c)));Y,X,p=min(B,key=lambda t:sum(v[2]==t[2]for v in B))
+ def f(y,x):c=[(y,x,g[y][x])];g[y][x]=0;[c:=c+f(Y,X)for Y,X in((y+1,x),(y-1,x),(y,x+1),(y,x-1))if w>X>-1<Y<l(g) and g[Y][X]];return c
+ C=[f(y,x)for y in r(l(g))for x in r(w)if g[y][x]];B=min(C,key=lambda c:(l(c)<3,l(c)));Y,X,p=min(B,key=lambda t:sum(c==t[2]for*_,c in B))
  for c in C:
-  a,b,_=min(A:=[v for v in c if v[2]==p or(q:=v[2])<0]);m=int(l(A)**.5)
+  a,b,_=min(A:=[v for v in c if v[2]==p or(q:=v[2])*0]);m=int(l(A)**.5)
   for y,x,t in B:
-   for i in r(m):o=b+(x-X)*m;g[a+(y-Y)*m+i][o:o+m]=[[p,q][t!=p]]*m
+   for i in r(m):o=b+(x-X)*m;g[a+(y-Y)*m+i][o:o+m]=[(p,q)[t!=p]]*m
  return g
