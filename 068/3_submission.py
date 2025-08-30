a41844c66ca6ffc4840ccd106ad775120e00e3e9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 23:59:44 +0900

    タスク 068: 更新 (147 -> 139)

--- 068/3_submission.py
+++ 068/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- a=sum(g,[]);y,x=divmod(a.index(b:=min(a,key=a.count)),10);g=[10*[0]for _ in g]
- for R in g[y-1:y+2]:R[x-1:x+2]=2,2,2
- g[y][x]=b;return g
+ a=sum(g,[]);g=[0]*100;i=a.index(b:=min(a,key=a.count))
+ for j in 1,9,10,-~10:g[i+j]=g[i-j]=2
+ g[i]=b;return[*zip(*[iter(g)]*10)]
