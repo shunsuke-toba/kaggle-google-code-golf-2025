b13354976e1f7f3c0c714cc7f2501752472b8a5b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 22:37:50 +0900

    タスク 260: 更新 (167 -> 153)

--- 260/3_submission.py
+++ 260/3_submission.py
@@ -1,2 +1,2 @@
 def p(g,r=range(10)):
- a,*_,b=sorted([j-i,(c:=v,d:=j-i)[1]][v%5>0]for i in r for j in r if(v:=g[i][j]));return[[c*(j-i in(d,b+2*(b>d),a-2*(a<d)))for j in r]for i in r]
+ a,*_,b=sorted(d:=j-i+0*(c:=v)for i in r for j in r if(v:=g[i][j]));return[[c*(j-i in(d,b+2*(b>d),a-2*(a<d)))for j in r]for i in r]
