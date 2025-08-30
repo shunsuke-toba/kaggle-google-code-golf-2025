10a54b6ba4918315b6ad2e56ef5b8c03920e0700
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 07:35:27 +0900

    タスク 054: 更新 (433 -> 430)

--- 054/3_submission.py
+++ 054/3_submission.py
@@ -1,14 +1,13 @@
 def p(g,r=range):
  D=1,0,-1,0
  for z in r(676):
-  i=z//26;j=z%26;q=[]
-  for k in r(5):q+=g[i+k][j:j+5]
-  if q==q[::-1]and len({*q})>2:b=q[0];break
- for h in g[i:i+5]:h[j:j+5]=[b]*5
+  i=z//26;j=z%26
+  if(q:=sum((g[i+k][j:j+5]for k in r(5)),[]))==q[::-1]and len({*q})>2:b=q[0];break
+ for u in g[i:i+5]:u[j:j+5]=[b]*5
  for i,j in[(i,j)for i in r(30)for j in r(30)if g[i][j]==q[12]]:
   for d in r(4):
    v=q[(22,10,2,14)[d]];x=i;y=j
-   while(v-b)*(g[x][y]-b):g[x][y]=v;x+=D[d];y+=D[d-3]
+   while v!=b!=g[x][y]:g[x][y]=v;x+=D[d];y+=D[d-3]
   for t in r(25):
-   if (v:=q[t])!=b!=g[x:=i-2+t//5][y:=j-2+t%5]:g[x][y]=v
+   if(v:=q[t])!=b!=g[x:=i-2+t//5][y:=j-2+t%5]:g[x][y]=v
  return g
