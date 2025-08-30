1928d9f2899303ef27c91e67ac6cf4519c2c7272
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-30 15:52:01 +0900

    タスク 134: 更新 (237 -> 235)

--- 134/3_submission.py
+++ 134/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  s={*sum(g,[])};E=enumerate
  for d in s:
-  x,y=zip(*[(i,j)for i,r in E(g)for j,v in E(r)if v==d]);a=min(x);b=min(y);n=max(x)-a+1;m=n//3
-  if 19>n==max(y)-b+1:t=0,1,2;return[[(sum(s)-d)*(g[a+i*m][b+j*m]==d)for j in t]for i in t]
+  x,y=zip(*[(i,j)for i,r in E(g)for j,v in E(r)if v==d]);a=min(x);b=min(y);n=max(x)-a
+  if n==max(y)-b<18:m=-~n//3;t=0,1,2;return[[(sum(s)-d)*(g[a+i*m][b+j*m]==d)for j in t]for i in t]
