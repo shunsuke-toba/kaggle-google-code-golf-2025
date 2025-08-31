3be58b1d303506e06891121f4ff987d5228f863e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 18:31:08 +0900

    タスク 096: 更新 (452 -> 450)

--- 096/3_submission.py
+++ 096/3_submission.py
@@ -2,9 +2,9 @@ def p(g):
  import re;r=range;b=max(f:=sum(g,[]),key=f.count);t=lambda m:[*map(list,zip(*m[::-1]))];L=[]
  for x in{*f}-{b}:
   s=str(x);c=2*(f.count(x)>1)+1;y=c,x,c
-  for w in r(2,6):
-   for d in r(1,8):
-    for _ in r(4):y=any(re.search(s*w+f"[^{s}]"*d+s,str(R)[1::3])for R in g)and(2*w+d,x,w)or y;g=t(g)
+  for n in r(28):
+   w=n//7+2;d=n%7+1
+   for _ in r(4):y=any(re.search(s*w+f"[^{s}]"*d+s,str(R)[1::3])for R in g)and(2*w+d,x,w)or y;g=t(g)
   L+=y,
  m=max(L)[0];o=[[b]*m for _ in r(m)]
  for z,x,w in L:
