5b8b87d799bb362ec02021d510d7ab8fdc0c2fbc
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 00:41:34 +0900

    タスク 330: 更新 (192 -> 191)

--- 330/3_submission.py
+++ 330/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):R=range(10);f=lambda x,y:0if(x//10|y//10 or g[x][y]-5or(x,y)in v)else v.add((x,y))or-~f(x+1,y)+f(x-1,y)+f(x,y+1)+f(x,y-1);return[[g[i][j]%2+(v:={0},f(i,j)==6)[1]for j in R]for i in R]
+def p(g):R=range(10);f=lambda x,y:0if x//10|y//10or g[x][y]^5or(x,y)in v else v.add((x,y))or-~f(x+1,y)+f(x-1,y)+f(x,y+1)+f(x,y-1);return[[g[i][j]%2+(v:={0},f(i,j)==6)[1]for j in R]for i in R]
