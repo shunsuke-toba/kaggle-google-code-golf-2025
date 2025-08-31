8b56fb0495d13d0dba8871995efe8080431e1f7c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 08:09:39 +0900

    タスク 268: 更新 (287 -> 286)

--- 268/3_submission.py
+++ 268/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- n=len(g);r=lambda x:[*map(list,zip(*x[::-1]))]
- (a,c),*_,(b,d)=[(i//n,i%n)for i in range(n*n)if g[i//n][i%n]]
+ n=len(g);r=lambda x:[*map(list,zip(*x[::-1]))];(a,c),*_,(b,d)=[(i//n,i%n)for i in range(n*n)if g[i//n][i%n]]
  if g[a][c+2]:return r(r(r(p(r(g)))))
  while b:
   b-=1;h=g[b];s=b<=a;t=a+~b;h[c+1+s:d-s]=[4]*(d+~c-2*s)
