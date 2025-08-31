2c5eb3ffa6ae4dbb06110282eb976c17b4895a39
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 14:05:46 +0900

    タスク 240: 更新 (167 -> 165)

--- 240/3_submission.py
+++ 240/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  g=[[*map(max,a,b[::-1],a[::-1],b)]for a,b in zip(g,g[::-1])]
- for i in 1,3,5:k=i+2;exec('g[i][k]=g[~i][k]=g[k][i]=g[k][~i]=g[i][i+2];k+=2;'*(8-i))
+ for i in 1,3,5:k=i;exec('g[i][k:=k+2]=g[~i][k]=g[k][i]=g[k][~i]=g[i][i+2];'*(8-i))
  return g
