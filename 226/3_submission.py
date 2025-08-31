f2f54955dbf8aa7b03bc5cd02f8c74615491ef3b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 10:37:56 +0900

    タスク 226: 更新 (160 -> 159)

--- 226/3_submission.py
+++ 226/3_submission.py
@@ -1,6 +1,4 @@
 def p(g):
  def f(r,c):
   if 0<=r<10>c>=0==g[r][c]:g[r][c]=k;[f(r+d,c)or f(r,c+d)for d in(-1,1)]
- k=1;f(0,0);k=3;f(9,9)
- for r in 4,5:k=2;f(4,r),f(5,r)
- return g
+ k=1;f(0,0);k=3;f(9,9);k=2;f(4,4);f(4,5);f(5,4);f(5,5);return g
