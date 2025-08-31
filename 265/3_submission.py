3c61e5eef017af518bd55c8c1099153a1b980daf
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-31 22:12:39 +0900

    265: 改善(167 -> 156)

--- 265/3_submission.py
+++ 265/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
  for a,b in zip(g,g[1:]):
-  t=8,*map(lambda x,y:(x+y)&-3,a,b),8
+  t=8,*map(lambda x,y:x+y>4,a,b),8
   for j in range(17):
-   if t[j+1]<1>t[j+2] and t[j]|t[j+3]^8:a[j:j+2]=b[j:j+2]=2,2
+   if t[j+1]<1>t[j+2]<t[j]+t[6]:a[j:j+2]=b[j:j+2]=2,2
  return g
