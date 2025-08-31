d6a8c94bf719c28768a2ff09d69e50cc21cb249a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 14:05:47 +0900

    タスク 285: 更新 (421 -> 420)

--- 285/3_submission.py
+++ 285/3_submission.py
@@ -1,14 +1,15 @@
 def p(g):
  n=len(o:=[*map(list,g)]);d=[(i//3-1,i%3-1)for i in range(9)if i^4]
  for k in range(n*n):
-  if(c:=o[y:=k//n][x:=k%n])>0:
-   o[y][x]=-c;q=[(y,x)]
+  if c:=o[y:=k//n][x:=k%n]:
+   o[y][x]=0;s,t=y,x;q=[(y,x)]
    for y,x in q:
     for a,b in d:
      Y=y+a;X=x+b;v=n>Y>-1<X<n and o[Y][X]
-     if v==c:o[Y][X]=-c;q+=(Y,X),
-     elif a*b==0!=v!=-c:s,t=y,x
+     if v==c:o[Y][X]=0;q+=(Y,X),
+     elif a*b==0<v:s,t=y,x
    for a,b in d:
-    if(v:=o[s+a][t+b])*(v+c):
-     for y,x in q:g[s+a+[y-s,s-y][a&1]][t+b+[x-t,t-x][b&1]]=abs(v)
+    if v:=o[s+a][t+b]:
+     for y,x in q:g[s+a+[y-s,s-y][a&1]][t+b+[x-t,t-x][b&1]]=v
+   o[s][t]=c
  return g
