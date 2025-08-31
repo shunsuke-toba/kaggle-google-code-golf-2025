bb04c761325ca4f558f3bf8d35caacab67f5cf84
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 14:05:48 +0900

    タスク 366: 更新 (626 -> 616)

--- 366/3_submission.py
+++ 366/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,E=enumerate,R=range,L=len,F=filter):
- s=sum(g,[]);*_,z,y,x=sorted({*s},key=s.count);a,b=[[*map(list,zip(*F(lambda r:c in r,zip(*F(lambda r:c in r,g)))))]for c in(y,x)];n,m,p=L(a),L(a[0]),[]
+ s=sum(g,[]);*_,z,y,x=sorted({*s},key=s.count);a,b=[[*map(list,zip(*F(t:=lambda r:c in r,zip(*F(t,g))))) ]for c in(y,x)];n,m,p=L(a),L(a[0]),[]
  for o in R(n*m):
   if a[k:=(i:=o//m)][j:=o%m]^y:
    while a[k+1-n][l:=j]^y:k+=1
