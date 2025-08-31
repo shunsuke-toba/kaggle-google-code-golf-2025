ed9858ae5a985ad94b3269e3829ab69d7f5b365b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 08:09:40 +0900

    タスク 286: 更新 (137 -> 135)

--- 286/3_submission.py
+++ 286/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  a,b={*sum(g,[])}-{0,8}
- for _ in[0]*280:g=[*zip(*[[c or(v==a)*b+a*(v==b)for c,v in zip(r,(0,*r))]for r in g[::-1]])]
+ for _ in[0]*280:g=[*zip(*[[c or v%8 and a^b^v for c,v in zip(r,(0,*r))]for r in g[::-1]])]
  return g
