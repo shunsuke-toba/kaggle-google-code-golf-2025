a90dc3e9cde451a60bb4fa8081e4baedbdb3ca05
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 16:10:23 +0900

    タスク 143: 更新 (177 -> 170)

--- 143/3_submission.py
+++ 143/3_submission.py
@@ -1,7 +1,5 @@
 def p(g):
- t=sum(g,[])
- s=[i+i//3*7 for i in range(9)if t[i+i//3*7]]
- n=1
+ t=sum(g,[]);s=[i for i in range(30)if i%10<3<9*t[i]];n=1
  while{v:=t[n+i]for i in s}-{v}or t.count(v)-len(s):n+=1
- for i in s:g[(n+i)//10][(n+i)%10]=5
+ for i in s:g[(k:=n+i)//10][k%10]=5
  return g
