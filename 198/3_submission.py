5a162972ccc97aa418531bc106111c5b918310b9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 14:05:45 +0900

    タスク 198: 更新 (223 -> 222)

--- 198/3_submission.py
+++ 198/3_submission.py
@@ -1,5 +1,6 @@
 def p(g):
- h=len(g);q=[(n//h,n%h)for n in range(h*h)if sum(g[n//h]+[r[n%h]for r in g])*2>h*max(g)[0]]
- for y,x in q:
-  if 0<=y<h>x>=0==g[y][x]:g[y][x]=4;q+=(y+1,x),(y-1,x),(y,x+1),(y,x-1)
+ h=len(g);q=[n for n in range(h*h)if sum(g[n//h]+[r[n%h]for r in g])*2>h*max(g)[0]]
+ for n in q:
+  y=n//h;x=n%h
+  if 0<=y<h>x>=0==g[y][x]:g[y][x]=4;q+=n+h,n-h,n+(x<h-1),n-(x>0)
  return eval(str(g).replace(*"03"))
