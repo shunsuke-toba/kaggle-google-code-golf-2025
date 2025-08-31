63e47d43672643f06751c94b40868d38661a1497
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 18:31:12 +0900

    タスク 285: 更新 (420 -> 418)

--- 285/3_submission.py
+++ 285/3_submission.py
@@ -2,7 +2,7 @@ def p(g):
  n=len(o:=[*map(list,g)]);d=[(i//3-1,i%3-1)for i in range(9)if i^4]
  for k in range(n*n):
   if c:=o[y:=k//n][x:=k%n]:
-   o[y][x]=0;s,t=y,x;q=[(y,x)]
+   o[y][x]=0;q=[(s:=y,t:=x)]
    for y,x in q:
     for a,b in d:
      Y=y+a;X=x+b;v=n>Y>-1<X<n and o[Y][X]
