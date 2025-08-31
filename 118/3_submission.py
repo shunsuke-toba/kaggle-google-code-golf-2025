6ef0460a500af938842d7b2ad995261714e71a11
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 16:10:23 +0900

    タスク 118: 更新 (361 -> 355)

--- 118/3_submission.py
+++ 118/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
- R=range;F=[-2,0,1]+[0]*6;b=eval(str(g));t=2;h=len(g);w=len(g[0])
+ R=range;F=[-2,0,1]+[0]*6;b=eval(str(g));h=len(g);w=len(g[0]);t=2
  while 1:
-  B,i,j,t=max((sum(F[b[a][j]]for a in R(i-r,i+r+1)if-1<a<h)+sum(F[b[i][c]]for c in R(j-r,j+r+1)if-1<c<w)-F[b[i][j]]-r/3,i,j,r)for r in(3,2)[:4-t]for i in R(h)for j in R(w))
+  B,i,j,t=max((sum((0<=i+d<h and F[b[i+d][j]])+(0<=j+d<w and F[b[i][j+d]])for d in R(-r,r+1))-F[b[i][j]]-r//3,i,j,r)for r in(3,2)[:2-(t>2)]for i in R(h)for j in R(w))
   if B<1:return g
   for d in R(-t,t+1):
    for y,x in(i+d,j),(i,j+d):g[y][x]+=3*(g[y][x]==5);b[y][x]=0
