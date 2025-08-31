b643dea1a60c4abd3474e233fac75d2bb29b67fd
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 18:31:11 +0900

    タスク 209: 更新 (467 -> 466)

--- 209/3_submission.py
+++ 209/3_submission.py
@@ -1,10 +1,10 @@
 def p(g,A=range):
- c=i=a=e=99;b=d=f=j=0;L=[]
+ c=i=a=e=99;b=d=f=j=0;L=()
  for r in A(len(g)):
   for o,v in enumerate(g[r]):
    if v&4:b=r;d=o;a>r and(a:=r,c:=o)
    elif v*(a<b<=r):e=min(e,r);f=r;i=min(i,o);j=max(j,o)
-   elif v:L+=[(r,o,v)]
+   elif v:L+=(r,o,v),
  f+=1-e;j+=1-i
  for k in A(5):
   for r in A(a,b):
