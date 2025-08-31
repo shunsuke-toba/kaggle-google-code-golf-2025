7fe3d0d7e31f236621398c871a5607eed32a615e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 08:09:36 +0900

    タスク 169: 更新 (173 -> 172)

--- 169/3_submission.py
+++ 169/3_submission.py
@@ -1,4 +1,4 @@
-def p(g,k=100):
+def p(g,k=99):
  f=lambda i,j,c=4:-1<i<10>j>-1<c<g[i][j]and(exec('g[i][j]=c')or-~sum(f(i+d,j,c)+f(i,j+d,c)for d in(-1,1)))
- while k:k-=1;d=k//10,k%10;f(*d,5-f(*d))
+ while~k:d=k//10,k%10;f(*d,5-f(*d));k-=1
  return g
