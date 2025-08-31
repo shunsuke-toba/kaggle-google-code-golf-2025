a51d071fdc59786b336757dcc87a0dd3a59ec249
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-31 18:55:55 +0900

    364: 改善(258 -> 255)

--- 364/3_submission.py
+++ 364/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
  for _ in g:z=[[0]*len(g[0])];g=[[(c>0)*(a|b|d|e|((v:=a+b+d+e&3)<2)*4|(v>2)*(((-a-e)%4)*16+((-b-d)%4)*8))for a,b,c,d,e in zip(s,[0]+t,t,t[1:]+[0],u)]for s,t,u in zip(z+g,g,g[1:]+z)]
- return[[{15:2,23:2,27:1,11:6,19:6}.get(c,c)for c in r]for r in g]
+ return[[c and(6+(c&24>16)*-5+(c&4>3)*-4)for c in r]for r in g]
