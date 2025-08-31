3bae0fbeda03cabf4283fb1927a3d142dc10cc66
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 00:41:35 +0900

    タスク 390: 更新 (190 -> 189)

--- 390/3_submission.py
+++ 390/3_submission.py
@@ -1,5 +1,4 @@
 def p(g,e=range(15)):
- a=[(i,j,g[i][j])for i in e for j in e]
- for y,x,v in a:
+ for y,x,v in(a:=[(i,j,g[i][j])for i in e for j in e]):
   if v&4:_,i,j=min((abs(x-j+y-i),i,j)for i,j,u in a if(i==y or j==x)*u&2);g[2*i-y][2*j-x],g[y][x]=5,0
  return g
