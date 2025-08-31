b13439fd80f7ffbb3eb0819413fc345faef5f44a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 00:41:34 +0900

    タスク 349: 更新 (303 -> 296)

--- 349/3_submission.py
+++ 349/3_submission.py
@@ -1,8 +1,8 @@
-def p(g,R=range):
+def p(g):
  n=len(o:=eval(str(g)))
- for m in R(n*n):
+ for m in range(n*n):
   if(u:=g[i:=m//n])[j:=m%n]>g[i-1][j]*i|u[j-1]*j:
-   while i<n>g[i][j]>0:i+=1;d=(*u,0).index(0,j);r=d-j>>1;l=max(j-r,0)
+   while i<n>g[i][j]>0:i+=1;d=(*u,0).index(0,j);r=d-j>>1;l=j-r-n;a=m//n-r-n
    for y in o[i:]:y[j:d]=[1]*r*2
-   for k in R(max(m//n-r,0),min(i+r,n)):o[k][l:d+r]=[c or 3for c in g[k][l:d+r]]
+   for y,x in zip(o[a:i+r],g[a:i+r]):y[l:d+r]=[c or 3for c in x[l:d+r]]
  return o
