279f6f57aafcb6dac816cfcc9eaa2c73d8af5f90
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-31 18:07:00 +0900

    364: 解法の大幅変更(323 -> 307)

--- 364/3_submission.py
+++ 364/3_submission.py
@@ -1,13 +1,3 @@
 def p(g):
- m=99;G=set()
- for i,r in enumerate(g):
-  for j,v in enumerate(r):
-   if v:G|={i*m+j};r[j]=0
- while G:
-  s=[G.pop()];k=0
-  for p in s:
-   for q in p+1,p-1,p+m,p-m:
-    if q in G:s+=q,;G-={q}
-   k+=((p+1 in s)+(p-1 in s))*((p+m in s)+(p-m in s))
-  for p in s:g[p//m][p%m]=-~k*5%9
- return g
+ for _ in range(10):z=[[0]*len(g[0])];g=[[(c>0)*(a|b|d|e|(((a>0)+(b>0)+(d>0)+(e>0)>2)*4+((a>0)+(b>0)+(d>0)+(e>0)==1)*((a>0)*16+(b>0)*8+(d>0)*8+(e>0)*16)))for a,b,c,d,e in zip(s,[0]+t,t,t[1:]+[0],u)]for s,t,u in zip(z+g,g,g[1:]+z)]
+ return[[{15:2,23:2,27:1,11:6,19:6}.get(c,c)for c in r]for r in g]
