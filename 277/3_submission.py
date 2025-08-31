f53af2e514509c52b6781422a2a673fd83958320
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 10:37:57 +0900

    タスク 277: 更新 (215 -> 211)

--- 277/3_submission.py
+++ 277/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- def f(i,j,c=1,d=8):
-  g[i][j]=c;return-~sum(f(x,y,c,d)for s in range(9)if-1<(x:=i+s//3-1)<10>(y:=j+s%3-1)>-1<g[x][y]==d)
- f(*min((f(i,j),i,j)for k in range(99)if g[i:=k//10][j:=k%10]==8)[1:],2,1);return g
+ def f(i,j,d=8):
+  g[i][j]=d%2+1;return-~sum(f(x,y,d)for s in range(9)if-1<(x:=i+s//3-1)<10>(y:=j+s%3-1)>-1<g[x][y]==d)
+ f(*min((f(i,j),i,j)for k in range(99)if g[i:=k//10][j:=k%10]==8)[1:],1);return g
