6661677491a5d450b1180d2d1123321a13fc85af
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 00:41:32 +0900

    タスク 255: 更新 (372 -> 371)

--- 255/3_submission.py
+++ 255/3_submission.py
@@ -8,6 +8,6 @@ def p(g,t=lambda x:[*map(list,zip(*x[::-1]))]):
   g=t(g)
  for _ in' '*4:
   for i in r:
-   if all(next((v for v in b if(v-3)*v),0)>9for b in g[i-(i>0):i+2]):u=g[i].index(13);g[i][:u]=u*[3]
+   if all(next((v>9for v in b if(v-3)*v),0)for b in g[i-(i>0):i+2]):u=g[i].index(13);g[i][:u]=u*[3]
   g=t(g)
  return[[x%10for x in b]for b in g]
