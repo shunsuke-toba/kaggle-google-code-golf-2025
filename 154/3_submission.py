295d9dc11b80010d28fef03d313251697b8a7b9f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 14:05:44 +0900

    タスク 154: 更新 (183 -> 177)

--- 154/3_submission.py
+++ 154/3_submission.py
@@ -1,6 +1,7 @@
 def p(g):
- for i,j in[(k//15,k%15)for k,v in enumerate(sum(g,[]))if v>4]:
-  y=(i<2)-(i>11);x=(j<2)-(j>11);a,b=i,j
-  while g[a:=a+y][b:=b+x]-2:0
-  g[i][j]=0;g[a*2-i][b*2-j]=5
+ for k,v in enumerate(sum(g,[])):
+  if v>4:
+   a,b=i,j=k//15,k%15;y=(i<2)-(i>11);x=(j<2)-(j>11)
+   while g[a:=a+y][b:=b+x]-2:0
+   g[i][j]=0;g[a*2-i][b*2-j]=5
  return g
