e3d08f95f1be20a1ada0d8582e136deb0400eb09
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-08-31 14:56:28 +0900

    367: 改善(189 -> 183)

--- 367/3_submission.py
+++ 367/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for _ in[0]*20:z=[[0]*len(g[0])];g=[*map(list,zip(*[[c|4*(p==4or u+v<1<9<p+q)for c,(u,v,p,q)in zip(r,zip([0]+a,[0,0]+b,b,[0]+r))]for a,b,r in zip(z*2+g,z+g,g)][::-1]))]
+ for _ in[0]*20:z=[[0]*len(g[0])];g=[*map(list,zip(*[[c|4*(p==4or u+v<1<9<p+q)for c,u,v,p,q in zip(r,[0]+a,[0,0]+b,b,[0]+r)]for a,b,r in zip(z*2+g,z+g,g)][::-1]))]
  return g
