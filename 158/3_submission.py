6d1105775a15f06f72f7a26d18a0bae3e20bcf88
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 12:48:43 +0900

    タスク 158: 更新 (397 -> 386)

--- 158/3_submission.py
+++ 158/3_submission.py
@@ -1,12 +1,12 @@
-def p(g,R=range):
+def p(g,r=range):
  w=len(g[0])-2;t=0
- while len({*(p:=sum((r[t//w:][:3]for r in g[t%w:][:3]),[]))})<4:t+=1
- for m in 3,2,1:
-  q=R(s:=3*m);a=[[p[i//m*3+j//m]for j in q]for i in q]
-  for _ in R(4):
-   for y in R(len(g)+1-s):
-    for x in R(w+3-s):
-     if all((g[-1][0],v:=a[i][j])[p.count(v)<2]==g[y+i][x+j]for i in q for j in q):
-      for i in q:g[y+i][x:x+s]=a[i]
-   a=[*zip(*a[::-1])]
+ while len({*(f:=sum(p:=[R[t//w:][:3]for R in g[t%w:][:3]],[]))})<4:t+=1
+ for _ in r(4):
+  for m in 3,2,1:
+   q=r(s:=3*m)
+   for y in r(len(g)+1-s):
+    for x in r(w+3-s):
+     if all((g[-1][0],v:=p[i//m][j//m])[f.count(v)<2]==g[y+i][x+j]for i in q for j in q):
+      for i in q:g[y+i][x:x+s]=[p[i//m][j//m]for j in q]
+  p=[*zip(*p[::-1])]
  return g
