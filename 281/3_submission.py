c33eafdb497938de0b213f3a8c1aef9b5b2e6673
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 12:48:45 +0900

    タスク 281: 更新 (221 -> 219)

--- 281/3_submission.py
+++ 281/3_submission.py
@@ -2,5 +2,8 @@ p=lambda g:exec('''a=c=99;d=0;p=[];e=enumerate
 for y,r in e(g):
  for x,k in e(r):
   if k:a=min(a,b:=y);c=min(c,x);d=max(d,x);p+=({k}-{8,*p})
-for y in range(a,b+1):
- for x in range(c,d+1):g[y][x]=p[(a<y<b)&(c<x<d)]''')or g
+y=a
+while y<=b:
+ x=c
+ while x<=d:g[y][x]=p[(a<y<b)&(c<x<d)];x+=1
+ y+=1''')or g
