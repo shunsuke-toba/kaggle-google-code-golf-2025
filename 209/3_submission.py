a0aad1342e39232d46a79d814ee71eddc1d0a852
Shuichi Tamayose <tmshuichi@gmail.com>
2025-08-31 14:05:45 +0900

    タスク 209: 更新 (469 -> 467)

--- 209/3_submission.py
+++ 209/3_submission.py
@@ -1,15 +1,16 @@
-def p(g,R=range):
- c=i=a=e=99;b=d=f=j=0;L=[];m=len(g[0])
- for t in R(len(g)*m):
-  if(v:=g[r:=t//m][o:=t%m])&4:b=r;d=o;a>r and(a:=r,c:=o)
-  elif v*(a<b<=r):e=min(e,r);f=r;i=min(i,o);j=max(j,o)
-  elif v:L+=[(r,o,v)]
+def p(g,A=range):
+ c=i=a=e=99;b=d=f=j=0;L=[]
+ for r in A(len(g)):
+  for o,v in enumerate(g[r]):
+   if v&4:b=r;d=o;a>r and(a:=r,c:=o)
+   elif v*(a<b<=r):e=min(e,r);f=r;i=min(i,o);j=max(j,o)
+   elif v:L+=[(r,o,v)]
  f+=1-e;j+=1-i
- for k in R(5):
-  for r in R(a,b):
-   for o in R(c,d):
+ for k in A(5):
+  for r in A(a,b):
+   for o in A(c,d):
     try:
      for x,y,z in L:1/(g[e+(x-r)//k][i+(y-o)//k]==z)
-     for q in R(f*k):g[r+q][o:o+j*k]=[g[e+q//k][i+p//k]for p in R(j*k)]
+     for q in A(f*k):g[r+q][o:o+j*k]=[g[e+q//k][i+p//k]for p in A(j*k)]
      return[r[c:d+1]for r in g[a:b+1]]
     except:0
