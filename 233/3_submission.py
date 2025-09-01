47521b9b0fdc3b22a0d3ea474d4b4b7a9b4d0cde
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 18:50:48 +0900

    タスク 233: 更新 (476 -> 471)

--- 233/3_submission.py
+++ 233/3_submission.py
@@ -1,13 +1,12 @@
 def p(g):
- r=range;T=r(3);f=filter;z=zip;l=len;t=[]
- for y in r(l(g)-2):
-  for x in r(l(g[0])-2):
-   b=[g[y+i][x:x+3]for i in T];m=sum(b,[])
-   if 0<min(m)<max(m):
+ R=range;T=R(3);Z=zip;F=filter;L=len;t=[]
+ for y in R(L(g)-2):
+  for x in R(L(g[0])-2):
+   if 0<min(m:=sum(b:=[g[y+i][x:x+3]for i in T],[]))<max(m):
     t+=(-m.count(2),b),
     for i in T:g[y+i][x:x+3]=0,0,0
- g=[*map(list,z(*f(any,z(*f(any,g)))))]
+ g=[*map(list,Z(*F(any,Z(*F(any,g)))))]
  for _,b in sorted(t):
-  while(q:=next(((y,x)for y in r(l(g)-2)for x in r(l(g[0])-2)if all(g[y+i][x+j]==2*(b[i][j]!=2)for i in T for j in T)),0))==0:b=[*z(*b[::-1])]
+  while(q:=next(((y,x)for y in R(L(g)-2)for x in R(L(g[0])-2)if all(g[y+i][x+j]==2*(b[i][j]!=2)for i in T for j in T)),0))==0:b=[*Z(*b[::-1])]
   for i in T:y,x=q;g[y+i][x:x+3]=b[i]
  return g
