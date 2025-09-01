4709943d090802a1f71d8a7b108849e9fd05777b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 18:50:41 +0900

    タスク 034: 更新 (208 -> 207)

--- 034/3_submission.py
+++ 034/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
  while 2in(f:=sum(g,[])):
-  i=f.index(2);k=sum({*f})-2;a,b=i//9,i%9;d,e=f[i+9]<1or-1,f[i+1]<1or-1
+  k=sum({*f})-2;a=f.index(2);d,e=f[a+9]<1or-1,f[a+1]<1or-1;b=a%9;a//=9
   for _ in g:
    for x,y in(a,b),(a+d,b),(a,b+e):
     if 9>x>-1<y<9:g[x][y]=k
