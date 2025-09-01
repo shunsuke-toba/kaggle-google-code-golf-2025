5149396c2580bf2ad4756322a01aa6a0b2f04af5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 18:50:44 +0900

    タスク 096: 更新 (450 -> 447)

--- 096/3_submission.py
+++ 096/3_submission.py
@@ -3,8 +3,7 @@ def p(g):
  for x in{*f}-{b}:
   s=str(x);c=2*(f.count(x)>1)+1;y=c,x,c
   for n in r(28):
-   w=n//7+2;d=n%7+1
-   for _ in r(4):y=any(re.search(s*w+f"[^{s}]"*d+s,str(R)[1::3])for R in g)and(2*w+d,x,w)or y;g=t(g)
+   for _ in r(4):w=n//7+2;d=n%7+1;y=any(re.search(s*w+f"[^{s}]"*d+s,str(R)[1::3])for R in g)and(2*w+d,x,w)or y;g=t(g)
   L+=y,
  m=max(L)[0];o=[[b]*m for _ in r(m)]
  for z,x,w in L:
