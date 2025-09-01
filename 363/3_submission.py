b8f043118edbd4329f444ca207efc430653877bb
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-02 08:01:24 +0900

    タスク 363: 更新 (252 -> 248)

--- 363/3_submission.py
+++ 363/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
- R=range(132);h=sum(([5,*r,5]for r in g),[5]*12)+[5]*40
- p=[i for i in R if h[i]&2];v=[k for k in R if max(h[i+k-p[0]]for i in p)<1];v=v[v==[28,79]:];v[1:3]*=v>v[5:]
+ r=range(120);h=sum(([5,*x,5]for x in g),[])+[5]*36;p=[i for i in r if h[i]&2];o=p[0];v=[k for k in r if max(h[i+k-o]for i in p)<1];v=v[v==[16,67]:];v[1:3]*=v>v[5:]
  for k in v:
-  for i in p:h[i+k-p[0]]=2
- return[h[i:i+10]for i in R[13::12]]
+  for i in p:h[i+k-o]=2
+ return[h[i+1:i+11]for i in r[::12]]
