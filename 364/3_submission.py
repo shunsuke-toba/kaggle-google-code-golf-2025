5427343b098f049bc323a1accbdca55de7bf345e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-02 01:44:00 +0900

    タスク 364: 更新 (236 -> 230)

--- 364/3_submission.py
+++ 364/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
  for _ in g:z=[[0]*30];g=[[c and(a|b|d|e|((v:=a+b+d+e&3)<2)*4|(v>2)*(8*(b+d>0)+16*(a+e>0)))for a,b,c,d,e in zip(s,[0]+t,t,t[1:]+[0],u)]for s,t,u in zip(z+g,g,g[1:]+z)]
- return[[c and(6-5*(c>23)-4*(c&4>3))for c in r]for r in g]
+ return[[c and[2,6,1,2,6][c%5]for c in r]for r in g]
