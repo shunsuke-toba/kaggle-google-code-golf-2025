915639ee416de6a578fcadd221e10a09c2fa3ce4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 12:54:02 +0900

    タスク 230: 更新 (129 -> 126)

--- 230/3_submission.py
+++ 230/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,e=enumerate):
  for r,a in e(g,-1):
   for c,v in e(a):
-   if v>4:t,u=g[r][c]>4,a[c-1]>4;g[r+2*t][c+2*u-1]=t*2-~u
+   if v>4:T=g[r][c]//2;u=a[c-1]>4;g[r+T][c+2*u-1]=T-~u
  return g
