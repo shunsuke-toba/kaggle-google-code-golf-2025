5b505e9476c9b59ceb977b0cc84571815296eece
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-02 00:12:14 +0900

    タスク 199: 更新 (99 -> 91)

--- 199/3_submission.py
+++ 199/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):r=g.index(R:=max(g))+1;g[r]=R[:];g[:r]=[([4,0]*8)[R.index(max(R))&1:][:len(g)]]*r;return g
+def p(g):r=g.index(R:=max(g))+1;g[:r+1]=[([4,0]*8)[max(R[::2])<1:][:len(g)]]*r+[R];return g
