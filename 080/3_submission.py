495b90a7ca9614114af60ecc13c08bebbe954cc5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 18:50:43 +0900

    タスク 080: 更新 (307 -> 306)

--- 080/3_submission.py
+++ 080/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,R=range,n=0):
- while 0 in g[n]:n+=1
+ while 0in g[n]:n+=1
  s=n+1;m=len(g)//s+1;p=max(([g[(y+i//3)*s][(x+i%3)*s]for i in R(9)]for y in R(m-2)for x in R(m-2)),key=sum)
  for i in R(m*m):
   for k in R(9*(g[i//m*s][i%m*s]==p[4])):
