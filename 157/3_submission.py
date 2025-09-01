cd7917e1ac8d53e8254569c5df9d46407304cf62
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 23:22:14 +0900

    タスク 157: 更新 (412 -> 411)

--- 157/3_submission.py
+++ 157/3_submission.py
@@ -8,6 +8,6 @@ def p(g):
     if u<10>-1<v<15>g[u][v]>4:g[u][v]=0;t+=(u-y,v-x),
   s+=t,
  while s:
-  m,t,i,j=min((-sum(i+a<3 for a,b in t),t,i,j)for t in s for i in(1,2)for j in r(15)if all(j+b<15>g[i+a][j+b]<1for a,b in t));s.remove(t)
+  _,t,i,j=min((~sum(i+a<3for a,b in t),t,i,j)for t in s for i in(1,2)for j in r(15)if all(j+b<15>g[i+a][j+b]<1for a,b in t));s.remove(t)
   for a,b in t:g[i+a][j+b]=1
  return g
