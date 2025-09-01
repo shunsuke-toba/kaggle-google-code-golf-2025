8ed397372bb10c4dd7076c1e01e3021a64953031
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-02 08:01:22 +0900

    タスク 173: 更新 (268 -> 267)

--- 173/3_submission.py
+++ 173/3_submission.py
@@ -4,5 +4,5 @@ def p(g):
   for d in 1,w,w+1,w-1:
    if(a:=g[j+d])==g[j-d]>0<(c:=g[j]):
     for k in P:
-     if c==g[k]or a==g[k+d]==g[k-d]:g[k-d:k+d+1:d]=a,c,a
+     if c==g[k]or a==g[k+d]==g[k-d]:g[k-d:k-~d:d]=a,c,a
  return[g[i:i+w-2]for i in P[::w]]
