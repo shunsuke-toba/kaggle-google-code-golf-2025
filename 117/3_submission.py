20382c075c38e7c3d9b55566283eb9212fba87fa
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-01 23:22:12 +0900

    タスク 117: 更新 (203 -> 199)

--- 117/3_submission.py
+++ 117/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
  w=len(g);f=sum(g,[]);k=w*w-w-2
  while(f[k]==f[k+~w]==f[k-w+1]==f[k+w-1]==f[k-~w]>0)<1:k-=1
- a=k//w*2
  for b,v in enumerate(f):
-  if v:g[r:=b//w][d:=k%w*2-b%w]=g[a-r][b%w]=g[a-r][d]=v
+  if v:g[y:=k//w*2-b//w][b%w]=g[y][d:=k%w*2-b%w]=g[b//w][d]=v
  return g
