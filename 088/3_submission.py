7f9d493ff850537c15b01e3a9b44ef5e27f3d3ad
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-03 04:15:10 +0900

    タスク 088: 更新 (109 -> 107)

--- 088/3_submission.py
+++ 088/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:exec('g[:]=zip(*g[::-1][not sum(g[-1]):]);'*44)or[[c and g[0][0]for c in r[1:-1]]for r in g[1:-1]]
+p=lambda g:exec('g[:]=zip(*g[::-1][sum(g[-1])<1:]);'*44)or[[c and g[0][0]for c in r[1:-1]]for r in g[1:-1]]
