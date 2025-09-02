2afd98a32b134285fe49e3069d58c001b301ee09
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-03 02:01:22 +0900

    タスク 324: 更新 (268 -> 266)

--- 324/3_submission.py
+++ 324/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
- f=sum(g,[]);a=f[f.count(f[0])<3];*c,=zip(*g);s=[];m={};e=enumerate
+ f=sum(g,[]);c=f.count;a=f[c(f[0])<3];*h,=zip(*g);s=[];m={};e=enumerate
  for i,r in e(g):
   for j,d in e(r):
-   if f.count(d)<3:s+=i+j,i-j-20;m[a in r and a in c[j]]=d
- return [[(d,m[a in r and a in c[j]])[i+j in s or i-j-20 in s]for j,d in e(r)]for i,r in e(g)]
+   if c(d)<3:s+=i+j,i-j-20;m[a in r and a in h[j]]=d
+ return [[(d,m[a in r and a in h[j]])[i+j in s or i-j-20 in s]for j,d in e(r)]for i,r in e(g)]
