45fe1da0add2172b639ce3ec0ca7b388320e4cdc
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-03 02:01:18 +0900

    タスク 156: 更新 (204 -> 198)

--- 156/3_submission.py
+++ 156/3_submission.py
@@ -1,8 +1,8 @@
 def p(g):
- T=[[],[]];A=[0,0];i=0
+ T=[[],[]];i=d=0
  for r in g:
-  if s:=sum(r)>>2:T[i]+=r,;A[i]+=s
-  elif A[i]:i=1
+  if s:=sum(r)>>2:T[i]+=r,;d+=s-s*2*i
+  elif T[i]:i=1
  for i in 0,1:
-  for r in T[i][1:-1]:x=r.index(4);w=sum(r)>>2;r[x+1:x+w-1]=[1+(A[i]>A[~i])]*(w-2)
+  for r in T[i][1:-1]:x=r.index(4);w=sum(r)>>2;r[x+1:x+w-1]=[-~((d>0)^i)]*(w-2)
  return g
