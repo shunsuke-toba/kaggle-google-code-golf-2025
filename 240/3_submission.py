10bd88532a188ed4cfe32bd92f00f1d3a381d70d
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-09-03 21:10:44 +0900

    240: 改善(102 -> 100)

--- 240/3_submission.py
+++ 240/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for k in range(1700):g[i:=k%19][j:=k%18]|=g[i][j-2]*(9>i+3<j<10)|g[j][i]|g[~i][j]
+ for k in range(1700):g[i:=k%19][j:=k%18]|=g[i][j-2]*(i+3<j<10)|g[j][i]|g[~i][j]
  return g
