03f0dd4d0d0505567f445b0f522255f31f0ae9ad
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-03 19:15:14 +0900

    タスク 160: 更新 (137 -> 136)

--- 160/3_submission.py
+++ 160/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,z=64):
  while z:
-  z-=1;a,b,c,*_=g[z>>3:];s=slice(z%8,z%8+3)
-  if(a[s]+b[s]+c[s]).count(0)<2:a[s]=c[s]=0,2,0;b[s]=2,2,2
+  z-=1;a,b,c,*_=g[z>>3:]
+  if(a[s:=slice(z%8,z%8+3)]+b[s]+c[s]).count(0)<2:a[s]=c[s]=0,2,0;b[s]=2,2,2
  return g
