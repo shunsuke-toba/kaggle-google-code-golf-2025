b59e7585d71be98406a661598c548088246d2d2a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-04 00:06:08 +0900

    タスク 255: 更新 (368 -> 367)

--- 255/3_submission.py
+++ 255/3_submission.py
@@ -7,6 +7,6 @@ def p(g,r=range(30),t=lambda x:[*map(list,zip(*x[::-1]))]):
   g=t(g)
  for _ in' '*4:
   for i in r:
-   if all(bytes(b).strip(b'\0\3')>=b'\n' for b in g[i-(i>0):i+2]):u=g[i].index(13);g[i][:u]=u*[3]
+   if all(bytes(b).strip(b'\0\3')>b'\n' for b in g[i-(i>0):i+2]):u=g[i].index(13);g[i][:u]=u*[3]
   g=t(g)
  return[[x%10for x in b]for b in g]
