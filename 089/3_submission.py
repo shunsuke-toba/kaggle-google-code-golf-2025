ddcd683b1f1d9de68e7af615420aa750303021e3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-04 00:06:06 +0900

    タスク 089: 更新 (320 -> 319)

--- 089/3_submission.py
+++ 089/3_submission.py
@@ -1,8 +1,10 @@
 R=range(-2,3)
 def p(g):
  for k in 2,3:
-  if(a:=[(i//13,i%13)for i in range(169)if sum(g,[])[i]==k]):_,T,B=max((len(t:=sorted((v,x,y)for x in R for y in R if-1<i+x<13>j+y>=0<(v:=g[i+x][j+y])!=k)),(i,j),t)for i,j in a);c=B[_>>1][0]
+  a=[(i//13,i%13)for i in range(169)if sum(g,[])[i]==k]
   for i,j in a:
+   if (B:=sorted((v,x,y)for x in R for y in R if-1<i+x<13>j+y>=0<(v:=g[i+x][j+y])!=k))[2:]:c=B[len(B)>>1][0];break
+  for I,J in a:
    for v,x,y in B:
-    if v==c:g[i+x][j+y*((i,j)==T or k*2-5)]=c
+    if v==c:g[I+x][J+y*((I,J)==(i,j)or k*2-5)]=c
  return g
