0bec5044a791f495f61c8119928f64729e960b1d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-04 00:06:08 +0900

    タスク 174: 更新 (130 -> 120)

--- 174/3_submission.py
+++ 174/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- for c in(sum(g,[])):
-  a=lambda x:c in x;f=lambda g:zip(*filter(a,g));s=[*f(f(g))]
-  if s==[r[::-1]for r in s]:return s
+p=lambda g:next(s for c in sum(g,[]) if (s:=[*zip(*filter(a:=lambda x:c in x,zip(*filter(a,g))))])==[r[::-1]for r in s])
