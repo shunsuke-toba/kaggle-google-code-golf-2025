cba9f23171d46e9a887559dbe88ff6daa0533cbc
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-03 13:45:10 +0900

    タスク 165: 更新 (188 -> 182)

--- 165/3_submission.py
+++ 165/3_submission.py
@@ -1,8 +1,6 @@
 def p(g):
- k=max(r[i]&r[i+1]&r[i+2]&p[i+1]for r,p in zip(g[1:],g)for i in range(18))
- i=0
+ k=max(r[i]&r[i+1]&r[i+2]&p[i+1]for r,p in zip(g[1:],g)for i in range(18));i=-1
  for c in zip(*g):
-  y=bytes(c).rfind(k)+1;v=max(c[y:])
-  for r in g[y:]*y:r[i]=v
-  i+=1
+  i+=1;y=bytes(c).rfind(k)+1
+  for r in g[y:]*y:r[i]=max(c[y:])
  return g
