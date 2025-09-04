c521d889373327e8e829b2eed87bffc686be99c2
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-09-05 05:44:19 +0900

    364: 改善(215 -> 189)

--- 364/3_submission.py
+++ 364/3_submission.py
@@ -1,3 +1,3 @@
-def p(g):
- for i in range(96):*g,=zip(*[[(c>0)*(u|c|l|(u>0<l)*(4**(i%4+1)))for u,c,l in zip(a,b,[0,*b])]for a,b in zip([[0]*30]+g,g)][::-1])
- return[[[0,0,0,1,6,2][bin(c).count('1')]if c else 0for c in r]for r in g]
+def p(g,i=96):
+ while i:*g,=zip(*[[(c>0)*(u|c|l|(u>0<l)*2**(i%4)*4)for u,c,l in zip(a,b,[0,*b])]for a,b in zip([30*[0]]+g,g)][::-1]);i-=1
+ return[[c.bit_count()*5%14%9for c in r]for r in g]
