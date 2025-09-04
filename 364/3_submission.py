745887304656ff8a2226646f08ba2e4c9c017ed9
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-09-05 06:12:33 +0900

    364: 改善(187 -> 185)

--- 364/3_submission.py
+++ 364/3_submission.py
@@ -1,3 +1,3 @@
 def p(g,i=4):
- for _ in g*4:*g,=zip(*[[(c>0)*(u|c|l|(u>0<l)*i&63)for u,c,l in zip(a,b,[0,*b])]for a,b in zip([30*[0]]+g,g)][::-1]);i*=2
+ for _ in g*4:*g,=zip(*[[(c>0)*(u|c|(u>0<l)*i&63)for u,c,l in zip(a,b,[0,*b])]for a,b in zip([30*[0]]+g,g)][::-1]);i*=2
  return[[c.bit_count()*5%14%9for c in r]for r in g]
