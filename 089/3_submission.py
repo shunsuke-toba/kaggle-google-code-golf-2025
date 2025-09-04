d69049256ef8eabbad93dfb92bb01951251f8968
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-04 20:52:28 +0900

    タスク 089: 更新 (319 -> 318)

--- 089/3_submission.py
+++ 089/3_submission.py
@@ -3,7 +3,7 @@ def p(g):
  for k in 2,3:
   a=[(i//13,i%13)for i in range(169)if sum(g,[])[i]==k]
   for i,j in a:
-   if (B:=sorted((v,x,y)for x in R for y in R if-1<i+x<13>j+y>=0<(v:=g[i+x][j+y])!=k))[2:]:c=B[len(B)>>1][0];break
+   if(B:=sorted((v,x,y)for x in R for y in R if-1<i+x<13>j+y>=0<(v:=g[i+x][j+y])!=k))[2:]:c=B[len(B)>>1][0];break
   for I,J in a:
    for v,x,y in B:
     if v==c:g[I+x][J+y*((I,J)==(i,j)or k*2-5)]=c
