ac2b8fcca48e61a9cde277aab33033a4bfe11175
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-04 20:52:30 +0900

    タスク 366: 更新 (611 -> 602)

--- 366/3_submission.py
+++ 366/3_submission.py
@@ -5,10 +5,10 @@ def p(g,E=enumerate,R=range,L=len,F=filter,S=sorted):
    while a[k+1-n][l:=j]^y:k+=1
    while l+1<m and a[k][l+1]^y:l+=1
    q=a[i:k+1];p+=(-L(s:=sum(t:=[r[j:l+1]for r in q],[]))+s.count(z),t),
-   for r in q:r[j:l+1]=[y]*(l-j+1)
+   for r in q:r[j:l+1]=[y]*(-~l-j)
  for _,r in S(p):
-  for i in R(L(b)-L(r)+1):
-   for j in R(L(b[0])-L(r[0])+1):
+  for i in R(n-L(r)+1):
+   for j in R(m-L(r[0])+1):
     if all(b[i+u][j+v]==(x,c)[c!=z]for u,s in E(r)for v,c in E(s)):
      for k,s in E(r):b[i+k][j:j+L(s)]=s
  return b
