f331ff75343fa632f06ab71ff20b47531db54c7d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-05 02:41:55 +0900

    タスク 018: 更新 (542 -> 541)

--- 018/3_submission.py
+++ 018/3_submission.py
@@ -1,10 +1,10 @@
 def p(g):
  R=range;L=len;h,w=L(g),L(g[0]);S=[]
- for p in R(h*w):
-  y=p//w;x=p%w;a=b=0
+ for y in R(h*w):
+  x=y%w;y//=w;a=b=0
   for Y in R(y+1,h+1):
    for X in R(x+1,w+1):
-    if L({*sum(m:=[q[x:X]for q in g[y:Y]],[0])})+all(map(sum,m+[*zip(*m)]))>5:a,b,r=Y,X,m
+    if L({*sum(m:=[q[x:X]for q in g[y:Y]],[0])})*all(map(sum,m+[*zip(*m)]))>4:a,b,r=Y,X,m
   if a:S+=r,
   for t in g[y:a]:t[x:b]=[0]*(b-x)
  for r in S:
