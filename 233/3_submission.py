08cd957d2a6328d671fb938a2f03fa7f71940621
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-05 02:41:59 +0900

    タスク 233: 更新 (471 -> 468)

--- 233/3_submission.py
+++ 233/3_submission.py
@@ -7,6 +7,6 @@ def p(g):
     for i in T:g[y+i][x:x+3]=0,0,0
  g=[*map(list,Z(*F(any,Z(*F(any,g)))))]
  for _,b in sorted(t):
-  while(q:=next(((y,x)for y in R(L(g)-2)for x in R(L(g[0])-2)if all(g[y+i][x+j]==2*(b[i][j]!=2)for i in T for j in T)),0))==0:b=[*Z(*b[::-1])]
+  while not any((q:=(y,x))for y in R(L(g)-2)for x in R(L(g[0])-2)if all(g[y+i][x+j]==2*(b[i][j]!=2)for i in T for j in T)):b=[*Z(*b[::-1])]
   for i in T:y,x=q;g[y+i][x:x+3]=b[i]
  return g
