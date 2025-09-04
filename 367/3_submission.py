8a02ec7b44be3f743c6338656f968aa401d7489e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-04 23:07:17 +0900

    タスク 367: 更新 (162 -> 161)

--- 367/3_submission.py
+++ 367/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for _ in g*4:z=[[0]*20];g=[*zip(*[[c|4*(p&q>4>u|v)|p%5for c,p,q,u,v in zip(r,b,[0,*r],[0,*a],[0,0,*b])]for a,b,r in zip(z*2+g,z+g,g)][::-1])]
+ for _ in g*4:z=[[0]*20];*g,=zip(*[[c|4*(p&q>4>u|v)|p%5for c,p,q,u,v in zip(r,b,[0,*r],[0,*a],[0,0,*b])]for a,b,r in zip(z*2+g,z+g,g)][::-1])
  return g
