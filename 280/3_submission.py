23bade8f2299048481d471deda8a017e81e6d489
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-04 13:25:43 +0900

    タスク 280: 更新 (181 -> 175)

--- 280/3_submission.py
+++ 280/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
  for i in range(6400):
-  n=len(g);i//=4;j=i%~-n;i%=n;a=0
-  while g[i][j+1]<(x:=g[i][j-a])<3+a:g[i+a][j:]=g[i-a][j:]=[x]*(n-j);a+=1
-  g=[*map(list,zip(*g[::-1]))]
+  g=[*map(list,zip(*g[::-1]))];n=len(g);i//=4;j=i%~-n;i%=n;a=0
+  while g[i][j+1]<(x:=g[i][j-a])<3+a:g[i+a][j:]=g[i-a][j:]=[x]*n;a+=1
  return g
