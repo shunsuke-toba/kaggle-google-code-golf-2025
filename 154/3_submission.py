0e603a222dfb584980dc39d47a33f072b86d6a04
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-09-05 13:38:38 +0900

    154,390: 全く同じコードで短縮

--- 154/3_submission.py
+++ 154/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for _ in[0]*4:
   for r in(g:=[*map(list,zip(*g[::-1]))]):
-   if{0,5}<{*r}:j=r.index(2);r[j-3:j+4]=0,0,0,2,*r[:j][:-4:-1]
+   if{0,5}<{*r}:j=r.index(2);r[j-3:j+4]=r[j-3:j+4][::-1]
  return g
