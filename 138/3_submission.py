73ffc659baa9f381b9d231f2706e45efaf2b47e8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-05 19:37:28 +0900

    タスク 138: 更新 (157 -> 156)

--- 138/3_submission.py
+++ 138/3_submission.py
@@ -1,1 +1,6 @@
-p=lambda g:eval("[*zip(*[[(c,r[0])[r[0]in r[i:]]for i,c in enumerate(r)]for r in"*4+"[*zip(*(lambda x:x[0in x[0]:])("*40+str(g)+")[::-1])]"*40+"[::-1]])]"*4)
+def p(g):
+ for i in range(8):
+  while 0in g[0]:g=g[1:]
+  if i&4:g=[[(c,r[0])[r[0]in r[j:]]for j,c in enumerate(r)]for r in g]
+  g=[*zip(*g)][::-1]
+ return g
