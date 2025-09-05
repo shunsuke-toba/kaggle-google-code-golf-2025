11d141612d4183117111a203ca18898ec7ca8b2f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-05 10:11:20 +0900

    タスク 368: 更新 (143 -> 142)

--- 368/3_submission.py
+++ 368/3_submission.py
@@ -2,5 +2,5 @@ r=range(100)
 def p(g):
  g=sum(g,[]);a=[i for i in r if g[i]%5]
  for i in r:
-  for s in a*(g[i]==5):g[i+s-a[0]]=g[s]
+  for s in(g[i]==5)*a:g[i+s-a[0]]=g[s]
  return*zip(*[iter(g)]*10),
