06d5d9f8803c04ec4ebaf3201534b46baf2f6378
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-09-05 13:35:22 +0900

    390: 154をほぼそのまま移植(189 -> 140)

--- 390/3_submission.py
+++ 390/3_submission.py
@@ -1,4 +1,5 @@
-def p(g,e=range(15)):
- for y,x,v in(a:=[(i,j,g[i][j])for i in e for j in e]):
-  if v&4:_,i,j=min((abs(x-j+y-i),i,j)for i,j,u in a if(i==y or j==x)*u&2);g[2*i-y][2*j-x],g[y][x]=5,0
+def p(g):
+ for _ in[0]*4:
+  for r in(g:=[*map(list,zip(*g[::-1]))]):
+   if{0,5}<{*r}:j=r.index(2);r[j-3:j+4]=*r[j:j+4][::-1],0,0,0
  return g
