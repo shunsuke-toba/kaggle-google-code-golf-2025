18ae65ced6eca5e7be3d9d2fb6b9f758f7df3fe2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-05 10:11:18 +0900

    タスク 265: 更新 (146 -> 145)

--- 265/3_submission.py
+++ 265/3_submission.py
@@ -1,6 +1,1 @@
-def p(g):
- for a,b in zip(g,g[1:]):
-  t=5,*map(sum,zip(a,b))
-  for j in range(17):
-   if t[j]|t[6]>4>t[j+1]|t[j+2]:a[j:j+2]=b[j:j+2]=2,2
- return g
+p=lambda g:[exec('t=5,*map(sum,zip(a,b))\nfor j in range(17):\n if t[j]|t[6]>4>t[j+1]|t[j+2]:a[j:j+2]=b[j:j+2]=2,2')for a,b in zip(g,g[1:])]and g
