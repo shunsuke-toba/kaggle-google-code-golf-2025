8399e1ef3253b8af6f944585032010e51cb625f0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-05 10:11:17 +0900

    タスク 184: 更新 (144 -> 142)

--- 184/3_submission.py
+++ 184/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,f=lambda m:zip([0]+(b:=[i for i,a in enumerate((*m,[]))if sum(a)<1]),b):[[max(max(g[a:b])[c:d])for c,d in f(zip(*g))]for a,b in f(g)]
+p=lambda g,f=lambda m:map(slice,[0]+(b:=[i for i,a in enumerate((*m,[]))if sum(a)<1]),b):[[max(max(g[s])[t])for t in f(zip(*g))]for s in f(g)]
