e6462e2b9071adba2baefe2e92147c7997d48550
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-09-05 17:02:08 +0900

    154,390: 改善(135 -> 133)

--- 390/3_submission.py
+++ 390/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for _ in[0]*4:
   for r in(g:=[*map(list,zip(*g[::-1]))]):
-   if{0,5}<{*r}:j=r.index(2);r[j-3:j+4]=r[j-3:j+4][::-1]
+   if{0,5}<{*r}:j=r.index(2)-3;r[j:j+7]=r[j:j+7][::-1]
  return g
