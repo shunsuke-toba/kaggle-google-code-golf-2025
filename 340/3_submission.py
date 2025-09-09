185cc46b661ee431d61e17ad5ae440d00da7c079
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-10 02:05:02 +0900

    タスク 340: 更新 (141 -> 140)

--- 340/3_submission.py
+++ 340/3_submission.py
@@ -2,5 +2,5 @@ def p(g):
  for r in g[1:-2]:
   j=1
   for c in r[2:-2]:
-   for a in-2,1:b=a//2;j-=b;r[j]=0;r[a]+=c*(c==r[b]);g[a][j]+=c*(c==g[b][j])
+   for a in-2,1:r[a]+=c*(c==r[b:=a//2]);r[j:=j-b]=0;g[a][j]+=c*(c==g[b][j])
  return g
