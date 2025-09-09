5dff2dce759bae3c63e7b58ee835cfb4fca5af46
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-09-10 02:56:15 +0900

    226: 改善(147 -> 145)

--- 226/3_submission.py
+++ 226/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- g[0][0]=1;g[9][9]=3;g[4|g[4][0]][4|g[0][4]]=2
- for _ in g*2:*g,=zip(*[[b|(5>a>b)*a for a,b in zip([5,*r],r)]for r in g][::-1])
- return g
+p=lambda g:exec("g[0][0]=1;g[9][9]=3;g[4|g[4][0]][4|g[0][4]]=2\n"+12*"g[:]=zip(*[[b|(a<5>b)*a for a,b in zip([5,*r],r)]for r in g][::-1])\n")or g
