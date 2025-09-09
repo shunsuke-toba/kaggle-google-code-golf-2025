df24ce474177d0c3a5125808e03f89152c02b110
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-09-09 21:48:18 +0900

    325: 改善(178 -> 168)

--- 325/3_submission.py
+++ 325/3_submission.py
@@ -1,8 +1,6 @@
-def p(g):
- n=0
- for y,r in enumerate(g):
-  for x,v in enumerate(r):
-   if v:
-    n+=1
-    for R in g[y:y+4]:R[max(x-3,0):x+4]=[0]*7
+def p(g,n=0,e=enumerate):
+ for y,r in e(g):
+  for x,v in e(r):
+   n+=v//8
+   for R in g[y:y+v//2]:R[max(x-2,0):x+4]=[0]*6
  return[[0]*i+[8]+[0]*(n+~i)for i in range(n)]
