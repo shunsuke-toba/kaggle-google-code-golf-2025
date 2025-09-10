68c33a08a6089d2f94e91ed453fbe396e1267b28
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-10 14:57:28 +0900

    タスク 138: 更新 (153 -> 149)

--- 138/3_submission.py
+++ 138/3_submission.py
@@ -1,4 +1,4 @@
-def p(g,i=-1):
+def p(g,i=8):
  while 0in g[0]:g=g[1:]
- if i>2:g=[[(c,r[0])[r[0]in r[j:]]for j,c in enumerate(r)]for r in g]
- return p([*zip(*g)][::-1],i+1)if i<7 else g
+ if i<5:g=[[(c,r[0])[r[0]in r[j:]]for j,c in enumerate(r)]for r in g]
+ return i and p([*zip(*g)][::-1],i-1)or g
