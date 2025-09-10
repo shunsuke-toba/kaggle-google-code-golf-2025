e35cc8a3a1285cdd15312a80894a883aadcfe12b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-10 10:04:43 +0900

    タスク 325: 更新 (168 -> 167)

--- 325/3_submission.py
+++ 325/3_submission.py
@@ -1,6 +1,6 @@
 def p(g,n=0,e=enumerate):
  for y,r in e(g):
   for x,v in e(r):
-   n+=v//8
+   n+=v>0
    for R in g[y:y+v//2]:R[max(x-2,0):x+4]=[0]*6
  return[[0]*i+[8]+[0]*(n+~i)for i in range(n)]
