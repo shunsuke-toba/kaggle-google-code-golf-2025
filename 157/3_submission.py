c64a866adfe81f70a5bbe819496cfcb6db94f0df
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-10 14:57:29 +0900

    タスク 157: 更新 (406 -> 405)

--- 157/3_submission.py
+++ 157/3_submission.py
@@ -4,7 +4,7 @@ def p(g):
   y=n//15;x=n%15;t=g[y][x]//5*[(0,0)];g[y][x]%=5;s+=t,
   for a,b in t:
    for k in r(4):
-    if (u:=y+a+d[k])<10>-1<(v:=x+b+d[~k])<15>g[u][v]>4:g[u][v]=0;t+=(u-y,v-x),
+    if(u:=y+a+d[k])<10>-1<(v:=x+b+d[~k])<15>g[u][v]>4:g[u][v]=0;t+=(u-y,v-x),
  while s:
   _,t,i,j=min((~sum(i+a<3for a,b in t),t,i,j)for t in s for i in(1,2)for j in r(15)if all(j+b<15>g[i+a][j+b]<1for a,b in t));s.remove(t)
   for a,b in t:g[i+a][j+b]=1
