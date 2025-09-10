557f9030758faa13b1b6a8fdd069c56827d31b23
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-11 07:50:20 +0900

    タスク 379: 更新 (233 -> 229)

--- 379/3_submission.py
+++ 379/3_submission.py
@@ -1,8 +1,7 @@
 def p(g):
  for _ in g*4:
   for i,b in enumerate(g):
-   try:
-    if b[j:=len(b)-b[::-1].index(2)]<1:k=b.index(8,j);b[j-1:k+2]=[2]*(k-j)+[8,2,8];g[i-1][k-1:k+2]=g[i+1][k-1:k+2]=[8]*3
+   try:1/(b[j:=len(b)-b[::-1].index(2)]<1);k=b.index(8,j);b[j-1:k+2]=[2]*(k-j)+[8,2,8];g[i-1][k-1:k+2]=g[i+1][k-1:k+2]=[8]*3
    except:0
   g=[*map(list,zip(*g[::-1]))]
  return g
