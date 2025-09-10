33204e478253ef142b6dbcecd718d2e86f4ba7b7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-10 14:57:27 +0900

    タスク 054: 更新 (424 -> 423)

--- 054/3_submission.py
+++ 054/3_submission.py
@@ -6,7 +6,7 @@ def p(g,r=range):
  for i,j in[(i,j)for i in r(30)for j in r(30)if g[i][j]==q[12]]:
   for d in r(4):
    v=q[(22,10,2,14)[d]];x=i;y=j
-   while v!=b!=g[x][y]:g[x][y]=v;x+=D[d];y+=D[d-3]
+   while v!=b!=g[x][y]:g[x][y]=v;x+=D[d];y+=D[~d]
   for t in r(25):
    if(v:=q[t])!=b!=g[x:=i-2+t//5][y:=j-2+t%5]:g[x][y]=v
  return g
