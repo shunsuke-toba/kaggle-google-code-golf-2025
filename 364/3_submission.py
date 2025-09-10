569f5b3d0ae7850881dfb5063648a8320a4ba907
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-10 10:04:45 +0900

    タスク 364: 更新 (169 -> 162)

--- 364/3_submission.py
+++ 364/3_submission.py
@@ -1,3 +1,1 @@
-def p(g,i=96):
- while i:*g,=zip(*[[c.bit_count()*5%14%9*(i<2)or(c>0)*(u|c|(u>0<l)*4<<i%4)for u,c,l in zip(a,b,[0,*b])]for a,b in zip([30*[0]]+g,g)][::-1]);i-=1
- return g
+p=lambda g,i=96:i and p([*zip(*[[c.bit_count()*5%14%9*(i<2)or(c>0)*(u|c|(u>0<l)*4<<i%4)for u,c,l in zip(a,b,[0,*b])]for a,b in zip([30*[0]]+g,g)][::-1])],i-1)or g
