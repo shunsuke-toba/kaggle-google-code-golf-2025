13f3409a68dc02bc03bf8d39acf849cc9a11a280
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-11 07:50:19 +0900

    タスク 255: 更新 (340 -> 324)

--- 255/3_submission.py
+++ 255/3_submission.py
@@ -1,4 +1,4 @@
-def p(g,r=range(30),t=lambda x:[*map(list,zip(*x[::-1]))]):
+def p(g,r=range(30)):
  for _ in g*4:
   u=-1
   for d in[i for i in r if{*g[i][5:]}-{0,3,13}]+[30]:
@@ -6,5 +6,5 @@ def p(g,r=range(30),t=lambda x:[*map(list,zip(*x[::-1]))]):
    u=d
   for i in r:
    if all(b'\n'<bytes(b).strip(b'\0\3')for b in g[:i+2][-3:]):u=g[i].index(13);g[i][:u]=u*[3]
-  g=t(g)
+  g=[*map(list,zip(*g[::-1]))]
  return[[x%10for x in b]for b in g]
