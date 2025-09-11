be3c8c870deec753fb54270dc947192ffe708136
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-12 07:52:20 +0900

    タスク 096: 更新 (427 -> 423)

--- 096/3_submission.py
+++ 096/3_submission.py
@@ -1,12 +1,12 @@
 def p(g):
- import re;r=range;f=sum(g,[]);C=f.count;b=max(f,key=C);t=lambda m:[*map(list,zip(*m[::-1]))];L=[]
+ import re;r=range;f=sum(g,[]);C=f.count;b=max(f,key=C);L=[]
  for x in{*f}-{b}:
   s=str(x);c=1+2*(C(x)>1);y=c,x,c
-  for n in r(128):w=n//32+2;d=n%7+1;y=any(re.search(s*w+f"[^{s}]"*d+s,str(R)[1::3])for R in g)and(2*w+d,x,w)or y;g=t(g)
+  for n in r(128):w=n//32+2;d=n%7+1;y=any(re.search(s*w+f"[^{s}]"*d+s,str(R)[1::3])for R in g)and(2*w+d,x,w)or y;g=[*zip(*g[::-1])]
   L+=y,
  m=max(L)[0];o=[[b]*m for _ in r(m)]
  for z,x,w in L:
   for _ in r(4):
    for k in r(w):o[i:=m-z>>1][i+k]=o[i+k][i]=x
-   o=t(o)
+   o=[*map(list,zip(*o[::-1]))]
  return o
