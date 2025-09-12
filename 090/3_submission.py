e9aa3604d5db9aa779b549aa9cd796d2089327c4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-13 08:12:26 +0900

    タスク 090: 圧縮版を適用 (210 -> 207)

--- 090/3_submission.py
+++ 090/3_submission.py
@@ -1,4 +1,2 @@
-def p(g):
- r,h,w=range,len(g),len(g[0]);_,G,x,b=max((all(1>max(R[x:x+b])for R in g[y:y+a])*a*b,g[y:y+a],x,b)for y in r(h)for x in r(w)for a in r(2,h-y+1)for b in r(2,w-x+1))
- for R in G:R[x:x+b]=[6]*b
- return g
+#coding:L1
+import zlib;exec(zlib.decompress(bytes("xÚ=M%„PíœbÚ·u÷J'®ãî~zÊÈSEÕ À:¹pÍŒY~DOWgŽê5`¿^«¹þfü¬˜IwW,0¯í)¼ŠKÁ¤Ñü0=Ð_å¥dâ‡P‘J³ñk½¦l56º§èŸ¼{ÄþÙÌŒyÉÖ$P>/ˆp0Õð—±úþ:|¨ÄA¤&iÔô×ª”@",'L1')))
