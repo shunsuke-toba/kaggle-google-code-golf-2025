b41635d55418df9a29290428b2616b62ba9d50d4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-13 08:12:34 +0900

    タスク 255: 圧縮版を適用 (324 -> 296)

---
+++ 255/3_submission_uncompressed.py
@@ -0,0 +1,11 @@
+def p(g):
+ r=range(30)
+ for _ in g*4:
+  u=-1
+  for d in[i for i in r if{*g[i][5:]}-{0,3,13}]+[30]:
+   for b in g[u+2:d-1]*(d-u>6):b[5:]=[13]*25
+   u=d
+  for i in r:
+   if all(bytes(b).strip(b'\0\3')>b'\n'for b in g[:i+2][-3:]):u=g[i].index(13);g[i][:u]=u*[3]
+  g=[*map(list,zip(*g[::-1]))]
+ return[[x%10for x in b]for b in g]
