4aeaaa97b71da8eb1b442fdee04922d5e4431cd4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-14 15:08:05 +0900

    タスク 363: 圧縮版を適用 (226 -> 225)

--- 363/3_submission.py
+++ 363/3_submission.py
@@ -1,5 +1,2 @@
-def p(g):
- r=range(110);h=sum((x+[1]for x in g),[])+[1]*36;p=[i for i in r if h[i]&2];v=[k for k in r if{h[i+k-p[0]]for i in p}=={0}];v[1:3]*=v>v[5:]
- for k in v[v==[14,61]:]:
-  for i in p:j=i+k-p[0];g[j//11][j%11]=2
- return g
+#coding:L1
+import zlib;exec(zlib.decompress(bytes("xÚdCzÆPpŸSüˆíÅ{^d¾Øv®_»c#/ÊË,Va.+¬ÉX¢e™­a;Ql´‚rZ/Í¥/•¤b ½†dÇ§3`sùÊ½¨òRcğv@Ïÿ©¤ïÅ[¥Ñf4ƒàÁzË·¯ùYzé@‹8çã‰	˜ïîO\0´\\Õ·æò£‘tğ9‘VØ†eØq/ì—‡ŠıXŸt5\0fÓCË",'L1')))
