533adbd980c3b8e51daa24eb3a6f4140519bad79
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-14 13:21:51 +0900

    タスク 255: 圧縮版を適用 (294 -> 289)

--- 255/3_submission.py
+++ 255/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("xÚeŽÁj„0†ïû¹”M¢.Æt{Hq_dv(JT¬•hÀvÙwï$§BOó;f¾ï÷Ã(V9)wãW‚1éWþ±­´ö¼Ê‘Ò‹Ð-Ó m­h|è	áêðY=êÒ–Æ>±\0[cbä“>C!ó•A-}ooÊõéªcQ7×“HFÏã¿%“hÝ<Ëþ{6Ù«Ë¶Ze¾×w{V7ËùÍQÑ TÖ¡r±M/´øáÆª÷\\ÙEl£‹LŸZÐŸÝ*gÚöò‡¹š	ŽË*Å¿Ã°Ç°\0/¦NŠ#·ÃC¶á/|Þ_",'L1')))
+import zlib;exec(zlib.decompress(bytes("xÚeŽÁj„0@ï~Å\\ŠIÔÅ˜n)îd‡b•€µ¢Ú.ûïsØzÊd`Þ{Ô°ˆQÚ†Þ Ì0ªgþBl+AZ¯=FX»yì…©enjt„îlñ^ÝêÒ”ÚÜ±p¦Æƒ‘N|‚ºX4–*JP//ÒúãªuÚ jÎFâç¿%‘Â\0Ý4	ÿµ÷›ðò´íkX„Ï¯õÕäòÂÃœÿ±Y*t•±(mlÆS˜©ÿÚÈ×”l#ËÙ™>¶N½w‹˜Â¶—ßÌUL°+%f°ö{\\gçèI×:¿6ü^æ_$",'L1')))
