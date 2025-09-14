8b6001f8280f15bc9ae902586bc455328263f2c9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-15 08:50:38 +0900

    タスク 125: 更新 (176 -> 175)

--- 125/3_submission.py
+++ 125/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- for r in g:r[1:]=[4|c&2for c in r[1:]]
- for i in range(3**9):t=g[x:=i//135%15][y:=i//9%15];u=g[(x+i%9//3-1)%15][(y+i%3-1)%15];g[x][y]+=4*(u-t==4)-5*(t>u>5)
+ for r in g:r[1:]=[4|c&2for c in r[1:]];i=6**6
+ while i:i-=1;t=g[x:=i//135%15][y:=i//9%15];u=g[(x+i%9//3-1)%15][(y+i%3-1)%15];g[x][y]+=4*(u-t==4)-5*(t>u>5)
  return g
