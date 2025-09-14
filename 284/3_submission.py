77b18bd23ec55092bd64a2baa2e95c226ed5ab87
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-14 13:21:52 +0900

    タスク 284: 圧縮版を適用 (247 -> 244)

--- 284/3_submission.py
+++ 284/3_submission.py
@@ -1,3 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("""xÚ%ŽÁŠ„0†ï>EŽiMîÞ:t^¤äP1ÖÂŽ+ÕŠøô£É÷’Œ$Næò‘6Q¶Ô+ÂD9_aüÏ(CšA0Vìi0«4ÂÎ¯É‰éßï®‹ƒl–­äÙë3-¨ôúüKëFÕD¥øªîk­fì©S„B¦»sÔM}}0?6´¿lëÈ¾Xw´«žØ]¢½»ì‹YÙ·bðëé¶ØËú“õÔÀ
-âCšM1""",'L1')))
+import zlib;exec(zlib.decompress(bytes('xÚ-ŽAŽ„ E÷ž¢–€ÅÂ™ú"•Z`,ÑdÚ1 3§ÄNHà=’ÿÿ$3ì*¢xÙÊKR8D»TÀQ£šP´§óo‚€	Ö\rDÅ†#¾oLzáÍÅ‹ŸÏ¡ƒÊÁN.ÉQÒFæoÝ•Ù™WØÕÏšl&jÍõtp¥˜kšq¨Å‚v¸v´ŸV)Ø/úovíÉTœ?ûlöUô—`Ÿ¨ØÜÓÉûvÅUGÉféàñ~N°','L1')))
