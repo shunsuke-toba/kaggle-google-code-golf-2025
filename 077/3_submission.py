f7bbb6cd1bed85ca1e97c6d71bd61382109f408c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-15 08:50:37 +0900

    タスク 077: 更新 (176 -> 173)

--- 077/3_submission.py
+++ 077/3_submission.py
@@ -1,3 +1,3 @@
-def p(g,e=enumerate):
- for S in[(2,4)]*24:*g,=zip(*[[(v,4)[v^2and(r[j+1:]and r[j+1]in S or g[i-(i>0)][j]in S)&(r[j-(j>0)]in S)]for j,v in e(r)]for i,r in e(g)][::-1])
+def p(g):
+ for S in[(2,4)]*24:z=g[:1];*g,=zip(*[[ (c,4)[c^2and l in S and(n in S or u in S)]for c,l,n,u in zip(r,[0,*r],[*r[1:],0],a)]for a,r in zip(z+g,g)][::-1])
  return g
