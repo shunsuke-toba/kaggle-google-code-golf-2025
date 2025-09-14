aa5196db7ae9d4a6bbbded875c84cc66b027c5ac
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-15 08:50:36 +0900

    タスク 025: 更新 (191 -> 190)

--- 025/3_submission.py
+++ 025/3_submission.py
@@ -1,6 +1,7 @@
 def p(g):
- o=0
- for x,k in enumerate(t:=[*zip(*g)]):
+ o=0;x=0
+ for k in(t:=[*zip(*g)]):
   if{*k}<{0,c:=k[0]}:
    for r,q in zip(g,o:=o or[[0]*len(r)for r in g]):q[x]=q[x+(c in r[x+1:])-(c in r[:x])]=c
+  x+=1
  return o or[*zip(*p(t))]
