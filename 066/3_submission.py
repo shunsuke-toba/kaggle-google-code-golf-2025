89436bda91df88e1b5f36c206573bd3d3cba0261
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-14 13:21:46 +0900

    タスク 066: 圧縮版を適用 (289 -> 287)

--- 066/3_submission.py
+++ 066/3_submission.py
@@ -1,2 +1,4 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("""xÚ5jÃ †_Å^z®$6’šçH	U„ö62ZØÃO	@ÿó»ïGçûâz’WOå2pƒ¯è@*žÎgÂôL>7–X"¶ŠžSC‹ªZB¿´zòjnÖöÀ;fÙõ4ò¹—“Q´>õò~²p!fÈñNJ¾÷2ªY«ICiÉÍ>¾—'xµX³há÷ÈÂâ‘º‹œq:ê æ*{]–oŸSö·eá¾Á“laˆªkŒXu9¬pú½®–1–YÕ†ú{\\K¯†ÍçŸXü‘Y""",'L1')))
+import zlib;exec(zlib.decompress(bytes("""xÚ5‹jì †_Åœ2³;vI
+´$kž#A$h4"ìNKjÃîÛW	å¦óý7Vñ;V·ÀåÓƒ%‡yTÒåÂ”þ3®Ÿ›H"±Ø,Ç\0|bL«ˆºŒ.óÒ˜~U7{wÞŠ'=¨QVmÇLzž-²¹ÂØ©ÇÙá•…e­R°w*êÉèÑ`YÉ§}x¯Ò
+µÅ‘´”eS¥ƒHG©h—Å[”‰Æcë]û^çù;ä”Ã}ža¤7ü§ì£nOV.¦<Nzó¯Ñ‚©°ci!OK™öÒa¿…ü³±ˆ¿‰˜ZC""",'L1')))
