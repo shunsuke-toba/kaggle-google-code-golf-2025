961239494a992f365ea936a951a4f97fd66e49c6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-16 07:39:22 +0900

    タスク 046: 更新 (197 -> 194)

--- 046/3_submission.py
+++ 046/3_submission.py
@@ -1,6 +1,6 @@
 def p(g):
- r=[],[],[];o=e=3;s=[]
+ r=[],[],[];o,e=2,3;s=[]
  for t in*zip(*g),():
   if sum(t):s+=t
-  elif s:o+=min(s.index(5),3)-e;i=~o;[r[(i:=i+1)%3].append(v and sum({*s})-5)for v in s];e,*s=(*s[-3:],5).index(5),
+  elif s:o-=min(s.index(5),3)-e;[r[(o:=o+1)%3].append(v and sum({*s})-5)for v in s];e,*s=(*s[-3:],5).index(5),
  return r
