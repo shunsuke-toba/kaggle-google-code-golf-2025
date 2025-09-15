230eea0b43e613843341879da6c9b30607edadd7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-16 07:39:23 +0900

    タスク 071: 更新 (126 -> 123)

--- 071/3_submission.py
+++ 071/3_submission.py
@@ -1,6 +1,6 @@
 def p(g,a=0):
  for r in g:
   for i,v in enumerate(r):
-   if a<1:a=v;t=i-r[::-1].index(v)
-   elif 0<v!=a:r[i]=r[~i+t]
+   if a<1:t=i-r[::-1].index(a:=v)
+   if 0<v!=a:r[i]=r[~i+t]
  return g
