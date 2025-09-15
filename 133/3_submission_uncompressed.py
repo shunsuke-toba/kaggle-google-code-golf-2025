42b70f00167783901d65cea5bd121e42f02d1f83
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-15 14:10:51 +0900

    タスク 133: 圧縮版を適用 (345 -> 343)

--- 133/3_submission_uncompressed.py
+++ 133/3_submission_uncompressed.py
@@ -5,11 +5,11 @@ def p(g):
   for y,x in(y+1,x),(y,x+1),(y-1,x),(y,x-1):
    if w>x>-1<y<h>g[y][x]>0:a+=f(y,x)
   return a
- S=[f(y,x)for y in range(h)for x in range(w)if g[y][x]]
- B=min(S,key=lambda t:(l(t)<3,l(t)))
- r,c,p=min(B,key=lambda t:sum(v==t[2]for y,x,v in B))
- for t in S:
+ s=[f(y,x)for y in range(h)for x in range(w)if g[y][x]]
+ b=min(s,key=lambda t:(l(t)<3,l(t)))
+ r,c,p=min(b,key=lambda t:sum(v==t[2]for y,x,v in b))
+ for t in s:
   a=[(y,x)for y,x,v in t if v==p or(q:=v)>9];m=int(l(a)**.5)
-  for y,x,v in B:
+  for y,x,v in b:
    for i,j in a:g[(y-r)*m+i][(x-c)*m+j]=(q,p)[v==p]
  return g
