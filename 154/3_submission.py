3f89d2ae09a648a2a16a4a8a55d5859f9de5a157
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-15 16:21:31 +0900

    タスク 154: 更新 (132 -> 126)

--- 154/3_submission.py
+++ 154/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for _ in g*4:
   for r in(g:=[*map(list,zip(*g[::-1]))]):
-   if{0,5}<{*r}:j=r.index(2)-3;r[j:j+7]=r[j:j+7][::-1]
+   if{0,5}<{*r}:j=r[3]<1;r[j:j+7]=r[j:j+7][::-1]
  return g
