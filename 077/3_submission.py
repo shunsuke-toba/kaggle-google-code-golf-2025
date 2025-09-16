93e898bb14ad26a33551a4b937cda7e16859776a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-16 12:34:33 +0900

    タスク 077: 更新 (158 -> 157)

--- 077/3_submission.py
+++ 077/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for _ in' '*24:*g,=zip(*[[(c,4)[c^2and(20>>l&(20>>n|20>>u))&1]for c,l,n,u in zip(r,[0,*r],[*r[1:],0],a)]for a,r in zip(g[:1]+g,g)][::-1])
+ for _ in' '*24:*g,=zip(*[[(c,4)[c^2and+20>>l&(20>>n|20>>u)&1]for c,l,n,u in zip(r,[0,*r],[*r[1:],0],a)]for a,r in zip(g[:1]+g,g)][::-1])
  return g
