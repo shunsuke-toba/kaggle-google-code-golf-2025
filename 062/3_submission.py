44b7f30962bf20e591fec47f6e0a31c1edbd8adc
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-17 01:27:09 +0900

    タスク 062: 更新 (193 -> 190)

--- 062/3_submission.py
+++ 062/3_submission.py
@@ -1,2 +1,1 @@
-def p(g):
- s=10;R=range(s);f=sum(g,[]);k=sum({*f})-2;i=f.index(2);a=(i//s,i%s)[b:=k in g[i//s]]+(k>f[i-s+9*b]);return[[(3,k)[g[r][c]|g[(2*a+~r,r)[b]%s][(c,2*a+~c)[b]%s]>0]for c in R]for r in R]
+def p(g):r=range(s:=10);f=sum(g,[]);k=sum({*f})-2;i=f.index(2);a=(i//s,i%s)[b:=k in g[i//s]]+(k>f[i-s+9*b]);return[[(3,k)[g[y][x]|g[(2*a+~y,y)[b]%s][(x,2*a+~x)[b]%s]>0]for x in r]for y in r]
