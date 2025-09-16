97b5601353d13d5007592299cb131af90fd25a0b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-16 12:34:36 +0900

    タスク 216: 更新 (156 -> 155)

--- 216/3_submission.py
+++ 216/3_submission.py
@@ -1,6 +1,6 @@
 def p(g,b=0):
  for n in range(400):
   a=[]
-  while(s:=(g+[[]])[n//20][n%20:]+[0])[0]:a+=s[:s.index(0)],;n+=20
+  while(s:=[*g,[]][n//20][n%20:]+[0])[0]:a+=s[:s.index(0)],;n+=20
   if(r:=str(a).count('2'))>b:o=a;b=r
  return o
