e120e0b8c7271971539c24165a4bf9612dec2bc3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-16 12:34:27 +0900

    タスク 008: 更新 (132 -> 131)

--- 008/3_submission.py
+++ 008/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  k=i=f=0
  while k<4:
-  m=max(g[i]);i+=1;f+=m>7
-  if f>m:g+=g.pop(i:=i-1),
+  if f>(m:=max(g[i])):g+=g.pop(i),
+  i+=f<=m;f+=m>7
   if m&3:g=[*zip(*g[::-1])];k+=1;i=f=0
  return g
