2671b6ae370c24f220fd628f959712273a6c2c95
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-16 22:40:37 +0900

    タスク 268: 更新 (263 -> 262)

--- 268/3_submission.py
+++ 268/3_submission.py
@@ -2,7 +2,7 @@ def p(g):
  n=len(g);(a,c),*_,(b,d)=[(i//n,i%n)for i in range(n*n)if g[i//n][i%n]]
  if g[a][c+2]:return[*zip(*p([*zip(*g)][::-1])[::-1])]
  while b:
-  b-=1;h=g[b]=[*g[b]];t=b-a;s=t<1;h[c-~s:d-s]=[4]*(d+~c-2*s)
-  for k in c+t+2,d-2-t:
+  b-=1;h=g[b]=[*g[b]];s=b<=a;h[c-~s:d-s]=[4]*(d+~c-2*s)
+  for k in c+b-a+2,d-2-b+a:
    if s>0<=k<n:h[k]=4
  return g
