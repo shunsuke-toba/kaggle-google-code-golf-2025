f9a51e9e34b7fde5a14dbc4564b9cf56fe0404fc
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-17 18:11:09 +0900

    タスク 002: 更新 (109 -> 108)

--- 002/3_submission.py
+++ 002/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:eval('[*zip(*[[c&(-v|3)for c,v in zip(r,(0,*r))]for r in'*64+str(g).replace(*'04')+'[::-1]])]'*64)
+p=lambda g:eval('[*zip(*[map(lambda c,v:c&(-v|3),r,(0,*r))for r in'*64+str(g).replace(*'04')+'[::-1]])]'*64)
