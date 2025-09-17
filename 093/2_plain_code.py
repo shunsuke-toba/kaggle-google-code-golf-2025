6243eac2728b38828f761e276d0c982e980c927a
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-09-17 20:06:02 +0900

    093:改善(162 -> 129)

--- 093/2_plain_code.py
+++ 093/2_plain_code.py
@@ -1,5 +1,45 @@
-f=lambda x:[*map(list,zip(*x))]
-def p(g):
- if 5not in g[0]:return f(p(f(g)))
- for i in range(14):a1=g[i][:7].count(0);a2=g[i][7:].count(0);g[i]=[0]*a1+[5]*(14-a1-a2)+[0]*a2
- return g
+# -*- coding: utf-8 -*-
+
+def p(grid):
+    """
+    解くための手続き:
+    - 以下の一連の操作を4回行う
+    - 14行見て、各行の0~6列までの0でないマスの個数をxとする
+    - 0~6列までを全部0にしたあと6列目から左x列分を5にする
+    - 90度回転
+    """
+
+    # 90度回転関数
+    def rotate_90(g):
+        return [list(row) for row in zip(*g[::-1])]
+
+    result = [row[:] for row in grid]
+
+    # 4回繰り返す
+    for _ in range(4):
+        # 90度回転
+        result = rotate_90(result)
+
+        # グレー矩形がない方向ならこのループの処理をスキップ
+        if result[0][6] != 5 and result[0][7] != 5:
+            continue
+
+        # 各行について処理
+        for r in range(14):
+            # 0~6列目の0でないマスの個数をカウント
+            x = 0
+            for c in range(7):
+                if result[r][c] != 0:
+                    x += 1
+
+            # 0~6列目を全て0にする
+            for c in range(7):
+                result[r][c] = 0
+
+            # 6列目から左にx列分を5にする
+            for i in range(x):
+                col = 6 - i
+                if col >= 0:
+                    result[r][col] = 5
+
+    return result
--- 093/3_submission.py
+++ 093/3_submission.py
@@ -1,1 +1,4 @@
-p=lambda g:5in g[0]and[*zip(*p([*map(list,zip(*g))]))]or exec('for z in range(9**5):u=g[x:=z//14%12+1][z:=z%14]>0;g[x][z]=g[i:=x+x//7*2-1][z]|u*5;g[i][z]*=u')or g
+def p(g):
+ for _ in g*2:
+  for r in(5in(g:=[*map(list,zip(*g[::-1]))])[0])*g:r[7:]=([5]*sum(map(bool,r[7:]))+[0]*7)[:7]
+ return g
