844aa30ab2777f0413e0cbbd3927ec8038d7c49d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 07:06:20 +0900

    タスク 102: 圧縮版を適用 (192 -> 190)

--- 102/3_submission.py
+++ 102/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("xÚeM3Ä@\0ìóŠéb–ÁGbÛÙ­_?ûÖ;Ì‹3Wñ&ƒrZÑ ±&cUpº~ohû‡Þàîu*¯1±à4%Æ‰p†&ÈÇNÞèÀ]÷êµf{ñ×¤õšDùý-ê7þ)ðtS¾\0*üæ§ÍñŒ@è¬¡ëˆêhC!",'L1')))
+import zlib;exec(zlib.decompress(bytes("xÚeM3Ä@\0ìóŠéb–ÁGbÛÙ­_?ûÖ;Ì‹3Wñ&ƒrZÑ ±&cUpº~ohû‡Þàîu*¯1±à4%Æ‰pn{õZ³½±ü5b½FP^64A>vû•¨ßtú§ÎÓMù<ªðK˜Ÿ6Ç3¡c°„®#ª3v?C!",'L1')))
