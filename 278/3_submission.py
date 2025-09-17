e1c81c10932352fb3a0c3c7f5bdb6f4bb64d5fb5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 07:06:07 +0900

    タスク 278: 更新 (146 -> 144)

--- 278/3_submission.py
+++ 278/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for _ in g*8:*g,=zip(*[[x^(y&3==2==x&3)*8^(x<1<8<y+z)*3for x,y,z in zip(r,[0,*r],[0,*a])]for a,r in zip([[0]*20]+g,g)][::-1])
+ for _ in g*8:*g,=zip(*[[x^(x*y&6==4)*8^(y+z-1>>3>x)*3for x,y,z in zip(r,[0,*r],[0,*a])]for a,r in zip([[0]*20]+g,g)][::-1])
  return g
