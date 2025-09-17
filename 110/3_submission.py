dbaaff89c1dca382702615e4b24af2ebac0d152a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-17 18:11:11 +0900

    タスク 110: 更新 (100 -> 89)

--- 110/3_submission.py
+++ 110/3_submission.py
@@ -1,3 +1,1 @@
-def p(g):
- for r in g:r[:]=map(max,*[t for t in g if 1>any(a*b*(a-b)for a,b in zip(r,t))])
- return g
+p=lambda g:[[*map(max,*(t for t in g if 1>any(a*b*(a-b)for a,b in zip(r,t))))]for r in g]
