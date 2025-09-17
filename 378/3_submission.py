0e5d1f0131f410a61815848c008e93ca122acc26
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-17 18:11:20 +0900

    タスク 378: 更新 (207 -> 206)

--- 378/3_submission.py
+++ 378/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for z in range((m:=~-len(g))*m):
-  if 0<(z:=(t:=g[y:=z//m])[x:=z%m])>g[a:=y+(d:=(g[y+1][x]-g[y-1][x])//z)][b:=x+(e:=(t[x+1]-t[x-1])//z)]:
+  if (t:=g[y:=z//m])[x:=z%m]>g[a:=y+(d:=(g[y+1][x]>0)-(g[y-1][x]>0))][b:=x+(e:=(t[x+1]>0)-(t[x-1]>0))]:
    while m>y>0<x<m:g[y:=y-d][x:=x-e]=g[a+d][b+e]
  return g
