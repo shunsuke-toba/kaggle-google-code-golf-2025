65bab9adcb88656de11a8895722652d1ef65fa19
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 07:06:06 +0900

    タスク 119: 更新 (172 -> 171)

--- 119/3_submission.py
+++ 119/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for _ in g:
   g=[*map(list,zip(*g[::-1]))];f=8in g[-1];i=0
-  for h in(g[6][0]==2)*g[::1-2*f]:k=h.count(2);k+=abs(g[-f].index(8)-i-k);h[k]=h[k]or 3;i+=1
+  for h in(g[6][0]==2)*g[::-f|1]:k=h.count(2);k+=abs(g[-f].index(8)-i-k);h[k]=h[k]or 3;i+=1
  return g
