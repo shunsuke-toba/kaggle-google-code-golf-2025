d35b67a0c404758c45cea9a5523b8877660398c7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 07:06:19 +0900

    タスク 076: 圧縮版を適用 (356 -> 344)

--- 076/3_submission.py
+++ 076/3_submission.py
@@ -1,2 +1,6 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("""xÚuPÍnƒ0¾÷)ršâª±^¦´áE¢BIQ¤B+CJØÓÏ	­Ð;Yúl­»²ï@îØ¬nnàþ4p•ný³¿·|=ï„6pðCë"?‚˜ÁìØõŽl‘ùr,”æV4P›0´CçøRVb)¾2Ülp$8ì¯,KBmë²:7ç¹î´5º15Ït‰ÎÉ¡¸¨ÿàûN“Ò¨{?ðÈÞFEe’&‰.¯p]aZ½ÜúÍÀwÊ»FØÀÕJ™ÆDoù$ßÄíf^×©£÷£övãá£bw$ù¥ ±hŒR!ÿ[‘¹!ôíäˆ>"Â_áÅøß—|“ËX$F!}P!û\\Î¤¥,+æäË#qLtO‡£ã@º) õóí”™b""",'L1')))
+import zlib;exec(zlib.decompress(bytes("""xÚuAnÃ E÷9«
+®JÚ1¾BŽm„d;vjÜÓw€$–,u…xfþÌoÚÝ°%â€Ù·#¶êC“ó$Uã~†kƒ§û€-Sš¼»±iþ$l!ú€º«G+Èh•
+V“HMdÞŒ¶ÅkÁÙJO	×€`×¡dI*S¼¬Ë¥²ÊhUë
+§qq8)9Øyv‘¿î†9ÏRy5¸_ˆL€‡r-|´°IòYòQzlû½-pŠ÷îáW‚ù°æ‹øÌ0,×¤¢°-Y‡è¶×™¾š.zãz¬Z)|­µ”íBËVûÌ^ä9ÿŸ>ñô¾št2‹}Þ19+!
+Ò!ÂôKA·óÝCŠ¡å""",'L1')))
