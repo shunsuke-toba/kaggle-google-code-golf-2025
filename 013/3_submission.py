fd2bd262fe0af982a39f32fb5f9e9d8c1ee9636f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-17 19:03:56 +0900

    タスク 013: 更新 (171 -> 170)

--- 013/3_submission.py
+++ 013/3_submission.py
@@ -1,3 +1,4 @@
 def p(g):
- if g[12:]:return*zip(*p([*zip(*g)])),
- (y,Y),c=zip(*sorted((r.index(v),v)for r in g for v in r if v));*s,=g[0];s[y::Y-y]=(c*8)[:len(s[y::Y-y])];return[s]*len(g)
+ *z,=zip(*g)
+ if g[12:]:return*zip(*p(z)),
+ (y,Y),c=zip(*((i,v)for i,v in enumerate(map(sum,z))if v));*s,=g[0];s[y::Y-y]=(c*8)[:len(s[y::Y-y])];return[s]*len(g)
