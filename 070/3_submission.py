011f63e5249a6ccabbb521db49877bc668a24e80
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 07:06:05 +0900

    タスク 070: 更新 (122 -> 115)

--- 070/3_submission.py
+++ 070/3_submission.py
@@ -1,4 +1,5 @@
 def p(g):
- c=[8in k for k in zip(*g)];a=c.index(1);b=a+sum(c)
- for r in g:r[a:b]=[x|2&-x*(8in r)for x in r[a:b]]
+ a=b=0
+ for k in zip(*g):b+=8in k;a+=b<1
+ for r in g:r[a:a+b]=[x|2&-x*(8in r)for x in r[a:a+b]]
  return g
