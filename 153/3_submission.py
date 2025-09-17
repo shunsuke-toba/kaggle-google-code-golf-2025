8ab41e1ab6fec3153b8e527f3ead35467b4f90cf
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 07:06:20 +0900

    タスク 153: 圧縮版を適用 (173 -> 172)

--- 153/3_submission.py
+++ 153/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("xÚ}LÃA\0ü§ŠùÅvJ9ÛëÝúcë5ž$ÍÀ;yw×BÆ$B”2¤yÚÎ›“}ö£/þ9ˆ¿—$ùš Ì6M'·Ã~éÚQ¿r½ÜŽO<9òÓ°|çÝ“Q=·ÈTImûÏÓËðóÛ=\0¸mJñ",'L1')))
+import zlib;exec(zlib.decompress(bytes("xÚ}LÃA\0ü§ŠùÅvJ9ÛëÝúcë5ž$ÍÀ;yw×BÆ$B”2¤yÚÎ›“}ö£/þ9ˆ¿ç$ùš Ì@JÚÉí°_ºvÔ¯\\/·ãOŽü4,Ãy÷dTÏÆí2ÕFRÛþóô2üüv¾OJü",'L1')))
--- 153/3_submission_uncompressed.py
+++ 153/3_submission_uncompressed.py
@@ -3,5 +3,5 @@ def p(g):
   for b in range(-2,8):
    for c in range(-2,8):
     for d in range(-2,8):
-     if all(g[a+i][b+j]^g[c+i][d+j]for i in range(3)for j in range(3)):
+     if min(g[a+i][b+j]^g[c+i][d+j]for i in range(3)for j in range(3)):
       return[[g[a+i][b+j]^g[c+i][d+j]for j in range(3)]for i in range(3)]
