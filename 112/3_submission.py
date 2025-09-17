aeba6e9e27942b9b43ac4046a399549a7942540a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-17 21:38:03 +0900

    タスク 112: 更新 (135 -> 134)

--- 112/3_submission.py
+++ 112/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  w=len(g[0]);s=sum(g,[]);i=j=s.index(3)
  while i:
-  if s[i:=i-1]:t=g[(k:=2*j-i-~w)//w];t[k%w]=g[i//w][k%w]=t[i%w]=2
+  if s[i:=i-1]:k=2*j-i-~w;t=g[k//w];t[k%w]=g[i//w][k%w]=t[i%w]=2
  return g
