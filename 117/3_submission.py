1e97e08180633a128aa0d5678d5cdf773162eb9e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 07:06:05 +0900

    タスク 117: 更新 (180 -> 179)

--- 117/3_submission.py
+++ 117/3_submission.py
@@ -3,5 +3,5 @@ def p(g):
  while(f[k+~w]==f[k-w+1]==f[k+w-1]==f[k-~w]>0)<1:k-=1
  for v in f:
   b+=1;m=2*k-b
-  if v:g[y:=m//w+w][b%w]=g[y][m%w]=g[b//w][m%w]=v
+  if v:(r:=g[m//w+w])[b%w]=r[m%w]=g[b//w][m%w]=v
  return g
