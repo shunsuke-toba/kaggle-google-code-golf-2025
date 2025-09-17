142e2b6cebc746eb72d9179f9f1adcb7e6e823d7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-17 18:11:16 +0900

    タスク 228: 更新 (149 -> 148)

--- 228/3_submission.py
+++ 228/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- g=sum(g,[]);i=0
- for a in (z:=[g.index(i)for i in g if g.count(i)&1]):g[a],g[z[~i]+22-4*i-i//2*32]=0,g[a];i+=1
+ g=sum(g,[]);i=4
+ for a in (z:=[g.index(i)for i in g if g.count(i)&1]):g[a],g[z[i:=i-1]-22+4*i+i//2*32]=0,g[a]
  return*zip(*[iter(g)]*10),
