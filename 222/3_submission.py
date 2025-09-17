089a8f21990c4b08d2a04991daeb2e4e114c3eeb
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-17 19:04:00 +0900

    タスク 222: 更新 (183 -> 181)

--- 222/3_submission.py
+++ 222/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,r=range(16):max((all({*i[b:d]}=={v}for i in g[a:c])*(c-a)*(d-b),[[v*(a<=i<c)*(b<=j<d)for j in r]for i in r])for a in r for b in r if(v:=g[a][b])for c in r[a:]for d in r)[1]
+p=lambda g,r=range(16):max((sum({*i[b:d]}=={v}for i in g[a:c])*(d-b),a-c,[[v*(a<=i<c)*(b<=j<d)for j in r]for i in r])for a in r for b in r if(v:=g[a][b])for c in r[a:]for d in r)[2]
