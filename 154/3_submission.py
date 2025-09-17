20cb24fd3f6d5bda8c0c173a74f83bab87aae34c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-17 21:38:05 +0900

    タスク 154: 更新 (125 -> 114)

--- 154/3_submission.py
+++ 154/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- for _ in g*4:
-  g=[{0,5}<{*r}and[*r[:(j:=r[3]<1)],*r[j:j+7][::-1],*r[j+7:]]or[*r]for r in zip(*g[::-1])]
- return g
+p=lambda g:[g:=[{0,5}<{*r}and r[:(j:=r[3]<1)]+r[j:j+7][::-1]+r[j+7:]or r for r in zip(*g[::-1])] for _ in g*4][-1]
