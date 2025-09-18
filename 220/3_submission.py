28ec7349891e7506c98aa53e9e59ce70a423fdc7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 19:28:08 +0900

    タスク 220: 更新 (113 -> 112)

--- 220/3_submission.py
+++ 220/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,e=enumerate:[[5*(sum(sum(r[j-(j>0):j+2])for r in g[i-(i>0):i+2])+c)%9 for j,c in e(r)]for i,r in e(g)]
+p=lambda g,e=enumerate:[[5*(sum(sum(r[j-(j>0):j+2])for r in g[i-(i>0):i+2])+c)%9for j,c in e(r)]for i,r in e(g)]
