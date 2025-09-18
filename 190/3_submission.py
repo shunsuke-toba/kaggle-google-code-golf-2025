3c1d7f17348bde25e331bb85166d6ed28dc8b8a2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 19:28:06 +0900

    タスク 190: 更新 (127 -> 126)

--- 190/3_submission.py
+++ 190/3_submission.py
@@ -1,3 +1,3 @@
 def p(g,b=1280):
- while b:b-=1;i=b%8;j=b//8%8;a=b>>6&2;c=b>>7&2;v=g[i+a];g[i+2-a][j+2-c]|=(v[j+c]>v[j+1])*g[i+1][j+1]
+ while b:b-=1;i=b%8;j=b//8%8;a=b>>6&2;c=b>>7&2;v=g[i+a];g[i+2-a][j+2-c]|=~v[j+1]&v[j+c]&g[i+1][j+1]
  return g
