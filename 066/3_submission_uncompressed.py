76ee3a274af6b6b126b2c044631ebc6c5cdbc873
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-19 02:18:12 +0900

    タスク 066: 圧縮版を適用 (281 -> 272)

--- 066/3_submission_uncompressed.py
+++ 066/3_submission_uncompressed.py
@@ -2,9 +2,8 @@ def p(g):
  n=len(g);(a,b),(c,d)=[(y,x)for y in range(n)for x in range(n)if g[y][x]&1]
  def f(y,x,a,b,t=2):
   if n>(Y:=y+a)>-1<(X:=x+b)<n:
-   v=g[Y][X]
-   if v==2:return 1
-   if t*v>7:return f(y,x,b,-a,t-1)or f(y,x,-b,a,t-1)
-   if v-8 and f(Y,X,a,b,t):g[Y][X]=3;return 1
+   if g[Y][X]==2:return g[Y][X]
+   if t*g[Y][X]>7:return f(y,x,b,-a,t-1)or f(y,x,-b,a,t-1)
+   if g[Y][X]-8 and f(Y,X,a,b,t):g[Y][X]=3;return g[Y][X]
  g[2*a-c][2*b-d]-8 and f(a,b,a-c,b-d)or f(c,d,c-a,d-b)
  return g
