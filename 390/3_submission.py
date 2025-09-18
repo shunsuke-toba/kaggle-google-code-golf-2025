07d862dc599876440c32aae5d96848f825859e3f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 19:28:11 +0900

    タスク 390: 更新 (118 -> 116)

--- 390/3_submission.py
+++ 390/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for _ in g*4: g=[(t[:(j:=t[3]<2)]+t[j:][6::-1]+t[j+7:])if{0,5}<{*t}else t for t in zip(*g[::-1])]
+ for _ in g*4: g=[{0,5}<{*t}and t[:(j:=t[3]<2)]+t[j:][6::-1]+t[j+7:]or t for t in zip(*g[::-1])]
  return g
