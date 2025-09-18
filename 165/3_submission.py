5c23aefb586ab43ed3c4ac6222473ab363cb6458
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-19 06:31:11 +0900

    タスク 165: 更新 (177 -> 173)

--- 165/3_submission.py
+++ 165/3_submission.py
@@ -1,3 +1,1 @@
-def p(g):
- k=max(r[i-1]&r[i]&r[i+1]&p[i]for r,p in zip(g[1:],g)for i in range(19))
- return[*zip(*[(y:=bytes(c).rfind(k)+1)and c[:y]+(max(c[y:]),)*(20-y) or c for c in zip(*g)])]
+p=lambda g:(k:=max(r[i-1]&r[i]&r[i+1]&p[i]for r,p in zip(g[1:],g)for i in range(19)))and[*zip(*[(c,c[:(y:=bytes(c).rfind(k)+1)]+(max(c[y:]),)*(20-y))[y>0]for c in zip(*g)])]
