d8f46a2f04d65874dca1792ba256c2e81a6956ab
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 13:26:28 +0900

    タスク 379: 更新 (208 -> 205)

--- 379/3_submission.py
+++ 379/3_submission.py
@@ -1,6 +1,5 @@
 def p(g):
  for _ in g*4:
-  for a,b,c in zip(g,g[1:],g[2:]):
+  for a,b,c in zip(g:=[*map(list,zip(*g))][::-1],g[1:],g[2:]):
    if(k:=(s:=bytes(b)).find(8,j:=s.rfind(2)))>j+1>0:b[j:k+2]=[2]*(k+~j)+[8,2,8];a[k-1:k+2]=c[k-1:k+2]=[8]*3
-  g=[*map(list,zip(*g))][::-1]
  return g
