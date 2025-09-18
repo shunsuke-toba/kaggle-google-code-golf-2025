4a6cb02dd9a85531c31b27e4d4fcfb11c67b5a4c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-18 13:26:25 +0900

    タスク 093: 更新 (129 -> 126)

--- 093/3_submission.py
+++ 093/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  for _ in g*2:
-  for r in(5in(g:=[*map(list,zip(*g[::-1]))])[0])*g:r[7:]=([5]*sum(map(bool,r[7:]))+[0]*7)[:7]
+  for r in(5in(g:=[*map(list,zip(*g[::-1]))])[0])*g:r[7:]=[5]*(7-(z:=r[7:].count(0)))+[0]*z
  return g
