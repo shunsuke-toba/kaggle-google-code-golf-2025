5f9482c9766ae5638cd4322e55190fd82d040ab0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-19 02:18:16 +0900

    タスク 319: 圧縮版を適用 (319 -> 315)

--- 319/3_submission_uncompressed.py
+++ 319/3_submission_uncompressed.py
@@ -13,4 +13,4 @@ def p(g):
  for c in C:
   for d in C:
    h=f(d);n=len(h[0]);q=f(c)
-   if(c-d)*any(all(q[i+k//n>>1][j+k%n>>1]^h[k//n][k%n]in[0,c^d]for k in range(len(h)*n))for i in range(len(q)*2-len(h)+1)for j in range(len(q[0])*2-n+1)):return q
+   if(c-d)*any(all(q[i+y>>1][j+x>>1]^h[y][x]in[0,c^d]for y in range(len(h))for x in range(n))for i in range(len(q)*2-len(h)+1)for j in range(len(q[0])*2-n+1)):return q
