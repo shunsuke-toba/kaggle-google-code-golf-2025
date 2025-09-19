2b30b80bec1240b9dc8e4836c0d6ba7c33bc4c03
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-19 11:00:40 +0900

    タスク 077: 更新 (154 -> 153)

--- 077/3_submission.py
+++ 077/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[g:=[*zip(*[[(c,4)[c-2and 20>>l&(20>>n|20>>u)&1]for c,l,n,u in zip(r,[0,*r],[*r[1:],0],a)]for a,r in zip(g[:1]+g,g)][::-1])]for _ in' '*24][-1]
+p=lambda g,n=24:n and p([*zip(*[[(c,4)[c-2and 20>>l&(20>>n|20>>u)&1]for c,l,n,u in zip(r,[0,*r],[*r[1:],0],a)]for a,r in zip(g[:1]+g,g)][::-1])],n-1)or g
