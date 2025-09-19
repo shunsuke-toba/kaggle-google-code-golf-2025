7db18d6675a81d6a9e13eb24c03b03e2d26a6689
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-19 14:06:56 +0900

    タスク 196: 更新 (124 -> 123)

--- 196/3_submission.py
+++ 196/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,s="[*zip(*[[c%s for c,v in zip(r,(2,*r))]for r in ":eval(s%"%2*c"+s%"|v-1&2"*8+s%" or v&2"*47+"g"+"[::-1]])]"*56)
+p=lambda g,s="[*zip(*((c%s for c,v in zip(r,(2,*r)))for r in ":eval(s%"%2*c"+s%"|v-1&2"*8+s%"|v&2-c"*47+"g"+"[::-1]))]"*56)
