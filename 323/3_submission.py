d6c2487106ca94b80d0db5a2e7d1b3aff3571473
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-20 01:50:05 +0900

    タスク 323: 更新 (135 -> 131)

--- 323/3_submission.py
+++ 323/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for s in-1,1:
-  x,y=divmod(sum(g,[]).index(8),13);u=d=1
-  while(x:=x-s*d)<13>(y:=y+s-s*d)>-1<x:g[x][y]=5;u^=1;d^=u
+  x,y=divmod(sum(g,[]).index(8),13);u=d=s
+  while(x:=x-d)<13>(y:=y+s-d)>-1<x:g[x][y]=5;u^=s;d^=u
  return g
