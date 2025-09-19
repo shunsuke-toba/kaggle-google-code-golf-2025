ee59c02a96e245ad0a833769b8d44ed825310d7b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-20 01:49:57 +0900

    タスク 037: 更新 (128 -> 123)

--- 037/3_submission.py
+++ 037/3_submission.py
@@ -1,6 +1,4 @@
 def p(g):
- f=sum(g,[]);d=f.index
- for v in{*f}-{0}:
-  i=d(v);j=d(v,i+1)
-  while j>i:j-=9+2*(j%9!=i%9);g[j//10][j%10]=v
+ for j,v in enumerate(f:=sum(g,[])):
+  while v and j>(i:=f.index(v)):j-=9+2*(j%9!=i%9);g[j//10][j%10]=v
  return g
