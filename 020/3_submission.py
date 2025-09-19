7e726818cb3eb8698082e0f1250ed3281fd509f8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-20 01:49:56 +0900

    タスク 020: 更新 (131 -> 126)

--- 020/3_submission.py
+++ 020/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- m,n=map(lambda h:[*map(any,h)].index(1),(g,zip(*g)));k=75
+ m,n=[[*map(any,h)].index(1)for h in(g,zip(*g))];k=75
  while k:k-=1;i=k//5%5;g[m+i][n+k%5]|=g[m+~k%5][n+i]
  return g
