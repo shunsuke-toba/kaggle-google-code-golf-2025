4f53b0eb759f531dc7a2045dad9e8f4c4b3aba5c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-20 01:50:08 +0900

    タスク 390: 更新 (113 -> 110)

--- 390/3_submission.py
+++ 390/3_submission.py
@@ -1,3 +1,1 @@
-def p(g):
- for _ in g*4:g=[[t,t[:(j:=t[3]<2)]+t[j:][6::-1]+t[j+7:]][{0,5}<{*t}] for t in zip(*g[::-1])]
- return g
+p=lambda g,i=4:p([[t,t[:(j:=t[3]<2)]+t[j:][6::-1]+t[j+7:]][{0,5}<{*t}] for t in zip(*g[::-1])],i-1)if i else g
