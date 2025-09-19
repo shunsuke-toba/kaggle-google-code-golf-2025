00de41ff20d19d2e6165ebf611af1cfdb136e1b7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-20 02:10:58 +0900

    タスク 102: 圧縮版を適用 (189 -> 188)

--- 102/3_submission.py
+++ 102/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("xÚeM3Ä@\0ìóŠéb–ÁGbÛÙ­_?ûÖ;Ì‹3Wñ&ƒrZÑ ±&cUpº~ohû‡Þàîu*¯1±à4%Æ‰pn{õZ³½±ü5b½FP^<v‚¡í?D¢~“éŸ2O7åðhÂ/a~ÚÏ„ŽÁZºŽ¨Î?FBò",'L1')))
+import zlib;exec(zlib.decompress(bytes('xÚmMµÂ@\0ì3Åuqí°Ep‡$ïR!«ãNÞÿt¹ZCx¿í`ÍJ”jÆ6+/Ï¯à\r­þÐ\\ÿÃ(×`ÜxÚÒÛVƒª]…uXŒü«C]›AÙ.ïXx¬ƒ"Û¨Ãü¦Ï¿õ´¼£4²íÏ€Þ µµ2V]tg>ØDG','L1')))
--- 102/3_submission_uncompressed.py
+++ 102/3_submission_uncompressed.py
@@ -2,7 +2,6 @@ def p(g):
  for i in range(11):
   for j in range(11):
    for k in range(11):
-    u=g[i:i+k+2]
-    if not(sum(sum(r[j:j+k+2])for r in u)+~k*20|sum(sum(r[j+1:j+k+1])for r in u[1:-1])):
-     for r in u[1:-1]:r[j+1:j+k+1]=[2]*k
+    if not(sum(sum(r[j:j+k+2])for r in g[i:i+k+2])+~k*20|sum(sum(r[j+1:j+k+1])for r in g[i+1:i+k+1])):
+     for r in g[i+1:i+k+1]:r[j+1:j+k+1]=[2]*k
  return g
