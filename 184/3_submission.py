f9602433f3a42275992461229e564e84274bf6d9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-20 20:12:05 +0900

    タスク 184: 更新 (105 -> 104)

--- 184/3_submission.py
+++ 184/3_submission.py
@@ -1,2 +1,2 @@
-import itertools as I
-p=lambda g,f=lambda g:[*zip(*(map(max,*v)for k,v in I.groupby(g,any)if k))]:f(f(g))
+from itertools import*
+p=lambda g,f=lambda g:[*zip(*(map(max,*v)for k,v in groupby(g,any)if k))]:f(f(g))
