d8af8e03b6c2903c06ce2dceccd8fb067620d75e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-20 20:12:01 +0900

    タスク 094: 更新 (134 -> 130)

--- 094/3_submission.py
+++ 094/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- for _ in"00":
-  for r in(L:=[i for i in range(15)if 1in g[i]])[2],L[-3]:g[r]=[c&1or 6for c in g[r]]
-  *g,=zip(*g)
+ for _ in'00':
+  for r in(L:=[R for R in g if 1in R])[2],L[-3]:r[:]=map({8:6}.get,r,r)
+  *g,=map(list,zip(*g))
  return g
