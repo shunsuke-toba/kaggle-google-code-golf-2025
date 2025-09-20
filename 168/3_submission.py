101bd151d9bf176bd9094c6d7a8e5e3f75307ce9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-20 20:12:04 +0900

    タスク 168: 更新 (189 -> 188)

--- 168/3_submission.py
+++ 168/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
  for r in range(81):
-  c=r%9;r//=9
-  if s:=sorted(v:=g[r][c:c+2]+g[r+1][c:c+2])[1]:
-   p=v.index(0);r+=p>1;c+=p&1
-   while(c:=c-1+p%2*2)>-1<(r:=r-1+(p&2))<10>c:g[r][c]=s
+  if s:=sorted(v:=g[q:=r//9][(c:=r%9):c+2]+g[q+1][c:c+2])[1]:
+   p=v.index(0);q+=p>1;c+=p&1
+   while(c:=c-1+p%2*2)>-1<(q:=q-1+(p&2))<10>c:g[q][c]=s
  return g
