77c9532cdf48cbb8c5cfa277b894309c639bfc9b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-20 20:11:59 +0900

    タスク 030: 更新 (119 -> 116)

--- 030/3_submission.py
+++ 030/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- b=sum(g,[]);I=b.index;g=[[0]*10for _ in g];j=0
- for v in b:g[(j+I(1)-I(v or 1))//10][j%10]+=v;j+=1
+ b=sum(g,[]);I=b.index;j=0
+ for v in b:g[j//10][j%10]-=v;g[(j+I(1)-I(v or 1))//10][j%10]+=v;j+=1
  return g
