4fab25120cf6fd1837874333ecc4e62323f47484
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-20 20:11:58 +0900

    タスク 022: 更新 (104 -> 103)

--- 022/3_submission.py
+++ 022/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:[[max(s[i+d+e]for i in range(121)if (s:=sum(g,[]))[i]==5)for e in(-1,0,1)]for d in(-11,0,11)]
+p=lambda g,r=(-1,0,1):[[max(s[i+d*11+e]for i in range(121)if(s:=sum(g,[]))[i]==5)for e in r]for d in r]
