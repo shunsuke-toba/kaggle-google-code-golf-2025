c4a12e2f406a8484f55e60924d6e6720a876af42
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-21 18:49:59 +0900

    タスク 008: 更新 (111 -> 109)

--- 008/3_submission.py
+++ 008/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for _ in[0]*4:c,d=map([*map(max,g:=[*zip(*g[::-1])])].index,(8,2));g+=g[c+2:d];g[c+2:d]=[]
+ for m in[map]*4:c,d=m([*m(max,g:=[*zip(*g[::-1])])].index,(8,2));g+=g[c+2:d];g[c+2:d]=[]
  return g
