05fecd5511d2f4d3dc8d6a5c74de8f08759be567
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-21 18:50:09 +0900

    タスク 369: 更新 (168 -> 166)

--- 369/3_submission.py
+++ 369/3_submission.py
@@ -1,6 +1,6 @@
 def p(g,n=99):
  def f(r,c):
-  if r<10>c>=g[r][c]<1:q[r,c]=g[r][c]=4;f(r-1,c);f(r,c-1);f(r,c+1)
+  if 10>c>=g[r][c]<1:q[r,c]=g[r][c]=4;f(r-1,c);f(r,c-1);f(r,c+1)
  q={};f(n//10,n%10)
  for r,c in q:g[r][c]-=len(q)
  if n:p(g,n-1);return g
