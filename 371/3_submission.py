87aab7ea4dd9da0e248e0575261dd8748496416e
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-21 18:50:09 +0900

    タスク 371: 更新 (114 -> 109)

--- 371/3_submission.py
+++ 371/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- a=bytes(sum(g,[]));w=len(g[0])
- for i in-w,-1,0,1,w:i+=a.find(1)+a.rfind(1)>>1;g[i//w][i%w]=3
+ w=len(g[0]);f=sum(g,[]).index
+ for i in-w,-1,0,1,w:i+=f(1)+f(1,f(1)+1)>>1;g[i//w][i%w]=3
  return g
