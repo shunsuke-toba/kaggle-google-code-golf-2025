7b2fa86e59332d5cee33603a820a8404e6283ff9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-21 18:50:17 +0900

    タスク 076: 圧縮版を適用 (344 -> 342)

--- 076/3_submission.py
+++ 076/3_submission.py
@@ -1,6 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("""xÚuAnÃ E÷9«
-®JÚ1¾BŽm„d;vjÜÓw€$–,u…xfþÌoÚÝ°%â€Ù·#¶êC“ó$Uã~†kƒ§û€-Sš¼»±iþ$l!ú€º«G+Èh•
-V“HMdÞŒ¶ÅkÁÙJO	×€`×¡dI*S¼¬Ë¥²ÊhUë
-§qq8)9Øyv‘¿î†9ÏRy5¸_ˆL€‡r-|´°IòYòQzlû½-pŠ÷îáW‚ù°æ‹øÌ0,×¤¢°-Y‡è¶×™¾š.zãz¬Z)|­µ”íBËVûÌ^ä9ÿŸ>ñô¾št2‹}Þ19+!
-Ò!ÂôKA·óÝCŠ¡å""",'L1')))
+import zlib;exec(zlib.decompress(bytes("xÚu5šÃ0…{ŸBÕF2;ÓEæ˜ípÌÚÓïŒq)àƒ/Œböä‰0–:×èŽO«é°Þ«p ÌêÛ#äEuã‰{±ÌîaÔòÐ±WXüÈ™Ô[–ÝYašÜÓ}AÔ#–{÷$âÒØèRÛvØŸq‹¸EœÅ,u=×ØØ¾Ý¸	x{ð÷.ï*Q]ao°S®Îgöäj!¬ÒnÙÂ¼y-^ÚfOÕsªžtRÞK9IÃ GTëõ×ýþïIä<^j%æ“?ƒÞÎzÓëhÔÂpïzEÚ-ñ±a˜“€ÔðÛjþÞq…ý²¨oõÛ¡©ë¿É3Çf«MIû®å¡_jç]	¦ilPRö?ò6*«û)gø",'L1')))
--- 076/3_submission_uncompressed.py
+++ 076/3_submission_uncompressed.py
@@ -1,10 +1,10 @@
 def p(g):
- w=len(g[0]);s=[divmod(sum(g,[]).index(3),w)]
- for y,x in s:s+=[(a,b)for a in range(y-1,y+2)for b in range(x-1,x+2)if len(g)>a>-1<b<w>g[a][b]>((a,b)in s)<1]
+ h=len(g);w=len(g[0]);s=[divmod(sum(g,[]).index(3),w)]
+ for y,x in s:s+=[(a,b)for a in range(y-1,y+2)for b in range(x-1,x+2)if h>a>-1<b<w>g[a][b]>((a,b)in s)<1]
  r,c=zip(*s);t=[r[min(c):max(c)+1]for r in g[min(r):max(r)+1]]
- for _ in range(2):
-  for _ in range(4):
-   for y in range(len(g)-len(t)+1):
+ for _ in'00':
+  for _ in'0000':
+   for y in range(h-len(t)+1):
     for x in range(w-len(t[0])+1):
      if all(t[a][b]&1 or g[y+a][x+b]==t[a][b] for a in range(len(t))for b in range(len(t[0]))):
       for a in range(len(t)):g[y+a][x:x+len(t[0])]=t[a]
