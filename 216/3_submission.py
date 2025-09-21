4660a5f707c9f5865b124644e8fd9b7474e51795
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-21 18:50:06 +0900

    タスク 216: 更新 (146 -> 145)

--- 216/3_submission.py
+++ 216/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):f=lambda n:(s:=(g+[[]])[n//20][n%20:]+[0])[0]and[s[:s.index(0)]]+f(n+20)or[];return max(map(f,range(400)),key=lambda a:str(a).count('2'))
+def p(g):f=lambda n:(s:=(g+[[]])[n//20][n%20:]+[0])[0]and[s[:s.index(0)]]+f(n+20)or[];return f(max(range(400),key=lambda n:str(f(n)).count('2')))
