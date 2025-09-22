0f2058db505f55ab038574de2b784aa7da1e3307
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-22 21:21:35 +0900

    タスク 201: 圧縮版を適用 (275 -> 273)

--- 201/3_submission_uncompressed.py
+++ 201/3_submission_uncompressed.py
@@ -4,12 +4,11 @@ def p(g):
  a,b=divmod(h.find(4),w)
  c,d=divmod(h.rfind(4),w)
  o=[r[b:d+1]for r in g[a:c+1]]
- t=o[1][0]
  for r,v in enumerate(h):
   r,k=divmod(r,w)
   if r-a|c-r|k-b|d-k<0<v!=4:
    if r<s:s=r
-   if k<j:j=k;f=v!=t
+   if k<j:j=k;f=v!=o[1][0]
  for r in o[1:-1]:
   r[1:-1]=g.pop(s)[j:j+d-b-1][::1-2*f]
  return o
