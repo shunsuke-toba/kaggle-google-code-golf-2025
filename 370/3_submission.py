8d6fddb6c886612c0eff125b40c64a7e6d3e743a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-23 23:33:16 +0900

    タスク 370: 更新 (227 -> 224)

--- 370/3_submission.py
+++ 370/3_submission.py
@@ -1,7 +1,7 @@
 def p(g):
  w=len(g[k:=0]);Z=[]
- for v in sum(g,[]):Z+=[k]*(v<1);v*(v-g[0][0])and(p:=k,d:=v);k+=1
- q=max(Z,key=lambda z:abs(p//w-z//w)+abs(p%w-z%w))
+ for v in sum(g,[]):Z+=[k]*(1>v);v*(v-g[0][0])and(p:=k,d:=v);k+=1
+ q=max(Z,key=lambda z:abs(p-z)//w+abs(p%w-z%w))
  for z in Z:
   while w>(c:=z%w+p%w-q%w)>=0<=(z:=z+p-q)<k:g[z//w][c]=d
  return g
