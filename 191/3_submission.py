c1d4776ca391b45879da857c2ea0987cc2ab14d0
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-09-24 05:28:16 +0900

    191: 改善(295 -> 292)

--- 191/3_submission.py
+++ 191/3_submission.py
@@ -1,3 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("""xÚeNÑnÂ0|ç+òÂHÚvPÆSŠù‘ÈšØš´VVP·=ìÛç€Øödûî|wê û*AÚsï)†õSàåóäúx…uhLKq§ÓFÙÓíÁéÂõ›ðžT¯ˆUïhÎecãœ+s/—BâLeæ93Ior¶àÍ³M;]·B#pÖÑUÇ_”*‚„´bœÏÿ¢åô7ßR9 ’XW£³¶nÐì¹sþÃ¿ê…ðÍâ×C­i·2–êïïk
-õzKuÜÂ´Ë(-FpÅtqó0ùÓ9‰Õ·Ðl""",'L1')))
+import zlib;exec(zlib.decompress(bytes("xÚeNÑnÂ0|ç+òÂHÚvPÆSŠù‘ÈšØš´VVP·=ìÛç€Øödûî|wê û*AÚsï)†õSàåóäúx…uhLKq§ÓFÙÓíÁéÂõ›ðžT¯ˆUïhÎecãœ+s/—BâLeæ93Ior¶àÍ³M;]·B#pÖÑUÇ_”*‚„´bœÏÿ¢åô7ßR9 ’XW£³¶nÐì¹sþÃ¿ê…ðÍâ×C­i·2–êïïk\nõzKuÜÂ´Ë(-FpÅtqó0ùÓ9‰Õ·Ðl",'L1')))
