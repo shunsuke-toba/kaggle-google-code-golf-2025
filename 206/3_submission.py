82f7c4ca6327564e50bd3cbfeb38189e0d5a825a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-23 23:33:12 +0900

    タスク 206: 更新 (156 -> 153)

--- 206/3_submission.py
+++ 206/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- e=enumerate;a,b=map(min,*[(i,j)for i,r in e(g)for j,v in e(r)if v*(v-5or(s:=i,t:=j)*0)])
- for k in-1,0,1:g[s+k][t-1:t+2]=g[a-~k][b:b+3]
+ e=enumerate;h=[(i,j)for i,r in e(g)for j,v in e(r)if v*(v-5or(s:=i,t:=j)*0)];a,b=map(min,*h)
+ for i,j in h:g[i+s+~a][j+t+~b]=g[i][j]
  return g
