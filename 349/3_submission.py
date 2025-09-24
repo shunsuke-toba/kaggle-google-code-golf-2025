5df408f71d57edf97fc754255a6fe6b3ef3b2348
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-24 21:13:43 +0900

    タスク 349: 圧縮版を適用 (276 -> 274)

--- 349/3_submission.py
+++ 349/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("xÚ=ŽÁNÄ †ï}Š¹˜@mÝVoÔé‹ÍB+„6¸¨k|xgãíøf¾ß‡®êÐ¦ƒ‚ácËêýVyÖa¤Šî`/.	êFGPÔ“àwÕ6¼œNä´MœÈ­ü6ÎÎ&×ÇŸfç>É|¾Å ¾’ ¬“‰8/Uß†I?EòáKMCÒKE?¦u—Œi¬#-›xH¢Ü’VwiU¸3w¶{‡–]µþ†&Èw¼ªb7vU7œæ…l<ÏhÏÀä‹àgÛß‡ë †[«løúYx",'L1')))
+import zlib;exec(zlib.decompress(bytes("xÚEŽAnÃ E÷>Ål*+µºƒŽ/‚XX» 2D4ÔJÕÃwFªÚÝ‡÷Fÿ‡¸ÁMíÚP1~®E}Ü¿µ%,‘TÕlµÁA[iŠFÒ¦ºÁÝ%ƒ×ó™¼v™Óù…ÿ¦Ù»ìÇôÝ]æ<f¹€ã=•éDay1ég0ÿóÀ¼» 0<0LÙ6<–e¶óÔ&²«”’D9”‰™XyŽ7ž<:.>þð©‹ð•nªº•k›?í¼f½pSãƒ°ù*úEäþü\0-Þ{#¨?)þ]m",'L1')))
--- 349/3_submission_uncompressed.py
+++ 349/3_submission_uncompressed.py
@@ -2,7 +2,8 @@ def p(g):
  o=eval(str(g));n=len(o)
  for m in range(n*n):
   if(u:=g[i:=m//n])[j:=m%n]>g[i-1][j]*i|u[j-1]*j:
-   while i<n>g[i][j]>0:i+=1;d=(*u,0).index(0,j);r=d-j>>1;l=j-r-n;a=m//n-r-n
-   for y in o[i:]:y[j:d]=[1]*r*2
+   while i<n>g[i][j]>0:i+=1;d=j
+   while d<n>u[d]>0:d+=1;w=d-j;r=w>>1;l=j-r-n;a=m//n-r-n
+   for y in o[i:]:y[j:d]=[1]*w
    for y,u in zip(o[a:i+r],g[a:]):y[l:d+r]=[c or 3for c in u[l:d+r]]
  return o
