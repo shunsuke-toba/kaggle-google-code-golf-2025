211840b7475d574869f92afd6ad57be91972e2f1
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-25 21:48:23 +0900

    タスク 071: 更新 (110 -> 106)

--- 071/3_submission.py
+++ 071/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,a=0:[[v and(a or{t:=i-r[::-1].index(a:=v)})and(r[~i+t],v)[v==a] for i,v in enumerate(r)]for r in g]
+p=lambda g,a=0:[[v and(a or{t:=i-r[::-1].index(a:=v)})and(r[~i+t]and a)for i,v in enumerate(r)]for r in g]
