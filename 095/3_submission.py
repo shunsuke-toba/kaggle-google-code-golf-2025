d05f1b9eada3ced075669619307fce5ee086e27f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-25 21:48:24 +0900

    タスク 095: 更新 (106 -> 105)

--- 095/3_submission.py
+++ 095/3_submission.py
@@ -1,4 +1,1 @@
-def p(g):
- for z in range(81):
-  for x in(g[i:=z//9][j:=z%9]>4)*g[i-1:i+2]:x[j-1:j+2]=1,x[j]|1,1
- return g
+p=lambda g,e=enumerate:[[v|any(5in s[j-(j>0):j+2]for s in g[i-(i>0):i+2])for j,v in e(r)]for i,r in e(g)]
