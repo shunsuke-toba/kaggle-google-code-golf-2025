c352d762c438d2791df37311d13ea8c9a83acdfa
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-27 00:37:45 +0900

    タスク 277: 更新 (193 -> 192)

--- 277/3_submission.py
+++ 277/3_submission.py
@@ -1,1 +1,1 @@
-def p(g,r=range(9)):f=lambda i,j,d=8:0<=i<10>j>-1<g[i][j]==d and(g[i].__setitem__(j,-d%3)or-~sum(f(i+s//3-1,j+s%3-1,d)for s in r));f(*min((f(i,j)or 17,i,j)for i in r for j in r)[1:],1);return g
+p=lambda g,r=range(9):(f:=lambda i,j,d=8:0<=i<10>j>-1<g[i][j]==d and(g[i].__setitem__(j,-d%3)or-~sum(f(i+s//3-1,j+s%3-1,d)for s in r)))(*min((f(i,j)or 17,i,j)for i in r for j in r)[1:],1)and g
