b4438484df31f1a40a907d36f6df545e80403d21
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-27 00:37:41 +0900

    タスク 153: 更新 (134 -> 133)

--- 153/3_submission.py
+++ 153/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g:next(h for k in range(10000)if all(sum(h:=[[(s:=sum(g,[]))[k//100-u-v]^s[k%100-u-v]for v in(2,1,0)]for u in(20,10,0)],[])))
+p=lambda g:next(h for k in range(9999)if all(sum(h:=[[(s:=sum(g,[]))[k//100-u-v]^s[k%100-u-v]for v in(2,1,0)]for u in(20,10,0)],[])))
