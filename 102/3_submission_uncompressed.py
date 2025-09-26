7826c4dd61b070715e7bd63cb64337577e007b7d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-27 00:37:52 +0900

    タスク 102: 圧縮版を適用 (186 -> 185)

--- 102/3_submission_uncompressed.py
+++ 102/3_submission_uncompressed.py
@@ -2,6 +2,6 @@ def p(g):
  for s in range(11):
   for y in range(11):
    for x in range(11):
-    if not(sum(sum(r[x:x+s+2])for r in g[y:y+s+2])-20-20*s|sum(sum(r[x+1:x+s+1])for r in g[y+1:y+s+1])):
+    if not(sum(sum(r[x:x+s+2])for r in g[y:y+s+2])-20*s-20|sum(sum(r[x+1:x+s+1])for r in g[y+1:y+s+1])):
      for r in g[y+1:y+s+1]:r[x+1:x+s+1]=[2]*s
  return g
