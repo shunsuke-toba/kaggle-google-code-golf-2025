b52047e3568c286e936aeb62aac9d84cddf7dc6f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-27 00:37:52 +0900

    タスク 145: 圧縮版を適用 (237 -> 236)

--- 145/3_submission.py
+++ 145/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("xÚJIt9)ƒ@Ýç³Ô!fU’èEÄE(Ö–ÁBW={GÛ¦¡»ïÿòæ¸±ÈÇ²¶®R†/lÎk¢Ý ‘jös}ÙÞq-:ëÖýøÏ²f>e+•ZMŠP%È*Z¨OŠ7®¶¸¤¸²ýB(#þu©n‡,tß½©ÄÄ5ß1Y;´ÌÅ	ÂHÊËþ^–Irqh¶TšöôSqûáè^&ùTí",'L1')))
+import zlib;exec(zlib.decompress(bytes("xÚJIt9)ƒ@Ýç³Ô!fU’èEÄE(Ö–ÁBW={GÛ¦%»ïÿòæ¸±ÈÇ²¶®R†/lÎk¢Ý ‘jös}ÙÞq-:ëÖýøÏ²f>e+•ZMŠP%È*Z¨OŠ7®¶¸¤¸²ýB¨#ÆºT·?‡,tß½©ÄÄ5ß1Y;´ÌÅ	ÂHÊËþ^–Iº-•®=ýdÜ~:ºÛUÃ",'L1')))
