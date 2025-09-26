c2fda9d74d356ef731f6e534b84d2981ef817306
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-27 00:37:41 +0900

    タスク 134: 更新 (214 -> 213)

--- 134/3_submission.py
+++ 134/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  for d in(s:=sum(g,[])):
   (a,*_,c),y=zip(*(divmod(i,len(g[0]))for i,v in enumerate(s)if v==d));c-=a-1
-  if len({*_})==c<19:c//=3;return[[sum({*s},-d)*(v==d)for v in r[min(y)::c][:3]]for r in g[a::c][:3]]
+  if len({*_})==c<19:return[[sum({*s},-d)*(v==d)for v in r[min(y)::c//3][:3]]for r in g[a:a+c:c//3]]
