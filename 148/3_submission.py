7b1384050e23082bf3e4ce8a4283ccf93f9a813a
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-27 00:37:41 +0900

    タスク 148: 更新 (174 -> 169)

--- 148/3_submission.py
+++ 148/3_submission.py
@@ -1,5 +1,4 @@
 def p(g):
- p=[r for r in g if r[0]or r[-1]]
- for a,b in zip(p,p[len(p)//2:]):
-  if 8in a:a[j:=a.index(8)]=4;f=a[0]==0;b[:]=[8-r*3for r in b];a[f*j+1:-f|j]=b[1:j^-f]
+ for a,b in zip(p:=[r for r in g if r[0]+r[-1]],p[len(p)//2:]):
+  if 8in a:a[j:=a.index(8)]=4;f=a[0]<1;b[:]=[8-r*3for r in b];a[f*j+1:-f|j]=b[1:j^-f]
  return g
