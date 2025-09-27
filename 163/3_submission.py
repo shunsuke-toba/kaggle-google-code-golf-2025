7059d94f18feb3ed7a95ad53811fefae5140bc92
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-27 22:44:08 +0900

    タスク 163: 更新 (147 -> 144)

--- 163/3_submission.py
+++ 163/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):R=range(11);i,k=divmod(sum(g,[]).index(4),11);return[[5*(g[y][x]==5)or g[i&-4|y&3][k&-4|x&3]*(y>>2==i&3)*(x>>2==k&3)for x in R]for y in R]
+def p(g):R=range(11);i,k=divmod(sum(g,[]).index(4),11);return[[5*(g[y][x]==5)or g[i&-4|y&3][k&-4|x&3]*((y^i*4|x^k*4)&12<4)for x in R]for y in R]
