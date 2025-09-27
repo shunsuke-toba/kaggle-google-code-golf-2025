a7a26c195062cc19f1e465a944030ee64ec49df7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-27 22:45:21 +0900

    タスク 201: 圧縮版を適用 (273 -> 272)

--- 201/3_submission_uncompressed.py
+++ 201/3_submission_uncompressed.py
@@ -1,9 +1,5 @@
 def p(g):
- s=j=w=13
- h=bytes(sum(g,[]))
- a,b=divmod(h.find(4),w)
- c,d=divmod(h.rfind(4),w)
- o=[r[b:d+1]for r in g[a:c+1]]
+ s=j=w=13;h=bytes(sum(g,[]));a,b=divmod(h.find(4),w);c,d=divmod(h.rfind(4),w);o=[r[b:d+1]for r in g[a:c+1]]
  for r,v in enumerate(h):
   r,k=divmod(r,w)
   if r-a|c-r|k-b|d-k<0<v!=4:
