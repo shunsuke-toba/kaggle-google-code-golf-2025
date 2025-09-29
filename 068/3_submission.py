a66c380030049676a202c1c657a37c58fe0cd1c2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-09-29 22:37:59 +0900

    タスク 068: 更新 (119 -> 118)

--- 068/3_submission.py
+++ 068/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):g=sum(g,[]);i=g.index(min(g,key=g.count));return*zip(*[(7172>>abs(j-i)&2|(j==i)*g[i]for j in range(100))]*10),
+def p(g):m=min(g:=sum(g,[]),key=g.count);return*zip(*[(7172>>abs(j-g.index(m))&2|m*(g[j]==m)for j in range(100))]*10),
