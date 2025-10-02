1b17960a2796fe0b6022d96708d0c1598f27d696
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-02 19:21:30 +0900

    タスク 281: 更新 (211 -> 210)

--- 281/3_submission.py
+++ 281/3_submission.py
@@ -1,9 +1,9 @@
-p=lambda g:exec('''a=9;d=0;l=[];e=enumerate
+p=lambda g:exec('''a=d=0;l=[];e=enumerate
 for y,r in e(g):
  for x,p in e(r):
-  if p:l or(c:=x);a=min(a,b:=y);d=max(d,x);l+={p}-{8,*l}
-y=a
-while y<=b:
+  if p:l or(c:=x);a=a or~y;b=y;d=max(d,x);l+={p}-{8,*l}
+y=~a
+while b>=y:
  x=c
- while x<=d:g[y][x]=l[a<y<b>=c<x<d];x+=1
+ while d>=x:g[y][x]=l[~a<y<b>=c<x<d];x+=1
  y+=1''')or g
