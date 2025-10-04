6a330784d26e995d09130fad191f13e16b74d3e7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-04 20:58:13 +0900

    タスク 185: 更新 (154 -> 153)

--- 185/3_submission.py
+++ 185/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for z in({*g[0]},)*2:g=[c for c in zip(*g)if{*c}-z]
+ for z in[{*g[0]}]*2:g=[c for c in zip(*g)if{*c}-z]
  return[[a*(a==b==c==d!=max(z))for a,b,c,d in zip(B,B[1:],D,D[1:])]for B,D in zip(g,g[1:])]
