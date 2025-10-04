9d7df259bec207a9014f83f65fe9005e3d07de2b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-04 20:58:12 +0900

    タスク 081: 更新 (118 -> 117)

--- 081/3_submission.py
+++ 081/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for y in range(36):x=y%6;y//=6;a,b=g[y][x:x+2];c,d=g[y+1][x:x+2];g[y+(c!=d)][x+(b!=d)]+=a&d|b&c>7
+ for y in range(36):x=y%6;y//=6;a,b=g[y][x:x+2];c,d=g[y+1][x:x+2];g[y+(c!=d)][x+(b!=d)]+=a&d!=c&b
  return g
