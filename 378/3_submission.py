523deb8b9a4dec191fd0fc6bf3c19f8c508a98ee
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-04 20:58:14 +0900

    タスク 378: 更新 (201 -> 199)

--- 378/3_submission.py
+++ 378/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
  m=~-len(g);i=m*m
  while i:
-  i-=1;t=g[y:=i//m];d=g[y+1][z:=i%m]>0;d-=g[y-1][z]>0;e=t[z+1]>0;e-=t[z-1]>0
-  c=t[z]>g[a:=y+d][b:=z+e]
+  i-=1;t=g[y:=i//m];d=g[y+1][z:=i%m]>0;d-=g[y-1][z]>0;e=t[z+1]>0;e-=t[z-1]>0;c=t[z]>g[a:=y+d][b:=z+e]
   while c>0<y<m>z>0:g[y:=y-d][z:=z-e]=g[a+d][b+e]
  return g
