7ebe60a9e39619fbcd8cace78ad0782e0232c820
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-06 21:46:26 +0900

    タスク 027: 更新 (110 -> 106)

--- 027/3_submission.py
+++ 027/3_submission.py
@@ -1,4 +1,4 @@
-def p(g,o=2):
+def p(g,o=4):
  for t in range(99):
-  if g[r:=t//10][c:=t%10]:b=g[c];o>1<(o:=b[-r]);b[~r+o]=2-b[~r+o]
+  if g[r:=t//10][t%10]:b=g[t%10];o%=b[-r]+2;b[~r+o]=2-b[~r+o]
  return g
