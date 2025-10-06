2b95c697c7f88153c5cd5b76358a2c07c08948e7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-06 21:46:35 +0900

    タスク 090: 圧縮版を適用 (200 -> 196)

--- 090/3_submission.py
+++ 090/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes('xÚeNÅC!½3Enx]¿,R’ºûöu/Nò©K14d‚ÁtæŽBŠÒAÓENû¶ts×êu¯›!\nÛ²¿XÃFs ™ä¥òêÁ¿5Üµ±vó‰)Í/ú·jø©6sí{Ã¿“×wŠu·þ÷nD¤dðN=“¥¨š•6ƒ5mwë9Îí|E•','L1')))
+import zlib;exec(zlib.decompress(bytes('xÚeNS0ýï)òWÏÆEæ6Élß~öê&OÄ}Xª®èºà¢CGõY8*¥®çu¯›XAKmÝ_¬a\r£9š¡llëZZ›hèI¼!Â±ó«)Ï/Ò·j|W3.mïn=üa4Sí;‰þI·¦G­üÅ©<“Ö›…¶!kÞîÖ—þOßF"','L1')))
--- 090/3_submission_uncompressed.py
+++ 090/3_submission_uncompressed.py
@@ -1,4 +1,4 @@
 def p(g):
- d,e,c,d=max(((e:=g[a:a+b])and(max(max(r[c:c+d])for r in e)<1)*b*d,e,c,d)for a in range(len(g))for c in range(len(g[0]))for b in range(2,1+len(g)-a)for d in range(2,1+len(g[0])-c))
- for r in e:r[c:c+d]=d*[6]
+ _,a,b,c,d=max(((max(max(r[c:c+d])for r in g[a:a+b])<1)*b*d,a,b,c,d)for a in range(len(g))for b in range(2,1+len(g)-a)for c in range(len(g[0]))for d in range(2,1+len(g[0])-c))
+ for r in g[a:a+b]:r[c:c+d]=[6]*d
  return g
