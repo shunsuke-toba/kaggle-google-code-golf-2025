cdc38549b52ee9324aa3dc3605b5de1c9303801d
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-06 21:46:29 +0900

    タスク 200: 更新 (112 -> 111)

--- 200/3_submission.py
+++ 200/3_submission.py
@@ -1,7 +1,6 @@
 def p(g):
- j=g[k:=9].index(c:=max(g[k]))
- while j<10:
-  g[k:=~k][j+(j<9)]=5
+ j=g[k:=9].index(c:=max(g[k]))-2
+ while j<8:
+  j+=2;g[k:=~k][j+(j<9)]=5
   for r in g:r[j]=c
-  j+=2
  return g
