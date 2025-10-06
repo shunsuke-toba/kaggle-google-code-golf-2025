4226846008fb588c9836f2422beee677e5597e85
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-07 08:02:34 +0900

    タスク 034: 更新 (172 -> 171)

--- 034/3_submission.py
+++ 034/3_submission.py
@@ -2,5 +2,5 @@ def p(g):
  while 2in(f:=sum(g,[])):
   a=f.index(2);d,e=(f[a+9]<1)*18-9,f[a+1]<1or-1
   for s in a,a+d,a+e:
-   while 81>s>-1<s%9-e<9:g[s//9][s%9]=sum({*f})-2;s+=d+e
+   while 81>s>=s%9!=e&8:g[s//9][s%9]=sum({*f})-2;s+=d+e
  return g
