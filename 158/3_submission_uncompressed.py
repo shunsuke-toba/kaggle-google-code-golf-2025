667154cdabd54660f9834493844ce907232fa85b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-06 21:46:35 +0900

    タスク 158: 圧縮版を適用 (295 -> 293)

--- 158/3_submission_uncompressed.py
+++ 158/3_submission_uncompressed.py
@@ -1,10 +1,11 @@
 def p(g,t=0):
- while len({*(f:=sum(p:=[r[t//(len(g[0])+1):][:3]for r in g[t%(len(g[0])+1):][:3]],[]))})<4:t+=1
+ while len({*(f:=sum(p:=[r[t//-~len(g[0]):][:3]for r in g[t%-~len(g[0]):][:3]],[]))})<4:t+=1
+ c=f.count
  for _ in range(4):
   for m in 3,2,1:
    for x in range(-~len(g[0])-3*m):
     for y in range(-~len(g)-3*m):
-     if all((g[-1][0],p[i//m][j//m])[f.count(p[i//m][j//m])<2]==g[y+i][x+j]for i in range(3*m)for j in range(3*m)):
+     if all((g[-1][0],p[i//m][j//m])[c(p[i//m][j//m])<2]==g[y+i][x+j]for i in range(3*m)for j in range(3*m)):
       for i in range(3*m):g[y+i][x:x+3*m]=[p[i//m][j//m]for j in range(3*m)]
   p=[*zip(*p[::-1])]
  return g
