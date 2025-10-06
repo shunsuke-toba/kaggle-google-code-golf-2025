b6f8ef12057155d76ccec7d9eeb9968ca8bc3237
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-06 21:46:30 +0900

    タスク 234: 更新 (123 -> 122)

--- 234/3_submission.py
+++ 234/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for s in[{0}]*4:[sum(r)*2<max(r)*len(s:=s|{*r})and(g:=g+g[:1]).remove(r)for r in g];g=[*zip(*g[::-1])]
+ for s in[{0}]*4:[sum(r)*2<max(r)*len(s:=s|{*r})and(g:=g+g[:1]).remove(r)for r in g];*g,=zip(*g[::-1])
  return g
