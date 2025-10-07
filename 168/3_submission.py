9fdeb0985619b24b80ad941a5697700f85a32645
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-07 21:02:49 +0900

    タスク 168: 更新 (176 -> 175)

--- 168/3_submission.py
+++ 168/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
  for r in range(81):
-  c=r%9;r//=9;v=g[r][c:c+2]+g[r+1][c:c+2];p=v.index(0)
-  while-1<(r:=r-1+(p&2))<9>(c:=c-1+p%2*2)>-1<v.count(0)<2:g[r+p//2][c+p%2]=v[~p]
+  c=r%9;r//=9;v=g[r][c:c+2]+g[r+1][c:c+2];p=v.index(0);q=p%2
+  while-1<(r:=r+p+~q)<9>(c:=c-1+q*2)>-1<v.count(0)<2:g[r+p//2][c+q]=v[~p]
  return g
