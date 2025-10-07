0d349710078a1899b5984a9f5b3a06437a060522
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-07 21:02:57 +0900

    タスク 066: 圧縮版を適用 (259 -> 256)

--- 066/3_submission_uncompressed.py
+++ 066/3_submission_uncompressed.py
@@ -3,7 +3,7 @@ def p(g):
  def f(a,b,c,d,t,u=0):
   if n>(y:=a+c)>-1<(x:=b+d)<n:
    if g[y][x]==2:return g[y][x]
-   if g[y][x]^8 and f(y,x,c,d,t,u+1):g[y][x]=3;return g[y][x]
+   if g[y][x]^8 and f(y,x,c,d,t,1):g[y][x]=3;return g[y][x]
    if u and g[y][x]*t>7:return f(a,b,d,-c,t-1,u)or f(a,b,-d,c,t-1,u)
  f(a,b,a-c,b-d,2)or f(c,d,c-a,d-b,2)
  return g
