4a7f2365bd246171cf7951b5807e14cfd9b25599
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-07 21:02:58 +0900

    タスク 285: 圧縮版を適用 (276 -> 275)

--- 285/3_submission_uncompressed.py
+++ 285/3_submission_uncompressed.py
@@ -5,18 +5,18 @@ def p(g):
    if v:=o[y][x]:
     c=v;o[y][x]=0;q=[(y,x)]
     for y,x in q:
-     for a in -1,0,1:
-      for b in -1,0,1:
+     for b in -1,0,1:
+      for a in -1,0,1:
        if v:=o[(y+a)%n][(x+b)%n]:
         if v==c:o[(y+a)%n][(x+b)%n]=0;q+=((y+a)%n,(x+b)%n),
     for y,x in q:
-     for a in -1,0,1:
-      for b in -1,0,1:
+     for b in -1,0,1:
+      for a in -1,0,1:
        if v:=o[(y+a)%n][(x+b)%n]:
         if a*b==0:u,w=y,x
     for y,x in q:
-     for a in -1,0,1:
-      for b in -1,0,1:
+     for b in -1,0,1:
+      for a in -1,0,1:
        if v:=o[(u+a)%n][(w+b)%n]:g[(y,2*u+a-y)[a%2]][(x,2*w+b-x)[b%2]]=v
     o[u][w]=c
  return g
