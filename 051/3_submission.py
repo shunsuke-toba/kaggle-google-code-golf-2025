dc87add2f3164d11d23ac1caf9d0c86ef4a70145
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-08 20:31:40 +0900

    タスク 051: 更新 (140 -> 139)

--- 051/3_submission.py
+++ 051/3_submission.py
@@ -1,5 +1,5 @@
 p=lambda g:exec("""g[:]=map(list,zip(*g[::-1]))
 for r in g:
  i=u=t=0
- for a in r:t|=u*a|-(u==a>0);u=u or a;r[i]=a or u*(t>0);i+=1
+ for a in r:t|=u*a|-(u==a>0);u=u or a;r[i]=a or(t>0)*u;i+=1
 """*4)or g
