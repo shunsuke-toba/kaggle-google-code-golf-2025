a6422ecfcdc89c61554c124c847f5199d045fd71
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-08 20:55:52 +0900

    タスク 069: 更新 (156 -> 155)

--- 069/3_submission.py
+++ 069/3_submission.py
@@ -1,5 +1,5 @@
 def p(g):
- g=sum(g,[]);r=range(100);p=[i for i in r if g[i]%8];h=g[:]
+ g=sum(g,[]);r=range(100);p=[i for i in r if g[i]%8];h=g*1
  for i in r:
   for j in(h[i]==8)*p:h[i+j-p[0]]=g[j];h[j]=0
  return*zip(*[iter(h)]*10),
