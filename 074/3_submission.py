f4d5eac5a262716e2d4ef0c3697d5e4332b40adf
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-10-11 00:18:39 +0900

    074: 改善(104 -> 101)

--- 074/3_submission.py
+++ 074/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
- for k in range(9999):i=k%30;j=k%31%30;g[i][j]=g[j][i]%9|g[i][1-j]*(j>1)%9|g[i][j]%9
+ for k in range(1900):r[j]=(r:=g[i:=k%30])[j:=k%31%30]%9|g[j][i]%9|r[1-j]*(j>1)%9
  return g
