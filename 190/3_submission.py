338414ab877050126cfd31559ae22f9905bb216b
Shunsuke Toba <toba@third-inc.co.jp>
2025-10-10 18:51:11 +0900

    190(126 -> 123)

--- 190/3_submission.py
+++ 190/3_submission.py
@@ -1,3 +1,3 @@
-def p(g,b=1280):
- while b:b-=1;i=b%8;j=b//8%8;a=b>>6&2;c=b>>7&2;v=g[i+a];g[i+2-a][j+2-c]|=~v[j+1]&v[j+c]&g[i+1][j+1]
+def p(g,b=9**5):
+ while b:b-=1;i=b%9%8;j=b%8;a=b%5&2;c=b%7&2;v=g[i+a];g[i+2-a][j+2-c]|=~v[j+1]&v[j+c]&g[i+1][j+1]
  return g
