e34b58f67932211895730d2249ed9dd8c223d3e7
sak74y <yusei.sakurai@third-inc.co.jp>
2025-10-10 16:59:28 +0900

    090:圧縮

--- 090/3_submission.py
+++ 090/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes('xÚeNS0ýï)òWÏÆEæ6Élß~öê&OÄ}Xª®èºà¢CGõY8*¥®çu¯›XAKmÝ_¬a\r£9š¡llëZZ›hèI¼!Â±ó«)Ï/Ò·j|W3.mïn=üa4Sí;‰þI·¦G­üÅ©<“Ö›…¶!kÞîÖ—þOßF"','L1')))
+import zlib;exec(zlib.decompress(bytes('xÚeNS0ýï)òWÏÆEæ6Élß~öê&OÄ}Xª®.¸èÐQ}ŽJ©ëyÝë&VÐR[÷kXÃhƒf¨Ûº–Ö&zoˆpE¬Ã|ÀjÊó‹ô­ßÕŒKÛ{Ç‡[ÍTûN¢Ò­éQkq*Ï¤õf¡mHÀš·»õ¥Q“F$','L1')))
--- 090/3_submission_uncompressed.py
+++ 090/3_submission_uncompressed.py
@@ -1,4 +1,4 @@
 def p(g):
- _,a,b,c,d=max(((max(max(r[c:c+d])for r in g[a:a+b])<1)*b*d,a,b,c,d)for a in range(len(g))for b in range(2,1+len(g)-a)for c in range(len(g[0]))for d in range(2,1+len(g[0])-c))
+ a,a,b,c,d=max(((max(max(r[c:c+d])for r in g[a:a+b])<1)*b*d,a,b,c,d)for a in range(len(g))for b in range(2,1+len(g)-a)for c in range(len(g[0]))for d in range(2,1+len(g[0])-c))
  for r in g[a:a+b]:r[c:c+d]=[6]*d
  return g
