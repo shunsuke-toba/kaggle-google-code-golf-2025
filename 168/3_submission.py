e0838407d08fadad8832e7aa9a2186a5a13d8d7a
Shunsuke Toba <toba@third-inc.co.jp>
2025-10-10 20:36:21 +0900

    168: 175->174

--- 168/3_submission.py
+++ 168/3_submission.py
@@ -1,5 +1,5 @@
-def p(g):
- for r in range(81):
-  c=r%9;r//=9;v=g[r][c:c+2]+g[r+1][c:c+2];p=v.index(0);q=p%2
-  while-1<(r:=r+p+~q)<9>(c:=c-1+q*2)>-1<v.count(0)<2:g[r+p//2][c+q]=v[~p]
+def p(g,b=9**4):
+ while b:
+  b-=1;i,j,a,c=b%9,b//9%9,b>>6&1,b>>7&1;x,y=i+a,j+c
+  while 0<x<9>y>0:x+=2*a-1;y+=2*c-1;v=g[i+1-a];g[x][y]|=g[i+a][j+1-c]&v[j+1-c]&v[j+c]
  return g
