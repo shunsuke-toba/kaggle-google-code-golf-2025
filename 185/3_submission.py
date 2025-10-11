d6b923a2d4c6ed09bf0953af9aa50f6258c9a80f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-11 11:27:49 +0900

    タスク 185: 更新 (150 -> 149)

--- 185/3_submission.py
+++ 185/3_submission.py
@@ -1,3 +1,3 @@
 def p(g):
  for z in[{*g[0]}]*2:g=[c for c in zip(*g)if{*c}-z]
- return[[(len(s:={*q})<2)*sum(s-z)for q in zip(B,D,B[1:],D[1:])]for B,D in zip(g,g[1:])]
+ return[[(q==q[:1]*4)*sum({*q}-z)for q in zip(B,D,B[1:],D[1:])]for B,D in zip(g,g[1:])]
