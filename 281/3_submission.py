0a431052b4034727f66514a72d33c892b0832d91
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-12 02:15:34 +0900

    タスク 281: 更新 (207 -> 205)

--- 281/3_submission.py
+++ 281/3_submission.py
@@ -2,11 +2,10 @@ p=lambda g:exec('''a=d=y=0;l=[]
 for r in g:
  x=0
  for p in r:
-  if p:l or(c:=x);a=a or~y;b=y;d=max(d,x);l+={p}-{8,*l}
+  if p:l or(c:=x);a=a or~y;B=b=y;d=max(d,x);l+={p}-{8,*l}
   x+=1
  y+=1
-y=b
-while~a<=y:
+while~a<=b:
  x=c
- while x<=d:g[y][x]=l[~a<y<b>=c<x<d];x+=1
- y-=1''')or g
+ while x<=d:g[b][x]=l[~a<b<B>=c<x<d];x+=1
+ b-=1''')or g
