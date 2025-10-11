96a7647deed826e4f36031c8f113ec488a883623
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-12 02:15:33 +0900

    タスク 168: 更新 (174 -> 170)

--- 168/3_submission.py
+++ 168/3_submission.py
@@ -1,5 +1,5 @@
-def p(g,b=9**4):
+def p(g,b=324):
  while b:
-  b-=1;i,j,a,c=b%9,b//9%9,b>>6&1,b>>7&1;x,y=i+a,j+c
-  while 0<x<9>y>0:x+=2*a-1;y+=2*c-1;v=g[i+1-a];g[x][y]|=g[i+a][j+1-c]&v[j+1-c]&v[j+c]
+  b-=1;d=b>>2;a=b&1;c=b&2>0;i,j=d%9,d//9;x,y=i+a,j+c;v=g[i+1-a]
+  while 0<x<9>y>0:x+=2*a-1;y+=2*c-1;g[x][y]|=g[i+a][j+1-c]&v[j]&v[j+1]
  return g
