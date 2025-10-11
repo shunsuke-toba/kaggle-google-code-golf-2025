dc2b0c0addb02cfdcbbc8cb06f3e672f04e33ad4
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-12 02:15:34 +0900

    タスク 335: 更新 (121 -> 120)

--- 335/3_submission.py
+++ 335/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  s,e=map(sum(g,[]).index,(8,2));w=len(g[0])
- while(s:=s+(-1,1)[e>s]*w**(e//w!=s//w))-e:g[s//w][s%w]=4
+ while(s:=s+(1|-(s>e))*w**(e//w!=s//w))-e:g[s//w][s%w]=4
  return g
