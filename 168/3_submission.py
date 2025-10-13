d2f66eeae78d0820f4d6df1c464e400d789e00d2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-13 19:10:21 +0900

    タスク 168: 更新 (167 -> 165)

--- 168/3_submission.py
+++ 168/3_submission.py
@@ -1,5 +1,5 @@
 def p(g,b=324):
  while b:
-  b-=1;a=b&1;c=b&2>0;i=b//4%9;j=b//36;x,y=i+a,j+c;v=g[i+1-a]
-  while 0<x<9>y>0:x+=2*a-1;y+=2*c-1;g[x][y]|=g[i+a][j+1-c]&v[j]&v[j+1]
+  b-=1;a=b&1;c=b&2<1;x=i=b//4%9+a;j=b//36;y=j+1-c;v=g[i+1-2*a]
+  while 0<x<9>y>0:x+=2*a-1;y+=1-2*c;g[x][y]|=g[i][j+c]&v[j]&v[j+1]
  return g
