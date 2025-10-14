b63db9584ee0794cc1e9d187a163522ecab83ba9
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-14 22:42:48 +0900

    タスク 025: 更新 (181 -> 166)

--- 025/3_submission.py
+++ 025/3_submission.py
@@ -1,7 +1,4 @@
 def p(g):
- x=o=0;t=*zip(*g),
- for k in t:
-  if c:=min(k):
-   for r,q in zip(g,o:=o or[[0]*len(r)for r in g]):q[x]=q[x+(c in r[x+1:])-(c in r[:x])]=c
-  x+=1
- return o or[*zip(*p(t))]
+ t=*zip(*g),
+ m=0,*map(min,t),0
+ return any(m)and[[m[x+1]or m[x]*(m[x]in r[x:])or m[x+2]*(m[x+2]in r[:x+1])for x in range(len(r))]for r in g]or[*zip(*p(t))]
