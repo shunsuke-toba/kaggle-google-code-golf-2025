daf29ae2b89898f19665160b10801564838146ff
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-14 22:42:59 +0900

    タスク 118: 圧縮版を適用 (264 -> 263)

--- 118/3_submission.py
+++ 118/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes('xÚeOUÂ0~çyL¶wèEp©Ì·?>ÁeÞß"ÚX(ÐÑ°[eNë÷‡cÅª9Ú©Ý:s¦ç L4ØŠHÄ*]_÷ÇQvE]´øå›¦Ûièëù4šÏ}Ô°Î4ÔÇ*òõ81ºi}N7²âçædóh3Ø¡ŒEì7ˆøïy×â‡‹ö†/x/ÏÖ;=ØwK"ž<´°·†;s8î2pÌ”{Ý6„÷\nµšêŒÜùê-x­Ñkä·éŽØôØt`eá','L1')))
+import zlib;exec(zlib.decompress(bytes('xÚeOUÂ0~çyL¶wèEp©Ì·?>ÁeÞß"ÚX(ÐÑ°[eNë÷‡cÅª9Ú©Ý:s¦ç L4ØŠHÄ*]_÷ÇQvE]´øå›¦Ûièëù4šÏ}Ô°Î4ÔÇ*òõ81n§õ9ÝÈrˆ#œ›“Íw !Ì`‡2±ß âÿ½ç]‹Z,Úw¾à½<[ïlô`ß-‰xòÐÂfÜîÌá¸ËÀ1SîuÛÞ+Ôjª3r/ä«·àµF¯‘ß¦;bÓK`ÓteÕ','L1')))
