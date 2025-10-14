f5092002c5f5a3f7473626b0b57af056b18b1c70
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-14 22:42:54 +0900

    タスク 282: 更新 (112 -> 109)

--- 282/3_submission.py
+++ 282/3_submission.py
@@ -1,1 +1,4 @@
-p=lambda g:[g[(m:=n+~9+k//6)//9].__setitem__(m%9,k%6)for n,v in enumerate(sum(g,[]))for k in b'<7Cqs}'*v]*0+g
+def p(g):
+ for n,v in enumerate(sum(g,[])):
+  for k in b'<7Cqs}'*v: m=n-10+k//6;g[m//9][m%9]=k%6
+ return g
