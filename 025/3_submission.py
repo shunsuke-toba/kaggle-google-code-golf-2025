cf2425afec5ff9c522f28ce5ceb9cd8409bc160f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-15 23:30:03 +0900

    タスク 025: 更新 (166 -> 160)

--- 025/3_submission.py
+++ 025/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  t=*zip(*g),
  m=0,*map(min,t),0
- return any(m)and[[m[x+1]or m[x]*(m[x]in r[x:])or m[x+2]*(m[x+2]in r[:x+1])for x in range(len(r))]for r in g]or[*zip(*p(t))]
+ return any(m)and[[m[x+1]+(m[x]in r[x:])*m[x]+(m[x+2]in r[:x])*m[x+2]for x in range(len(r))]for r in g]or[*zip(*p(t))]
