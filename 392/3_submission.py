45d459bb0d8cd4bef69ad6ebad592634210b6c80
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-15 23:30:07 +0900

    タスク 392: 更新 (192 -> 186)

--- 392/3_submission.py
+++ 392/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,R=range(10):[o for r in R for c in R for s in(2,3)if(o:=[[[5,max(a:=sum(g,[]))][max(i-r+2-s,r-i,j-c+2-s,c-j)%s&1]for j in R]for i in R])if[y*(x>0)for x,y in zip(a,sum(o,[]))]==a][0]
+p=lambda g,R=range(10):[o for s in(2,3)for r in R for c in R if(o:=[[[5,max(a:=sum(g,[]))][max(i-r+2-s,r-i,j-c+2-s,c-j)%s&1]for j in R]for i in R])if(max(a),5)not in zip(a,sum(o,[]))][0]
