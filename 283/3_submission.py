e2098ea4f27734c6e90c6888a08ab71c961f6719
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-15 23:30:06 +0900

    タスク 283: 更新 (106 -> 105)

--- 283/3_submission.py
+++ 283/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,z=[[0]*10]:[[a*sum(n)%22%5for a,*n in zip(r,r[1:]+[0],[0]+r,*m)]for r,*m in zip(g,z+g,g[1:]+z)]
+p=lambda g,z=[[0]*10]:[[a*sum(n)%22%5for*n,a in zip(r[1:]+[0],[0]+r,*m,r)]for r,*m in zip(g,z+g,g[1:]+z)]
