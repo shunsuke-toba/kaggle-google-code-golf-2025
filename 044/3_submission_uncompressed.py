c8797760bbb404e176268dd268070e0617cc30c7
sak74y <yusei.sakurai@third-inc.co.jp>
2025-10-17 16:54:15 +0900

    044:圧縮改善(294->292)

--- 044/3_submission_uncompressed.py
+++ 044/3_submission_uncompressed.py
@@ -1,16 +1,16 @@
-def p(g):
- r=sum(g,[]);g=r[:];o={};e={}
- for i,v in enumerate(g):
+def p(n):
+ o=sum(n,[]);n=o[:];f={};e={}
+ for i,v in enumerate(n):
   if v-5:
-   q=[i];g[i]=5;f=1>v;t=0,
+   q=[i];n[i]=5;u=1>v;t=0,
    for j in q:
     for k in j-1,j+1,j-10,j+10:
-     if g[k:k+1]==[v]:g[k]=5;q+=k,;t+=k-i,
-     f*=g[k:k+1]==[5]
-   if f:o[t]=i
-   if r.count(v)==len(t)>1:e[t]=i,v
+     if n[k:k+1]==[v]:n[k]=5;q+=k,;t+=k-i,
+     u*=n[k:k+1]==[5]
+   if u:f[t]=i
+   if o.count(v)==len(t)>1:e[t]=i,v
  for t in e:
-  if t in o:
+  if t in f:
    i,v=e[t]
-   for k in t:r[i+k],r[o[t]+k]=0,v
- return*zip(*[iter(r)]*10),
+   for k in t:o[i+k],o[f[t]+k]=0,v
+ return*zip(*[iter(o)]*10),
