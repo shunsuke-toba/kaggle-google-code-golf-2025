5b680bbe259620ddf7ea8ce10d665defcbf0dfc5
sak74y <yusei.sakurai@third-inc.co.jp>
2025-10-17 17:16:44 +0900

    096:圧縮改善

--- 096/3_submission_uncompressed.py
+++ 096/3_submission_uncompressed.py
@@ -1,17 +1,17 @@
-def p(g):
- import re;f=sum(g,[]);C=f.count;b=max(f,key=C);L=[]
- for R in{*f}-{b}:
-  s=str(R);c=1|2*(C(R)>1);y=c,R,c
-  for n in range(112):w=n//28+2;d=n%7+1;y=any(re.search(s*w+f"[^{s}]"*d+s,str(R)[1::3])for R in g)and(2*w+d,R,w)or y;g=[*map(list,zip(*g[::-1]))]
-  L+=y,
- m=max(L)[0];g=[[b]*m for _ in range(m)]
- for z,R,w in L:
-  for k in range(w):g[i:=m-z>>1][i+k]=g[i+k][i]=R
-  g=[*map(list,zip(*g[::-1]))]
-  for k in range(w):g[i:=m-z>>1][i+k]=g[i+k][i]=R
-  g=[*map(list,zip(*g[::-1]))]
-  for k in range(w):g[i:=m-z>>1][i+k]=g[i+k][i]=R
-  g=[*map(list,zip(*g[::-1]))]
-  for k in range(w):g[i:=m-z>>1][i+k]=g[i+k][i]=R
-  g=[*map(list,zip(*g[::-1]))]
- return g
+def p(m):
+ import re;i=sum(m,[]);e=i.count;b=max(i,key=e);u=[]
+ for r in{*i}-{b}:
+  i=str(r);l=1|2*(e(r)>1);y=l,r,l
+  for k in range(112):o=k//28+2;t=k%7+1;y=any(re.search(i*o+f"[^{i}]"*t+i,str(r)[1::3])for r in m)and(2*o+t,r,o)or y;m=[*map(list,zip(*m[::-1]))]
+  u+=y,
+ c=max(u)[0];m=[[b]*c for _ in range(c)]
+ for s,r,o in u:
+  for k in range(o):m[i:=c-s>>1][i+k]=m[i+k][i]=r
+  m=[*map(list,zip(*m[::-1]))]
+  for k in range(o):m[i:=c-s>>1][i+k]=m[i+k][i]=r
+  m=[*map(list,zip(*m[::-1]))]
+  for k in range(o):m[i:=c-s>>1][i+k]=m[i+k][i]=r
+  m=[*map(list,zip(*m[::-1]))]
+  for k in range(o):m[i:=c-s>>1][i+k]=m[i+k][i]=r
+  m=[*map(list,zip(*m[::-1]))]
+ return m
