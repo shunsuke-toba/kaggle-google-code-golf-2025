e0c08ae6e13e61c8a6304234765ae9251a8ad637
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-10-18 03:52:34 +0900

    168: 正規表現解(157 -> 127)

--- 168/3_submission.py
+++ 168/3_submission.py
@@ -1,5 +1,2 @@
-def p(g,b=324):
- while b:
-  b-=1;e=(b&2)-1;d=b%2*2-1;x=b//4%9;k=b//36+(e>0);v=g[x-d];v=g[x][k-e]&v[k-e]&v[k]
-  while 0<x<9>k>0:g[x:=x+d][k:=k+e]+=v
- return g
+import re
+p=lambda g,k=28:k and p(eval(re.sub(r'0((.{34}[^0])*.{37}([^0]).{28}\3, \3)',r'\3\1',str([*zip(*g[::-1])]))),k-1)or g
