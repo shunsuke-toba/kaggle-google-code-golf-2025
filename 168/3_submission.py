abd3efeb2e125f273ed863de03f7a2177f0eeee9
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-10-18 05:47:32 +0900

    168: 改善(127 -> 120)

--- 168/3_submission.py
+++ 168/3_submission.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=28:k and p(eval(re.sub(r'0((.{34}[^0])*.{37}([^0]).{28}\3, \3)',r'\3\1',str([*zip(*g[::-1])]))),k-1)or g
+p=lambda g,k=28:k and p(eval(re.sub(r'0((.{35})+, ([^0]).{28}\3, \3)',r'\3\1',str([*zip(*g[::-1])]))),k-1)or g
