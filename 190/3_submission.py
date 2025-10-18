43f313be2fe1d56d048ff1b2860ff6c9ae3baee6
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-18 23:13:42 +0900

    タスク 190: 更新 (114 -> 112)

--- 190/3_submission.py
+++ 190/3_submission.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=20:k and p(eval(re.sub(r'0(.{25}0, ([^0]).{28}\2)',r'\2\1',str([*zip(*g[::-1])]))),k-1)or g
+p=lambda g,k=20:k and p(eval(re.sub(r'0(?=.{25}0, (\d).{28}\1)',r'\1',str([*zip(*g[::-1])]))),k-1)or g
