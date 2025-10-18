3e29770ccbde07eb1cd37589c6f8467d8829c387
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-18 23:13:43 +0900

    タスク 250: 更新 (147 -> 146)

--- 250/3_submission.py
+++ 250/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- a=sum(g,[]);r,d=divmod(a.index(2)-8,10);i=j=0;I=r
- for v in a:g[i][j]-=v;g[I][min(max(j,d-3),d)]|=v;i+=j>8;j=-~j%10;I+=i>I<r+3
+ a=sum(g,[]);k=a.index(2)-8;I=r=k//10;i=j=0
+ for v in a:g[i][j]-=v;g[I][min(max(j,k%10-3),k%10)]|=v;i+=j>8;j=-~j%10;I+=i>I<r+3
  return g
