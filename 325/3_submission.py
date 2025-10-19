166428ac0670202a7e36879a576ab867097112b3
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-19 23:27:42 +0900

    タスク 325: 更新 (155 -> 154)

--- 325/3_submission.py
+++ 325/3_submission.py
@@ -1,7 +1,7 @@
-def p(g,o=[]):
+def p(g,o=()):
  while g:
   r,*g=g
   while 8in r:
-   o=[R+[0]for R in o]+[[0]*len(o)+[8]];x=r.index(8)
+   o+=o,;x=r.index(8)
    for R in r,*g[:3]:R[x-2*(x>1):x+4]=[0]*6
- return o
+ return[[8*(i==j)for j in o]for i in o]
