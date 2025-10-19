e5f13b3ac6bd72eb3f371e492a2e4b25793024bd
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-19 23:27:49 +0900

    タスク 066: 圧縮版を適用 (253 -> 252)

--- 066/3_submission.py
+++ 066/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes("xÚmNEv€@ÝsŠ¬ú&%SWì õÑz(yp~œúî¿ï>DøP5f	pùx„¹2$HÊ‘ÇòZj0ÖžÄðcP<Íwâ9B}n¯›ÛÃÛ¦Ú8õÐØB-uåÁ4£+²Ò¤+}X¨&+%õXð$ÃWMYeÚNxc~êw`ØCœþ­#‡˜máãüßl7e6n·­Î·‰å«'í¨Õ‡Ôa-+§=m\\²RftÉÈ-®iÜiC^ËH%°-H¹gE",'L1')))
+import zlib;exec(zlib.decompress(bytes('xÚmNEv€@ÝsŠ¬ú&%SWì õÑz(yp~œúî¿ï>DøP5f	pùx„¹2$HÊ‘ÇòZj0ÖžÄðcP<Íwâ9B}n¯›ÛÃÛ¦Ú8õÐØB-uåÁ4£+²Ò¤+}X¨&+%õXð$ÃWMYeÚNxc~êú{ˆÓ¿uä³-|œÿ›í¦ÌÆí¶Õù6±|õ¤µú:¬eå´§KVÊŒ.ù£Å5;mÈk©¶å)·g','L1')))
--- 066/3_submission_uncompressed.py
+++ 066/3_submission_uncompressed.py
@@ -3,7 +3,7 @@ def p(o):
  def f(a,r,c,d,t,u=0):
   if n>(e:=a+c)>-1<(q:=r+d)<n:
    if o[e][q]==2:return o[e][q]
-   if o[e][q]^8 and f(e,q,c,d,t,1):o[e][q]=3;return o[e][q]
+   if o[e][q]-8 and f(e,q,c,d,t,1):o[e][q]=3;return o[e][q]
    if u and o[e][q]*t>7:return f(a,r,d,-c,t-1,u)or f(a,r,-d,c,t-1,u)
  f(a,r,a-c,r-d,2)or f(c,d,c-a,d-r,2)
  return o
