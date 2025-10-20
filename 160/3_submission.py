c7d7c9f4697bd5c1fae490bffec75fa9488e8047
Shunsuke Toba <toba@third-inc.co.jp>
2025-10-20 14:17:14 +0900

    160 正規表現 121 -> 122 伸びてるけど短縮を期待して

--- 160/3_submission.py
+++ 160/3_submission.py
@@ -1,4 +1,2 @@
-def p(g,z=63):
- a,b,c,*_=g[z>>3:]
- if min(a[s:=slice(z%8,z%8+3)]+c[s]):a[s]=c[s]=0,2,0;b[s]=2,2,2
- if z:p(g,z-1);return g
+import re
+p=lambda g,k=4:k and p(eval(re.sub(r'(1.{6})(.{25})1.{5}1(.{25})\1',r'0,2,0\2(2),2,2\3(0),2,0',str(g))),k-1)or g
