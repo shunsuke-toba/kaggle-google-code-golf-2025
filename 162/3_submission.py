cba453439c0c4c4d31a8b234ace0e471f059146c
sak74y <yusei.sakurai@third-inc.co.jp>
2025-10-20 16:13:51 +0900

    162:改善(120->118)

--- 162/3_submission.py
+++ 162/3_submission.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=4:k and p(eval(re.sub(r'(0, 0, 0)(.{55})\1(.{55})\1',r'1,1,1\2(1),1,1\3(1),1,1',str(g))),k-1)or g
+p=lambda g,k=4:k and p(eval(re.sub(r'(0, 0, 0)(.{55})\1(.{55})\1',r'1,1,1\2 1,1,1\3 1,1,1',str(g))),k-1)or g
