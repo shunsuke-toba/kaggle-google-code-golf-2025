2c97e128d67578948b521542738fdd9bba149e82
Shunsuke Toba <toba@third-inc.co.jp>
2025-10-20 14:20:21 +0900

    378 169 -> 168

--- 378/3_submission.py
+++ 378/3_submission.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=36:k and p(eval(re.sub('0((.{%d})*.{%d}([^0]), \\3.{%d}0, \\3.{%d}([^0]))'%((n:=len(g)*3)-1,n-5,n-2,n-5),'\\4\\1',str([*zip(*g[::-1])]))),k-1)or g
+p=lambda g,k=36:k and p(eval(re.sub('0((.{%d})*.{%d}([^0]), \\3.{%d}0, \\3.{%d}([^0]))'%((n:=len(g)*3)-1,n-5,n-2,n-5),r'\4\1',str([*zip(*g[::-1])]))),k-1)or g
