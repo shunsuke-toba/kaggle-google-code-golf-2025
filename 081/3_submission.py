461ea43245057bc50a2ead85606f1b9b3306dfc0
sak74y <yusei.sakurai@third-inc.co.jp>
2025-10-20 15:50:36 +0900

    081:肯定後読みで更新(99->98)

--- 081/3_submission.py
+++ 081/3_submission.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=8:k and p(eval(re.sub('0(, 8.{19}8)',r'1\1',str([*zip(*g[::-1])]))),k-1)or g
+p=lambda g,k=8:k and p(eval(re.sub('0(?=, 8.{19}8)','1',str([*zip(*g[::-1])]))),k-1)or g
