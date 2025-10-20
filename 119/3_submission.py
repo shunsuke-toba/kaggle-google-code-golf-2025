ada7c358f086262dd54ff74d01892834fec0a580
Shunsuke Toba <toba@third-inc.co.jp>
2025-10-20 12:56:51 +0900

    119 正規表現 151 -> 117

--- 119/3_submission.py
+++ 119/3_submission.py
@@ -1,5 +1,2 @@
-def p(g):
- for _ in g:
-  g=*map(list,zip(*g[::-1])),;b=p=g[6][0]==2
-  for h in p*g[::8in g[0]or-1]:h[p:=p-b or h.index(8)]|=3>>h[p];b-=h[p-1]
- return g
+import re
+p=lambda g,k=40:k and p(eval(re.sub('0((.{35})*.{34}[38].{34}[28])',r'3\1',str([*zip(*g[::-1])]))),k-1)or g
