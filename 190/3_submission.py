26781c4a6b63c142ccf619b283194ca1a7c58027
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-10-21 05:40:08 +0900

    190: 改善(111 -> 109)

--- 190/3_submission.py
+++ 190/3_submission.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=20:k and p(eval(re.sub(r'0(?=.{25}0, (.).{28}\1)',r'\1',str([*zip(*g[::-1])]))),k-1)or g
+p=lambda g,k=19:-k*g or p(eval(re.sub(r'0(?=.{25}0, (.).{28}\1)',r'\1',str([*zip(*g[::-1])]))),k-1)
