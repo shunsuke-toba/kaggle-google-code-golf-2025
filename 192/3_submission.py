2e111ea2a969f3856698c7f4710763fed4f8a6d0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-22 20:10:32 +0900

    タスク 192: 更新 (127 -> 115)

--- 192/3_submission.py
+++ 192/3_submission.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=23:-k*g or p(eval(re.sub(rf'(\d), [^0](?=, \1|{", 0|"*(k<12)}\))',r'\1, \1',str([*zip(*g[::-1])]))),k-1)
+p=lambda g,k=23:-k*g or p(eval(re.sub(r'(\d), [^0](?=, \1|, 0|\))',r'\1, \1',str([*zip(*g[::-1])]))),k-1)
