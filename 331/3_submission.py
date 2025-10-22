0de6d5f1c00402cc4dd1797c7a66eea8c4194074
sak74y <yusei.sakurai@third-inc.co.jp>
2025-10-22 18:12:36 +0900

    331:更新(96->89)

--- 331/3_submission.py
+++ 331/3_submission.py
@@ -1,1 +1,1 @@
-import re;p=lambda g,k=3:-k*g or p(eval(re.sub('0(?=, 1)',"7268"[k],str([*zip(*g[::-1])]))),k-1)
+p=lambda g,k=3:-k*g or p(eval(str([*zip(*g[::-1])]).replace('0, 1','7268'[k]+", 1")),k-1)
