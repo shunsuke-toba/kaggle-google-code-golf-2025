181169001c853b51b51b7339c70c5065942525db
Shunsuke Toba <toba@third-inc.co.jp>
2025-10-22 15:36:21 +0900

    034 正規表現 171 -> 155

--- 034/3_submission.py
+++ 034/3_submission.py
@@ -1,6 +1,1 @@
-def p(g):
- while 2in(f:=sum(g,[])):
-  a=f.index(2);d,e=(f[a+9]<1)*18-9,f[a+1]<1or-1
-  for s in a,a+d,a+e:
-   while 81>s>=s%9!=e&8:g[s//9][s%9]=sum({*f})-2;s+=d+e
- return g
+import re;p=lambda g,k=3:-k*g or p(eval(re.sub('(?=(((0.{31})*(0, ){1,3}.{23})?0, )?2, [^0].{25}[^0])[02](?=.*([13-9]))',r'\5',str([*zip(*g[::-1])]))),k-1)
