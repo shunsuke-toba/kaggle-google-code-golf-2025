7527f197d2c2940524caa594c3a40faacd5a22d0
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-22 20:10:28 +0900

    タスク 048: 更新 (136 -> 134)

--- 048/3_submission.py
+++ 048/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- for r in g:r+=0,;w=len(r);f=sum(g,[])+[0]*w;q=[f.index(2)]
- for i in q:q+=f[i]*[i+1,i-1,i+w,i-w];f[i]=0
+ f=sum(((r+[0]*9)[:9]for r in g),[])+[0]*9;q=[f.index(2)]
+ for i in q:q+=f[i]*[i+1,i-1,i+9,i-9];f[i]=0
  return[8-8*(2in f)],
