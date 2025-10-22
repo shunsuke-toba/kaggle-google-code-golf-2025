73193c1a5524b54051692b17fd562944641420c7
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-22 20:10:29 +0900

    タスク 093: 更新 (104 -> 102)

--- 093/3_submission.py
+++ 093/3_submission.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=11:-k*g or p(eval(re.sub('5((, 0)*), [^05]','5,5\\1',str([*zip(*g[::-1])]))),k-1)
+p=lambda g,k=11:-k*g or p(eval(re.sub('(5(, 0)*), [^05]','5,\\1',f'{*zip(*g[::-1]),}')),k-1)
