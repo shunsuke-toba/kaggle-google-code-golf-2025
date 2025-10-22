05b9bfa112213195eaf1e9242cf535ef929dcfaa
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-22 20:10:30 +0900

    タスク 151: 更新 (103 -> 102)

--- 151/3_submission.py
+++ 151/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- e,k=[s.index(max(s))for s in(g,g[0])]
- for t in b'*+,02678':g[e+t//6-8][k+t%3-1]=4
+ e,k=(s.index(max(s))for s in(g,g[0]))
+ for t in b'3%,-;<CJ':g[e+t%7-3][k+t%3-1]=4
  return g
