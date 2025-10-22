a7028450b1bc06ccb087b09e9f6d29853b6b2b5b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-22 20:10:35 +0900

    タスク 381: 更新 (89 -> 88)

--- 381/3_submission.py
+++ 381/3_submission.py
@@ -1,5 +1,1 @@
-def p(g):
- for r in g[1:9]:
-  k=p=0
-  while 2in r[k:]:p=r[k]=r[k]or(p>1)*9;k+=1
- return g
+p=lambda g:0*[exec('k=p=0\nwhile 2in r[k:]:p=r[k]=r[k]or(p>1)*9;k+=1')for r in g[1:9]]+g
