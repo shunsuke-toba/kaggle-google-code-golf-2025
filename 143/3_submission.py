750c3407cf37ee2794868d9729b6eb3307d13f70
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-22 20:10:30 +0900

    タスク 143: 更新 (162 -> 160)

--- 143/3_submission.py
+++ 143/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  t=sum(g,[]);s=[i for i in range(30)if(i%10<3)*t[i]];n=1
  while{v:=t[n+i]for i in s}-{v+len(s)%t.count(v)}:n+=1
- return eval(str(g).replace(str(v),"5"))
+ return eval(str(g).replace(*f'{v}5'))
