4dc8040418eacf921cf00fc5f41fc7ee28f9f73f
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-23 21:09:27 +0900

    タスク 160: 更新 (117 -> 116)

--- 160/3_submission.py
+++ 160/3_submission.py
@@ -1,3 +1,3 @@
 import re
-f=lambda x:eval(re.sub(r'(1.+1)(.{25})1.{5}1(.{25})\1',r'0,2,0\2+2,2,2\3+0,2,0',str(x)))
+f=lambda x:eval(re.sub(r'(1.+)(.{25})1.{5}1(.{25})\1',r'0,2,0\2 2,2,2\3 0,2,0',str(x)))
 p=lambda g:f(f(g))
