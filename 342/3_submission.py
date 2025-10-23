197be665857c09223a28a6d47a6a264b1dd56e70
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-23 21:09:32 +0900

    タスク 342: 更新 (126 -> 124)

--- 342/3_submission.py
+++ 342/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  a=sum(g,[]);k=a.index(8);j=0;c=10
- for v in a:g[j//c][j%c]-=v;g[k//c+(j>k)][k%c+(j%c>k%c)]+=v*(v!=8);j+=1
+ for v in a:g[j//c][j%c]-=v;g[k//c+(j>k)][k%c+(j%c>k%c)]+=v&v%~7;j+=1
  return g
