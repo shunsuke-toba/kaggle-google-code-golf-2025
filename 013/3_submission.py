f84225fe823904f122343fbf6061d7214ccadb0b
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-24 19:24:03 +0900

    タスク 013: 更新 (150 -> 144)

--- 013/3_submission.py
+++ 013/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
  *z,=zip(*g)
  if g[12:]:return*zip(*p(z)),
- b=[*map(sum,z)];y,Y=(i for i,v in enumerate(b)if v);b[y:]=b[y:2*Y-y]*8;return[b[:len(z)]]*len(g)
+ *b,=map(sum,z);y,Y=map(b.index,filter(int,b));b[y:]=b[y:2*Y-y]*8;return[b[:len(z)]]*len(g)
