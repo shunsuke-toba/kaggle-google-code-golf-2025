97d3facdacc9ecf83458868fd7ea1280018486d2
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-26 00:13:55 +0900

    タスク 182: 圧縮版を適用 (202 -> 201)

--- 182/3_submission.py
+++ 182/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes('xÚŽÃ0…È¯x0’UuÂý’Ær¸ÍéØÏ_Z£40àÝç{Ï=2ãe‹•$y¼œ­*1<M>Eøä“ã§‚Qz—’íÕ;TÆQK«–wm¾¹8‡jGãÜÚ‰¨ÅÚEäA¿6ª]—*QdüùæVÚåŽ‰æå­‚òäVÊ','L1')))
+import zlib;exec(zlib.decompress(bytes('xÚŽÃ0†ÈSü0’UuÂ=Ic¹8¸ÍéØã¯íPZp>ßwwÊxù’ƒÖÏä3÷—³×D-×Ž‡!–lŸb	ü40DawmÕ_cÆÞ*‹U7k¼…<¦•Úþú~œãBi¹9Ï3ÇEÉa¥v †eÆO®ÍæJÚ¤V§·)ÚÈÅVj','L1')))
--- 182/3_submission_uncompressed.py
+++ 182/3_submission_uncompressed.py
@@ -1,7 +1,7 @@
 def p(g):
- i=next([f-0*(n:=g[e+f//5][r+f%5])for f in range(25) if g[e+f//5][r+f%5]]for e in range(16)[1:]for r in range(16)[1:] if g[e-1][r]==g[e][r-1]==5)
+ i=next([f-0*(n:=g[e+f//5][r+f%5])for f in range(25)if g[e+f//5][r+f%5]]for e in range(16)[1:]for r in range(16)[1:]if g[e-1][r]==5==g[e][r-1])
  for e in range(16):
   for r in range(16):
-   if i==[f for f in range(25) if g[e+f//5][r+f%5]]:
+   if i==[f for f in range(25)if g[e+f//5][r+f%5]]:
     for f in i:g[e+f//5][r+f%5]=n
  return g
