a8ca13a395d7071e52c70b63fbdb29b13636e711
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-26 00:13:56 +0900

    タスク 349: 圧縮版を適用 (252 -> 245)

--- 349/3_submission.py
+++ 349/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes('xÚ]ŽÁJÅ0E÷~Å,ûžvQÜ¥¦?2Ìb`-ÄQ‚%&øñNª”ÒÝ{î	‘ás(7÷\0ÑcBGñ#C‚U¡Ð¬>5jÐÚÚÛÌúíl¾`%ò•ÂaC¦ÅFãD–îõÇ\nËwÞÐ|ÅóžËÛš´V‚­®=úé„åo(äÄØœ½Œ¼,£ãïë˜G5=“KÈû!vx|çï!==ßŽíÓõ™ævßÉaNg³srøÚ²Í¢eP','L1')))
+import zlib;exec(zlib.decompress(bytes("xÚ]ŽÁjÇ Æï{ŠÛnÊnvöEÄC ê\n.²±){ø´ ¥'“ï÷ý‚ä|MqÖOŒMV»ð™!ÁÁÝÆ&y*PÒÒÒŒýÄMè!ÞC8D[œE·Ë«Ö6-åÿÌd]°× š²‘Á>ÿ¾É½1 Ó©“ÓôlÖ®×vSWÁ[6¤pßÏÞø{°EeÅRÉN'‹}!YŒýÀ¿)½¼Î£{Åî~¦êîkæz5'ûïŸ,õê½eÅ",'L1')))
--- 349/3_submission_uncompressed.py
+++ 349/3_submission_uncompressed.py
@@ -1,12 +1,11 @@
-def p(w):
- f=[l[:]for l in w];n=len(w)
+def p(g):
+ f=[l[:]for l in g];n=len(g)
  for y in range(n):
-  u=w[y]
   for a in range(n):
-   if u[a]>w[y-1][a]*y|u[a-1]*a:
+   if g[y][a]>g[y-1][a]*y|g[y][a-1]*a:
     z=y;d=a
-    while z<n and w[z][a]:z+=1
-    while d<n and u[d]:d+=1;r=d-a>>1
+    while d<n and g[y][d]:d+=1
+    while z<n and g[z][a]:z+=1;r=d-a>>1
     for l in f[y-r-n:z+r]:l[a-r-n:d+r]=[max(l,3)for l in l[a-r-n:d+r]]
     for l in f[z:]:l[a:d]=[max(l,1)for l in l[a:d]]
  return f
