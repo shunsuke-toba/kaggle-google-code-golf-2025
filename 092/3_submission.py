b85961b4c41c0538a7957f92cfae26449cb5f432
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-26 00:13:47 +0900

    タスク 092: 更新 (97 -> 95)

--- 092/3_submission.py
+++ 092/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,h=0:[[c or sum({*r[:i]}&{*r[i:]})for i,c in enumerate(r)]for r in zip(*(h or p(g,g)))]
+p=lambda g,h=0:[[c or sum({*r[:i]}&{*r[i:]})for i,c in enumerate(r)]for r in zip(*h or p(g,g))]
