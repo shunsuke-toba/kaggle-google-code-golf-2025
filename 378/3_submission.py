40d5a64ffa9b63580f1a2e09fdafc0c86ef91afa
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-26 23:02:43 +0900

    タスク 378: 更新 (159 -> 158)

--- 378/3_submission.py
+++ 378/3_submission.py
@@ -1,1 +1,1 @@
-import re;p=lambda g,k=8:k and p(eval(re.sub(r'0(?=(.{%d})*.{%d}(, [^0])\2.{%d}0\2.{%d}(.))'%(n:=len(g)*3-1,n-6,n-1,n-4),r'\3',str([*zip(*g[::-1])]))),k-1)or g
+import re;p=lambda g,k=8:k and p(eval(re.sub(r'0(?=(.{%d}.)*.{%d}(, [^0])\2.{%d}0\2.{%d}(.))'%(n:=len(g)*3-2,n-5,n,n-3),r'\3',str([*zip(*g[::-1])]))),k-1)or g
