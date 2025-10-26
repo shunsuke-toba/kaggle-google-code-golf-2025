a85e012a9a3788718c6d71010953865de14030f4
Yui Yoshihara <yui.yoshihara@third-inc.co.jp>
2025-10-27 04:40:15 +0900

    再帰構造の変更

--- 378/3_submission.py
+++ 378/3_submission.py
@@ -1,1 +1,1 @@
-import re;p=lambda g,k=8:k and p(eval(re.sub(r'0(?=(.{%d}.)*.{%d}(, [^0])\2.{%d}0\2.{%d}(.))'%(n:=len(g)*3-2,n-5,n,n-3),r'\3',str([*zip(*g[::-1])]))),k-1)or g
+import re;p=lambda g,k=7:-k*g or[*zip(*eval(re.sub(r'0(?=(.{%d}.)*.{%d}(, [^0])\2.{%d}0\2.{%d}(.))'%(n:=len(g)*3-2,n-5,n,n-3),r'\3',str(p(g,k-1)[::-1]))))]
