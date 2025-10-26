e43f896d76346016a4975b2f75919c1e49668cd8
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-26 23:02:48 +0900

    タスク 284: 圧縮版を適用 (233 -> 232)

--- 284/3_submission.py
+++ 284/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes('xÚµ±NÀ @÷~Å@AÝ0ôG.7˜x—0-)_/ãäê@à=Â>Eá4%Jn_R?n±a“°[4ŠÍFš ¥BÂ12ˆ);æ…Õ&…ÌïWl¾ÇËƒ“×‚*w«™ÈU~ÎÌ@O§q§ùCËÖò%v¡ßíBÉ¿†´¿q˜K&\r1ïâ.ŽCìàXI½LÁ4´æ a8ú‘ì®Õoèÿï‚õm(ß%Fnþ','L1')))
+import zlib;exec(zlib.decompress(bytes('xÚµ±NÀ @÷~Å@AÝ0ôG.7˜x—0-)_/ãäê@à=Â>Eá4%Jn_R?n±a“°[4ŠÍFš ¥BÂ12ˆ);æ…Õ&…ÌïWl¾ÇËƒ“×‚*w«™ÈU~ÎÌ@O§q§ùCËÖò%6ô£ô»_(ù×ö7sÉ¤!æ]ÜÅqˆý+©—)˜Æ‹Ö4G?’Ýµ.èøýõm(ß˜nþ','L1')))
--- 284/3_submission_uncompressed.py
+++ 284/3_submission_uncompressed.py
@@ -2,8 +2,8 @@ def p(o,e=enumerate):
  (i,z),(f,u)=[(i,z)for i,r in e(o)for z,n in e(r)if n];q=u-z>>1
  if i-f:
   return[[*r]for r in zip(*p([[*r]for r in zip(*o)]))]
- n,e=z,1
- for r in o[i-2:i+3]:o[i-2][f:=n+e*q]=o[i+2][f]=r[f-e]=o[i][n];o[i][n:f:e]=[o[i][n]]*q
  n,e=u,-1
+ for r in o[i-2:i+3]:o[i-2][f:=n+e*q]=o[i+2][f]=r[f-e]=o[i][n];o[i][n:f:e]=[o[i][n]]*q
+ n,e=z,1
  for r in o[i-2:i+3]:o[i-2][f:=n+e*q]=o[i+2][f]=r[f-e]=o[i][n];o[i][n:f:e]=[o[i][n]]*q
  return o
