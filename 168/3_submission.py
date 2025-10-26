35f202a1ff975fd80063271e3f09320aeec5ce60
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-26 23:02:41 +0900

    タスク 168: 更新 (118 -> 117)

--- 168/3_submission.py
+++ 168/3_submission.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=27:-k*g or p(eval(re.sub(r'0((.{35})+, ([^0]).{28}\3, \3)',r'\3\1',str([*zip(*g[::-1])]))),k-1)
+p=lambda g,k=3:-k*g or p(eval(re.sub(r'0(?=(.{35})+, ([^0]).{28}\2, \2)',r'\2',str([*zip(*g[::-1])]))),k-1)
