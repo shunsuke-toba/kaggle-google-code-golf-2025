d623b4400897b1175b682769b88da13cfc3de38d
Shunsuke Toba <toba@third-inc.co.jp>
2025-10-28 03:39:01 +0900

    246 128 -> 120

--- 246/3_submission.py
+++ 246/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- s,e=map(sum(g,[]).index,(2,3));w=len(g[0])
- while(s:=s+((e%w>s%w)-(e%w<s%w)or(w,-w)[e<s]))-e:g[s//w][s%w]=8
+ s,e=map(sum(g,[]).index,(3,2));w=len(g[0])
+ while(s:=s+(1|-(s>e))*w**(e//w!=s//w))-e:g[s//w][s%w]=8
  return g
