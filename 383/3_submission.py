5389337043a53207c1b5c0b2430917cdf2a8b772
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-28 12:17:02 +0900

    タスク 383: 更新 (122 -> 121)

--- 383/3_submission.py
+++ 383/3_submission.py
@@ -1,1 +1,1 @@
-def p(g):_,o,p={}.fromkeys(sum(g,[]));return[[[(p,o)[x>0],x][r.count(o)^3>1<c.count(o)^3]for x,*c in zip(r,*g)]for r in g]
+def p(g):_,o,p={}.fromkeys(sum(g,[]));return[[[(p,o)[x>_],x][r.count(o)^3>1<c.count(o)^3]for*c,x in zip(*g,r)]for r in g]
