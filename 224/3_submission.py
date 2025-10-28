fef8543f0b00fb246e36e6e3f5749876b9d815f5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-28 12:17:00 +0900

    タスク 224: 更新 (182 -> 181)

--- 224/3_submission.py
+++ 224/3_submission.py
@@ -1,4 +1,4 @@
 def p(g):
- e=enumerate;(u,*_,a),v,w=zip(*((i,b,c)for i,r in e(g)for b,c in e(r)if c));c=min(v)+1;v=max(v)
- for r in g[u+1:a]:r[c]=r[v-1]=k=w[3]
- g[u+1][c:v]=r[c:v]=[k]*(v-c);return g
+ e=enumerate;(u,*_,a),v,w=zip(*((i,b,c)for i,r in e(g)for b,c in e(r)if c));m=max(v)
+ for r in g[u+1:a]:r[c:=min(v)+1]=r[m-1]=k=w[3]
+ g[u+1][c:m]=r[c:m]=[k]*(m-c);return g
