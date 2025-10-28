9067669d10e6e2145ca6b0cac0aa073b084a3eea
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-28 12:17:00 +0900

    タスク 134: 更新 (183 -> 177)

--- 134/3_submission.py
+++ 134/3_submission.py
@@ -1,3 +1,3 @@
 def p(g,f=filter):
- for b in{*sum(g,[])}:
-  if len({*(h:=[*zip(*f(any,zip(*f(any,[[d*(d!=b)for d in r]for r in g]))))])})<4:k=len(h)//3;return[[b*(c>0)for c in r[::k]]for r in h[::k]]
+ for b in sum(g,[]):
+  if len({*(h:=[*zip(*f(any,zip(*f(any,[[0<d!=b for d in r]for r in g]))))])})<4:k=len(h)//3;return[[c*b for c in r[::k]]for r in h[::k]]
