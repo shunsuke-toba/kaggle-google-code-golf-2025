0a9ba0268ce2228204160138f53f56242de03d5c
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-29 21:55:27 +0900

    タスク 099: 更新 (130 -> 129)

--- 099/3_submission.py
+++ 099/3_submission.py
@@ -1,1 +1,1 @@
-import re;p=lambda g,k=15:-k*g or p(eval(re.sub('0(?=, ([2-9]))'+'(?=.{31}0)'*(k>11)+'x'*(k==3),r'\1',str([*zip(*g)][::-1]))),k-1)
+import re;p=lambda g,k=15:-k*g or p(eval(re.sub('0(?=, ([2-9]))'+'(?=.{31}0)'*(k>11)+'x'[k^3:],r'\1',str([*zip(*g)][::-1]))),k-1)
