d45dcaf04f5f6d48a5e639f4c8e95a7f231c8e6e
pech1 <pechi2327@gmail.com>
2025-10-29 21:07:18 +0900

    102:4×4までしかないことを利用して簡略化

--- 102/3_submission.py
+++ 102/3_submission.py
@@ -1,1 +1,1 @@
-p=lambda g,t=95,e=8:-t*g or p([[((int((b:=(v.bit_count()))**0.5)**2==b<20)*2+(v&5),v|(v!=5)*(n&~5)|(v<1)*(e:=e*2))[t>0]for v,n in zip(R,(0,*R))]for R in zip(*g[::-1])],t-1)
+p=lambda g,t=95,e=8:-t*g or p([[((v.bit_count()in{1,4,9,16})*2+(v&5),v|(v!=5)*(n&~5)|(v<1)*(e:=e*2))[t>0]for v,n in zip(R,(0,*R))]for R in zip(*g[::-1])],t-1)
