4e53e6c05aebd49e572d4671869b186763671ef5
Shuichi Tamayose <tmshuichi@gmail.com>
2025-10-30 19:58:24 +0900

    タスク 018: 圧縮版を適用 (356 -> 354)

--- 018/3_submission.py
+++ 018/3_submission.py
@@ -1,2 +1,2 @@
 #coding:L1
-import zlib;exec(zlib.decompress(bytes('xÚuPezÄ ýŸSÔ‹Õ-½õ¡FX"Ô{÷2a]ãOfÞ{»È•ÅŠUÚ¤k¿^iW^üJ}çŸ€8ð	E¸Cü¢÷MW‚ÚÇ’Ü˜Ôò‘-øAÇËŒrÌð#[I+/v¿þ°&–äS*í´—¥Ù?èV:cªþ(»sŽ”w$¶øäš}¿Â>iZîêX"*\'Tdë -WQàÛ”c0ç€QÉ—áäV"²¬,æBžè´jB¹²¸Q~°²lY¯H‰:0"iÚ:MØ}èGßv/Vª¨ÝÞí6!å½<¤Ì*•ÒðüÙsfVèÐ{e£²Sl‚{ƒR£µ”;†šž»>Âfvkx‡º’×ÐÆ:Õù;œ±R','L1')))
+import zlib;exec(zlib.decompress(bytes('xÚ…PezÄ ýŸSÔ‹Ô½¤ô"|Sj„%B½w_fÝ7ž1ï	íJ`\\e+VHÏVµÒ¬¼ú•êÞ?#sèJpñˆ9€.¸ô½ˆä†¤Fît-ä!ñzŒbÈð=Y\rI+¯v…VEö¥´qÆ«ö¦Q`‡Tÿ\\Ü;ÇŠûÀ{çKñó˜øâÀùõ‰Šú+šì"Ï±ûAº¬›”•:îÌØ}à*åH)ÛKT»E¢Y•M‰.º#ÌœÙ¯<Ü\r]Ù¼–‰‚#åÐÀ¬nª”°÷ØŠ¾éüX¥£qûûÆm"p~uÄ…Õš¦è.{IÀäœy×{y\0DºWs4Jí‡ÜÝS3{~`U#KWØÄÊ¯<¶ç¶','L1')))
--- 018/3_submission_uncompressed.py
+++ 018/3_submission_uncompressed.py
@@ -5,15 +5,15 @@ def p(c):
    p=0
    for l in range(t+1,len(c)+1):
     for m in range(n+1,len(c[0])+1):
-     if len({*sum(x:=[l[n:m]for l in c[t:l]],[0])})*all(map(sum,x+[*zip(*x)]))>4:p,e,u=l,m,x
+     if len({*sum(x:=[l[n:m]for l in c[t:l]],[0])})*all(map(sum,x+[*zip(*x)]))>4:u=x;p=len(u);e=len(u[0])
    if p:f+=u,
-   for l in c[t:p]:l[n:e]=[0]*len(u[0])
+   for l in range(p):c[t+l][n:n+e]=[0]*e
  for u in f:
   for l in range(8):
    p=len(u);e=len(u[0])
    for t in range(len(c)+1-p):
     for n in range(len(c[0])+1-e):
      if all((str(u).count(str(f:=u[l//e][l%e]))<2)*f==c[t+l//e][n+l%e]for l in range(p*e)):
-      for l in range(p):c[t+l][n:e+n]=u[l]
+      for l in range(p):c[t+l][n:n+e]=u[l]
    u=[*zip(*u[::-1])];l^3or u.reverse()
  return c
