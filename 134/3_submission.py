998eba53b478a9f389563f0eb7ff9828c6cbbd9d
pech1 <pechi2327@gmail.com>
2025-10-30 22:46:50 +0900

    134:空白の除去

--- 134/3_submission.py
+++ 134/3_submission.py
@@ -1,3 +1,3 @@
 def p(g,f=filter):
  for b in sum(g,[]):
-  if 0<len({*(h:=[*f(any,zip(*f(any,[[b*(0<d!=b) for d in r]for r in g])))])})<4:k=len(h)//3;return[*zip(*h[::k])][::k]
+  if 0<len({*(h:=[*f(any,zip(*f(any,[[b*(0<d!=b)for d in r]for r in g])))])})<4:k=len(h)//3;return[*zip(*h[::k])][::k]
