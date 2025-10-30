b2a884b06830acd63348abd836b2a1cc6e130032
sak74y <yusei.sakurai@third-inc.co.jp>
2025-10-30 19:25:05 +0900

    027:r=10はサボってよいことを思い出す(106->103)

--- 027/3_submission.py
+++ 027/3_submission.py
@@ -1,4 +1,4 @@
 def p(g,o=4):
- for t in range(99):
-  if g[r:=t//10][t%10]:b=g[t%10];o%=b[-r]+2;b[~r+o]=2-b[~r+o]
+ for t in range(89):
+  if g[r:=t//9][t%9]:b=g[t%9];o%=b[-r]+2;b[~r+o]=2-b[~r+o]
  return g
